b3b5a992a8010945bde4e2ea2ab5147658ccf20e
Yoni K <yoni.kid15@gmail.com>
2021-06-08 16:59:28 -0400

    Remove all files/components related to react-admin

--- admin/resources/js/components/Dashboard.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-import { Card, CardContent, CardHeader } from "@material-ui/core";
-
-export default (): React.ReactElement => (
-  <Card>
-    <CardHeader title="GC Digital Talent" />
-    <CardContent>GC Digital Talent</CardContent>
-  </Card>
-);
