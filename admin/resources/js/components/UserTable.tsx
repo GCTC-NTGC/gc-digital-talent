cc77005c1086ad8a1d231ae41a4b00013c00849d
Matt Nigh <matt@mattnigh.com>
2021-06-09 15:41:24 -0400

    Fix formatting and syntax

--- admin/resources/js/components/UserTable.tsx
+++ admin/resources/js/components/UserTable.tsx
@@ -1,48 +1,50 @@
-import * as React from 'react';
-import { useTable, useGlobalFilter, useSortBy } from 'react-table'
-import GlobalFilter from './GlobalFilter'
+import React, { useMemo } from "react";
+import { useTable, useGlobalFilter, useSortBy, Column } from "react-table";
+import GlobalFilter from "./GlobalFilter";
 
 interface UserTableProps {
-  users: any
+  users: Array<{
+    id: number;
+    firstName: string;
+    lastName: string;
+    email: string;
+    telephone: string;
+    preferredLang: string;
+  }>;
 }
 
-const UserTable : React.FC<UserTableProps> = (
-  users,
-) => {
-  const columns = React.useMemo(
+const UserTable: React.FC<UserTableProps> = (users) => {
+  const columns: Array<Column> = useMemo(
     () => [
       {
-        Header: 'ID',
-        accessor: 'id',
+        Header: "ID",
+        accessor: "id",
       },
       {
-        Header: 'First Name',
-        accessor: 'firstName',
+        Header: "First Name",
+        accessor: "firstName",
       },
       {
-        Header: 'Last Name',
-        accessor: 'lastName',
+        Header: "Last Name",
+        accessor: "lastName",
       },
       {
-        Header: 'Email',
-        accessor: 'email',
+        Header: "Email",
+        accessor: "email",
       },
       {
-        Header: 'Telephone',
-        accessor: 'telephone',
+        Header: "Telephone",
+        accessor: "telephone",
       },
       {
-        Header: 'Preferred Language',
-        accessor: 'preferredLang',
+        Header: "Preferred Language",
+        accessor: "preferredLang",
       },
     ],
-    []
-  )
+    [],
+  );
 
-  const data = React.useMemo(() =>
-    users,
-    []
-  )
+  const data = useMemo(() => users, [users]);
 
   const {
     getTableProps,
@@ -54,15 +56,13 @@ const UserTable : React.FC<UserTableProps> = (
     visibleColumns,
     preGlobalFilteredRows,
     setGlobalFilter,
-  } = useTable({ columns, data: data.users }, useGlobalFilter, useSortBy)
+  } = useTable({ columns, data: data.users }, useGlobalFilter, useSortBy);
 
   return (
     <table {...getTableProps()}>
       <thead>
         <tr>
-          <th
-            colSpan={visibleColumns.length}
-          >
+          <th colSpan={visibleColumns.length}>
             <GlobalFilter
               preGlobalFilteredRows={preGlobalFilteredRows}
               globalFilter={state.globalFilter}
@@ -70,37 +70,43 @@ const UserTable : React.FC<UserTableProps> = (
             />
           </th>
         </tr>
-        {headerGroups.map(headerGroup => (
-          <tr {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map(column => (
-              <th {...column.getHeaderProps(column.getSortByToggleProps())}>
-                {column.render('Header')}
+        {headerGroups.map((headerGroup) => (
+          <tr
+            {...headerGroup.getHeaderGroupProps()}
+            key={`header-group-${headerGroup.id}`}
+          >
+            {headerGroup.headers.map((column) => (
+              <th
+                {...column.getHeaderProps(column.getSortByToggleProps())}
+                key={column.id}
+              >
+                {column.render("Header")}
                 <span>
-                  {column.isSorted ? (column.isSortedDesc ? ' ▼' : ' ▲') : ''}
-              </span>
+                  {column.isSorted && (column.isSortedDesc ? " ▼" : " ▲")}
+                </span>
               </th>
             ))}
           </tr>
         ))}
       </thead>
       <tbody {...getTableBodyProps()}>
-        {rows.map(row => {
-          prepareRow(row)
+        {rows.map((row) => {
+          prepareRow(row);
           return (
-            <tr {...row.getRowProps()}>
-              {row.cells.map(cell => {
+            <tr {...row.getRowProps()} key={`row-${row.id}`}>
+              {row.cells.map((cell) => {
                 return (
-                  <td {...cell.getCellProps()}>
-                    {cell.render('Cell')}
+                  <td {...cell.getCellProps()} key={`cell-${cell.column.id}`}>
+                    {cell.render("Cell")}
                   </td>
-                )
+                );
               })}
             </tr>
-          )
+          );
         })}
       </tbody>
     </table>
-  )
-}
+  );
+};
 
 export default UserTable;
--- admin/resources/js/stories/Users.stories.tsx
+++ admin/resources/js/stories/Users.stories.tsx
@@ -1,11 +1,8 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import UserTable from '../components/UserTable'
-import { fakeUsers } from '../fakeData/fakeUsers'
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import UserTable from "../components/UserTable";
+import { fakeUsers } from "../fakeData/fakeUsers";
 
-storiesOf('Users', module)
-.add('User Table', () =>
-    <UserTable users={fakeUsers} />
-)
-
-
+storiesOf("Users", module).add("User Table", () => (
+  <UserTable users={fakeUsers} />
+));
