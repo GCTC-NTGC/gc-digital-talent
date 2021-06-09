b5f3256817c1e454ceb519f055e19c8ae5dee707
Matt Nigh <matt@mattnigh.com>
2021-06-09 15:45:33 -0400

    Fix formatting, syntax

--- admin/resources/js/stories/Users.stories.tsx
+++ admin/resources/js/stories/Users.stories.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
 import UserTable from "../components/UserTable";
-import { fakeUsers } from "../fakeData/fakeUsers";
+import fakeUsers from "../fakeData/fakeUsers";
 
 storiesOf("Users", module).add("User Table", () => (
   <UserTable users={fakeUsers} />
