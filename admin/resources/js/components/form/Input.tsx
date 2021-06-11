c8ab2db394be5c473a95bc63f10bb0393fd69aa3
tristan-orourke <tristan.m.orourke@gmail.com>
2021-06-11 16:12:02 -0400

    Create alternate input components which use react-hook-form's uncontrolled inputs The performance benefit must be minimal, but I did observe fewer component updates, and this seems to be the library's recommended approach.

--- admin/resources/js/components/form/Input.tsx
+++ admin/resources/js/components/form/Input.tsx
@@ -1,46 +1,37 @@
 import * as React from "react";
-import { Control, useController } from "react-hook-form";
+import { RegisterOptions, useFormContext } from "react-hook-form";
 
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
-  /** Object that connects controlled components to react hook form. */
-  control: Control<any>;
   /** The text for label associated with select input. */
   label: string;
   /** A string specifying a name for the input control. */
   name: string;
   /** Object set of validation rules to impose on input. */
-  rules?: { [key: string]: any };
-  /** List of validation messages that correspond with the rules list. */
-  ruleMessages?: { [key: string]: string };
+  rules?: RegisterOptions;
 }
 
 const Input: React.FunctionComponent<InputProps> = ({
-  control,
   label,
   name,
   rules = {},
-  ruleMessages,
   type,
   ...rest
 }) => {
-  const { field, fieldState } = useController({
-    control,
-    name,
-    rules,
-  });
-
+  const {
+    register,
+    formState: { errors },
+  } = useFormContext();
+  const error = errors[name]?.message;
   return (
     <div>
       <label htmlFor={name}>{label}</label>
       <input
-        {...field}
+        {...register(name, rules)}
         type={type}
-        aria-invalid={fieldState.error ? "true" : "false"}
+        aria-invalid={error ? "true" : "false"}
         {...rest}
       />
-      {fieldState.error?.type && ruleMessages && (
-        <span role="alert">{ruleMessages[fieldState.error?.type]}</span>
-      )}
+      {error && <span role="alert">{error}</span>}
     </div>
   );
 };
--- admin/resources/js/components/form/Select.tsx
+++ admin/resources/js/components/form/Select.tsx
@@ -1,9 +1,7 @@
 import * as React from "react";
-import { Control, useController } from "react-hook-form";
+import { RegisterOptions, useFormContext } from "react-hook-form";
 
 interface SelectProps extends React.SelectHTMLAttributes<HTMLSelectElement> {
-  /** Object that connects controlled components to react hook form. */
-  control: Control<any>;
   /** The text for label associated with select input. */
   label: string;
   /** A string specifying a name for the input control. */
@@ -11,31 +9,27 @@ interface SelectProps extends React.SelectHTMLAttributes<HTMLSelectElement> {
   /** List of options for the select element. */
   options: { value: string | number; text: string }[];
   /** Object set of validation rules to impose on input. */
-  rules?: { [key: string]: any };
-  /** List of validation messages that correspond with the rules list. */
-  ruleMessages?: { [key: string]: string };
+  rules?: RegisterOptions;
 }
 
 const Select: React.FunctionComponent<SelectProps> = ({
-  control,
   label,
   name,
   options,
   rules,
-  ruleMessages,
   ...rest
 }) => {
-  const { field, fieldState } = useController({
-    control,
-    name,
-    rules,
-  });
+  const {
+    register,
+    formState: { errors },
+  } = useFormContext();
+  const error = errors[name]?.message;
   return (
     <div>
       <label htmlFor={name}>{label}</label>
       <select
-        {...field}
-        aria-invalid={fieldState.error ? "true" : "false"}
+        {...register(name, rules)}
+        aria-invalid={error ? "true" : "false"}
         {...rest}
       >
         {options.map(({ value, text }) => (
@@ -44,9 +38,7 @@ const Select: React.FunctionComponent<SelectProps> = ({
           </option>
         ))}
       </select>
-      {fieldState.error?.type && ruleMessages && (
-        <span role="alert">{ruleMessages[fieldState.error?.type]}</span>
-      )}
+      {error && <span role="alert">{error}</span>}
     </div>
   );
 };
