eb1d55c32d24aac92204b0773bab2663fcdc45ed
jaysonmc <jayson.r.mcintosh@gmail.com>
2021-06-18 10:23:43 -0400

    removing unused import

--- admin/resources/js/stories/ClassificationTable.stories.tsx
+++ admin/resources/js/stories/ClassificationTable.stories.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
-import { action } from "@storybook/addon-actions";
 import { createClient } from "urql";
 import ClassificationTable from "../components/ClassificationsTable";
 import fakeClassifications from "../fakeData/fakeClassifications";
