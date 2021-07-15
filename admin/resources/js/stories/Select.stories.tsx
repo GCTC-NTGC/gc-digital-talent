c35569accc1b00bd422612439411b6f160c53be1
jaysonmc <jayson.r.mcintosh@gmail.com>
2021-07-15 13:17:20 -0400

    adding more styling. Last addition is the question mark icon in the top right for showing context

--- admin/resources/js/stories/Select.stories.tsx
+++ admin/resources/js/stories/Select.stories.tsx
@@ -16,19 +16,24 @@ export default {
 } as Meta;
 
 const TemplateSelect: Story<SelectProps> = (args) => {
+  const style = {
+    width: "25%",
+  };
+
   const methods = useForm<SelectProps>();
   return (
-    <FormProvider {...methods}>
-      <div>
-        <Select {...args} />
-      </div>
-    </FormProvider>
+    <div style={style}>
+      <FormProvider {...methods}>
+        <div>
+          <Select {...args} />
+        </div>
+      </FormProvider>
+    </div>
   );
 };
 
-export const SelectComp = TemplateSelect.bind({});
-
-SelectComp.args = {
+export const SelectDefault = TemplateSelect.bind({});
+SelectDefault.args = {
   id: "uniqueId",
   label: "Select a dept",
   name: "departments",
@@ -36,4 +41,48 @@ SelectComp.args = {
     { value: 1, text: "CRA" },
     { value: 2, text: "CBSA" },
   ],
+};
+
+export const SelectRequired = TemplateSelect.bind({});
+SelectRequired.args = {
+  id: "uniqueId",
+  label: "Select a dept",
+  name: "departments",
+  options: [
+    { value: 1, text: "CRA" },
+    { value: 2, text: "CBSA" },
+  ],
+  rules: {
+    required: true,
+  },
+};
+
+export const SelectRequiredwInfo = TemplateSelect.bind({});
+SelectRequiredwInfo.args = {
+  id: "uniqueId",
+  label: "Select a dept",
+  name: "departments",
+  options: [
+    { value: 1, text: "CRA" },
+    { value: 2, text: "CBSA" },
+  ],
+  context: "We collect the above data for account purposes.",
+  rules: {
+    required: true,
+  },
+};
+
+export const SelectRequiredwError = TemplateSelect.bind({});
+SelectRequiredwError.args = {
+  id: "uniqueId",
+  label: "Select a dept",
+  name: "departments",
+  options: [
+    { value: 1, text: "CRA" },
+    { value: 2, text: "CBSA" },
+  ],
+  error: "The field above needs to be complete in order to proceed.",
+  rules: {
+    required: true,
+  },
 };
