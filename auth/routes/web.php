0227cb98df822ea10d0166f9b9162cd910ae6679
tristan-orourke <tristan.m.orourke@gmail.com>
2021-07-20 16:36:40 -0400

    Install Breeze, Laravel's basic auth starter kit To test the created routes on local infrastructure, replace `RewriteRule ^(.*)$ admin/public/$1 [L]` with `RewriteRule ^(.*)$ auth/public/$1 [L]` in infrastructure/php-container/src/.htaccess

--- auth/routes/web.php
+++ auth/routes/web.php
@@ -13,10 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/auth/home', function () {
+Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/auth/hello', function () {
-    return "Hello World";
-});
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
