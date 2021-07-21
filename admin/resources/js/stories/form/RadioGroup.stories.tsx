7764d4d19945375744dbd8711f297c88ba00d13d
Yoni K <yoni.kid15@gmail.com>
2021-07-21 12:28:21 -0400

    Add stories for radio and radio group

---
+++ admin/resources/js/stories/form/RadioGroup.stories.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { Story, Meta } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
+import RadioGroup, { RadioGroupProps } from "../../components/form/RadioGroup";
+import Form from "../../components/form/Form";
+import Submit from "../../components/form/Submit";
+
+export default {
+  component: RadioGroup,
+  title: "Form/RadioGroup",
+} as Meta;
+
+const TemplateRadioGroup: Story<RadioGroupProps> = (args) => {
+  return (
+    <Form onSubmit={action("Submit Form")}>
+      <RadioGroup {...args} />
+      <Submit />
+    </Form>
+  );
+};
+
+export const BasicRadioGroup = TemplateRadioGroup.bind({});
+BasicRadioGroup.args = {
+  idPrefix: "radiogroup",
+  legend: "Which items do you want to check?",
+  name: "radiogroup",
+  items: [
+    { value: "one", label: "Box One" },
+    { value: "two", label: "Box Two" },
+    { value: "three", label: "Box Three" },
+  ],
+};
+
+export const RequiredRadioGroup = TemplateRadioGroup.bind({});
+RequiredRadioGroup.args = {
+  ...BasicRadioGroup.args,
+  rules: { required: "All items must be checked!" },
+};
+
+export const RadioGroupWithContext = TemplateRadioGroup.bind({});
+RadioGroupWithContext.args = {
+  ...BasicRadioGroup.args,
+  context:
+    "Check the action tab after submitting to see how the form values are represented.",
+};
+
+export const DisabledRadioGroup = TemplateRadioGroup.bind({});
+DisabledRadioGroup.args = {
+  ...BasicRadioGroup.args,
+  disabled: true,
+};
+
+export const RadioGroupHiddenOptional = TemplateRadioGroup.bind({});
+RadioGroupHiddenOptional.args = {
+  ...BasicRadioGroup.args,
+  hideOptional: true,
+};
