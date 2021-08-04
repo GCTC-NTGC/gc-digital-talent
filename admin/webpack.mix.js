a9bb41e56fe0dce32077d70f17435bbc61ce4971
Yoni K <yoni.kid15@gmail.com>
2021-08-04 16:24:07 -0400

    Remove hydrogen.css asset from laravel mix config

--- admin/webpack.mix.js
+++ admin/webpack.mix.js
@@ -11,7 +11,6 @@ let dotenvplugin = new webpack.DefinePlugin({
 })
 
 mix.ts("resources/js/dashboard.tsx", "public/js")
-  .css("resources/css/hydrogen.css", "public/css")
   .css("resources/css/app.css", "public/css")
   .polyfill({
   enabled: true,
