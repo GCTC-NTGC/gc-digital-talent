2afa795cac5be3aac361b60b25c64e9a01fbfd6c
jaysonmc <jayson.r.mcintosh@gmail.com>
2021-08-13 09:29:07 -0400

    adding story to call API

--- admin/resources/js/stories/PoolTable.stories.tsx
+++ admin/resources/js/stories/PoolTable.stories.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
-import { PoolTable } from "../components/PoolTable";
+import { createClient } from "urql";
+import { PoolTable, PoolTableApi } from "../components/PoolTable";
 import fakePools from "../fakeData/fakePools";
+import ClientProvider from "../components/ClientProvider";
 
 const poolData = fakePools();
 // Its possible data may come back from api with missing data.
@@ -9,3 +11,13 @@ const poolData = fakePools();
 const stories = storiesOf("Pools", module);
 
 stories.add("Pool Table", () => <PoolTable pools={poolData} editUrlRoot="#" />);
+
+// TODO: Pool Candidates Table API
+const client = createClient({
+  url: "http://localhost:8000/graphql",
+});
+stories.add("Pool Table Table with API data", () => (
+  <ClientProvider client={client}>
+    <PoolTableApi />
+  </ClientProvider>
+));
