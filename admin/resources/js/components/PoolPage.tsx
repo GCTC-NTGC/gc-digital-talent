9ba2f4100561e6d0bbc4f7dc2ceab83c823fbd0b
Matt Nigh <matt@mattnigh.com>
2021-08-17 15:44:42 -0400

    Add PoolPage component

---
+++ admin/resources/js/components/PoolPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { PoolTableApi } from "./PoolTable";
+
+export const PoolPage: React.FC = () => {
+  return (
+    <div>
+      <PoolTableApi />
+    </div>
+  );
+};
+
+export default PoolPage;
