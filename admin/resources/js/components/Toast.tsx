0292a73d0cfde86284648ee78bae4d3cc00e2e71
Yoni K <yoni.kid15@gmail.com>
2021-08-20 15:55:22 -0400

    Add new toast component

---
+++ admin/resources/js/components/Toast.tsx
@@ -0,0 +1,43 @@
+import * as React from "react";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { XCircleIcon } from "@heroicons/react/solid";
+
+const contextClass = {
+  success: "toast-success",
+  error: "toast-error",
+  info: "",
+  warning: "",
+  default: "",
+  dark: "",
+};
+
+const CloseButton = ({
+  closeToast,
+}: {
+  closeToast: React.MouseEventHandler;
+}) => <XCircleIcon style={{ width: "1rem" }} onClick={closeToast} />;
+
+const Toast: React.FunctionComponent = () => {
+  return (
+    <ToastContainer
+      position="top-center"
+      autoClose={5000}
+      hideProgressBar
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      role="alert"
+      toastClassName={(classOptions) =>
+        `${contextClass[classOptions?.type || "default"]} toast`
+      }
+      style={{ width: "400px" }}
+      closeButton={CloseButton}
+    />
+  );
+};
+
+export default Toast;
