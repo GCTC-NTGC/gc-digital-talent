ffeb791b218c68d1111a6060342e3d8fd81c4cf4
Yoni K <yoni.kid15@gmail.com>
2021-08-26 10:18:40 -0400

    Add form input i18n for UpdateOperationalRequirement

--- admin/resources/js/components/operationalRequirement/UpdateOperationalRequirement.tsx
+++ admin/resources/js/components/operationalRequirement/UpdateOperationalRequirement.tsx
@@ -57,34 +57,34 @@ export const UpdateOperationalRequirementForm: React.FunctionComponent<UpdateOpe
             <Input
               id="key"
               name="key"
-              label="Key: "
+              label={intl.formatMessage(messages.keyLabel)}
               type="text"
               rules={{ required: errorMessages.required }}
             />
             <Input
               id="name_en"
               name="name.en"
-              label="Name: "
+              label={intl.formatMessage(messages.nameLabelEn)}
               type="text"
               rules={{ required: errorMessages.required }}
             />
             <Input
               id="name_fr"
               name="name.fr"
-              label="Name FR: "
+              label={intl.formatMessage(messages.nameLabelFr)}
               type="text"
               rules={{ required: errorMessages.required }}
             />
             <TextArea
               id="description_en"
               name="description.en"
-              label="Description: "
+              label={intl.formatMessage(messages.descriptionLabelEn)}
               rules={{ required: errorMessages.required }}
             />
             <TextArea
               id="description_fr"
               name="description.fr"
-              label="Description FR: "
+              label={intl.formatMessage(messages.descriptionLabelFr)}
               rules={{ required: errorMessages.required }}
             />
             <Submit />
