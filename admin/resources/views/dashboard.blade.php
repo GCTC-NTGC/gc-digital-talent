962ab0b3fb9ab7cf4f42acc7a39aaa84ecef1329
Matt Nigh <matt@mattnigh.com>
2021-08-26 09:21:36 -0400

    Update admin asset function, dashboard blade

--- admin/resources/views/dashboard.blade.php
+++ admin/resources/views/dashboard.blade.php
@@ -5,16 +5,16 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="description" content="Put your description here.">
-  <link rel="apple-touch-icon" sizes="180x180" href="{{ config('app.url') }}/public/apple-touch-icon.png">
-  <link rel="icon" type="image/png" sizes="32x32" href="{{ config('app.url') }}/public/images/favicon-32x32.png">
-  <link rel="icon" type="image/png" sizes="16x16" href="{{ config('app.url') }}/public/images/favicon-16x16.png")>
-  <link rel="manifest" href="public/site.webmanifest">
-  <link href="{{ config('app.url') }}/public/css/hydrogen.css" rel="stylesheet" type="text/css"/>
-  <link href="{{ config('app.url') }}/public/css/app.css" rel="stylesheet" type="text/css"/>
+  <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('/apple-touch-icon.png') }}">
+  <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('/images/favicon-32x32.png') }}">
+  <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('/images/favicon-16x16.png') }}">
+  <link rel="manifest" href="{{ asset('/site.webmanifest') }}">
+  <link href="{{ asset(mix('/css/hydrogen.css')) }}" rel="stylesheet" type="text/css"/>
+  <link href="{{ asset(mix('/css/app.css')) }}" rel="stylesheet" type="text/css"/>
   <title>{{ config('app.name') }}</title>
 </head>
 <body>
   <div id="app" data-h2-font-family="b(sans)"></div>
-  <script src="{{ config('app.url') }}/public/js/dashboard.js"></script>
+  <script src="{{ asset(mix('/js/dashboard.js')) }}"></script>
 </body>
 </html>
