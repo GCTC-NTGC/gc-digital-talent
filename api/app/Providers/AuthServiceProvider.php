c39e6e08388c409d73288565b1e5a380de01aae6
Matt Nigh <matt@mattnigh.com>
2021-09-01 16:33:06 -0400

    Add api Auth middleware extract access token from request, validate it, and find sub match

--- api/app/Providers/AuthServiceProvider.php
+++ api/app/Providers/AuthServiceProvider.php
@@ -2,9 +2,20 @@
 
 namespace App\Providers;
 
+use DateTimeZone;
 use App\Models\User;
-use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Log;
+use Lcobucci\JWT\Configuration;
+use Lcobucci\JWT\Signer;
+use Lcobucci\JWT\Signer\Key\InMemory;
+use Lcobucci\JWT\UnencryptedToken;
+use Lcobucci\JWT\Validation\Constraint\IssuedBy;
+use Lcobucci\JWT\Validation\Constraint\RelatedTo;
+use Lcobucci\JWT\Validation\Constraint\LooseValidAt;
+use Lcobucci\JWT\Validation\Constraint\SignedWith;
+use Lcobucci\Clock\SystemClock;
+use Lcobucci\JWT\Validation\RequiredConstraintsViolated;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -33,6 +44,40 @@ class AuthServiceProvider extends ServiceProvider
         $this->app['auth']->viaRequest('api', function ($request) {
             if ($request->input('api_token')) {
                 return User::where('api_token', $request->input('api_token'))->first();
+            }
+            if ($request->bearerToken()) {
+                $bearerToken = $request->bearerToken(); // 1. extract JWT access token from request.
+                if ($bearerToken) {
+                    $config = Configuration::forAsymmetricSigner(
+                        new Signer\Rsa\Sha256(),
+                        InMemory::empty(),
+                        InMemory::plainText(env('AUTH_SERVER_PUBLIC_KEY')),
+                    );
+                    $clock = new SystemClock(new DateTimeZone(config('app.timezone')));
+                    $token = $config->parser()->parse($bearerToken);
+                    assert($token instanceof UnencryptedToken);
+                    $config->setValidationConstraints(
+                        new IssuedBy(env('AUTH_SERVER_ISS')),
+                        new RelatedTo($token->claims()->get('sub')),
+                        new LooseValidAt($clock),
+                        new SignedWith($config->signer(), $config->verificationKey()),
+                    );
+                    $constraints = $config->validationConstraints();
+
+                    try { // 2. validate access token.
+                        $config->validator()->assert($token, ...$constraints);
+                    } catch (RequiredConstraintsViolated $e) {
+                        Log::notice($e->violations());
+                        return abort(401, 'Authorization token not valid.');
+                    }
+
+                    $userMatch = User::where('id', $token->claims()->get('sub'))->first(); // 3. match "sub" claim to user id.
+                    if($userMatch) {
+                        return $userMatch;
+                    } else {
+                        return null;
+                    }
+                }
             }
         });
     }
