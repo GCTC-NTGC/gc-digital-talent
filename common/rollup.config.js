672123e39909f1da52d2d6b12dcf4abb9672a5c0
Yoni K <yoni.kid15@gmail.com>
2021-09-09 10:18:43 -0400

    Refactor rollup config to build modules for each resource type

--- common/rollup.config.js
+++ common/rollup.config.js
@@ -4,9 +4,38 @@ import commonjs from "@rollup/plugin-commonjs";
 import typescript from "rollup-plugin-typescript2";
 import postcss from "rollup-plugin-postcss";
 
+const resources = ["components", "constants", "helpers", "messages"];
 const packageJson = require("./package.json");
+const plugins = [
+  peerDepsExternal({
+    includeDependencies: true,
+  }),
+  resolve(),
+  commonjs(),
+  typescript({ useTsconfigDeclarationDir: true }),
+  postcss({
+      extensions: ['.css']
+  })
+];
 
-export default {
+const modules = resources.map((resource) => ({
+  input: `src/${resource}/index.ts`,
+  output: [
+    {
+      file: `lib/${resource}/index.js`,
+      format: "cjs",
+      sourcemap: true
+    },
+    {
+      file: `lib/${resource}/index.esm.js`,
+      format: "esm",
+      sourcemap: true
+    }
+  ],
+  plugins
+}))
+
+export default [{
   input: "src/index.ts",
   output: [
     {
@@ -20,15 +49,5 @@ export default {
       sourcemap: true
     }
   ],
-  plugins: [
-    peerDepsExternal({
-      includeDependencies: true,
-    }),
-    resolve(),
-    commonjs(),
-    typescript({ useTsconfigDeclarationDir: true }),
-    postcss({
-        extensions: ['.css']
-    })
-  ]
-};
+  plugins
+}, ...modules];
