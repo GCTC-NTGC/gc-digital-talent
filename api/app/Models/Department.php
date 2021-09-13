d80dfe2008f8a6b9dbc88c78dab9459d353f5fb6
Yoni K <yoni.kid15@gmail.com>
2021-09-13 15:35:52 -0400

    Create Department eloquent model

---
+++ api/app/Models/Department.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+/**
+ * Class Department
+ *
+ * @property int $id
+ * @property int department_number
+ * @property array $name
+ * @property Illuminate\Support\Carbon $created_at
+ * @property Illuminate\Support\Carbon $updated_at
+ * @property Illuminate\Support\Carbon $deleted_at
+ */
+
+ class Department extends Model
+ {
+    use SoftDeletes;
+
+    /**
+      * The attributes that should be case.
+      *
+      * @var array
+      */
+      protected $casts = [
+        'name' => 'array',
+      ];
+ }
