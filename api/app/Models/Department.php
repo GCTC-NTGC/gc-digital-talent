89072a7c8447d1cc083c8e4b6bbb9d5f4d182c54
Yoni K <yoni.kid15@gmail.com>
2021-09-17 17:14:56 -0400

    Set function type in Department model

--- api/app/Models/Department.php
+++ api/app/Models/Department.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 /**
@@ -29,7 +30,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
         'name' => 'array',
       ];
 
-      public function candidateSearchRequests() // phpcs:ignore
+      public function candidateSearchRequests(): HasMany
       {
           return $this->hasMany(CandidateSearchRequest::class);
       }
