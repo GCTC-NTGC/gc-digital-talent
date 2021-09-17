d9af0e32473752d92d5040f04b96c5e14dcba7ec
Yoni K <yoni.kid15@gmail.com>
2021-09-17 12:44:02 -0400

    Generate migration to create candidate search requestes table

---
+++ api/database/migrations/2021_09_17_135548_create_candidate_search_requests_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCandidateSearchRequestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('candidate_search_requests', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string("full_name");
+            $table->string("email");
+            $table->string('department_id')->references('id')->on('departments');
+            $table->string("job_title");
+            $table->text("additional_comments")->nullable();
+            // $table->jsonb("summary_of_filters");
+            $table->date("requested_date");
+            $table->text("status");
+            $table->text("admin_notes")->nullable();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('candidate_search_requests');
+    }
+}
