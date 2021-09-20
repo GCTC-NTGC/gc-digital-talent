4d4e3492995684fff5cca5f9dc624c2a8f860715
Yoni K <yoni.kid15@gmail.com>
2021-09-20 12:56:38 -0400

    Add button stories to common folder

--- common/src/components/Button/Button.stories.tsx
+++ common/src/components/Button/Button.stories.tsx
@@ -1,14 +1,140 @@
-import { Meta } from "@storybook/react";
 import React from "react";
-import Button from "./Button";
+import { Story, Meta } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
+import { Button, ButtonProps } from "./Button";
 
 export default {
-  title: "Components/Button",
   component: Button,
+  title: "Components/Button",
+  args: {
+    label: "Button Label",
+  },
+  argTypes: {
+    label: {
+      name: "label",
+      type: { name: "string", required: true },
+      control: {
+        type: "text",
+      },
+    },
+    onClick: {
+      table: {
+        disable: true,
+      },
+    },
+  },
 } as Meta;
 
-export const PrimaryButton = () => (
-  <Button color="primary" mode="solid">
-    Button
-  </Button>
-);
+const TemplateButton: Story<ButtonProps & { label: string }> = (args) => {
+  const { label, ...rest } = args;
+  return (
+    <Button {...rest}>
+      <span>{label}</span>
+    </Button>
+  );
+};
+
+export const ButtonPrimary = TemplateButton.bind({});
+export const ButtonPrimaryBlock = TemplateButton.bind({});
+export const ButtonPrimaryOutline = TemplateButton.bind({});
+export const ButtonPrimaryInline = TemplateButton.bind({});
+export const ButtonSecondary = TemplateButton.bind({});
+export const ButtonSecondaryOutline = TemplateButton.bind({});
+export const ButtonSecondaryInline = TemplateButton.bind({});
+export const ButtonCTA = TemplateButton.bind({});
+export const ButtonCTAOutline = TemplateButton.bind({});
+export const ButtonCTAInline = TemplateButton.bind({});
+export const ButtonWhite = TemplateButton.bind({});
+export const ButtonWhiteOutline = TemplateButton.bind({});
+export const ButtonWhiteInline = TemplateButton.bind({});
+
+ButtonPrimary.args = {
+  color: "primary",
+  mode: "solid",
+  onClick: action("Button clicked"),
+};
+
+ButtonPrimaryBlock.args = {
+  color: "primary",
+  mode: "solid",
+  block: true,
+  onClick: action("Button clicked"),
+};
+
+ButtonPrimaryOutline.args = {
+  color: "primary",
+  mode: "outline",
+  onClick: action("Button clicked"),
+};
+
+ButtonPrimaryInline.args = {
+  color: "primary",
+  mode: "inline",
+  onClick: action("Button clicked"),
+};
+
+ButtonSecondary.args = {
+  color: "secondary",
+  mode: "solid",
+  onClick: action("Button clicked"),
+};
+
+ButtonSecondaryOutline.args = {
+  color: "secondary",
+  mode: "outline",
+  onClick: action("Button clicked"),
+};
+
+ButtonSecondaryInline.args = {
+  color: "secondary",
+  mode: "inline",
+  onClick: action("Button clicked"),
+};
+
+ButtonCTA.args = {
+  color: "cta",
+  mode: "solid",
+  onClick: action("Button clicked"),
+};
+
+ButtonCTAOutline.args = {
+  color: "cta",
+  mode: "outline",
+  onClick: action("Button clicked"),
+};
+
+ButtonCTAInline.args = {
+  color: "cta",
+  mode: "inline",
+  onClick: action("Button clicked"),
+};
+
+ButtonWhite.args = {
+  color: "white",
+  mode: "solid",
+  onClick: action("Button clicked"),
+};
+
+ButtonWhiteOutline.args = {
+  color: "white",
+  mode: "outline",
+  onClick: action("Button clicked"),
+};
+
+ButtonWhiteInline.args = {
+  color: "white",
+  mode: "inline",
+  onClick: action("Button clicked"),
+};
+
+ButtonWhite.parameters = {
+  backgrounds: { default: "dark" },
+};
+
+ButtonWhiteOutline.parameters = {
+  backgrounds: { default: "dark" },
+};
+
+ButtonWhiteInline.parameters = {
+  backgrounds: { default: "dark" },
+};
