1805e9c4b5e53a4b822600f73e66c49d2f5aae59
tristan-orourke <tristan.m.orourke@gmail.com>
2021-09-22 17:15:34 -0400

    Make db seeding more predictable Always have an admin user and only two pools.

--- api/database/seeders/DatabaseSeeder.php
+++ api/database/seeders/DatabaseSeeder.php
@@ -22,8 +22,9 @@ class DatabaseSeeder extends Seeder
         $this->call(OperationalRequirementSeeder::class);
         $this->call(DepartmentSeeder::class);
 
-        User::factory()->count(5)->create();
-        Pool::factory()->count(2)->create();
+        $this->call(UserSeeder::class);
+        $this->call(PoolSeeder::class);
+
         PoolCandidate::factory()
             ->count(60)
             ->state(new Sequence(
--- api/database/seeders/UserSeeder.php
+++ api/database/seeders/UserSeeder.php
@@ -14,13 +14,12 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        if (!User::where(['email' => 'admin@test.com'])->first()) {
+        if (!User::where('email', 'admin@test.com')->first()) {
             User::factory()->create([
                 'email' => 'admin@test.com',
                 'sub' => 'admin@test.com',
                 'roles' => ['ADMIN']
             ]);
         }
-        User::factory()->count(50)->create();
     }
 }
