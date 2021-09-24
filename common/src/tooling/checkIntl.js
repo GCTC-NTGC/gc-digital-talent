bd9dff0ea845c1ce4b2de27e6094a07dbe35e507
tristan-orourke <tristan.m.orourke@gmail.com>
2021-09-24 17:00:19 -0400

    Write script to check for missing, untranslated or missing intl entries

---
+++ common/src/tooling/checkIntl.js
@@ -0,0 +1,45 @@
+import yargs from "yargs";
+import fs from "fs/promises";
+
+const { argv } = yargs(process.argv.slice(2))
+  .option("en", {
+    demandOption: true,
+    describe: "path to en.json file extracted by Format.js",
+  })
+  .option("fr", {
+    demandOption: true,
+    describe: "path to fr.json with same format as en.json",
+  })
+  .help();
+
+const en = await fs.readFile(argv.en).then(JSON.parse);
+const fr = await fs.readFile(argv.fr).then(JSON.parse);
+
+const reduceEn = (newEn, enKey) => {
+  newEn[enKey] = en[enKey];
+  return newEn;
+};
+const reduceFr = (newFr, frKey) => {
+  newFr[frKey] = fr[frKey];
+  return newFr;
+};
+
+const enKeys = Object.keys(en);
+
+const missing = enKeys
+  .filter((enKey) => {
+    return fr[enKey] === undefined;
+  })
+  .reduce(reduceEn, {});
+
+const untranslated = enKeys
+  .filter((enKey) => fr[enKey]?.defaultMessage === en[enKey].defaultMessage)
+  .reduce(reduceEn, {});
+
+const orphaned = Object.keys(fr)
+  .filter((frKey) => en[frKey] === undefined)
+  .reduce(reduceFr, {});
+
+console.log("These values are missing translations:", missing);
+console.log("These values are identical in en and fr:", untranslated);
+console.log("These values are present in fr but not in en:", orphaned);
