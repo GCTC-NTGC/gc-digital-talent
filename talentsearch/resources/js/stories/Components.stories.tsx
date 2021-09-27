96c6db792b9abdb5d0c9c5186899f8a036f87107
Matt Nigh <matt@mattnigh.com>
2021-09-27 16:52:46 -0400

    Add Header component to talentsearch

--- talentsearch/resources/js/stories/Components.stories.tsx
+++ talentsearch/resources/js/stories/Components.stories.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
 import Footer from "../components/Footer";
+import Header from "../components/Header";
 
 const stories = storiesOf("Components", module);
+
+stories.add("Header", () => {
+  return <Header />;
+});
 
 stories.add("Footer", () => {
   return <Footer />;
