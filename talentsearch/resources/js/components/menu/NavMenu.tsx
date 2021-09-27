cdda7bd81aa68160cd325caae5887f5d103d602b
Matt Nigh <matt@mattnigh.com>
2021-09-27 16:55:19 -0400

    Update NavMenu component in talentsearch

--- talentsearch/resources/js/components/menu/NavMenu.tsx
+++ talentsearch/resources/js/components/menu/NavMenu.tsx
@@ -4,23 +4,33 @@ export const NavMenu: React.FunctionComponent<{
   items: ReactElement[];
 }> = ({ items }) => {
   return (
-    <nav>
-      <ul className="reset-ul">
-        {items.map((item) => {
-          return (
-            <li data-h2-margin="b(top, none) b(bottom, s)" key={item.key}>
-              <span
-                data-h2-display="b(block)"
-                data-h2-margin="b(bottom, s)"
-                data-h2-font-style="b:h(reset)"
-              >
-                {item}
-              </span>
-            </li>
-          );
-        })}
-      </ul>
-    </nav>
+    <div data-h2-flex-grid="b(middle, contained, flush, xl)">
+      <div
+        data-h2-flex-item="b(1of1) m(1of2)"
+        data-h2-text-align="b(center) m(left)"
+      >
+        <nav>
+          <ul className="reset-ul">
+            {items.map((item) => {
+              return (
+                <li
+                  data-h2-margin="b(top, none) b(bottom, s) b(right, m)"
+                  data-h2-display="b(block) m(inline-block)"
+                  key={item.key}
+                >
+                  <span
+                    data-h2-margin="b(bottom, s)"
+                    data-h2-font-style="b:h(reset)"
+                  >
+                    {item}
+                  </span>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+      </div>
+    </div>
   );
 };
 
