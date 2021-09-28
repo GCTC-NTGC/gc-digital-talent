b401c602dba6bff74914f371001ff3755047d290
tristan-orourke <tristan.m.orourke@gmail.com>
2021-09-28 10:02:06 -0400

    Add whitelist for strings that don't need translation

--- common/src/tooling/checkIntl.js
+++ common/src/tooling/checkIntl.js
@@ -10,10 +10,22 @@ const { argv } = yargs(process.argv.slice(2))
     demandOption: true,
     describe: "path to fr.json with same format as en.json",
   })
+  //   .boolean("rm-orphaned", {
+  //     alias: "r",
+  //     describe: "delete entries in fr.json file which don't appear in en.json",
+  //   })
+  .option("whitelist", {
+    alias: "w",
+    describe:
+      "path to json file containing an array of keys which are expected to be identical in fr and en",
+  })
   .help();
 
 const en = await fs.readFile(argv.en).then(JSON.parse);
 const fr = await fs.readFile(argv.fr).then(JSON.parse);
+const whitelist = argv.whitelist
+  ? await fs.readFile(argv.whitelist).then(JSON.parse)
+  : [];
 
 const reduceEn = (newEn, enKey) => {
   newEn[enKey] = en[enKey];
@@ -33,7 +45,11 @@ const missing = enKeys
   .reduce(reduceEn, {});
 
 const untranslated = enKeys
-  .filter((enKey) => fr[enKey]?.defaultMessage === en[enKey].defaultMessage)
+  .filter(
+    (enKey) =>
+      fr[enKey]?.defaultMessage === en[enKey].defaultMessage &&
+      !whitelist.includes(enKey),
+  )
   .reduce(reduceEn, {});
 
 const orphaned = Object.keys(fr)
