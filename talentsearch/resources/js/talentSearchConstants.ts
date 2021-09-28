f60b2b5731f241ce33dc0147cae8bf80f99ece54
Matt Nigh <matt@mattnigh.com>
2021-09-28 08:51:37 -0400

    Change APP_URL to TALENTSEARCH_APP_URL

--- talentsearch/resources/js/talentSearchConstants.ts
+++ talentsearch/resources/js/talentSearchConstants.ts
@@ -1,3 +1,3 @@
 // eslint-disable-next-line import/prefer-default-export
 export const BASE_URL =
-  process.env.APP_URL ?? "http://localhost:8000/talentsearch";
+  process.env.TALENTSEARCH_APP_URL ?? "http://localhost:8000/talentsearch";
