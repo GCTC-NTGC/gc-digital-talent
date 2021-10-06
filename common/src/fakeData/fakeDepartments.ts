f0e5d28043221bd8233a1db8f6bcf81592c7b100
Yoni K <yoni.kid15@gmail.com>
2021-10-06 15:04:11 -0400

    Move fake data to common folder

---
+++ common/src/fakeData/fakeDepartments.ts
@@ -0,0 +1,44 @@
+import { Department } from "../api/generated";
+
+export default (): Department[] => [
+  {
+    id: "dd9b0a72-e2b8-4e61-b7fb-79ebae7978fb",
+    department_number: 4,
+    name: {
+      en: "Public Service Commission",
+      fr: "Commission de la fonction publique",
+    },
+  },
+  {
+    id: "54ca5bee-2928-4c72-a6e2-2241d65d5c4f",
+    department_number: 6,
+    name: {
+      en: "Finance (Department of)",
+      fr: "Finances (Ministère des)",
+    },
+  },
+  {
+    id: "790176b0-b3de-4f95-b259-dda8262327f7",
+    department_number: 22,
+    name: {
+      en: "Health (Department of)",
+      fr: "Santé (Ministère de la)",
+    },
+  },
+  {
+    id: "1a02573f-665e-4460-a3c5-aa1a52ef508d",
+    department_number: 34,
+    name: {
+      en: "Transport (Department of)",
+      fr: "Transports (Ministère des)",
+    },
+  },
+  {
+    id: "3d2c4bd8-616f-467e-a4b4-2c0c5f2322b0",
+    department_number: 56,
+    name: {
+      en: "Treasury Board Secretariat",
+      fr: "Secrétariat du Conseil du Trésor",
+    },
+  },
+];
