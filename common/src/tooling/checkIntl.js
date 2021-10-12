08dcb68bc1c0ed0ed1f7431a7b493065c411a7ed
tristan-orourke <tristan.m.orourke@gmail.com>
2021-10-12 16:58:46 -0400

    Add additional documentation to checkIntl script

--- common/src/tooling/checkIntl.js
+++ common/src/tooling/checkIntl.js
@@ -38,18 +38,32 @@ const { argv } = yargs(process.argv.slice(2))
   })
   .help();
 
+// First read all relevant files and convert to js objects.
 const en = JSON.parse(fs.readFileSync(argv.en));
 const frOriginal = JSON.parse(fs.readFileSync(argv.fr));
-const frNew = argv["merge-fr"]
-  ? JSON.parse(fs.readFileSync(argv["merge-fr"]))
-  : {};
-const fr = { ...frOriginal, ...frNew };
 const whitelist = argv.whitelist
   ? JSON.parse(fs.readFileSync(argv.whitelist))
   : [];
-// Used to track changes that may need to be made to fr.json, so we only update it once.
+const frNew = argv["merge-fr"]
+  ? JSON.parse(fs.readFileSync(argv["merge-fr"]))
+  : {};
+
+// If merge-fr file was specified, use it to overwrite values from frOriginal.
+const fr = { ...frOriginal, ...frNew };
+
+// This variable is used to track changes that may need to be made to fr.json. That way, we only update the file once, at the end.
 let outputFr = null;
 
+/**
+ * T
+ */
+
+/**
+ * This function takes a js object and a file path, and writes the object to the file as json.
+ * @param {string} file - A file path.
+ * @param {Record<string, unknown>} obj - A js object to be saved as a json.
+ * @returns undefined
+ */
 const saveJson = (file, obj) =>
   fs.writeFile(file, JSON.stringify(obj, null, 2), {}, () => {
     /** Do nothing */
@@ -60,6 +74,13 @@ if (Object.keys(frNew).length > 0) {
   console.log(`Entries from ${argv["merge-fr"]} merged into ${argv.fr}`);
 }
 
+/**
+ * Iterates through the keys of obj, creating a new object identical to the original,
+ * but only with the keys for which predicate returns true.
+ * @param {Record<string, unknown>} obj
+ * @param {(key: string) => boolean} predicate
+ * @returns {Record<string, unknown>}
+ */
 const transform = (obj, predicate) => {
   return Object.keys(obj).reduce((memo, key) => {
     if (predicate(obj[key], key)) {
@@ -69,15 +90,31 @@ const transform = (obj, predicate) => {
     return memo;
   }, {});
 };
-const omit = (obj, items) =>
-  transform(obj, (value, key) => !items.includes(key));
-const pick = (obj, items) =>
-  transform(obj, (value, key) => items.includes(key));
 
+/**
+ * Return a copy of obj, excluding specified keys.
+ * @param {Record<string, unknown>} obj
+ * @param {string[]} keys
+ * @returns {Record<string, unknown>}
+ */
+const omit = (obj, keys) => transform(obj, (value, key) => !keys.includes(key));
+/**
+ * Return a copy of obj, excluding all keys not specified.
+ * @param {Record<string, unknown>} obj
+ * @param {string[]} keys
+ * @returns {Record<string, unknown>}
+ */
+const pick = (obj, keys) => transform(obj, (value, key) => keys.includes(key));
+
+// All keys in the original en file.
 const enKeys = Object.keys(en);
+
+// Keys which appear in the en file, but not at all in the fr file.
 const missingKeys = enKeys.filter((enKey) => {
   return fr[enKey] === undefined;
 });
+
+// Keys for whom the value is the same in en and fr (not including those whitelisted).
 const untranslatedKeys = enKeys.filter(
   (enKey) =>
     fr[enKey]?.defaultMessage === en[enKey].defaultMessage &&
@@ -93,6 +130,8 @@ if (untranslatedKeys.length > 0) {
   console.error("These values are identical in en and fr:", untranslated);
 }
 
+// Orphaned keys are those which appear in the fr file, but which no longer appear in the en file,
+// which suggests they are no longer used in code.
 const orphanedKeys = Object.keys(fr).filter((frKey) => en[frKey] === undefined);
 const orphaned = pick(fr, orphanedKeys);
 
@@ -118,6 +157,9 @@ if (argv["rm-orphaned"] && orphanedKeys.length > 0) {
 }
 
 if (outputFr !== null) {
+  // outputFr will not be null if either:
+  //   - we have merged in entries from a merge-fr file
+  //   - we have removed orphaned keys
   saveJson(argv.fr, outputFr);
 }
 
