9cf70e9b3e4a7ec1c599996285ca70b6bbe3992e
Peter Gies <petertgiles@gmail.com>
2021-10-27 16:29:57 -0400

    work in progress for maintenance container, still some issues to resolve

---
+++ maintenance/scripts/setup.sh
@@ -0,0 +1,72 @@
+#! /bin/bash
+
+#setup nvm
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+#ensure latest npm
+nvm install-latest-npm
+
+# setup auth project
+cd /var/www/html/auth
+cp .env.example .env
+composer install
+nvm install
+nvm use
+npm install
+npm run dev
+php artisan key:generate
+php artisan migrate
+php artisan passport:keys
+php artisan passport:client --personal --name="Laravel Personal Access Client" > personal_access_client.txt
+/root/scripts/update_auth_env.sh
+rm personal_access_client.txt
+php artisan config:cache
+
+# setup api project
+cd /var/www/html/api
+cp .env.example .env
+/root/scripts/update_env_appkey.sh .env
+composer install
+php artisan migrate:fresh
+php artisan db:seed
+php artisan lighthouse:print-schema --write
+/root/scripts/update_api_env.sh
+
+# setup common project
+cd /var/www/html/common
+nvm install
+nvm use
+npm install
+npm run codegen
+
+# setup admin project
+cd /var/www/html/admin
+cp .env.example .env
+/root/scripts/update_env_appkey.sh .env
+composer install
+nvm install
+nvm use
+npm install
+npm rebuild node-sass 
+npm run h2-compile
+npm run codegen
+npm run dev
+cd /var/www/html/auth
+php artisan passport:client --user_id="1" --name="admin" --redirect_uri="http://localhost:8000/admin/auth-callback" > admin_secret.txt
+/root/scripts/update_admin_env.sh
+rm admin_secret.txt
+php artisan config:cache
+
+# setup talentsearch project
+cd /var/www/html/talentsearch
+cp .env.example .env
+/root/scripts/update_env_appkey.sh .env
+composer install
+nvm install
+nvm use
+npm install
+npm rebuild node-sass 
+npm run h2-compile
+npm run codegen
+npm run dev
