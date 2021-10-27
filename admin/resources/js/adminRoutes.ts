da6280ebbb3edde5113ad52d5d4b165fab2b0b19
Yoni K <yoni.kid15@gmail.com>
2021-10-27 16:14:37 -0400

    Add table route to adminRoutes

--- admin/resources/js/adminRoutes.ts
+++ admin/resources/js/adminRoutes.ts
@@ -1,7 +1,8 @@
 import { ADMIN_APP_DIR } from "./adminConstants";
 
 export const homePath = (): string => `/${ADMIN_APP_DIR}`;
-export const homePublicPath = (): string => `/${ADMIN_APP_DIR}/public/${ADMIN_APP_DIR}`;
+export const homePublicPath = (): string =>
+  `/${ADMIN_APP_DIR}/public/${ADMIN_APP_DIR}`;
 
 export const classificationTablePath = (): string =>
   `/${ADMIN_APP_DIR}/classifications`;
@@ -11,11 +12,13 @@ export const classificationUpdatePath = (id: string): string =>
   `/${ADMIN_APP_DIR}/classifications/${id}/edit`;
 
 export const cmoAssetTablePath = (): string => `/${ADMIN_APP_DIR}/cmo-assets`;
-export const cmoAssetCreatePath = (): string => `/${ADMIN_APP_DIR}/cmo-assets/create`;
+export const cmoAssetCreatePath = (): string =>
+  `/${ADMIN_APP_DIR}/cmo-assets/create`;
 export const cmoAssetUpdatePath = (id: string): string =>
   `/${ADMIN_APP_DIR}/cmo-assets/${id}/edit`;
 
-export const departmentTablePath = (): string => `/${ADMIN_APP_DIR}/departments`;
+export const departmentTablePath = (): string =>
+  `/${ADMIN_APP_DIR}/departments`;
 export const departmentCreatePath = (): string =>
   `/${ADMIN_APP_DIR}/departments/create`;
 export const departmentUpdatePath = (id: string): string =>
@@ -47,3 +50,8 @@ export const userTablePath = (): string => `/${ADMIN_APP_DIR}/users`;
 export const userCreatePath = (): string => `/${ADMIN_APP_DIR}/users/create`;
 export const userUpdatePath = (id: string): string =>
   `/${ADMIN_APP_DIR}/users/${id}/edit`;
+
+export const searchRequestTablePath = (): string =>
+  `/${ADMIN_APP_DIR}/searchRequests`;
+export const searchRequestUpdatePath = (id: string): string =>
+  `/${ADMIN_APP_DIR}/searchRequests/${id}/edit`;
