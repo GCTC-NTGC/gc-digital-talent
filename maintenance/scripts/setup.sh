293a2b737e3c5953c8c5e4304cee7bdfcefb4ddc
Peter Gies <petertgiles@gmail.com>
2021-10-28 14:49:00 -0400

    some bug fixes, switch to latest version of node

--- maintenance/scripts/setup.sh
+++ maintenance/scripts/setup.sh
@@ -3,28 +3,35 @@
 #setup nvm
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+nvm install --lts
+nvm use --lts
 
-#ensure latest npm
-nvm install-latest-npm
+# setup common project
+cd /var/www/html/common
+npm install
+npm run codegen
 
 # setup auth project
 cd /var/www/html/auth
+chown -R www-data ./storage ./vendor
+chmod -R 775 ./ ./storage
 cp .env.example .env
 composer install
-nvm install
-nvm use
 npm install
 npm run dev
 php artisan key:generate
 php artisan migrate
-php artisan passport:keys
+#php artisan passport:keys # gives an "Encryption keys already exist." error.
 php artisan passport:client --personal --name="Laravel Personal Access Client" > personal_access_client.txt
 /root/scripts/update_auth_env.sh
 rm personal_access_client.txt
+php artisan db:seed
 php artisan config:cache
 
 # setup api project
 cd /var/www/html/api
+chown -R www-data ./storage ./vendor
+chmod -R 775 ./ ./storage
 cp .env.example .env
 /root/scripts/update_env_appkey.sh .env
 composer install
@@ -33,20 +40,26 @@ php artisan db:seed
 php artisan lighthouse:print-schema --write
 /root/scripts/update_api_env.sh
 
-# setup common project
-cd /var/www/html/common
-nvm install
-nvm use
-npm install
-npm run codegen
-
-# setup admin project
-cd /var/www/html/admin
+# setup talentsearch project
+cd /var/www/html/talentsearch
+chown -R www-data ./storage ./vendor
+chmod -R 775 ./ ./storage
 cp .env.example .env
 /root/scripts/update_env_appkey.sh .env
 composer install
-nvm install
-nvm use
+npm install
+npm rebuild node-sass 
+npm run h2-compile
+npm run codegen
+npm run dev
+
+# setup admin project
+cd /var/www/html/admin
+chown -R www-data ./storage ./vendor
+chmod -R 775 ./ ./storage
+cp .env.example .env
+/root/scripts/update_env_appkey.sh .env
+composer install
 npm install
 npm rebuild node-sass 
 npm run h2-compile
@@ -58,15 +71,3 @@ php artisan passport:client --user_id="1" --name="admin" --redirect_uri="http://
 rm admin_secret.txt
 php artisan config:cache
 
-# setup talentsearch project
-cd /var/www/html/talentsearch
-cp .env.example .env
-/root/scripts/update_env_appkey.sh .env
-composer install
-nvm install
-nvm use
-npm install
-npm rebuild node-sass 
-npm run h2-compile
-npm run codegen
-npm run dev
--- maintenance/scripts/update_env_appkey.sh
+++ maintenance/scripts/update_env_appkey.sh
@@ -1,4 +1,3 @@
 #! /bin/bash
-
-appkey=`cat /dev/urandom | tr -dc '[a-zA-Z0-9]' | fold -w ${1:-32} | head -n 1`
+appkey=`cat /dev/urandom | tr -dc '[a-zA-Z0-9]' | fold -w32 - | head -n 1`
 sed -i "s/APP_KEY=.*/APP_KEY=$appkey/" $1
