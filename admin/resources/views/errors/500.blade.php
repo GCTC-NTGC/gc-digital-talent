5a535135485cc79b91df2857bb3f805e619e87eb
tristan-orourke <tristan.m.orourke@gmail.com>
2021-11-02 16:35:52 -0400

    Add custom 500 error pages to talentsearch and admin projects which say which project is rendering them

---
+++ admin/resources/views/errors/500.blade.php
@@ -0,0 +1,5 @@
+@extends('errors::minimal')
+
+@section('title', __('Something went wrong (in ADMIN)'))
+@section('code', '500')
+@section('message', __('This is the admin project. I hope you can find logs somewhere.'))
---
+++ talentsearch/resources/views/errors/500.blade.php
@@ -0,0 +1,5 @@
+@extends('errors::minimal')
+
+@section('title', __('Something went wrong (in TALENTSEARCH)'))
+@section('code', '500')
+@section('message', __('This is the talentsearch project. I hope you can find logs somewhere.'))
