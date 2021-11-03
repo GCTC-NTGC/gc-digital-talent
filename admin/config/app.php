eb4c2ed774b7157cdabe01bf3ffbe79729bfc0f0
tristan-orourke <tristan.m.orourke@gmail.com>
2021-11-03 10:39:19 -0400

    Remove debugbar from service provider list - auto discovery makes this unnecessary

--- admin/config/app.php
+++ admin/config/app.php
@@ -166,7 +166,6 @@ return [
         /*
          * Package Service Providers...
          */
-        Barryvdh\Debugbar\ServiceProvider::class,
 
         /*
          * Application Service Providers...
