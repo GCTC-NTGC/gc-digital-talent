8c57de2a752957897f12978399b5a7ffdd6129c5
Matt Nigh <matt@mattnigh.com>
2021-11-03 09:25:42 -0400

    Add Debugbar to ServiceProviders for admin

--- admin/config/app.php
+++ admin/config/app.php
@@ -175,7 +175,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
+        Barryvdh\Debugbar\ServiceProvider::class,
     ],
 
     /*
