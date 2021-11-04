450f0803a6d91f51d79348dc3af417963eb73fa3
Yoni K <yoni.kid15@gmail.com>
2021-11-04 10:49:06 -0400

    Update admin/resources/js/components/searchRequest/SearchRequestTable.tsx
    Co-authored-by: Matt <mnigh@users.noreply.github.com>

--- admin/resources/js/components/searchRequest/SearchRequestTable.tsx
+++ admin/resources/js/components/searchRequest/SearchRequestTable.tsx
@@ -44,7 +44,7 @@ export const SearchRequestTable: React.FunctionComponent<GetPoolCandidateSearchR
      {
        Header: intl.formatMessage({
         defaultMessage: "Manager",
-        description: "Title displayed on the search request table first name column.",
+        description: "Title displayed on the search request table manager column.",
       }),
        accessor: "fullName"
      },
