83e6046337d9d4243e54f003188d75f3723edf50
Yoni K <yoni.kid15@gmail.com>
2021-11-04 10:49:46 -0400

    Update admin/resources/js/stories/SearchRequest.stories.tsx
    Co-authored-by: Matt <mnigh@users.noreply.github.com>

--- admin/resources/js/stories/SearchRequest.stories.tsx
+++ admin/resources/js/stories/SearchRequest.stories.tsx
@@ -3,7 +3,6 @@ import { storiesOf } from "@storybook/react";
 import { fakeSearchRequests } from "@common/fakeData";
 import { SearchRequestTable } from "../components/searchRequest/SearchRequestTable";
 
-// It is possible data may come back from api with missing data.
 
 const stories = storiesOf("Search Results", module);
 
