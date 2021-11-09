e463f028299f3702eaab8aba6e3a8fc521956870
Yoni K <yoni.kid15@gmail.com>
2021-11-09 11:52:21 -0500

    Update admin/resources/js/stories/SearchRequest.stories.tsx
    Co-authored-by: Matt <mnigh@users.noreply.github.com>

--- admin/resources/js/stories/SearchRequest.stories.tsx
+++ admin/resources/js/stories/SearchRequest.stories.tsx
@@ -3,7 +3,6 @@ import { storiesOf } from "@storybook/react";
 import { fakeSearchRequests } from "@common/fakeData";
 import { SearchRequestTable } from "../components/searchRequest/SearchRequestTable";
 
-
 const stories = storiesOf("Search Results", module);
 
 stories.add("Search Results Table", () => (
