98b8bd6d6705e9b691b4c01a82cf9dd08cbab952
Peter Giles <petertgiles@gmail.com>
2021-12-06 15:12:26 -0500

    Build each admin path separately

--- admin/resources/js/adminRoutes.ts
+++ admin/resources/js/adminRoutes.ts
@@ -2,52 +2,55 @@ import path from "path-browserify";
 import { ADMIN_APP_DIR } from "./adminConstants";
 
 export const homePath = (): string => path.join("/", ADMIN_APP_DIR); // leading slash in case empty base url
-export const homePublicPath = (): string => path.join(homePath(), "public");
+export const homePublicPath = (): string =>
+  path.join("/", ADMIN_APP_DIR, "public");
 
 export const classificationTablePath = (): string =>
-  path.join(homePath(), "classifications");
+  path.join("/", ADMIN_APP_DIR, "classifications");
 export const classificationCreatePath = (): string =>
-  path.join(homePath(), "classifications", "create");
+  path.join("/", ADMIN_APP_DIR, "classifications", "create");
 export const classificationUpdatePath = (id: string): string =>
-  path.join(homePath(), "classifications", id, "edit");
+  path.join("/", ADMIN_APP_DIR, "classifications", id, "edit");
 
 export const cmoAssetTablePath = (): string =>
-  path.join(homePath(), "cmo-assets");
+  path.join("/", ADMIN_APP_DIR, "cmo-assets");
 export const cmoAssetCreatePath = (): string =>
-  path.join(homePath(), "cmo-assets", "create");
+  path.join("/", ADMIN_APP_DIR, "cmo-assets", "create");
 export const cmoAssetUpdatePath = (id: string): string =>
-  path.join(homePath(), "cmo-assets", id, "edit");
+  path.join("/", ADMIN_APP_DIR, "cmo-assets", id, "edit");
 
 export const departmentTablePath = (): string =>
-  path.join(homePath(), "departments");
+  path.join("/", ADMIN_APP_DIR, "departments");
 export const departmentCreatePath = (): string =>
-  path.join(homePath(), "departments", "create");
+  path.join("/", ADMIN_APP_DIR, "departments", "create");
 export const departmentUpdatePath = (id: string): string =>
-  path.join(homePath(), "departments", id, "edit");
+  path.join("/", ADMIN_APP_DIR, "departments", id, "edit");
 
 export const operationalRequirementTablePath = (): string =>
-  path.join(homePath(), "operational-requirements");
+  path.join("/", ADMIN_APP_DIR, "operational-requirements");
 export const operationalRequirementCreatePath = (): string =>
-  path.join(homePath(), "operational-requirements", "create");
+  path.join("/", ADMIN_APP_DIR, "operational-requirements", "create");
 export const operationalRequirementUpdatePath = (id: string): string =>
-  path.join(homePath(), "operational-requirements", id, "edit");
+  path.join("/", ADMIN_APP_DIR, "operational-requirements", id, "edit");
 
-export const poolTablePath = (): string => path.join(homePath(), "pools");
+export const poolTablePath = (): string =>
+  path.join("/", ADMIN_APP_DIR, "pools");
 export const poolCreatePath = (): string =>
-  path.join(homePath(), "pools", "create");
+  path.join("/", ADMIN_APP_DIR, "pools", "create");
 export const poolUpdatePath = (id: string): string =>
-  path.join(homePath(), "pools", id, "edit");
+  path.join("/", ADMIN_APP_DIR, "pools", id, "edit");
 
 export const poolCandidateTablePath = (poolId: string): string =>
-  path.join(homePath(), "pools", poolId, "pool-candidates");
+  path.join("/", ADMIN_APP_DIR, "pools", poolId, "pool-candidates");
 export const poolCandidateCreatePath = (poolId: string): string =>
-  path.join(homePath(), "pools", poolId, "pool-candidates", "create");
+  path.join("/", ADMIN_APP_DIR, "pools", poolId, "pool-candidates", "create");
 export const poolCandidateUpdatePath = (
   poolId: string,
   poolCandidateId: string,
 ): string =>
   path.join(
-    homePath(),
+    "/",
+    ADMIN_APP_DIR,
     "pools",
     poolId,
     "pool-candidates",
@@ -55,13 +58,14 @@ export const poolCandidateUpdatePath = (
     "edit",
   );
 
-export const userTablePath = (): string => path.join(homePath(), "users");
+export const userTablePath = (): string =>
+  path.join("/", ADMIN_APP_DIR, "users");
 export const userCreatePath = (): string =>
-  path.join(homePath(), "users", "create");
+  path.join("/", ADMIN_APP_DIR, "users", "create");
 export const userUpdatePath = (id: string): string =>
-  path.join(homePath(), "users", id, "edit");
+  path.join("/", ADMIN_APP_DIR, "users", id, "edit");
 
 export const searchRequestTablePath = (): string =>
-  path.join(homePath(), "search-requests");
+  path.join("/", ADMIN_APP_DIR, "search-requests");
 export const searchRequestUpdatePath = (id: string): string =>
-  path.join(homePath(), "search-requests", id, "edit");
+  path.join("/", ADMIN_APP_DIR, "search-requests", id, "edit");
