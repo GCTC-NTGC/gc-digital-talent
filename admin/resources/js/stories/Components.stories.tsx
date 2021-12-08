123aaa74a32e3d81111be59430e66ecbb705bd80
Matt Nigh <matt@mattnigh.com>
2021-12-08 14:31:49 -0500

    Remove admin, talentsearch legacy Footer components

--- admin/resources/js/stories/Components.stories.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { storiesOf } from "@storybook/react";
-import Footer from "../components/Footer";
-
-const stories = storiesOf("Components", module);
-
-stories.add("Footer", () => {
-  return <Footer />;
-});
