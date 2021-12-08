2d727d2f7861363b6c675444e07938311b385072
Matt Nigh <matt@mattnigh.com>
2021-12-08 14:00:46 -0500

    Add Header component to common

---
+++ common/src/components/Header/Header.stories.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Story, Meta } from "@storybook/react";
+import { Header as HeaderComponent } from "./Header";
+
+export default {
+  component: HeaderComponent,
+  title: "Components/Header",
+} as Meta;
+
+const TemplateHeader: Story = (args) => {
+  const { baseUrl } = args;
+  return <HeaderComponent baseUrl={baseUrl} />;
+};
+
+export const Header = TemplateHeader.bind({});
+
+Header.args = {
+  baseUrl: "http://localhost:8000",
+};
