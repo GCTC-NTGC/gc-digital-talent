8e6c40d0c8e0e30b02578de56db747470880adac
Matt Nigh <matt@mattnigh.com>
2021-12-08 14:02:29 -0500

    Remove admin, talentsearch legacy Header components

--- talentsearch/resources/js/stories/Components.stories.tsx
+++ talentsearch/resources/js/stories/Components.stories.tsx
@@ -1,13 +1,8 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
 import Footer from "../components/Footer";
-import Header from "../components/Header";
 
 const stories = storiesOf("Components", module);
-
-stories.add("Header", () => {
-  return <Header />;
-});
 
 stories.add("Footer", () => {
   return <Footer />;
