7d721e06624816a34233fd1c37c9a006157804b0
Matt <mnigh@users.noreply.github.com>
2021-12-10 09:33:25 -0500

    Update talentsearch/resources/js/talentSearchRoutes.ts
    Remove extra space

--- talentsearch/resources/js/talentSearchRoutes.ts
+++ talentsearch/resources/js/talentSearchRoutes.ts
@@ -1,4 +1,4 @@
-import TALENTSEARCH_APP_DIR  from "./talentSearchConstants";
+import TALENTSEARCH_APP_DIR from "./talentSearchConstants";
 
 export const homePath = (): string => `${TALENTSEARCH_APP_DIR}`;
 export const searchPath = (): string => `${homePath()}/search`;
