218f096f5f614898b5e778d0d6d8bc297cabc113
Matt Nigh <matt@mattnigh.com>
2021-12-14 22:22:25 -0500

    Add Language Toggle to Header component

--- common/src/components/Header/Header.tsx
+++ common/src/components/Header/Header.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import { useIntl } from "react-intl";
+import { getLocale } from "../../helpers/localize";
 import { imageUrl } from "../../helpers/router";
 
 export const Header: React.FunctionComponent<{
   baseUrl: string;
-}> = ({ baseUrl }) => {
+  currentPath: string;
+}> = ({ baseUrl, currentPath }) => {
   const intl = useIntl();
+  const languageTogglePath = `/${
+    intl.locale !== "en" ? "en" : "fr"
+  }${currentPath.substring(3)}`; // Get first three characters (e.g. /en)
   return (
     <header data-h2-border="b(gray, bottom, solid, s)">
       <div data-h2-flex-grid="b(middle, contained, flush, xl)">
@@ -14,7 +19,7 @@ export const Header: React.FunctionComponent<{
           data-h2-text-align="b(center) m(left)"
         >
           <a
-            href={`https://www.canada.ca/${intl.locale}.html`}
+            href={`https://www.canada.ca/${getLocale(intl)}.html`}
             title={intl.formatMessage({
               defaultMessage: "Visit Canada.ca",
               description: "Title for the Canada logo in the Header.",
@@ -30,6 +35,24 @@ export const Header: React.FunctionComponent<{
                 description: "Alt text for the Canada logo in the Header.",
               })}
             />
+          </a>
+        </div>
+        <div
+          data-h2-flex-item="b(1of1) m(1of2)"
+          data-h2-text-align="b(center) m(right)"
+        >
+          <a
+            href={languageTogglePath}
+            title={intl.formatMessage({
+              defaultMessage: "Change language",
+              description: "Title for the language toggle link.",
+            })}
+          >
+            {intl.formatMessage({
+              defaultMessage: "Français",
+              description:
+                "Title for the language toggle alternate language in that Language.",
+            })}
           </a>
         </div>
       </div>
