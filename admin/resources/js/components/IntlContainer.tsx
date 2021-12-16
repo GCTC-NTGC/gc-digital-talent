cdfb0e19fafb0ba0b371fb95089d5488c287b1d5
Peter Giles <petertgiles@gmail.com>
2021-12-16 09:29:36 -0500

    revert container to main

--- admin/resources/js/components/IntlContainer.tsx
+++ admin/resources/js/components/IntlContainer.tsx
@@ -1,23 +1,11 @@
 import React from "react";
 import { IntlProvider } from "react-intl";
 
-function loadLocaleData(locale: string) {
-  switch (locale) {
-    case "fr":
-      // eslint-disable-next-line global-require
-      return require("../lang/frCompiled.json");
-    default:
-      return undefined;
-  }
-}
-
 const IntlContainer: React.FunctionComponent<{ locale: string }> = ({
   locale,
   children,
 }): React.ReactElement => (
-  <IntlProvider locale={locale} messages={loadLocaleData(locale)}>
-    {children}
-  </IntlProvider>
+  <IntlProvider locale={locale}>{children}</IntlProvider>
 );
 
 export default IntlContainer;
