ddb4e50229ad9dc4c582066152ef71a90ab9a201
Brinda Sasikumar <brinda.sasikumar@gmail.com>
2022-01-04 17:29:51 -0500

    1464-apply peer feedback to move coverage folder and add to .gitignore

--- talentsearch/jest.config.js
+++ talentsearch/jest.config.js
@@ -13,6 +13,7 @@ module.exports = {
     "resources/js/**/*.{js,jsx,ts,tsx}",
     "!<rootDir>/node_modules/",
   ],
+  coverageDirectory: "resources/js/tests/coverage",
 
   // Module file extensions for importing
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
