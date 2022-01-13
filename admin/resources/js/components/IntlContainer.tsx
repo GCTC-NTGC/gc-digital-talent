3c67113a5d1d01793cd7dd56ee2f8b372cad8536
tristan-orourke <tristan.m.orourke@gmail.com>
2022-01-13 14:49:30 -0500

    Load french text data if using fr locale

--- admin/resources/js/components/IntlContainer.tsx
+++ admin/resources/js/components/IntlContainer.tsx
@@ -1,11 +1,25 @@
 import React from "react";
 import { IntlProvider } from "react-intl";
 
+function loadLocaleData(locale: string) {
+  switch (locale) {
+    case "fr":
+      // eslint-disable-next-line global-require
+      return require("../lang/frCompiled.json");
+    default:
+      return undefined;
+  }
+}
+
 const IntlContainer: React.FunctionComponent<{ locale: string }> = ({
   locale,
   children,
-}): React.ReactElement => (
-  <IntlProvider locale={locale}>{children}</IntlProvider>
-);
+}): React.ReactElement => {
+  return (
+    <IntlProvider locale={locale} messages={loadLocaleData(locale)}>
+      {children}
+    </IntlProvider>
+  );
+};
 
 export default IntlContainer;
