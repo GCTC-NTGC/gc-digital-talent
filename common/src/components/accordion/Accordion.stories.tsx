6454eaffa4c92932a2dde8721ef1a12f032ae172
Brinda Sasikumar <brinda.sasikumar@gmail.com>
2022-01-18 10:08:49 -0500

    1534-add more props and styles

--- common/src/components/accordion/Accordion.stories.tsx
+++ common/src/components/accordion/Accordion.stories.tsx
@@ -1,25 +1,50 @@
 import React from "react";
 import { Story, Meta } from "@storybook/react";
-import { Accordion as AccordionComponent, Section } from "./Accordion";
+import {
+  Accordion as AccordionComponent,
+  Section,
+  AccordionProps,
+} from "./Accordion";
 
 export default {
   component: AccordionComponent,
   title: "Components/Accordion",
+  args: {
+    title: "Accordion Title",
+  },
+  argTypes: {
+    title: {
+      name: "title",
+      type: { name: "string", required: true },
+      control: {
+        type: "text",
+      },
+    },
+  },
 } as Meta;
 
-const TemplateAccordion: Story = () => {
+const TemplateAccordion: Story<AccordionProps & { title: string }> = (args) => {
+  const { SectionTitle1, SectionTitle2, SectionTitle3, ...rest } = args;
+
   return (
-    <AccordionComponent>
-      <Section heading="One">
-        <AccordionComponent>
-          <Section heading="level1">Level 1 </Section>
-          <Section heading="level2">Level 2 </Section>
-        </AccordionComponent>
+    <AccordionComponent defaultOpenIndex="undefined" {...rest}>
+      <Section title={SectionTitle1} subtitle="subtitle" icon="">
+        {" "}
+        first section
       </Section>
-      <Section heading="Two">Lorem ipsum dolor sit amet.</Section>
-      <Section heading="Three">Lorem ipsum dolor sit amet.</Section>
+      <Section title={SectionTitle2} subtitle="" icon={undefined}>
+        Lorem ipsum dolor sit amet.
+      </Section>
+      <Section title={SectionTitle3} subtitle="" icon={undefined}>
+        Lorem ipsum dolor sit amet.
+      </Section>
     </AccordionComponent>
   );
 };
 
 export const Accordion = TemplateAccordion.bind({});
+Accordion.args = {
+  SectionTitle1: "^ Section 1 title",
+  SectionTitle2: "Section 2 title",
+  SectionTitle3: "Section 3 title",
+};
