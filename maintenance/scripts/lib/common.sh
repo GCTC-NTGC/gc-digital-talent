1aade9e3cbe5cd1f3583aee8dd9c523a4f0b8445
Patrick Connolly <patrick.c.connolly@gmail.com>
2022-01-18 16:09:17 -0500

    Added comment to help with debugging scripts.

--- maintenance/scripts/lib/common.sh
+++ maintenance/scripts/lib/common.sh
@@ -9,5 +9,9 @@ set -o errexit
 # Don't mask errors in piped commands (raise first non-zero exit code).
 set -o pipefail
 
+# Print commands (and bash scripts/source files) while executing.
+# Scripts get very noisy. Uncomment for debugging.
+#set -o verbose
+
 # Fail if using undefined variables.
 set -o nounset
