fc5cd2eca5e601eea205bb3ea9d03d2bcf87b846
Patrick Connolly <patrick.c.connolly@gmail.com>
2022-01-18 00:54:02 -0500

    Removed mention of node_modules from .storybook/main.js

--- common/.storybook/main.js
+++ common/.storybook/main.js
@@ -39,10 +39,6 @@ module.exports = {
 
     config.resolve.alias = {
         ...config.resolve.alias,
-        "react": path.resolve('./node_modules/react'),
-        "react-dom": path.resolve('./node_modules/react-dom'),
-        "react-hook-form": path.resolve('./node_modules/react-hook-form'),
-        "react-intl": path.resolve("./node_modules/react-intl"),
     }
 
     config.module.rules = [
--- talentsearch/.storybook/main.js
+++ talentsearch/.storybook/main.js
@@ -40,10 +40,6 @@ module.exports = {
 
     config.resolve.alias = {
         ...config.resolve.alias,
-        "react": path.resolve('./node_modules/react'),
-        "react-dom": path.resolve('./node_modules/react-dom'),
-        "react-hook-form": path.resolve('./node_modules/react-hook-form'),
-        "react-intl": path.resolve("./node_modules/react-intl"),
         "@common": path.resolve('../common/src'),
     }
 
