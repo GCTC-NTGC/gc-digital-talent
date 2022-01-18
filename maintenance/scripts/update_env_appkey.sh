2922bc5830b51d456b3e749175efc0df0556f03a
Patrick Connolly <patrick.c.connolly@gmail.com>
2022-01-18 15:08:29 -0500

    Use random string generation that will work on more platforms, and won't fail out with pipefail.

--- maintenance/scripts/update_env_appkey.sh
+++ maintenance/scripts/update_env_appkey.sh
@@ -3,5 +3,6 @@
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 source ${parent_path}/lib/common.sh
 
-appkey=`cat /dev/urandom | tr -dc '[a-zA-Z0-9]' | fold -w32 - | head -n 1`
+# Random string generation that works on OSX and Linux. (Plus not pipefails.)
+appkey=`dd if=/dev/urandom bs=48 count=1 2>/dev/null | base64 | fold -w32 - | head -n 1`
 sed -i "s/APP_KEY=.*/APP_KEY=$appkey/" $1
