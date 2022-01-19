78ff55f7094bef299e3923948aec70b0ae69211f
Brinda Sasikumar <brinda.sasikumar@gmail.com>
2022-01-18 19:09:31 -0500

    1534-fix  styling for collapsed border

--- frontend/common/src/components/accordion/Section.tsx
+++ frontend/common/src/components/accordion/Section.tsx
@@ -33,43 +33,50 @@ export const Section: React.FC<InternalSectionProps> = ({
     isActive && "accordion-section__content--active",
   );
   return (
-    <div
-      className={sectionClass}
-      data-h2-border="b(darkpurple, left, solid, m)"
+    <><div
+      data-h2-shadow="b(m)"
+      data-h2-radius="b(none, s, s, none)"
+      data-h2-overflow="b(all, hidden)"
     >
       <button
         type="button"
         data-h2-text-align="b(left)"
+        data-h2-bg-color="b(white)"
+        data-h2-padding="b(top-bottom, s) b(right, m) b(left, s)"
+        data-h2-width="b(100)"
         onClick={() => setActiveIndex(index)}
-      >
-        <div className="header">
-          <span>
-            {isActive ? (
-              <ChevronDownIcon height="20" />
-            ) : (
-              <ChevronRightIcon height="20" />
-            )}
-          </span>
-          <span
-            className="title"
-            data-h2-font-family="b(sans)"
-            data-h2-font-size="b(h5)"
-          >
+        data-h2-border="b(darkpurple, left, solid, m)"
+        style={{borderTop:"none", borderRight:"none", borderBottom:"none"}}
+        >
+
+      <div data-h2-flex-grid="b(middle, expanded, flush, s)">
+        <span>
+          {isActive ? (
+            <ChevronDownIcon height="20" />
+          ) : (
+            <ChevronRightIcon height="20" />
+          )}
+        </span>
+        <div data-h2-flex-item="b(auto)" data-h2-text-align="b(left)">
+          <p data-h2-margin="b(all, none)" data-h2-font-size="b(h5)">
             {title}
-          </span>
+          </p>
+          <p data-h2-margin="b(top, xxs) b(bottom, none)">{subtitle}</p>
+        </div>
+        <div data-h2-flex-item="b(content)">
           <span className="icon" data-h2-text-align="b(left)">
             {Icon && <Icon height="20px" width="20px" />}
           </span>
-          <div>
-            <span data-h2-font-family="b(sans)" data-h2-font-size="b(normal)">
-              {subtitle}
-            </span>
-          </div>
         </div>
-      </button>
-      <div className={contentClass} ref={contentRef}>
-        {isActive && children}
       </div>
+    </button><div
+      className={contentClass}
+      ref={contentRef}
+      data-h2-border="b(lightpurple, left, solid, m)"
+      id="content"
+    >
+        {isActive && children}
+      </div></>
     </div>
   );
 };
