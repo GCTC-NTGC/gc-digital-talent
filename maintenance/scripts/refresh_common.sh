ebb59d7338c99d55211023e1063300c0b18e48a0
Patrick Connolly <patrick.c.connolly@gmail.com>
2022-01-19 00:55:01 -0500

    Updated paths in refresh scripts.

--- maintenance/scripts/refresh_common.sh
+++ maintenance/scripts/refresh_common.sh
@@ -4,8 +4,10 @@
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
-cd /var/www/html/common
+cd /var/www/html/frontend
 nvm install --latest-npm
 npm install
+
+cd /var/www/html/frontend/common
 npm run h2-build
 npm run codegen
