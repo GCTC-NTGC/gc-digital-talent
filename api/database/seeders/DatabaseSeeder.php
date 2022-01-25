bfbfc6696888a9f4ec2e2fa55c39dcc24b395aa0
Peter Giles <petertgiles@gmail.com>
2022-01-25 09:23:43 -0500

    Move test users to separate seeder

--- api/database/seeders/DatabaseSeeder.php
+++ api/database/seeders/DatabaseSeeder.php
@@ -29,7 +29,7 @@ class DatabaseSeeder extends Seeder
         $this->call(OperationalRequirementSeeder::class);
         $this->call(DepartmentSeeder::class);
 
-        $this->loadUsers();
+        $this->call(UserSeederLocal::class);
         $this->call(PoolSeeder::class);
 
         PoolCandidate::factory()
@@ -63,51 +63,5 @@ class DatabaseSeeder extends Seeder
         PoolCandidateFilter::truncate();
         PoolCandidateSearchRequest::truncate();
         User::truncate();
-    }
-
-    // set up some users to allow authentication
-    private function loadUsers()
-    {
-        // shared Laravel auth user
-        User::factory()->create([
-            'email' => 'admin@test.com',
-            'sub' => 'admin@test.com',
-            'roles' => ['ADMIN']
-        ]);
-
-        $fakeEmailDomain = '@test.com';
-        User::factory()->create([
-            'email' => 'petertgiles'.$fakeEmailDomain,
-            'sub' => '4810df0d-fcb6-4353-af93-b25c0a5a9c3e', // SiC to localhost - PG
-            'roles' => ['ADMIN']
-        ]);
-        User::factory()->create([
-            'email' => 'gggrant'.$fakeEmailDomain,
-            'sub' => 'cd537460-1fee-40bd-ada6-8ee40b6f63c9', // SiC to localhost - GB
-            'roles' => ['ADMIN']
-        ]);
-        User::factory()->create([
-            'email' => 'yonikid15'.$fakeEmailDomain,
-            'sub' => 'c65dd054-db44-4bf6-af39-37eedb39305d', // SiC to localhost - YK
-            'roles' => ['ADMIN']
-        ]);
-        User::factory()->create([
-            'email' => 'JamesHuf'.$fakeEmailDomain,
-            'sub' => 'e64b8057-0eaf-4a19-a14a-4a93fa2e8a04', // SiC to localhost - JH
-            'roles' => ['ADMIN']
-        ]);
-        User::factory()->create([
-            'email' => 'brindasasi'.$fakeEmailDomain,
-            'sub' => '2e72b97b-017a-4ed3-a803-a8773c2e1b14', // SiC to localhost - BS
-            'roles' => ['ADMIN']
-        ]);
-        User::factory()->create([
-            'email' => 'tristan-orourke'.$fakeEmailDomain,
-            'sub' => 'b4c734a1-dcf3-4fb8-a860-c642700cb0b8', // SiC to localhost - TO
-            'roles' => ['ADMIN']
-        ]);
-
-
-
     }
 }
