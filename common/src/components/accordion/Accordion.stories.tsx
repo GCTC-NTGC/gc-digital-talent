3c4a67ebb887755e01e3104c1f67284c88a95e32
Brinda Sasikumar <Brinda.Sasikumar@gmail.com>
2022-02-01 08:49:52 -0500

    1534 - use same story with diff args

--- common/src/components/accordion/Accordion.stories.tsx
+++ common/src/components/accordion/Accordion.stories.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Story, Meta } from "@storybook/react";
 import { AcademicCapIcon } from "@heroicons/react/solid";
 import { Accordion as AccordionComponent, AccordionProps } from "./Accordion";
+import { Button } from "../Button";
 
 export default {
   component: AccordionComponent,
@@ -9,7 +10,7 @@ export default {
 } as Meta;
 
 const TemplateAccordion: Story<AccordionProps> = (args) => {
-  const { title, subtitle, context } = args;
+  const { title, subtitle, context, simple, defaultOpen, children } = args;
 
   return (
     <AccordionComponent
@@ -17,17 +18,58 @@ const TemplateAccordion: Story<AccordionProps> = (args) => {
       subtitle={subtitle}
       context={context}
       Icon={AcademicCapIcon}
-      defaultOpen
-      simple={false}
+      defaultOpen={defaultOpen}
+      simple={simple}
     >
-      Lorem ipsum dolor sit amet.
+      {children}
     </AccordionComponent>
   );
 };
 
-export const Accordion = TemplateAccordion.bind({});
-Accordion.args = {
+export const AccordionFull = TemplateAccordion.bind({});
+export const AccordionSimple = TemplateAccordion.bind({});
+export const AccordionNested = TemplateAccordion.bind({});
+export const AccordionDefaultOpen = TemplateAccordion.bind({});
+
+AccordionFull.args = {
+  title: "Full Accordion With Borders",
+  subtitle: "subtitle",
+  context: "context",
+  children: "Lorem ipsum dolor sit amet.",
+};
+
+AccordionSimple.args = {
+  title: "Simple Accordion",
+  subtitle: "subtitle",
+  context: "context",
+  simple: true,
+  children: "Lorem ipsum dolor sit amet.",
+};
+
+AccordionNested.args = {
   title: "title",
   subtitle: "subtitle",
   context: "context",
+  children: (
+    <AccordionComponent
+      title="nested title"
+      subtitle="nested subtitle"
+      context="nested context"
+      Icon={AcademicCapIcon}
+      defaultOpen={false}
+      simple={false}
+    >
+      <Button mode="outline" color="primary">
+        <span>SUBMIT </span>
+      </Button>
+    </AccordionComponent>
+  ),
+};
+
+AccordionDefaultOpen.args = {
+  title: "title",
+  subtitle: "subtitle",
+  context: "context",
+  defaultOpen: true,
+  children: "Lorem ipsum dolor sit amet.",
 };
