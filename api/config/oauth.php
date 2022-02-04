cf395ebda65db5058f595ad86b5505dc6f62d560
Peter Giles <petertgiles@gmail.com>
2022-02-04 15:13:28 -0500

    Add allowable clock skew to token validation

--- api/config/oauth.php
+++ api/config/oauth.php
@@ -36,4 +36,17 @@ return [
     |
     */
     'server_public_key' => env('AUTH_SERVER_PUBLIC_KEY'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Allowable Clock Skew
+    |--------------------------------------------------------------------------
+    |
+    | According to CATSv3, section 4.1, Deployments MUST allow between three (3)
+    | and five (5) minutes of clock skew — in either direction — when interpreting
+    | the exp and nbf claims in ID tokens and when enforcing security policies
+    | based thereupon.
+    |
+    */
+    'allowable_clock_skew' =>  DateInterval::createFromDateString('4 minutes'),
 ];
