439257d221b35bd9854b9a33c4e036f0e35bead2
vd1992 <40485260+vd1992@users.noreply.github.com>
2022-02-08 12:43:30 -0700

    Storybook+toggle
    Storybook fixes + toggle functionality

--- frontend/common/src/components/WorkExperience/WorkExperience.stories.tsx
+++ frontend/common/src/components/WorkExperience/WorkExperience.stories.tsx
@@ -4,62 +4,21 @@ import { Meta, Story } from "@storybook/react";
 import { Checkbox } from "../form/Checkbox";
 import { Input } from "../form/Input";
 import WorkExperience from "./WorkExperience";
+import Form from "../form/BasicForm";
+import Submit from "../form/Submit";
 
 export default {
   component: WorkExperience,
   title: "WorkExperience",
 } as Meta;
 
-const TemplateWorkExperience = (args) => {
+const TemplateWorkExperience: Story = () => {
   return (
-    <div>
-      <Input
-        id="pending"
-        label="My Role"
-        name="role"
-        type="text"
-        rules={args.required}
-      />
-
-      <Input
-        id="pending"
-        label="Organization"
-        name="organization"
-        type="text"
-        rules={args.required}
-      />
-
-      <Input
-        id="pending"
-        label="Team, Group or Division"
-        name="team"
-        type="text"
-        hideOptional
-      />
-
-      <Checkbox id="pending" label="Current Role" name="current-role" />
-
-      <Input
-        id="pending"
-        label="Start Date"
-        name="start-date"
-        type="date"
-        rules={args.required}
-      />
-
-      <Input
-        id="pending"
-        label="End Date"
-        name="end-date"
-        type="date"
-        rules={args.required}
-      />
-    </div>
+    <Form onSubmit={action("submit")}>
+      <WorkExperience />
+      <Submit />
+    </Form>
   );
 };
 
 export const IndividualWorkExperience = TemplateWorkExperience.bind({});
-
-IndividualWorkExperience.args = {
-  required: true,
-};
--- frontend/common/src/components/WorkExperience/WorkExperience.tsx
+++ frontend/common/src/components/WorkExperience/WorkExperience.tsx
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import { useIntl } from "react-intl";
 import { Checkbox } from "../form/Checkbox";
 import { Input } from "../form/Input";
 
 export const WorkExperience = () => {
   const rules = { required: true };
+  const intl = useIntl();
+
+  // given that the current role state needs to effect whether an end date is required according to the wireframe, here is something to handle that
+  // default state is required as box is unchecked by default, onChange in the toggle input calls the function to flip its state which is then called in the end date input
+  // adjusting required or not
+  const [checkboxState, setCheckbox] = useState({ required: true });
+  const checkboxToggle = () => {
+    if (checkboxState.required === true) {
+      setCheckbox({ required: false });
+    } else {
+      setCheckbox({ required: true });
+    }
+  };
+
   return (
     <div>
       <Input
         id="pending"
-        label="My Role"
+        label={intl.formatMessage({
+          defaultMessage: "My Role",
+          description: "Label displayed on Work Experience form for role input",
+        })}
         name="role"
         type="text"
         rules={rules}
@@ -16,7 +34,11 @@ export const WorkExperience = () => {
 
       <Input
         id="pending"
-        label="Organization"
+        label={intl.formatMessage({
+          defaultMessage: "Organization",
+          description:
+            "Label displayed on Work Experience form for organization input",
+        })}
         name="organization"
         type="text"
         rules={rules}
@@ -24,17 +46,34 @@ export const WorkExperience = () => {
 
       <Input
         id="pending"
-        label="Team, Group or Division"
+        label={intl.formatMessage({
+          defaultMessage: "Team, Group, or Division",
+          description:
+            "Label displayed on Work Experience form for team/group/division input",
+        })}
         name="team"
         type="text"
         hideOptional
       />
 
-      <Checkbox id="pending" label="Current Role" name="current-role" />
+      <Checkbox
+        id="pending"
+        label={intl.formatMessage({
+          defaultMessage: "Current Role",
+          description:
+            "Label displayed on Work Experience form for current role input",
+        })}
+        name="current-role"
+        onChange={checkboxToggle}
+      />
 
       <Input
         id="pending"
-        label="Start Date"
+        label={intl.formatMessage({
+          defaultMessage: "Start Date",
+          description:
+            "Label displayed on Work Experience form for start date input",
+        })}
         name="start-date"
         type="date"
         rules={rules}
@@ -42,10 +81,15 @@ export const WorkExperience = () => {
 
       <Input
         id="pending"
-        label="End Date"
+        label={intl.formatMessage({
+          defaultMessage: "End Date",
+          description:
+            "Label displayed on Work Experience form for end date input",
+        })}
         name="end-date"
         type="date"
-        rules={rules}
+        rules={checkboxState}
+        hideOptional
       />
     </div>
   );
