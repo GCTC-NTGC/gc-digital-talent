3230f1c41ad4d586e9060b1e170869dcdb0e293c
vd1992 <40485260+vd1992@users.noreply.github.com>
2022-02-08 16:13:23 -0700

    states method change
    setState -> useWatch

--- frontend/common/src/components/WorkExperience/WorkExperience.tsx
+++ frontend/common/src/components/WorkExperience/WorkExperience.tsx
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React from "react";
 import { useIntl } from "react-intl";
+import { useWatch } from "react-hook-form";
 import { Checkbox } from "../form/Checkbox";
 import { Input } from "../form/Input";
 
@@ -7,17 +8,8 @@ export const WorkExperience = () => {
   const rules = { required: true };
   const intl = useIntl();
 
-  // given that the current role state needs to effect whether an end date is required according to the wireframe, here is something to handle that
-  // default state is required as box is unchecked by default, onChange in the toggle input calls the function to flip its state which is then called in the end date input
-  // adjusting required or not
-  const [checkboxState, setCheckbox] = useState({ required: true });
-  const checkboxToggle = () => {
-    if (checkboxState.required === true) {
-      setCheckbox({ required: false });
-    } else {
-      setCheckbox({ required: true });
-    }
-  };
+  // to toggle whether End Date is required, the state of the Current Role checkbox must be monitored and have to adjust the form accordingly
+  const isCurrent = useWatch({ name: "current-role", defaultValue: false });
 
   return (
     <div>
@@ -64,7 +56,6 @@ export const WorkExperience = () => {
             "Label displayed on Work Experience form for current role input",
         })}
         name="current-role"
-        onChange={checkboxToggle}
       />
 
       <Input
@@ -79,18 +70,20 @@ export const WorkExperience = () => {
         rules={rules}
       />
 
-      <Input
-        id="pending"
-        label={intl.formatMessage({
-          defaultMessage: "End Date",
-          description:
-            "Label displayed on Work Experience form for end date input",
-        })}
-        name="end-date"
-        type="date"
-        rules={checkboxState}
-        hideOptional
-      />
+      {/* conditionally render the end-date based off the state attached to the checkbox input */}
+      {!isCurrent && (
+        <Input
+          id="pending"
+          label={intl.formatMessage({
+            defaultMessage: "End Date",
+            description:
+              "Label displayed on Work Experience form for end date input",
+          })}
+          name="end-date"
+          type="date"
+          rules={isCurrent ? {} : rules}
+        />
+      )}
     </div>
   );
 };
