c7b982af02d30f3f2b0267a2f92dd6c5d7c6d629
vd1992 <40485260+vd1992@users.noreply.github.com>
2022-02-10 09:53:21 -0700

    requested changes

--- frontend/talentsearch/resources/js/components/WorkExperience/WorkExperience.tsx
+++ frontend/talentsearch/resources/js/components/WorkExperience/WorkExperience.tsx
@@ -3,9 +3,9 @@ import { useIntl } from "react-intl";
 import { useWatch } from "react-hook-form";
 import { Input } from "@common/components/form/Input";
 import { Checkbox } from "@common/components/form/Checkbox";
+import { errorMessages } from "@common/messages";
 
-export const WorkExperience = () => {
-  const rules = { required: true };
+export const WorkExperience: React.FunctionComponent = () => {
   const intl = useIntl();
 
   // to toggle whether End Date is required, the state of the Current Role checkbox must be monitored and have to adjust the form accordingly
@@ -13,19 +13,23 @@ export const WorkExperience = () => {
 
   return (
     <div>
-      <h3>1. Work Experience Details</h3>
-      <p>Share your experiences gained from full-time, part-time, self-employment, fellowships or internships.</p>
+      <h2>1. Work Experience Details</h2>
+      <p>
+        Share your experiences gained from full-time, part-time,
+        self-employment, fellowships or internships.
+      </p>
       <div data-h2-display="b(flex)" data-h2-padding="b(top, m)">
         <div data-h2-padding="b(right, l)">
           <Input
             id="role"
             label={intl.formatMessage({
               defaultMessage: "My Role",
-              description: "Label displayed on Work Experience form for role input",
+              description:
+                "Label displayed on Work Experience form for role input",
             })}
             name="role"
             type="text"
-            rules={rules}
+            rules={{ required: intl.formatMessage(errorMessages.required) }}
           />
 
           <Input
@@ -37,7 +41,7 @@ export const WorkExperience = () => {
             })}
             name="organization"
             type="text"
-            rules={rules}
+            rules={{ required: intl.formatMessage(errorMessages.required) }}
           />
 
           <Input
@@ -49,51 +53,54 @@ export const WorkExperience = () => {
             })}
             name="team"
             type="text"
-
           />
         </div>
         <div>
-            <Checkbox
-              id="current-role"
-              label={intl.formatMessage({
-                defaultMessage: "I am currently active in this role",
-                description:
-                  "Label displayed on Work Experience form for current role input",
-              })}
-              name="current-role"
-            />
-            <div data-h2-display="b(flex)" data-h2-flex-direction="MEDIA(row)" >
-              <div data-h2-padding="b(right, l)">
+          <Checkbox
+            id="current-role"
+            label={intl.formatMessage({
+              defaultMessage: "I am currently active in this role",
+              description:
+                "Label displayed on Work Experience form for current role input",
+            })}
+            name="current-role"
+          />
+          <div data-h2-display="b(flex)">
+            <div data-h2-padding="b(right, l)">
+              <Input
+                id="start-date"
+                label={intl.formatMessage({
+                  defaultMessage: "Start Date",
+                  description:
+                    "Label displayed on Work Experience form for start date input",
+                })}
+                name="start-date"
+                type="date"
+                rules={{ required: intl.formatMessage(errorMessages.required) }}
+              />
+            </div>
+            <div>
+              {/* conditionally render the end-date based off the state attached to the checkbox input */}
+              {!isCurrent && (
                 <Input
-                  id="start-date"
+                  id="end-date"
                   label={intl.formatMessage({
-                    defaultMessage: "Start Date",
+                    defaultMessage: "End Date",
                     description:
-                      "Label displayed on Work Experience form for start date input",
+                      "Label displayed on Work Experience form for end date input",
                   })}
-                  name="start-date"
+                  name="end-date"
                   type="date"
-                  rules={rules}
+                  rules={
+                    isCurrent
+                      ? {}
+                      : { required: intl.formatMessage(errorMessages.required) }
+                  }
                 />
-              </div>
-              <div>
-                {/* conditionally render the end-date based off the state attached to the checkbox input */}
-                {!isCurrent && (
-                  <Input
-                    id="end-date"
-                    label={intl.formatMessage({
-                      defaultMessage: "End Date",
-                      description:
-                        "Label displayed on Work Experience form for end date input",
-                    })}
-                    name="end-date"
-                    type="date"
-                    rules={isCurrent ? {} : rules}
-                  />
-                )}
-              </div>
+              )}
             </div>
           </div>
+        </div>
       </div>
     </div>
   );
