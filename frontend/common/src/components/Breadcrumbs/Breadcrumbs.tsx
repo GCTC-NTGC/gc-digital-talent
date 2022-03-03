342cbfbc553d3b22509ef35386a3b4e25afb1034
vd1992 <40485260+vd1992@users.noreply.github.com>
2022-03-03 14:36:45 -0700

    menu with chevrons generated

--- frontend/common/src/components/Breadcrumbs/Breadcrumbs.tsx
+++ frontend/common/src/components/Breadcrumbs/Breadcrumbs.tsx
@@ -6,6 +6,7 @@ export interface BreadcrumbsProps {
 }
 
 const Breadcrumbs: React.FunctionComponent<BreadcrumbsProps> = ({ links }) => {
+  // taking in the array of links as defined above, create an array of Link components
   const arrayLinks = links.map((link, index) => (
     // eslint-disable-next-line react/no-array-index-key
     <Link href={link.href} title={link.title} key={index}>
@@ -13,7 +14,24 @@ const Breadcrumbs: React.FunctionComponent<BreadcrumbsProps> = ({ links }) => {
     </Link>
   ));
 
-  return <div data-h2-display="b(flex)">{arrayLinks}</div>;
+  // menu requires ">" after each link except the last one
+  // provided it is an array of more than one link, build another array of Links except this time add the ">" after every single one with an exception
+  // for if you are at the last link in which case do not add anything after it
+  let menuBar = [];
+  if (arrayLinks.length > 1) {
+    for (let i = 0; i < arrayLinks.length; i += 1) {
+      if (i < arrayLinks.length - 1) {
+        menuBar.push(arrayLinks[i]);
+        menuBar.push(<span data-h2-padding="b(right-left, xs)">&gt;</span>);
+      } else {
+        menuBar.push(arrayLinks[i]);
+      }
+    }
+  } else {
+    menuBar = arrayLinks;
+  }
+
+  return <div data-h2-display="b(flex)">{menuBar}</div>;
 };
 
 export default Breadcrumbs;
