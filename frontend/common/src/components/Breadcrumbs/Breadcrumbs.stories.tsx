6c2e72d0edfce737d24c4ab6c6cd2f376845dc78
vd1992 <40485260+vd1992@users.noreply.github.com>
2022-03-07 11:50:22 -0700

    white font story

--- frontend/common/src/components/Breadcrumbs/Breadcrumbs.stories.tsx
+++ frontend/common/src/components/Breadcrumbs/Breadcrumbs.stories.tsx
@@ -17,13 +17,38 @@ export default {
   },
 } as Meta;
 
-const TemplateBreadcrumbs: Story<BreadcrumbsProps> = (args) => {
-  return <Breadcrumbs {...args} />;
+const BlackTemplateBreadcrumbs: Story<BreadcrumbsProps> = (args) => {
+  return (
+    <div data-h2-font-color="b(black)">
+      <Breadcrumbs {...args} />
+    </div>
+  );
 };
 
-export const IndividualBreadcrumbs = TemplateBreadcrumbs.bind({});
+const WhiteTemplateBreadcrumbs: Story<BreadcrumbsProps> = (args) => {
+  return (
+    <div data-h2-font-color="b(white)" data-h2-bg-color="b(black)">
+      <Breadcrumbs {...args} />
+    </div>
+  );
+};
 
-IndividualBreadcrumbs.args = {
+export const BlackFontBreadcrumbs = BlackTemplateBreadcrumbs.bind({});
+export const WhiteFontBreadcrumbs = WhiteTemplateBreadcrumbs.bind({});
+
+BlackFontBreadcrumbs.args = {
+  links: [
+    {
+      title: "My Name",
+      href: "localnothost",
+      icon: <FilterIcon style={{ width: "1rem", marginRight: "5px" }} />,
+    },
+    { title: "Not My Name", href: "nothost" },
+    { title: "No Link" },
+  ],
+};
+
+WhiteFontBreadcrumbs.args = {
   links: [
     {
       title: "My Name",
