34037fee376f1a7f1d1c4c67c6ea8a211ad76360
Peter Giles <petertgiles@gmail.com>
2022-03-14 15:30:35 -0400

    Fallback for DIR const

--- frontend/talentsearch/resources/js/talentSearchConstants.ts
+++ frontend/talentsearch/resources/js/talentSearchConstants.ts
@@ -1,4 +1,5 @@
-const TALENTSEARCH_APP_DIR = process.env.TALENTSEARCH_APP_DIR as string;
+const TALENTSEARCH_APP_DIR =
+  (process.env.TALENTSEARCH_APP_DIR as string) ?? "/talent"; // fallback used by storybook
 export default TALENTSEARCH_APP_DIR;
 export const DIGITAL_CAREERS_POOL_KEY =
   (process.env.DIGITAL_CAREERS_POOL_KEY as string) ?? "digital_careers";
