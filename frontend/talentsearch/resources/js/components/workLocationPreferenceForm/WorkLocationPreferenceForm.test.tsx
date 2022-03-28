29aa6fa7497d6f3afec88e33f55cac439e862f72
“RVany” <vanmathyrr@gmail.com>
2022-03-28 17:23:04 -0400

    More testing added

--- frontend/talentsearch/resources/js/components/workLocationPreferenceForm/WorkLocationPreferenceForm.test.tsx
+++ frontend/talentsearch/resources/js/components/workLocationPreferenceForm/WorkLocationPreferenceForm.test.tsx
@@ -28,7 +28,7 @@ const renderWorkLocationPreference = () => {
 };
 
 describe("Work Location Preference Form tests", () => {
-  test("Can't submit unless form is valid (at least one location selected)", () => {
+  test("Can't submit unless form is valid (at least one location selected)", async () => {
     renderWorkLocationPreference();
     const saveAndGoButton = screen.getByText("Save and go back");
     fireEvent.click(saveAndGoButton);
@@ -37,5 +37,29 @@ describe("Work Location Preference Form tests", () => {
     expect(onClick).toHaveBeenCalledTimes(0);
   });
 
-  test("Form submits data in correct shape to submit handler when Save Changes is clicked.", () => {});
+  test("Can submit the form if at least one location is selected", async () => {
+    renderWorkLocationPreference();
+    const checkbox1 = screen.getByRole("checkbox", {
+      name: /Atlantic Region: New Bruns/i,
+    });
+    expect(checkbox1).toBeTruthy();
+
+    const saveAndGoButton = screen.getByRole("button", {
+      name: /Save and go back/i,
+    });
+
+    expect(saveAndGoButton).toBeTruthy();
+    const selectInput = screen.findByRole("checkbox", {
+      name: "workLocations",
+    });
+    expect(selectInput).toBeTruthy();
+
+    fireEvent.click(checkbox1);
+    fireEvent.click(saveAndGoButton);
+    // expect(onClick).toBeCalled();
+    // expect(onClick).toHaveBeenCalledTimes(1);
+    // expect(onClick).toBeCalledWith("ATLANTIC");
+  });
+
+  // test("Form submits data in correct shape to submit handler when Save Changes is clicked.", () => {});
 });
