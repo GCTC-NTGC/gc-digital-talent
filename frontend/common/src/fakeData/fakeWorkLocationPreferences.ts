896923ba8413537f56a6daaff1a4f42d29856ac8
“RVany” <vanmathyrr@gmail.com>
2022-03-30 21:14:53 -0400

    fakeWorkLocationPreferences.ts deleted

--- frontend/common/src/fakeData/fakeWorkLocationPreferences.ts
+++
@@ -1,9 +0,0 @@
-import faker from "faker";
-import { WorkRegion } from "../api/generated";
-
-const generateWorkLocation = () => ({
-  locationExemptions: faker.random.word(),
-  locationPreferences: [WorkRegion.Atlantic],
-});
-
-export default generateWorkLocation;
