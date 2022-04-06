8982e751bcfb3bd6fe9d119031b411a09344b06d
Peter Giles <8978655+petertgiles@users.noreply.github.com>
2022-04-06 11:39:19 -0400

    Check that hashes look right

--- frontend/common/src/tooling/checkIntegrity.js
+++ frontend/common/src/tooling/checkIntegrity.js
@@ -17,21 +17,67 @@ const { argv } = yargs(process.argv.slice(2))
   })
   .help();
 
-const countMissingIntegrityValues = (dependencies) =>
+// https://stackoverflow.com/a/55910865
+const base64Decode = (encodedString) => {
+  const buff = Buffer.from(encodedString, "base64");
+  const text = buff.toString("hex");
+  return text;
+};
+
+// Regular expressions to check for the right number of hex digits in hash
+const sha1RegExp = /^[a-f0-9]{40}$/i;
+const sha256RegExp = /^[a-f0-9]{64}$/i;
+const sha384RegExp = /^[a-f0-9]{96}$/i;
+const sha512RegExp = /^[a-f0-9]{128}$/i;
+
+const isValidIntegrity = (integrity) => {
+  const metadata = integrity.split("-");
+  const algo = metadata[0];
+  const digestHex = base64Decode(metadata[1]);
+  switch (algo) {
+    case "sha1":
+      return sha1RegExp.test(digestHex); // may want deprecate these at some point https://w3c.github.io/webappsec-subresource-integrity/#hash-functions
+    case "sha256":
+      return sha256RegExp.test(digestHex);
+    case "sha384":
+      return sha384RegExp.test(digestHex);
+    case "sha512":
+      return sha512RegExp.test(digestHex);
+    default:
+      return false; // unknown algorithm
+  }
+};
+
+const countInvalidIntegrityValues = (dependencies) =>
   Object.entries(dependencies).reduce(
     (previousCount, [packageName, packageProperties]) => {
-      let missingIntegrityCount = 0;
+      let invalidIntegrityCount = 0;
+
+      // test if package has an integrity value
       if (!packageProperties.integrity && argv.allow.indexOf(packageName) < 0) {
         console.log(`Package ${packageName} is missing the integrity`);
-        missingIntegrityCount += 1;
+        invalidIntegrityCount += 1;
       }
+
+      // test if package integrity is a valid hash
+      if (
+        packageProperties.integrity &&
+        !isValidIntegrity(packageProperties.integrity)
+      ) {
+        console.log(
+          `Package ${packageName} has the malformed integrity value ${packageProperties.integrity}`,
+        );
+        invalidIntegrityCount += 1;
+      }
+
+      // recurse if this package has its own dependencies
       if (packageProperties.dependencies) {
-        missingIntegrityCount += countMissingIntegrityValues(
+        invalidIntegrityCount += countInvalidIntegrityValues(
           packageProperties.dependencies,
         );
       }
 
-      return previousCount + missingIntegrityCount;
+      return previousCount + invalidIntegrityCount;
     },
     0,
   );
@@ -39,11 +85,11 @@ const countMissingIntegrityValues = (dependencies) =>
 console.log("Checking lock file integrity");
 const lockObj = JSON.parse(fs.readFileSync(argv["lock-file"]));
 
-const missingIntegrityCount = countMissingIntegrityValues(lockObj.dependencies);
+const invalidIntegrityCount = countInvalidIntegrityValues(lockObj.dependencies);
 
-if (missingIntegrityCount > 0) {
+if (invalidIntegrityCount > 0) {
   console.log(
-    `${missingIntegrityCount} dependencies are missing the integrity`,
+    `${invalidIntegrityCount} dependencies have an invalid integrity value`,
   );
   process.exit(1); // error exit code
 }
