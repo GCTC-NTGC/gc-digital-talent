ff6c0726e32a60caf3ec8d05195074c4c9e1136d
Eric Sizer <eric.sizer@gmail.com>
2022-04-06 14:02:38 -0400

    move profile wrapper to form component

--- frontend/talentsearch/src/js/components/aboutMeForm/AboutMeForm.tsx
+++ frontend/talentsearch/src/js/components/aboutMeForm/AboutMeForm.tsx
@@ -123,161 +123,183 @@ export const AboutMeForm: React.FunctionComponent<AboutMeFormProps> = ({
   };
 
   return (
-    <BasicForm
-      onSubmit={handleSubmit}
-      options={{
-        defaultValues: initialDataToFormValues(initialUser),
-      }}
+    <ProfileFormWrapper
+      description={intl.formatMessage({
+        defaultMessage:
+          "This information is used for account management and communications purposes, it has no impact on the job matching or job recommendations.",
+        description:
+          "Description text for the Profile Form wrapper in the About Me form",
+      })}
+      title={intl.formatMessage({
+        defaultMessage: "About me",
+        description: "Title for Profile Form wrapper in About me form",
+      })}
+      crumbs={[
+        {
+          title: intl.formatMessage({
+            defaultMessage: "About Me",
+            description: "Display text for About Me Form Page Link",
+          }),
+        },
+      ]}
     >
-      <h2 data-h2-font-size="b(h3)" data-h2-font-weight="b(700)">
-        {intl.formatMessage({
-          defaultMessage: "Personal Information",
-          description:
-            "Title for Personal Information section of the About Me form",
-        })}
-      </h2>
-      <p>
-        {intl.formatMessage({
-          defaultMessage:
-            "This additional personal information will be used only for communication purposes.",
-          description:
-            "Description for Personal Information section of the About Me form",
-        })}
-      </p>
-      <div data-h2-flex-item="b(1of1)" data-h2-padding="b(top, m)">
-        <div data-h2-padding="b(right, xxl)">
-          <RadioGroup
-            idPrefix="required-lang-preferences"
-            legend={intl.formatMessage({
-              defaultMessage: "Language preference for communication",
-              description:
-                "Legend text for required language preference in About Me form",
-            })}
-            name="preferredLang"
-            rules={{ required: intl.formatMessage(errorMessages.required) }}
-            items={enumToOptions(Language).map(({ value }) => ({
-              value,
-              label: intl.formatMessage(getLanguage(value)),
-            }))}
-          />
-          <Select
-            id="currentProvince"
-            name="currentProvince"
-            label={intl.formatMessage({
-              defaultMessage: "Current province or territory",
-              description:
-                "Label for current province or territory field in About Me form",
-            })}
-            nullSelection={intl.formatMessage({
-              defaultMessage: "Select a province or territory...",
-              description:
-                "Placeholder displayed on the About Me form province or territory field.",
-            })}
-            options={enumToOptions(ProvinceOrTerritory).map(({ value }) => ({
-              value,
-              label: intl.formatMessage(getProvinceOrTerritory(value)),
-            }))}
-            rules={{
-              required: intl.formatMessage(errorMessages.required),
-            }}
-          />
-          <Input
-            id="currentCity"
-            name="currentCity"
-            type="text"
-            label={intl.formatMessage({
-              defaultMessage: "Current city",
-              description: "Label for current city field in About Me form",
-            })}
-            placeholder={intl.formatMessage({
-              defaultMessage: "Start writing here...",
-              description:
-                "Placeholder displayed on the About Me form current city field.",
-            })}
-            rules={{
-              required: intl.formatMessage(errorMessages.required),
-            }}
-          />
-          <Input
-            id="telephone"
-            name="telephone"
-            type="tel"
-            label={intl.formatMessage({
-              defaultMessage: "Telephone",
-              description: "Label for telephone field in About Me form",
-            })}
-            placeholder={intl.formatMessage({
-              defaultMessage: "+123243234",
-              description:
-                "Placeholder displayed on the About Me form telephone field.",
-            })}
-            rules={{
-              required: intl.formatMessage(errorMessages.required),
-              pattern: {
-                value: phoneNumberRegex,
-                message: intl.formatMessage(errorMessages.telephone),
-              },
-            }}
-          />
+      <BasicForm
+        onSubmit={handleSubmit}
+        options={{
+          defaultValues: initialDataToFormValues(initialUser),
+        }}
+      >
+        <h2 data-h2-font-size="b(h3)" data-h2-font-weight="b(700)">
+          {intl.formatMessage({
+            defaultMessage: "Personal Information",
+            description:
+              "Title for Personal Information section of the About Me form",
+          })}
+        </h2>
+        <p>
+          {intl.formatMessage({
+            defaultMessage:
+              "This additional personal information will be used only for communication purposes.",
+            description:
+              "Description for Personal Information section of the About Me form",
+          })}
+        </p>
+        <div data-h2-flex-item="b(1of1)" data-h2-padding="b(top, m)">
+          <div data-h2-padding="b(right, xxl)">
+            <RadioGroup
+              idPrefix="required-lang-preferences"
+              legend={intl.formatMessage({
+                defaultMessage: "Language preference for communication",
+                description:
+                  "Legend text for required language preference in About Me form",
+              })}
+              name="preferredLang"
+              rules={{ required: intl.formatMessage(errorMessages.required) }}
+              items={enumToOptions(Language).map(({ value }) => ({
+                value,
+                label: intl.formatMessage(getLanguage(value)),
+              }))}
+            />
+            <Select
+              id="currentProvince"
+              name="currentProvince"
+              label={intl.formatMessage({
+                defaultMessage: "Current province or territory",
+                description:
+                  "Label for current province or territory field in About Me form",
+              })}
+              nullSelection={intl.formatMessage({
+                defaultMessage: "Select a province or territory...",
+                description:
+                  "Placeholder displayed on the About Me form province or territory field.",
+              })}
+              options={enumToOptions(ProvinceOrTerritory).map(({ value }) => ({
+                value,
+                label: intl.formatMessage(getProvinceOrTerritory(value)),
+              }))}
+              rules={{
+                required: intl.formatMessage(errorMessages.required),
+              }}
+            />
+            <Input
+              id="currentCity"
+              name="currentCity"
+              type="text"
+              label={intl.formatMessage({
+                defaultMessage: "Current city",
+                description: "Label for current city field in About Me form",
+              })}
+              placeholder={intl.formatMessage({
+                defaultMessage: "Start writing here...",
+                description:
+                  "Placeholder displayed on the About Me form current city field.",
+              })}
+              rules={{
+                required: intl.formatMessage(errorMessages.required),
+              }}
+            />
+            <Input
+              id="telephone"
+              name="telephone"
+              type="tel"
+              label={intl.formatMessage({
+                defaultMessage: "Telephone",
+                description: "Label for telephone field in About Me form",
+              })}
+              placeholder={intl.formatMessage({
+                defaultMessage: "+123243234",
+                description:
+                  "Placeholder displayed on the About Me form telephone field.",
+              })}
+              rules={{
+                required: intl.formatMessage(errorMessages.required),
+                pattern: {
+                  value: phoneNumberRegex,
+                  message: intl.formatMessage(errorMessages.telephone),
+                },
+              }}
+            />
+          </div>
         </div>
-      </div>
-      <h2 data-h2-font-size="b(h3)" data-h2-font-weight="b(700)">
-        {intl.formatMessage({
-          defaultMessage: "Account Details",
-          description: "Title for Account Details section of the About Me form",
-        })}
-      </h2>
-      <p>
-        {intl.formatMessage({
-          defaultMessage:
-            "This information is used to manage your account and to communicate.",
-          description:
-            "Description for Account Details section of the About Me form",
-        })}
-      </p>
-      <div data-h2-flex-item="b(1of1)" data-h2-padding="b(top, m)">
-        <div data-h2-padding="b(right, l)">
-          <Input
-            id="firstName"
-            name="firstName"
-            type="text"
-            label={intl.formatMessage({
-              defaultMessage: "First Name and middle Name",
-              description:
-                "Label for first and middle name field in About Me form",
-            })}
-            rules={{
-              required: intl.formatMessage(errorMessages.required),
-            }}
-          />
-          <Input
-            id="lastName"
-            name="lastName"
-            type="text"
-            label={intl.formatMessage({
-              defaultMessage: "Last Name(s)",
-              description: "Label for last name field in About Me form",
-            })}
-            rules={{
-              required: intl.formatMessage(errorMessages.required),
-            }}
-          />
-          <Input
-            id="email"
-            name="email"
-            type="email"
-            label={intl.formatMessage({
-              defaultMessage: "Email",
-              description: "Label for email field in About Me form",
-            })}
-            rules={{
-              required: intl.formatMessage(errorMessages.required),
-            }}
-          />
+        <h2 data-h2-font-size="b(h3)" data-h2-font-weight="b(700)">
+          {intl.formatMessage({
+            defaultMessage: "Account Details",
+            description:
+              "Title for Account Details section of the About Me form",
+          })}
+        </h2>
+        <p>
+          {intl.formatMessage({
+            defaultMessage:
+              "This information is used to manage your account and to communicate.",
+            description:
+              "Description for Account Details section of the About Me form",
+          })}
+        </p>
+        <div data-h2-flex-item="b(1of1)" data-h2-padding="b(top, m)">
+          <div data-h2-padding="b(right, l)">
+            <Input
+              id="firstName"
+              name="firstName"
+              type="text"
+              label={intl.formatMessage({
+                defaultMessage: "First Name and middle Name",
+                description:
+                  "Label for first and middle name field in About Me form",
+              })}
+              rules={{
+                required: intl.formatMessage(errorMessages.required),
+              }}
+            />
+            <Input
+              id="lastName"
+              name="lastName"
+              type="text"
+              label={intl.formatMessage({
+                defaultMessage: "Last Name(s)",
+                description: "Label for last name field in About Me form",
+              })}
+              rules={{
+                required: intl.formatMessage(errorMessages.required),
+              }}
+            />
+            <Input
+              id="email"
+              name="email"
+              type="email"
+              label={intl.formatMessage({
+                defaultMessage: "Email",
+                description: "Label for email field in About Me form",
+              })}
+              rules={{
+                required: intl.formatMessage(errorMessages.required),
+              }}
+            />
+          </div>
         </div>
-      </div>
-      <ProfileFormFooter mode="saveButton" />
-    </BasicForm>
+        <ProfileFormFooter mode="saveButton" />
+      </BasicForm>
+    </ProfileFormWrapper>
   );
 };
 
@@ -313,28 +335,7 @@ const AboutMeFormContainer: React.FunctionComponent = () => {
   }
 
   return (
-    <ProfileFormWrapper
-      description={intl.formatMessage({
-        defaultMessage:
-          "This information is used for account management and communications purposes, it has no impact on the job matching or job recommendations.",
-        description:
-          "Description text for the Profile Form wrapper in the About Me form",
-      })}
-      title={intl.formatMessage({
-        defaultMessage: "About me",
-        description: "Title for Profile Form wrapper in About me form",
-      })}
-      crumbs={[
-        {
-          title: intl.formatMessage({
-            defaultMessage: "About Me",
-            description: "Display text for About Me Form Page Link",
-          }),
-        },
-      ]}
-    >
-      <AboutMeForm initialUser={data.me} onUpdateAboutMe={handleUpdateUser} />
-    </ProfileFormWrapper>
+    <AboutMeForm initialUser={data.me} onUpdateAboutMe={handleUpdateUser} />
   );
 };
 
