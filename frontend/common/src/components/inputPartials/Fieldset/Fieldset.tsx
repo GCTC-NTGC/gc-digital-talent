74404e8a68ebba6b9d6edd6bd26356cb38348b10
Eric Sizer <eric.sizer@gmail.com>
2022-04-08 11:51:52 -0400

    move legend to be direct descendant of fieldset

--- frontend/common/src/components/inputPartials/Fieldset/Fieldset.tsx
+++ frontend/common/src/components/inputPartials/Fieldset/Fieldset.tsx
@@ -44,13 +44,22 @@ export const Fieldset: React.FC<FieldsetProps> = ({
       }}
       data-h2-margin="b(bottom, xxs)"
     >
+      <legend data-h2-visibility="b(invisible)" data-h2-font-size="b(caption)">
+        {legend}
+      </legend>
       <div
         data-h2-display="b(flex)"
         data-h2-flex-wrap="b(wrap)"
         data-h2-margin="b(bottom, xxs)"
       >
         <div style={{ flex: "1" }}>
-          <legend data-h2-font-size="b(caption)">{legend}</legend>
+          <span
+            aria-hidden="true"
+            role="presentation"
+            data-h2-font-size="b(caption)"
+          >
+            {legend}
+          </span>
         </div>
         <div>
           {
