37ba5196934d17bb613f853745b4e4995e951ac1
“RVany” <vanmathyrr@gmail.com>
2022-04-11 16:03:06 -0400

    Unit Test Improved

--- frontend/talentsearch/src/js/components/workLocationPreferenceForm/WorkLocationPreferenceForm.test.tsx
+++ frontend/talentsearch/src/js/components/workLocationPreferenceForm/WorkLocationPreferenceForm.test.tsx
@@ -4,9 +4,13 @@
 import React from "react";
 import "@testing-library/jest-dom";
 import { IntlProvider, MessageFormatElement } from "react-intl";
+import { fakeUsers } from "@common/fakeData";
 import { WorkLocationPreferenceQuery, WorkRegion } from "../../api/generated";
-import { render, screen, fireEvent, act } from "../../tests/testUtils";
-import { WorkLocationPreferenceForm } from "./WorkLocationPreferenceForm";
+import { render, screen, fireEvent, act, waitFor } from "../../tests/testUtils";
+import {
+  WorkLocationPreferenceForm,
+  WorkLocationPreferenceFormProps,
+} from "./WorkLocationPreferenceForm";
 
 const renderWithReactIntl = (
   component: React.ReactNode,
@@ -22,9 +26,9 @@ const renderWithReactIntl = (
   });
 };
 
-const onClick = jest.fn();
+const mockUser = fakeUsers()[0];
 
-const mockUser: WorkLocationPreferenceQuery | undefined = {
+const mockInitialData: WorkLocationPreferenceQuery | undefined = {
   __typename: "Query",
   me: {
     __typename: "User",
@@ -33,148 +37,101 @@ const mockUser: WorkLocationPreferenceQuery | undefined = {
     locationExemptions: "dagu",
   },
 };
-
-const renderWorkLocationPreference = () => {
-  return renderWithReactIntl(
-    <WorkLocationPreferenceForm
-      initialData={mockUser}
-      handleWorkLocationPreference={onClick}
-    />,
-  );
+const mockInitialEmptyData: WorkLocationPreferenceQuery | undefined = {
+  __typename: "Query",
+  me: {
+    __typename: "User",
+    id: "thanka11",
+    locationPreferences: undefined,
+    locationExemptions: "",
+  },
 };
 
+const renderWorkLocationPreferenceForm = ({
+  initialData,
+  handleWorkLocationPreference,
+}: WorkLocationPreferenceFormProps) => (
+  <>
+    {renderWithReactIntl(
+      <WorkLocationPreferenceForm
+        initialData={initialData}
+        handleWorkLocationPreference={handleWorkLocationPreference}
+      />,
+    )}
+  </>
+);
+
 describe("Work Location Preference Form tests", () => {
-  test("Can't submit unless form is valid (at least one location selected)", async () => {
-    renderWorkLocationPreference();
-    const saveAndGoButton = screen.getByText("Save and go back");
-    act(() => {
-      fireEvent.click(saveAndGoButton);
+  test("should render fields", () => {
+    const onClick = jest.fn();
+    renderWorkLocationPreferenceForm({
+      initialData: mockInitialData,
+      handleWorkLocationPreference: onClick,
     });
-    expect(saveAndGoButton).toBeTruthy();
-    expect(onClick).not.toBeCalled();
-    expect(onClick).toHaveBeenCalledTimes(0);
-  });
-
-  test("Can submit the form if at least one location is selected", async () => {
-    renderWorkLocationPreference();
-
-    const checkbox1 = screen.getByRole("checkbox", {
-      name: /Virtual: Work from home/i,
-    });
-    expect(checkbox1).toBeTruthy();
-
-    const saveAndGoButton = screen.getByRole("button", {
-      name: /Save and go back/i,
-    });
-    expect(saveAndGoButton).toBeTruthy();
-
-    const selectInput = screen.findByRole("checkbox", {
-      name: "workLocations",
-    });
-    expect(selectInput).toBeTruthy();
-
-    act(() => {
-      fireEvent.click(checkbox1);
-    });
-    act(() => {
-      fireEvent.click(saveAndGoButton);
-    });
-  });
-
-  test("Form submits data in correct shape to submit handler when Save Changes is clicked.", () => {
-    renderWorkLocationPreference();
-
-    const saveAndGoButton = screen.getByRole("button", {
-      name: /Save and go back/i,
-    });
-    expect(saveAndGoButton).toBeTruthy();
-
-    const checkbox1 = screen.getByRole("checkbox", {
-      name: /Virtual: Work from home/i,
-    });
-    expect(checkbox1).toBeInTheDocument();
-    act(() => {
-      fireEvent.click(checkbox1);
-    });
-    act(() => {
-      fireEvent.click(saveAndGoButton);
-    });
-    const checkbox2 = screen.getByRole("checkbox", {
-      name: /National Capital Region: Ottawa/i,
-    });
-    expect(checkbox2).toBeTruthy();
-    act(() => {
-      fireEvent.click(checkbox2);
-    });
-    act(() => {
-      fireEvent.click(saveAndGoButton);
-    });
-    const checkbox3 = screen.getByRole("checkbox", {
-      name: /Atlantic Region: New Brunswick/i,
-    });
-    expect(checkbox3).toBeTruthy();
-    act(() => {
-      fireEvent.click(checkbox3);
-    });
-    act(() => {
-      fireEvent.click(saveAndGoButton);
-    });
-    const checkbox4 = screen.getByRole("checkbox", {
-      name: /Quebec Region: excluding Gatineau/i,
-    });
-    expect(checkbox4).toBeTruthy();
-
-    act(() => {
-      fireEvent.click(checkbox4);
-    });
-    act(() => {
-      fireEvent.click(saveAndGoButton);
-    });
-    const checkbox5 = screen.getByRole("checkbox", {
-      name: /Ontario Region: excluding Ottawa/i,
-    });
-    expect(checkbox5).toBeTruthy();
-
-    act(() => {
-      fireEvent.click(checkbox5);
-    });
-    act(() => {
-      fireEvent.click(saveAndGoButton);
-    });
-    const checkbox6 = screen.getByRole("checkbox", {
-      name: /Prairie Region: Manitoba, Saskatchewan/i,
-    });
-    expect(checkbox6).toBeTruthy();
-    act(() => {
-      fireEvent.click(checkbox6);
-    });
-    act(() => {
-      fireEvent.click(saveAndGoButton);
-    });
-    const checkbox7 = screen.getByRole("checkbox", {
-      name: /British Columbia Region/i,
-    });
-    expect(checkbox7).toBeTruthy();
-    act(() => {
-      fireEvent.click(checkbox7);
-    });
-    act(() => {
-      fireEvent.click(saveAndGoButton);
-    });
-    const checkbox8 = screen.getByRole("checkbox", {
-      name: /North Region: Yukon/i,
-    });
-    expect(checkbox8).toBeTruthy();
-    act(() => {
-      fireEvent.click(checkbox8);
-    });
-    act(() => {
-      fireEvent.click(saveAndGoButton);
-    });
+    expect(
+      screen.getByRole("button", {
+        name: /Save and go back/i,
+      }),
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole("checkbox", {
+        name: /Virtual: Work from home/i,
+      }),
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole("checkbox", {
+        name: /National Capital Region: Ottawa/i,
+      }),
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole("checkbox", {
+        name: /Atlantic Region: New Brunswick/i,
+      }),
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole("checkbox", {
+        name: /Quebec Region: excluding Gatineau/i,
+      }),
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole("checkbox", {
+        name: /Ontario Region: excluding Ottawa/i,
+      }),
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole("checkbox", {
+        name: /Prairie Region: Manitoba, Saskatchewan/i,
+      }),
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole("checkbox", {
+        name: /British Columbia Region/i,
+      }),
+    ).toBeInTheDocument();
     const textarea1 = screen.getByLabelText(/Location exemptions/i);
     act(() => {
       fireEvent.change(textarea1, { target: { value: "Woodstock" } });
     });
     expect(textarea1).toHaveValue("Woodstock");
+  });
+  test("Can't submit unless form is valid (at least one location selected)", async () => {
+    const onClick = jest.fn();
+    renderWorkLocationPreferenceForm({
+      initialData: { ...mockInitialEmptyData },
+      handleWorkLocationPreference: onClick,
+    });
+    fireEvent.submit(screen.getByRole("button", { name: /save/i }));
+    expect(onClick).not.toHaveBeenCalled();
+  });
+  test("Should submit successfully with required fields", async () => {
+    const onClick = jest.fn(() => Promise.resolve(mockUser));
+    renderWorkLocationPreferenceForm({
+      initialData: { ...mockInitialData },
+      handleWorkLocationPreference: onClick,
+    });
+    fireEvent.submit(screen.getByRole("button", { name: /save/i }));
+    await waitFor(() => {
+      expect(onClick).toHaveBeenCalled();
+    });
   });
 });
