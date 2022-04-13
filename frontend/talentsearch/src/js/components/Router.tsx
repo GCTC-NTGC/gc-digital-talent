e1d17376909467ef73fa91e5c842997bea2b86cb
Eric Sizer <eric.sizer@gmail.com>
2022-04-13 13:55:21 -0400

    remove testing route

--- frontend/talentsearch/src/js/components/Router.tsx
+++ frontend/talentsearch/src/js/components/Router.tsx
@@ -17,7 +17,6 @@ import {
 import RequestPage from "./request/RequestPage";
 import WorkLocationPreferenceApi from "./workLocationPreferenceForm/WorkLocationPreferenceForm";
 import { ProfilePage } from "./profile/ProfilePage/ProfilePage";
-import ExperienceFormContainer from "./experienceForm/ExperienceForm";
 
 const routes = (
   talentPaths: TalentSearchRoutes,
@@ -52,12 +51,6 @@ const routes = (
     path: profilePaths.workLocation(),
     action: () => ({
       component: <WorkLocationPreferenceApi />,
-    }),
-  },
-  {
-    path: profilePaths.skillsAndExperiences(),
-    action: () => ({
-      component: <ExperienceFormContainer />,
     }),
   },
 ];
