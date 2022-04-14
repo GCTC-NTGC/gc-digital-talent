4e61557e22aa1c2ce06ff3fe94f96621f7571ade
tristan-orourke <tristan.m.orourke@gmail.com>
2022-04-14 12:52:51 -0400

    Fix typo and remove unused imports

--- api/database/migrations/2022_04_13_190734_convert_overtime_enum.php
+++ api/database/migrations/2022_04_13_190734_convert_overtime_enum.php
@@ -5,15 +5,13 @@ use App\Models\PoolCandidate;
 use App\Models\PoolCandidateFilter;
 use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
 
 class ConvertOvertimeEnum extends Migration
 {
 
     protected function convertOperationalEnumValue($oldValue, $newValue)
     {
-        $converEnumValue = function($item) use ($oldValue, $newValue) {
+        $convertEnumValue = function($item) use ($oldValue, $newValue) {
             if($item === $oldValue) {
                 return $newValue;
             }
@@ -23,25 +21,25 @@ class ConvertOvertimeEnum extends Migration
         $users = User::whereJsonContains('accepted_operational_requirements', $oldValue)->get();
         foreach($users as $user) {
             $user->accepted_operational_requirements = collect($user->accepted_operational_requirements)
-                ->map($converEnumValue)->toArray();
+                ->map($convertEnumValue)->toArray();
             $user->save();
         }
         $poolCandidates = PoolCandidate::whereJsonContains('accepted_operational_requirements', $oldValue)->get();
         foreach ($poolCandidates as $poolCandidate) {
             $poolCandidate->accepted_operational_requirements = collect($poolCandidate->accepted_operational_requirements)
-                ->map($converEnumValue)->toArray();
+                ->map($convertEnumValue)->toArray();
             $poolCandidate->save();
         }
         $pools = Pool::whereJsonContains('operational_requirements', $oldValue)->get();
         foreach ($pools as $pool) {
             $pool->operational_requirements = collect($pool->operational_requirements)
-                ->map($converEnumValue)->toArray();
+                ->map($convertEnumValue)->toArray();
             $pool->save();
         }
         $poolCandidateFilters = PoolCandidateFilter::whereJsonContains('operational_requirements', $oldValue)->get();
         foreach($poolCandidateFilters as $poolCandidateFilter) {
             $poolCandidateFilter->operational_requirements = collect($poolCandidateFilter->operational_requirements)
-                ->map($converEnumValue)->toArray();
+                ->map($convertEnumValue)->toArray();
             $poolCandidateFilter->save();
         }
     }
