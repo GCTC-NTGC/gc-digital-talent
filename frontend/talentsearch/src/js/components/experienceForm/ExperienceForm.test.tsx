aca84e8992b76732185b04383c3631c47bfe9fd4
Eric Sizer <eric.sizer@gmail.com>
2022-04-19 09:39:14 -0400

    test: experience form submissions

--- frontend/talentsearch/src/js/components/experienceForm/ExperienceForm.test.tsx
+++ frontend/talentsearch/src/js/components/experienceForm/ExperienceForm.test.tsx
@@ -5,10 +5,15 @@ import "@testing-library/jest-dom";
 import { screen, fireEvent, waitFor } from "@testing-library/react";
 import React from "react";
 import { fakeSkills } from "@common/fakeData";
+import { act } from "react-dom/test-utils";
+import fakeExperiences from "@common/fakeData/fakeExperiences";
+import { AwardedScope, AwardedTo } from "@common/api/generated";
 import { render } from "../../tests/testUtils";
 import { ExperienceForm, ExperienceFormProps } from "./ExperienceForm";
+import type { ExperienceQueryData, ExperienceType } from "./types";
 
 const mockSkills = fakeSkills(3);
+const mockExperiences = fakeExperiences(5);
 
 const renderExperienceForm = (props: ExperienceFormProps) =>
   render(<ExperienceForm {...props} />);
@@ -132,5 +137,95 @@ describe("ExperienceForm", () => {
 
     expect(screen.getByLabelText("Start Date")).toBeInTheDocument();
     expect(screen.getByLabelText("End Date")).toBeInTheDocument();
+  });
+
+  it("should render work fields", async () => {
+    const mockSave = jest.fn();
+    renderExperienceForm({
+      experienceType: "work", // Type of form shouldn't matter here
+      onUpdateExperience: mockSave,
+      skills: mockSkills,
+    });
+
+    expect(
+      screen.getByRole("heading", { name: /skills displayed/i }),
+    ).toBeInTheDocument();
+
+    expect(
+      screen.getByRole("heading", { name: /skills in detail/i }),
+    ).toBeInTheDocument();
+
+    const mainstreamSkills = screen.getByRole("tab", {
+      name: /mainstream skills/i,
+    });
+    expect(mainstreamSkills).toBeInTheDocument();
+
+    fireEvent.click(mainstreamSkills);
+    expect(
+      await screen.getByTestId("skillChecklist").parentElement,
+    ).toHaveStyle("display:block;");
+  });
+
+  it("should render additional information", () => {
+    const mockSave = jest.fn();
+    renderExperienceForm({
+      experienceType: "work", // Type of form shouldn't matter here
+      onUpdateExperience: mockSave,
+      skills: mockSkills,
+    });
+
+    expect(screen.getByRole("textbox", { name: /additional information/i }));
+  });
+
+  it("should not submit award with empty fields", async () => {
+    const mockSave = jest.fn();
+    renderExperienceForm({
+      experienceType: "award",
+      onUpdateExperience: mockSave,
+      skills: mockSkills,
+    });
+
+    act(() => {
+      fireEvent.submit(screen.getByRole("button", { name: /save/i }));
+    });
+
+    await waitFor(() => {
+      expect(mockSave).not.toHaveBeenCalled();
+    });
+  });
+
+  it("should submit with required fields", async () => {
+    const mockSave = jest.fn();
+    // const experience = mockExperiences[0];
+    // let experienceType: ExperienceType = "award";
+    // // eslint-disable-next-line no-underscore-dangle
+    // if (experience.__typename) {
+    //   // eslint-disable-next-line no-underscore-dangle
+    //   experienceType = experience.__typename
+    //     ?.replace("Experience", "")
+    //     .toLowerCase() as ExperienceType;
+    // }
+
+    renderExperienceForm({
+      experienceType: "award",
+      onUpdateExperience: mockSave,
+      skills: mockSkills,
+      experience: {
+        title: "Fake Award",
+        details: "fake experience",
+        issuedBy: "John Doe",
+        awardedDate: "2021-05-23",
+        awardedTo: AwardedTo.Me,
+        awardedScope: AwardedScope.Community,
+      } as ExperienceQueryData,
+    });
+
+    act(() => {
+      fireEvent.submit(screen.getByRole("button", { name: /save/i }));
+    });
+
+    await waitFor(() => {
+      expect(mockSave).toHaveBeenCalled();
+    });
   });
 });
