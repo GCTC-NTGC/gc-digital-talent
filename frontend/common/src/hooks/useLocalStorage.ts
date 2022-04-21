f6d71e1348b6db607d7e08a45402dfe1341211bc
tristan-orourke <tristan.m.orourke@gmail.com>
2022-04-21 12:55:45 -0400

    Refactor useLocalStorage hook so retrieval function can be used seperate from hook.

--- frontend/common/src/hooks/useLocalStorage.ts
+++ frontend/common/src/hooks/useLocalStorage.ts
@@ -1,5 +1,28 @@
 import { useState } from "react";
 
+/**
+ * Retrieves data from local storage. Use this function to retrieve the data outside a hook.
+ * Assumes the data was originally set using the useLocalStorage hook (ie was json stringified)
+ * @param key
+ * @param defaultValue
+ * @returns
+ */
+export function getFromLocalStorage<T>(key: string, defaultValue: T): T {
+  if (typeof window === "undefined") {
+    return defaultValue;
+  }
+  try {
+    // Get from local storage by key
+    const item = window.localStorage.getItem(key);
+    // Parse stored json or if none return defaultValue
+    return item ? JSON.parse(item) : defaultValue;
+  } catch (error) {
+    // If error also return defaultValue
+    // console.log(error);
+    return defaultValue;
+  }
+}
+
 //
 /** Sync state to local storage so that it persists through a page refresh. Usage is similar to useState except we pass in a local storage key so that we can default to that value on page load instead of the specified initial value.
  * https://usehooks.com/useLocalStorage/
@@ -10,20 +33,7 @@ export default function useLocalStorage<T>(key: string, initialValue: T) {
   // State to store our value
   // Pass initial state function to useState so logic is only executed once
   const [storedValue, setStoredValue] = useState<T>(() => {
-    if (typeof window === "undefined") {
-      return initialValue;
-    }
-
-    try {
-      // Get from local storage by key
-      const item = window.localStorage.getItem(key);
-      // Parse stored json or if none return initialValue
-      return item ? JSON.parse(item) : initialValue;
-    } catch (error) {
-      // If error also return initialValue
-      // console.log(error);
-      return initialValue;
-    }
+    return getFromLocalStorage(key, initialValue);
   });
 
   // Return a wrapped version of useState's setter function that ...
