30b9d49807c551948950c96d35bbed50664756eb
tristan-orourke <tristan.m.orourke@gmail.com>
2022-04-22 14:51:23 -0400

    Ensure setter function isn't recreated every re-render.

--- frontend/common/src/hooks/useStorage.ts
+++ frontend/common/src/hooks/useStorage.ts
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { getFromStorage, setInStorage } from "../helpers/storageUtils";
 
 //
@@ -20,20 +20,25 @@ export default function useStorage<T>(
 
   // Return a wrapped version of useState's setter function that ...
   // ... persists the new value to localStorage.
-  const setValue = (value: T | ((val: T) => T)) => {
-    try {
-      // Allow value to be a function so we have same API as useState
-      const valueToStore =
-        value instanceof Function ? value(storedValue) : value;
-      // Save state
-      setStoredValue(valueToStore);
-      // Save to local storage
-      setInStorage(store, key, valueToStore);
-    } catch (error) {
-      // A more advanced implementation would handle the error case
-      // console.log(error);
-    }
-  };
+  const setValue = useCallback(
+    (value: T | ((val: T) => T)) => {
+      try {
+        // Allow value to be a function so we have same API as useState
+        const valueToStore =
+          value instanceof Function
+            ? value(getFromStorage(store, key, initialValue))
+            : value;
+        // Save state
+        setStoredValue(valueToStore);
+        // Save to local storage
+        setInStorage(store, key, valueToStore);
+      } catch (error) {
+        // A more advanced implementation would handle the error case
+        // console.log(error);
+      }
+    },
+    [store, key, initialValue],
+  );
 
   return [storedValue, setValue] as const;
 }
