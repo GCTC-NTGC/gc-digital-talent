c9781d4b95d926f776228ca5d0a806b9258ebeec
vd1992 <40485260+vd1992@users.noreply.github.com>
2022-04-27 17:20:01 -0600

    checkbox from id to name

--- frontend/common/src/components/form/Checkbox/Checkbox.tsx
+++ frontend/common/src/components/form/Checkbox/Checkbox.tsx
@@ -79,7 +79,7 @@ export const Checkbox: React.FunctionComponent<CheckboxProps> = ({
             data-h2-align-items="b(center)"
             style={{ width: "100%" }}
           >
-            <label htmlFor={id}>
+            <label htmlFor={name}>
               <input
                 id={id}
                 {...register(name, rules)}
