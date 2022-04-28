9be6559828e46959c267518f968413a0d0f963be
vd1992 <40485260+vd1992@users.noreply.github.com>
2022-04-28 15:27:53 -0600

    a few suggested tweaks

--- frontend/common/src/components/form/Checkbox/Checkbox.tsx
+++ frontend/common/src/components/form/Checkbox/Checkbox.tsx
@@ -79,7 +79,7 @@ export const Checkbox: React.FunctionComponent<CheckboxProps> = ({
             data-h2-align-items="b(center)"
             style={{ width: "100%" }}
           >
-            <label htmlFor={name}>
+            <label htmlFor={id}>
               <input
                 id={id}
                 {...register(name, rules)}
--- frontend/common/src/components/inputPartials/Fieldset/Fieldset.stories.tsx
+++ frontend/common/src/components/inputPartials/Fieldset/Fieldset.stories.tsx
@@ -11,17 +11,17 @@ const TemplateFieldset: Story<FieldsetProps> = (args) => {
   const { name } = args;
   return (
     <Fieldset {...args}>
-      <label data-h2-display="b(block)" htmlFor={name}>
+      <label data-h2-display="b(block)" htmlFor="one">
         One
-        <input type="checkbox" name={name} value="One" id={name} />
+        <input type="checkbox" name={name} value="One" id="one" />
       </label>
-      <label data-h2-display="b(block)" htmlFor={name}>
+      <label data-h2-display="b(block)" htmlFor="two">
         Two
-        <input type="checkbox" name={name} value="Two" id={name} />
+        <input type="checkbox" name={name} value="Two" id="two" />
       </label>
-      <label data-h2-display="b(block)" htmlFor={name}>
+      <label data-h2-display="b(block)" htmlFor="three">
         Three
-        <input type="checkbox" name={name} value="Three" id={name} />
+        <input type="checkbox" name={name} value="Three" id="three" />
       </label>
     </Fieldset>
   );
