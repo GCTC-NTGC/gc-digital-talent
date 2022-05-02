dd0730d111b30b81fcf8cbd29ca23c64d062a469
Vachan <40485260+vd1992@users.noreply.github.com>
2022-05-02 15:24:14 -0600

    bump size limit

--- .bundlewatch.config.js
+++ .bundlewatch.config.js
@@ -11,7 +11,7 @@ module.exports = {
   files: [
     {
       path: "frontend/talentsearch/dist/app.js",
-      maxSize: "210 kB",
+      maxSize: "220 kB",
     },
     {
       path: "frontend/admin/dist/app.js",
