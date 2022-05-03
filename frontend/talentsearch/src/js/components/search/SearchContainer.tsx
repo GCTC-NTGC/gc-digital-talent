edf83ff67b89366f5f9b6a81d4f128217ce52cad
Eric Sizer <eric.sizer@gmail.com>
2022-05-03 12:03:54 -0400

    apply changes to second spinner

--- frontend/talentsearch/src/js/components/search/SearchContainer.tsx
+++ frontend/talentsearch/src/js/components/search/SearchContainer.tsx
@@ -58,11 +58,7 @@ export const SearchContainer: React.FC<SearchContainerProps> = ({
     candidateFilter?.operationalRequirements?.length ?? 0;
 
   function span(msg: string) {
-    return updatePending ? (
-      <div className="lds-dual-ring" />
-    ) : (
-      <span data-h2-font-color="b(lightpurple)">{msg}</span>
-    );
+    return <span data-h2-font-color="b(lightpurple)">{msg}</span>;
   }
 
   function a(msg: string) {
@@ -70,6 +66,52 @@ export const SearchContainer: React.FC<SearchContainerProps> = ({
       <a href={`mailto:${poolOwner?.email}`} data-h2-font-weight="b(700)">
         {msg}
       </a>
+    );
+  }
+
+  function candidateResults() {
+    return candidateCount > 0 ? (
+      <div
+        data-h2-shadow="b(m)"
+        data-h2-border="b(lightnavy, left, solid, l)"
+        data-h2-margin="b(top, s) b(bottom, m)"
+        data-h2-flex-grid="b(middle, contained, flush, xl)"
+      >
+        <SearchPools
+          candidateCount={candidateCount}
+          pool={pool}
+          poolOwner={poolOwner}
+          handleSubmit={handleSubmit}
+        />
+      </div>
+    ) : (
+      <div
+        data-h2-shadow="b(m)"
+        data-h2-margin="b(top, s) b(bottom, m)"
+        data-h2-padding="b(top-bottom, xs) b(left, s)"
+        data-h2-border="b(darkgray, left, solid, l)"
+      >
+        <p data-h2-margin="b(bottom, none)">
+          {intl.formatMessage({
+            defaultMessage: "We can still help!",
+            description:
+              "Heading for helping user if no candidates matched the filters chosen.",
+          })}
+        </p>
+        <p data-h2-margin="b(top, xxs)" data-h2-font-size="b(caption)">
+          {intl.formatMessage(
+            {
+              defaultMessage:
+                "If there are no matching candidates <a>Get in touch!</a>",
+              description:
+                "Message for helping user if no candidates matched the filters chosen.",
+            },
+            {
+              a,
+            },
+          )}
+        </p>
+      </div>
     );
   }
 
@@ -150,50 +192,19 @@ export const SearchContainer: React.FC<SearchContainerProps> = ({
           />
         </div>
         <div data-h2-flex-item="b(1of1)" style={{ paddingTop: "0" }}>
-          {!updatePending &&
-            (candidateCount > 0 ? (
-              <div
-                data-h2-shadow="b(m)"
-                data-h2-border="b(lightnavy, left, solid, l)"
-                data-h2-margin="b(top, s) b(bottom, m)"
-                data-h2-flex-grid="b(middle, contained, flush, xl)"
-              >
-                <SearchPools
-                  candidateCount={candidateCount}
-                  pool={pool}
-                  poolOwner={poolOwner}
-                  handleSubmit={handleSubmit}
-                />
-              </div>
-            ) : (
-              <div
-                data-h2-shadow="b(m)"
-                data-h2-margin="b(top, s) b(bottom, m)"
-                data-h2-padding="b(top-bottom, xs) b(left, s)"
-                data-h2-border="b(darkgray, left, solid, l)"
-              >
-                <p data-h2-margin="b(bottom, none)">
-                  {intl.formatMessage({
-                    defaultMessage: "We can still help!",
-                    description:
-                      "Heading for helping user if no candidates matched the filters chosen.",
-                  })}
-                </p>
-                <p data-h2-margin="b(top, xxs)" data-h2-font-size="b(caption)">
-                  {intl.formatMessage(
-                    {
-                      defaultMessage:
-                        "If there are no matching candidates <a>Get in touch!</a>",
-                      description:
-                        "Message for helping user if no candidates matched the filters chosen.",
-                    },
-                    {
-                      a,
-                    },
-                  )}
-                </p>
-              </div>
-            ))}
+          {!updatePending ? (
+            candidateResults
+          ) : (
+            <span className="lds-dual-ring">
+              <span data-h2-visibility="b(invisible)">
+                {intl.formatMessage({
+                  defaultMessage: "Searching...",
+                  description:
+                    "Message to display when a search is in progress.",
+                })}
+              </span>
+            </span>
+          )}
         </div>
       </div>
     </div>
@@ -240,9 +251,8 @@ export const SearchContainerApi: React.FC = () => {
   });
   const pool = data?.poolByKey;
 
-  const [candidateFilter, setCandidateFilter] = useState<
-    PoolCandidateFilterInput | undefined
-  >(undefined);
+  const [candidateFilter, setCandidateFilter] =
+    useState<PoolCandidateFilterInput | undefined>(undefined);
 
   const [{ data: countData, fetching: countFetching }] =
     useCountPoolCandidatesQuery({
