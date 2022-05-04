98296f18996e36c9a626b0bcb906dd829eb65cd6
Eric Sizer <eric.sizer@gmail.com>
2022-05-04 18:05:39 -0400

    missed link story import

--- frontend/common/src/components/Link/Link.stories.tsx
+++ frontend/common/src/components/Link/Link.stories.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Story, Meta } from "@storybook/react";
-import { Link } from "./Link";
+import Link from "./Link";
 import type { LinkProps } from "./Link";
 
 export default {
