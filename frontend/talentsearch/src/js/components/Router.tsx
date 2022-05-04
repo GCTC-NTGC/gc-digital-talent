7fb671bbacd0bbfd0362ea2128a209ff62aa5dcf
JamesHuf <jamiehufnagel@hotmail.com>
2022-05-04 17:08:43 -0600

    Added new edit parameter and display different toasts for updating

--- frontend/talentsearch/src/js/components/Router.tsx
+++ frontend/talentsearch/src/js/components/Router.tsx
@@ -92,6 +92,7 @@ const routes = (
           <ExperienceFormContainer
             experienceType={experienceType}
             experienceId={experienceId}
+            edit
           />
         ),
       };
