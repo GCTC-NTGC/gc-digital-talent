6d8179ab532450f8c9e81a96deb3687a0163a971
Eric Sizer <eric.sizer@gmail.com>
2022-05-05 16:25:13 -0400

    add common page header component

---
+++ frontend/common/src/components/PageHeader/PageHeader.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+import "./pageHeader.css";
+
+export interface PageHeaderProps {
+  icon?: React.FC<{ className: string }>;
+}
+
+const PageHeader: React.FC<PageHeaderProps> = ({ icon, children, ...rest }) => {
+  const Icon = icon || null;
+
+  return (
+    <h1
+      data-h2-display="b(flex)"
+      data-h2-font-weight="b(800)"
+      data-h2-align-items="b(center)"
+      data-h2-justify-content="b(start)"
+    >
+      {Icon && <Icon className="page-header__icon" />}
+      <span>{children}</span>
+    </h1>
+  );
+};
+
+export default PageHeader;
