e276cbf89f7ef86d726a2e47d548b012de4086b9
Patrick Connolly <patrick.c.connolly@gmail.com>
2022-05-11 12:18:44 -0400

    Added logout command for Cypress tests (non-UI).

--- frontend/cypress/support/commands.js
+++ frontend/cypress/support/commands.js
@@ -23,3 +23,26 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('expireAppSession', () => {
+  window.localStorage.removeItem('refresh_token')
+  window.localStorage.removeItem('id_token')
+  window.localStorage.removeItem('access_token')
+})
+
+Cypress.Commands.add('discardLaravelCookie', () => {
+  cy.clearCookie('laravel_session')
+}
+
+Cypress.Commands.add('discardIdentityProviderCookie', () => {
+  cy.clearCookie('api_session')
+})
+
+Cypress.Commands.add('logout', () => {
+  cy.expireAppSession()
+  // These are not currently done as part of logout in UI.
+  // This purging perhaps could be added later, in order for user to be able to
+  // get to login screen again, but for now, tests should keep parity with UI.
+  //cy.discardLaravelCookie()
+  //cy.discardIdentityProviderCookie()
+})
