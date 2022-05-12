7a24d29908859f1debe7c3991ed476937e1830c7
Tristan O'Rourke <tristan.orourke@gmail.com>
2022-05-12 16:52:21 -0400

    Ran lint:fix in all frontend projects

--- frontend/admin/src/js/components/cmoAsset/CreateCmoAsset.tsx
+++ frontend/admin/src/js/components/cmoAsset/CreateCmoAsset.tsx
@@ -18,129 +18,130 @@ interface CreateCmoAssetFormProps {
   handleCreateCmoAsset: (data: FormValues) => Promise<FormValues>;
 }
 
-export const CreateCmoAssetForm: React.FunctionComponent<CreateCmoAssetFormProps> =
-  ({ handleCreateCmoAsset }) => {
-    const intl = useIntl();
-    const paths = useAdminRoutes();
-    const methods = useForm<FormValues>();
-    const { handleSubmit } = methods;
+export const CreateCmoAssetForm: React.FunctionComponent<
+  CreateCmoAssetFormProps
+> = ({ handleCreateCmoAsset }) => {
+  const intl = useIntl();
+  const paths = useAdminRoutes();
+  const methods = useForm<FormValues>();
+  const { handleSubmit } = methods;
 
-    const onSubmit: SubmitHandler<FormValues> = async (data: FormValues) => {
-      return handleCreateCmoAsset(data)
-        .then(() => {
-          navigate(paths.cmoAssetTable());
-          toast.success(
-            intl.formatMessage({
-              defaultMessage: "CMO Asset created successfully!",
-              description:
-                "Message displayed to user after cmo asset is created successfully.",
-            }),
-          );
-        })
-        .catch(() => {
-          toast.error(
-            intl.formatMessage({
-              defaultMessage: "Error: creating cmo asset failed",
-              description:
-                "Message displayed to user after cmo asset fails to get created.",
-            }),
-          );
-        });
-    };
-    return (
-      <section>
-        <h2 data-h2-text-align="b(center)" data-h2-margin="b(top, none)">
-          {intl.formatMessage({
-            defaultMessage: "Create CMO Asset",
-            description: "Title displayed on the create a cmo asset form.",
-          })}
-        </h2>
-        <div data-h2-container="b(center, s)">
-          <FormProvider {...methods}>
-            <form onSubmit={handleSubmit(onSubmit)}>
-              <Input
-                id="key"
-                name="key"
-                label={intl.formatMessage({
-                  defaultMessage: "Key",
-                  description:
-                    "Label displayed on the create a cmo asset form key field.",
-                })}
-                context={intl.formatMessage({
-                  defaultMessage:
-                    "The 'key' is a string that uniquely identifies a CMO Asset. It should be based on the CMO Asset's English name, and it should be concise. A good example would be \"information_management\". It may be used in the code to refer to this particular CMO Asset, so it cannot be changed later.",
-                  description:
-                    "Additional context describing the purpose of the CMO Asset's 'key' field.",
-                })}
-                type="text"
-                rules={{
-                  required: intl.formatMessage(errorMessages.required),
-                  pattern: {
-                    value: keyStringRegex,
-                    message: intl.formatMessage({
-                      defaultMessage:
-                        "Please use only lowercase letters and underscores.",
-                    }),
-                  },
-                }}
-              />
-              <Input
-                id="name_en"
-                name="name.en"
-                label={intl.formatMessage({
-                  defaultMessage: "Name (English)",
-                  description:
-                    "Label displayed on the create a cmo asset form name (English) field.",
-                })}
-                type="text"
-                rules={{
-                  required: intl.formatMessage(errorMessages.required),
-                }}
-              />
-              <Input
-                id="name_fr"
-                name="name.fr"
-                label={intl.formatMessage({
-                  defaultMessage: "Name (French)",
-                  description:
-                    "Label displayed on the create a cmo asset form name (French) field.",
-                })}
-                type="text"
-                rules={{
-                  required: intl.formatMessage(errorMessages.required),
-                }}
-              />
-              <TextArea
-                id="description_en"
-                name="description.en"
-                label={intl.formatMessage({
-                  defaultMessage: "Description (English)",
-                  description:
-                    "Label displayed on the create a cmo asset form description (English) field.",
-                })}
-                rules={{
-                  required: intl.formatMessage(errorMessages.required),
-                }}
-              />
-              <TextArea
-                id="description_fr"
-                name="description.fr"
-                label={intl.formatMessage({
-                  defaultMessage: "Description (French)",
-                  description:
-                    "Label displayed on the create a cmo asset form description (French) field.",
-                })}
-                rules={{
-                  required: intl.formatMessage(errorMessages.required),
-                }}
-              />
-              <Submit />
-            </form>
-          </FormProvider>
-        </div>
-      </section>
-    );
+  const onSubmit: SubmitHandler<FormValues> = async (data: FormValues) => {
+    return handleCreateCmoAsset(data)
+      .then(() => {
+        navigate(paths.cmoAssetTable());
+        toast.success(
+          intl.formatMessage({
+            defaultMessage: "CMO Asset created successfully!",
+            description:
+              "Message displayed to user after cmo asset is created successfully.",
+          }),
+        );
+      })
+      .catch(() => {
+        toast.error(
+          intl.formatMessage({
+            defaultMessage: "Error: creating cmo asset failed",
+            description:
+              "Message displayed to user after cmo asset fails to get created.",
+          }),
+        );
+      });
   };
+  return (
+    <section>
+      <h2 data-h2-text-align="b(center)" data-h2-margin="b(top, none)">
+        {intl.formatMessage({
+          defaultMessage: "Create CMO Asset",
+          description: "Title displayed on the create a cmo asset form.",
+        })}
+      </h2>
+      <div data-h2-container="b(center, s)">
+        <FormProvider {...methods}>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Input
+              id="key"
+              name="key"
+              label={intl.formatMessage({
+                defaultMessage: "Key",
+                description:
+                  "Label displayed on the create a cmo asset form key field.",
+              })}
+              context={intl.formatMessage({
+                defaultMessage:
+                  "The 'key' is a string that uniquely identifies a CMO Asset. It should be based on the CMO Asset's English name, and it should be concise. A good example would be \"information_management\". It may be used in the code to refer to this particular CMO Asset, so it cannot be changed later.",
+                description:
+                  "Additional context describing the purpose of the CMO Asset's 'key' field.",
+              })}
+              type="text"
+              rules={{
+                required: intl.formatMessage(errorMessages.required),
+                pattern: {
+                  value: keyStringRegex,
+                  message: intl.formatMessage({
+                    defaultMessage:
+                      "Please use only lowercase letters and underscores.",
+                  }),
+                },
+              }}
+            />
+            <Input
+              id="name_en"
+              name="name.en"
+              label={intl.formatMessage({
+                defaultMessage: "Name (English)",
+                description:
+                  "Label displayed on the create a cmo asset form name (English) field.",
+              })}
+              type="text"
+              rules={{
+                required: intl.formatMessage(errorMessages.required),
+              }}
+            />
+            <Input
+              id="name_fr"
+              name="name.fr"
+              label={intl.formatMessage({
+                defaultMessage: "Name (French)",
+                description:
+                  "Label displayed on the create a cmo asset form name (French) field.",
+              })}
+              type="text"
+              rules={{
+                required: intl.formatMessage(errorMessages.required),
+              }}
+            />
+            <TextArea
+              id="description_en"
+              name="description.en"
+              label={intl.formatMessage({
+                defaultMessage: "Description (English)",
+                description:
+                  "Label displayed on the create a cmo asset form description (English) field.",
+              })}
+              rules={{
+                required: intl.formatMessage(errorMessages.required),
+              }}
+            />
+            <TextArea
+              id="description_fr"
+              name="description.fr"
+              label={intl.formatMessage({
+                defaultMessage: "Description (French)",
+                description:
+                  "Label displayed on the create a cmo asset form description (French) field.",
+              })}
+              rules={{
+                required: intl.formatMessage(errorMessages.required),
+              }}
+            />
+            <Submit />
+          </form>
+        </FormProvider>
+      </div>
+    </section>
+  );
+};
 
 export const CreateCmoAsset: React.FunctionComponent = () => {
   const [, executeMutation] = useCreateCmoAssetMutation();
--- frontend/talentsearch/src/js/components/workLocationPreferenceForm/WorkLocationPreferenceForm.tsx
+++ frontend/talentsearch/src/js/components/workLocationPreferenceForm/WorkLocationPreferenceForm.tsx
@@ -32,126 +32,127 @@ export interface WorkLocationPreferenceFormProps {
   ) => Promise<CreateWorkLocationPreferenceMutation["updateUserAsUser"]>;
 }
 
-export const WorkLocationPreferenceForm: React.FC<WorkLocationPreferenceFormProps> =
-  ({ initialData, handleWorkLocationPreference }) => {
-    const intl = useIntl();
+export const WorkLocationPreferenceForm: React.FC<
+  WorkLocationPreferenceFormProps
+> = ({ initialData, handleWorkLocationPreference }) => {
+  const intl = useIntl();
 
-    function bold(msg: string) {
-      return <span data-h2-font-weight="b(700)">{msg}</span>;
+  function bold(msg: string) {
+    return <span data-h2-font-weight="b(700)">{msg}</span>;
+  }
+
+  const dataToFormValues = (
+    data: WorkLocationPreferenceQuery | undefined,
+  ): FormValues => ({
+    ...data,
+    locationPreferences: data?.me?.locationPreferences,
+    locationExemptions: data?.me?.locationExemptions,
+  });
+  const formValuesToSubmitData = (
+    values: FormValues,
+  ): UpdateUserAsUserInput => ({
+    locationPreferences: values.locationPreferences,
+    locationExemptions: values.locationExemptions,
+  });
+  const methods = useForm<FormValues>({
+    defaultValues: dataToFormValues(initialData),
+  });
+  const { handleSubmit } = methods;
+
+  const onSubmit: SubmitHandler<FormValues> = async (data: FormValues) => {
+    if (initialData?.me) {
+      await handleWorkLocationPreference(
+        initialData.me?.id,
+        formValuesToSubmitData(data),
+      );
     }
+  };
 
-    const dataToFormValues = (
-      data: WorkLocationPreferenceQuery | undefined,
-    ): FormValues => ({
-      ...data,
-      locationPreferences: data?.me?.locationPreferences,
-      locationExemptions: data?.me?.locationExemptions,
-    });
-    const formValuesToSubmitData = (
-      values: FormValues,
-    ): UpdateUserAsUserInput => ({
-      locationPreferences: values.locationPreferences,
-      locationExemptions: values.locationExemptions,
-    });
-    const methods = useForm<FormValues>({
-      defaultValues: dataToFormValues(initialData),
-    });
-    const { handleSubmit } = methods;
-
-    const onSubmit: SubmitHandler<FormValues> = async (data: FormValues) => {
-      if (initialData?.me) {
-        await handleWorkLocationPreference(
-          initialData.me?.id,
-          formValuesToSubmitData(data),
-        );
-      }
-    };
-
-    return (
-      <ProfileFormWrapper
-        description={intl.formatMessage({
-          defaultMessage:
-            "Indicate all locations where you are willing to work, including your current location (if you are interested in working there).",
-          description:
-            "Description text for Profile Form wrapper in Work Location Preferences Form",
-        })}
-        title={intl.formatMessage({
-          defaultMessage: "Work location",
-          description:
-            "Title for Profile Form wrapper  in Work Location Preferences Form",
-        })}
-        crumbs={[
-          {
-            title: intl.formatMessage({
-              defaultMessage: "Work Location Preference",
-              description:
-                "Display Text for the current page in Work Location Preference Form Page",
-            }),
-          },
-        ]}
-      >
-        <FormProvider {...methods}>
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <div>
-              <div data-h2-flex-item="b(1of1)" data-h2-padding="b(top, m)">
-                <div data-h2-padding="b(right, l)" data-testid="workLocation">
-                  <Checklist
-                    idPrefix="work-location"
-                    legend={intl.formatMessage({
-                      defaultMessage: "Work location",
-                      description:
-                        "Legend for optional work preferences check list in work preferences form",
-                    })}
-                    name="locationPreferences"
-                    items={enumToOptions(WorkRegion).map(({ value }) => ({
-                      value,
-                      label: intl.formatMessage(getWorkRegionsDetailed(value), {
-                        bold,
-                      }),
-                    }))}
-                    rules={{
-                      required: intl.formatMessage(errorMessages.required),
-                    }}
-                  />
-                </div>
-              </div>
-              <div data-h2-flex-item="b(1of1)" data-h2-padding="b(top, m)">
-                <div data-h2-padding="b(right, l)">
-                  <p>
-                    {intl.formatMessage({
-                      defaultMessage:
-                        "Indicate if there is a city that you would like to exclude from a region.",
-                      description:
-                        "Explanation text for Location exemptions field in work location preference form",
-                    })}
-                  </p>
-                  <p data-h2-font-color="b([dark]gray)">
-                    {intl.formatMessage({
-                      defaultMessage:
-                        "E.g.: You want to be considered for the Quebec region, but not for Montréal.",
-                      description:
-                        "Example for Location exemptions field in work location preference form",
-                    })}
-                  </p>
-                </div>
-              </div>
-              <div data-h2-flex-item="b(1of2)" data-h2-padding="b(top, m)">
-                <div data-h2-padding="b(right, l)">
-                  <TextArea
-                    id="location-exemptions"
-                    label="Location exemptions"
-                    name="locationExemptions"
-                    placeholder="Optionally, add a city or village here..."
-                  />
-                </div>
+  return (
+    <ProfileFormWrapper
+      description={intl.formatMessage({
+        defaultMessage:
+          "Indicate all locations where you are willing to work, including your current location (if you are interested in working there).",
+        description:
+          "Description text for Profile Form wrapper in Work Location Preferences Form",
+      })}
+      title={intl.formatMessage({
+        defaultMessage: "Work location",
+        description:
+          "Title for Profile Form wrapper  in Work Location Preferences Form",
+      })}
+      crumbs={[
+        {
+          title: intl.formatMessage({
+            defaultMessage: "Work Location Preference",
+            description:
+              "Display Text for the current page in Work Location Preference Form Page",
+          }),
+        },
+      ]}
+    >
+      <FormProvider {...methods}>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div>
+            <div data-h2-flex-item="b(1of1)" data-h2-padding="b(top, m)">
+              <div data-h2-padding="b(right, l)" data-testid="workLocation">
+                <Checklist
+                  idPrefix="work-location"
+                  legend={intl.formatMessage({
+                    defaultMessage: "Work location",
+                    description:
+                      "Legend for optional work preferences check list in work preferences form",
+                  })}
+                  name="locationPreferences"
+                  items={enumToOptions(WorkRegion).map(({ value }) => ({
+                    value,
+                    label: intl.formatMessage(getWorkRegionsDetailed(value), {
+                      bold,
+                    }),
+                  }))}
+                  rules={{
+                    required: intl.formatMessage(errorMessages.required),
+                  }}
+                />
               </div>
             </div>
-            <ProfileFormFooter mode="saveButton" />
-          </form>
-        </FormProvider>
-      </ProfileFormWrapper>
-    );
-  };
+            <div data-h2-flex-item="b(1of1)" data-h2-padding="b(top, m)">
+              <div data-h2-padding="b(right, l)">
+                <p>
+                  {intl.formatMessage({
+                    defaultMessage:
+                      "Indicate if there is a city that you would like to exclude from a region.",
+                    description:
+                      "Explanation text for Location exemptions field in work location preference form",
+                  })}
+                </p>
+                <p data-h2-font-color="b([dark]gray)">
+                  {intl.formatMessage({
+                    defaultMessage:
+                      "E.g.: You want to be considered for the Quebec region, but not for Montréal.",
+                    description:
+                      "Example for Location exemptions field in work location preference form",
+                  })}
+                </p>
+              </div>
+            </div>
+            <div data-h2-flex-item="b(1of2)" data-h2-padding="b(top, m)">
+              <div data-h2-padding="b(right, l)">
+                <TextArea
+                  id="location-exemptions"
+                  label="Location exemptions"
+                  name="locationExemptions"
+                  placeholder="Optionally, add a city or village here..."
+                />
+              </div>
+            </div>
+          </div>
+          <ProfileFormFooter mode="saveButton" />
+        </form>
+      </FormProvider>
+    </ProfileFormWrapper>
+  );
+};
 
 export const WorkLocationPreferenceApi: React.FunctionComponent = () => {
   const intl = useIntl();
