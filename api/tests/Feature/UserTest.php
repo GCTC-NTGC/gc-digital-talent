d690c3958dbba1fceb977ee8d2a6d5ea2f896e41
JamesHuf <jamiehufnagel@hotmail.com>
2022-05-16 17:28:50 -0600

    Added first tests for filters

--- api/tests/Feature/UserTest.php
+++ api/tests/Feature/UserTest.php
@@ -2,6 +2,7 @@
 
 use App\Models\User;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Nuwave\Lighthouse\Testing\ClearsSchemaCache;
 use Nuwave\Lighthouse\Testing\MakesGraphQLRequests;
 use Tests\TestCase;
 
@@ -9,6 +10,21 @@ class UserTest extends TestCase
 {
     use DatabaseMigrations;
     use MakesGraphQLRequests;
+    use ClearsSchemaCache;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->bootClearsSchemaCache();
+
+        // Create admin user we run tests as
+        // Note: this extra user does change the results of a couple queries
+        $newUser = new User;
+        $newUser->email = 'admin@test.com';
+        $newUser->sub = 'admin@test.com';
+        $newUser->roles = ['ADMIN'];
+        $newUser->save();
+    }
 
     public function testCreateUserDefaultRoles()
     {
@@ -110,5 +126,113 @@ class UserTest extends TestCase
         ]);
         // Ensure change was saved
         $this->assertContains('ADMIN', $user->fresh()->roles);
+    }
+
+    public function testFilterByLanguageAbility(): void
+    {
+        User::factory()->count(5)->create([
+            'language_ability' => 'TEST'
+        ]);
+
+        // Create new LanguageAbility and attach to 3 new pool candidates.
+        User::factory()->create([
+            'language_ability' => 'FRENCH'
+        ]);
+        User::factory()->create([
+            'language_ability' => 'ENGLISH'
+        ]);
+        User::factory()->create([
+            'language_ability' => 'BILINGUAL'
+        ]);
+
+        // Assert query with no LanguageAbility filter will return all candidates
+        $this->graphQL(/** @lang Graphql */ '
+            query getUsersPaginated($where: UserFilterAndOrderInput) {
+                usersPaginated(where: $where) {
+                    paginatorInfo {
+                        total
+                    }
+                }
+            }
+        ', [
+            'where' => []
+        ])->assertJson([
+            'data' => [
+                'usersPaginated' => [
+                    'paginatorInfo' => [
+                        'total' => 9
+                    ]
+                ]
+            ]
+        ]);
+
+        // Assert query with ENGLISH filter will return correct candidate count
+        $this->graphQL(/** @lang Graphql */ '
+            query getUsersPaginated($where: UserFilterAndOrderInput) {
+                usersPaginated(where: $where) {
+                    paginatorInfo {
+                        total
+                    }
+                }
+            }
+        ', [
+            'where' => [
+                'languageAbility' => "ENGLISH"
+            ]
+        ])->assertJson([
+            'data' => [
+                'usersPaginated' => [
+                    'paginatorInfo' => [
+                        'total' => 2
+                    ]
+                ]
+            ]
+        ]);
+
+        // Assert query with FRENCH filter will return correct candidate count
+        $this->graphQL(/** @lang Graphql */ '
+            query getUsersPaginated($where: UserFilterAndOrderInput) {
+                usersPaginated(where: $where) {
+                    paginatorInfo {
+                        total
+                    }
+                }
+            }
+        ', [
+            'where' => [
+                'languageAbility' => "FRENCH"
+            ]
+        ])->assertJson([
+            'data' => [
+                'usersPaginated' => [
+                    'paginatorInfo' => [
+                        'total' => 2
+                    ]
+                ]
+            ]
+        ]);
+
+        // Assert query with BILINGUAL filter will return correct candidate count
+        $this->graphQL(/** @lang Graphql */ '
+            query getUsersPaginated($where: UserFilterAndOrderInput) {
+                usersPaginated(where: $where) {
+                    paginatorInfo {
+                        total
+                    }
+                }
+            }
+        ', [
+            'where' => [
+                'languageAbility' => "BILINGUAL"
+            ]
+        ])->assertJson([
+            'data' => [
+                'usersPaginated' => [
+                    'paginatorInfo' => [
+                        'total' => 1
+                    ]
+                ]
+            ]
+        ]);
     }
 }
