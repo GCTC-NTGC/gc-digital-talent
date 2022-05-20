a099c925f014693d49e0720ead43fce41d7c7a21
Patrick Connolly <patrick.c.connolly@gmail.com>
2022-05-20 02:53:42 -0400

    Refactored cypress login commands.

--- frontend/cypress/support/commands.js
+++ frontend/cypress/support/commands.js
@@ -50,22 +50,7 @@ Cypress.Commands.add('login', (...args) => {
 
 // Logs in with no user specified, assuming no login page.
 // Works only when mock-oauth2-server.json has set interactiveLogin:FALSE
-// TODO: Get this working without browser using cy.request().
-const loginNonInteractive = () => {
-  cy.request({ url: '/login', followRedirect: false }).then((resp) => {
-    cy.wrap(resp.redirectedToUrl).as('oauth2AuthorizeUrl')
-  })
-  cy.get('@oauth2AuthorizeUrl').then((url) => {
-    cy.request({ url: url, followRedirect: false }).then((resp) => {
-      cy.wrap(resp.redirectedToUrl).as('appCallbackUrl')
-    })
-  })
-  cy.get('@appCallbackUrl').then((url) => {
-    cy.request({ url: url, followRedirect: false })
-      .then(responseToQueryParams)
-      .then(queryParamsToLocalStorageTokens)
-  })
-}
+const loginNonInteractive = () => _login()
 
 // Logs in by role, using email from `fixtures/users.json`.
 // Works only when mock-oauth2-server.json has set interactiveLogin:TRUE
@@ -74,36 +59,30 @@ const loginByRole = (userRole = 'admin') => {
   cy.fixture('users.json').then(users => {
     const user = users[userRole]
 
-    cy.request('/login', {followRedirect: false}).then((data) => {
-      const resp = data.allRequestResponses[0]
-      const redirect = resp['Response Headers']['location']
-      const cookieHeaders = resp['Response Headers']['set-cookie']
-      cookieHeaders.forEach(cookieHeader => {
-        let [cookieName, cookieValue] = cookieHeader.split(';')[0].split('=')
-        cy.setCookie(cookieName, cookieValue)
-      })
-      cy.request({
-        method: 'POST',
-        followRedirect: false,
-        url: redirect,
-        form: true,
-        body: {
-          username: user.email,
-        },
-      }).then((data) => {
-        const resp = data.allRequestResponses[0]
-        const redirect = resp['Response Headers']['location']
-        cy.request(redirect, {followRedirect: false}).then((data) => {
-          const resp = data.allRequestResponses[0]
-          const redirect = resp['Response Headers']['location']
-          const urlObj = new URL(redirect)
-          const urlParams = new URLSearchParams(urlObj.search)
-          window.localStorage.setItem('id_token', urlParams.get('id_token'))
-          window.localStorage.setItem('access_token', urlParams.get('access_token'))
-          window.localStorage.setItem('refresh_token', urlParams.get('refresh_token'))
-        })
-      })
+    const authorizeReqOptions = {
+      method: 'POST',
+      form: true,
+      body: {
+        username: user.email
+      },
+    }
+    _login(authorizeReqOptions)
+  })
+}
+
+const _login = (authorizeReqOptions = {}) => {
+  cy.request({ url: '/login', followRedirect: false }).then((resp) => {
+    cy.wrap(resp.redirectedToUrl).as('oauth2AuthorizeUrl')
+  })
+  cy.get('@oauth2AuthorizeUrl').then((url) => {
+    cy.request({ url: url, followRedirect: false, ...authorizeReqOptions }).then((resp) => {
+      cy.wrap(resp.redirectedToUrl).as('appCallbackUrl')
     })
+  })
+  cy.get('@appCallbackUrl').then((url) => {
+    cy.request({ url: url, followRedirect: false })
+      .then(responseToQueryParams)
+      .then(queryParamsToLocalStorageTokens)
   })
 }
 
