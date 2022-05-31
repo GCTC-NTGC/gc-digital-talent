177c3bbc5ad6d4395e10061ea971fb0662ad12dd
Yonathan K <yoni.kid15@gmail.com>
2022-05-31 11:04:34 -0400

    Add database migration to add to Pools table

---
+++ api/database/migrations/2022_05_30_182823_update_pools_table_with_key_tasks_and_status.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdatePoolsTableWithKeyTasksAndStatus extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('pools', function (Blueprint $table) {
+            $table->jsonb('key_tasks')->nullable(false)->default(json_encode(['en' => '', 'fr' => '']));
+            $table->string('pool_status')->nullable(true);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('pools', function (Blueprint $table) {
+            $table->dropColumn(['key_tasks', 'pool_status']);
+        });
+    }
+}
