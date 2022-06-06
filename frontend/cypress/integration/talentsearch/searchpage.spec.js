2f464637c9505a0b58449ae4111e7773f4dde51e
vd1992 <40485260+vd1992@users.noreply.github.com>
2022-06-06 09:27:25 -0600

    testfile changes

--- frontend/cypress/integration/talentsearch/searchpage.spec.js
+++ frontend/cypress/integration/talentsearch/searchpage.spec.js
@@ -2,20 +2,16 @@ describe("Talentsearch Search Page", () => {
   beforeEach(() => cy.visit("/en/talent/search"));
 
   it("loads page successfully", () => {
-    cy.wait(5000);
     cy.findByRole("button", { name: /Request Candidates/i }).should("exist");
   });
 
   it("checks, submits, and navigates to /request", () => {
-    cy.wait(5000);
     cy.get("form")
       .findByRole("radio", {
         name: /Required diploma from post-secondary institution/i,
       })
       .check();
-    cy.wait(2000);
     cy.findByRole("button", { name: /Request Candidates/i }).click();
-    cy.wait(5000);
-    cy.url().should("include", "/talent/request");
+    cy.findByRole("button", { name: /Submit Request/i }).should("exist");
   });
 });
