6cd471150cd3302d50569581a422b968468114c3
Peter Giles <8978655+petertgiles@users.noreply.github.com>
2022-06-08 10:03:46 -0400

    Set up talentsearch with contexts

--- frontend/talentsearch/src/js/components/Router.tsx
+++ frontend/talentsearch/src/js/components/Router.tsx
@@ -4,7 +4,6 @@ import { Routes } from "universal-router";
 import { RouterResult } from "@common/helpers/router";
 import Toast from "@common/components/Toast";
 import { checkFeatureFlag } from "@common/helpers/runtimeVariable";
-import ClientProvider from "./ClientProvider";
 import PageContainer, { MenuLink } from "./PageContainer";
 import SearchPage from "./search/SearchPage";
 import {
@@ -38,6 +37,7 @@ import PoolApplyPage from "./pool/PoolApplyPage";
 import PoolApplicationThanksPage from "./pool/PoolApplicationThanksPage";
 import RegisterPage from "./register/RegisterPage";
 import LoginPage from "./login/LoginPage";
+import { Role } from "../api/generated";
 
 const talentRoutes = (
   talentPaths: TalentSearchRoutes,
@@ -85,30 +85,35 @@ const profileRoutes = (
     path: profilePaths.home(),
     action: () => ({
       component: <ProfilePage />,
+      authorizedRoles: [Role.Applicant],
     }),
   },
   {
     path: profilePaths.governmentInformation(),
     action: () => ({
       component: <GovInfoFormContainer />,
+      authorizedRoles: [Role.Applicant],
     }),
   },
   {
     path: profilePaths.languageInformation(),
     action: () => ({
       component: <LanguageInformationFormContainer />,
+      authorizedRoles: [Role.Applicant],
     }),
   },
   {
     path: profilePaths.workLocation(),
     action: () => ({
       component: <WorkLocationPreferenceApi />,
+      authorizedRoles: [Role.Applicant],
     }),
   },
   {
     path: profilePaths.roleSalary(),
     action: () => ({
       component: <RoleSalaryFormContainer />,
+      authorizedRoles: [Role.Applicant],
     }),
   },
   {
@@ -117,6 +122,7 @@ const profileRoutes = (
       const experienceType = context.params.type as ExperienceType;
       return {
         component: <ExperienceFormContainer experienceType={experienceType} />,
+        authorizedRoles: [Role.Applicant],
       };
     },
   },
@@ -133,6 +139,7 @@ const profileRoutes = (
             edit
           />
         ),
+        authorizedRoles: [Role.Applicant],
       };
     },
   },
@@ -140,30 +147,35 @@ const profileRoutes = (
     path: profilePaths.workPreferences(),
     action: () => ({
       component: <WorkPreferencesApi />,
+      authorizedRoles: [Role.Applicant],
     }),
   },
   {
     path: profilePaths.aboutMe(),
     action: () => ({
       component: <AboutMeFormContainer />,
+      authorizedRoles: [Role.Applicant],
     }),
   },
   {
     path: profilePaths.diversityEquityInclusion(),
     action: () => ({
       component: <DiversityEquityInclusionFormApi />,
+      authorizedRoles: [Role.Applicant],
     }),
   },
   {
     path: profilePaths.skillsAndExperiences(),
     action: () => ({
       component: <ExperienceAndSkillsRouterApi />,
+      authorizedRoles: [Role.Applicant],
     }),
   },
   {
     path: profilePaths.profilePage(),
     action: () => ({
       component: <ProfilePage />,
+      authorizedRoles: [Role.Applicant],
     }),
   },
 ];
@@ -171,7 +183,6 @@ const profileRoutes = (
 const directIntakeRoutes = (
   directIntakePaths: DirectIntakeRoutes,
 ): Routes<RouterResult> => [
-  // placeholder, switch with real routes
   {
     path: directIntakePaths.home(),
     action: () => ({
@@ -200,6 +211,7 @@ const directIntakeRoutes = (
       const poolId = context.params.id as string;
       return {
         component: <PoolApplyPage id={poolId} />,
+        authorizedRoles: [Role.Applicant],
       };
     },
   },
@@ -240,7 +252,7 @@ export const Router: React.FC = () => {
     />,
   ];
   return (
-    <ClientProvider>
+    <>
       <PageContainer
         menuItems={menuItems}
         contentRoutes={[
@@ -255,7 +267,7 @@ export const Router: React.FC = () => {
         ]}
       />
       <Toast />
-    </ClientProvider>
+    </>
   );
 };
 
