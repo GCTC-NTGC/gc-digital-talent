fbaa151da58fc0647eed0aa1212e67245e1bcb0f
Patrick Connolly <patrick.c.connolly@gmail.com>
2022-06-09 13:00:56 -0400

    Added intermediary wait within e2e tests.

--- frontend/cypress/integration/talentsearch/searchpage.spec.js
+++ frontend/cypress/integration/talentsearch/searchpage.spec.js
@@ -1,16 +1,26 @@
+import { setUpGraphqlIntercepts } from '../../support/graphql-test-utils'
+
 describe("Talentsearch Search Page", () => {
-  beforeEach(() => cy.visit("/en/talent/search"));
+  beforeEach(() => {
+    setUpGraphqlIntercepts()
+    cy.visit("/en/talent/search")
+  });
 
   it("loads page successfully", () => {
     cy.findByRole("button", { name: /Request Candidates/i }).should("exist");
   });
 
   it("checks, submits, and navigates to /request", () => {
+    const doneLoading = () => {
+      cy.wait('@gqlcountPoolCandidatesQuery')
+    }
+
     cy.get("form")
       .findByRole("radio", {
         name: /Required diploma from post-secondary institution/i,
       })
       .check();
+    doneLoading()
     cy.findByRole("button", { name: /Request Candidates/i }).click();
     cy.findByRole("button", { name: /Submit Request/i }).should("exist");
   });
