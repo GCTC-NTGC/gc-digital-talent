5a62127016f0541e4b2b76f16e443e7eb8a6df98
Peter Giles <8978655+petertgiles@users.noreply.github.com>
2022-06-16 10:52:50 -0400

    Fill out talent search smoke test

--- frontend/cypress/integration/talentsearch/search-page.spec.js
+++ frontend/cypress/integration/talentsearch/search-page.spec.js
@@ -1,4 +1,5 @@
-import { setUpCommonGraphqlIntercepts, aliasQuery } from '../../support/graphql-test-utils'
+import { aliasMutation, aliasQuery, setUpCommonGraphqlIntercepts } from '../../support/graphql-test-utils'
+import { getInputByLabel } from '../../support/helpers';
 
 describe("Talentsearch Search Page", () => {
   beforeEach(() => {
@@ -7,6 +8,8 @@ describe("Talentsearch Search Page", () => {
     // page specific requests
     cy.intercept('POST', '/graphql', (req) => {
       aliasQuery(req, 'countPoolCandidates')
+      aliasQuery(req, 'getPoolCandidateSearchRequestData')
+      aliasMutation(req, 'createPoolCandidateSearchRequest')
     })
 
     cy.visit("/en/talent/search")
@@ -16,18 +19,34 @@ describe("Talentsearch Search Page", () => {
     cy.findByRole("button", { name: /Request Candidates/i }).should("exist");
   });
 
-  it("checks, submits, and navigates to /request", () => {
-    const doneLoading = () => {
-      cy.wait('@gqlcountPoolCandidatesQuery')
-    }
-
+  it("searches for candidates and submits a request", () => {
     cy.get("form")
       .findByRole("radio", {
         name: /Required diploma from post-secondary institution/i,
       })
       .check();
-    doneLoading()
+
+    cy.wait('@gqlcountPoolCandidatesQuery')
+
+    // integer greater than 0
+    cy.contains(/Results: [1-9][0-9]* matching candidate/i);
+
     cy.findByRole("button", { name: /Request Candidates/i }).click();
-    cy.findByRole("button", { name: /Submit Request/i }).should("exist");
+
+    cy.wait('@gqlgetPoolCandidateSearchRequestDataQuery');
+
+    getInputByLabel('Full Name').clear().type('Test Full Name');
+    getInputByLabel('Government e-mail').clear().type('test@tbs-sct.gc.ca');
+    getInputByLabel('What is the job title for this position?').clear().type('Test Job Title');
+    getInputByLabel('Additional Comments').clear().type('Test Comments');
+    getInputByLabel('Department / Hiring Organization').select('Treasury Board Secretariat');
+
+    cy.contains("Required diploma from post-secondary institution");
+
+    cy.findByRole("button", { name: /Submit Request/i }).click();
+
+    cy.wait('@gqlcreatePoolCandidateSearchRequestMutation');
+
+    cy.contains("Request created successfully");
   });
 });
