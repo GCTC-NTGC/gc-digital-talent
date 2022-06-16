6d1197fd5ce5b07ef9aa0ce5d0f3cf5f62596774
Peter Giles <8978655+petertgiles@users.noreply.github.com>
2022-06-16 15:28:41 -0400

    linting

--- frontend/cypress/integration/admin/admin-smoke-tests.spec.js
+++ frontend/cypress/integration/admin/admin-smoke-tests.spec.js
@@ -1,64 +1,66 @@
-import { aliasMutation, aliasQuery, setUpCommonGraphqlIntercepts } from '../../support/graphql-test-utils'
-import { getInputByLabel } from '../../support/helpers';
+import {
+  aliasMutation,
+  aliasQuery,
+  setUpCommonGraphqlIntercepts,
+} from "../../support/graphql-test-utils";
+import { getInputByLabel } from "../../support/helpers";
 
 describe("Admin Smoke Tests", () => {
-
   const loginViaUI = (role) => {
-    cy.fixture('users.json').then(users => {
-      const user = users[role]
-      cy.get('input[name=username]').type(user.email)
-    })
-    cy.findByText('Sign-in').click()
-  }
+    cy.fixture("users.json").then((users) => {
+      const user = users[role];
+      cy.get("input[name=username]").type(user.email);
+    });
+    cy.findByText("Sign-in").click();
+  };
 
   beforeEach(() => {
     // common requests
-    setUpCommonGraphqlIntercepts()
+    setUpCommonGraphqlIntercepts();
     // page specific requests
-    cy.intercept('POST', '/graphql', (req) => {
-      aliasQuery(req, 'AllUsers')
-      aliasQuery(req, 'User')
-      aliasMutation(req, 'UpdateUserAsAdmin')
-    })
+    cy.intercept("POST", "/graphql", (req) => {
+      aliasQuery(req, "AllUsers");
+      aliasQuery(req, "User");
+      aliasMutation(req, "UpdateUserAsAdmin");
+    });
 
-    cy.visit("/en/admin")
+    cy.visit("/en/admin");
   });
 
   it("Searches for a user, reviews the profile, and edits the phone number", () => {
-
     // hit the home page, login, hit the dashboard
     cy.contains("h1", "Home");
     cy.contains("a", "Login").click();
     loginViaUI("admin");
     cy.contains("h1", "Welcome back");
-    cy.url().should('contain', '/dashboard');
+    cy.url().should("contain", "/dashboard");
 
     // find the applicant user to play with
     cy.contains("a", "Manage users").click();
     cy.wait("@gqlAllUsersQuery");
-    getInputByLabel('Search').clear().type('Applicant');
+    getInputByLabel("Search").clear().type("Applicant");
     cy.contains("a", "applicant@test.com").click();
 
     // exercise profile page
     cy.contains("h1", "Candidate Details");
     cy.contains("button", "Print Profile");
-    cy.contains('General Information').click();
+    cy.contains("General Information").click();
     cy.contains("span", "About");
-    cy.contains('Candidate Profile').click();
+    cy.contains("Candidate Profile").click();
     cy.contains("span", "About");
 
     // find the applicant user to play with
     cy.contains("a", "Users").click();
     //cy.wait("@gqlAllUsersQuery");  // will be cached and not fired a second time
-    getInputByLabel('Search').clear().type('Applicant');
-    cy.contains('td', 'applicant@test.com')
+    getInputByLabel("Search").clear().type("Applicant");
+    cy.contains("td", "applicant@test.com")
       .siblings()
-      .contains('a', 'Edit')
+      .contains("a", "Edit")
       .click();
     cy.wait("@gqlUserQuery");
 
     // edit the user in a small way
-    getInputByLabel('Telephone').clear().type('+10123456789');
+    getInputByLabel("Telephone").clear().type("+10123456789");
     cy.contains("button", "Submit").click();
     cy.wait("@gqlUpdateUserAsAdminMutation");
     cy.contains("User updated successfully");
--- frontend/cypress/integration/talentsearch/search-smoke-tests.spec.js
+++ frontend/cypress/integration/talentsearch/search-smoke-tests.spec.js
@@ -1,18 +1,22 @@
-import { aliasMutation, aliasQuery, setUpCommonGraphqlIntercepts } from '../../support/graphql-test-utils'
-import { getInputByLabel } from '../../support/helpers';
+import {
+  aliasMutation,
+  aliasQuery,
+  setUpCommonGraphqlIntercepts,
+} from "../../support/graphql-test-utils";
+import { getInputByLabel } from "../../support/helpers";
 
 describe("Talent Search Smoke Tests", () => {
   beforeEach(() => {
     // common requests
-    setUpCommonGraphqlIntercepts()
+    setUpCommonGraphqlIntercepts();
     // page specific requests
-    cy.intercept('POST', '/graphql', (req) => {
-      aliasQuery(req, 'countPoolCandidates')
-      aliasQuery(req, 'getPoolCandidateSearchRequestData')
-      aliasMutation(req, 'createPoolCandidateSearchRequest')
-    })
+    cy.intercept("POST", "/graphql", (req) => {
+      aliasQuery(req, "countPoolCandidates");
+      aliasQuery(req, "getPoolCandidateSearchRequestData");
+      aliasMutation(req, "createPoolCandidateSearchRequest");
+    });
 
-    cy.visit("/en/talent/search")
+    cy.visit("/en/talent/search");
   });
 
   it("searches for candidates and submits a request", () => {
@@ -22,26 +26,30 @@ describe("Talent Search Smoke Tests", () => {
       })
       .check();
 
-    cy.wait('@gqlcountPoolCandidatesQuery')
+    cy.wait("@gqlcountPoolCandidatesQuery");
 
     // integer greater than 0
     cy.contains(/Results: [1-9][0-9]* matching candidate/i);
 
     cy.findByRole("button", { name: /Request Candidates/i }).click();
 
-    cy.wait('@gqlgetPoolCandidateSearchRequestDataQuery');
+    cy.wait("@gqlgetPoolCandidateSearchRequestDataQuery");
 
-    getInputByLabel('Full Name').clear().type('Test Full Name');
-    getInputByLabel('Government e-mail').clear().type('test@tbs-sct.gc.ca');
-    getInputByLabel('What is the job title for this position?').clear().type('Test Job Title');
-    getInputByLabel('Additional Comments').clear().type('Test Comments');
-    getInputByLabel('Department / Hiring Organization').select('Treasury Board Secretariat');
+    getInputByLabel("Full Name").clear().type("Test Full Name");
+    getInputByLabel("Government e-mail").clear().type("test@tbs-sct.gc.ca");
+    getInputByLabel("What is the job title for this position?")
+      .clear()
+      .type("Test Job Title");
+    getInputByLabel("Additional Comments").clear().type("Test Comments");
+    getInputByLabel("Department / Hiring Organization").select(
+      "Treasury Board Secretariat",
+    );
 
     cy.contains("Required diploma from post-secondary institution");
 
     cy.findByRole("button", { name: /Submit Request/i }).click();
 
-    cy.wait('@gqlcreatePoolCandidateSearchRequestMutation');
+    cy.wait("@gqlcreatePoolCandidateSearchRequestMutation");
 
     cy.contains("Request created successfully");
   });
