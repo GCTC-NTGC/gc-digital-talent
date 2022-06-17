b5922b717ce38409168575a0a696603b31346207
Patrick Connolly <patrick.c.connolly@gmail.com>
2022-06-17 11:49:49 -0400

    Added stub of filter modal.

---
+++ frontend/admin/src/js/components/SearchFilter.stories.tsx
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import Button from "@common/components/Button";
+import SearchFilter from "./SearchFilter";
+
+export default {
+  title: 'Admin/SearchFilter'
+  component: SearchFilter,
+} as ComponentMeta<typeof SearchFilter>;
+
+export const Default: ComponentStory<typeof SearchFilter> = () => {
+  const [isOpen, setIsOpen] = useState(true);
+
+  const handleOpen = () => setIsOpen(true);
+  const handleDismiss = () => setIsOpen(false);
+  return (
+    <>
+      <Button onClick={handleOpen}>Filters</Button>
+      <SearchFilter isOpen={isOpen} onDismiss={handleDismiss} />
+    </>
+  );
+};
---
+++ frontend/admin/src/js/components/SearchFilter.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import Dialog from "@common/components/Dialog";
+import { Button } from "@common/components";
+import { FormattedMessage } from "react-intl";
+
+type SearchFilterFooterProps = {
+  handleClear: () => void;
+  handleSubmit: () => void;
+};
+
+const SearchFilterFooter = ({
+  handleClear,
+  handleSubmit,
+}: SearchFilterFooterProps): JSX.Element => (
+  <>
+    <Button type="reset" color="secondary" mode="outline" onClick={handleClear}>
+      <FormattedMessage
+        description="Reset button within the search filter dialog"
+        defaultMessage="Clear filters"
+      />
+    </Button>
+    <Button type="submit" color="cta" onClick={handleSubmit}>
+      <FormattedMessage
+        description="Submit button within the search filter dialog"
+        defaultMessage="Show results"
+      />
+    </Button>
+  </>
+);
+
+type SearchFilterProps = {
+  isOpen: boolean;
+  onDismiss: (e: React.MouseEvent | React.KeyboardEvent) => void;
+};
+
+const SearchFilter = ({
+  isOpen,
+  onDismiss,
+}: SearchFilterProps): JSX.Element => {
+  const handleSubmit = () => {};
+  const handleClear = () => {};
+  return (
+    <Dialog
+      isOpen={isOpen}
+      onDismiss={onDismiss}
+      title="Select filters"
+      subtitle="Narrow down you table results using the following filters."
+      footer={
+        <SearchFilterFooter
+          handleClear={handleClear}
+          handleSubmit={handleSubmit}
+        />
+      }
+    />
+  );
+};
+
+export default SearchFilter;
