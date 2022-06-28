f07e0fb52f5d4254630570d660b5e5c65f4ed19c
Eric Sizer <eric.sizer@gmail.com>
2022-06-28 14:10:41 -0400

    show loading spinner when auth is loading

--- frontend/common/src/components/context/AuthorizationProvider.tsx
+++ frontend/common/src/components/context/AuthorizationProvider.tsx
@@ -2,18 +2,20 @@ import React from "react";
 
 import { AuthorizationContainer } from "../Auth";
 import { useGetMeQuery } from "../../api/generated";
+import Pending from "../Pending";
 
 const AuthorizationProvider: React.FC = ({ children }) => {
   const [result] = useGetMeQuery();
   const { data, fetching, stale } = result;
+  const isLoaded = !fetching && !stale;
 
   return (
     <AuthorizationContainer
       userRoles={data?.me?.roles}
       email={data?.me?.email}
-      isLoaded={!fetching && !stale}
+      isLoaded={isLoaded}
     >
-      {children}
+      <Pending fetching={!isLoaded}>{children}</Pending>
     </AuthorizationContainer>
   );
 };
