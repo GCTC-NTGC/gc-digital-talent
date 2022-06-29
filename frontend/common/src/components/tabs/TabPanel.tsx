c7108442fcad6d04a65ae7ad2c657d6b68495a89
Eric Sizer <eric.sizer@gmail.com>
2022-06-29 14:19:56 -0400

    remove lower case tabs duplicate

--- frontend/common/src/components/tabs/TabPanel.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { TabPanel as ReachTabPanel, type TabPanelProps } from "@reach/tabs";
-
-const TabPanel = (props: TabPanelProps) => (
-  <ReachTabPanel data-h2-padding="b(all, s)" {...props} />
-);
-
-export default TabPanel;
