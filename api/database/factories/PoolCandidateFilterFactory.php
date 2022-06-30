c5f8a8a27345169f4338867ac9ad2f7e58cdc976
tristan-orourke <tristan.orourke@gmail.com>
2022-06-30 09:54:38 -0400

    Add fields to PoolCandidateFilterFactory and randomly leave some fields null

--- api/database/factories/PoolCandidateFilterFactory.php
+++ api/database/factories/PoolCandidateFilterFactory.php
@@ -6,6 +6,7 @@ use App\Models\Classification;
 use App\Models\CmoAsset;
 use App\Models\Pool;
 use App\Models\PoolCandidateFilter;
+use App\Models\Skill;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Database\Helpers\ApiEnums;
 
@@ -19,20 +20,35 @@ class PoolCandidateFilterFactory extends Factory
     protected $model = PoolCandidateFilter::class;
 
     /**
+     * Most real filters won't use every possible field.
+     * This gives a chance of returning null instead of intended value.
+     *
+     * @param [type] $value
+     * @param int $chance Percentage chance of returning $value instead of null.
+     * @return void
+     */
+    function maybe($value, $chance = 40)
+    {
+        return $this->faker->boolean($chance) ?
+            $value : null;
+    }
+
+    /**
      * Define the model's default state.
      *
      * @return array
      */
     public function definition()
     {
+
         return [
-            'has_diploma' => $this->faker->boolean(),
-            'has_disability' => $this->faker->boolean(),
-            'is_indigenous' => $this->faker->boolean(),
-            'is_visible_minority' => $this->faker->boolean(),
-            'is_woman' => $this->faker->boolean(),
-            'language_ability' => $this->faker->randomElement(['FRENCH', 'ENGLISH', 'BILINGUAL', null]),
-            'work_regions' => $this->faker->randomElements(
+            'has_diploma' => $this->maybe($this->faker->boolean()),
+            'has_disability' => $this->maybe($this->faker->boolean()),
+            'is_indigenous' => $this->maybe($this->faker->boolean()),
+            'is_visible_minority' => $this->maybe($this->faker->boolean()),
+            'is_woman' => $this->maybe($this->faker->boolean()),
+            'language_ability' => $this->maybe($this->faker->randomElement(['FRENCH', 'ENGLISH', 'BILINGUAL', null])),
+            'work_regions' => $this->maybe($this->faker->randomElements(
                 [
                     'TELEWORK',
                     'NATIONAL_CAPITAL',
@@ -44,20 +60,31 @@ class PoolCandidateFilterFactory extends Factory
                     'NORTH',
                 ],
                 3
-            ),
-            'operational_requirements' => $this->faker->optional->randomElements(ApiEnums::operationalRequirements(), 2),
+            )),
+            'operational_requirements' => $this->maybe($this->faker->optional->randomElements(ApiEnums::operationalRequirements(), 2)),
+            'would_accept_temporary' => $this->maybe($this->faker->boolean()),
         ];
     }
 
     public function configure()
     {
         return $this->afterCreating(function (PoolCandidateFilter $filter) {
-            $assets = CmoAsset::inRandomOrder()->limit(4)->get();
-            $classifications = Classification::inRandomOrder()->limit(3)->get();
-            $pools = Pool::inRandomOrder()->limit(1)->get();
-            $filter->cmoAssets()->saveMany($assets);
-            $filter->classifications()->saveMany($classifications);
-            $filter->pools()->saveMany($pools);
+            if ($this->maybe(true)) {
+                $assets = CmoAsset::inRandomOrder()->limit(4)->get();
+                $filter->cmoAssets()->saveMany($assets);
+            }
+            if ($this->maybe(true)) {
+                $classifications = Classification::inRandomOrder()->limit(3)->get();
+                $filter->classifications()->saveMany($classifications);
+            }
+            if ($this->maybe(true)) {
+                $skills = Skill::inRandomOrder()->limit(3)->get();
+                $filter->skills()->saveMany($skills);
+            }
+            if ($this->maybe(true)) {
+                $pools = Pool::inRandomOrder()->limit(1)->get();
+                $filter->pools()->saveMany($pools);
+            }
         });
     }
 }
