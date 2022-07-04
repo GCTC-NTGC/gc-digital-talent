81ab43554fbd27c298f91762b890aeb91eb1e003
Patrick Connolly <patrick.c.connolly@gmail.com>
2022-07-04 10:29:32 -0400

    Added MultiSelectFieldV2 components to filter modal.

--- frontend/admin/src/js/components/SearchFilter.tsx
+++ frontend/admin/src/js/components/SearchFilter.tsx
@@ -2,6 +2,10 @@ import React from "react";
 import Dialog from "@common/components/Dialog";
 import { Button } from "@common/components";
 import { FormattedMessage } from "react-intl";
+import { BasicForm } from "@common/components/form";
+import SelectFieldV2 from "@common/components/form/Select/SelectFieldV2";
+import MultiSelectFieldV2 from "@common/components/form/MultiSelect/MultiSelectFieldV2";
+import "./SearchFilter.css";
 
 interface SearchFilterFooterProps {
   handleClear: () => void;
@@ -33,12 +37,60 @@ interface SearchFilterProps {
   onDismiss: (e: React.MouseEvent | React.KeyboardEvent) => void;
 }
 
+const generateOptionsFromValues = (item: string, index: number) => ({
+  label: item,
+  value: index,
+});
+
 const SearchFilter = ({
   isOpen,
   onDismiss,
 }: SearchFilterProps): JSX.Element => {
   const handleSubmit = () => {};
   const handleClear = () => {};
+
+  const optionsData = {
+    pools: [
+      "Digital Talent",
+      "Women in STEM",
+      "Indigenous Apprenticeship Program",
+    ].map(generateOptionsFromValues),
+    languages: ["Any", "English only", "French only", "Bilingual"].map(
+      generateOptionsFromValues,
+    ),
+    classifications: ["CS-01", "CS-02", "CS-03", "CS-04", "CS-05"].map(
+      generateOptionsFromValues,
+    ),
+    workPreferences: [
+      "Overtime",
+      "Shift-work",
+      "24/7 on-call",
+      "Travel",
+      "Transport & Lift (20kg)",
+      "Driver license",
+    ].map(generateOptionsFromValues),
+    workLocations: [
+      "Virtual",
+      "National Capital Region",
+      "Atlantic Region",
+      "Quebec Region",
+      "Ontario Region",
+      "Prairie Region",
+      "British Columbia Region",
+      "North Region",
+    ].map(generateOptionsFromValues),
+    education: ["Combination", "Has diploma"].map(generateOptionsFromValues),
+    durationPreferences: ["Term", "Indeterminate"].map(
+      generateOptionsFromValues,
+    ),
+    availability: ["Actively looking", "Open to opportunities", "Inactive"].map(
+      generateOptionsFromValues,
+    ),
+    skillFilter: ["Data Analysis", "Data Cleaning", "Database Design"].map(
+      generateOptionsFromValues,
+    ),
+  };
+
   return (
     <Dialog
       isOpen={isOpen}
@@ -51,7 +103,81 @@ const SearchFilter = ({
           handleSubmit={handleSubmit}
         />
       }
-    />
+    >
+      <BasicForm onSubmit={() => {}}>
+        <div style={{ display: "flex" }}>
+          <div style={{ flexGrow: 1 }}>
+            <MultiSelectFieldV2
+              id="pools"
+              label="Pools"
+              options={optionsData.pools}
+            />
+          </div>
+          <div style={{ minWidth: 200, marginLeft: 20 }}>
+            <SelectFieldV2
+              id="languages"
+              label="Languages"
+              options={optionsData.languages}
+            />
+          </div>
+        </div>
+        <div style={{ display: "flex" }}>
+          <div style={{ flexGrow: 1, minWidth: 175 }}>
+            <MultiSelectFieldV2
+              id="classifications"
+              label="Classifications"
+              options={optionsData.classifications}
+            />
+          </div>
+          <div style={{ flexGrow: 1, marginLeft: 20 }}>
+            <MultiSelectFieldV2
+              id="workPreferences"
+              label="Work Preferences"
+              options={optionsData.workPreferences}
+            />
+          </div>
+          <div style={{ flexGrow: 1, marginLeft: 20 }}>
+            <MultiSelectFieldV2
+              id="workLocation"
+              label="Work Location"
+              options={optionsData.workLocations}
+            />
+          </div>
+        </div>
+        <div style={{ display: "flex" }}>
+          <div style={{ flexGrow: 1 }}>
+            <SelectFieldV2
+              id="education"
+              label="Education"
+              options={optionsData.education}
+            />
+          </div>
+          <div style={{ flexGrow: 1, marginLeft: 20 }}>
+            <SelectFieldV2
+              id="durationPreference"
+              label="Duration Preference"
+              options={optionsData.durationPreferences}
+            />
+          </div>
+          <div style={{ flexGrow: 1, marginLeft: 20 }}>
+            <MultiSelectFieldV2
+              id="availability"
+              label="Availability"
+              options={optionsData.availability}
+            />
+          </div>
+        </div>
+        <div style={{ display: "flex" }}>
+          <div style={{ flexGrow: 1 }}>
+            <MultiSelectFieldV2
+              id="skillFilter"
+              label="Skill filter"
+              options={optionsData.skillFilter}
+            />
+          </div>
+        </div>
+      </BasicForm>
+    </Dialog>
   );
 };
 
