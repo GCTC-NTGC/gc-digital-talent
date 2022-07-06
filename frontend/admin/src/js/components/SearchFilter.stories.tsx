7145ee5fa3adb749c773d9ce6b51ba2a7611188e
Patrick Connolly <patrick.c.connolly@gmail.com>
2022-07-06 13:46:47 -0400

    Added decorator to help chromatic snapshot dialog components better.

--- frontend/admin/src/js/components/SearchFilter.stories.tsx
+++ frontend/admin/src/js/components/SearchFilter.stories.tsx
@@ -1,14 +1,21 @@
 import React, { useState } from "react";
-import { ComponentMeta, ComponentStory } from "@storybook/react";
+import type { ComponentMeta, ComponentStory, StoryFn } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
 import Button from "@common/components/Button";
 import type { SubmitHandler } from "react-hook-form";
 import SearchFilter, { defaultFormValues } from "./SearchFilter";
 import type { FormValues } from "./SearchFilter";
 
+const OverlayOrDialogDecorator = (Story: StoryFn) => (
+  <div style={{ width: "100%", height: "100vh" }}>
+    <Story />
+  </div>
+);
+
 export default {
   title: "Admin/SearchFilter",
   component: SearchFilter,
+  decorators: [OverlayOrDialogDecorator],
 } as ComponentMeta<typeof SearchFilter>;
 
 export const Default: ComponentStory<typeof SearchFilter> = () => {
