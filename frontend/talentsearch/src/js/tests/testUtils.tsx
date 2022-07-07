b3c122a2da987d1ce787fd046a643f44e8aa8393
Eric Sizer <eric.sizer@gmail.com>
2022-07-07 14:44:08 -0400

    create jest-axe test util

--- frontend/talentsearch/src/js/tests/testUtils.tsx
+++ frontend/talentsearch/src/js/tests/testUtils.tsx
@@ -1,5 +1,6 @@
 import React, { FC, ReactElement } from "react";
 import { render, RenderOptions } from "@testing-library/react";
+import { axe } from "jest-axe";
 import IntlProvider from "react-intl/src/components/provider";
 
 const AllTheProviders: FC = ({ children }) => {
@@ -10,6 +11,11 @@ const customRender = (
   ui: ReactElement,
   options?: Omit<RenderOptions, "wrapper">,
 ) => render(ui, { wrapper: AllTheProviders, ...options });
+
+export const axeTest = async (container: HTMLElement) => {
+  const results = await axe(container);
+  expect(results).toHaveNoViolations();
+};
 
 export * from "@testing-library/react";
 export { customRender as render };
