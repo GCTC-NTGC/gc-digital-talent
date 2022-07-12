1268228e1d7e1b31637eac52cf6b26d6fc47147a
Eric Sizer <eric.sizer@gmail.com>
2022-07-12 15:24:32 -0400

    fix linting warnings

--- frontend/common/src/components/Card/Card.stories.tsx
+++ frontend/common/src/components/Card/Card.stories.tsx
@@ -5,11 +5,6 @@ import Card from "./Card";
 export default {
   component: Card,
   title: "Components/Card",
-  args: {
-    title: "Card Title",
-    content:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sed purus tristique, finibus nisi sit amet, lacinia elit. Nam vitae tellus condimentum, iaculis mauris pellentesque, venenatis ipsum.",
-  },
 } as Meta;
 
 const Spacer: React.FC = ({ children }) => (
@@ -23,45 +18,33 @@ const Spacer: React.FC = ({ children }) => (
   </div>
 );
 
-const Template: Story = (args) => {
-  const { title, content } = args;
-
-  return (
-    <div
-      data-h2-display="b(flex)"
-      data-h2-justify-content="b(space-around)"
-      style={{ margin: "-1rem" }}
-    >
-      <Spacer>
-        <Card title="TS Primary">
-          <p>
-            Etiam auctor bibendum lectus, ornare dapibus est placerat vitae.
-          </p>
-        </Card>
-      </Spacer>
-      <Spacer>
-        <Card title="TS Secondary" color="ts-secondary">
-          <p>
-            Etiam auctor bibendum lectus, ornare dapibus est placerat vitae.
-          </p>
-        </Card>
-      </Spacer>
-      <Spacer>
-        <Card title="IA Primary" color="ia-primary">
-          <p>
-            Etiam auctor bibendum lectus, ornare dapibus est placerat vitae.
-          </p>
-        </Card>
-      </Spacer>
-      <Spacer>
-        <Card title="IA Secondary" color="ia-secondary">
-          <p>
-            Etiam auctor bibendum lectus, ornare dapibus est placerat vitae.
-          </p>
-        </Card>
-      </Spacer>
-    </div>
-  );
-};
+const Template: Story = () => (
+  <div
+    data-h2-display="b(flex)"
+    data-h2-justify-content="b(space-around)"
+    style={{ margin: "-1rem" }}
+  >
+    <Spacer>
+      <Card title="TS Primary">
+        <p>Etiam auctor bibendum lectus, ornare dapibus est placerat vitae.</p>
+      </Card>
+    </Spacer>
+    <Spacer>
+      <Card title="TS Secondary" color="ts-secondary">
+        <p>Etiam auctor bibendum lectus, ornare dapibus est placerat vitae.</p>
+      </Card>
+    </Spacer>
+    <Spacer>
+      <Card title="IA Primary" color="ia-primary">
+        <p>Etiam auctor bibendum lectus, ornare dapibus est placerat vitae.</p>
+      </Card>
+    </Spacer>
+    <Spacer>
+      <Card title="IA Secondary" color="ia-secondary">
+        <p>Etiam auctor bibendum lectus, ornare dapibus est placerat vitae.</p>
+      </Card>
+    </Spacer>
+  </div>
+);
 
 export const CardStory = Template.bind({});
