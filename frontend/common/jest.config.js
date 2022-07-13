722bf4dc7616242ec3590a538b1862c52516f90f
Matt Nigh <matt@mattnigh.com>
2022-07-13 11:24:30 -0400

    Revert "Remove jest config verbose"
    This reverts commit 4f449ff9f32faedec751ee9ad280badc9291dad3.

--- frontend/common/jest.config.js
+++ frontend/common/jest.config.js
@@ -37,5 +37,6 @@ module.exports = {
   // Module file extensions for importing
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
 
+  verbose: true,
   setupFilesAfterEnv: ["./src/tests/setup.ts"],
 };
