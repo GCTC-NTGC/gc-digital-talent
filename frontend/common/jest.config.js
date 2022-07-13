4f449ff9f32faedec751ee9ad280badc9291dad3
Matt Nigh <matt@mattnigh.com>
2022-07-13 10:41:58 -0400

    Remove jest config verbose

--- frontend/common/jest.config.js
+++ frontend/common/jest.config.js
@@ -37,6 +37,5 @@ module.exports = {
   // Module file extensions for importing
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
 
-  verbose: true,
   setupFilesAfterEnv: ["./src/tests/setup.ts"],
 };
