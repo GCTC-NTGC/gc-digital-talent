209a70bcb1730a599ebd9d203a3999400539e2ba
Peter Giles <8978655+petertgiles@users.noreply.github.com>
2022-07-14 14:21:20 -0400

    formatting helper

--- frontend/common/src/helpers/format.tsx
+++ frontend/common/src/helpers/format.tsx
@@ -15,3 +15,13 @@ export const strong = (text: React.ReactNode): React.ReactNode => (
 export const hidden = (text: React.ReactNode): React.ReactNode => (
   <span data-h2-visibility="b(invisible)">{text}</span>
 );
+
+/**
+ * Wraps text in tags to make it the primary color with a heavy weight
+ * @param text text to wrap.
+ */
+export const heavyPrimary = (text: string): React.ReactNode => (
+  <span data-h2-font-color="b(lightpurple)" data-h2-font-weight="b(800)">
+    {text}
+  </span>
+);
