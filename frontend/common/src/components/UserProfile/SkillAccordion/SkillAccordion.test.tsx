f0dd6e05c6f45938b19a35d194447a9176a629a4
vd1992 <40485260+vd1992@users.noreply.github.com>
2022-07-14 10:48:53 -0600

    eliminate title element

--- frontend/common/src/components/UserProfile/SkillAccordion/SkillAccordion.test.tsx
+++ frontend/common/src/components/UserProfile/SkillAccordion/SkillAccordion.test.tsx
@@ -67,10 +67,8 @@ describe("SkillAccordion tests", () => {
     renderSkillAccordion(testSkill);
     const context = screen.getByText("1 Experience");
     const detail = screen.getByTestId("detail");
-    const titleElement = screen.getByTitle("award");
     expect(context).not.toBeNull();
     expect(detail).not.toBeNull();
-    expect(titleElement.innerHTML.trim()).toEqual(experience.title);
   });
   test("It renders proper context and detail when a work experience is provided", () => {
     const experience = experienceGenerator.workExperiences()[0];
