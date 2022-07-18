20e089dc28557061d4a8f0faa5dfef8ebb169531
Patrick Connolly <patrick.c.connolly@gmail.com>
2022-07-18 13:53:14 -0400

    eslint fixup.

--- frontend/common/src/helpers/util.ts
+++ frontend/common/src/helpers/util.ts
@@ -11,7 +11,9 @@ export function identity<T>(value: T): T {
  * @param item
  * @see https://stackoverflow.com/a/46700791
  */
-export function notEmpty<TValue>(value: TValue | null | undefined): value is TValue {
+export function notEmpty<TValue>(
+  value: TValue | null | undefined,
+): value is TValue {
   return value !== null && value !== undefined;
 }
 
