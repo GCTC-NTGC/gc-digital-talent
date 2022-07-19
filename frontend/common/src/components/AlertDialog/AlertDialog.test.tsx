29e9e4008d1e05da472527691c0956a17c29efe9
Eric Sizer <eric.sizer@gmail.com>
2022-07-19 11:24:31 -0400

    remove unused import

--- frontend/common/src/components/AlertDialog/AlertDialog.test.tsx
+++ frontend/common/src/components/AlertDialog/AlertDialog.test.tsx
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 import "@testing-library/jest-dom";
-import { screen, fireEvent } from "@testing-library/react";
+import { screen } from "@testing-library/react";
 import React from "react";
 import { render } from "../../helpers/testUtils";
 import AlertDialog from "./AlertDialog";
