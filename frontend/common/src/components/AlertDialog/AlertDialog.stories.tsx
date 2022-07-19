8fde5af60dfa60b7d0e507c193703f19f6c107fd
Eric Sizer <eric.sizer@gmail.com>
2022-07-19 15:00:37 -0400

    write better alert dialog stories

--- frontend/common/src/components/AlertDialog/AlertDialog.stories.tsx
+++ frontend/common/src/components/AlertDialog/AlertDialog.stories.tsx
@@ -1,17 +1,45 @@
 import React from "react";
 import { Story, Meta } from "@storybook/react";
-import AlertDialogComponent from ".";
+import AlertDialog from "./AlertDialog";
+import type { AlertDialogProps } from "./AlertDialog";
 
 import Button from "../Button";
 
 export default {
-  component: AlertDialogComponent.Content,
+  component: AlertDialog,
   title: "Components/Alert Dialog",
+  args: {
+    title: "Alert Dialog",
+    description: null,
+  },
+  argTypes: {
+    title: {
+      name: "title",
+      type: { name: "string", required: true },
+      control: {
+        type: "text",
+      },
+    },
+    description: {
+      name: "description",
+      type: { name: "string", required: false },
+      control: {
+        type: "text",
+      },
+    },
+  },
 } as Meta;
 
-const Template: Story = () => {
+interface StoryArgs {
+  title: AlertDialogProps["title"];
+  description?: string;
+}
+
+const Template: Story<StoryArgs> = (args) => {
   const leastDestructiveRef = React.useRef(null);
   const [isOpen, setOpen] = React.useState<boolean>(true);
+
+  const { title, description } = args;
 
   const open = () => {
     setOpen(true);
@@ -27,36 +55,40 @@ const Template: Story = () => {
         Open Alert Dialog
       </Button>
       {isOpen && (
-        <AlertDialogComponent.Overlay leastDestructiveRef={leastDestructiveRef}>
-          <AlertDialogComponent.Content>
-            <AlertDialogComponent.Heading onDismiss={close}>
-              Alert Dialog
-            </AlertDialogComponent.Heading>
-            <AlertDialogComponent.Description>
-              Integer nec sem vel quam lacinia dignissim eget in turpis. Nam
-              volutpat, neque sed finibus volutpat, velit velit dignissim enim,
-              sed condimentum quam leo ac purus.
-            </AlertDialogComponent.Description>
-            <AlertDialogComponent.Actions>
-              <Button
-                mode="outline"
-                color="black"
-                ref={leastDestructiveRef}
-                onClick={close}
-              >
-                Cancel
+        <AlertDialog
+          isOpen={isOpen}
+          onDismiss={close}
+          title={title}
+          leastDestructiveRef={leastDestructiveRef}
+        >
+          {description && (
+            <AlertDialog.Description> {description}</AlertDialog.Description>
+          )}
+          <AlertDialog.Actions>
+            <Button
+              mode="outline"
+              color="black"
+              ref={leastDestructiveRef}
+              onClick={close}
+            >
+              Cancel
+            </Button>
+            <div data-h2-margin="b(left, s)">
+              <Button mode="solid" color="primary" onClick={close}>
+                Confirm
               </Button>
-              <div data-h2-margin="b(left, s)">
-                <Button mode="solid" color="primary" onClick={close}>
-                  Confirm
-                </Button>
-              </div>
-            </AlertDialogComponent.Actions>
-          </AlertDialogComponent.Content>
-        </AlertDialogComponent.Overlay>
+            </div>
+          </AlertDialog.Actions>
+        </AlertDialog>
       )}
     </>
   );
 };
 
-export const AlertDialog = Template.bind({});
+export const BasicAlertDialog = Template.bind({});
+export const DescriptionAlertDialog = Template.bind({});
+
+DescriptionAlertDialog.args = {
+  title: "Description Alert Dialog",
+  description: "Are you sure you would like to read this alert?",
+};
