2c2e7b9ff609fa8cb2e9cb5e1c8589701175f41d
Eric Sizer <eric.sizer@gmail.com>
2022-08-12 14:58:17 -0400

    update exp and skills story with missing skills

--- frontend/talentsearch/src/js/components/experienceAndSkills/ExperienceAndSkills.stories.tsx
+++ frontend/talentsearch/src/js/components/experienceAndSkills/ExperienceAndSkills.stories.tsx
@@ -1,29 +1,62 @@
 import React from "react";
-import { Story, Meta } from "@storybook/react";
-import fakeExperiences from "@common/fakeData/fakeExperiences";
-import {
-  ExperienceAndSkills,
-  ExperienceAndSkillsProps,
-} from "./ExperienceAndSkills";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { fakeSkills, fakeExperiences } from "@common/fakeData";
+import { flatten, notEmpty } from "@common/helpers/util";
+import { ExperienceAndSkills } from "./ExperienceAndSkills";
 
 export default {
   component: ExperienceAndSkills,
   title: "ApplicantProfile/ExperienceAndSkillsPage",
-} as Meta;
+} as ComponentMeta<typeof ExperienceAndSkills>;
 
-const TemplateExperienceAndSkillsPage: Story<ExperienceAndSkillsProps> = (
-  args,
-) => {
+const Template: ComponentStory<typeof ExperienceAndSkills> = (args) => {
   return <ExperienceAndSkills {...args} />;
 };
 
-export const NoExperiences = TemplateExperienceAndSkillsPage.bind({});
-export const WithExperiences = TemplateExperienceAndSkillsPage.bind({});
+export const NoExperiences = Template.bind({});
+export const WithExperiences = Template.bind({});
+export const NoExperiencesMissingSkills = Template.bind({});
+export const WithExperiencesMissingSkills = Template.bind({});
+
+const mockExperiences = fakeExperiences(10);
+const mockExperienceSkills = flatten(
+  mockExperiences
+    .map((experience) => {
+      return experience.skills;
+    })
+    .filter(notEmpty),
+);
+
+const mockSkills = [...fakeSkills(20), ...mockExperienceSkills];
+
+function getRandom<T>(items: T[], n: number) {
+  const shuffled = items.sort(() => 0.5 - Math.random());
+  return shuffled.slice(0, n);
+}
+
+const mockRequiredSkills = getRandom(mockSkills, 5);
+const mockOptionalSkills = getRandom(mockSkills, 5);
 
 NoExperiences.args = {
   experiences: [],
 };
 
 WithExperiences.args = {
-  experiences: fakeExperiences(10),
+  experiences: mockExperiences,
+};
+
+NoExperiencesMissingSkills.args = {
+  experiences: [],
+  missingSkills: {
+    requiredSkills: mockRequiredSkills,
+    optionalSkills: mockOptionalSkills,
+  },
+};
+
+WithExperiencesMissingSkills.args = {
+  experiences: mockExperiences,
+  missingSkills: {
+    requiredSkills: mockRequiredSkills,
+    optionalSkills: mockOptionalSkills,
+  },
 };
