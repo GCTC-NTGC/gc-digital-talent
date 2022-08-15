c16ac8f7602d1e670dcf57c3861b0c40f897edb5
Yonathan K <yoni.kid15@gmail.com>
2022-08-15 16:42:28 -0400

    Add SignAndSubmit page to Router

--- frontend/talentsearch/src/js/directIntakeRoutes.ts
+++ frontend/talentsearch/src/js/directIntakeRoutes.ts
@@ -16,6 +16,8 @@ const directIntakeRoutes = (lang: string) => {
     poolAdvertisement: (id: string) => path.join(home(), "pools", id, "view"),
     poolApplyThanks: (id: string) =>
       path.join(home(), "pools", id, "apply", "thanks"),
+    signAndSubmit: (id: string) =>
+      path.join(home(), "applications", id, "submit"),
   };
 };
 
