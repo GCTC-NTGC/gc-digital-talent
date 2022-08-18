548f2a9acb7b1240478e38b255c81b1b148a98c0
Josh Beveridge <git@joshbeveridge.ca>
2022-08-18 15:10:24 -0400

    Fixes the new object button overlapping the other table filter controls

--- frontend/admin/src/js/components/Table/Table.tsx
+++ frontend/admin/src/js/components/Table/Table.tsx
@@ -129,7 +129,7 @@ function Table<T extends Record<string, unknown>>({
         <div data-h2-margin="base(x2, 0, x.5, 0)">
           <p>{title && <span data-h2-font-weight="base(700)">{title}</span>}</p>
           <div data-h2-flex-grid="base(center, 0, x1)">
-            <div data-h2-flex-item="base(fill)">
+            <div data-h2-flex-item="base(1of1) l-tablet(fill)">
               <div data-h2-flex-grid="base(center, 0, x.5)">
                 <div data-h2-flex-item="base(content)">
                   <SearchForm onChange={setGlobalFilter} />
@@ -193,7 +193,7 @@ function Table<T extends Record<string, unknown>>({
                 </div>
               </div>
             </div>
-            <div data-h2-flex-item="base(content)">
+            <div data-h2-flex-item="base(1of1) l-tablet(content)">
               {addBtn && (
                 <Link
                   mode="solid"
