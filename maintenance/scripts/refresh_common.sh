c82774fc7d93956d8c0525c41e09ad9e0cd06fb4
Yonathan K <yoni.kid15@gmail.com>
2022-08-24 10:44:07 -0400

    Fix typo in refresh_common.sh

--- maintenance/scripts/refresh_common.sh
+++ maintenance/scripts/refresh_common.sh
@@ -6,7 +6,7 @@ source ${parent_path}/lib/common.sh
 cd /var/www/html/frontend
 npm install
 
-cd /var/www/html/frontend
+cd /var/www/html/frontend/common
 (cd ..;node node_modules/@hydrogen-design-system/hydrogen.css/bin/build.js)
 npm run codegen
 npm run intl-compile
