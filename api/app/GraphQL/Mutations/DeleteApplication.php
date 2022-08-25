bdfd8361ff0c4b57f00cedeacdf1d9fde8afa9a7
vd1992 <40485260+vd1992@users.noreply.github.com>
2022-08-25 13:25:55 -0600

    copypaste fail

--- api/app/GraphQL/Mutations/DeleteApplication.php
+++ api/app/GraphQL/Mutations/DeleteApplication.php
@@ -17,8 +17,8 @@ final class DeleteApplication
     {
         // grab the specific application, validate to ensure the status of the application is DRAFT or DRAFT_EXPIRED
         $application = PoolCandidate::find($args['id']);
-        $archivalValidator = new DeleteApplicationValidator;
-        $validator = Validator::make($application->toArray(), $archivalValidator->rules(), $archivalValidator->messages());
+        $deletionValidator = new DeleteApplicationValidator;
+        $validator = Validator::make($application->toArray(), $deletionValidator->rules(), $deletionValidator->messages());
         if ($validator->fails()) {
             throw new ValidationException($validator->errors()->first(), $validator);
         }
