66ef5f0642a56483a5846f1004b07141e10393cd
Eric Sizer <eric.sizer@gmail.com>
2022-08-29 10:36:39 -0400

    fix removal of search skills copy

--- frontend/talentsearch/src/js/components/skills/AddSkillsToFilter/AddSkillsToFilter.tsx
+++ frontend/talentsearch/src/js/components/skills/AddSkillsToFilter/AddSkillsToFilter.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useIntl } from "react-intl";
 
 import type { Skill } from "@common/api/generated";
 import SkillPicker from "@common/components/skills/SkillPicker";
@@ -10,6 +11,7 @@ export interface AddSkillsToFilterProps {
 }
 
 const AddSkillsToFilter: React.FC<AddSkillsToFilterProps> = ({ allSkills }) => {
+  const intl = useIntl();
   const { control, watch } = useFormContext();
   const watchedSkills = watch("skills");
   const { replace } = useFieldArray({
@@ -43,6 +45,32 @@ const AddSkillsToFilter: React.FC<AddSkillsToFilterProps> = ({ allSkills }) => {
 
   return (
     <div data-h2-margin="base(x1.5, 0)">
+      <h3
+        data-h2-font-size="base(h6, 1)"
+        data-h2-font-weight="base(700)"
+        data-h2-margin="base(x3, 0, x1, 0)"
+      >
+        {intl.formatMessage({
+          defaultMessage: "Skills as filters",
+          description: "Title for the skill filters on search page.",
+        })}
+      </h3>
+      <p>
+        {intl.formatMessage({
+          defaultMessage:
+            "Find candidates with the right skills for the job. Use the following tabs to find skills that are necessary for the job and select them to use them as filters for matching candidates.",
+          description:
+            "Describing how to use the skill filters on search page, paragraph one.",
+        })}
+      </p>
+      <p data-h2-margin="base(x.5, 0, x1, 0)">
+        {intl.formatMessage({
+          defaultMessage:
+            " Why are there a limited number of skills? It’s important that applicants and managers are pulling from the same list of skills in order to create matches.",
+          description:
+            "Describing how to use the skill filters on search page, paragraph two.",
+        })}
+      </p>
       <SkillPicker
         skills={allSkills || []}
         onChange={handleChange}
