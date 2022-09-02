ba17f9fc4a55e3d9a18e3ed8986d872d9fc5554c
Matt Nigh <matt@mattnigh.com>
2022-09-02 15:44:21 -0400

    Add pgcrypto postgres extension

--- infrastructure/db/create-tc-db.sql
+++ infrastructure/db/create-tc-db.sql
@@ -9,3 +9,6 @@ CREATE DATABASE gctalent
         CONNECTION LIMIT = 25;
 GRANT CONNECT, TEMPORARY ON DATABASE gctalent TO public;
 GRANT ALL ON DATABASE gctalent TO postgres;
+
+/* extension required for gen_random_uuid() via the pgcrypto contrib module before PostgreSQL 13 */
+CREATE extension pgcrypto;
