92b972070a77cb5968528911a3fbd18343a2a83a
vd1992 <40485260+vd1992@users.noreply.github.com>
2022-09-06 09:21:22 -0600

    string -> int

--- api/database/migrations/2022_09_06_032007_generated_column_priority.php
+++ api/database/migrations/2022_09_06_032007_generated_column_priority.php
@@ -14,13 +14,13 @@ class GeneratedColumnPriority extends Migration
      */
     public function up()
     {
-        DB::statement("ALTER TABLE users ADD COLUMN priority_status VARCHAR(50)
+        DB::statement("ALTER TABLE users ADD COLUMN priority_status INT
         GENERATED ALWAYS AS
         (case
-        when has_priority_entitlement = TRUE then 'PRIORITY'
-        when armed_forces_status = 'VETERAN' then 'VETERAN'
-        when citizenship = 'CITIZEN' OR citizenship = 'PERMANENT_RESIDENT' then 'CITIZEN_OR_PR'
-        else 'OTHER'
+        when has_priority_entitlement = TRUE then 1
+        when armed_forces_status = 'VETERAN' then 2
+        when citizenship = 'CITIZEN' OR citizenship = 'PERMANENT_RESIDENT' then 3
+        else 4
         end)
         STORED;");
     }
