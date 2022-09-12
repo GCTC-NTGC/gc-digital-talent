014b509198efba5f64b52aa888959795421c2820
Vachan <40485260+vd1992@users.noreply.github.com>
2022-09-12 14:16:41 -0600

    Apply suggestions from code review
    grouped together
    
    Co-authored-by: Patrick Connolly <patrick.c.connolly@gmail.com>

--- frontend/admin/src/js/components/user/util.ts
+++ frontend/admin/src/js/components/user/util.ts
@@ -9,14 +9,8 @@ import {
 export function stringToEnumLanguage(
   selection: string,
 ): LanguageAbility | undefined {
-  if (selection === LanguageAbility.Bilingual) {
-    return LanguageAbility.Bilingual;
-  }
-  if (selection === LanguageAbility.English) {
-    return LanguageAbility.English;
-  }
-  if (selection === LanguageAbility.French) {
-    return LanguageAbility.French;
+  if (Object.values(LanguageAbility).includes(selection as LanguageAbility)) {
+    return selection as LanguageAbility;
   }
   return undefined;
 }
@@ -24,29 +18,8 @@ export function stringToEnumLanguage(
 export function stringToEnumLocation(
   selection: string,
 ): WorkRegion | undefined {
-  if (selection === WorkRegion.Atlantic) {
-    return WorkRegion.Atlantic;
-  }
-  if (selection === WorkRegion.BritishColumbia) {
-    return WorkRegion.BritishColumbia;
-  }
-  if (selection === WorkRegion.NationalCapital) {
-    return WorkRegion.NationalCapital;
-  }
-  if (selection === WorkRegion.North) {
-    return WorkRegion.North;
-  }
-  if (selection === WorkRegion.Ontario) {
-    return WorkRegion.Ontario;
-  }
-  if (selection === WorkRegion.Prairie) {
-    return WorkRegion.Prairie;
-  }
-  if (selection === WorkRegion.Quebec) {
-    return WorkRegion.Quebec;
-  }
-  if (selection === WorkRegion.Telework) {
-    return WorkRegion.Telework;
+  if (Object.values(WorkRegion).includes(selection as WorkRegion)) {
+    return selection as WorkRegion;
   }
   return undefined;
 }
@@ -54,35 +27,8 @@ export function stringToEnumLocation(
 export function stringToEnumOperational(
   selection: string,
 ): OperationalRequirement | undefined {
-  if (selection === OperationalRequirement.DriversLicense) {
-    return OperationalRequirement.DriversLicense;
-  }
-  if (selection === OperationalRequirement.OnCall) {
-    return OperationalRequirement.OnCall;
-  }
-  if (selection === OperationalRequirement.OvertimeOccasional) {
-    return OperationalRequirement.OvertimeOccasional;
-  }
-  if (selection === OperationalRequirement.OvertimeRegular) {
-    return OperationalRequirement.OvertimeRegular;
-  }
-  if (selection === OperationalRequirement.OvertimeScheduled) {
-    return OperationalRequirement.OvertimeScheduled;
-  }
-  if (selection === OperationalRequirement.OvertimeShortNotice) {
-    return OperationalRequirement.OvertimeShortNotice;
-  }
-  if (selection === OperationalRequirement.ShiftWork) {
-    return OperationalRequirement.ShiftWork;
-  }
-  if (selection === OperationalRequirement.TransportEquipment) {
-    return OperationalRequirement.TransportEquipment;
-  }
-  if (selection === OperationalRequirement.Travel) {
-    return OperationalRequirement.Travel;
-  }
-  if (selection === OperationalRequirement.WorkWeekends) {
-    return OperationalRequirement.WorkWeekends;
+  if (Object.values(OperationalRequirement).includes(selection as OperationalRequirement)) {
+    return selection as OperationalRequirement;
   }
   return undefined;
 }
@@ -90,14 +36,8 @@ export function stringToEnumOperational(
 export function stringToEnumJobLooking(
   selection: string,
 ): JobLookingStatus | undefined {
-  if (selection === JobLookingStatus.ActivelyLooking) {
-    return JobLookingStatus.ActivelyLooking;
-  }
-  if (selection === JobLookingStatus.Inactive) {
-    return JobLookingStatus.Inactive;
-  }
-  if (selection === JobLookingStatus.OpenToOpportunities) {
-    return JobLookingStatus.OpenToOpportunities;
+  if (Object.values(JobLookingStatus).includes(selection as JobLookingStatus)) {
+    return selection as JobLookingStatus;
   }
   return undefined;
 }
