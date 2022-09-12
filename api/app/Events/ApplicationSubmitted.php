309bcde15ed9b40932436ad2dd1837df1549c624
Eric Sizer <eric.sizer@gmail.com>
2022-09-12 15:11:03 -0400

    add snapshot event and listener

---
+++ api/app/Events/ApplicationSubmitted.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\PoolCandidate;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
+
+class ApplicationSubmitted
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * The pool candidate being submitted
+     *
+     * @var PoolCandidate
+     *
+     */
+    public $poolCandidate;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct(PoolCandidate $poolCandidate)
+    {
+        $this->poolCandidate = $poolCandidate;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new PrivateChannel('channel-name');
+    }
+}
---
+++ api/app/Listeners/StoreApplicationSnapshot.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\ApplicationSubmitted;
+use App\GraphQL\Util\GraphQLClient;
+
+
+class StoreApplicationSnapshot
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  \App\Events\ApplicationSubmitted  $event
+     * @return void
+     */
+    public function handle(ApplicationSubmitted $event)
+    {
+        $poolCandidate = $event->poolCandidate;
+
+        $query = file_get_contents(base_path('app/GraphQL/Mutations/PoolCandidateSnapshot.graphql'), true);
+        $result = GraphQLClient::graphQL($query, ['userId' => $poolCandidate->user_id]);
+
+        $poolCandidate->profile_snapshot = $result;
+        $poolCandidate->save();
+    }
+}
