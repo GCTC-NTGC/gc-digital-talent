970f5f898be17ef2a7cfbc4bacad4d332ea154b4
Eric Sizer <eric.sizer@gmail.com>
2022-09-14 16:15:54 -0400

    remove channels and unused imports

--- api/app/Events/ApplicationSubmitted.php
+++ api/app/Events/ApplicationSubmitted.php
@@ -3,14 +3,9 @@
 namespace App\Events;
 
 use App\Models\PoolCandidate;
-use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Broadcasting\PresenceChannel;
-use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
-use Illuminate\Support\Facades\Log;
 
 class ApplicationSubmitted
 {
@@ -32,15 +27,5 @@ class ApplicationSubmitted
     public function __construct(PoolCandidate $poolCandidate)
     {
         $this->poolCandidate = $poolCandidate;
-    }
-
-    /**
-     * Get the channels the event should broadcast on.
-     *
-     * @return \Illuminate\Broadcasting\Channel|array
-     */
-    public function broadcastOn()
-    {
-        return new PrivateChannel('channel-name');
     }
 }
