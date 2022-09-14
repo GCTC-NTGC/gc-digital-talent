c95c369168f653e5b0ba851d723602120bb66210
Eric Sizer <eric.sizer@gmail.com>
2022-09-14 09:36:14 -0400

    create application status form stories

--- frontend/admin/src/js/components/poolCandidate/ApplicationStatusForm/ApplicationStatusForm.stories.tsx
+++ frontend/admin/src/js/components/poolCandidate/ApplicationStatusForm/ApplicationStatusForm.stories.tsx
@@ -2,12 +2,17 @@ import React from "react";
 import type { ComponentMeta, ComponentStory } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
 import { fakePoolCandidates } from "@common/fakeData";
-import { ApplicationStatusForm, type FormValues } from ".";
+import { UpdatePoolCandidateAsAdminInput } from "admin/src/js/api/generated";
+import {
+  ApplicationStatusForm,
+  type FormValues,
+} from "./ApplicationStatusForm";
 
 type Meta = ComponentMeta<typeof ApplicationStatusForm>;
 type Story = ComponentStory<typeof ApplicationStatusForm>;
 
 const mockApplications = fakePoolCandidates(1);
+const mockApplication = mockApplications[0];
 
 export default {
   component: ApplicationStatusForm,
@@ -15,27 +20,41 @@ export default {
 } as Meta;
 
 const Template: Story = (args) => {
+  const [isSubmitting, setSubmitting] = React.useState<boolean>(false);
   const { application } = args;
 
-  const handleSubmit = async (id: string, values: FormValues) => {
-    await action(JSON.stringify({ id, values }));
+  const handleSubmit = (values: UpdatePoolCandidateAsAdminInput) => {
+    action(JSON.stringify({ values }));
+    setSubmitting(true);
+    setTimeout(() => {
+      setSubmitting(false);
+    }, 1000);
   };
 
   return (
-    <ApplicationStatusForm application={application} onSubmit={handleSubmit} />
+    <div style={{ maxWidth: "30rem" }}>
+      <ApplicationStatusForm
+        isSubmitting={isSubmitting}
+        application={application}
+        onSubmit={handleSubmit}
+      />
+    </div>
   );
 };
 
 export const FormWithData = Template.bind({});
 
 FormWithData.args = {
-  application: mockApplications[0],
+  application: mockApplication,
 };
 
 export const FormNoData = Template.bind({});
 
-FormWithData.args = {
+FormNoData.args = {
   application: {
-    status: null,
+    ...mockApplication,
+    status: undefined,
+    expiryDate: undefined,
+    notes: "",
   },
 };
