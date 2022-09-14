01cf18f19f2a2d5e62f1966cc14f439251618789
Patrick Connolly <patrick.c.connolly@gmail.com>
2022-09-14 12:56:41 -0400

    Corrected French ellipsis to 3-char version.

--- frontend/talentsearch/src/js/components/workPreferencesForm/WorkPreferencesForm.tsx
+++ frontend/talentsearch/src/js/components/workPreferencesForm/WorkPreferencesForm.tsx
@@ -42,6 +42,12 @@ export interface WorkPreferencesFormProps {
 interface WithEllipsisPrefixProps {
   children: React.ReactNode;
 }
+/**
+ * Helps prepend ellipses to other strings.
+ * (Whitespace conventions for using the ellipsis varies between languages.)
+ *
+ * @see https://www.btb.termiumplus.gc.ca/tcdnstyl-chap?lang=eng&lettr=chapsect17&info0=17.07
+ */
 const WithEllipsisPrefix = ({ children }: WithEllipsisPrefixProps) => {
   const { formatMessage } = useIntl();
   const ellipsisPrefix = formatMessage({
