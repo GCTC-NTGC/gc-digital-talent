30927e5483ba5328c6920d019a44283dcc60a16b
Eric Sizer <eric.sizer@gmail.com>
2022-09-14 09:36:04 -0400

    add application status form with operations

---
+++ frontend/admin/src/js/components/poolCandidate/ApplicationStatusForm/ApplicationStatusForm.tsx
@@ -0,0 +1,275 @@
+import React from "react";
+import { FormProvider, SubmitHandler, useForm } from "react-hook-form";
+import { useIntl } from "react-intl";
+import { toast } from "react-toastify";
+import { Input, Select, Submit, TextArea } from "@common/components/form";
+import Pending from "@common/components/Pending";
+import NotFound from "@common/components/NotFound";
+import Heading from "@common/components/Heading";
+import { CalendarIcon, PencilSquareIcon } from "@heroicons/react/24/outline";
+import { getLocalizedName } from "@common/helpers/localize";
+import { enumToOptions } from "@common/helpers/formUtils";
+import { getPoolCandidateStatus } from "@common/constants/localizedConstants";
+import { strToFormDate } from "@common/helpers/dateUtils";
+import { commonMessages } from "@common/messages";
+import {
+  PoolCandidateStatus,
+  Scalars,
+  UpdatePoolCandidateAsAdminInput,
+  useGetPoolCandidateStatusQuery,
+  useUpdatePoolCandidateStatusMutation,
+  type PoolCandidate,
+} from "../../../api/generated";
+
+export type FormValues = {
+  status?: PoolCandidate["status"];
+  notes?: PoolCandidate["notes"];
+  expiryDate?: PoolCandidate["expiryDate"];
+};
+
+export interface ApplicationStatusFormProps {
+  isSubmitting: boolean;
+  application: Omit<PoolCandidate, "user">;
+  onSubmit: (values: UpdatePoolCandidateAsAdminInput) => void;
+}
+
+export const ApplicationStatusForm = ({
+  application,
+  onSubmit,
+  isSubmitting,
+}: ApplicationStatusFormProps) => {
+  const intl = useIntl();
+  const methods = useForm<FormValues>({
+    defaultValues: {
+      status: application.status ?? undefined,
+      notes: application.notes ?? "",
+      expiryDate: application.expiryDate
+        ? strToFormDate(application.expiryDate)
+        : undefined,
+    },
+  });
+  const { handleSubmit } = methods;
+
+  const handleFormSubmit: SubmitHandler<FormValues> = (values: FormValues) => {
+    onSubmit({
+      status: values.status,
+      notes: values.notes,
+      expiryDate: values.expiryDate,
+    });
+  };
+
+  return (
+    <div
+      data-h2-position="base(sticky)"
+      data-h2-width="base(100%)"
+      style={{ top: 0 }}
+    >
+      <Heading
+        level="h2"
+        data-h2-margin="base(0, 0, x0.5, 0)"
+        data-h2-font-size="base(h3)"
+      >
+        {intl.formatMessage({
+          defaultMessage: "Application status",
+          id: "/s66sg",
+          description: "Title for admins to edit an applications status.",
+        })}
+      </Heading>
+      <div
+        data-h2-background-color="base(light.dt-gray)"
+        data-h2-radius="base(x1)"
+        data-h2-padding="base(x1, x1, x1, x1)"
+      >
+        <FormProvider {...methods}>
+          <form onSubmit={handleSubmit(handleFormSubmit)}>
+            <Heading
+              level="h3"
+              data-h2-font-size="base(h4)"
+              data-h2-margin="base(0, 0, x0.25, 0)"
+              data-h2-display="base(flex)"
+              data-h2-align-items="base(center)"
+              data-h2-gap="base(0, x0.25)"
+            >
+              <CalendarIcon
+                data-h2-width="base(0.75em)"
+                data-h2-height="base(0.75em)"
+                data-h2-margin="base(0, x0.25, 0, 0)"
+              />
+              <span>
+                {intl.formatMessage({
+                  defaultMessage: "Candidate status",
+                  id: "ETrCOq",
+                  description:
+                    "Title for admin editing a pool candidates status",
+                })}
+              </span>
+            </Heading>
+            <Select
+              label={intl.formatMessage({
+                defaultMessage: "Pool status",
+                id: "4idVCF",
+                description: "Label for the current applications pool status",
+              })}
+              required
+              rules={{ required: true }}
+              id="status"
+              name="status"
+              nullSelection={intl.formatMessage({
+                defaultMessage: "Select a status",
+                id: "VMhVyJ",
+                description: "Placeholder text for the pool status field",
+              })}
+              options={enumToOptions(PoolCandidateStatus).map(({ value }) => ({
+                value,
+                label: intl.formatMessage(getPoolCandidateStatus(value)),
+              }))}
+            />
+            <Input
+              id="expiryDate"
+              name="expiryDate"
+              label={intl.formatMessage({
+                defaultMessage: "Pool expiry date",
+                id: "W4O1Ip",
+                description:
+                  "Label displayed on the pool candidate application form expiry date field.",
+              })}
+              type="date"
+            />
+            <Heading
+              level="h3"
+              data-h2-font-size="base(h4)"
+              data-h2-display="base(flex)"
+              data-h2-align-items="base(center)"
+            >
+              <PencilSquareIcon
+                data-h2-width="base(0.75em)"
+                data-h2-height="base(0.75em)"
+                data-h2-margin="base(0, x0.25, 0, 0)"
+              />
+              <span>
+                {intl.formatMessage({
+                  defaultMessage: "Notes",
+                  id: "npC3bT",
+                  description:
+                    "Title for admin editing a pool candidates notes",
+                })}
+              </span>
+            </Heading>
+            <p data-h2-margin="base(x0.5, 0, x0.5, 0)">
+              {intl.formatMessage({
+                id: "zLvpBy",
+                defaultMessage:
+                  "These notes are shared between all managers of this pool, but not to candidates.",
+                description: "Description of the pool candidate notes field.",
+              })}
+            </p>
+            <TextArea
+              id="notes"
+              name="notes"
+              label={intl.formatMessage(
+                {
+                  defaultMessage: "Notes - {poolName}",
+                  id: "Yr4DW5",
+                  description:
+                    "Label for the notes field on the pool candidate application",
+                },
+                {
+                  poolName: getLocalizedName(application.pool.name, intl),
+                },
+              )}
+            />
+            <Submit
+              color="cta"
+              isSubmitting={isSubmitting}
+              text={intl.formatMessage({
+                id: "OunUSG",
+                defaultMessage: "Save changes",
+                description:
+                  "Text for the pool candidate application status submit button",
+              })}
+              isSubmittingText={intl.formatMessage({
+                defaultMessage: "Saving...",
+                id: "4Czd5U",
+                description:
+                  "Text displayed on the pool candidate application submit button while saving",
+              })}
+            />
+          </form>
+        </FormProvider>
+      </div>
+    </div>
+  );
+};
+
+interface ApplicationStatusFormApiProps {
+  id: Scalars["ID"];
+}
+
+const ApplicationStatusFormApi = ({ id }: ApplicationStatusFormApiProps) => {
+  const intl = useIntl();
+  const [{ data, fetching, error }] = useGetPoolCandidateStatusQuery({
+    variables: { id },
+  });
+
+  const [{ fetching: mutationFetching }, executeMutation] =
+    useUpdatePoolCandidateStatusMutation();
+
+  const handleError = () => {
+    toast.error(
+      intl.formatMessage({
+        defaultMessage: "Error: could not update pool candidate status",
+        id: "FSlrKF",
+        description:
+          "Message displayed when an error occurs while an admin updates a pool candidate",
+      }),
+    );
+  };
+
+  const handleUpdate = (input: UpdatePoolCandidateAsAdminInput) => {
+    executeMutation({ id, input })
+      .then((result) => {
+        if (result.data?.updatePoolCandidateAsAdmin) {
+          toast.success(
+            intl.formatMessage({
+              defaultMessage: "Pool candidate status updated successfully",
+              id: "uSdcX4",
+              description:
+                "Message displayed when a pool candidate has been updated by and admin",
+            }),
+          );
+        } else {
+          handleError();
+        }
+      })
+      .catch(() => {
+        handleError();
+      });
+  };
+
+  return (
+    <Pending fetching={fetching} error={error}>
+      {data?.poolCandidate ? (
+        <ApplicationStatusForm
+          isSubmitting={mutationFetching}
+          application={data.poolCandidate}
+          onSubmit={handleUpdate}
+        />
+      ) : (
+        <NotFound headingMessage={intl.formatMessage(commonMessages.notFound)}>
+          <p>
+            {intl.formatMessage(
+              {
+                defaultMessage: "Pool Candidate {id} not found.",
+                id: "atniLV",
+                description: "Message displayed for pool candidate not found.",
+              },
+              { id },
+            )}
+          </p>
+        </NotFound>
+      )}
+    </Pending>
+  );
+};
+
+export default ApplicationStatusFormApi;
