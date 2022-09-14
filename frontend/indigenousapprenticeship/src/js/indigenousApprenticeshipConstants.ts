0f22148a5c15eb4bd91fbf6d87cdff0fa8d55fc9
Brinda Sasikumar <brinda.sasikumar@gmail.com>
2022-09-14 16:44:50 -0400

    Feature/3219 iap analytics2 (#3941)
    * draft changes
    
    * use react plugin
    
    * add dependency @microsoft/applicationinsights-react-js
    
    * add applicationinsights-web dependency
    
    * Add APPLICATIONINSIGHTS_CONNECTION_STRING
    
    * Move appInsights to Router
    replace React plugin with JavaScript SDK
    
    * Update frontend/indigenousapprenticeship/package.json
    
    Co-authored-by: Matt <mnigh@users.noreply.github.com>
    
    * remove AppInsights.tsx
    
    * remove AppInsights dependency from root
    
    Co-authored-by: Matt Nigh <matt@mattnigh.com>
    Co-authored-by: Matt <mnigh@users.noreply.github.com>

--- frontend/indigenousapprenticeship/src/js/indigenousApprenticeshipConstants.ts
+++ frontend/indigenousapprenticeship/src/js/indigenousApprenticeshipConstants.ts
@@ -1,4 +1,5 @@
 const INDIGENOUSAPPRENTICESHIP_APP_DIR =
   (process.env.INDIGENOUSAPPRENTICESHIP_APP_DIR as string) ??
   "/indigenous-it-apprentice"; // fallback used by storybook
+
 export default INDIGENOUSAPPRENTICESHIP_APP_DIR;
