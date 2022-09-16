7c43873984aef83e6a92a796b6b62b574513c43c
JamesHuf <jamiehufnagel@hotmail.com>
2022-09-16 15:09:03 -0700

    Changed subtitle in PageHeader to be ReactNode

--- frontend/common/src/components/PageHeader/PageHeader.tsx
+++ frontend/common/src/components/PageHeader/PageHeader.tsx
@@ -3,7 +3,7 @@ import Heading from "../Heading";
 
 export interface PageHeaderProps {
   icon?: React.FC<{ className: string }>;
-  subtitle?: string;
+  subtitle?: React.ReactNode;
 }
 
 const PageHeader: React.FC<PageHeaderProps> = ({
