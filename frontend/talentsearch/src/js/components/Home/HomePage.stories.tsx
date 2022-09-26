2c78cbe93d3555de63d9952ec7707cc646a4c0b5
Eric Sizer <eric.sizer@gmail.com>
2022-09-26 10:47:45 -0400

    add viewport snapshots

--- frontend/talentsearch/src/js/components/Home/HomePage.stories.tsx
+++ frontend/talentsearch/src/js/components/Home/HomePage.stories.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { ComponentMeta, ComponentStory } from "@storybook/react";
+import { INITIAL_VIEWPORTS } from "@storybook/addon-viewport";
+import { widthOf, heightOf } from "@common/helpers/storybookUtils";
 import Home from "./HomePage";
 
 type Meta = ComponentMeta<typeof Home>;
@@ -12,4 +14,12 @@ export default {
 
 const Template: Story = () => <Home />;
 
-export const StaticHomePage = Template.bind({});
+const VIEWPORTS = [
+  widthOf(INITIAL_VIEWPORTS.iphonex), // Modern iPhone
+  heightOf(INITIAL_VIEWPORTS.ipad12p), // Most common viewport size that falls within chromatic range
+];
+
+export const Default = Template.bind({});
+Default.parameters = {
+  chromatic: { viewports: VIEWPORTS },
+};
