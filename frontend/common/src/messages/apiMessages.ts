14421a80715188bc360343d4973e7c0ba5e8b956
vd1992 <40485260+vd1992@users.noreply.github.com>
2022-09-28 13:25:21 -0600

    update pool publishing messages

--- frontend/common/src/messages/apiMessages.ts
+++ frontend/common/src/messages/apiMessages.ts
@@ -41,11 +41,69 @@ export const messages: { [key: string]: MessageDescriptor } = defineMessages({
     description:
       "Error message that the pool advertisement must have a location if it is not for remote work.",
   },
-  "validator:essential_skills.required": {
-    defaultMessage: "You must have at least 1 one essential skill.",
-    id: "3GT1s/",
+
+  // pool publishing validation
+  EnglishWorkTasksRequired: {
+    defaultMessage: "English - Work Tasks is required.",
+    id: "sWfvhe",
+    description: "Error message that Work Tasks in English must be filled",
+  },
+  FrenchWorkTasksRequired: {
+    defaultMessage: "French - Work Tasks is required.",
+    id: "/75c61",
+    description: "Error message that Work Tasks in French must be filled",
+  },
+  EnglishYourImpactRequired: {
+    defaultMessage: "English - Your Impact is required.",
+    id: "sE3hl+",
+    description: "Error message that Your Impact in English must be filled",
+  },
+  FrenchYourImpactRequired: {
+    defaultMessage: "French - Your Impact is required.",
+    id: "hxs++x",
+    description: "Error message that Your Impact in French must be filled",
+  },
+  EssentialSkillRequired: {
+    defaultMessage: "You must have at least one Essential Skill.",
+    id: "Mco0Km",
+    description: "Error message that at least one Essential Skill is required",
+  },
+  AdvertisementLocationRequired: {
+    defaultMessage:
+      "You must enter Advertisement Location in both languages if advertisement is not remote.",
+    id: "m/pEFY",
     description:
-      "Error message that at least one essential skill must be selected.",
+      "Error message that advertisement locations must both be filled.",
+  },
+  "expiry date required": {
+    defaultMessage: "An Expiry Date is required.",
+    id: "+jrWEb",
+    description:
+      "Error message that the pool advertisement must have an expiry date.",
+  },
+  "stream required": {
+    defaultMessage: "A Stream selection is required.",
+    id: "KWkTXZ",
+    description:
+      "Error message that the pool advertisement must have a stream.",
+  },
+  "advertisement language required": {
+    defaultMessage: "An Advertisement Language selection is required.",
+    id: "EFH6Pt",
+    description:
+      "Error message that the pool advertisement must have an advertisement language.",
+  },
+  "security clearance required": {
+    defaultMessage: "A Security Clearance selection is required.",
+    id: "QAvKyg",
+    description:
+      "Error message that the pool advertisement must have a security clearance.",
+  },
+  "is remote required": {
+    defaultMessage: "Location must be filled in or the Remote option selected",
+    id: "3e4sM7",
+    description:
+      "Error message that the pool advertisement must have location filled.",
   },
 });
 
@@ -53,7 +111,7 @@ export const tryFindMessageDescriptor = (
   defaultMessage: string,
 ): MessageDescriptor | null => {
   const matchedKey = Object.keys(messages).find(
-    (key) => messages[key].defaultMessage === defaultMessage,
+    (key) => key === defaultMessage,
   );
 
   if (!matchedKey) return null;
