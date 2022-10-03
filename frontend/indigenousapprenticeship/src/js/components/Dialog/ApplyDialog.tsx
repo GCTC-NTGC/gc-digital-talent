9d403119d515746929f3c083d2878a12af1c65a0
Eric Sizer <eric.sizer@gmail.com>
2022-10-03 15:42:09 -0400

    replace iap dialogs

--- frontend/indigenousapprenticeship/src/js/components/Dialog/ApplyDialog.tsx
+++ frontend/indigenousapprenticeship/src/js/components/Dialog/ApplyDialog.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useIntl } from "react-intl";
 
-import { DeprecatedDialog } from "@common/components/Dialog";
+import Dialog from "@common/components/Dialog";
 import CloseButton from "./CloseButton";
 
 import type { BasicDialogProps } from "./types";
@@ -17,53 +17,59 @@ const mailAccessor = (...chunks: string[]) => (
 
 const ApplyDialog: React.FC<BasicDialogProps> = ({ isOpen, onDismiss }) => {
   const intl = useIntl();
-  const Close = React.useMemo(
-    () => <CloseButton onClick={onDismiss} />,
-    [onDismiss],
-  );
   return (
-    <DeprecatedDialog
-      centered
-      isOpen={isOpen}
-      onDismiss={onDismiss}
-      color="ia-secondary"
-      title={intl.formatMessage({
-        defaultMessage: "Apply Now",
-        id: "ce892L",
-        description: "Heading for the apply now dialog",
-      })}
+    <Dialog.Root
+      open={isOpen}
+      onOpenChange={(open: boolean) => {
+        if (!open) {
+          onDismiss();
+        }
+      }}
     >
-      <p>
-        {intl.formatMessage(
-          {
+      <Dialog.Content>
+        <Dialog.Header color="ia-secondary">
+          {intl.formatMessage({
+            defaultMessage: "Apply Now",
+            id: "ce892L",
+            description: "Heading for the apply now dialog",
+          })}
+        </Dialog.Header>
+        <p>
+          {intl.formatMessage(
+            {
+              defaultMessage:
+                "Please send your resume and cover letter explaining your passion for IT and why you're interested in joining the program to: <mailLink>edsc.pda-iap.esdc@hrsdc-rhdcc.gc.ca</mailLink>. A team member will contact you in 3-5 business days",
+              id: "EqVMCE",
+              description: "First paragraph for apply now dialog",
+            },
+            {
+              mailLink: mailAccessor,
+            },
+          )}
+        </p>
+        <p data-h2-margin="base(x1, 0, 0, 0)">
+          {intl.formatMessage({
             defaultMessage:
-              "Please send your resume and cover letter explaining your passion for IT and why you're interested in joining the program to: <mailLink>edsc.pda-iap.esdc@hrsdc-rhdcc.gc.ca</mailLink>. A team member will contact you in 3-5 business days",
-            id: "EqVMCE",
-            description: "First paragraph for apply now dialog",
-          },
-          {
-            mailLink: mailAccessor,
-          },
-        )}
-      </p>
-      <p data-h2-margin="base(x1, 0, 0, 0)">
-        {intl.formatMessage({
-          defaultMessage:
-            "We want to let you know that in the meantime, updates are being made on this site that will allow Indigenous peoples who are interested in joining the IT Apprenticeship Program to apply online.",
-          id: "BSSYnh",
-          description: "Second paragraph for apply now dialog",
-        })}
-      </p>
-      <p data-h2-margin="base(x1, 0, 0, 0)">
-        {intl.formatMessage({
-          defaultMessage:
-            "Thank you for your interest in this Program. We look forward to hearing from you!",
-          id: "/7NWbF",
-          description: "Third paragraph for apply now dialog",
-        })}
-      </p>
-      <DeprecatedDialog.Footer>{Close}</DeprecatedDialog.Footer>
-    </DeprecatedDialog>
+              "We want to let you know that in the meantime, updates are being made on this site that will allow Indigenous peoples who are interested in joining the IT Apprenticeship Program to apply online.",
+            id: "BSSYnh",
+            description: "Second paragraph for apply now dialog",
+          })}
+        </p>
+        <p data-h2-margin="base(x1, 0, 0, 0)">
+          {intl.formatMessage({
+            defaultMessage:
+              "Thank you for your interest in this Program. We look forward to hearing from you!",
+            id: "/7NWbF",
+            description: "Third paragraph for apply now dialog",
+          })}
+        </p>
+        <Dialog.Footer>
+          <Dialog.Close>
+            <CloseButton />
+          </Dialog.Close>
+        </Dialog.Footer>
+      </Dialog.Content>
+    </Dialog.Root>
   );
 };
 
--- frontend/indigenousapprenticeship/src/js/components/Dialog/RequirementDialog.tsx
+++ frontend/indigenousapprenticeship/src/js/components/Dialog/RequirementDialog.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useIntl } from "react-intl";
 
-import { DeprecatedDialog } from "@common/components/Dialog";
+import Dialog from "@common/components/Dialog";
 import CloseButton from "./CloseButton";
 
 import type { BasicDialogProps } from "./types";
@@ -11,72 +11,78 @@ const RequirementDialog: React.FC<BasicDialogProps> = ({
   onDismiss,
 }) => {
   const intl = useIntl();
-  const Close = React.useMemo(
-    () => <CloseButton onClick={onDismiss} />,
-    [onDismiss],
-  );
-
   return (
-    <DeprecatedDialog
-      isOpen={isOpen}
-      onDismiss={onDismiss}
-      color="ia-secondary"
-      title={intl.formatMessage({
-        defaultMessage: "Applicants must meet the following requirements:",
-        id: "6l9YH+",
-        description: "Heading for the applicant requirements dialog",
-      })}
+    <Dialog.Root
+      open={isOpen}
+      onOpenChange={(open: boolean) => {
+        if (!open) {
+          onDismiss();
+        }
+      }}
     >
-      <ul data-h2-padding="base(0, 0, 0, x1)">
-        <li>
+      <Dialog.Content>
+        <Dialog.Header color="ia-secondary">
           {intl.formatMessage({
-            defaultMessage:
-              "Are First Nations (status or non-status), Inuit, or Métis",
-            id: "pPOGwF",
-            description: "IAP Requirement list item one",
+            defaultMessage: "Applicants must meet the following requirements:",
+            id: "6l9YH+",
+            description: "Heading for the applicant requirements dialog",
           })}
-        </li>
-        <li>
-          {intl.formatMessage({
-            defaultMessage: "Have a high school diploma or equivalent (a GED)",
-            id: "wgIThY",
-            description: "IAP Requirement list item two",
-          })}
-        </li>
-        <li>
-          {intl.formatMessage({
-            defaultMessage: "Be a minimum of 16 years old",
-            id: "cNXORI",
-            description: "IAP Requirement list item three",
-          })}
-        </li>
-        <li>
-          {intl.formatMessage({
-            defaultMessage:
-              "Have a passion for and an interest in information technology (IT), with that passion and interest demonstrated through personal, volunteer, community, or professional life experience",
-            id: "27FjxB",
-            description: "IAP Requirement list item four",
-          })}
-        </li>
-        <li>
-          {intl.formatMessage({
-            defaultMessage:
-              "Have a commitment to learn throughout the duration of the program, both on-the-job and by following an online training program",
-            id: "/ar49+",
-            description: "IAP Requirement list item five",
-          })}
-        </li>
-        <li>
-          {intl.formatMessage({
-            defaultMessage:
-              "Individuals who are offered positions with the flexibility of working from home must have adequate internet access to support remote work.",
-            id: "+cn9MN",
-            description: "IAP Requirement list item six",
-          })}
-        </li>
-      </ul>
-      <DeprecatedDialog.Footer>{Close}</DeprecatedDialog.Footer>
-    </DeprecatedDialog>
+        </Dialog.Header>
+
+        <ul data-h2-padding="base(0, 0, 0, x1)">
+          <li>
+            {intl.formatMessage({
+              defaultMessage:
+                "Are First Nations (status or non-status), Inuit, or Métis",
+              id: "pPOGwF",
+              description: "IAP Requirement list item one",
+            })}
+          </li>
+          <li>
+            {intl.formatMessage({
+              defaultMessage:
+                "Have a high school diploma or equivalent (a GED)",
+              id: "wgIThY",
+              description: "IAP Requirement list item two",
+            })}
+          </li>
+          <li>
+            {intl.formatMessage({
+              defaultMessage: "Be a minimum of 16 years old",
+              id: "cNXORI",
+              description: "IAP Requirement list item three",
+            })}
+          </li>
+          <li>
+            {intl.formatMessage({
+              defaultMessage:
+                "Have a passion for and an interest in information technology (IT), with that passion and interest demonstrated through personal, volunteer, community, or professional life experience",
+              id: "27FjxB",
+              description: "IAP Requirement list item four",
+            })}
+          </li>
+          <li>
+            {intl.formatMessage({
+              defaultMessage:
+                "Have a commitment to learn throughout the duration of the program, both on-the-job and by following an online training program",
+              id: "/ar49+",
+              description: "IAP Requirement list item five",
+            })}
+          </li>
+          <li>
+            {intl.formatMessage({
+              defaultMessage:
+                "Individuals who are offered positions with the flexibility of working from home must have adequate internet access to support remote work.",
+              id: "+cn9MN",
+              description: "IAP Requirement list item six",
+            })}
+          </li>
+        </ul>
+        <Dialog.Footer>
+          <CloseButton />
+        </Dialog.Footer>
+      </Dialog.Content>
+    </Dialog.Root>
   );
 };
 
