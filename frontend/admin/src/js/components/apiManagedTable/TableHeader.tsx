30053e7868ae2f054815cb8631cc35951f40523c
Eric Sizer <eric.sizer@gmail.com>
2022-10-04 08:52:32 -0400

    replace api table column dialog

--- frontend/admin/src/js/components/apiManagedTable/TableHeader.tsx
+++ frontend/admin/src/js/components/apiManagedTable/TableHeader.tsx
@@ -1,9 +1,9 @@
-import React, { ReactElement, useState } from "react";
+import React, { ReactElement } from "react";
 import { useIntl } from "react-intl";
 import { Button, Link } from "@common/components";
 import { PlusIcon, TableCellsIcon } from "@heroicons/react/24/outline";
 import { SubmitHandler } from "react-hook-form";
-import { DeprecatedDialog } from "@common/components/Dialog";
+import Dialog from "@common/components/Dialog";
 import { Fieldset } from "@common/components/inputPartials";
 import SearchForm from "./SearchForm";
 import { ButtonIcon, IndeterminateCheckbox } from "../Table/tableComponents";
@@ -55,8 +55,6 @@ function TableHeader<T extends Record<string, unknown>>({
   onFilterChange,
 }: TableHeaderProps<T>): ReactElement {
   const intl = useIntl();
-
-  const [showList, setShowList] = useState(false);
   const handleFilterSubmit: SubmitHandler<FormValues> = (data) => {
     // this state lives in the UserTable component, this step also acts like a formValuesToSubmitData function
     onFilterChange({
@@ -113,83 +111,87 @@ function TableHeader<T extends Record<string, unknown>>({
                 </div>
                 <div data-h2-flex-item="base(content)">
                   <div data-h2-position="base(relative)">
-                    <Button
-                      mode="solid"
-                      color="secondary"
-                      type="button"
-                      data-h2-display="base(inline-flex)"
-                      data-h2-align-items="base(center)"
-                      onClick={() => setShowList(!showList)}
-                    >
-                      <ButtonIcon icon={TableCellsIcon} />
-                      <span>
-                        {intl.formatMessage({
-                          defaultMessage: "Columns",
-                          id: "xcBl1q",
-                          description:
-                            "Label displayed on the Table Columns toggle button.",
-                        })}
-                      </span>
-                    </Button>
-                    <DeprecatedDialog
-                      color="ts-primary"
-                      isOpen={showList}
-                      onDismiss={() => setShowList(false)}
-                      title={intl.formatMessage({
-                        defaultMessage: "Table columns",
-                        id: "YH6bFU",
-                        description:
-                          "Dialog title for the admin tables columns toggle.",
-                      })}
-                    >
-                      <Fieldset
-                        legend={intl.formatMessage({
-                          defaultMessage: "Visible columns",
-                          id: "H9rxOR",
-                          description:
-                            "Legend for the column toggle in admin tables.",
-                        })}
-                      >
-                        <div data-h2-margin="base(x.125, 0)">
-                          <IndeterminateCheckbox
-                            checked={hiddenColumnIds.length === 0}
-                            indeterminate={
-                              hiddenColumnIds.length > 0 &&
-                              hiddenColumnIds.length < columns.length
-                            }
-                            onChange={() => {
-                              if (onColumnHiddenChange) {
-                                onColumnHiddenChange({
-                                  setHidden: hiddenColumnIds.length === 0,
-                                });
+                    <Dialog.Root>
+                      <Dialog.Trigger>
+                        <Button
+                          mode="solid"
+                          color="secondary"
+                          type="button"
+                          data-h2-display="base(inline-flex)"
+                          data-h2-align-items="base(center)"
+                        >
+                          <ButtonIcon icon={TableCellsIcon} />
+                          <span>
+                            {intl.formatMessage({
+                              defaultMessage: "Columns",
+                              id: "xcBl1q",
+                              description:
+                                "Label displayed on the Table Columns toggle button.",
+                            })}
+                          </span>
+                        </Button>
+                      </Dialog.Trigger>
+                      <Dialog.Content>
+                        <Dialog.Header color="ts-primary">
+                          {intl.formatMessage({
+                            defaultMessage: "Table columns",
+                            id: "YH6bFU",
+                            description:
+                              "Dialog title for the admin tables columns toggle.",
+                          })}
+                        </Dialog.Header>
+                        <Fieldset
+                          legend={intl.formatMessage({
+                            defaultMessage: "Visible columns",
+                            id: "H9rxOR",
+                            description:
+                              "Legend for the column toggle in admin tables.",
+                          })}
+                        >
+                          <div data-h2-margin="base(x.125, 0)">
+                            <IndeterminateCheckbox
+                              checked={hiddenColumnIds.length === 0}
+                              indeterminate={
+                                hiddenColumnIds.length > 0 &&
+                                hiddenColumnIds.length < columns.length
                               }
-                            }}
-                          />
-                        </div>
-                        {columns.map((column) => (
-                          <div key={column.id} data-h2-margin="base(x.125, 0)">
-                            <label htmlFor={column.id}>
-                              <input
-                                id={column.id}
-                                type="checkbox"
-                                checked={!hiddenColumnIds.includes(column.id)}
-                                onChange={() => {
-                                  if (onColumnHiddenChange) {
-                                    onColumnHiddenChange({
-                                      columnId: column.id,
-                                      setHidden: !hiddenColumnIds.includes(
-                                        column.id,
-                                      ),
-                                    });
-                                  }
-                                }}
-                              />{" "}
-                              {column.label}
-                            </label>
+                              onChange={() => {
+                                if (onColumnHiddenChange) {
+                                  onColumnHiddenChange({
+                                    setHidden: hiddenColumnIds.length === 0,
+                                  });
+                                }
+                              }}
+                            />
                           </div>
-                        ))}
-                      </Fieldset>
-                    </DeprecatedDialog>
+                          {columns.map((column) => (
+                            <div
+                              key={column.id}
+                              data-h2-margin="base(x.125, 0)"
+                            >
+                              <label htmlFor={column.id}>
+                                <input
+                                  id={column.id}
+                                  type="checkbox"
+                                  checked={!hiddenColumnIds.includes(column.id)}
+                                  onChange={() => {
+                                    if (onColumnHiddenChange) {
+                                      onColumnHiddenChange({
+                                        columnId: column.id,
+                                        setHidden: !hiddenColumnIds.includes(
+                                          column.id,
+                                        ),
+                                      });
+                                    }
+                                  }}
+                                />{" "}
+                                {column.label}
+                              </label>
+                            </div>
+                          ))}
+                        </Fieldset>
+                      </Dialog.Content>
+                    </Dialog.Root>
                   </div>
                 </div>
               </div>
