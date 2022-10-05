6f8487f4af1f2d9cf4e354c425d8e288c312b33c
tristan-orourke <tristan.orourke@gmail.com>
2022-10-05 10:38:09 -0400

    Create a `@self` directive to help organize schema

---
+++ api/app/GraphQL/Directives/SelfDirective.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\GraphQL\Directives;
+
+use Nuwave\Lighthouse\Schema\Directives\BaseDirective;
+use Nuwave\Lighthouse\Schema\Values\FieldValue;
+use Nuwave\Lighthouse\Support\Contracts\FieldResolver;
+
+final class SelfDirective extends BaseDirective implements FieldResolver
+{
+    // TODO implement the directive https://lighthouse-php.com/master/custom-directives/getting-started.html
+
+    public static function definition(): string
+    {
+        return /** @lang GraphQL */ <<<'GRAPHQL'
+"""
+The field will be resolved to the same value as its parent.
+For example, useful in the following situation:
+type User {
+    profile: Applicant @self
+}
+"""
+directive @self on FIELD_DEFINITION
+GRAPHQL;
+    }
+
+    /**
+     * Set a field resolver on the FieldValue.
+     *
+     * This must call $fieldValue->setResolver() before returning
+     * the FieldValue.
+     *
+     * @param  \Nuwave\Lighthouse\Schema\Values\FieldValue  $fieldValue
+     * @return \Nuwave\Lighthouse\Schema\Values\FieldValue
+     */
+    public function resolveField(FieldValue $fieldValue)
+    {
+        $fieldValue->setResolver(function ($root, array $args) {
+            return $root;
+        });
+
+        return $fieldValue;
+    }
+}
