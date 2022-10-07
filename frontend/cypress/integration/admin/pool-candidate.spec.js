7638763820e5e6d6de3827a840b52cea3e283255
tristan-orourke <tristan.orourke@gmail.com>
2022-10-07 12:52:56 -0400

    Branch cypress test on feature flag variable

--- frontend/cypress/integration/admin/pool-candidate.spec.js
+++ frontend/cypress/integration/admin/pool-candidate.spec.js
@@ -10,7 +10,7 @@ describe("Pool Candidates", () => {
       .and("be.visible");
 
     cy.url().should("contain", "/admin/pools");
-  }
+  };
 
   beforeEach(() => {
     cy.intercept("POST", "/graphql", (req) => {
@@ -20,47 +20,77 @@ describe("Pool Candidates", () => {
       aliasQuery(req, "GetPoolCandidatesPaginated");
 
       aliasMutation(req, "UpdatePoolCandidateStatus");
+
+      // Only used if FEATURE_APPLICANTSEARCH is false
+      aliasQuery(req, "getUpdatePoolCandidateData");
+      aliasMutation(req, "updatePoolCandidate");
     });
 
     loginAndGoToPoolsPage();
   });
 
-  it("should update pool candidate status", () => {
-    cy.wait("@gqlgetPoolsQuery");
+  if (Cypress.env("FEATURE_APPLICANTSEARCH")) {
+    it("should update pool candidate status", () => {
+      cy.wait("@gqlgetPoolsQuery");
 
-    cy.findAllByRole("link", { name: /view candidates/i })
-      .eq(0)
-      .click();
+      cy.findAllByRole("link", { name: /view candidates/i })
+        .eq(0)
+        .click();
 
-    cy.wait("@gqlGetPoolCandidatesPaginatedQuery");
+      cy.wait("@gqlGetPoolCandidatesPaginatedQuery");
 
-    cy.findAllByRole("link", { name: /view application/i })
-      .eq(0)
-      .click();
+      cy.findAllByRole("link", { name: /view application/i })
+        .eq(0)
+        .click();
 
-    cy.wait("@gqlgetPoolCandidateSnapshotQuery");
-    cy.wait("@gqlGetPoolCandidateStatusQuery");
+      cy.wait("@gqlgetPoolCandidateSnapshotQuery");
+      cy.wait("@gqlGetPoolCandidateStatusQuery");
 
-    cy.findByRole("combobox", { name: /candidate pool status/i })
-      .select("Screened In")
-      .within(() => {
-        cy.get("option:selected")
-          .should("have.text", "Screened In");
-      });
+      cy.findByRole("combobox", { name: /candidate pool status/i })
+        .select("Screened In")
+        .within(() => {
+          cy.get("option:selected").should("have.text", "Screened In");
+        });
 
-    cy.findByLabelText(/Candidate expiry date/i)
-      .type("2023-12-01");
+      cy.findByLabelText(/Candidate expiry date/i).type("2023-12-01");
 
-    cy.findByRole("textbox", { name: /notes/i })
-      .clear()
-      .type("New Notes")
+      cy.findByRole("textbox", { name: /notes/i }).clear().type("New Notes");
 
-    cy.findByRole("button", { name: /save changes/i })
-      .click();
+      cy.findByRole("button", { name: /save changes/i }).click();
 
-    cy.wait("@gqlUpdatePoolCandidateStatusMutation")
+      cy.wait("@gqlUpdatePoolCandidateStatusMutation");
 
-    cy.expectToast(/pool candidate status updated successfully/i);
+      cy.expectToast(/pool candidate status updated successfully/i);
+    });
+  } else {
+    it("should edit and update pool candidate", () => {
+      cy.wait("@gqlgetPoolsQuery");
 
-  });
+      cy.findAllByRole("link", { name: /view candidates/i })
+        .eq(0)
+        .click();
+
+      cy.wait("@gqlGetPoolCandidatesPaginatedQuery");
+
+      cy.findAllByRole("link", { name: /view edit/i })
+        .eq(0)
+        .click();
+
+      cy.wait("@gqlgetUpdatePoolCandidateDataQuery");
+
+      cy.findByRole("heading", { name: /Update Pool Candidate/i })
+        .should("exist")
+        .and("be.visible");
+
+      cy.findByLabelText(/expiry date/i).type("2023-12-01");
+
+      cy.findByLabelText(/status/i).select("Placed Casual");
+
+      cy.findByRole("button", { name: /submit/i }).click();
+
+      cy.wait("@gqlupdatePoolCandidateMutation");
+
+      cy.expectToast(/pool candidate updated successfully/i);
+    });
+  }
 });
