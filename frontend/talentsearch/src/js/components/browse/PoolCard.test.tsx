330131b645744f1379a906f4d769bc5230f758d9
vd1992 <40485260+vd1992@users.noreply.github.com>
2022-10-11 09:37:00 -0600

    jesting null state

--- frontend/talentsearch/src/js/components/browse/PoolCard.test.tsx
+++ frontend/talentsearch/src/js/components/browse/PoolCard.test.tsx
@@ -9,6 +9,11 @@ import { fakePoolAdvertisements } from "@common/fakeData";
 import PoolCard, { CardProps } from "./PoolCard";
 
 const fakedPool = fakePoolAdvertisements()[0];
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const nullPool: any = {};
+Object.keys(fakedPool).forEach((key) => {
+  nullPool[key] = null;
+});
 
 const renderPoolCard = (props: CardProps) => render(<PoolCard {...props} />);
 
@@ -40,5 +45,19 @@ describe("PoolCard", () => {
       "href",
       `/en/browse/pools/${fakedPool.id}`,
     );
+  });
+
+  it("should render the null state correctly", async () => {
+    renderPoolCard({ pool: nullPool });
+
+    expect(screen.getByText(/(No Classification)/i)).toBeInTheDocument();
+    expect(screen.getByText(/(Needs classification)/i)).toBeInTheDocument();
+    expect(screen.getByText(/(No skills required)/i)).toBeInTheDocument();
+    expect(screen.getByText(/(To be determined)/i)).toBeInTheDocument();
+
+    const applyLink = screen.getByRole("link", {
+      name: /apply to this recruitment/i,
+    });
+    expect(applyLink).toHaveAttribute("href", `#`);
   });
 });
