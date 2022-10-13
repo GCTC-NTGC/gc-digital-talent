4181c320b7291fe947203ddc300dda42f73daf96
Eric Sizer <eric.sizer@gmail.com>
2022-10-13 15:07:55 -0400

    wrap chips with button when dismissible

--- frontend/common/src/components/Chip/Chip.tsx
+++ frontend/common/src/components/Chip/Chip.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { XCircleIcon } from "@heroicons/react/24/outline";
+import { useIntl } from "react-intl";
 import Pill from "../Pill";
+
+import "./chip.css";
 
 export interface ChipProps extends React.HTMLProps<HTMLElement> {
   /** The style type of the element. */
@@ -13,8 +16,10 @@ export interface ChipProps extends React.HTMLProps<HTMLElement> {
   label: string;
 }
 
+type ChipColor = "primary" | "secondary" | "neutral";
+
 const colorMap: Record<
-  "primary" | "secondary" | "neutral",
+  ChipColor,
   Record<"solid" | "outline", Record<string, string>>
 > = {
   primary: {
@@ -43,32 +48,63 @@ const colorMap: Record<
   },
 };
 
+const ChipDismiss = (props: React.ComponentProps<"button">) => (
+  <button
+    type="button"
+    data-h2-background-color="base(transparent)"
+    data-h2-border="base(none)"
+    data-h2-cursor="base(pointer)"
+    data-h2-padding="base(0)"
+    data-h2-radius="base(m)"
+    data-h2-shadow="base(none) base:hover(s)"
+    {...props}
+  />
+);
+
 const Chip: React.FC<ChipProps> = ({
   color,
   mode,
   onDismiss,
   label,
 }): React.ReactElement => {
+  const intl = useIntl();
+  const wrapperProps = onDismiss
+    ? {
+        className: `Chip__Dismiss Chip__Dismiss--${color}`,
+        onClick: onDismiss,
+        "aria-label": intl.formatMessage(
+          {
+            defaultMessage: "Remove {label}",
+            id: "rsxrMs",
+            description: "Button text for a dismissible chip",
+          },
+          {
+            label,
+          },
+        ),
+      }
+    : {};
+
+  const Wrapper = onDismiss ? ChipDismiss : React.Fragment;
+
   return (
-    <Pill
-      color={color}
-      mode={mode}
-      role="listitem"
-      data-h2-padding="base(x.25, x.5)"
-    >
-      {label}
-      {onDismiss && (
-        <XCircleIcon
-          data-h2-cursor="base(pointer)"
-          data-h2-width="base(1rem)"
-          data-h2-margin="base(0, 0, 0, x.25)"
-          data-h2-vertical-align="base(middle)"
-          {...colorMap[color][mode]}
-          role="button"
-          onClick={onDismiss}
-        />
-      )}
-    </Pill>
+    <Wrapper {...wrapperProps}>
+      <Pill
+        color={color}
+        mode={mode}
+        role="listitem"
+        data-h2-padding="base(x.25, x.5)"
+      >
+        {label}
+        {onDismiss && (
+          <XCircleIcon
+            data-h2-width="base(1rem)"
+            data-h2-margin="base(0, 0, 0, x.25)"
+            data-h2-vertical-align="base(middle)"
+          />
+        )}
+      </Pill>
+    </Wrapper>
   );
 };
 
