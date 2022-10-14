47332f800def6d69bd535487dfe769ca0ddc8e9f
Eric Sizer <eric.sizer@gmail.com>
2022-10-14 12:38:16 -0400

    update breadcrumb separator

--- frontend/common/src/components/Breadcrumbs/v2/Crumb.tsx
+++ frontend/common/src/components/Breadcrumbs/v2/Crumb.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { ChevronRightIcon } from "@heroicons/react/24/solid";
 
 interface CrumbProps {
   children: React.ReactNode;
@@ -23,10 +24,13 @@ const Crumb = ({ children, isCurrent, url }: CrumbProps) => (
     {!isCurrent && (
       <span
         aria-hidden="true"
+        data-h2-height="base(x1)"
+        data-h2-width="base(x1)"
         data-h2-display="base(inline-block)"
         data-h2-margin="base(0, 0, 0, x.5)"
+        data-h2-vertical-align="base(middle)"
       >
-        /
+        <ChevronRightIcon />
       </span>
     )}
   </li>
