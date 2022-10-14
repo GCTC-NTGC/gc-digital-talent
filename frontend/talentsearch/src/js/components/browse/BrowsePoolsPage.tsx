764c7e0de0ddac97903159feda3f264606fe5421
Eric Sizer <eric.sizer@gmail.com>
2022-10-14 12:32:58 -0400

    add in other opportunities card

--- frontend/talentsearch/src/js/components/browse/BrowsePoolsPage.tsx
+++ frontend/talentsearch/src/js/components/browse/BrowsePoolsPage.tsx
@@ -5,10 +5,13 @@ import { useIntl } from "react-intl";
 import Hero from "@common/components/Hero";
 import Pending from "@common/components/Pending";
 import NotFound from "@common/components/NotFound";
+import Flourish from "@common/components/Flourish";
 
 import { commonMessages } from "@common/messages";
 import { imageUrl } from "@common/helpers/router";
 import Heading from "@common/components/Heading";
+import useTheme from "@common/hooks/useTheme";
+import { Link } from "@common/components";
 import {
   AdvertisementStatus,
   PublishingGroup,
@@ -19,6 +22,25 @@ import PoolCard from "./PoolCard/PoolCard";
 import TALENTSEARCH_APP_DIR from "../../talentSearchConstants";
 import useBreadcrumbs from "../../hooks/useBreadcrumbs";
 import { useDirectIntakeRoutes } from "../../directIntakeRoutes";
+import Block from "../Home/partials/Opportunities/Block";
+import { useApplicantProfileRoutes } from "../../applicantProfileRoutes";
+
+const flourishTopLight = imageUrl(TALENTSEARCH_APP_DIR, "browse_top_light.png");
+const flourishBottomLight = imageUrl(
+  TALENTSEARCH_APP_DIR,
+  "browse_bottom_light.png",
+);
+const flourishTopDark = imageUrl(TALENTSEARCH_APP_DIR, "browse_top_dark.png");
+const flourishBottomDark = imageUrl(
+  TALENTSEARCH_APP_DIR,
+  "browse_bottom_dark.png",
+);
+
+const getFlourishStyles = (isTop: boolean) => ({
+  "data-h2-position": "base(absolute)",
+  "data-h2-width": "base(25vw)",
+  "data-h2-offset": isTop ? "base(0, 0, auto, auto)" : "base(auto, auto, 0, 0)",
+});
 
 export interface BrowsePoolsProps {
   poolAdvertisements: PoolAdvertisement[];
@@ -27,8 +49,10 @@ export interface BrowsePoolsProps {
 export const BrowsePools: React.FC<BrowsePoolsProps> = ({
   poolAdvertisements,
 }) => {
+  const { mode } = useTheme();
   const intl = useIntl();
   const paths = useDirectIntakeRoutes();
+  const profilePaths = useApplicantProfileRoutes();
 
   const title = intl.formatMessage({
     defaultMessage: "Browse IT jobs",
@@ -74,9 +98,15 @@ export const BrowsePools: React.FC<BrowsePoolsProps> = ({
           data-h2-border="base(bottom, 1px, solid, black.50)"
           data-h2-position="base(relative)"
         >
+          <img
+            alt=""
+            src={mode === "dark" ? flourishTopDark : flourishTopLight}
+            {...getFlourishStyles(true)}
+          />
           <div
             data-h2-position="base(relative)"
             data-h2-container="base(center, large, x1) p-tablet(center, large, x2)"
+            style={{ zIndex: 1 }}
           >
             <div data-h2-padding="base(x3, 0) p-tablet(x4, 0)">
               <Heading
@@ -110,19 +140,79 @@ export const BrowsePools: React.FC<BrowsePoolsProps> = ({
                     "Description of how the application process works, paragraph two",
                 })}
               </p>
-              <ul
-                data-h2-list-style="base(none)"
-                data-h2-margin="base(0)"
-                data-h2-padding="base(x2, 0, 0, 0) p-tablet(x3, 0, 0, 0)"
-              >
-                {filteredPoolAdvertisements.map((poolAdvertisement) => (
-                  <li key={poolAdvertisement.id}>
-                    <PoolCard pool={poolAdvertisement} />
-                  </li>
-                ))}
-              </ul>
+              <div data-h2-padding="base(x2, 0, 0, 0) p-tablet(x3, 0, 0, 0)">
+                <ul
+                  data-h2-margin="base(0)"
+                  data-h2-padding="base(0)"
+                  data-h2-list-style="base(none)"
+                >
+                  {filteredPoolAdvertisements.map((poolAdvertisement) => (
+                    <li key={poolAdvertisement.id}>
+                      <PoolCard pool={poolAdvertisement} />
+                    </li>
+                  ))}
+                </ul>
+                <div
+                  data-h2-background-color="base(white) base:dark(black.light)"
+                  data-h2-shadow="base(large)"
+                  data-h2-padding="base(x1) p-tablet(x2)"
+                  data-h2-radius="base(rounded)"
+                >
+                  <div
+                    data-h2-display="p-tablet(flex)"
+                    data-h2-gap="base(x3)"
+                    data-h2-align-items="base(center)"
+                  >
+                    <div>
+                      <Heading
+                        level="h3"
+                        size="h6"
+                        data-h2-margin="base(0, 0, x1, 0)"
+                      >
+                        {intl.formatMessage({
+                          defaultMessage: "More opportunities are coming soon!",
+                          id: "g+JcDC",
+                          description:
+                            "Heading for message about upcoming opportunities",
+                        })}
+                      </Heading>
+                      <p>
+                        {intl.formatMessage({
+                          defaultMessage:
+                            "We're posting new opportunities all the time. By starting your profile now, you'll be able to submit applications lightning fast when the time comes.",
+                          id: "sehm2K",
+                          description:
+                            "Text describing upcoming opportunities instructing users to create a profile",
+                        })}
+                      </p>
+                    </div>
+                    <div data-h2-margin="base(x1, 0, 0, 0) p-tablet(0)">
+                      <Link
+                        color="blue"
+                        mode="outline"
+                        type="button"
+                        weight="bold"
+                        href={profilePaths.myProfile()}
+                        style={{ whiteSpace: "nowrap" }}
+                      >
+                        {intl.formatMessage({
+                          defaultMessage: "Create a profile",
+                          id: "obSyWw",
+                          description:
+                            "Link text to direct users to the profile page",
+                        })}
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
+          <img
+            alt=""
+            src={mode === "dark" ? flourishBottomDark : flourishBottomLight}
+            {...getFlourishStyles(false)}
+          />
         </div>
       ) : (
         <NotFound headingMessage={intl.formatMessage(commonMessages.notFound)}>
@@ -136,6 +226,81 @@ export const BrowsePools: React.FC<BrowsePoolsProps> = ({
           </p>
         </NotFound>
       )}
+      <div
+        data-h2-background-color="base:dark(black.light)"
+        data-h2-padding="base(x3, 0) p-tablet(x4, 0) l-tablet(x6, 0)"
+      >
+        <div
+          data-h2-position="base(relative)"
+          data-h2-container="base(center, large, x1) p-tablet(center, large, x2)"
+        >
+          <div
+            data-h2-display="base(grid)"
+            data-h2-grid-template-columns="base(1fr) p-tablet(repeat(2, minmax(0, 1fr)))"
+            data-h2-gap="base(x2) p-tablet(x3)"
+          >
+            <Block
+              content={{
+                color: "purple",
+                title: intl.formatMessage({
+                  defaultMessage:
+                    "Browse IT opportunities for the Indigenous community",
+                  id: "GZrICV",
+                  description:
+                    "Title for Indigenous community job opportunities on Browse IT jobs page",
+                }),
+                summary: intl.formatMessage({
+                  defaultMessage:
+                    "Designed by, with, and for the Indigenous community, the program recruits First Nations, Inuit, and Métis applicants who have a passion for IT, for entry level employment, learning and development opportunities.",
+                  id: "+6QgII",
+                  description:
+                    "Summary for Indigenous community job opportunities on Browse IT jobs page",
+                }),
+                link: {
+                  path: `${paths.home()}/indigenous-apprentice`,
+                  mode: "outline",
+                  label: intl.formatMessage({
+                    defaultMessage:
+                      "<hidden>Learn more about </hidden>Indigenous IT apprenticeship<hidden> opportunities</hidden>",
+                    id: "xgocDD",
+                    description:
+                      "Link text to go to IAP homepage on browse IT jobs page",
+                  }),
+                },
+              }}
+            />
+            <Block
+              content={{
+                color: "purple",
+                title: intl.formatMessage({
+                  defaultMessage: "Hire talent for your team",
+                  id: "jTN0bg",
+                  description:
+                    "Title for to go to the search page on Browse IT jobs page",
+                }),
+                summary: intl.formatMessage({
+                  defaultMessage:
+                    "Let our team save you time and energy by matching your needs to pre-qualified IT professionals with the right skills for the job. All the talent in our pools has been qualified through a competitive process, so you can jump straight to the interview and decide if they are a good fit for your team.",
+                  id: "Ms6O4W",
+                  description:
+                    "Summary for to go to the search page on Browse IT jobs page",
+                }),
+                link: {
+                  path: `${paths.home()}/indigenous-apprentice`,
+                  mode: "outline",
+                  label: intl.formatMessage({
+                    defaultMessage: "Visit the talent search page",
+                    id: "BhfG7a",
+                    description:
+                      "Link text to go to the search page on browse IT jobs page",
+                  }),
+                },
+              }}
+            />
+          </div>
+        </div>
+      </div>
+      <Flourish />
     </>
   );
 };
