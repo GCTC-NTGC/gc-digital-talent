986244ca5b26ce827af5c9ed0f83b782f2ba4063
Matt Nigh <matt@mattnigh.com>
2022-10-17 13:36:41 -0400

    Add support route group to api

--- api/routes/api.php
+++ api/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SupportController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,3 +14,7 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::prefix('support')->controller(SupportController::class)->group(function () {
+    Route::post('/tickets', 'createTicket');
+});
+
