bca2cc23ec3c639fd14f00352f224c5d9f4b5b23
Eric Sizer <eric.sizer@gmail.com>
2022-10-19 14:17:59 -0400

    fix alert and fieldset stories

--- frontend/common/src/components/Alert/Alert.stories.tsx
+++ frontend/common/src/components/Alert/Alert.stories.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { BellIcon } from "@heroicons/react/24/outline";
 import { Meta, Story } from "@storybook/react";
-import Alert, { AlertProps } from "./Alert";
+import Alert, { AlertProps, Color } from "./Alert";
+
+const types: Array<Color> = ["success", "warning"];
 
 export default {
   component: Alert,
@@ -10,16 +12,23 @@ export default {
     title: "Alert",
     children:
       "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum eligendi dolore vel optio! Amet non adipisci blanditiis accusantium? Laborum nobis facilis vel dolore numquam libero velit aspernatur, ut consectetur neque.",
-    icon: <BellIcon style={{ width: "1.4rem" }} />,
+    icon: BellIcon,
   },
 } as Meta;
 
 const TemplateAlert: Story<AlertProps> = (args) => {
-  return <Alert {...args} />;
+  return (
+    <div data-h2-display="base(flex)" data-h2-flex-direction="base(column)">
+      {types.map((type) => (
+        <Alert
+          key={type}
+          {...args}
+          type={type}
+          title={type.charAt(0).toUpperCase() + type.slice(1)}
+        />
+      ))}
+    </div>
+  );
 };
 
-export const Success = TemplateAlert.bind({});
-
-Success.args = {
-  type: "success",
-};
+export const Default = TemplateAlert.bind({});
