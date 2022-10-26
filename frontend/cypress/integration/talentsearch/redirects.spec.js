0191051a7b798452c7c71384627d04798b2edbd6
Eric Sizer <eric.sizer@gmail.com>
2022-10-26 13:04:13 -0400

    use match instead of contains for regex

--- frontend/cypress/integration/talentsearch/redirects.spec.js
+++ frontend/cypress/integration/talentsearch/redirects.spec.js
@@ -2,33 +2,30 @@ import { aliasQuery } from "../../support/graphql-test-utils";
 
 describe("Redirects", () => {
 
+
+
   const expectToBeOnProfile = () => {
     cy.wait("@gqlgetMeQuery");
     cy.url()
-    .should('contain', /en\/users\/[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}\/profile/i);
+    .should('match', /users\/[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}\/profile$/g);
   }
 
   beforeEach(() => {
-
     cy.intercept("POST", "/graphql", (req) => {
       aliasQuery(req, "getMe");
     });
-
-
     cy.loginByRole('applicant')
   });
 
   it("redirects /talent/profile", () => {
     cy.visit("/en/talent/profile");
     cy.wait("@gqlgetMeQuery");
-
     expectToBeOnProfile();
   });
 
   it("redirects /users/me", () => {
     cy.visit("/en/users/me");
     cy.wait("@gqlgetMeQuery");
-
     expectToBeOnProfile();
   });
 });
