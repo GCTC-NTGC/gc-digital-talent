1bd8aed6a5a3f93f5988c742c6361b15f42b07f2
Eric Sizer <eric.sizer@gmail.com>
2022-10-27 15:49:39 -0400

    move uuid regex to variable for more clarity

--- frontend/cypress/integration/talentsearch/redirects.spec.js
+++ frontend/cypress/integration/talentsearch/redirects.spec.js
@@ -1,13 +1,12 @@
 import { aliasQuery } from "../../support/graphql-test-utils";
 
+const uuidRegEx = /[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}/;
+
 describe("Redirects", () => {
-
-
-
   const expectToBeOnProfile = () => {
     cy.wait("@gqlgetMeQuery");
     cy.url()
-    .should('match', /users\/[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}\/profile$/g);
+    .should('match', new RegExp("users" + uuidRegEx + "profile", "gi"));
   }
 
   beforeEach(() => {
