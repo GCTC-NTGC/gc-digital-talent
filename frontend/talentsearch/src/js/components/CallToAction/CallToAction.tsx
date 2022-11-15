59e822f027fe30ad0409780f047f9932ec8524a9
Eric Sizer <eric.sizer@gmail.com>
2022-11-15 11:43:49 -0500

    fix flex alignment of homepage call to action

--- frontend/talentsearch/src/js/components/CallToAction/CallToAction.tsx
+++ frontend/talentsearch/src/js/components/CallToAction/CallToAction.tsx
@@ -84,7 +84,7 @@ const CallToAction: React.FC<CallToActionProps> = ({
         >
           <div
             data-h2-display="base(flex)"
-            data-h2-height="base(100%)"
+            data-h2-align-self="base(stretch)"
             data-h2-padding="base(x.5) p-tablet(x.25, x.4)"
             data-h2-radius="base(rounded, 0, 0, rounded)"
             data-h2-width="base:children[svg](var(--h2-font-size-h5))"
