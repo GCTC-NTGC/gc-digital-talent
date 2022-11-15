22c16d304553f193772d3a723cca7600f8beb39f
Eric Sizer <eric.sizer@gmail.com>
2022-11-15 10:48:02 -0500

    update accordion icon class name

--- frontend/admin/src/js/components/poolCandidate/PoolCandidateDocument.tsx
+++ frontend/admin/src/js/components/poolCandidate/PoolCandidateDocument.tsx
@@ -113,7 +113,7 @@ export const PoolCandidateDocument = React.forwardRef<
         .accordion-header-context .icon {
           width: 1.5rem;
         }
-        .accordion-header-wrapper svg {
+        .Accordion__Chevron {
           display: none;
         }
       }
