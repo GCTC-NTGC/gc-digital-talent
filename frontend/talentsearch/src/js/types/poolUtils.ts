046b1d79c2cd02d3257c4f4cf0f3556bc49e4cd6
tristan-orourke <tristan.orourke@gmail.com>
2022-11-21 16:00:32 -0500

    Filter pools by selected stream, just like classification

--- frontend/talentsearch/src/js/types/poolUtils.ts
+++ frontend/talentsearch/src/js/types/poolUtils.ts
@@ -1,6 +1,11 @@
-import { Classification, Maybe } from "@common/api/generated";
-import { notEmpty } from "@common/helpers/util";
-import { Pool } from "../api/generated";
+import {
+  Pool,
+  Classification,
+  Maybe,
+  UserPublicProfile,
+  Skill,
+  SkillFamily,
+} from "../api/generated";
 
 export type SimpleClassification = Pick<Classification, "group" | "level">;
 
@@ -8,16 +13,15 @@ export type SimplePool = Pick<Pool, "id" | "owner" | "description" | "name"> & {
   classifications?: SimpleClassification[];
 };
 
-export const filterPoolsBySelectedClassification = (
-  allPools: SimplePool[],
-  classification: Maybe<SimpleClassification>,
-) =>
-  allPools
-    ?.filter((pool) => {
-      return pool?.classifications?.some(
-        (x) =>
-          x?.group === classification?.group &&
-          x?.level === classification?.level,
-      );
-    })
-    .filter(notEmpty);
+export const poolMatchesClassification = (
+  pool: SimplePool,
+  classification: SimpleClassification,
+): boolean => {
+  return (
+    pool.classifications?.some(
+      (poolClassification) =>
+        poolClassification?.group === classification?.group &&
+        poolClassification?.level === classification?.level,
+    ) ?? false
+  );
+};
