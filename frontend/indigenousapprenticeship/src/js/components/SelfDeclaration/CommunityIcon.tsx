c84ac8507b444860cf2d3f2dc85330d6bba7ddd4
Eric Sizer <eric.sizer@gmail.com>
2022-11-22 12:58:10 -0500

    add fade animation to icons

--- frontend/indigenousapprenticeship/src/js/components/SelfDeclaration/CommunityIcon.tsx
+++ frontend/indigenousapprenticeship/src/js/components/SelfDeclaration/CommunityIcon.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { AnimatePresence, motion } from "framer-motion";
 
 import imageUrl from "@common/helpers/imageUrl";
 import INDIGENOUSAPPRENTICESHIP_APP_DIR from "../../constants/indigenousApprenticeshipConstants";
@@ -9,17 +10,50 @@ interface CommunityIconProps {
 }
 
 const CommunityIcon = ({ community, on }: CommunityIconProps) => {
+  const styles = {
+    "data-h2-location": "base(0)",
+    "data-h2-position": "base(absolute)",
+    "data-h2-width": "base(100%)",
+    "data-h2-height": "base(100%)",
+    // Fade animation
+    initial: { opacity: 0 },
+    exit: { opacity: 0 },
+    animate: { opacity: 1 },
+  };
+
   return (
-    <img
-      data-h2-padding="p-tablet(0, x1)"
-      data-h2-width="base(100%)"
-      data-h2-height="base(auto)"
-      alt=""
-      src={imageUrl(
-        INDIGENOUSAPPRENTICESHIP_APP_DIR,
-        `${community}-${!!on}.png`,
-      )}
-    />
+    <div data-h2-padding="p-tablet(0, x1)">
+      <div
+        data-h2-position="base(relative)"
+        data-h2-width="base(100%)"
+        data-h2-height="base(0)"
+        data-h2-padding="base(0, 0, 100%, 0)"
+      >
+        <AnimatePresence>
+          {on ? (
+            <motion.img
+              {...styles}
+              alt=""
+              key={`${community}-true`}
+              src={imageUrl(
+                INDIGENOUSAPPRENTICESHIP_APP_DIR,
+                `${community}-true.png`,
+              )}
+            />
+          ) : (
+            <motion.img
+              {...styles}
+              alt=""
+              key={`${community}-false`}
+              src={imageUrl(
+                INDIGENOUSAPPRENTICESHIP_APP_DIR,
+                `${community}-false.png`,
+              )}
+            />
+          )}
+        </AnimatePresence>
+      </div>
+    </div>
   );
 };
 
