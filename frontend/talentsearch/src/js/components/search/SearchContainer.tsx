79728bc98c4d8c4af94cf72c2e5371cb3eee8b4b
vd1992 <40485260+vd1992@users.noreply.github.com>
2022-11-25 13:54:52 -0700

    eric's suggestion

--- frontend/talentsearch/src/js/components/search/SearchContainer.tsx
+++ frontend/talentsearch/src/js/components/search/SearchContainer.tsx
@@ -70,6 +70,82 @@ const applicantFilterToQueryArgs = (
   return {};
 };
 
+function a(chunks: React.ReactNode): React.ReactNode {
+  return (
+    <a
+      href={`mailto:${TALENTSEARCH_RECRUITMENT_EMAIL}`}
+      data-h2-font-weight="base(700)"
+    >
+      {chunks}
+    </a>
+  );
+}
+
+interface ResultsDisplayProps {
+  pending: boolean;
+  results?: Array<CandidateSearchPoolResult>;
+  handleSubmit: (
+    candidateCount: number,
+    poolId: string,
+    selectedClassifications: SimpleClassification[],
+  ) => Promise<void>;
+}
+
+const ResultsDisplay = ({
+  pending,
+  results,
+  handleSubmit,
+}: ResultsDisplayProps) => {
+  const intl = useIntl();
+
+  if (pending) {
+    return <Spinner />;
+  }
+
+  return results && results.length ? (
+    <div>
+      {results.map(({ pool, candidateCount }) => (
+        <CandidateResults
+          key={pool.id}
+          candidateCount={candidateCount}
+          pool={pool}
+          handleSubmit={handleSubmit}
+        />
+      ))}
+    </div>
+  ) : (
+    <div
+      data-h2-shadow="base(m)"
+      data-h2-margin="base(x.5, 0, 0, 0)"
+      data-h2-padding="base(x1)"
+      data-h2-border="base(left, x1, solid, dt-gray.dark)"
+    >
+      <p>
+        {intl.formatMessage({
+          defaultMessage: "We can still help!",
+          id: "5U+V2Y",
+          description:
+            "Heading for helping user if no candidates matched the filters chosen.",
+        })}
+      </p>
+      <p data-h2-margin="base(x.5, 0, 0, 0)">
+        {intl.formatMessage(
+          {
+            defaultMessage:
+              "If there are no matching candidates <a>Get in touch!</a>",
+            id: "+ZXZj+",
+            description:
+              "Message for helping user if no candidates matched the filters chosen.",
+          },
+          {
+            a,
+          },
+        )}
+      </p>
+    </div>
+  );
+};
+
 export interface SearchContainerProps {
   applicantFilter?: ApplicantFilterInput;
   classifications: SimpleClassification[];
@@ -89,17 +165,6 @@ export interface SearchContainerProps {
 const testId = (chunks: React.ReactNode): React.ReactNode => (
   <span data-testid="candidateCount">{chunks}</span>
 );
-
-function a(chunks: React.ReactNode): React.ReactNode {
-  return (
-    <a
-      href={`mailto:${TALENTSEARCH_RECRUITMENT_EMAIL}`}
-      data-h2-font-weight="base(700)"
-    >
-      {chunks}
-    </a>
-  );
-}
 
 export const SearchContainer: React.FC<SearchContainerProps> = ({
   applicantFilter,
@@ -246,52 +311,11 @@ export const SearchContainer: React.FC<SearchContainerProps> = ({
           />
         </div>
         <div>
-          {!updatePending &&
-            (poolCandidateResults && poolCandidateResults.length > 0 ? (
-              /* there are results to show */
-              <div>
-                {poolCandidateResults.map(({ pool, candidateCount }) => (
-                  <CandidateResults
-                    key={pool.id}
-                    candidateCount={candidateCount}
-                    pool={pool}
-                    handleSubmit={tryHandleSubmit}
-                  />
-                ))}
-              </div>
-            ) : (
-              /* there are no results to show */
-              <div
-                data-h2-shadow="base(m)"
-                data-h2-margin="base(x.5, 0, 0, 0)"
-                data-h2-padding="base(x1)"
-                data-h2-border="base(left, x1, solid, dt-gray.dark)"
-              >
-                <p>
-                  {intl.formatMessage({
-                    defaultMessage: "We can still help!",
-                    id: "5U+V2Y",
-                    description:
-                      "Heading for helping user if no candidates matched the filters chosen.",
-                  })}
-                </p>
-                <p data-h2-margin="base(x.5, 0, 0, 0)">
-                  {intl.formatMessage(
-                    {
-                      defaultMessage:
-                        "If there are no matching candidates <a>Get in touch!</a>",
-                      id: "+ZXZj+",
-                      description:
-                        "Message for helping user if no candidates matched the filters chosen.",
-                    },
-                    {
-                      a,
-                    },
-                  )}
-                </p>
-              </div>
-            ))}
-          {updatePending && <Spinner />}
+          <ResultsDisplay
+            pending={updatePending}
+            results={poolCandidateResults}
+            handleSubmit={tryHandleSubmit}
+          />
         </div>
       </div>
     </div>
