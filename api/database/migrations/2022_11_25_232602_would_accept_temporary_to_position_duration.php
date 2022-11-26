6d778f5be1ed07d80b4b763d2c3f3e417c54c28a
vd1992 <40485260+vd1992@users.noreply.github.com>
2022-11-25 17:54:11 -0700

    migration + enum

---
+++ api/database/migrations/2022_11_25_232602_would_accept_temporary_to_position_duration.php
@@ -0,0 +1,68 @@
+<?php
+
+use Database\Helpers\ApiEnums;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+class WouldAcceptTemporaryToPositionDuration extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->jsonb('position_duration')->nullable()->default(null);
+        });
+        Schema::table('applicant_filters', function (Blueprint $table) {
+            $table->jsonb('position_duration')->nullable()->default(null);
+        });
+
+        DB::statement(
+        <<<SQL
+            UPDATE users
+                SET position_duration =
+                    case would_accept_temporary
+                        when true then :wouldAccept::jsonb
+                        when false then :wouldNotAccept::jsonb
+                        when null then null
+                    end
+        SQL, [
+            'wouldAccept' => json_encode([ApiEnums::POSITION_DURATION_TEMPORARY, ApiEnums::POSITION_DURATION_PERMANENT]),
+            'wouldNotAccept' => json_encode([ApiEnums::POSITION_DURATION_PERMANENT]),
+        ]);
+
+        DB::statement(
+        <<<SQL
+            UPDATE applicant_filters
+                SET position_duration =
+                    case would_accept_temporary
+                        when true then :wouldAccept::jsonb
+                        when false then :wouldNotAccept::jsonb
+                        when null then null
+                    end
+        SQL, [
+            'wouldAccept' => json_encode([ApiEnums::POSITION_DURATION_TEMPORARY, ApiEnums::POSITION_DURATION_PERMANENT]),
+            'wouldNotAccept' => json_encode([ApiEnums::POSITION_DURATION_PERMANENT]),
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('position_duration');
+        });
+        Schema::table('applicant_filters', function (Blueprint $table) {
+            $table->dropColumn('position_duration');
+        });
+    }
+}
