d8fe65dba8c1d385e92973021aded9b4b9862b95
Eric Sizer <eric.sizer@gmail.com>
2022-12-06 12:03:39 -0500

    undo pool factory change

--- api/database/factories/PoolFactory.php
+++ api/database/factories/PoolFactory.php
@@ -29,7 +29,6 @@ class PoolFactory extends Factory
     {
         $name = $this->faker->unique()->company();
         $isRemote = $this->faker->boolean();
-        $publishingGroup = $this->faker->optional->randomElement(ApiEnums::publishingGroups());
         return [
             'name' => ['en' => $name, 'fr' => $name],
             'key' => KeyStringHelpers::toKeyString($name),
@@ -47,7 +46,7 @@ class PoolFactory extends Factory
             'is_remote' => $isRemote,
             'stream' => $this->faker->optional->randomElement(ApiEnums::poolStreams()),
             'process_number' => $this->faker->optional->word(),
-            'publishing_group' => $publishingGroup
+            'publishing_group' => $this->faker->optional->randomElement(ApiEnums::publishingGroups())
         ];
     }
 
