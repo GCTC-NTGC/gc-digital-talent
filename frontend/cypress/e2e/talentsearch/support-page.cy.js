e8c0e96d3223a5780e375b051f7c5fc2bc92cbd5
Matt Nigh <matt@mattnigh.com>
2022-12-06 22:36:41 -0500

    Add e2e test for Support page

---
+++ frontend/cypress/e2e/talentsearch/support-page.cy.js
@@ -0,0 +1,33 @@
+describe("Support page", () => {
+
+  const name = "Test Person"
+  const email = "test@test.tld"
+  const details = "Test comments to send."
+  const subject = "question"
+
+  context('Support page', () => {
+    it('should exist', () => {
+      cy.visit('/en/support')
+      cy.findByRole('heading', { name: 'Contact and support', level: 1 }).should('exist')
+    })
+  })
+
+  context('Support form', () => {
+    it("should send POST request to existing api endpoint", () => {
+      cy.request({
+        method: "POST",
+        url: "/api/support/tickets",
+        failOnStatusCode: false,
+        body: {
+          name,
+          email,
+          details,
+          subject
+        },
+      }).then((response) => {
+        if(response.status === 404) // verify if route exists.
+          throw new Error("404: Not Found")
+      })
+    })
+  })
+})
