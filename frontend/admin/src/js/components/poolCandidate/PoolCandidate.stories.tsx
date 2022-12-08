e3f52558cddefd5d0bab24ab9cacfdb38f36081e
vd1992 <40485260+vd1992@users.noreply.github.com>
2022-12-08 12:27:23 -0700

    remove obsolete fields

--- frontend/admin/src/js/components/poolCandidate/PoolCandidate.stories.tsx
+++ frontend/admin/src/js/components/poolCandidate/PoolCandidate.stories.tsx
@@ -8,7 +8,6 @@ import {
   fakePools,
   fakePoolCandidates,
 } from "@common/fakeData";
-import { OperationalRequirementV1 } from "@common/constants/localizedConstants";
 import PoolCandidatesTable from "./PoolCandidatesTable";
 import {
   CreatePoolCandidateAsAdminInput,
@@ -20,9 +19,6 @@ import {
   PoolCandidate,
   UpdatePoolCandidateMutation,
   PoolCandidateStatus,
-  WorkRegion,
-  LanguageAbility,
-  SalaryRange,
 } from "../../api/generated";
 import { CreatePoolCandidateForm } from "./CreatePoolCandidate";
 import { UpdatePoolCandidateForm } from "./UpdatePoolCandidate";
@@ -80,25 +76,8 @@ stories.add("Create Pool Candidate Form", () => (
 stories.add("Update Pool Candidate Form", () => {
   const poolCandidate: PoolCandidate = {
     id: "1",
-    acceptedOperationalRequirements: [
-      OperationalRequirementV1[0],
-      OperationalRequirementV1[1],
-    ],
-    cmoAssets: [fakeCmoAssets()[0], fakeCmoAssets()[1]],
     cmoIdentifier: "cmo1",
-    expectedClassifications: [
-      fakeClassifications()[0],
-      fakeClassifications()[1],
-    ],
-    expectedSalary: [SalaryRange["100KPlus"], SalaryRange["80_89K"]],
     expiryDate: "2999-12-31",
-    hasDiploma: true,
-    hasDisability: false,
-    isIndigenous: true,
-    isVisibleMinority: false,
-    isWoman: true,
-    languageAbility: LanguageAbility.Bilingual,
-    locationPreferences: [WorkRegion.BritishColumbia, WorkRegion.Ontario],
     pool: { id: "pool-id" },
     user: { id: "user-id" },
     status: PoolCandidateStatus.NewApplication,
