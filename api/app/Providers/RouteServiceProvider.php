01612d5679c456ced0e06f059427e32d50289d40
Matt Nigh <matt@mattnigh.com>
2022-12-08 14:45:11 -0500

    Update configureRateLimiting increase api to 100 per minute; add graphql

--- api/app/Providers/RouteServiceProvider.php
+++ api/app/Providers/RouteServiceProvider.php
@@ -57,7 +57,10 @@ class RouteServiceProvider extends ServiceProvider
     protected function configureRateLimiting()
     {
         RateLimiter::for('api', function (Request $request) {
-            return Limit::perMinute(60)->by(optional($request->user())->id ?: $request->ip());
+            return Limit::perMinute(100)->by(optional($request->user())->id ?: $request->ip());
+        });
+        RateLimiter::for('graphql', function (Request $request) {
+            return Limit::perMinute(100)->by(optional($request->user())->id ?: $request->ip());
         });
     }
 }
