81cb091af04089489e4578152a146a8cb6dbd956
vd1992 <40485260+vd1992@users.noreply.github.com>
2022-12-09 09:44:57 -0700

    delete create/update PC

--- frontend/admin/src/js/components/poolCandidate/PoolCandidate.stories.tsx
+++ frontend/admin/src/js/components/poolCandidate/PoolCandidate.stories.tsx
@@ -1,27 +1,7 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
-import { action } from "@storybook/addon-actions";
-import {
-  fakeClassifications,
-  fakeCmoAssets,
-  fakeUsers,
-  fakePools,
-  fakePoolCandidates,
-} from "@common/fakeData";
+import { fakePoolCandidates } from "@common/fakeData";
 import PoolCandidatesTable from "./PoolCandidatesTable";
-import {
-  CreatePoolCandidateAsAdminInput,
-  User,
-  Classification,
-  Pool,
-  CmoAsset,
-  UpdatePoolCandidateAsAdminInput,
-  PoolCandidate,
-  UpdatePoolCandidateMutation,
-  PoolCandidateStatus,
-} from "../../api/generated";
-import { CreatePoolCandidateForm } from "./CreatePoolCandidate";
-import { UpdatePoolCandidateForm } from "./UpdatePoolCandidate";
 
 const poolCandidateData = fakePoolCandidates();
 
@@ -54,52 +34,3 @@ stories.add(
     },
   },
 );
-
-stories.add("Create Pool Candidate Form", () => (
-  <CreatePoolCandidateForm
-    users={fakeUsers() as User[]}
-    classifications={fakeClassifications() as Classification[]}
-    pools={fakePools() as Pool[]}
-    cmoAssets={fakeCmoAssets() as CmoAsset[]}
-    handleCreatePoolCandidate={async (
-      data: CreatePoolCandidateAsAdminInput,
-    ) => {
-      await new Promise((resolve) => {
-        setTimeout(resolve, 1000);
-      });
-      action("Create Pool Candidate")(data);
-      return null;
-    }}
-  />
-));
-
-stories.add("Update Pool Candidate Form", () => {
-  const poolCandidate: PoolCandidate = {
-    id: "1",
-    cmoIdentifier: "cmo1",
-    expiryDate: "2999-12-31",
-    pool: { id: "pool-id" },
-    user: { id: "user-id" },
-    status: PoolCandidateStatus.NewApplication,
-  };
-
-  return (
-    <UpdatePoolCandidateForm
-      classifications={fakeClassifications() as Classification[]}
-      cmoAssets={fakeCmoAssets() as CmoAsset[]}
-      initialPoolCandidate={poolCandidate}
-      handleUpdatePoolCandidate={async (
-        id: string,
-        data: UpdatePoolCandidateAsAdminInput,
-      ): Promise<UpdatePoolCandidateMutation["updatePoolCandidateAsAdmin"]> => {
-        await new Promise((resolve) => {
-          setTimeout(resolve, 1000);
-        });
-        action("Update Pool Candidate")(id, data);
-        return Promise.resolve(
-          data as UpdatePoolCandidateMutation["updatePoolCandidateAsAdmin"],
-        );
-      }}
-    />
-  );
-});
