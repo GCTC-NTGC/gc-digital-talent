eccae692a30586a4baa8953a4b13b8988a7a7e0c
Matt Nigh <matt@mattnigh.com>
2022-12-12 10:21:06 -0500

    Uncomment NestedLanguageProvider, LanguageSelector for IAP

--- frontend/indigenousapprenticeship/src/js/components/Layout.tsx
+++ frontend/indigenousapprenticeship/src/js/components/Layout.tsx
@@ -11,28 +11,22 @@ import Header from "@common/components/Header";
 import Footer from "@common/components/Footer";
 import { getLocale } from "@common/helpers/localize";
 import { getRuntimeVariable } from "@common/helpers/runtimeVariable";
-// import { Messages } from "@common/components/context/LanguageProvider";
-// import NestedLanguageProvider from "@common/components/context/NestedLanguageProvider";
+import { Messages } from "@common/components/context/LanguageProvider";
+import NestedLanguageProvider from "@common/components/context/NestedLanguageProvider";
 
 import { useIndigenousApprenticeshipRoutes } from "../routes/indigenousApprenticeshipRoutes";
 
-/**
- * TODO: This will be implemented in #4617
- *
- * Wrap entire component in <NestedLanguageProvider messages={messages} />
- * */
+import * as crgMessages from "../lang/crgCompiled.json";
+import * as crkMessages from "../lang/crkCompiled.json";
+import * as ojwMessages from "../lang/ojwCompiled.json";
+import * as micMessages from "../lang/micCompiled.json";
 
-// import * as crgMessages from "../lang/crgCompiled.json";
-// import * as crkMessages from "../lang/crkCompiled.json";
-// import * as ojwMessages from "../lang/ojwCompiled.json";
-// import * as micMessages from "../lang/micCompiled.json";
-
-// const messages: Map<string, Messages> = new Map([
-//   ["crg", crgMessages],
-//   ["crk", crkMessages],
-//   ["ojw", ojwMessages],
-//   ["mic", micMessages],
-// ]);
+const messages: Map<string, Messages> = new Map([
+  ["crg", crgMessages],
+  ["crk", crkMessages],
+  ["ojw", ojwMessages],
+  ["mic", micMessages],
+]);
 
 export const Layout = () => {
   const intl = useIntl();
@@ -55,67 +49,69 @@ export const Layout = () => {
   }
 
   return (
-    <AnimatePresence>
-      <React.Fragment key={location.pathname}>
-        <Helmet>
-          <html lang={locale} />
-          <title>
-            {intl.formatMessage({
-              defaultMessage: "Indigenous Apprenticeship Program",
-              id: "C5tUG2",
-              description:
-                "Title tag for Indigenous Apprenticeship Program site",
-            })}
-          </title>
-          <meta
-            name="description"
-            content={intl.formatMessage({
-              defaultMessage:
-                "Apply now to get started on your IT career journey.",
-              id: "Oh1/Gc",
-              description:
-                "Meta tag description for Indigenous Apprenticeship Program site",
-            })}
-          />
-        </Helmet>
-        <a href="#main" data-h2-visibility="base(hidden)">
-          {intl.formatMessage({
-            defaultMessage: "Skip to main content",
-            id: "Srs7a4",
-            description: "Assistive technology skip link",
-          })}
-        </a>
-        <div
-          className="container"
-          data-h2-display="base(flex)"
-          data-h2-flex-direction="base(column)"
-          style={{ height: "100vh", margin: "0" }}
-        >
-          <div>
-            <Header />
-            <NavMenu
-              mainItems={[
-                <MenuLink key="home" to={paths.home()}>
-                  {intl.formatMessage({
-                    defaultMessage: "Home",
-                    id: "TFeQL2",
-                    description:
-                      "Link to the homepage for indigenous apprenticeship program.",
-                  })}
-                </MenuLink>,
-              ]}
+    <NestedLanguageProvider messages={messages}>
+      <AnimatePresence>
+        <React.Fragment key={location.pathname}>
+          <Helmet>
+            <html lang={locale} />
+            <title>
+              {intl.formatMessage({
+                defaultMessage: "Indigenous Apprenticeship Program",
+                id: "C5tUG2",
+                description:
+                  "Title tag for Indigenous Apprenticeship Program site",
+              })}
+            </title>
+            <meta
+              name="description"
+              content={intl.formatMessage({
+                defaultMessage:
+                  "Apply now to get started on your IT career journey.",
+                id: "Oh1/Gc",
+                description:
+                  "Meta tag description for Indigenous Apprenticeship Program site",
+              })}
             />
+          </Helmet>
+          <a href="#main" data-h2-visibility="base(hidden)">
+            {intl.formatMessage({
+              defaultMessage: "Skip to main content",
+              id: "Srs7a4",
+              description: "Assistive technology skip link",
+            })}
+          </a>
+          <div
+            className="container"
+            data-h2-display="base(flex)"
+            data-h2-flex-direction="base(column)"
+            style={{ height: "100vh", margin: "0" }}
+          >
+            <div>
+              <Header />
+              <NavMenu
+                mainItems={[
+                  <MenuLink key="home" to={paths.home()}>
+                    {intl.formatMessage({
+                      defaultMessage: "Home",
+                      id: "TFeQL2",
+                      description:
+                        "Link to the homepage for indigenous apprenticeship program.",
+                    })}
+                  </MenuLink>,
+                ]}
+              />
+            </div>
+            <main id="main">
+              <Outlet />
+            </main>
+            <div style={{ marginTop: "auto" }}>
+              <Footer />
+            </div>
           </div>
-          <main id="main">
-            <Outlet />
-          </main>
-          <div style={{ marginTop: "auto" }}>
-            <Footer />
-          </div>
-        </div>
-        <ScrollRestoration />
-      </React.Fragment>
-    </AnimatePresence>
+          <ScrollRestoration />
+        </React.Fragment>
+      </AnimatePresence>
+    </NestedLanguageProvider>
   );
 };
 
