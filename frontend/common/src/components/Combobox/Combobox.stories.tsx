005f28025c643c7dbe3d52785037e253f29fcb3c
Eric Sizer <eric.sizer@gmail.com>
2022-12-13 11:58:08 -0500

    add story for API driven combobox

--- frontend/common/src/components/Combobox/Combobox.stories.tsx
+++ frontend/common/src/components/Combobox/Combobox.stories.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-import type { ComponentStory, ComponentMeta } from "@storybook/react";
+import type { Story, Meta } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
 
+import { Maybe } from "../../api/generated";
 import { getStaticSkills } from "../../fakeData";
 
-import Combobox from "./Combobox";
+import Combobox, { Option, ComboboxProps } from "./Combobox";
 import BasicForm from "../form/BasicForm";
 import Submit from "../form/Submit";
 
@@ -19,23 +20,77 @@ const defaultArgs = {
   options: skills,
 };
 
+type ComboboxType = ComboboxProps & {
+  mockSearch?: (term: string) => Promise<Option[]>;
+};
+
 export default {
   component: Combobox,
   title: "Components/Combobox",
-} as ComponentMeta<typeof Combobox>;
+} as Meta<ComboboxType>;
 
-const Template: ComponentStory<typeof Combobox> = (args) => (
-  <BasicForm
-    onSubmit={action("onSubmit")}
-    options={{ defaultValues: { skill: "" } }}
-  >
-    <Combobox {...args} />
-    <Submit />
-  </BasicForm>
-);
+const Template: Story<ComboboxType> = (args) => {
+  const { mockSearch, options, ...rest } = args;
+  const [isSearching, setIsSearching] = React.useState<boolean>(false);
+  const [filteredOptions, setFilteredOptions] = React.useState<Option[]>([]);
+
+  const handleSearch = React.useMemo(() => {
+    return mockSearch
+      ? (term: string) => {
+          setIsSearching(true);
+          mockSearch(term)
+            .then((newOptions) => {
+              setFilteredOptions(newOptions);
+            })
+            .finally(() => {
+              setIsSearching(false);
+            });
+        }
+      : undefined;
+  }, [mockSearch]);
+
+  return (
+    <BasicForm
+      onSubmit={action("onSubmit")}
+      options={{ defaultValues: { skill: "" } }}
+    >
+      <Combobox
+        {...rest}
+        onSearch={handleSearch}
+        fetching={isSearching}
+        options={mockSearch ? filteredOptions : options}
+      />
+      <Submit />
+    </BasicForm>
+  );
+};
 
 export const Default = Template.bind({});
 Default.args = defaultArgs;
+
+export const Loading = Template.bind({});
+Loading.args = {
+  ...defaultArgs,
+  fetching: true,
+};
+
+export const APIDriven = Template.bind({});
+APIDriven.args = {
+  ...defaultArgs,
+  mockSearch: async (term): Promise<Option[]> => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        const filteredOptions = defaultArgs.options.filter((option) => {
+          return option.label
+            ?.toLocaleString()
+            .toLowerCase()
+            .includes(term.toLowerCase());
+        });
+        resolve(filteredOptions);
+      }, 1000);
+    });
+  },
+};
 
 export const Required = Template.bind({});
 Required.args = {
