1377711566f6a884c69bd7a6390d24aa2e841ac0
Eric Sizer <eric.sizer@gmail.com>
2022-12-17 09:50:40 -0500

    update router, hardcode locale

--- apps/web/src/components/Router.tsx
+++ apps/web/src/components/Router.tsx
@@ -1,36 +1,36 @@
 import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
+import { Locales } from "@common/helpers/localize";
 import Loading from "@common/components/Pending/Loading";
 
+import useLocale from "@common/hooks/useLocale";
 import Home from "./Home/Home";
 import Layout from "./Layout";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    children: [
-      {
-        path: ":locale",
-        children: [
-          {
-            path: "indigenous-it-apprentice",
-            children: [
-              {
-                index: true,
-                element: <Home />,
-              },
-            ],
-          },
-        ],
-      },
-    ],
-  },
-]);
+const createRoute = (locale: Locales) =>
+  createBrowserRouter([
+    {
+      path: `/${locale}`,
+      element: <Layout />,
+      children: [
+        {
+          path: "indigenous-it-apprentice",
+          children: [
+            {
+              index: true,
+              element: <Home />,
+            },
+          ],
+        },
+      ],
+    },
+  ]);
 
-export const Router = () => (
-  <RouterProvider router={router} fallbackElement={<Loading />} />
-);
+const Router = () => {
+  const { locale } = useLocale();
+  const router = createRoute(locale);
+  return <RouterProvider router={router} fallbackElement={<Loading />} />;
+};
 
 export default Router;
