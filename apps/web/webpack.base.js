8867f73e5583a587671e069599b6cd9e9120777f
Eric Sizer <eric.sizer@gmail.com>
2022-12-17 12:18:18 -0500

    fix public path for chunks

--- apps/web/webpack.base.js
+++ apps/web/webpack.base.js
@@ -8,7 +8,7 @@ module.exports = merge(base(basePath), {
   entry: {
     app: [
       "./src/main.tsx",
-      path.resolve(basePath, "../../frontend/common/src/css/hydrogen.css"),
+      "../../frontend/common/src/css/hydrogen.css",
       "../../frontend/common/src/css/common.css",
       "./src/assets/css/app.css",
     ],
@@ -22,7 +22,7 @@ module.exports = merge(base(basePath), {
   },
   // TO DO: We should be able to remove this after merging all apps
   output: {
-    publicPath: "/indigenous-it-apprentice", // final path for routing
+    publicPath: "/indigenous-it-apprentice/", // final path for routing
     filename: "[name].[contenthash].js", // file hashing for cache busting
     chunkFilename: "[name].[contenthash].js", // file hashing for cache busting
     path: path.resolve(basePath, "dist"), // output folder
