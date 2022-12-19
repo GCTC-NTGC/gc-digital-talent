93c23ab5d6eb5f941b15d0046a4cde4294f3fccb
Eric Sizer <eric.sizer@gmail.com>
2022-12-19 14:39:11 -0500

    move permission update

--- maintenance/scripts/setup.sh
+++ maintenance/scripts/setup.sh
@@ -26,6 +26,6 @@ cp ./apps/web/.env.example ./apps/web/.env --preserve=all
 # build projects
 cd /var/www/html
 npm ci
-chmod -R a+r,a+w node_modules
 ./node_modules/.bin/cypress install
 npm run build
+chmod -R a+r,a+w node_modules
