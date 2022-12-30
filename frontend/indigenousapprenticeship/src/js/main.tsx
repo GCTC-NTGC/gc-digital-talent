d39964c930c5ae5d820d866a1a67e522ea77360b
Eric Sizer <eric.sizer@gmail.com>
2022-12-30 09:51:05 -0500

    fix root render

--- frontend/indigenousapprenticeship/src/js/main.tsx
+++ frontend/indigenousapprenticeship/src/js/main.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 
 import ContextContainer from "@common/components/context/ContextContainer";
 import Toast from "@common/components/Toast/Toast";
@@ -7,12 +7,15 @@ import * as messages from "./lang/frCompiled.json";
 
 import Router from "./components/Router";
 
-ReactDOM.render(
-  <>
-    <ContextContainer messages={messages}>
-      <Router />
-    </ContextContainer>
-    <Toast />
-  </>,
-  document.getElementById("app"),
-);
+const container = document.getElementById("app");
+if (container) {
+  const root = createRoot(container);
+  root.render(
+    <>
+      <ContextContainer messages={messages}>
+        <Router />
+      </ContextContainer>
+      <Toast />
+    </>,
+  );
+}
