472543a5aae74eea470d01ac23925b45b1693edb
Yonathan K <yoni.kid15@gmail.com>
2023-01-05 02:06:25 -0500

    Create method to get skill-justification values for each column

---
+++ frontend/common/src/components/UserProfile/ExperienceAccordion/labels.tsx
@@ -0,0 +1,24 @@
+import { defineMessages } from "react-intl";
+
+export default defineMessages({
+  awardIssuedBy: {
+    defaultMessage: "{title} issued by {issuedBy}",
+    id: "4BpFoX",
+    description: "The award title is issued by some group",
+  },
+  communityAt: {
+    defaultMessage: "{title} at {organization}",
+    id: "vV0SDz",
+    description: "Title at organization",
+  },
+  educationAt: {
+    defaultMessage: "{areaOfStudy} at {institution}",
+    id: "UrsGGK",
+    description: "Study at institution",
+  },
+  workAt: {
+    defaultMessage: "{role} at {organization}",
+    id: "wTAdQe",
+    description: "Role at organization",
+  },
+});
