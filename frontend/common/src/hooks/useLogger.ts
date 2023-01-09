d1ae05ad87729466c7953767fe8acb09ffaaaac5
Peter Giles <8978655+petertgiles@users.noreply.github.com>
2023-01-09 15:32:18 -0500

    tidy up for pr

--- frontend/common/src/hooks/useLogger.ts
+++ frontend/common/src/hooks/useLogger.ts
@@ -2,7 +2,7 @@
 
 import { SeverityLevel, levelIncludes } from "../helpers/loggingUtils";
 import { getLoggingLevel } from "../helpers/runtimeVariable";
-import useAppInsightsCustomEvent from "./useAppInsightsCustomEvent";
+import { reactPlugin } from "../components/AppInsights/ReactPlugin";
 
 export interface Logger {
   emergency: (message: string) => void;
@@ -14,74 +14,87 @@ export interface Logger {
   info: (message: string) => void;
   debug: (message: string) => void;
 }
-
-// a no-op function for when messages won't be logged
-// eslint-disable-next-line @typescript-eslint/no-empty-function
-const noop = () => {};
-
 // A simple logger for logging to the browser console.
 // Mostly useful during development.
 // Controlled by setting the LOG_CONSOLE_LEVEL environment variable.
 const consoleLoggerLevel = getLoggingLevel("LOG_CONSOLE_LEVEL");
 const consoleLogger: Logger = {
-  emergency: levelIncludes(consoleLoggerLevel, SeverityLevel.Emergency)
-    ? (message: string) => console.error(message)
-    : noop,
-  alert: levelIncludes(consoleLoggerLevel, SeverityLevel.Alert)
-    ? (message: string) => console.error(message)
-    : noop,
-  critical: levelIncludes(consoleLoggerLevel, SeverityLevel.Critical)
-    ? (message: string) => console.error(message)
-    : noop,
-  error: levelIncludes(consoleLoggerLevel, SeverityLevel.Error)
-    ? (message: string) => console.error(message)
-    : noop,
-  warning: levelIncludes(consoleLoggerLevel, SeverityLevel.Warning)
-    ? (message: string) => console.warn(message)
-    : noop,
-  notice: levelIncludes(consoleLoggerLevel, SeverityLevel.Notice)
-    ? (message: string) => console.info(message)
-    : noop,
-  info: levelIncludes(consoleLoggerLevel, SeverityLevel.Info)
-    ? (message: string) => console.info(message)
-    : noop,
-  debug: levelIncludes(consoleLoggerLevel, SeverityLevel.Debug)
-    ? (message: string) => console.debug(message)
-    : noop,
+  emergency: (message: string) => {
+    if (levelIncludes(consoleLoggerLevel, SeverityLevel.Emergency))
+      console.error(message);
+  },
+  alert: (message: string) => {
+    if (levelIncludes(consoleLoggerLevel, SeverityLevel.Alert))
+      console.error(message);
+  },
+  critical: (message: string) => {
+    if (levelIncludes(consoleLoggerLevel, SeverityLevel.Critical))
+      console.error(message);
+  },
+  error: (message: string) => {
+    if (levelIncludes(consoleLoggerLevel, SeverityLevel.Error))
+      console.error(message);
+  },
+  warning: (message: string) => {
+    if (levelIncludes(consoleLoggerLevel, SeverityLevel.Warning))
+      console.warn(message);
+  },
+  notice: (message: string) => {
+    if (levelIncludes(consoleLoggerLevel, SeverityLevel.Notice))
+      console.info(message);
+  },
+  info: (message: string) => {
+    if (levelIncludes(consoleLoggerLevel, SeverityLevel.Info))
+      console.info(message);
+  },
+  debug: (message: string) => {
+    if (levelIncludes(consoleLoggerLevel, SeverityLevel.Debug))
+      console.debug(message);
+  },
 };
 
 // A logger for logging to the Azure Application Insights custom events.
 // Only useful when deployed on an Azure server.
 // Controlled by setting the LOG_APPLICATIONINSIGHTS_LEVEL environment variable.
 const aiLoggerLevel = getLoggingLevel("LOG_APPLICATIONINSIGHTS_LEVEL");
-const aiLogger: Logger = {
-  emergency: levelIncludes(aiLoggerLevel, SeverityLevel.Emergency)
-    ? (message: string) => useAppInsightsCustomEvent("EMERGENCY", { message })
-    : noop,
-  alert: levelIncludes(aiLoggerLevel, SeverityLevel.Alert)
-    ? (message: string) => useAppInsightsCustomEvent("ALERT", { message })
-    : noop,
-  critical: levelIncludes(aiLoggerLevel, SeverityLevel.Critical)
-    ? (message: string) => useAppInsightsCustomEvent("CRITICAL", { message })
-    : noop,
-  error: levelIncludes(aiLoggerLevel, SeverityLevel.Error)
-    ? (message: string) => useAppInsightsCustomEvent("ERROR", { message })
-    : noop,
-  warning: levelIncludes(aiLoggerLevel, SeverityLevel.Warning)
-    ? (message: string) => useAppInsightsCustomEvent("WARNING", { message })
-    : noop,
-  notice: levelIncludes(aiLoggerLevel, SeverityLevel.Notice)
-    ? (message: string) => useAppInsightsCustomEvent("NOTICE", { message })
-    : noop,
-  info: levelIncludes(aiLoggerLevel, SeverityLevel.Info)
-    ? (message: string) => useAppInsightsCustomEvent("INFO", { message })
-    : noop,
-  debug: levelIncludes(aiLoggerLevel, SeverityLevel.Debug)
-    ? (message: string) => useAppInsightsCustomEvent("DEBUG", { message })
-    : noop,
+const applicationInsightsLogger: Logger = {
+  emergency: (message: string) => {
+    if (levelIncludes(aiLoggerLevel, SeverityLevel.Emergency))
+      reactPlugin.trackEvent({ name: "Emergency" }, { message });
+  },
+  alert: (message: string) => {
+    if (levelIncludes(aiLoggerLevel, SeverityLevel.Alert))
+      reactPlugin.trackEvent({ name: "Alert" }, { message });
+  },
+  critical: (message: string) => {
+    if (levelIncludes(aiLoggerLevel, SeverityLevel.Critical))
+      reactPlugin.trackEvent({ name: "Critical" }, { message });
+  },
+  error: (message: string) => {
+    if (levelIncludes(aiLoggerLevel, SeverityLevel.Error))
+      reactPlugin.trackEvent({ name: "Error" }, { message });
+  },
+  warning: (message: string) => {
+    if (levelIncludes(aiLoggerLevel, SeverityLevel.Warning))
+      reactPlugin.trackEvent({ name: "Warning" }, { message });
+  },
+  notice: (message: string) => {
+    if (levelIncludes(aiLoggerLevel, SeverityLevel.Notice))
+      reactPlugin.trackEvent({ name: "Notice" }, { message });
+  },
+  info: (message: string) => {
+    if (levelIncludes(aiLoggerLevel, SeverityLevel.Info))
+      reactPlugin.trackEvent({ name: "Info" }, { message });
+  },
+  debug: (message: string) => {
+    if (levelIncludes(aiLoggerLevel, SeverityLevel.Debug))
+      reactPlugin.trackEvent({ name: "Debug" }, { message });
+  },
 };
 
-const childLoggers: Logger[] = [consoleLogger, aiLogger];
+// A logger that sends all messages to the child loggers.
+// Logging by child loggers is controlled by the level settings of each child logger.
+const childLoggers: Logger[] = [consoleLogger, applicationInsightsLogger];
 const stackLogger: Logger = {
   emergency: (message: string) =>
     childLoggers.forEach((l) => l.emergency(message)),
--- frontend/talentsearch/src/js/components/Home/HomePage.tsx
+++ frontend/talentsearch/src/js/components/Home/HomePage.tsx
@@ -3,24 +3,13 @@ import { useIntl } from "react-intl";
 
 import SEO from "@common/components/SEO/SEO";
 
-import { Button } from "@common/components";
-import { useLogger } from "@common/hooks/useLogger";
 import { About, Featured, Hero, Opportunities, Profile } from "./partials";
 import type { HeroProps } from "./partials/Hero/Hero";
 
 const HomePage = ({ defaultImage }: HeroProps) => {
-  const logger = useLogger();
   const intl = useIntl();
   return (
     <>
-      <Button onClick={() => logger.emergency("Emergency!")}>Emergency</Button>
-      <Button onClick={() => logger.alert("Alert!")}>Alert</Button>
-      <Button onClick={() => logger.critical("Critical!")}>Critical</Button>
-      <Button onClick={() => logger.error("Error!")}>Error</Button>
-      <Button onClick={() => logger.warning("Warning!")}>Warning</Button>
-      <Button onClick={() => logger.notice("Notice!")}>Notice</Button>
-      <Button onClick={() => logger.info("Info!")}>Info</Button>
-      <Button onClick={() => logger.debug("Debug!")}>Debug</Button>
       <SEO
         title={intl.formatMessage({
           defaultMessage: "Welcome",
