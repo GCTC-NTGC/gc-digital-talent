16778aaf942f9591ae3949531c6727286d86a917
Josh Beveridge <git@joshbeveridge.ca>
2023-01-09 12:17:41 -0500

    Replaces offset with location in apps dir

--- apps/web/src/components/Banner/Banner.tsx
+++ apps/web/src/components/Banner/Banner.tsx
@@ -4,21 +4,21 @@ const Banner: React.FC<{ children?: React.ReactNode }> = ({ children }) => (
   <div
     data-h2-display="base(inline-block)"
     data-h2-position="base(relative)"
-    data-h2-offset="base(-x3, auto, auto, auto)"
+    data-h2-location="base(-x3, auto, auto, auto)"
   >
     <div
       data-h2-position="base(absolute)"
       data-h2-background-color="base(ia-primary.dark)"
       data-h2-height="base(75%)"
       data-h2-width="base(45%)"
-      data-h2-offset="base(auto, auto, -20%, -15%)"
+      data-h2-location="base(auto, auto, -20%, -15%)"
     />
     <div
       data-h2-position="base(absolute)"
       data-h2-background-color="base(ia-primary.dark)"
       data-h2-height="base(75%)"
       data-h2-width="base(45%)"
-      data-h2-offset="base(auto, -15%, -20%, auto)"
+      data-h2-location="base(auto, -15%, -20%, auto)"
     />
     <div
       data-h2-padding="base(1rem) p-tablet(x1.5)"
