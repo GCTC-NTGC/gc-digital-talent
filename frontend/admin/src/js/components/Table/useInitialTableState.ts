3566ab0ecb2a7445dec4a33ca876f777403d5d80
Eric Sizer <eric.sizer@gmail.com>
2023-01-10 16:20:08 -0500

    stabilize initial sort param

--- frontend/admin/src/js/components/Table/useInitialTableState.ts
+++ frontend/admin/src/js/components/Table/useInitialTableState.ts
@@ -5,16 +5,18 @@ const useInitialTableState = (searchParams: URLSearchParams) => {
   const pageSize = searchParams.get("pageSize");
   const hiddenColumns = searchParams.get("hiddenColumns");
   const sortByEncoded = searchParams.get("sortBy");
-  const sortBy = sortByEncoded
-    ? JSON.parse(decodeURIComponent(sortByEncoded))
-    : undefined;
+  const sortBy = useMemo(
+    () =>
+      sortByEncoded ? JSON.parse(decodeURIComponent(sortByEncoded)) : undefined,
+    [sortByEncoded],
+  );
 
   return useMemo(
     () => ({
-      pageIndex: pageIndex ? parseInt(pageIndex, 10) : undefined,
-      pageSize: pageSize ? parseInt(pageSize, 10) : undefined,
-      hiddenColumns: hiddenColumns?.split(",") || undefined,
-      sortBy: sortBy || undefined,
+      pageIndex: pageIndex ? parseInt(pageIndex, 10) : null,
+      pageSize: pageSize ? parseInt(pageSize, 10) : null,
+      hiddenColumns: hiddenColumns?.split(",") || null,
+      sortBy: sortBy || null,
     }),
     [pageIndex, pageSize, hiddenColumns, sortBy],
   );
