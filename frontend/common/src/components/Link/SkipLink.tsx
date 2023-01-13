73bca53cec05d7747b9954b098b16d9788d6b42e
Eric Sizer <eric.sizer@gmail.com>
2023-01-13 09:30:57 -0500

    update skip link with new attr

--- frontend/common/src/components/Link/SkipLink.tsx
+++ frontend/common/src/components/Link/SkipLink.tsx
@@ -11,16 +11,14 @@ const SkipLink = ({ href = "#main" }: SkipLinkProps) => {
   return (
     <a
       href={href}
+      data-h2-visually-hidden="base(invisible) base:focus-visible(visible)"
       data-h2-background-color="base:focus-visible(white)"
-      data-h2-border="base(0) base:focus-visible(medium, dashed, black)"
-      data-h2-height="base(1px) base:focus-visible(auto)"
+      data-h2-border="base:focus-visible(medium, dashed, black)"
+      data-h2-display="base:focus-visible(inline-block)"
       data-h2-location="base:focus-visible(0, auto, auto, x1)"
-      data-h2-overflow="base(hidden) base:focus-visible(visible)"
-      data-h2-margin="base(-1px) base:focus-visible(0)"
-      data-h2-padding="base(0) base:focus-visible(x.25, x.5)"
-      data-h2-position="base:(absolute) base:focus-visible(fixed)"
-      data-h2-white-space="base(nowrap)"
-      data-h2-width="base(1px) base:focus-visible(auto)"
+      data-h2-padding="base:focus-visible(x.25, x.5)"
+      data-h2-position="base:focus-visible(fixed!important)"
+      data-h2-width="base:focus-visible(auto)"
       data-h2-z-index="base:focus-visible(9999)"
     >
       {intl.formatMessage({
