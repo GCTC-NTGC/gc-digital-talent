2916f5b5356914ea82038319cea6eeab54baedb1
Eric Sizer <eric.sizer@gmail.com>
2023-01-16 10:45:55 -0500

    add hero props for variations

--- frontend/common/src/components/Hero/Hero.tsx
+++ frontend/common/src/components/Hero/Hero.tsx
@@ -2,53 +2,104 @@ import React from "react";
 
 import Heading from "../Heading";
 import Breadcrumbs, { type BreadcrumbsProps } from "../Breadcrumbs/v2";
+import Flourish from "../Flourish";
 
 import "./hero.css";
 
-interface HeroProps {
+export interface HeroProps {
   imgPath: string;
   title: React.ReactNode;
   subtitle?: React.ReactNode;
   crumbs?: BreadcrumbsProps["crumbs"];
+  children?: React.ReactNode;
+  centered?: boolean;
 }
 
-const Hero = ({ imgPath, title, subtitle, crumbs }: HeroProps) => (
-  <>
-    <div
-      data-h2-background-color="base(black)"
-      data-h2-padding="base(x3, 0, 50vh, 0) p-tablet(x4, 0, 60vh, 0) l-tablet(x4, 0)"
-      className="header-bg-image"
-      style={{
-        backgroundImage: `url('${imgPath}')`,
-      }}
-    >
+const Hero = ({
+  imgPath,
+  title,
+  subtitle,
+  crumbs,
+  children,
+  centered = false,
+}: HeroProps) => {
+  const showImg = imgPath && !centered && !children;
+  const breadCrumbs =
+    crumbs && crumbs.length > 0 ? <Breadcrumbs crumbs={crumbs} /> : null;
+  const textAlignment = centered
+    ? {
+        "data-h2-text-align": "base(center)",
+      }
+    : {
+        "data-h2-text-align": "base(center) l-tablet(left)",
+      };
+  return (
+    <>
       <div
-        data-h2-position="base(relative)"
-        data-h2-container="base(center, large, x1) p-tablet(center, large, x2)"
-        data-h2-layer="base(1, relative)"
+        data-h2-background-color="base(black)"
+        {...(children
+          ? {
+              "data-h2-padding":
+                "base(x3, 0, 50vh, 0) p-tablet(x3, 0, 60vh, 0) l-tablet(x3, 0, x6, 0)",
+            }
+          : {
+              "data-h2-padding":
+                "base(x3, 0, 50vh, 0) p-tablet(x3, 0, 60vh, 0) l-tablet(x3, 0)",
+            })}
+        className="header-bg-image"
+        style={
+          showImg
+            ? {
+                backgroundImage: `url('${imgPath}')`,
+              }
+            : {}
+        }
       >
         <div
-          data-h2-color="base(white)"
-          data-h2-text-align="base(center) l-tablet(left)"
+          data-h2-position="base(relative)"
+          data-h2-container="base(center, large, x1) p-tablet(center, large, x2)"
+          data-h2-layer="base(1, relative)"
         >
-          <Heading level="h1" data-h2-margin="base(0, 0, x0.5, 0)">
-            {title}
-          </Heading>
-          {subtitle && (
-            <p
-              data-h2-font-size="base(h6, 1.4)"
-              data-h2-font-weight="base(300)"
-              data-h2-margin="base(x1, 0, 0, 0)"
-              data-h2-max-width="l-tablet(50%)"
-            >
-              {subtitle}
-            </p>
-          )}
+          <div data-h2-color="base(white)" {...textAlignment}>
+            <Heading level="h1" data-h2-margin="base(0, 0, x0.5, 0)">
+              {title}
+            </Heading>
+            {subtitle && (
+              <p
+                data-h2-font-size="base(h6, 1.4)"
+                data-h2-font-weight="base(300)"
+                data-h2-margin="base(x1, 0, 0, 0)"
+                {...(!centered && { "data-h2-max-width": "l-tablet(50%)" })}
+              >
+                {subtitle}
+              </p>
+            )}
+          </div>
         </div>
       </div>
-    </div>
-    {crumbs && crumbs.length > 0 ? <Breadcrumbs crumbs={crumbs} /> : null}
-  </>
-);
+      {children ? (
+        <>
+          <Flourish />
+          <div
+            data-h2-container="base(center, large, x1) p-tablet(center, large, x2)"
+            data-h2-margin="base(-x3, auto, 0, auto)"
+          >
+            <div
+              data-h2-background-color="base(white)"
+              data-h2-radius="base(rounded)"
+              data-h2-padding="base(x2, x1)"
+              data-h2-shadow="base(s)"
+              {...textAlignment}
+            >
+              {children}
+            </div>
+          </div>
+        </>
+      ) : (
+        breadCrumbs
+      )}
+    </>
+  );
+};
 
 export default Hero;
