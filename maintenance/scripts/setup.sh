7b40f1a1c80f52096b165cd0671e82d69c03ddbe
Eric Sizer <eric.sizer@gmail.com>
2023-01-18 13:20:46 -0500

    downgrade turbo, remove global install

--- maintenance/scripts/setup.sh
+++ maintenance/scripts/setup.sh
@@ -24,8 +24,6 @@ cp ./apps/web/.env.example ./apps/web/.env --preserve=all
 
 # build projects
 cd /var/www/html
-npm install --location=global turbo
 npm install
-chmod -R a+r,a+w node_modules
 npm run build
 chmod -R a+r,a+w node_modules apps/web/.turbo frontend/admin/.turbo frontend/common/.turbo frontend/talentsearch/.turbo
