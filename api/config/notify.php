abb436f01e03e316ec3bfee1d8d02067347440c3
Eric Sizer <esizer@users.noreply.github.com>
2023-01-20 14:15:01 -0500

    Update api/config/notify.php
    Co-authored-by: Matt <mnigh@users.noreply.github.com>

--- api/config/notify.php
+++ api/config/notify.php
@@ -26,7 +26,6 @@ return [
     'templates' => [
         'email' => 'c386053b-e65c-477e-8f09-da19c2e9dea0',
         'sms' => '3525f0e3-845a-4001-b456-7cb02c13c69f',
-
         'bulk_email' => '90fa07f5-fbe8-402e-9547-f3259159a2da',
         'bulk_sms' => 'd662aac4-08bb-4917-860f-cc9999f4648d',
     ],
