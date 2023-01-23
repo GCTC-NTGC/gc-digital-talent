baab43dcd7ac999412b7902f9ae97ac30c71a78a
Josh Beveridge <git@joshbeveridge.ca>
2023-01-23 09:56:40 -0500

    Swaps remaining instances of tm-yellow for focus

--- frontend/common/src/components/Alert/utils.ts
+++ frontend/common/src/components/Alert/utils.ts
@@ -31,25 +31,25 @@ export const styleMap: Record<AlertType, Record<string, string>> = {
 export const dismissStyleMap: Record<AlertType, Record<string, string>> = {
   success: {
     "data-h2-background-color":
-      "base(transparent) base:hover(tm-green.lightest) base:focus-visible(tm-yellow.light)",
+      "base(transparent) base:hover(tm-green.lightest) base:focus-visible(focus.light)",
     "data-h2-color":
       "base:(inherit) base:hover(tm-green.darker)  base:focus-visible(black)",
   },
   warning: {
     "data-h2-background-color":
-      "base(transparent) base:hover(warning.lightest) base:focus-visible(tm-yellow.light)",
+      "base(transparent) base:hover(warning.lightest) base:focus-visible(focus.light)",
     "data-h2-color":
       "base:(inherit) base:hover(warning.darker)  base:focus-visible(black)",
   },
   info: {
     "data-h2-background-color":
-      "base(transparent) base:hover(secondary.lightest) base:focus-visible(tm-yellow.light)",
+      "base(transparent) base:hover(secondary.lightest) base:focus-visible(focus.light)",
     "data-h2-color":
       "base:(inherit) base:hover(secondary.darker)  base:focus-visible(black)",
   },
   error: {
     "data-h2-background-color":
-      "base(transparent) base:hover(tertiary.lightest) base:focus-visible(tm-yellow.light)",
+      "base(transparent) base:hover(tertiary.lightest) base:focus-visible(focus.light)",
     "data-h2-color":
       "base:(inherit) base:hover(tertiary.darker)  base:focus-visible(black)",
   },
--- frontend/common/src/components/Toast/styles.ts
+++ frontend/common/src/components/Toast/styles.ts
@@ -7,31 +7,31 @@ export const iconStyles = {
 export const closeButtonStyles: Record<TypeOptions, Record<string, string>> = {
   default: {
     "data-h2-background-color":
-      "base(transparent) base:hover(black.lightest) base:focus-visible(tm-yellow.light)",
+      "base(transparent) base:hover(black.lightest) base:focus-visible(focus.light)",
     "data-h2-color":
       "base:(inherit) base:hover(black.darker)  base:focus-visible(black)",
   },
   success: {
     "data-h2-background-color":
-      "base(transparent) base:hover(tm-green.lightest) base:focus-visible(tm-yellow.light)",
+      "base(transparent) base:hover(tm-green.lightest) base:focus-visible(focus.light)",
     "data-h2-color":
       "base:(inherit) base:hover(tm-green.darker)  base:focus-visible(black)",
   },
   warning: {
     "data-h2-background-color":
-      "base(transparent) base:hover(warning.lightest) base:focus-visible(tm-yellow.light)",
+      "base(transparent) base:hover(warning.lightest) base:focus-visible(focus.light)",
     "data-h2-color":
       "base:(inherit) base:hover(warning.darker)  base:focus-visible(black)",
   },
   info: {
     "data-h2-background-color":
-      "base(transparent) base:hover(secondary.lightest) base:focus-visible(tm-yellow.light)",
+      "base(transparent) base:hover(secondary.lightest) base:focus-visible(focus.light)",
     "data-h2-color":
       "base:(inherit) base:hover(secondary.darker)  base:focus-visible(black)",
   },
   error: {
     "data-h2-background-color":
-      "base(transparent) base:hover(tertiary.lightest) base:focus-visible(tm-yellow.light)",
+      "base(transparent) base:hover(tertiary.lightest) base:focus-visible(focus.light)",
     "data-h2-color":
       "base:(inherit) base:hover(tertiary.darker)  base:focus-visible(black)",
   },
