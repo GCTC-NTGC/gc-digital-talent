9a69e21369aafac54e25d3d0ce1bb32da916996e
Brinda Sasikumar <brinda.sasikumar@gmail.com>
2023-01-24 13:05:17 -0500

    Feature/5418 url redirect (#5433)
    * draft changes
    
    * fix localhost for the port
    
    * enforce relative path in redirect
    
    * add relative path in redirect
    
    * add test
    
    * code cleanup
    
    * address pr feedback

--- apps/e2e/cypress/e2e/talentsearch/redirects.cy.js
+++ apps/e2e/cypress/e2e/talentsearch/redirects.cy.js
@@ -27,4 +27,41 @@ describe("Redirects", () => {
     cy.wait("@gqlgetMeQuery");
     expectToBeOnProfile();
   });
+
+});
+
+describe("Verify search page redirect", () => {
+const oldEnglishUrl = "/en/talent/search";
+const newEnglishUrl = "http://localhost:8000/en/search";
+
+const oldFrenchUrl = "/fr/talent/search";
+const newFrenchUrl = "http://localhost:8000/fr/search";
+
+
+it("redirects to proper url", () => {
+  cy.visit(oldEnglishUrl);
+  cy.url().should("eq", newEnglishUrl);
+  cy.visit(oldFrenchUrl);
+  cy.url().should("eq", newFrenchUrl);
+});
+
+it("gives 301 response code while redirect", () => {
+  cy.request({
+    url: oldEnglishUrl,
+    followRedirect: false, // turn off following redirects
+  }).then((resp) => {
+    // redirect status code is 301
+    expect(resp.status).to.eq(301);
+    expect(resp.redirectedToUrl).to.eq(newEnglishUrl);
+  });
+  cy.request({
+    url: oldFrenchUrl,
+    followRedirect: false, // turn off following redirects
+  }).then((resp) => {
+    // redirect status code is 301
+    expect(resp.status).to.eq(301);
+    expect(resp.redirectedToUrl).to.eq(newFrenchUrl);
+  });
+});
+
 });
