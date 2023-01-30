7680f533f1b3ea7ccc4ac0adb758184765771c2c
Eric Sizer <eric.sizer@gmail.com>
2023-01-30 14:24:13 -0500

    add common stories back

--- packages/storybook-helpers/main.js
+++ packages/storybook-helpers/main.js
@@ -29,7 +29,7 @@ module.exports = (basePath) => {
 
     "stories": [
       "../src/**/*.stories.@(js|jsx|ts|tsx|mdx)",
-      //"../../frontend/common/src/**/*.stories.@(js|jsx|ts|tsx|mdx)"
+      "../../../frontend/common/src/**/*.stories.@(js|jsx|ts|tsx|mdx)"
     ],
     "addons": [
       "@storybook/addon-links",
