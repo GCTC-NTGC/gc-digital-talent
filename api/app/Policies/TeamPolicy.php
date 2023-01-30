d14c2f650270ba01eea58ff3c036c62cd5df9a49
vd1992 <40485260+vd1992@users.noreply.github.com>
2023-01-30 16:32:18 -0700

    query and add policy

---
+++ api/app/Policies/TeamPolicy.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Team;
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class TeamPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view any models.
+     *
+     * @param  \App\Models\User|null  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function viewAny(User $user = null)
+    {
+        return $user->isAdmin();
+    }
+}
