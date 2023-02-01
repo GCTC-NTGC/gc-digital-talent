86c052caf3b210ed1e0647c328b7d6cd5b4ce45f
Eric Sizer <eric.sizer@gmail.com>
2023-02-01 12:00:37 -0500

    fix nested accordion styles

--- frontend/common/src/components/Accordion/styles.ts
+++ frontend/common/src/components/Accordion/styles.ts
@@ -5,35 +5,37 @@ type AccordionStyles = Record<string, string | undefined>;
 const cardStyles: AccordionStyles = {
   "data-h2-background-color": "base(white)",
   "data-h2-color":
-    "base:children[.Accordion__Separator](dt-gray) base:children[.Accordion__Chevron](dt-gray)",
+    "base:selectors[>.Accordion__Item .Accordion__Separator](dt-gray) base:selectors[>.Accordion__Item .Accordion__Chevron](dt-gray)",
   "data-h2-border-left": `
-  base:children[.Accordion__Item](x.5 solid tm-blue)
-  base:children[.Accordion__Item[data-state='open']](x.5 solid tm-purple)
+  base:selectors[>.Accordion__Item](x.5 solid tm-blue)
+  base:selectors[>.Accordion__Item[data-state='open']](x.5 solid tm-purple)
   `,
-  "data-h2-height": "base:children[.Accordion__Separator](1px)",
+  "data-h2-height":
+    "base:selectors[>.Accordion__Item .Accordion__Separator](1px)",
   "data-h2-margin": `
-    base:children[.Accordion__Item](x.5, 0)
-    base:children[.Accordion__Separator](-x.25 0 x.5 0)
+    base:selectors[>.Accordion__Item](x.5, 0)
+    base:selectors[>.Accordion__Item .Accordion__Separator](-x.25 0 x.5 0)
   `,
   "data-h2-padding": `
-      base:children[.Accordion__Item](x1 x2 0 x1)
-      base:children[.Accordion__Content](0 0 x1 x2)
+      base:selectors[>.Accordion__Item](x1 x2 0 x1)
+      base:selectors[>.Accordion__Item .Accordion__Content](0 0 x1 x2)
   `,
   "data-h2-radius":
-    "base:children[.Accordion__Item](0px, rounded, rounded, 0px)",
-  "data-h2-shadow": "base:children[.Accordion__Item](l)",
+    "base:selectors[>.Accordion__Item](0px, rounded, rounded, 0px)",
+  "data-h2-shadow": "base:selectors[>.Accordion__Item](l)",
 };
 
 const simpleStyles: AccordionStyles = {
   "data-h2-color": `
-    base:children[.Accordion__Separator](tm-blue)
-    base:children[.Accordion__Chevron](tm-blue)
-    base:children[.Accordion__Subtitle](tm-blue)
+    base:selectors[>.Accordion__Item .Accordion__Separator](tm-blue)
+    base:selectors[>.Accordion__Item .Accordion__Chevron](tm-blue)
+    base:selectors[>.Accordion__Item .Accordion__Subtitle](tm-blue)
   `,
-  "data-h2-display": "base:children[.Accordion__Separator](none)",
+  "data-h2-display":
+    "base:selectors[>.Accordion__Item .Accordion__Separator](none)",
   "data-h2-padding": `
-    base:children[.Accordion__Item](x1 x2 0 x1)
-    base:children[.Accordion__Content](0 0 x1 x2)
+    base:selectors[>.Accordion__Item](x1 x2 0 x1)
+    base:selectors[>.Accordion__Item .Accordion__Content](0 0 x1 x2)
   `,
 };
 
