7c91921a26142eb0f6feede18e8215bbcff3bde7
Eric Sizer <eric.sizer@gmail.com>
2023-02-03 14:37:59 -0500

    revert accidental commits

--- frontend/common/src/components/Link/ScrollToLink.tsx
+++ frontend/common/src/components/Link/ScrollToLink.tsx
@@ -1,52 +1,50 @@
 import React from "react";
-import { useLocation, Link, LinkProps } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
-const scrollToSection = (section: HTMLElement | null) => {
-  if (section) {
-    setTimeout(() => {
-      section.scrollIntoView({
-        behavior: "smooth",
-        block: "start",
-      });
-    }, 10);
-  }
-};
-
-interface ScrollToLinkProps extends Omit<LinkProps, "to"> {
+export interface ScrollToLinkProps
+  extends Omit<React.HTMLProps<HTMLAnchorElement>, "href" | "onClick"> {
   to: string;
+  children?: React.ReactNode;
 }
 
 const ScrollToLink = ({ to, children, ...rest }: ScrollToLinkProps) => {
+  const navigate = useNavigate();
   const { pathname, hash } = useLocation();
   const [targetSection, setTargetSection] = React.useState<HTMLElement | null>(
     null,
   );
 
-  React.useEffect(() => {
-    if (hash && hash === `#${to}`) {
-      scrollToSection(targetSection);
+  const scrollToSection = React.useCallback(() => {
+    if (targetSection) {
+      targetSection.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      });
     }
-  }, [pathname, hash, to, targetSection]);
+  }, [targetSection]);
 
   React.useEffect(() => {
-    const section = document.getElementById(to.toString());
+    if (hash && hash === `#${to}`) {
+      scrollToSection();
+    }
+  }, [pathname, hash, to, scrollToSection]);
+
+  React.useEffect(() => {
+    const section = document.getElementById(to);
     setTargetSection(section);
   }, [to]);
 
-  const handleClick = () => {
-    scrollToSection(targetSection);
+  const handleClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    e.preventDefault();
+    e.stopPropagation();
+    navigate(`#${to}`);
+    scrollToSection();
   };
 
   return (
-    <Link
-      to={{ hash: to }}
-      replace
-      preventScrollReset={false}
-      {...rest}
-      onClick={handleClick}
-    >
+    <a href={`#${to}`} onClick={handleClick} {...rest}>
       {children}
-    </Link>
+    </a>
   );
 };
 
--- frontend/common/src/components/Link/index.ts
+++ frontend/common/src/components/Link/index.ts
@@ -7,7 +7,14 @@ import type { ExternalLinkProps } from "./ExternalLink";
 import type { LinkProps } from "./Link";
 import type { IconLinkProps } from "./IconLink";
 import type { DownloadCsvProps } from "./DownloadCsv";
+import type { ScrollToLinkProps } from "./ScrollToLink";
 
 export default Link;
 export { DownloadCsv, ExternalLink, IconLink, ScrollToLink };
-export type { DownloadCsvProps, ExternalLinkProps, LinkProps, IconLinkProps };
+export type {
+  DownloadCsvProps,
+  ExternalLinkProps,
+  LinkProps,
+  IconLinkProps,
+  ScrollToLinkProps,
+};
