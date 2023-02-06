1d46296872febac7bbe4df61e0b234e3df0b66d3
Eric Sizer <eric.sizer@gmail.com>
2023-02-06 12:23:11 -0500

    remove useLocation import

--- apps/web/src/pages/Auth/CreateAccountPage/CreateAccountPage.tsx
+++ apps/web/src/pages/Auth/CreateAccountPage/CreateAccountPage.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { useLocation, useNavigate, useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import { useIntl } from "react-intl";
 
 import Hero from "@common/components/Hero/Hero";
