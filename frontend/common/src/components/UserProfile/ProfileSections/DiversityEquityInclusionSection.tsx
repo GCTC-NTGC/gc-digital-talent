6f75e49821f71c738446b636e87926b9967cb98f
Eric Sizer <eric.sizer@gmail.com>
2023-02-08 11:22:55 -0500

    fix indigenous community icon layout

--- frontend/common/src/components/UserProfile/ProfileSections/DiversityEquityInclusionSection.tsx
+++ frontend/common/src/components/UserProfile/ProfileSections/DiversityEquityInclusionSection.tsx
@@ -140,7 +140,8 @@ const DiversityEquityInclusionSection: React.FunctionComponent<{
                 <div
                   data-h2-display="base(flex)"
                   data-h2-justify-content="base(center)"
-                  data-h2-flex-wrap="base(nowrap)"
+                  data-h2-flex-wrap="base(wrap)"
+                  data-h2-gap="base(x.25)"
                 >
                   {nonLegacyIndigenousCommunities.map((community) => {
                     switch (community) {
