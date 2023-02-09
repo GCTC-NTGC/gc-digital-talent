34c99c2e2cbacb76ff9812eb0f0e5663bfbb5c78
Eric Sizer <eric.sizer@gmail.com>
2023-02-09 16:56:30 -0500

    create app insights package

---
+++ packages/app-insights/src/index.tsx
@@ -0,0 +1,9 @@
+import { AppInsightsContext, AppInsightsProvider } from "./components/Provider";
+
+import useAppInsightsContext from "./hooks/useAppInsightsContext";
+import useAppInsightsCustomEvent from "./hooks/useAppInsightsCustomEvent";
+import useLogger from "./hooks/useLogger";
+
+export { AppInsightsContext, AppInsightsProvider };
+
+export { useAppInsightsContext, useAppInsightsCustomEvent, useLogger };
---
+++ packages/app-insights/src/utils/reactPlugin.ts
@@ -0,0 +1,34 @@
+import { ApplicationInsights } from "@microsoft/applicationinsights-web";
+import {
+  ReactPlugin,
+  withAITracking,
+} from "@microsoft/applicationinsights-react-js";
+
+import { getRuntimeVariable } from "@gc-digital-talent/runtime";
+import { ComponentType } from "react";
+
+const aiConnectionString = getRuntimeVariable(
+  "APPLICATIONINSIGHTS_CONNECTION_STRING",
+);
+
+const reactPlugin = new ReactPlugin();
+
+const ai = new ApplicationInsights({
+  config: {
+    extensions: [reactPlugin],
+    enableAutoRouteTracking: true,
+    autoTrackPageVisitTime: true,
+    disableFetchTracking: false,
+    connectionString: aiConnectionString,
+  },
+});
+
+if (aiConnectionString) {
+  ai.loadAppInsights();
+}
+
+const { appInsights } = ai;
+export { reactPlugin, appInsights };
+
+export default (Component: ComponentType<unknown>) =>
+  withAITracking(reactPlugin, Component);
