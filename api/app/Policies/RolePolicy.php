f5002183fefa5204d403fe97bc9c41064903c6fb
vd1992 <40485260+vd1992@users.noreply.github.com>
2023-02-10 10:32:34 -0700

    role policy

---
+++ api/app/Policies/RolePolicy.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class RolePolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view any models.
+     *
+     * @param  \App\Models\User|null  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function viewAny(User $user = null)
+    {
+        if ($user) {
+            return $user->isAdmin();
+        }
+        return false;
+    }
+}
