15216eb655ec74fa707041d9c3768c7a8a58b82f
vd1992 <40485260+vd1992@users.noreply.github.com>
2023-02-14 15:14:48 -0700

    fill in ViewTeam

--- apps/web/src/pages/Teams/ViewTeamPage/ViewTeamPage.tsx
+++ apps/web/src/pages/Teams/ViewTeamPage/ViewTeamPage.tsx
@@ -1,31 +1,30 @@
 import * as React from "react";
 import { useIntl } from "react-intl";
 import { BuildingOffice2Icon } from "@heroicons/react/24/outline";
-
+import { Scalars, useViewTeamQuery } from "~/api/generated";
+import { useParams } from "react-router-dom";
 import PageHeader from "@common/components/PageHeader";
 import SEO from "@common/components/SEO/SEO";
-import { Scalars, useViewTeamQuery } from "~/api/generated";
-
 import { commonMessages } from "@common/messages";
 import Pending from "@common/components/Pending";
 import NotFound from "@common/components/NotFound";
-import { useParams } from "react-router-dom";
+import Link from "@common/components/Link";
+import useRoutes from "~/hooks/useRoutes";
 import { ViewTeam } from "./components/ViewTeam";
 
 type RouteParams = {
   teamId: Scalars["ID"];
 };
 
-export const ViewTeamApi = () => {
+interface ViewTeamApiProps {
+  pageTitle: string;
+}
+
+export const ViewTeamApi = ({ pageTitle }: ViewTeamApiProps) => {
   const intl = useIntl();
   const { teamId } = useParams<RouteParams>();
   const [{ data, fetching, error }] = useViewTeamQuery({
     variables: { id: teamId || "" },
-  });
-  const pageTitle = intl.formatMessage({
-    defaultMessage: "Team information",
-    id: "SXoCma",
-    description: "Page title for the view team page",
   });
 
   return (
@@ -34,7 +33,19 @@ export const ViewTeamApi = () => {
         <>
           <PageHeader icon={BuildingOffice2Icon}>{pageTitle}</PageHeader>
           <p>{data.team.displayName?.en}</p>
+          <hr
+            data-h2-margin="base(x2, 0, 0, 0)"
+            data-h2-height="base(1px)"
+            data-h2-background-color="base(dt-gray)"
+            data-h2-border="base(none)"
+          />
           <ViewTeam team={data.team} />
+          <hr
+            data-h2-margin="base(x2, 0, 0, 0)"
+            data-h2-height="base(1px)"
+            data-h2-background-color="base(dt-gray)"
+            data-h2-border="base(none)"
+          />
         </>
       ) : (
         <NotFound headingMessage={intl.formatMessage(commonMessages.notFound)}>
@@ -56,6 +67,7 @@ export const ViewTeamApi = () => {
 
 const ViewTeamPage = () => {
   const intl = useIntl();
+  const paths = useRoutes();
 
   const pageTitle = intl.formatMessage({
     defaultMessage: "Team information",
@@ -66,7 +78,22 @@ const ViewTeamPage = () => {
   return (
     <>
       <SEO title={pageTitle} />
-      <ViewTeamApi />
+      <ViewTeamApi pageTitle={pageTitle} />
+      <p data-h2-margin="base(x2, 0, 0, 0)">
+        <Link
+          type="button"
+          mode="solid"
+          color="secondary"
+          href={paths.teamTable()}
+        >
+          {intl.formatMessage({
+            defaultMessage: "Back to teams",
+            id: "ZxENUS",
+            description:
+              "Link text for button to go back to the teams table page",
+          })}
+        </Link>
+      </p>
     </>
   );
 };
--- apps/web/src/pages/Teams/ViewTeamPage/components/ViewTeam.tsx
+++ apps/web/src/pages/Teams/ViewTeamPage/components/ViewTeam.tsx
@@ -1,6 +1,10 @@
 import React from "react";
-import { IntlShape, useIntl } from "react-intl";
+import { FormProvider, useForm } from "react-hook-form";
+import { useIntl } from "react-intl";
 import { Team } from "~/api/generated";
+import { Input, TextArea } from "@common/components/form";
+import Pill from "@common/components/Pill/Pill";
+import useLocale from "~/../../../frontend/common/src/hooks/useLocale";
 
 interface ViewTeamProps {
   team: Team;
@@ -8,8 +12,134 @@ interface ViewTeamProps {
 
 export const ViewTeam = ({ team }: ViewTeamProps) => {
   const intl = useIntl();
+  const { locale } = useLocale();
+  const form = useForm();
 
-  return <p />;
+  const departmentsPillsArray =
+    team?.departments && team.departments.length > 0
+      ? team.departments.map((department) => {
+          return (
+            <Pill color="primary" mode="outline" key={department?.id}>
+              {department?.name?.[locale]}
+            </Pill>
+          );
+        })
+      : null;
+
+  return (
+    <FormProvider {...form}>
+      <h2 data-h2-margin="base(x2, 0, 0, 0)" data-h2-font-size="base(h3)">
+        {intl.formatMessage({
+          defaultMessage: "Basic information",
+          id: "Rg0SpL",
+          description: "Basic information",
+        })}
+      </h2>
+      <div data-h2-flex-grid="base(flex-start, x1, 0)">
+        <div data-h2-flex-item="base(1of1) p-tablet(1of2)">
+          <Input
+            id="displayNameEnglish"
+            name="displayNameEnglish"
+            type="text"
+            readOnly
+            hideOptional
+            value={team.displayName?.en ?? ""}
+            label={intl.formatMessage({
+              defaultMessage: "Organization's name (English)",
+              id: "DziQ2u",
+              description:
+                "Label displayed on the organization name in English field.",
+            })}
+          />
+        </div>
+        <div data-h2-flex-item="base(1of1) p-tablet(1of2)">
+          <Input
+            id="displayNameFrench"
+            name="displayNameFrench"
+            type="text"
+            readOnly
+            hideOptional
+            value={team.displayName?.fr ?? ""}
+            label={intl.formatMessage({
+              defaultMessage: "Organization's name (French)",
+              id: "oTWaul",
+              description:
+                "Label displayed on the organization name in French field.",
+            })}
+          />
+        </div>
+        <div data-h2-flex-item="base(1of1)">
+          <p data-h2-margin-bottom="base(x.25)">
+            {intl.formatMessage({
+              defaultMessage: "Departments",
+              id: "457hEW",
+              description:
+                "Heading displayed above the Department Table component.",
+            })}
+          </p>
+          {departmentsPillsArray}
+        </div>
+        <div data-h2-flex-item="base(1of1) p-tablet(1of2)">
+          <Input
+            id="key"
+            name="key"
+            type="text"
+            readOnly
+            hideOptional
+            value={team.name}
+            label={intl.formatMessage({
+              defaultMessage: "Key",
+              id: "i8zhiL",
+              description: "Key",
+            })}
+          />
+        </div>
+        <div data-h2-flex-item="base(1of1) p-tablet(1of2)">
+          <Input
+            id="contactEmail"
+            name="contactEmail"
+            type="text"
+            readOnly
+            hideOptional
+            value={team.contactEmail ?? ""}
+            label={intl.formatMessage({
+              defaultMessage: "Contact email",
+              id: "nGNj5Q",
+              description: "Contact email",
+            })}
+          />
+        </div>
+        <div data-h2-flex-item="base(1of1) p-tablet(1of2)">
+          <TextArea
+            id="teamDescriptionEnglish"
+            name="teamDescriptionEnglish"
+            readOnly
+            hideOptional
+            value={team.description?.en ?? ""}
+            label={intl.formatMessage({
+              defaultMessage: "Team's short description (English)",
+              id: "whsH/g",
+              description: "Short description for a team in English",
+            })}
+          />
+        </div>
+        <div data-h2-flex-item="base(1of1) p-tablet(1of2)">
+          <TextArea
+            id="teamDescriptionFrench"
+            name="teamDescriptionFrench"
+            readOnly
+            hideOptional
+            value={team.description?.fr ?? ""}
+            label={intl.formatMessage({
+              defaultMessage: "Team's short description (French)",
+              id: "PtbLq+",
+              description: "Short description for a team in French",
+            })}
+          />
+        </div>
+      </div>
+    </FormProvider>
+  );
 };
 
 export default ViewTeam;
