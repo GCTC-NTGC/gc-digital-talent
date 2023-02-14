c277ec2c7bfe3f0982545ce9d8a1a514e923afa7
vd1992 <40485260+vd1992@users.noreply.github.com>
2023-02-14 15:37:26 -0700

    Heading component localized

--- apps/web/src/pages/Teams/ViewTeamPage/ViewTeamPage.tsx
+++ apps/web/src/pages/Teams/ViewTeamPage/ViewTeamPage.tsx
@@ -10,6 +10,8 @@ import Pending from "@common/components/Pending";
 import NotFound from "@common/components/NotFound";
 import Link from "@common/components/Link";
 import useRoutes from "~/hooks/useRoutes";
+import Heading from "@common/components/Heading";
+import useLocale from "@common/hooks/useLocale";
 import { ViewTeam } from "./components/ViewTeam";
 
 type RouteParams = {
@@ -22,6 +24,7 @@ interface ViewTeamApiProps {
 
 export const ViewTeamApi = ({ pageTitle }: ViewTeamApiProps) => {
   const intl = useIntl();
+  const { locale } = useLocale();
   const { teamId } = useParams<RouteParams>();
   const [{ data, fetching, error }] = useViewTeamQuery({
     variables: { id: teamId || "" },
@@ -32,7 +35,9 @@ export const ViewTeamApi = ({ pageTitle }: ViewTeamApiProps) => {
       {data?.team ? (
         <>
           <PageHeader icon={BuildingOffice2Icon}>{pageTitle}</PageHeader>
-          <p>{data.team.displayName?.en}</p>
+          <Heading size="h4" level="h4" data-h2-margin-left="base(x3)">
+            {data.team.displayName?.[locale]}
+          </Heading>
           <hr
             data-h2-margin="base(x2, 0, 0, 0)"
             data-h2-height="base(1px)"
