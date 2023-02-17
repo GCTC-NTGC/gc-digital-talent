61663928fde578d07837a97da70eef8559a8264c
Eric Sizer <eric.sizer@gmail.com>
2023-02-17 11:48:26 -0500

    add update team form

--- apps/web/src/pages/Teams/UpdateTeamPage/UpdateTeamPage.tsx
+++ apps/web/src/pages/Teams/UpdateTeamPage/UpdateTeamPage.tsx
@@ -5,8 +5,41 @@ import { BuildingOffice2Icon } from "@heroicons/react/24/outline";
 import PageHeader from "@common/components/PageHeader";
 import SEO from "@common/components/SEO/SEO";
 
+import {
+  Scalars,
+  UpdateTeamInput,
+  useDepartmentsQuery,
+  useGetTeamQuery,
+  useUpdateTeamMutation,
+} from "~/api/generated";
+import { useParams } from "react-router-dom";
+import Pending from "~/../../../frontend/common/src/components/Pending";
+import NotFound from "~/../../../frontend/common/src/components/NotFound";
+import { commonMessages } from "~/../../../frontend/common/src/messages";
+import { notEmpty } from "~/../../../frontend/common/src/helpers/util";
+import { getLocalizedName } from "~/../../../frontend/common/src/helpers/localize";
+import UpdateTeamForm from "./components/UpdateTeamForm";
+
 const EditTeamPage = () => {
   const intl = useIntl();
+  const { teamId } = useParams();
+  const [{ data: teamData, fetching: teamFetching, error: teamError }] =
+    useGetTeamQuery({
+      variables: {
+        teamId: teamId || "",
+      },
+    });
+  const [
+    {
+      data: departmentsData,
+      fetching: departmentsFetching,
+      error: departmentsError,
+    },
+  ] = useDepartmentsQuery();
+  const [, executeMutation] = useUpdateTeamMutation();
+
+  const departments = departmentsData?.departments.filter(notEmpty);
+  const team = teamData?.team;
 
   const pageTitle = intl.formatMessage({
     defaultMessage: "Edit team information",
@@ -14,11 +47,53 @@ const EditTeamPage = () => {
     description: "Page title for the edit team page",
   });
 
+  const handleSubmit = async (id: Scalars["UUID"], data: UpdateTeamInput) => {
+    return executeMutation({
+      teamId: id,
+      team: data,
+    }).then((result) => {
+      if (result.data?.updateTeam) {
+        return Promise.resolve(result.data?.updateTeam);
+      }
+      return Promise.reject(result.error);
+    });
+  };
+
   return (
-    <>
-      <SEO title={pageTitle} />
-      <PageHeader icon={BuildingOffice2Icon}>{pageTitle}</PageHeader>
-    </>
+    <Pending
+      fetching={teamFetching || departmentsFetching}
+      error={teamError || departmentsError}
+    >
+      {team ? (
+        <>
+          <SEO title={pageTitle} />
+          <PageHeader
+            icon={BuildingOffice2Icon}
+            subtitle={getLocalizedName(team.displayName, intl)}
+          >
+            {pageTitle}
+          </PageHeader>
+          <UpdateTeamForm
+            team={team}
+            departments={departments}
+            onSubmit={handleSubmit}
+          />
+        </>
+      ) : (
+        <NotFound headingMessage={intl.formatMessage(commonMessages.notFound)}>
+          <p>
+            {intl.formatMessage(
+              {
+                defaultMessage: "Team {teamId} not found.",
+                id: "VJYI6K",
+                description: "Message displayed for team not found.",
+              },
+              { teamId },
+            )}
+          </p>
+        </NotFound>
+      )}
+    </Pending>
   );
 };
 
---
+++ apps/web/src/pages/Teams/UpdateTeamPage/components/NameField.tsx
@@ -0,0 +1,75 @@
+import React from "react";
+import { useIntl } from "react-intl";
+import { useFormContext } from "react-hook-form";
+
+import Input from "@common/components/form/Input";
+import { kebabCase } from "lodash";
+import { errorMessages } from "~/../../../frontend/common/src/messages";
+
+const NameField = () => {
+  const intl = useIntl();
+  const { setValue, getValues } = useFormContext();
+
+  const handleBlur = (e: React.FocusEvent<HTMLInputElement>) => {
+    const { value: newValue } = e.target;
+    const value = getValues("name");
+    if (!value) {
+      setValue("name", kebabCase(newValue));
+    }
+  };
+
+  return (
+    <>
+      <div data-h2-flex-item="base(1/2)">
+        <Input
+          type="text"
+          id="displayName_en"
+          name="displayName.en"
+          onBlur={handleBlur}
+          label={intl.formatMessage({
+            defaultMessage: "Organization's name (English)",
+            id: "1cPlHY",
+            description: "Label for the English team display name input",
+          })}
+          rules={{
+            required: intl.formatMessage(errorMessages.required),
+          }}
+        />
+      </div>
+      <div data-h2-flex-item="base(1/2)">
+        <Input
+          type="text"
+          id="displayName_fr"
+          name="displayName.fr"
+          onBlur={handleBlur}
+          label={intl.formatMessage({
+            defaultMessage: "Organization's name (French)",
+            id: "OOnxtm",
+            description: "Label for the French team display name input",
+          })}
+          rules={{
+            required: intl.formatMessage(errorMessages.required),
+          }}
+        />
+      </div>
+      <div data-h2-flex-item="base(1/2)">
+        <Input
+          type="text"
+          id="name"
+          name="name"
+          label={intl.formatMessage({
+            defaultMessage: "Key",
+            id: "3sw5qd",
+            description: "Label for the team's key input",
+          })}
+          rules={{
+            required: intl.formatMessage(errorMessages.required),
+          }}
+        />
+      </div>
+      <div data-h2-flex-item="base(1/2)" />
+    </>
+  );
+};
+
+export default NameField;
