36bd42cb14a80c16b181644a2f387728fb4bb66f
JamesHuf <jamiehufnagel@hotmail.com>
2023-02-20 17:01:57 -0800

    Main form setup and working

--- apps/web/src/pages/Teams/CreateTeamPage/CreateTeamPage.tsx
+++ apps/web/src/pages/Teams/CreateTeamPage/CreateTeamPage.tsx
@@ -1,22 +1,33 @@
 import * as React from "react";
 import { useIntl } from "react-intl";
 import { BuildingOffice2Icon } from "@heroicons/react/24/outline";
-import { SubmitHandler } from "react-hook-form";
 
 import PageHeader from "@common/components/PageHeader";
+import Pending from "@common/components/Pending";
 import SEO from "@common/components/SEO/SEO";
-import { BasicForm, Input } from "@common/components/form";
-import { Button, Link } from "@common/components";
-import MultiSelectField from "@common/components/form/MultiSelect/MultiSelectField";
-import useRoutes from "~/hooks/useRoutes";
+import { notEmpty } from "@common/helpers/util";
+
+import {
+  CreateTeamInput,
+  useDepartmentsQuery,
+  useCreateTeamMutation,
+} from "~/api/generated";
+
+import CreateTeamForm from "./components/CreateTeamForm";
 
 const CreateTeamPage = () => {
   const intl = useIntl();
-  const paths = useRoutes();
 
-  type FormValues = {
-    classification: string[];
-  };
+  const [
+    {
+      data: departmentsData,
+      fetching: departmentsFetching,
+      error: departmentsError,
+    },
+  ] = useDepartmentsQuery();
+  const [, executeMutation] = useCreateTeamMutation();
+
+  const departments = departmentsData?.departments.filter(notEmpty);
 
   const pageTitle = intl.formatMessage({
     defaultMessage: "Create a new team",
@@ -24,14 +35,19 @@ const CreateTeamPage = () => {
     description: "Page title for the create team page",
   });
 
-  const handleSubmit: SubmitHandler<FormValues> = async (data: FormValues) => {
-    // TODO: Handle submitting form
-    console.log("Form submitted");
-    console.log(data);
+  const handleSubmit = async (data: CreateTeamInput) => {
+    return executeMutation({
+      team: data,
+    }).then((result) => {
+      if (result.data?.createTeam) {
+        return Promise.resolve(result.data?.createTeam);
+      }
+      return Promise.reject(result.error);
+    });
   };
 
   return (
-    <>
+    <Pending fetching={departmentsFetching} error={departmentsError}>
       <SEO title={pageTitle} />
       <PageHeader icon={BuildingOffice2Icon}>{pageTitle}</PageHeader>
       <p>
@@ -48,89 +64,8 @@ const CreateTeamPage = () => {
         data-h2-background-color="base(dt-gray)"
         data-h2-border="base(none)"
       />
-      <BasicForm onSubmit={handleSubmit}>
-        <div data-h2-flex-grid="base(flex-start, x1, 0)">
-          <div data-h2-flex-item="base(1of1) p-tablet(1of2)">
-            <Input
-              id="displayNameEnglish"
-              name="displayNameEnglish"
-              type="text"
-              rules={{ required: true }}
-              label={intl.formatMessage({
-                defaultMessage: "Organization's name (English)",
-                id: "QC23B1",
-                description: "Name of an organization/team in English field.",
-              })}
-            />
-          </div>
-          <div data-h2-flex-item="base(1of1) p-tablet(1of2)">
-            <Input
-              id="displayNameFrench"
-              name="displayNameFrench"
-              type="text"
-              rules={{ required: true }}
-              label={intl.formatMessage({
-                defaultMessage: "Organization's name (French)",
-                id: "W0BVd+",
-                description: "Name of an organization/team in French field.",
-              })}
-            />
-          </div>
-          <div data-h2-flex-item="base(1of1) p-tablet(1of2)">
-            <Input
-              id="key"
-              name="key"
-              type="text"
-              rules={{ required: true }}
-              label={intl.formatMessage({
-                defaultMessage: "Key",
-                id: "i8zhiL",
-                description: "Key",
-              })}
-            />
-          </div>
-          <div
-            data-h2-flex-item="base(1of1) p-tablet(1of2)"
-            data-h2-padding="base:(x1, 0, 0, 0)"
-          >
-            <MultiSelectField
-              id="departments"
-              name="departments"
-              rules={{ required: true }}
-              label={intl.formatMessage({
-                defaultMessage: "Contact email",
-                id: "nGNj5Q",
-                description: "Contact email",
-              })}
-            />
-          </div>
-        </div>
-        <div
-          data-h2-align-items="base(center)"
-          data-h2-display="base(flex)"
-          style={{ gap: "0 1.5rem" }}
-        >
-          <Link href={paths.teamTable()}>
-            {intl.formatMessage({
-              defaultMessage: "Cancel and go back to teams",
-              id: "/1s5pZ",
-              description: "Cancel button for the create team page in admin",
-            })}
-          </Link>
-          <Button
-            type="submit"
-            color="cta"
-            data-h2-text-decoration="base(underline)"
-          >
-            {intl.formatMessage({
-              defaultMessage: "Create new team",
-              id: "DYzX/8",
-              description: "Submit button for the create team page in admin",
-            })}
-          </Button>
-        </div>
-      </BasicForm>
-    </>
+      <CreateTeamForm departments={departments} onSubmit={handleSubmit} />
+    </Pending>
   );
 };
 
---
+++ apps/web/src/pages/Teams/CreateTeamPage/components/CreateTeamForm.tsx
@@ -0,0 +1,151 @@
+import * as React from "react";
+import { SubmitHandler } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import { useIntl } from "react-intl";
+
+import { Link } from "@common/components";
+import { BasicForm, Input, Submit } from "@common/components/form";
+import MultiSelectField from "@common/components/form/MultiSelect/MultiSelectField";
+import { toast } from "@common/components/Toast";
+import { errorMessages } from "@common/messages";
+import { notEmpty } from "@common/helpers/util";
+import { getLocalizedName } from "@common/helpers/localize";
+
+import {
+  CreateTeamInput,
+  CreateTeamMutation,
+  Department,
+  LocalizedStringInput,
+  Maybe,
+  Scalars,
+} from "~/api/generated";
+import useRoutes from "~/hooks/useRoutes";
+
+import NameField from "./NameField";
+
+type FormValues = {
+  name: string;
+  displayName?: Maybe<LocalizedStringInput>;
+  contactEmail?: Maybe<Scalars["Email"]>;
+  departments?: Array<Scalars["UUID"]>;
+};
+
+const formValuesToSubmitData = (data: FormValues): CreateTeamInput => {
+  const { departments, ...rest } = data;
+  return {
+    ...rest,
+    departments: { sync: departments },
+  };
+};
+
+interface CreateTeamFormProps {
+  departments?: Maybe<Array<Maybe<Omit<Department, "teams">>>>;
+  onSubmit: (
+    data: CreateTeamInput,
+  ) => Promise<CreateTeamMutation["createTeam"]>;
+}
+
+const CreateTeamForm = ({ departments, onSubmit }: CreateTeamFormProps) => {
+  const intl = useIntl();
+  const paths = useRoutes();
+  const navigate = useNavigate();
+
+  const handleSubmit: SubmitHandler<FormValues> = async (data) => {
+    return onSubmit(formValuesToSubmitData(data))
+      .then(() => {
+        navigate(paths.teamTable());
+        toast.success(
+          intl.formatMessage({
+            defaultMessage: "Team updated successfully!",
+            id: "2ni0ia",
+            description: "Message displayed after a team is updated",
+          }),
+        );
+      })
+      .catch(() => {
+        toast.error(
+          intl.formatMessage({
+            defaultMessage: "Error: updating team failed",
+            id: "h486wf",
+            description: "Messaged displayed after updating team fails",
+          }),
+        );
+      });
+  };
+
+  const departmentOptions = departments?.filter(notEmpty).map((department) => ({
+    value: department.id,
+    label: getLocalizedName(department.name, intl),
+  }));
+
+  return (
+    <BasicForm onSubmit={handleSubmit}>
+      <div data-h2-flex-grid="base(center, x1, 0)">
+        <NameField />
+        <div data-h2-flex-item="base(1/2)">
+          <MultiSelectField
+            id="departments"
+            name="departments"
+            label={intl.formatMessage({
+              defaultMessage: "Departments",
+              id: "ytHvhD",
+              description: "Label for the team departments input",
+            })}
+            placeholder={intl.formatMessage({
+              defaultMessage: "Select on or more departments...",
+              id: "jzax3k",
+              description: "Placeholder text for the team departments input",
+            })}
+            rules={{
+              required: intl.formatMessage(errorMessages.required),
+            }}
+            options={departmentOptions}
+          />
+        </div>
+        <div data-h2-flex-item="base(1/2)">
+          <Input
+            type="email"
+            id="contactEmail"
+            name="contactEmail"
+            label={intl.formatMessage({
+              defaultMessage: "Contact email",
+              id: "PhrOLp",
+              description: "Label for the French team display name input",
+            })}
+            placeholder={intl.formatMessage({
+              defaultMessage: "contact@email.com",
+              id: "hTzoAW",
+              description:
+                "Placeholder email format example for team contact email input",
+            })}
+            rules={{
+              required: intl.formatMessage(errorMessages.required),
+            }}
+          />
+        </div>
+      </div>
+      <div
+        data-h2-display="base(flex)"
+        data-h2-gap="base(x1)"
+        data-h2-align-items="base(center)"
+      >
+        <Link href={paths.teamTable()}>
+          {intl.formatMessage({
+            defaultMessage: "Cancel and go back to teams",
+            id: "i0IT1I",
+            description: "Link text to cancel updating a team",
+          })}
+        </Link>
+        <Submit
+          text={intl.formatMessage({
+            defaultMessage: "Create new team",
+            id: "WX6NnA",
+            description: "Button text for the create team form submit button",
+          })}
+        />
+      </div>
+    </BasicForm>
+  );
+};
+
+export default CreateTeamForm;
