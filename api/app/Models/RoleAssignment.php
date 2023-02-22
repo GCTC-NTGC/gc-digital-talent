3f439fda61d8466524f5a8de48d7bd205f33f0d7
Peter Giles <8978655+petertgiles@users.noreply.github.com>
2023-02-22 09:50:01 -0500

    Add missing type hint

--- api/app/Models/RoleAssignment.php
+++ api/app/Models/RoleAssignment.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
 
 /**
  * Class User
@@ -34,7 +35,7 @@ class RoleAssignment extends Model
         return $this->belongsTo(Team::class);
     }
 
-    public function user()
+    public function user(): MorphTo
     {
         return $this->morphTo('user');
     }
