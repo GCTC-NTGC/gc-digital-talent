48f51c8bf5d5d65fcfff42c2c1db6972d70ad253
Eric Sizer <eric.sizer@gmail.com>
2023-02-24 14:57:17 -0500

    clean up and add classification tests

--- api/tests/Feature/ClassificationTest.php
+++ api/tests/Feature/ClassificationTest.php
@@ -9,7 +9,6 @@ use Nuwave\Lighthouse\Testing\MakesGraphQLRequests;
 use Tests\TestCase;
 use App\Models\Classification;
 use App\Models\User;
-use App\Models\Role;
 use Database\Seeders\RolePermissionSeeder;
 
 class ClassificationTest extends TestCase
@@ -20,7 +19,9 @@ class ClassificationTest extends TestCase
     use WithFaker;
 
     protected $baseUser;
+    protected $adminUser;
     protected $uuid;
+    protected $toBeDeletedUUID;
 
     protected function setUp(): void
     {
@@ -36,11 +37,21 @@ class ClassificationTest extends TestCase
         ]);
         $this->baseUser->attachRole("base_user");
 
+        $this->adminUser = User::create([
+            'email' => 'admin-user@test.com',
+            'sub' => 'admin-user@test.com',
+        ]);
+        $this->adminUser->attachRole("platform_admin");
 
         $this->uuid = $this->faker->UUID();
+        $this->toBeDeletedUUID = $this->faker->UUID();
 
         Classification::factory()->create([
             'id' => $this->uuid
+        ]);
+
+        Classification::factory()->create([
+            'id' => $this->toBeDeletedUUID
         ]);
     }
 
@@ -63,11 +74,129 @@ class ClassificationTest extends TestCase
      */
     public function test_view_classification()
     {
+
+        $variables = [ 'id' => $this->uuid ];
+
+        $query = /** @lang GraphQL */
+        '
+            query GetClassification($id: UUID!) {
+                classification(id: $id) {
+                    id
+                }
+            }
+        ';
+
         $this->actingAs($this->baseUser, 'api')
-            ->graphQL(
-                'query GetClassification($id: UUID!){ classification(id: $id) { id } }',
-                [ 'id' => $this->uuid ]
-            )
-            ->assertJsonFragment([ 'id' => $this->uuid ]);
+            ->graphQL($query, $variables)
+            ->assertJsonFragment($variables);
+    }
+
+    /**
+     * Test updating a classification
+     *
+     * @return void
+     */
+    public function test_update_classification()
+    {
+
+        $variables = [
+            'id' => $this->uuid,
+            'classification' => [
+                'name' => [
+                    'en' => 'New Name (EN)',
+                    'fr' => 'New Name (FR)'
+                ]
+            ]
+        ];
+
+        $mutation = /** @lang GraphQL */
+        '
+            mutation UpdateClassification($id: ID!, $classification: UpdateClassificationInput!) {
+                updateClassification(id: $id, classification: $classification) {
+                    id
+                    name {
+                        en
+                        fr
+                    }
+                }
+            }
+        ';
+
+        $this->actingAs($this->baseUser, 'api')
+            ->graphQL($mutation, $variables )
+            ->assertGraphQLErrorMessage('This action is unauthorized.');
+
+        $this->actingAs($this->adminUser, 'api')
+            ->graphQL($mutation, $variables )
+            ->assertJsonFragment([ 'id' => $this->uuid, 'name' => $variables['classification']['name'] ]);
+    }
+
+    /**
+     * Test creation of classification
+     *
+     * @return void
+     */
+    public function test_create_classification()
+    {
+        $variables = [
+            'classification' => [
+                'name' => [
+                    'en' => 'New Name (EN)',
+                    'fr' => 'New Name (FR)'
+                ],
+                'group' => 'IT',
+                'level' => 1,
+            ]
+        ];
+
+        $mutation = /** @lang GraphQL */
+        '
+            mutation Create($classification: CreateClassificationInput!) {
+                createClassification(classification: $classification) {
+                    id
+                    name {
+                        en
+                        fr
+                    }
+                }
+            }
+        ';
+
+        $this->actingAs($this->baseUser, 'api')
+            ->graphQL($mutation, $variables)
+            ->assertGraphQLErrorMessage('This action is unauthorized.');
+
+        $classification = $this->actingAs($this->adminUser, 'api')
+            ->graphQL($mutation, $variables)
+            ->assertJsonFragment(['name' => $variables['classification']['name'] ])
+            ->json('data.createClassification');
+
+    }
+
+    /**
+     * Test base user cannot update or delete
+     *
+     * @return void
+     */
+    public function test_delete_classification()
+    {
+        $variables = [ 'id' => $this->toBeDeletedUUID ];
+
+        $mutation = /** @lang GraphQL */
+        '
+            mutation Delete($id: ID!) {
+                deleteClassification(id: $id) {
+                    id
+                }
+            }
+        ';
+
+        $this->actingAs($this->baseUser, 'api')
+            ->graphQL($mutation, $variables)
+            ->assertGraphQLErrorMessage('This action is unauthorized.');
+
+        $this->actingAs($this->adminUser, 'api')
+            ->graphQL($mutation, $variables)
+            ->assertJsonFragment(['id' => $this->toBeDeletedUUID ]);
     }
 }
