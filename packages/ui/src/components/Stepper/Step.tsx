e5a6c2efdb0e46735fe645805f248574a2c70e04
Eric Sizer <eric.sizer@gmail.com>
2023-03-10 10:14:07 -0500

    merge in josh's styles

--- packages/ui/src/components/Stepper/Step.tsx
+++ packages/ui/src/components/Stepper/Step.tsx
@@ -60,38 +60,49 @@ const Step = ({
 
   return (
     <li>
-      <a
-        href="https://google.com"
-        title=""
+      <StepLink
+        href={href}
+        state={state}
+        preventDisable={preventDisable}
         data-h2-display="base(block)"
         data-h2-width="base(100%)"
         data-h2-padding-left="base(x1.5)"
         data-h2-position="base(relative)"
+        data-h2-outline="base(none)"
       >
-        <div
+        {message && (
+          <span data-h2-visually-hidden="base(hidden)">
+            {intl.formatMessage(message)}
+          </span>
+        )}
+        <span
           data-h2-position="base(absolute)"
           data-h2-location="base(0, auto, auto, 0)"
           data-h2-width="base(x1.5)"
           data-h2-height="base(100%)"
         >
-          <div
-            data-h2-position="base(absolute)"
-            data-h2-location="base(-x.1, auto, auto, 50%)"
-            data-h2-transform="base(translate(-50%, 0))"
-            data-h2-height="base(calc(100% + x.85))"
-            data-h2-width="base(x.15)"
-            data-h2-background="base(gray.light)"
-          />
-          <div
-            data-h2-position="base(absolute)"
-            data-h2-location="base(-x.1, auto, auto, 50%)"
-            data-h2-transform="base(translate(-50%, 0))"
-            data-h2-background="base(primary.light)"
-            data-h2-radius="base(circle)"
-            data-h2-height="base(x1.25)"
-            data-h2-width="base(x1.25)"
-          >
-            {Icon && (
+          {!last && (
+            <div
+              className="Step__Tail Step__Flair"
+              data-h2-position="base(absolute)"
+              data-h2-location="base(-x.1, auto, auto, 50%)"
+              data-h2-transform="base(translate(-50%, 0))"
+              data-h2-height="base(calc(100% + x.85))"
+              data-h2-width="base(x.15)"
+            />
+          )}
+
+          {Icon && (
+            <span
+              className="Step__Icon Step__Flair"
+              data-h2-position="base(absolute)"
+              data-h2-location="base(-x.1, auto, auto, 50%)"
+              data-h2-transform="base(translate(-50%, 0))"
+              data-h2-background="base(primary.light)"
+              data-h2-radius="base(circle)"
+              data-h2-height="base(x1.25)"
+              data-h2-width="base(x1.25)"
+            >
               <Icon
                 data-h2-position="base(absolute)"
                 data-h2-location="base(50%, auto, auto, 50%)"
@@ -99,65 +110,17 @@ const Step = ({
                 data-h2-height="base(x.65)"
                 data-h2-width="base(x.65)"
               />
-            )}
-          </div>
-        </div>
-        <div data-h2-margin-left="base(x.5)">
-          <p>{children}</p>
-        </div>
-      </a>
-      {/* <StepLink
-        href={href}
-        state={state}
-        preventDisable={preventDisable}
-        data-h2-align-items="base(flex-start)"
-        data-h2-display="base(flex)"
-        data-h2-gap="base(0, x.25)"
-        data-h2-justify-content="base(flex-start)"
-        data-h2-outline="base(none)"
-        data-h2-position="base(relative)"
-      >
-        {message && (
-          <span data-h2-visually-hidden="base(hidden)">
-            {intl.formatMessage(message)}
-          </span>
-        )}
-        {Icon && (
-          <span
-            className="Step__Icon Step__Flair"
-            data-h2-align-items="base(center)"
-            data-h2-display="base(flex)"
-            data-h2-flex-shrink="base(0)"
-            data-h2-justify-content="base(center)"
-            data-h2-height="base(x1.25)"
-            data-h2-width="base(x1.25)"
-            data-h2-position="base(relative)"
-            data-h2-radius="base(circle)"
-          >
-            <Icon data-h2-height="base(x.5)" data-h2-width="base(x.5)" />
-          </span>
-        )}
-        {!last && (
-          <span
-            className="Step__Tail Step__Flair"
-            data-h2-align-self="base(stretch)"
-            data-h2-display="base(block)"
-            data-h2-height="base(100%)"
-            data-h2-left="base(x.5)"
-            data-h2-position="base(absolute)"
-            data-h2-top="base(x1.25)"
-            data-h2-transform="base(translateX(50%))"
-            data-h2-width="base(x.15)"
-          />
-        )}
+            </span>
+          )}
+        </span>
         <span
           className="Step__Text"
-          data-h2-flex-item="base(content)"
-          data-h2-margin="base(x.1, 0, 0, 0)"
+          data-h2-display="base(inline-block)"
+          data-h2-margin-left="base(x.5)"
         >
           {children}
         </span>
-      </StepLink> */}
+      </StepLink>
     </li>
   );
 };
