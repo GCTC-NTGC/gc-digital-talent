f0c6179e1a8e0c767466fd1bbfa20d66b9c00e8b
vd1992 <40485260+vd1992@users.noreply.github.com>
2023-03-13 17:06:42 -0600

    backend suspended filter

--- api/app/Models/PoolCandidate.php
+++ api/app/Models/PoolCandidate.php
@@ -261,6 +261,20 @@ class PoolCandidate extends Model
         return $query;
     }
 
+    public static function scopeSuspendedStatus(Builder $query, ?string $suspendedStatus)
+    {
+        $suspendedStatus = isset($suspendedStatus) ? $suspendedStatus : ApiEnums::CANDIDATE_SUSPENDED_FILTER_ACTIVE;
+        if ($suspendedStatus == ApiEnums::CANDIDATE_SUSPENDED_FILTER_ACTIVE) {
+            $query->where(function ($query) {
+                $query->whereDate('suspended_at', '>=', Carbon::now())
+                    ->orWhereNull('suspended_at');
+            });
+        } else if ($suspendedStatus == ApiEnums::CANDIDATE_SUSPENDED_FILTER_SUSPENDED) {
+            $query->whereDate('suspended_at', '<', Carbon::now());
+        }
+        return $query;
+    }
+
     public function scopeNotDraft(Builder $query): Builder
     {
 
