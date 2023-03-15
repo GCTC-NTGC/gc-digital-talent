2b551c8b936ce297d245b0e102e394be8e9dd9fe
Peter Giles <8978655+petertgiles@users.noreply.github.com>
2023-03-15 11:25:14 -0400

    skill family pages

--- apps/web/src/pages/SkillFamilies/IndexSkillFamilyPage.tsx
+++ apps/web/src/pages/SkillFamilies/IndexSkillFamilyPage.tsx
@@ -4,11 +4,14 @@ import { UserGroupIcon } from "@heroicons/react/24/outline";
 
 import PageHeader from "~/components/PageHeader";
 import SEO from "~/components/SEO/SEO";
+import AdminContentWrapper from "~/components/AdminContentWrapper/AdminContentWrapper";
+import useRoutes from "~/hooks/useRoutes";
 
 import SkillFamilyTableApi from "./components/SkillFamilyTable";
 
 const IndexSkillFamilyPage: React.FC = () => {
   const intl = useIntl();
+  const routes = useRoutes();
 
   const pageTitle = intl.formatMessage({
     defaultMessage: "Skill families",
@@ -16,12 +19,31 @@ const IndexSkillFamilyPage: React.FC = () => {
     description: "Page title for the skill family index page",
   });
 
+  const navigationCrumbs = [
+    {
+      label: intl.formatMessage({
+        defaultMessage: "Home",
+        id: "EBmWyo",
+        description: "Link text for the home link in breadcrumbs.",
+      }),
+      url: routes.adminDashboard(),
+    },
+    {
+      label: intl.formatMessage({
+        defaultMessage: "Skill families",
+        id: "yeXUjo",
+        description: "Breadcrumb title for the skill families page link.",
+      }),
+      url: routes.skillFamilyTable(),
+    },
+  ];
+
   return (
-    <>
+    <AdminContentWrapper crumbs={navigationCrumbs}>
       <SEO title={pageTitle} />
       <PageHeader icon={UserGroupIcon}>{pageTitle}</PageHeader>
       <SkillFamilyTableApi />
-    </>
+    </AdminContentWrapper>
   );
 };
 
