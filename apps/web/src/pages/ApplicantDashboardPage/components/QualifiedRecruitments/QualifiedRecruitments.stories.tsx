648f96360a405cb893c7ce94a79f6a03c5eb50d7
Yonathan K <yoni.kid15@gmail.com>
2023-03-17 08:58:51 -0400

    Add new component for qualified recruitment section

---
+++ apps/web/src/pages/ApplicantDashboardPage/components/QualifiedRecruitments/QualifiedRecruitments.stories.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import type { ComponentMeta, ComponentStory } from "@storybook/react";
+
+import { fakePoolCandidates } from "@gc-digital-talent/fake-data";
+
+import { FAR_PAST_DATE } from "@gc-digital-talent/date-helpers";
+import { PoolCandidateStatus } from "~/api/generated";
+import QualifiedRecruitments, { Application } from "./QualifiedRecruitments";
+
+type Story = ComponentStory<typeof QualifiedRecruitments>;
+type Meta = ComponentMeta<typeof QualifiedRecruitments>;
+
+const mockApplications = fakePoolCandidates(20);
+
+const activeRecruitments: Application[] = Object.values(
+  PoolCandidateStatus,
+).map((status, index) => {
+  return {
+    ...mockApplications[index],
+    status,
+    archivedAt: null,
+  };
+});
+
+const expiredRecruitments: Application[] = fakePoolCandidates(5).map(
+  (application) => ({
+    ...application,
+    expiryDate: FAR_PAST_DATE,
+  }),
+);
+
+export default {
+  component: QualifiedRecruitments,
+  title: "Pages/Applicant Dashboard/Qualified Recruitment",
+  args: {
+    activeRecruitments,
+    expiredRecruitments,
+  },
+} as Meta;
+
+const Template: Story = (args) => {
+  return <QualifiedRecruitments {...args} />;
+};
+
+export const DefaultQualifiedRecruitments = Template.bind({});
+export const NoQualifiedRecruitments = Template.bind({});
+NoQualifiedRecruitments.args = {
+  activeRecruitments: [],
+  expiredRecruitments: [],
+};
