7f54183ff2b801c990d3791d3f68604cd738035d
Brinda Sasikumar <Brinda.Sasikumar@gmail.com>
2023-03-17 23:44:42 -0400

    add stream into applicant filter input

--- apps/web/src/pages/SearchRequests/SearchPage/components/SearchForm.tsx
+++ apps/web/src/pages/SearchRequests/SearchPage/components/SearchForm.tsx
@@ -254,6 +254,7 @@ const SearchForm = React.forwardRef<SearchFormRef, SearchFormProps>(
             ? durationSelectionToEnum(values.employmentDuration)
             : null,
           locationPreferences: values.locationPreferences || [],
+          stream: values.stream ? values.stream : undefined,
           pools: pools
             ? pools
                 .filter(notEmpty)
