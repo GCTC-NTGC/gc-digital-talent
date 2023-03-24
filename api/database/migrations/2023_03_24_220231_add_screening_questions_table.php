c9538ac8bdb8b448651dfc2af452247f6e794a4a
vd1992 <40485260+vd1992@users.noreply.github.com>
2023-03-24 16:26:51 -0600

    create migration

---
+++ api/database/migrations/2023_03_24_220231_add_screening_questions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('screening_questions', function (Blueprint $table) {
+            $table->uuid('id')->primary('id');
+            $table->uuid('pool_id');
+            $table->foreign("pool_id")->references("id")->on("pools");
+            $table->integer('sort_order')->nullable()->default(null);
+            $table->jsonb('question')->nullable()->default(null);
+            $table->timestamps();
+        });
+
+        DB::statement('ALTER TABLE screening_questions ALTER COLUMN id SET DEFAULT gen_random_uuid();');
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('screening_questions');
+    }
+};
