206ac980d0cfab77780045c5dcac6abcd756e6eb
Brinda Sasikumar <Brinda.Sasikumar@gmail.com>
2023-03-28 12:05:40 -0400

    fix typo

--- api/database/migrations/2023_03_28_144927_add_stream_to_applicant_filter_table.php
+++ api/database/migrations/2023_03_28_144927_add_stream_to_applicant_filter_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('applicant_filter', function (Blueprint $table) {
+        Schema::table('applicant_filters', function (Blueprint $table) {
             $table->string('stream')->nullable();
         });
     }
@@ -21,7 +21,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('applicant_filter', function (Blueprint $table) {
+        Schema::table('applicant_filters', function (Blueprint $table) {
             $table->dropColumn('stream');
         });
     }
