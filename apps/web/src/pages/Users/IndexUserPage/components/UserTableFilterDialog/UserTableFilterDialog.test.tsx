ba0571083717d052ee5678a6a0284089ec5aea9b
Brinda Sasikumar <Brinda.Sasikumar@gmail.com>
2023-03-28 12:36:49 -0400

    fix tests

--- apps/web/src/pages/Users/IndexUserPage/components/UserTableFilterDialog/UserTableFilterDialog.test.tsx
+++ apps/web/src/pages/Users/IndexUserPage/components/UserTableFilterDialog/UserTableFilterDialog.test.tsx
@@ -41,6 +41,7 @@ const emptyFormValues = {
   priorityWeight: [],
   expiryStatus: [],
   suspendedStatus: [],
+  stream: [],
 };
 
 interface ProvidersProps {
@@ -175,7 +176,7 @@ describe("UserTableFilterDialog", () => {
         expect(mockSubmit).toHaveBeenCalledTimes(1);
 
         const activeFilter = mockSubmit.mock.lastCall[0];
-        expect(Object.keys(activeFilter)).toHaveLength(16);
+        expect(Object.keys(activeFilter)).toHaveLength(17);
         // Static filters.
         expect(activeFilter.workRegion).toHaveLength(1);
         expect(activeFilter.employmentDuration).toHaveLength(1);
