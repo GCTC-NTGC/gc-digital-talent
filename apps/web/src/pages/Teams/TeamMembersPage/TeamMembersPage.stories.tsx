2abb01046dd0ffda2e8e0b06196d0098c7244d21
Peter Giles <8978655+petertgiles@users.noreply.github.com>
2023-03-30 14:17:27 -0400

    bonus story for team members

---
+++ apps/web/src/pages/Teams/TeamMembersPage/TeamMembersPage.stories.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+
+import { fakeUsers, fakeTeams, fakeRoles } from "@gc-digital-talent/fake-data";
+
+import TeamMembersPage from "./TeamMembersPage";
+
+const availableRoles = fakeRoles();
+
+const teamsData = fakeTeams(10);
+const usersData = fakeUsers(10);
+
+const teamData = {
+  ...teamsData[0],
+  roleAssignments: [
+    { id: "assignment1", role: availableRoles[2], user: usersData[0] },
+    { id: "assignment2", role: availableRoles[2], user: usersData[1] },
+    { id: "assignment3", role: availableRoles[2], user: usersData[3] },
+  ],
+};
+
+export default {
+  component: TeamMembersPage,
+  title: "Pages/Team Members Page",
+  parameters: {
+    themeKey: "admin",
+    defaultPath: {
+      path: "/en/admin/teams/:teamId/members",
+      initialEntries: [`/en/admin/teams/${teamData.id}/members`],
+    },
+    apiResponses: {
+      GetTeam: {
+        data: {
+          team: teamData,
+        },
+      },
+      AllUsers: {
+        data: {
+          users: usersData,
+        },
+      },
+      ListRoles: {
+        data: {
+          roles: availableRoles,
+        },
+      },
+    },
+  },
+} as ComponentMeta<typeof TeamMembersPage>;
+
+const Template: ComponentStory<typeof TeamMembersPage> = () => (
+  <TeamMembersPage />
+);
+
+export const Default = Template.bind({});
