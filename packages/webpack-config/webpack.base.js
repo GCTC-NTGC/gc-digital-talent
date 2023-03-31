db973c21e03ba93dbe42baa1f379a294585fa015
vd1992 <40485260+vd1992@users.noreply.github.com>
2023-03-31 15:11:49 -0600

    compress on prod

--- packages/webpack-config/webpack.base.js
+++ packages/webpack-config/webpack.base.js
@@ -4,7 +4,6 @@ const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
 const TsTransformer = require("@formatjs/ts-transformer");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CompressionPlugin = require("compression-webpack-plugin");
 const { DefinePlugin } = require("webpack");
 require("dotenv").config({ path: "./.env" });
 const shell = require("shelljs");
@@ -92,13 +91,6 @@ module.exports = (basePath) => {
         inject: false,
         environment: process.env,
         minify: false, // minify in production causes this to fail
-      }),
-
-      new CompressionPlugin({
-        algorithm: "gzip",
-        test: /\.js$|\.css$|\.html$/,
-        threshold: 10240,
-        minRatio: 0.8,
       }),
     ],
     module: {
