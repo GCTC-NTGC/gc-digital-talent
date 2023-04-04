eeef46c55e1b5fa23cd089df98af82d33892c5c2
Yonathan K <yoni.kid15@gmail.com>
2023-04-04 09:37:42 -0400

    Add story for TreeView component

---
+++ packages/ui/src/components/TreeView/TreeView.stories.tsx
@@ -0,0 +1,71 @@
+import React from "react";
+import { Meta, ComponentStory } from "@storybook/react";
+import { faker } from "@faker-js/faker";
+import TreeView from "./TreeView";
+import Button from "../Button";
+import Accordion from "../Accordion";
+
+export default {
+  component: TreeView.Root,
+  title: "Components/TreeView",
+} as Meta;
+
+const TreeViewWithNoExperiences: ComponentStory<typeof TreeView.Root> = () => {
+  return (
+    <>
+      <TreeView.Root
+        title="Communication of technical information to non-technical audiences"
+        subtitle="Communication of technical information is defined as the ability to share information in plain language so anyone can understand, regardless of technical knowledge."
+        error="This required skill must have at least 1 résumé experience associated with it."
+      >
+        <TreeView.Item>
+          <Button color="blue" mode="solid" type="button">
+            <p>Connect a résumé experience</p>
+          </Button>
+        </TreeView.Item>
+      </TreeView.Root>
+      <TreeView.Root
+        data-h2-margin-top="base(x2)"
+        title="IT troubleshooting"
+        subtitle="IT troubleshooting is defined as the ability to determine causes of operating errors and decide what to do about them."
+        error="This required skill must have at least 1 résumé experience associated with it."
+      >
+        <TreeView.Item>
+          <Button color="blue" mode="solid" type="button">
+            <p>Connect a résumé experience</p>
+          </Button>
+        </TreeView.Item>
+      </TreeView.Root>
+    </>
+  );
+};
+
+const TreeViewWithExperiences: ComponentStory<typeof TreeView.Root> = () => {
+  return (
+    <TreeView.Root
+      title="Communication of technical information to non-technical audiences"
+      subtitle="Communication of technical information is defined as the ability to share information in plain language so anyone can understand, regardless of technical knowledge."
+    >
+      <TreeView.Item>
+        <Accordion.Root type="single" collapsible data-h2-margin-top="base(x1)">
+          <Accordion.Item value="one">
+            <Accordion.Trigger subtitle="Fortune 500 company">
+              Junior Developer
+            </Accordion.Trigger>
+            <Accordion.Content>
+              <p>{faker.lorem.sentences(5)}</p>
+            </Accordion.Content>
+          </Accordion.Item>
+        </Accordion.Root>
+      </TreeView.Item>
+      <TreeView.Item>
+        <Button color="blue" mode="solid" type="button">
+          <p>Connect a résumé experience</p>
+        </Button>
+      </TreeView.Item>
+    </TreeView.Root>
+  );
+};
+
+export const WithNoExperiences = TreeViewWithNoExperiences.bind({});
+export const WithExperiences = TreeViewWithExperiences.bind({});
