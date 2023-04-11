2754f6a81901af8992728005b33a3a5756434819
Eric Sizer <eric.sizer@gmail.com>
2023-04-11 10:42:49 -0400

    update to reuse existing translations

--- apps/web/src/pages/Applications/ApplicationResumeAddPage/components/AddExperienceForm.tsx
+++ apps/web/src/pages/Applications/ApplicationResumeAddPage/components/AddExperienceForm.tsx
@@ -195,8 +195,8 @@ const AddExperienceForm = ({ applicationId }: AddExperienceFormProps) => {
           >
             {intl.formatMessage({
               defaultMessage: "Cancel and go back",
-              id: "rMYmPd",
-              description: "Label for cancel button on profile form.",
+              id: "fMcKtJ",
+              description: "Text to cancel changes to a form",
             })}
           </Link>
         </div>
