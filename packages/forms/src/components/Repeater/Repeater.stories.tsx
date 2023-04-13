9dba681d532d73f0f40764bb3dc44cf023e39c58
Eric Sizer <eric.sizer@gmail.com>
2023-04-13 10:43:55 -0400

    add max items story

--- packages/forms/src/components/Repeater/Repeater.stories.tsx
+++ packages/forms/src/components/Repeater/Repeater.stories.tsx
@@ -16,6 +16,7 @@ type StoryProps = RepeaterProps &
   Pick<RepeaterFieldsetProps, "hideLegend"> & {
     defaultValues: Array<LocalizedString>;
     name: string;
+    maxItems?: number;
   };
 
 export default {
@@ -30,16 +31,18 @@ const defaultArgs = {
 };
 
 const Fields = (props: Omit<StoryProps, "defaultValues">) => {
-  const { name, hideLegend, ...rootProps } = props;
+  const { name, hideLegend, maxItems, ...rootProps } = props;
   const { control } = useFormContext();
   const { remove, move, append, fields } = useFieldArray({
     control,
     name,
   });
+  const canAdd = maxItems ? fields.length < maxItems : true;
 
   return (
     <Repeater.Root
       {...rootProps}
+      showAdd={canAdd}
       onAdd={() => {
         const newValues = {
           en: "",
@@ -49,44 +52,64 @@ const Fields = (props: Omit<StoryProps, "defaultValues">) => {
         action("add")(newValues);
       }}
     >
-      {fields.length ? (
-        fields.map((item, index) => (
-          <Repeater.Fieldset
-            key={item.id}
-            index={index}
-            total={fields.length}
-            onMove={move}
-            onRemove={remove}
-            legend={`Screening Question ${index + 1}`}
-            hideLegend={hideLegend}
-          >
-            <div
-              data-h2-display="base(grid)"
-              data-h2-grid-template-columns="base(1fr 1fr)"
-              data-h2-gap="base(0, x.5)"
-              data-h2-margin="base(-x1, 0)"
+      <>
+        {fields.length ? (
+          fields.map((item, index) => (
+            <Repeater.Fieldset
+              key={item.id}
+              index={index}
+              total={fields.length}
+              onMove={move}
+              onRemove={remove}
+              legend={`Screening Question ${index + 1}`}
+              hideLegend={hideLegend}
             >
-              <TextArea
-                id={`${name}.${index}.en`}
-                name={`${name}.${index}.en`}
-                label="Question (EN)"
-              />
-              <TextArea
-                id={`${name}.${index}.fr`}
-                name={`${name}.${index}.fr`}
-                label="Question (FR)"
-              />
-            </div>
-          </Repeater.Fieldset>
-        ))
-      ) : (
-        <Well>
-          <p data-h2-font-weight="base(700)" data-h2-margin-bottom="base(x.5)">
-            You have no questions.
-          </p>
-          <p>Start adding some questions using the following button.</p>
-        </Well>
-      )}
+              <div
+                data-h2-display="base(grid)"
+                data-h2-grid-template-columns="base(1fr 1fr)"
+                data-h2-gap="base(0, x.5)"
+                data-h2-margin="base(-x1, 0)"
+              >
+                <TextArea
+                  id={`${name}.${index}.en`}
+                  name={`${name}.${index}.en`}
+                  label="Question (EN)"
+                />
+                <TextArea
+                  id={`${name}.${index}.fr`}
+                  name={`${name}.${index}.fr`}
+                  label="Question (FR)"
+                />
+              </div>
+            </Repeater.Fieldset>
+          ))
+        ) : (
+          <Well>
+            <p
+              data-h2-font-weight="base(700)"
+              data-h2-margin-bottom="base(x.5)"
+            >
+              You have no questions.
+            </p>
+            <p>Start adding some questions using the following button.</p>
+          </Well>
+        )}
+        {!canAdd && maxItems && (
+          <Well>
+            <p
+              data-h2-font-weight="base(700)"
+              data-h2-margin-bottom="base(x.5)"
+            >
+              You have reached the maximum amount ({maxItems}) of screening
+              questions per poster.
+            </p>
+            <p>
+              Remember, applicants will submit information on how they meet each
+              skill requirement through the regular application process.
+            </p>
+          </Well>
+        )}
+      </>
     </Repeater.Root>
   );
 };
@@ -132,6 +155,22 @@ WithDefaultValues.args = {
     {
       en: "Question 1 (EN)",
       fr: "Question 1 (FR)",
+    },
+  ],
+};
+
+export const WithMaxItems = Template.bind({});
+WithMaxItems.args = {
+  ...defaultArgs,
+  maxItems: 2,
+  defaultValues: [
+    {
+      en: "Question 1 (EN)",
+      fr: "Question 1 (FR)",
+    },
+    {
+      en: "Question 2 (EN)",
+      fr: "Question 2 (FR)",
     },
   ],
 };
