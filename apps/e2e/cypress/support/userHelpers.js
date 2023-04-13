a287c9ef61d0168c812efc003ec180116fc5035b
vd1992 <40485260+vd1992@users.noreply.github.com>
2023-04-13 15:46:55 -0600

    update field

--- apps/e2e/cypress/support/userHelpers.js
+++ apps/e2e/cypress/support/userHelpers.js
@@ -72,7 +72,7 @@ export function addRolesToUser(userId, roles = [], team = undefined) {
       .map((role) => role.id);
     cy.updateUser(userId, {
       legacyRoles: undefined,
-      roles: {
+      rolesSetter: {
         attach: {
           roles: roleIds,
           team: team,
