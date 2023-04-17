f5c3633241fded61b40830eb164c93587152eb08
Brinda Sasikumar <Brinda.Sasikumar@gmail.com>
2023-04-17 16:29:24 -0400

    fix Expression

--- api/database/migrations/2023_04_05_172101_create_applicant_filter_qualified_classification_table.php
+++ api/database/migrations/2023_04_05_172101_create_applicant_filter_qualified_classification_table.php
@@ -4,6 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Query\Expression;
+
 
 return new class extends Migration
 {
