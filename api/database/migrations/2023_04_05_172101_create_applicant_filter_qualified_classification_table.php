79b09eb6bfa2e0f708b350cbce8bf3de8e60f528
Brinda Sasikumar <Brinda.Sasikumar@gmail.com>
2023-04-17 13:42:45 -0400

    address PR feedback on default expression

--- api/database/migrations/2023_04_05_172101_create_applicant_filter_qualified_classification_table.php
+++ api/database/migrations/2023_04_05_172101_create_applicant_filter_qualified_classification_table.php
@@ -13,13 +13,12 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('applicant_filter_qualified_classification', function (Blueprint $table) {
-            $table->uuid('id')->primary('id');
+            $table->uuid('id')->primary('id')->default(new Expression('gen_random_uuid()'));
             $table->timestamps();
             $table->uuid('applicant_filter_id')->references('id')->on('applicant_filters')->nullable(false)->cascadeOnDelete();
             $table->uuid('classification_id')->references('id')->on('classifications')->nullable(false);
             $table->unique(['applicant_filter_id', 'classification_id']);
         });
-        DB::statement('ALTER TABLE applicant_filter_qualified_classification ALTER COLUMN id SET DEFAULT gen_random_uuid();');
     }
 
     /**
