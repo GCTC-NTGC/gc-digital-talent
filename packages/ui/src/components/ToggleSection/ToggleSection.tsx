62e2d9fbb423a854ff49153e63a09e92bddeff54
Eric Sizer <eric.sizer@gmail.com>
2023-04-19 13:42:57 -0400

    change toggle align to be in center

--- packages/ui/src/components/ToggleSection/ToggleSection.tsx
+++ packages/ui/src/components/ToggleSection/ToggleSection.tsx
@@ -271,7 +271,7 @@ const Header = React.forwardRef<HTMLHeadingElement, HeaderProps>(
       <div
         data-h2-display="base(flex)"
         data-h2-flex-direction="base(column) l-tablet(row)"
-        data-h2-align-items="base(center) l-tablet(flex-end)"
+        data-h2-align-items="base(center)"
         data-h2-justify-content="base(space-between)"
       >
         <Heading
