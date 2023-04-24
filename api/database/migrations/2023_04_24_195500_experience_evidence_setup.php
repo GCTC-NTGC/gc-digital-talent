869f1f75f7bd647a520cc2232c76314c4a85f6e3
vd1992 <40485260+vd1992@users.noreply.github.com>
2023-04-24 14:09:35 -0600

    add migration

---
+++ api/database/migrations/2023_04_24_195500_experience_evidence_setup.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Query\Expression;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pool_candidate_experience', function (Blueprint $table) {
+            $table->uuid('id')->primary('id')->default(new Expression('gen_random_uuid()'));
+            $table->uuid('pool_candidate_id')->nullable(false);
+            $table->foreign('pool_candidate_id')->references('id')->on('pool_candidates');
+            $table->uuid('experience_id')->nullable(false);
+            $table->string('experience_type')->nullable(false);
+            $table->unique(['pool_candidate_id', 'experience_id', 'experience_type']);
+            $table->timestamps();
+        });
+
+        Schema::table('pool_candidates', function (Blueprint $table) {
+            $table->string('minimum_criteria')->nullable(true)->default(null);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pool_candidate_experience');
+        Schema::table('pool_candidates', function (Blueprint $table) {
+            $table->dropColumn('minimum_criteria');
+        });
+    }
+};
