4dc7c5eab73503a5134e337247715d0bc6590ee8
vd1992 <40485260+vd1992@users.noreply.github.com>
2023-04-28 09:25:09 -0600

    add --force

--- maintenance/scripts/refresh_api.sh
+++ maintenance/scripts/refresh_api.sh
@@ -4,7 +4,7 @@ parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 source ${parent_path}/lib/common.sh
 
 cd /var/www/html/api
-rm ./bootstrap/cache/*.php
+rm ./bootstrap/cache/*.php --force
 composer install --prefer-dist
 php artisan migrate
 php artisan lighthouse:print-schema --write
