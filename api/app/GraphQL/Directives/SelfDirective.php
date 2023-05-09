5204763dfb5bcc79785b5bbc5fa8ef2aa4dd8f9a
Eric Sizer <eric.sizer@gmail.com>
2023-05-09 10:39:39 -0400

    update self directive with key arg, wrap pool candidate

--- api/app/GraphQL/Directives/SelfDirective.php
+++ api/app/GraphQL/Directives/SelfDirective.php
@@ -5,6 +5,7 @@ namespace App\GraphQL\Directives;
 use Nuwave\Lighthouse\Schema\Directives\BaseDirective;
 use Nuwave\Lighthouse\Schema\Values\FieldValue;
 use Nuwave\Lighthouse\Support\Contracts\FieldResolver;
+use Illuminate\Support\Facades\Log;
 
 final class SelfDirective extends BaseDirective implements FieldResolver
 {
@@ -12,7 +13,9 @@ final class SelfDirective extends BaseDirective implements FieldResolver
 
     public static function definition(): string
     {
-        return /** @lang GraphQL */ <<<'GRAPHQL'
+        return
+            /** @lang GraphQL */
+            <<<'GRAPHQL'
 """
 The field will be resolved to the same value as its parent.
 For example, useful in the following situation:
@@ -20,7 +23,12 @@ type User {
     profile: Applicant @self
 }
 """
-directive @self on FIELD_DEFINITION
+directive @self(
+    """
+    The key to use from the parent.
+    """
+    key: String on FIELD_DEFINITION
+) on FIELD_DEFINITION
 GRAPHQL;
     }
 
@@ -36,6 +44,10 @@ GRAPHQL;
     public function resolveField(FieldValue $fieldValue)
     {
         $fieldValue->setResolver(function ($root, array $args) {
+            $key = $this->directiveArgValue('key');
+            if ($key) {
+                return $root->{$key};
+            }
             return $root;
         });
 
