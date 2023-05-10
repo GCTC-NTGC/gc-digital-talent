1daea36a19657027393b484b0e4db41ec2a3d0a8
Eric Sizer <eric.sizer@gmail.com>
2023-05-10 17:06:56 -0400

    update client with breaking changes

--- packages/client/src/components/ClientProvider/ClientProvider.tsx
+++ packages/client/src/components/ClientProvider/ClientProvider.tsx
@@ -1,17 +1,13 @@
-import { authExchange } from "@urql/exchange-auth";
-import jwtDecode, { JwtPayload } from "jwt-decode";
-import React, { useCallback, useEffect, useMemo, useRef } from "react";
+import React, { useEffect, useMemo, useRef } from "react";
 import {
   Client,
   CombinedError,
   createClient,
-  dedupExchange,
   cacheExchange,
   fetchExchange,
   errorExchange,
   Provider,
   Operation,
-  makeOperation,
   AnyVariables,
 } from "urql";
 import { useIntl } from "react-intl";
@@ -20,6 +16,8 @@ import { useAuthentication } from "@gc-digital-talent/auth";
 import { useLogger } from "@gc-digital-talent/logger";
 import { toast } from "@gc-digital-talent/toast";
 
+import { authExchange } from "@urql/exchange-auth";
+import jwtDecode, { JwtPayload } from "jwt-decode";
 import {
   buildRateLimitErrorMessageNode,
   buildValidationErrorMessageNode,
@@ -33,61 +31,6 @@ import {
 // const nonce = ...
 
 const apiUri = process.env.API_URI ?? "http://localhost:8000/graphql";
-
-interface AuthState {
-  accessToken: string | null;
-  refreshToken: string | null;
-  idToken: string | null;
-}
-
-const addAuthToOperation = ({
-  authState,
-  operation,
-}: {
-  authState: AuthState | null;
-  operation: Operation;
-}): Operation => {
-  if (!authState || !authState.accessToken) {
-    return operation;
-  }
-
-  const fetchOptions =
-    typeof operation.context.fetchOptions === "function"
-      ? operation.context.fetchOptions()
-      : operation.context.fetchOptions || {};
-
-  return makeOperation(operation.kind, operation, {
-    ...operation.context,
-    fetchOptions: {
-      ...fetchOptions,
-      headers: {
-        ...fetchOptions.headers,
-        Authorization: `Bearer ${authState.accessToken}`,
-      },
-    },
-  });
-};
-
-const didAuthError = ({ error }: { error: CombinedError }): boolean => {
-  return error && error.response
-    ? error.response.status === 401 ||
-        error.graphQLErrors.some(
-          (e) => e.extensions?.category === "authentication",
-        )
-    : false;
-};
-
-const willAuthError = ({ authState }: { authState: AuthState | null }) => {
-  let tokenIsKnownToBeExpired = false;
-  if (authState?.accessToken) {
-    const decoded = jwtDecode<JwtPayload>(authState.accessToken);
-    if (decoded.exp) tokenIsKnownToBeExpired = Date.now() > decoded.exp * 1000; // JWT expiry date in seconds, not milliseconds
-  }
-
-  if (tokenIsKnownToBeExpired) return true;
-
-  return false;
-};
 
 const ClientProvider = ({
   client,
@@ -105,53 +48,6 @@ const ClientProvider = ({
   useEffect(() => {
     authRef.current = authContext;
   }, [authContext]);
-
-  const getAuth: (params: {
-    authState: AuthState | null;
-  }) => Promise<AuthState | null> = useCallback(
-    async ({ authState: existingAuthState }) => {
-      // getAuth could be called for the first request or as the result of an error
-
-      // At runtime, get the current auth state
-      const { accessToken, refreshToken, idToken, logout, refreshTokenSet } =
-        authRef.current;
-
-      if (!existingAuthState) {
-        // no existing auth state so this is probably just the first request
-        if (accessToken) {
-          return { accessToken, refreshToken, idToken };
-        }
-        return null;
-      }
-
-      /**
-       * Logout the user and return null AuthState
-       *
-       * @returns null
-       */
-      const logoutNullState = () => {
-        const currentLocation = window.location.pathname; // Can't use react-router hooks because we may not be inside the Router context.
-        logout(currentLocation); // After logging out, try to return to the page the user was on.
-        return null;
-      };
-
-      // If authState is not null, and getAuth is called again, then it means authentication failed for some reason.
-      // let's try to use a refresh token to get new tokens
-      if (refreshToken) {
-        const refreshedAuthState = await refreshTokenSet();
-        if (refreshedAuthState) {
-          return refreshedAuthState;
-        }
-
-        return logoutNullState();
-      }
-
-      return logoutNullState();
-    },
-    // This function is inside of `useCallback` to prevent breaking the memoization of internalClient.
-    // If internalClient is re-instantiated it will lose its error count and can cause refresh loops.
-    [],
-  );
 
   const internalClient = useMemo(() => {
     return (
@@ -204,19 +100,71 @@ const ClientProvider = ({
               }
             },
           }),
-          dedupExchange,
           cacheExchange,
-          authExchange({
-            getAuth,
-            addAuthToOperation,
-            didAuthError,
-            willAuthError,
+          authExchange(async (utils) => {
+            const { accessToken, refreshToken, logout, refreshTokenSet } =
+              authRef.current;
+
+            return {
+              addAuthToOperation(operation) {
+                if (accessToken) {
+                  return utils.appendHeaders(operation, {
+                    Authorization: `Bearer ${accessToken}`,
+                  });
+                }
+                return operation;
+              },
+              willAuthError() {
+                let tokenIsKnownToBeExpired = false;
+                if (accessToken) {
+                  const decoded = jwtDecode<JwtPayload>(accessToken);
+                  if (decoded.exp)
+                    tokenIsKnownToBeExpired = Date.now() > decoded.exp * 1000; // JWT expiry date in seconds, not milliseconds
+                }
+
+                if (tokenIsKnownToBeExpired) return true;
+
+                return false;
+              },
+              didAuthError(error) {
+                return error && error.response
+                  ? error.response.status === 401 ||
+                      error.graphQLErrors.some(
+                        (e) => e.extensions?.category === "authentication",
+                      )
+                  : false;
+              },
+              async refreshAuth() {
+                /**
+                 * Logout the user and return null AuthState
+                 *
+                 * @returns null
+                 */
+                const logoutNullState = () => {
+                  const currentLocation = window.location.pathname; // Can't use react-router hooks because we may not be inside the Router context.
+                  logout(currentLocation); // After logging out, try to return to the page the user was on.
+                };
+
+                // If authState is not null, and getAuth is called again, then it means authentication failed for some reason.
+                // let's try to use a refresh token to get new tokens
+                if (refreshToken) {
+                  const refreshedAuthState = await refreshTokenSet();
+                  if (refreshedAuthState) {
+                    return;
+                  }
+
+                  logoutNullState();
+                }
+
+                logoutNullState();
+              },
+            };
           }),
           fetchExchange,
         ],
       })
     );
-  }, [client, getAuth, intl, logger]);
+  }, [client, intl, logger, authRef]);
 
   return <Provider value={internalClient}>{children}</Provider>;
 };
@@ -225,6 +173,5 @@ export default ClientProvider;
 
 // https://stackoverflow.com/questions/54116070/how-can-i-unit-test-non-exported-functions
 export const exportedForTesting = {
-  willAuthError,
   extractValidationErrorMessages,
 };
