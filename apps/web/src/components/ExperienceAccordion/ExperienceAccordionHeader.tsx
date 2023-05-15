0f96f6b5fff1f1f56db3323c7835a558bfcf54eb
Eric Sizer <eric.sizer@gmail.com>
2023-05-15 11:21:38 -0400

    fix dangling brace

--- apps/web/src/components/ExperienceAccordion/ExperienceAccordionHeader.tsx
+++ apps/web/src/components/ExperienceAccordion/ExperienceAccordionHeader.tsx
@@ -16,66 +16,60 @@ export const ExperienceAccordionHeader = ({
   children,
   actions,
   ...rest
-}: ExperienceAccordionHeaderProps) =>  (
-    <Accordion.Header
-      data-h2-flex-grow="base(1)"
-      data-h2-display="base(flex)"
-      data-h2-flex-direction="base(row)"
-      data-h2-gap="base(x.5 0)"
-      {...rest}
-    >
-      <Accordion.Trigger>
+}: ExperienceAccordionHeaderProps) => (
+  <Accordion.Header
+    data-h2-flex-grow="base(1)"
+    data-h2-display="base(flex)"
+    data-h2-flex-direction="base(row)"
+    data-h2-gap="base(x.5 0)"
+    {...rest}
+  >
+    <Accordion.Trigger>
+      <div
+        data-h2-flex-grow="base(1)"
+        data-h2-display="base(flex)"
+        data-h2-flex-direction="base(row)"
+        data-h2-gap="base(x.5 0)"
+      >
         <div
           data-h2-flex-grow="base(1)"
           data-h2-display="base(flex)"
-          data-h2-flex-direction="base(row)"
+          data-h2-flex-direction="base(column)"
           data-h2-gap="base(x.5 0)"
         >
+          <span data-h2-display="base(block)" data-h2-font-size="base(h6, 1)">
+            {children}
+          </span>
           <div
-            data-h2-flex-grow="base(1)"
-            data-h2-display="base(flex)"
-            data-h2-flex-direction="base(column)"
-            data-h2-gap="base(x.5 0)"
+            className="Accordion__Subtitle"
+            data-h2-display="base(block)"
+            data-h2-font-size="base(copy)"
+            data-h2-margin="base(x.25 0 0 0)"
           >
-            <span data-h2-display="base(block)" data-h2-font-size="base(h6, 1)">
-              {children}
-            </span>
-            <div
-              className="Accordion__Subtitle"
-              data-h2-display="base(block)"
-              data-h2-font-size="base(copy)"
-              data-h2-margin="base(x.25 0 0 0)"
-            >
-              {category ? (
-                <span data-h2-color="base(primary)">{category}</span>
-              ) : null}
-              {category && dateRange ? (
-                <span
-                  data-h2-margin="base(0 x.5)"
-                  data-h2-color="base(gray.50)"
-                >
-                  |
-                </span>
-              ) : null}
-              {dateRange ? <span>{dateRange}</span> : null}
-            </div>
+            {category ? (
+              <span data-h2-color="base(primary)">{category}</span>
+            ) : null}
+            {category && dateRange ? (
+              <span data-h2-margin="base(0 x.5)" data-h2-color="base(gray.50)">
+                |
+              </span>
+            ) : null}
+            {dateRange ? <span>{dateRange}</span> : null}
           </div>
         </div>
-      </Accordion.Trigger>
-      {notEmpty(actions) ? (
-        <>
-          <div
-            data-h2-margin="base(x1)"
-            data-h2-display="base(flex)"
-            data-h2-flex-direction="base(column)"
-            data-h2-justify-content="base(center)"
-          >
-           {actions}
-          </div>
-        </>
-      ) : null}
-    </Accordion.Header>
-  );
-};
+      </div>
+    </Accordion.Trigger>
+    {notEmpty(actions) ? (
+      <div
+        data-h2-margin="base(x1)"
+        data-h2-display="base(flex)"
+        data-h2-flex-direction="base(column)"
+        data-h2-justify-content="base(center)"
+      >
+        {actions}
+      </div>
+    ) : null}
+  </Accordion.Header>
+);
 
 export default ExperienceAccordionHeader;
