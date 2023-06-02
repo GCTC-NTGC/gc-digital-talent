5d3cd156550a17f7da54dacc3c0a6d0a6bffd107
Brinda Sasikumar <Brinda.Sasikumar@gmail.com>
2023-06-02 14:27:24 -0400

    change copy for search page title and description

--- apps/web/src/pages/SearchRequests/SearchPage/components/SearchHeading.tsx
+++ apps/web/src/pages/SearchRequests/SearchPage/components/SearchHeading.tsx
@@ -25,8 +25,8 @@ const SearchHeading = () => {
       >
         <h2 data-h2-color="base(black)" data-h2-margin="base(0, 0, x1, 0)">
           {intl.formatMessage({
-            defaultMessage: "About the Digital Talent Pool",
-            id: "9hdbFi",
+            defaultMessage: "About the GC Digital Talent database",
+            id: "qnECBT",
             description:
               "Heading displayed in the About area of the hero section of the Search page.",
           })}
@@ -34,8 +34,8 @@ const SearchHeading = () => {
         <p>
           {intl.formatMessage({
             defaultMessage:
-              "This pool is open to most departments and agencies. Candidates in the pool vary from just starting their career to seasoned professionals with several years of work experience. This is an ongoing recruitment pool, which means new candidates are being added every week.",
-            id: "XSxaGk",
+              "This talent database is open to most departments and agencies. Candidates in the database vary from just starting their career to seasoned professionals with several years of work experience. There are several ongoing recruitment processes, which means new candidates are being added every week.",
+            id: "BVwsnS",
             description:
               "Content displayed in the About area of the hero section of the Search page.",
           })}
