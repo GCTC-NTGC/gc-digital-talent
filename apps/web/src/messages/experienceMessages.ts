8dc46150767804c02c40cb7544bfc7445d366a30
Eric Sizer <eric.sizer@gmail.com>
2023-06-02 08:39:17 -0400

    add messages and utilities needed for cards

---
+++ apps/web/src/messages/experienceMessages.ts
@@ -0,0 +1,36 @@
+import { defineMessages } from "react-intl";
+
+const messages = defineMessages({
+  award: {
+    defaultMessage: "Awards and recognition",
+    id: "DRYl88",
+    description: "Title for award experience section",
+  },
+  community: {
+    defaultMessage: "Community participation",
+    id: "Uy5Dg2",
+    description: "Title for community experience section",
+  },
+  education: {
+    defaultMessage: "Education and certificates",
+    id: "PFoM2I",
+    description: "Title for education experience section",
+  },
+  personal: {
+    defaultMessage: "Personal learning",
+    id: "+AUFyC",
+    description: "Title for personal learning section",
+  },
+  work: {
+    defaultMessage: "Work experience",
+    id: "giUfys",
+    description: "Title for work experience section",
+  },
+  unknown: {
+    defaultMessage: "Unknown Experience",
+    id: "U/Lv8i",
+    description: "Title for unknown experiences",
+  },
+});
+
+export default messages;
