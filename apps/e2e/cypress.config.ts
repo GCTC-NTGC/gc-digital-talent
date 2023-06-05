8b2c680fa6b52fbe0fad13cd77b65999ca5b26e3
Patrick Connolly <patrick.c.connolly@gmail.com>
2023-06-05 18:34:18 +0100

    Enable webkit e2e testing.

--- apps/e2e/cypress.config.ts
+++ apps/e2e/cypress.config.ts
@@ -25,6 +25,7 @@ export default defineConfig({
   viewportHeight: 1080,
   videoCompression: 15,
   chromeWebSecurity: true,
+  experimentalWebKitSupport: true,
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
