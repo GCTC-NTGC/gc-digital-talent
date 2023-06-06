7c50d9627544a714fd642657b5b9ba5dc0e90951
Yonathan K <yoni.kid15@gmail.com>
2023-06-06 11:23:49 -0400

    Use native list and listitem elements (ul > li) for chips

--- packages/ui/src/components/Chip/Chips.tsx
+++ packages/ui/src/components/Chip/Chips.tsx
@@ -9,18 +9,15 @@ interface ChipsProps {
 const Chips = ({ children }: ChipsProps) => (
   <div>
     {isArray(children) && children.length > 1 ? (
-      <div
-        role="list"
+      <ul
         data-h2-display="base(flex)"
         data-h2-flex-wrap="base(wrap)"
         data-h2-gap="base(x.25, x.125)"
       >
         {children.map((child) => (
-          <div role="listitem" key={child.key}>
-            {child}
-          </div>
+          <li key={child.key}>{child}</li>
         ))}
-      </div>
+      </ul>
     ) : (
       children
     )}
