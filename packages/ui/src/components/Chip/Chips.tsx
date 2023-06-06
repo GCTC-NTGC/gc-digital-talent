ad695a06eda6a868cef69cf01ff01220df3b1547
Yonathan K <yoni.kid15@gmail.com>
2023-06-06 10:06:00 -0400

    Fix chips role=list nesting error

--- packages/ui/src/components/Chip/Chips.tsx
+++ packages/ui/src/components/Chip/Chips.tsx
@@ -1,17 +1,29 @@
-import React from "react";
+import React, { ReactElement } from "react";
+import isArray from "lodash/isArray";
+import { ChipProps } from "./Chip";
 
 interface ChipsProps {
-  children: React.ReactNode;
+  children: ReactElement<ChipProps> | Array<ReactElement<ChipProps>>;
 }
 
 const Chips = ({ children }: ChipsProps) => (
-  <div
-    role="list"
-    data-h2-display="base(flex)"
-    data-h2-flex-wrap="base(wrap)"
-    data-h2-gap="base(x.25, x.125)"
-  >
-    {children}
+  <div>
+    {isArray(children) && children.length > 1 ? (
+      <div
+        role="list"
+        data-h2-display="base(flex)"
+        data-h2-flex-wrap="base(wrap)"
+        data-h2-gap="base(x.25, x.125)"
+      >
+        {children.map((child) => (
+          <div role="listitem" key={child.key}>
+            {child}
+          </div>
+        ))}
+      </div>
+    ) : (
+      children
+    )}
   </div>
 );
 
