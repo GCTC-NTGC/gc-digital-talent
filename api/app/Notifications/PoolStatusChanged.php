1ec37cbe9c8c27d06ed9657c36f7d7ba0a383531
Peter Giles <8978655+petertgiles@users.noreply.github.com>
2023-06-07 12:20:02 -0400

    queue notifications

---
+++ api/app/Notifications/PoolStatusChanged.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class PoolStatusChanged extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct(
+        public string $pool_id,
+        public string $new_status
+    ) {
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['database'];
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            'pool_id' => $this->pool_id,
+            'new_status' => $this->new_status
+        ];
+    }
+}
