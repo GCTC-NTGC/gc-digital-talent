95da43b0c8ff2f9182dadcf19c01d5fb8cd94437
Yoni K <yoni.kid15@gmail.com>
2023-06-07 15:06:29 -0400

    Update packages/ui/src/components/Chip/Chips.tsx
    Co-authored-by: Eric Sizer <esizer@users.noreply.github.com>

--- packages/ui/src/components/Chip/Chips.tsx
+++ packages/ui/src/components/Chip/Chips.tsx
@@ -11,6 +11,7 @@ const Chips = ({ children }: ChipsProps) => (
     data-h2-display="base(flex)"
     data-h2-flex-wrap="base(wrap)"
     data-h2-gap="base(x.25, x.125)"
+    data-h2-list-style="base(none)"
   >
     {isArray(children) ? (
       children.map((child) => <li key={child.key}>{child}</li>)
