0fcdc4c3df9eadb9e81be88adac93cabdfb7fc7a
vd1992 <40485260+vd1992@users.noreply.github.com>
2023-06-09 14:57:43 -0600

    jesting for filter dialog

---
+++ apps/web/src/components/SearchRequestTable/components/SearchRequestsTableFilter.test.tsx
@@ -0,0 +1,92 @@
+/**
+ * @jest-environment jsdom
+ */
+import "@testing-library/jest-dom";
+
+import React from "react";
+import { Provider as GraphqlProvider } from "urql";
+import { fromValue } from "wonka";
+
+import { axeTest, renderWithProviders } from "@gc-digital-talent/jest-helpers";
+import { act, screen } from "@testing-library/react";
+import {
+  fakeClassifications,
+  fakeDepartments,
+} from "@gc-digital-talent/fake-data";
+import { PoolStream } from "@gc-digital-talent/graphql";
+
+import { selectFilterOption, submitFilters } from "~/utils/jestUtils";
+
+import { SearchRequestsTableFilterDialog } from "./SearchRequestsTableFilterDialog";
+
+const departments = [fakeDepartments()[0]];
+const classifications = [fakeClassifications()[0]];
+
+const mockSubmit = jest.fn();
+const mockToggle = jest.fn();
+const mockClient = {
+  executeQuery: () =>
+    fromValue({
+      data: {
+        departments,
+        classifications,
+      },
+    }),
+  // See: https://github.com/FormidableLabs/urql/discussions/2057#discussioncomment-1568874
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+} as any;
+
+const renderSearchRequestsTableFilterDialog = () =>
+  renderWithProviders(
+    <GraphqlProvider value={mockClient}>
+      <SearchRequestsTableFilterDialog
+        onSubmit={mockSubmit}
+        onOpenChange={mockToggle}
+        isOpen
+        activeFilters={{
+          departments: [],
+          status: [],
+          classifications: [],
+          streams: [],
+        }}
+      />
+    </GraphqlProvider>,
+  );
+
+describe("SearchRequestsTableFilterDialog", () => {
+  it("should have no accessibility errors", async () => {
+    await act(async () => {
+      const { container } = renderSearchRequestsTableFilterDialog();
+      await axeTest(container);
+    });
+  });
+
+  it("Should render the filter", async () => {
+    await act(async () => {
+      renderSearchRequestsTableFilterDialog();
+    });
+
+    // assert filter has everything present
+    expect(screen.getAllByText("Status")).toBeTruthy();
+    expect(screen.getAllByText("Departments")).toBeTruthy();
+    expect(screen.getAllByText("Classifications")).toBeTruthy();
+    expect(screen.getAllByText("Streams")).toBeTruthy();
+    expect(screen.getAllByText("Clear filters")).toBeTruthy();
+    expect(screen.getAllByText("Show results")).toBeTruthy();
+    expect(screen.getAllByRole("combobox")).toHaveLength(4);
+
+    // select something and then submit
+    await selectFilterOption(/Status/i);
+    await selectFilterOption(/Departments/i);
+    await selectFilterOption(/Classifications/i);
+    await selectFilterOption(/Streams/i);
+    await submitFilters();
+
+    expect(mockSubmit).toHaveBeenCalledWith({
+      classifications: [classifications[0].id],
+      departments: [departments[0].id],
+      status: ["DONE"],
+      streams: [PoolStream.BusinessAdvisoryServices],
+    });
+  });
+});
