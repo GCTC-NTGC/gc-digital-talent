0504d1bd9dbb61f0f0d92d1c82fcb53ba9d8e7ac
Peter Giles <8978655+petertgiles@users.noreply.github.com>
2023-06-16 14:41:23 -0400

    iap copy

--- apps/web/src/pages/Auth/RegisterPage/RegisterPage.tsx
+++ apps/web/src/pages/Auth/RegisterPage/RegisterPage.tsx
@@ -3,7 +3,7 @@ import { useIntl } from "react-intl";
 import { useSearchParams } from "react-router-dom";
 
 import { Link } from "@gc-digital-talent/ui";
-import { getLocale } from "@gc-digital-talent/i18n";
+import { getLocale, useLocale } from "@gc-digital-talent/i18n";
 import { useApiRoutes } from "@gc-digital-talent/auth";
 import { useFeatureFlags } from "@gc-digital-talent/env";
 import { useTheme } from "@gc-digital-talent/theme";
@@ -13,7 +13,7 @@ import useBreadcrumbs from "~/hooks/useBreadcrumbs";
 import SEO from "~/components/SEO/SEO";
 import Hero from "~/components/Hero/Hero";
 
-const keyRegistrationLink = (path: string, chunks: React.ReactNode) => (
+const buildExternalLink = (path: string, chunks: React.ReactNode) => (
   <Link external href={path}>
     {chunks}
   </Link>
@@ -21,13 +21,15 @@ const keyRegistrationLink = (path: string, chunks: React.ReactNode) => (
 
 const RegisterPage = () => {
   const intl = useIntl();
+  const localeState = useLocale();
   const paths = useRoutes();
   const apiPaths = useApiRoutes();
   const { applicantDashboard } = useFeatureFlags();
   const [searchParams] = useSearchParams();
   const { key: themeKey, setKey: setThemeKey } = useTheme();
   const fromPath = searchParams.get("from");
-  const isFromIap = fromPath === paths.iap();
+  const iapMode =
+    fromPath === paths.iap() || searchParams.get("personality") === "iap";
   const fallbackPath = applicantDashboard
     ? paths.dashboard()
     : paths.myProfile();
@@ -53,10 +55,10 @@ const RegisterPage = () => {
   ]);
 
   useEffect(() => {
-    if (isFromIap && themeKey !== "iap") {
+    if (iapMode && themeKey !== "iap") {
       setThemeKey("iap");
     }
-  }, [isFromIap, themeKey, setThemeKey]);
+  }, [iapMode, themeKey, setThemeKey]);
 
   return (
     <>
@@ -64,38 +66,119 @@ const RegisterPage = () => {
       <Hero title={pageTitle} crumbs={crumbs} />
       <div data-h2-padding="base(x3, 0)">
         <div data-h2-container="base(center, small, x1) p-tablet(center, small, x2)">
-          <p>
-            {intl.formatMessage({
-              defaultMessage:
-                "You can log into your GC Digital Talent profile using your existing GCKey, even if you've never used this platform before.",
-              id: "NcB+TT",
-              description: "Instructions on how to login with GCKey",
-            })}
-          </p>
-          <p data-h2-margin="base(x.5, 0, 0, 0)">
-            {intl.formatMessage({
-              defaultMessage:
-                "If you're unsure whether you have an existing GCKey account, continue to the website and try logging in. If you can't remember your password, you can also reset it there.",
-              id: "NuGNEF",
-              description:
-                "Instructions on what to do if user doesn't know if they have a GCKey",
-            })}
-          </p>
-          <p data-h2-margin="base(x.5, 0, 0, 0)">
-            {intl.formatMessage(
-              {
-                defaultMessage:
-                  "<strong>Don't have a GCKey account?</strong> <a>Register for one</a>.",
-                id: "q53yRm",
-                description:
-                  "Instruction on what to do if user does not have a GCKey",
-              },
-              {
-                a: (chunks: React.ReactNode) =>
-                  keyRegistrationLink(loginPath, chunks),
-              },
-            )}
-          </p>
+          {!iapMode ? (
+            <>
+              {/* Standard copy */}
+              <p>
+                {intl.formatMessage({
+                  defaultMessage:
+                    "You can log into your GC Digital Talent profile using your existing GCKey, even if you've never used this platform before.",
+                  id: "NcB+TT",
+                  description: "Instructions on how to login with GCKey",
+                })}
+              </p>
+              <p data-h2-margin="base(x.5, 0, 0, 0)">
+                {intl.formatMessage({
+                  defaultMessage:
+                    "If you're unsure whether you have an existing GCKey account, continue to the website and try logging in. If you can't remember your password, you can also reset it there.",
+                  id: "NuGNEF",
+                  description:
+                    "Instructions on what to do if user doesn't know if they have a GCKey",
+                })}
+              </p>
+              <p data-h2-margin="base(x.5, 0, 0, 0)">
+                {intl.formatMessage(
+                  {
+                    defaultMessage:
+                      "<strong>Don't have a GCKey account?</strong> <a>Register for one</a>.",
+                    id: "q53yRm",
+                    description:
+                      "Instruction on what to do if user does not have a GCKey",
+                  },
+                  {
+                    a: (chunks: React.ReactNode) =>
+                      buildExternalLink(loginPath, chunks),
+                  },
+                )}
+              </p>
+            </>
+          ) : (
+            <>
+              {/* IAP copy */}
+              <p>
+                {intl.formatMessage({
+                  defaultMessage:
+                    "In order to get started on your profile with the IT Indigenous Apprenticeship Program, you <strong>first have to register for the platform using GCKey</strong>.",
+                  id: "LVPNZ3",
+                  description:
+                    "Instructions on how to register with GCKey - IAP variant",
+                })}
+              </p>
+              <p data-h2-margin="base(x.5, 0, 0, 0)">
+                {intl.formatMessage(
+                  {
+                    defaultMessage:
+                      "GCKey is a service that allows you to log into <a>many different government services</a> with a single username and password. The button below will take you to GCKey’s website, where you’ll be able to create an account if you don’t already have one. Once you’ve registered for GCKey, you’ll be brought back here to complete your profile.",
+                    id: "cPgH94",
+                    description:
+                      "Instructions on how to register with GCKey paragraph 2- IAP variant",
+                  },
+                  {
+                    a: (chunks: React.ReactNode) =>
+                      buildExternalLink(
+                        localeState.locale === "en"
+                          ? "https://clegc-gckey.gc.ca/j/eng/ES-01?ReqID=s232921cb5fd612440c0bcab640d5432d02168eb26"
+                          : "https://clegc-gckey.gc.ca/j/fra/ES-01?ReqID=s232921cb5fd612440c0bcab640d5432d02168eb26",
+                        chunks,
+                      ),
+                  },
+                )}
+              </p>
+              <p data-h2-margin="base(x.5, 0, 0, 0)">
+                {intl.formatMessage(
+                  {
+                    defaultMessage:
+                      "<strong>Already have a GCKey account?</strong> <a>Login instead</a>.",
+                    id: "xfYpNf",
+                    description:
+                      "Instruction on what to do if user already has a GCKey",
+                  },
+                  {
+                    a: (chunks: React.ReactNode) =>
+                      buildExternalLink(loginPath, chunks),
+                  },
+                )}
+              </p>
+              <p data-h2-margin="base(x1, 0, 0, 0)">
+                {intl.formatMessage({
+                  defaultMessage:
+                    "<strong>Heads up!</strong> Before we send you off to set up your GCKey credentials, we ask that you take another look at the program's <strong>eligibility criteria</strong> to ensure that you meet the requirements.",
+                  id: "F1mBCq",
+                  description:
+                    "A request for the user to review the IAP eligibility criteria",
+                })}
+              </p>
+              <p data-h2-margin="base(x2, 0, 0, 0)">
+                {intl.formatMessage(
+                  {
+                    defaultMessage:
+                      "If you have questions concerning this step, or if you are unsure about how to proceed, please feel free to reach out to our support team at <a>edsc.pda-iap.esdc@hrsdc-rhdcc.gc.ca</a>.",
+                    id: "lRUGPY",
+                    description:
+                      "How to get help with logging in - IAP variant",
+                  },
+                  {
+                    a: (chunks: React.ReactNode) =>
+                      buildExternalLink(
+                        "mailto:edsc.pda-iap.esdc@hrsdc-rhdcc.gc.ca",
+                        chunks,
+                      ),
+                  },
+                )}
+              </p>
+            </>
+          )}
+
           <hr data-h2-margin="base(x2, 0)" />
           <div
             data-h2-display="base(flex)"
