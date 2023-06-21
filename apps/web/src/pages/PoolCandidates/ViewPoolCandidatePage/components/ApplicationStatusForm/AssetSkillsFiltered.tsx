4189331f48dae72e3733ccc08b127a8693b71be4
vd1992 <40485260+vd1992@users.noreply.github.com>
2023-06-21 11:34:14 -0600

    remove function

--- apps/web/src/pages/PoolCandidates/ViewPoolCandidatePage/components/ApplicationStatusForm/AssetSkillsFiltered.tsx
+++ apps/web/src/pages/PoolCandidates/ViewPoolCandidatePage/components/ApplicationStatusForm/AssetSkillsFiltered.tsx
@@ -18,62 +18,53 @@ const AssetSkillsFiltered = ({
 }: AssetSkillsFilteredProps) => {
   const intl = useIntl();
 
-  // conditional rendering function, dependent on input array length
-  const renderAssetsSkillTree = (assetSkills: Skill[]) => {
-    if (assetSkills.length > 0) {
-      return (
-        <>
-          <p>
-            {intl.formatMessage({
-              defaultMessage: "Represented by the following experiences:",
-              id: "mDowK/",
-              description:
-                "Lead in text for experiences that represent the users skills",
-            })}
-          </p>
-          {assetSkills.map((optionalTechnicalSkill) => (
-            <SkillTree
-              key={optionalTechnicalSkill.id}
-              skill={optionalTechnicalSkill}
-              experiences={experiences}
-              showDisclaimer
-              hideConnectButton
-              hideEdit
-              disclaimerMessage={
-                <p>
-                  {intl.formatMessage({
-                    defaultMessage:
-                      "There are no experiences attached to this skill.",
-                    id: "XrfkBm",
-                    description:
-                      "Message displayed when no experiences have been attached to a skill",
-                  })}
-                </p>
-              }
-            />
-          ))}
-        </>
-      );
-    }
-
-    return (
-      <Well>
-        {intl.formatMessage({
-          defaultMessage: "There are no asset skills attached.",
-          id: "VeiXJP",
-          description:
-            "Informing that a section is empty of any associated asset skills.",
-        })}
-      </Well>
-    );
-  };
-
   const experiencesSkillIds = getExperiencesSkillIds(experiences);
   const usedAssetsSkills = poolAssetSkills.filter((assetSkill) =>
     experiencesSkillIds.includes(assetSkill.id),
   );
 
-  return <>{renderAssetsSkillTree(usedAssetsSkills)}</>;
+  return usedAssetsSkills.length > 0 ? (
+    <>
+      <p>
+        {intl.formatMessage({
+          defaultMessage: "Represented by the following experiences:",
+          id: "mDowK/",
+          description:
+            "Lead in text for experiences that represent the users skills",
+        })}
+      </p>
+      {usedAssetsSkills.map((optionalTechnicalSkill) => (
+        <SkillTree
+          key={optionalTechnicalSkill.id}
+          skill={optionalTechnicalSkill}
+          experiences={experiences}
+          showDisclaimer
+          hideConnectButton
+          hideEdit
+          disclaimerMessage={
+            <p>
+              {intl.formatMessage({
+                defaultMessage:
+                  "There are no experiences attached to this skill.",
+                id: "XrfkBm",
+                description:
+                  "Message displayed when no experiences have been attached to a skill",
+              })}
+            </p>
+          }
+        />
+      ))}
+    </>
+  ) : (
+    <Well>
+      {intl.formatMessage({
+        defaultMessage: "There are no asset skills attached.",
+        id: "VeiXJP",
+        description:
+          "Informing that a section is empty of any associated asset skills.",
+      })}
+    </Well>
+  );
 };
 
 export default AssetSkillsFiltered;
