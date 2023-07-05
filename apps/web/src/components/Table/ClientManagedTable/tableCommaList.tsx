ed7e4de78d0094ed66f803b3079dbc9905ef7691
Yonathan K <yoni.kid15@gmail.com>
2023-07-05 15:48:42 -0400

    Use join function on comma separated list

--- apps/web/src/components/Table/ClientManagedTable/tableCommaList.tsx
+++ apps/web/src/components/Table/ClientManagedTable/tableCommaList.tsx
@@ -5,15 +5,7 @@ interface TableCommaListProps {
 }
 
 const TableCommaList = ({ list }: TableCommaListProps) => {
-  return list.length > 0 ? (
-    <p>
-      {list.map((item, index) => {
-        if (index + 1 === list.length) return item;
-
-        return `${item}, `;
-      })}
-    </p>
-  ) : null;
+  return list.length > 0 ? <p>{list.join(", ")}</p> : null;
 };
 
 const tableCommaList = (props: TableCommaListProps) => (
