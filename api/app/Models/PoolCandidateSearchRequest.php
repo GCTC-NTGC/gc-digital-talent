667ee89d7f24f14339effedc970afc25a04d5235
vd1992 <40485260+vd1992@users.noreply.github.com>
2023-07-06 09:28:21 -0600

    add columns, remove getter

--- api/app/Models/PoolCandidateSearchRequest.php
+++ api/app/Models/PoolCandidateSearchRequest.php
@@ -2,15 +2,11 @@
 
 namespace App\Models;
 
-use Carbon\CarbonImmutable;
-use Database\Helpers\ApiEnums;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Carbon\Carbon;
-use UnexpectedValueException;
 
 /**
  * Class PoolCandidateSearchRequest
@@ -24,6 +20,8 @@ use UnexpectedValueException;
  * @property string $pool_candidate_filter_id
  * @property boolean $was_empty
  * @property string $admin_notes
+ * @property string $request_status
+ * @property int $request_status_weight
  * @property Illuminate\Support\Carbon $created_at
  * @property Illuminate\Support\Carbon $updated_at
  * @property Illuminate\Support\Carbon $deleted_at
@@ -70,20 +68,11 @@ class PoolCandidateSearchRequest extends Model
      */
     public static function scopeSearchRequestStatus(Builder $query, ?array $searchRequestStatuses)
     {
-        // currently status is either done or pending, so selecting both is the same as doing nothing
-        if (empty($searchRequestStatuses) || count($searchRequestStatuses) >= 2) {
+        if (empty($searchRequestStatuses)) {
             return $query;
         }
 
-        // $searchRequestStatuses comes from enum PoolCandidateSearchStatus
-        // status is based off field done_at, a getter found below
-        if ($searchRequestStatuses[0] == ApiEnums::POOL_CANDIDATE_SEARCH_STATUS_PENDING) {
-            $query->whereDate('done_at', '>', Carbon::now())
-                ->orWhereNull('done_at');
-        }
-        if ($searchRequestStatuses[0] == ApiEnums::POOL_CANDIDATE_SEARCH_STATUS_DONE) {
-            $query->whereDate('done_at', '<=', Carbon::now());
-        }
+        $query->whereIn('request_status', $searchRequestStatuses);
         return $query;
     }
 
@@ -190,27 +179,5 @@ class PoolCandidateSearchRequest extends Model
             });
         }
         return $query;
-    }
-
-    /**
-     * Getters/Mutators
-     */
-    public function getStatusAttribute(): string
-    {
-        $thisDoneAt = $this->done_at;
-        if (!is_null($thisDoneAt) && $thisDoneAt->isPast())
-            return ApiEnums::POOL_CANDIDATE_SEARCH_STATUS_DONE;
-        else
-            return ApiEnums::POOL_CANDIDATE_SEARCH_STATUS_PENDING;
-    }
-
-    public function setStatusAttribute($statusInput): void
-    {
-        if ($statusInput == ApiEnums::POOL_CANDIDATE_SEARCH_STATUS_DONE)
-            $this->done_at = CarbonImmutable::now();
-        else if ($statusInput == ApiEnums::POOL_CANDIDATE_SEARCH_STATUS_PENDING)
-            $this->done_at = null;
-        else
-            throw new UnexpectedValueException("status");
     }
 }
