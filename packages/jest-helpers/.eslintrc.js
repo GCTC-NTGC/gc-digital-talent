9a608556f7b17a57b937398b0c7167213a84b880
Eric Sizer <eric.sizer@gmail.com>
2023-07-26 10:59:26 -0400

    fix jest-helpers package exports

--- packages/jest-helpers/.eslintrc.js
+++ packages/jest-helpers/.eslintrc.js
@@ -5,5 +5,7 @@ module.exports = {
   rules: {
     // this package does not have a translation script
     "formatjs/blocklist-elements": ["error", ["literal"]],
+    // This package does not contain stories
+    "import/no-unused-modules": [1, { unusedExports: true, ignoreExports: ["src/index.{ts,tsx}"] }],
   },
 };
