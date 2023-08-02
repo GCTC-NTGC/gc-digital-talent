6917c3b51c568422d5def1cab84c3eb78943e2a5
Eric Sizer <eric.sizer@gmail.com>
2023-08-02 15:48:45 -0400

    attempt with additional typenames

--- apps/web/src/pages/Applications/ApplicationCareerTimelinePage/ApplicationCareerTimelinePage.tsx
+++ apps/web/src/pages/Applications/ApplicationCareerTimelinePage/ApplicationCareerTimelinePage.tsx
@@ -35,6 +35,7 @@ import ExperienceSortAndFilter, {
 } from "~/components/ExperienceSortAndFilter/ExperienceSortAndFilter";
 import { sortAndFilterExperiences } from "~/components/ExperienceSortAndFilter/sortAndFilterUtil";
 
+import { OperationContext } from "urql";
 import { ApplicationPageProps } from "../ApplicationApi";
 import { useApplicationContext } from "../ApplicationContext";
 
@@ -432,6 +433,17 @@ export const ApplicationCareerTimeline = ({
   );
 };
 
+const context: Partial<OperationContext> = {
+  additionalTypenames: [
+    "AwardExperience",
+    "CommunityExperience",
+    "EducationExperience",
+    "PersonalExperience",
+    "WorkExperience",
+  ], // This lets urql know when to invalidate cache if request returns empty list. https://formidable.com/open-source/urql/docs/basics/document-caching/#document-cache-gotchas
+  requestPolicy: "cache-first",
+};
+
 const ApplicationCareerTimelinePage = () => {
   const { applicationId } = useParams();
   const [
@@ -453,7 +465,7 @@ const ApplicationCareerTimelinePage = () => {
       error: experienceError,
     },
   ] = useGetMyExperiencesQuery({
-    requestPolicy: "cache-first",
+    context,
   });
 
   const application = applicationData?.poolCandidate;
