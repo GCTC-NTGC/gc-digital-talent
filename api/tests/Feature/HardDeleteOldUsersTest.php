5e5d0258445e2a2e113598b28843d3e819cb9523
JamesHuf <jamiehufnagel@hotmail.com>
2023-08-21 13:44:34 -0700

    Add basic tests for command

---
+++ api/tests/Feature/HardDeleteOldUsersTest.php
@@ -0,0 +1,59 @@
+<?php
+
+use App\Models\User;
+use App\Console\Commands\HardDeleteOldUsers;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Nuwave\Lighthouse\Testing\RefreshesSchemaCache;
+use Nuwave\Lighthouse\Testing\MakesGraphQLRequests;
+use Tests\TestCase;
+
+class HardDeleteOldUsersTest extends TestCase
+{
+    use RefreshDatabase;
+    use MakesGraphQLRequests;
+    use RefreshesSchemaCache;
+
+    protected $adminUser;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->seed(RolePermissionSeeder::class);
+    }
+
+    public function testActiveUserIsNotDestroyed(): void
+    {
+        User::factory()->create();
+
+        Artisan::call(HardDeleteOldUsers::class);
+
+        $this->assertEquals(1, User::count());
+    }
+
+    public function testRecentlyDeletedUserIsNotDestroyed(): void
+    {
+        $deletedDate = Carbon::now()->subYears(4);
+        User::factory()->create([
+            'deleted_at' => $deletedDate
+        ]);
+
+        Artisan::call(HardDeleteOldUsers::class);
+
+        $this->assertEquals(1, User::withTrashed()->count());
+    }
+
+    public function testOlderDeletedUserIsDestroyed(): void
+    {
+        $deletedDate = Carbon::now()->subYears(5);
+        User::factory()->create([
+            'deleted_at' => $deletedDate
+        ]);
+
+        Artisan::call(HardDeleteOldUsers::class);
+
+        $this->assertEquals(0, User::withTrashed()->count());
+    }
+}
