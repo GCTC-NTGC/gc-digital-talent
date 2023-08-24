28a04213719e9dcfb6818876ff3a3abb8fc0e124
Josh Beveridge <git@joshbeveridge.ca>
2023-08-24 14:04:25 -0400

    Modifies the styleguide story for testing purposes

--- packages/ui/src/components/Styleguide/Styleguide.stories.tsx
+++ packages/ui/src/components/Styleguide/Styleguide.stories.tsx
@@ -1,30 +1,20 @@
-import React from "react";
-import type { ComponentMeta, ComponentStory } from "@storybook/react";
-import { faker } from "@faker-js/faker";
+import type { Meta, StoryObj } from "@storybook/react";
 
 import Styleguide from "./Styleguide";
 
-type ComponentType = typeof Styleguide;
-type Meta = ComponentMeta<ComponentType>;
-type Story = ComponentStory<ComponentType>;
-
-faker.seed(0);
-
-export default {
+const meta: Meta<typeof Styleguide> = {
   component: Styleguide,
   title: "Components/Styleguide",
-  args: {
-    content: faker.lorem.sentences(2),
-  },
-} as Meta;
-
-const Template: Story = (args) => {
-  const { content } = args;
-  return (
-    <div>
-      <Styleguide>{content}</Styleguide>
-    </div>
-  );
 };
 
-export const Default = Template.bind({});
+export default meta;
+type Story = StoryObj<typeof Styleguide>;
+
+export const Primary: Story = {
+  render: () => (
+    <Styleguide>
+      <p>Hello</p>
+      <br />
+    </Styleguide>
+  ),
+};
