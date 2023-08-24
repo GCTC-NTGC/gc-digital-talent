bf6a78f7a8482f8b3869555844da60c90df157a3
Eric Sizer <eric.sizer@gmail.com>
2023-08-24 08:18:43 -0400

    fix message ids

--- packages/i18n/src/messages/localizedConstants.tsx
+++ packages/i18n/src/messages/localizedConstants.tsx
@@ -1769,7 +1769,7 @@ export const getIndigenousCommunity = (
 const technicalSkillLevels = defineMessages({
   [SkillLevel.Beginner]: {
     defaultMessage: "Entry-level",
-    id: "wS7hx7",
+    id: "AOJjMk",
     description: "The technical skill level for beginner",
   },
   [SkillLevel.Intermediate]: {
@@ -1779,7 +1779,7 @@ const technicalSkillLevels = defineMessages({
   },
   [SkillLevel.Expert]: {
     defaultMessage: "Advanced",
-    id: "ouZ7ji",
+    id: "5/C1lJ",
     description: "The technical skill level for expert",
   },
   [SkillLevel.Lead]: {
@@ -1802,25 +1802,25 @@ const technicalSkillLevelDefinitions = defineMessages({
   [SkillLevel.Beginner]: {
     defaultMessage:
       "Demonstrated ability to consistently deliver on tasks of low complexity under minimal supervision and tasks of moderate complexity under strong supervision or with direct mentoring. Usually associated with junior or entry-level roles.",
-    id: "wS7hx7",
+    id: "dXiRk8",
     description: "The technical skill level definition for beginner",
   },
   [SkillLevel.Intermediate]: {
     defaultMessage:
       "Demonstrated ability to consistently deliver on tasks of low to moderate complexity under minimal supervision and tasks of higher-level complexity under moderate supervision and mentoring. Usually associated with intermediate roles.",
-    id: "rt4mVU",
+    id: "/62GQm",
     description: "The technical skill level definition for intermediate",
   },
   [SkillLevel.Expert]: {
     defaultMessage:
       "Demonstrated ability to scope, undertake, and deliver on tasks of moderate to high complexity under minimal levels of supervision. Demonstrated ability to mentor others in lower levels of skill development, either through direct supervision or by setting examples and providing explanations. Usually associated with senior technical advisor or entry-level supervisory roles.",
-    id: "ouZ7ji",
+    id: "EPEKU2",
     description: "The technical skill level definition for expert",
   },
   [SkillLevel.Lead]: {
     defaultMessage:
       "Demonstrated ability to identify, scope, undertake, and deliver on tasks of significant complexity with wide areas of impact, even in challenging circumstances. Demonstrated ability to lead the development of tasks, and potentially teams, for the organization in this area. Usually associated with manager or technical lead roles.",
-    id: "Glb1Te",
+    id: "fdv2bM",
     description: "The technical skill level definition for lead",
   },
 });
@@ -1837,22 +1837,22 @@ export const getTechnicalSkillLevelDefinition = (
 const behaviouralSkillLevels = defineMessages({
   [SkillLevel.Beginner]: {
     defaultMessage: "In early development",
-    id: "wS7hx7",
+    id: "K9SPL+",
     description: "The behavioural skill level for beginner",
   },
   [SkillLevel.Intermediate]: {
     defaultMessage: "Moderately developed",
-    id: "rt4mVU",
+    id: "clDpR4",
     description: "The behavioural skill level for intermediate",
   },
   [SkillLevel.Expert]: {
     defaultMessage: "Strongly developed",
-    id: "ouZ7ji",
+    id: "zdxy8s",
     description: "The behavioural skill level for expert",
   },
   [SkillLevel.Lead]: {
     defaultMessage: "Deep-level mastery",
-    id: "Glb1Te",
+    id: "/OQLmo",
     description: "The behavioural skill level for lead",
   },
 });
@@ -1870,25 +1870,25 @@ const behaviouralSkillLevelDefinitions = defineMessages({
   [SkillLevel.Beginner]: {
     defaultMessage:
       "Demonstrated, but consistency varies significantly by audience and circumstance. Intermittently demonstrated in daily situations and inconsistently demonstrated with certain audiences or under conditions of stress or in moderately challenging situations. Potential is there, but there is learning to be done.",
-    id: "wS7hx7",
+    id: "Y3FL2a",
     description: "The behavioural skill level definition for beginner",
   },
   [SkillLevel.Intermediate]: {
     defaultMessage:
       "Frequently demonstrated as well-developed in most daily situations with most audiences, and intermittently demonstrated under conditions of stress and in moderately challenging situations.",
-    id: "rt4mVU",
+    id: "lYtXQJ",
     description: "The behavioural skill level definition for intermediate",
   },
   [SkillLevel.Expert]: {
     defaultMessage:
       "Consistently demonstrated as well-developed in daily conditions with all audiences, frequently demonstrated in situations of stress and in moderately challenging situations with most audiences, and intermittently demonstrated under conditions of significant stress and in challenging situations.",
-    id: "ouZ7ji",
+    id: "Ko0JX/",
     description: "The behavioural skill level definition for expert",
   },
   [SkillLevel.Lead]: {
     defaultMessage:
       "Consistently demonstrated as well-developed with all audiences, even under conditions of significant stress and in challenging situations. Behaviour is adapted and nuanced to the situation without drifting away from its core value.",
-    id: "Glb1Te",
+    id: "CrzH4k",
     description: "The behavioural skill level definition for lead",
   },
 });
