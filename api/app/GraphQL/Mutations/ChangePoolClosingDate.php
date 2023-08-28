904eff72231b1031015d1fd38e610f78bb1a3256
vd1992 <40485260+vd1992@users.noreply.github.com>
2023-08-28 11:13:51 -0600

    skill validation re-opening pools

--- api/app/GraphQL/Mutations/ChangePoolClosingDate.php
+++ api/app/GraphQL/Mutations/ChangePoolClosingDate.php
@@ -3,6 +3,9 @@
 namespace App\GraphQL\Mutations;
 
 use App\Models\Pool;
+use App\Models\Skill;
+use Carbon\Carbon;
+use Nuwave\Lighthouse\Exceptions\ValidationException;
 
 final class ChangePoolClosingDate
 {
@@ -13,8 +16,32 @@ final class ChangePoolClosingDate
      */
     public function __invoke($_, array $args)
     {
-        $pool = Pool::find($args['id']);
-        $pool->update(['closing_date' => $args['new_closing_date']]);
+        $pool = Pool::find($args['id'])->load([
+            'essentialSkills' => function ($query) {
+                $query->withTrashed(); // eager load soft deleted skills too
+            },
+            'nonessentialSkills' => function ($query) {
+                $query->withTrashed();
+            }
+        ]);
+        $newClosingDate = $args['new_closing_date'];
+        $now = Carbon::now();
+
+        // check for deleted skills if attempting to re-open a closed pool
+        if ($pool->closing_date <= $now) {
+
+            $deletedSkillsIds = Skill::onlyTrashed()->get()->pluck('id')->toArray();
+            $essentialSkillsIds = $pool->essentialSkills->pluck('id')->toArray();
+            $nonessentialSkillsIds = $pool->nonessentialSkills->pluck('id')->toArray();
+            $poolSkillsIds = array_merge($essentialSkillsIds, $nonessentialSkillsIds);
+            $expiredPoolSkills = array_intersect($poolSkillsIds, $deletedSkillsIds);
+
+            if (count($expiredPoolSkills) > 0) {
+                throw ValidationException::withMessages(['CannotReopenUsingDeletedSkill']);
+            }
+        }
+
+        $pool->update(['closing_date' => $newClosingDate]);
         return $pool;
     }
 }
