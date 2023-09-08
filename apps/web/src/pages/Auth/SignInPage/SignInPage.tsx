55720ca05e95719e09ad0fd30c10228f9ec9d54e
Peter Giles <8978655+petertgiles@users.noreply.github.com>
2023-09-08 15:41:33 -0400

    factor out steps

--- apps/web/src/pages/Auth/SignInPage/SignInPage.tsx
+++ apps/web/src/pages/Auth/SignInPage/SignInPage.tsx
@@ -4,8 +4,6 @@ import { useSearchParams } from "react-router-dom";
 import SparklesIcon from "@heroicons/react/24/outline/SparklesIcon";
 import ArrowLeftOnRectangleIcon from "@heroicons/react/24/outline/ArrowLeftOnRectangleIcon";
 import InformationCircleIcon from "@heroicons/react/24/outline/InformationCircleIcon";
-import ArrowRightCircleIcon from "@heroicons/react/24/solid/ArrowRightCircleIcon";
-import ArrowDownCircleIcon from "@heroicons/react/24/solid/ArrowDownCircleIcon";
 
 import { Accordion, Heading, Link, Separator } from "@gc-digital-talent/ui";
 import { useApiRoutes } from "@gc-digital-talent/auth";
@@ -14,6 +12,7 @@ import { useTheme } from "@gc-digital-talent/theme";
 
 import Hero from "~/components/Hero/Hero";
 import SEO from "~/components/SEO/SEO";
+import Step from "~/components/Steps/Steps";
 import useRoutes from "~/hooks/useRoutes";
 import useBreadcrumbs from "~/hooks/useBreadcrumbs";
 import step1Image from "~/assets/img/sign-in-steps-1.jpg";
@@ -26,55 +25,6 @@ const buildExternalLink = (path: string, chunks: React.ReactNode) => (
     {chunks}
   </Link>
 );
-
-export interface StepProps {
-  image: string;
-  includeArrow?: boolean;
-  children: React.ReactNode;
-}
-
-const Step = ({ image, includeArrow = true, children }: StepProps) => {
-  return (
-    <div
-      data-h2-display="base(flex)"
-      data-h2-flex-direction="base(column)"
-      data-h2-flex-item="base(1of1) p-tablet(1of4)"
-    >
-      <div data-h2-display="base(flex)" data-h2-flex-direction="base(row)">
-        {image && <img src={image} alt="" />}
-        {includeArrow && (
-          <ArrowRightCircleIcon
-            data-h2-display="base(none) p-tablet(block)"
-            data-h2-vertical-align="base(middle)"
-            data-h2-color="base(black.lighter)"
-            data-h2-height="base(auto)"
-            data-h2-width="base(x1.5)"
-            data-h2-overflow="base(visible)"
-            data-h2-padding-left="base(x0.5)"
-          />
-        )}
-      </div>
-      <div
-        data-h2-flex-grow="base(1)"
-        data-h2-flex="base(1)"
-        data-h2-text-indent="base(-1em)"
-        data-h2-padding-left="base(1em)"
-      >
-        {children}
-      </div>
-      {includeArrow && (
-        <ArrowDownCircleIcon
-          data-h2-display="base(block) p-tablet(none)"
-          data-h2-color="base(black.lighter)"
-          data-h2-height="base(auto)"
-          data-h2-width="base(x1.5)"
-          data-h2-margin="base(auto)"
-          data-h2-padding="base(x0.5, 0)"
-        />
-      )}
-    </div>
-  );
-};
 
 const SignInPage = () => {
   const intl = useIntl();
--- apps/web/src/pages/Auth/SignUpPage/SignUpPage.tsx
+++ apps/web/src/pages/Auth/SignUpPage/SignUpPage.tsx
@@ -2,8 +2,6 @@ import React, { useEffect } from "react";
 import { useIntl } from "react-intl";
 import { useSearchParams } from "react-router-dom";
 import InformationCircleIcon from "@heroicons/react/24/outline/InformationCircleIcon";
-import ArrowRightCircleIcon from "@heroicons/react/24/solid/ArrowRightCircleIcon";
-import ArrowDownCircleIcon from "@heroicons/react/24/solid/ArrowDownCircleIcon";
 import MapIcon from "@heroicons/react/24/outline/MapIcon";
 import PencilSquareIcon from "@heroicons/react/24/outline/PencilSquareIcon";
 import ShieldCheckIcon from "@heroicons/react/24/outline/ShieldCheckIcon";
@@ -15,6 +13,7 @@ import { useTheme } from "@gc-digital-talent/theme";
 
 import Hero from "~/components/Hero/Hero";
 import SEO from "~/components/SEO/SEO";
+import Step from "~/components/Steps/Steps";
 import useRoutes from "~/hooks/useRoutes";
 import useBreadcrumbs from "~/hooks/useBreadcrumbs";
 import createStep1Image from "~/assets/img/sign-up-create-step-1.jpg";
@@ -35,55 +34,6 @@ const buildExternalLink = (path: string, chunks: React.ReactNode) => (
     {chunks}
   </Link>
 );
-
-export interface StepProps {
-  image: string;
-  includeArrow?: boolean;
-  children: React.ReactNode;
-}
-
-const Step = ({ image, includeArrow = true, children }: StepProps) => {
-  return (
-    <div
-      data-h2-display="base(flex)"
-      data-h2-flex-direction="base(column)"
-      data-h2-flex-item="base(1of1) p-tablet(1of4)"
-    >
-      <div data-h2-display="base(flex)" data-h2-flex-direction="base(row)">
-        {image && <img src={image} alt="" />}
-        {includeArrow && (
-          <ArrowRightCircleIcon
-            data-h2-display="base(none) p-tablet(block)"
-            data-h2-vertical-align="base(middle)"
-            data-h2-color="base(black.lighter)"
-            data-h2-height="base(auto)"
-            data-h2-width="base(x1.5)"
-            data-h2-overflow="base(visible)"
-            data-h2-padding-left="base(x0.5)"
-          />
-        )}
-      </div>
-      <div
-        data-h2-flex-grow="base(1)"
-        data-h2-flex="base(1)"
-        data-h2-text-indent="base(-1em)"
-        data-h2-padding-left="base(1em)"
-      >
-        {children}
-      </div>
-      {includeArrow && (
-        <ArrowDownCircleIcon
-          data-h2-display="base(block) p-tablet(none)"
-          data-h2-color="base(black.lighter)"
-          data-h2-height="base(auto)"
-          data-h2-width="base(x1.5)"
-          data-h2-margin="base(auto)"
-          data-h2-padding="base(x0.5, 0)"
-        />
-      )}
-    </div>
-  );
-};
 
 const SignUpPage = () => {
   const intl = useIntl();
