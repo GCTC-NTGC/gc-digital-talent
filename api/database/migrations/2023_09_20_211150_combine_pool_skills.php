3b9aa52e9b387d7995a293045b9701d5382a5c78
vd1992 <40485260+vd1992@users.noreply.github.com>
2023-09-20 16:06:25 -0600

    migration for pools_skills

---
+++ api/database/migrations/2023_09_20_211150_combine_pool_skills.php
@@ -0,0 +1,91 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        // create the combined table
+        Schema::create('pools_skills', function (Blueprint $table) {
+            $table->uuid('skill_id');
+            $table->foreign('skill_id')->references('id')->on('skills');
+            $table->uuid('pool_id');
+            $table->foreign('pool_id')->references('id')->on('pools');
+            $table->unique(['skill_id', 'pool_id']);
+            $table->timestamps();
+            $table->string('type');
+        });
+
+        // fill the new table with pool-skill rows using the prior two tables
+        $essentialPoolSkills = DB::table('pools_essential_skills')->get();
+        foreach ($essentialPoolSkills as $instance) {
+            DB::table('pools_skills')->insert([
+                'skill_id' => $instance->skill_id,
+                'pool_id' => $instance->pool_id,
+                'type' => 'ESSENTIAL',
+            ]);
+        }
+        $nonessentialPoolSkills = DB::table('pools_nonessential_skills')->get();
+        foreach ($nonessentialPoolSkills as $instance) {
+            DB::table('pools_skills')->insert([
+                'skill_id' => $instance->skill_id,
+                'pool_id' => $instance->pool_id,
+                'type' => 'NONESSENTIAL',
+            ]);
+        }
+
+        // drop the two tables
+        Schema::dropIfExists('pools_essential_skills');
+        Schema::dropIfExists('pools_nonessential_skills');
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        // create again the two tables that were removed
+        Schema::create('pools_essential_skills', function (Blueprint $table) {
+            $table->uuid('skill_id')->nullable();
+            $table->foreign('skill_id')->references('id')->on('skills');
+            $table->uuid('pool_id')->nullable();
+            $table->foreign('pool_id')->references('id')->on('pools');
+            $table->unique(['skill_id', 'pool_id']);
+            $table->timestamps();
+        });
+        Schema::create('pools_nonessential_skills', function (Blueprint $table) {
+            $table->uuid('skill_id')->nullable();
+            $table->foreign('skill_id')->references('id')->on('skills');
+            $table->uuid('pool_id')->nullable();
+            $table->foreign('pool_id')->references('id')->on('pools');
+            $table->unique(['skill_id', 'pool_id']);
+            $table->timestamps();
+        });
+
+        // fill the two tables using the combined one
+        $essentialPoolSkills = DB::table('pools_skills')->where('type', 'ESSENTIAL')->get();
+        foreach ($essentialPoolSkills as $instance) {
+            DB::table('pools_essential_skills')->insert([
+                'skill_id' => $instance->skill_id,
+                'pool_id' => $instance->pool_id,
+            ]);
+        }
+        $nonessentialPoolSkills = DB::table('pools_skills')->where('type', 'NONESSENTIAL')->get();
+        foreach ($nonessentialPoolSkills as $instance) {
+            DB::table('pools_nonessential_skills')->insert([
+                'skill_id' => $instance->skill_id,
+                'pool_id' => $instance->pool_id,
+            ]);
+        }
+
+        // drop the combined table
+        Schema::dropIfExists('pools_skills');
+    }
+};
