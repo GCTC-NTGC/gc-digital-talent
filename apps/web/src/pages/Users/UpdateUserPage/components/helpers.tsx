777073eb26724321ce90f7a71e8373eb8d788cb2
Eric Sizer <eric.sizer@gmail.com>
2023-09-26 13:20:06 -0400

    replace team roles table

--- apps/web/src/pages/Users/UpdateUserPage/components/helpers.tsx
+++ apps/web/src/pages/Users/UpdateUserPage/components/helpers.tsx
@@ -1,16 +1,35 @@
 import React from "react";
+import { IntlShape } from "react-intl";
 
 import { Role, User } from "@gc-digital-talent/graphql";
-import { Pill } from "@gc-digital-talent/ui";
+import { Link, Pill } from "@gc-digital-talent/ui";
+import { getLocalizedName } from "@gc-digital-talent/i18n";
+import { notEmpty } from "@gc-digital-talent/helpers";
 
 import RemoveIndividualRoleDialog from "./RemoveIndividualRoleDialog";
-import { UpdateUserFunc } from "../types";
+import { TeamAssignment, UpdateUserFunc, UpdateUserHandler } from "../types";
+import EditTeamRoleDialog from "./EditTeamRoleDialog";
+import RemoveTeamRoleDialog from "./RemoveTeamRoleDialog";
 
 export function roleCell(displayName: string) {
   return (
     <Pill color="black" mode="solid">
       {displayName}
     </Pill>
+  );
+}
+
+export function teamRolesCell(displayNames: string[]) {
+  return (
+    <div data-h2-display="base(flex)" data-h2-gap="base(0, x.25)">
+      {displayNames.map((displayName) => {
+        return (
+          <Pill color="black" mode="solid" key={displayName}>
+            {displayName}
+          </Pill>
+        );
+      })}
+    </div>
   );
 }
 
@@ -26,4 +45,48 @@ export function actionCell(
       onUpdateUser={onUpdateUser}
     />
   );
+}
+
+export function teamActionCell(
+  teamAssignment: TeamAssignment,
+  user: User,
+  handleUserUpdate: UpdateUserHandler,
+  availableRoles: Role[],
+) {
+  return (
+    <div data-h2-display="base(flex)" data-h2-gap="base(0, x.25)">
+      <EditTeamRoleDialog
+        initialRoles={teamAssignment.roles}
+        user={user}
+        team={teamAssignment.team}
+        onEditRoles={handleUserUpdate}
+        allRoles={availableRoles}
+      />
+      <RemoveTeamRoleDialog
+        roles={teamAssignment.roles}
+        user={user}
+        team={teamAssignment.team}
+        onRemoveRoles={handleUserUpdate}
+      />
+    </div>
+  );
+}
+
+export function teamCell(displayName: string, href: string) {
+  return (
+    <Link color="black" href={href}>
+      {displayName}
+    </Link>
+  );
+}
+
+export function teamRolesAccessor(
+  teamAssignment: TeamAssignment,
+  intl: IntlShape,
+) {
+  return teamAssignment.roles
+    .map((role) => getLocalizedName(role.displayName, intl), true)
+    .filter(notEmpty)
+    .sort((a, b) => a.localeCompare(b))
+    .join();
 }
