9473f3823b10a2a977fc965388c6bd93e9211b46
vd1992 <40485260+vd1992@users.noreply.github.com>
2023-09-26 12:34:31 -0600

    setup some models

--- api/app/Models/Pool.php
+++ api/app/Models/Pool.php
@@ -128,6 +128,16 @@ class Pool extends Model
             ->wherePivot('type', PoolSkillType::NONESSENTIAL->name);
     }
 
+    public function poolSkills(): HasMany
+    {
+        return $this->hasMany(PoolSkill::class);
+    }
+
+    public function assessmentSteps(): HasMany
+    {
+        return $this->hasMany(AssessmentStep::class);
+    }
+
     /**
      * Sync the essential skills in pool_skill
      *
---
+++ api/app/Models/PoolSkill.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+/**
+ * Class PoolSkill
+ *
+ * @property string $id
+ */
+class PoolSkill extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'pool_skill';
+
+    protected $keyType = 'string';
+
+    public function skill(): BelongsTo
+    {
+        return $this->belongsTo(Skill::class);
+    }
+
+    public function pool(): BelongsTo
+    {
+        return $this->belongsTo(Pool::class);
+    }
+
+    public function assessmentSteps(): BelongsToMany
+    {
+        return $this->belongsToMany(AssessmentStep::class, 'assessment_step_pool_skill');
+    }
+}
