c2d66cd2883aa60b1d7c9d93fc15d10199c9aeef
Eric Sizer <eric.sizer@gmail.com>
2023-09-26 10:29:40 -0400

    move required components up a level

---
+++ apps/web/src/components/Table/ViewButton.tsx
@@ -0,0 +1,52 @@
+import React, { AriaAttributes } from "react";
+import { useIntl } from "react-intl";
+
+import { Link, LinkProps } from "@gc-digital-talent/ui";
+
+interface TableViewItemButtonProps extends LinkProps {
+  /** The destination url. */
+  viewUrl: string;
+  /** Name of the type of object */
+  name: string;
+  /** Label for link text  */
+  hiddenLabel?: string;
+}
+
+function TableViewItemButton({
+  viewUrl,
+  name,
+  hiddenLabel,
+  ...rest
+}: TableViewItemButtonProps): React.ReactElement {
+  const intl = useIntl();
+  return (
+    <Link href={viewUrl} {...rest}>
+      {intl.formatMessage(
+        {
+          defaultMessage: "View {name} <hidden>{hiddenLabel}</hidden>",
+          id: "+wG2Ap",
+          description: "Title displayed for the View Item column.",
+        },
+        { name, hiddenLabel },
+      )}
+    </Link>
+  );
+}
+
+function tableViewItemButtonAccessor(
+  viewUrl: string,
+  name: string,
+  hiddenLabel?: string,
+  ariaLabel?: AriaAttributes["aria-label"],
+) {
+  return (
+    <TableViewItemButton
+      aria-label={ariaLabel}
+      viewUrl={viewUrl}
+      name={name}
+      hiddenLabel={hiddenLabel}
+    />
+  );
+}
+
+export default tableViewItemButtonAccessor;
