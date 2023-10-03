1e8ab521b4d59fc774bf897e894392e96c9774e7
Vachan <40485260+vd1992@users.noreply.github.com>
2023-10-03 13:29:32 -0600

    [Feature] Assessment plan backend (#8022)
    * assessment steps migrations
    
    * setup some models
    
    * setting the assessments mutation
    
    * change assessment operations
    
    * add some policy and permissions
    
    * add new permissions to test
    
    * factory + seed assessments
    
    * starting testing
    
    * don't add new permissions
    
    * missing 'find' option
    
    * more tests
    
    * add assessment type enum
    
    * add application screening enum
    
    * constrain assessment type
    
    * add descriptions to enums
    
    * add with timestamps
    
    * validation assessment step input
    
    * add markup file
    
    * remove redundancy
    
    * adjust permissions

---
+++ api/app/Policies/AssessmentStepPolicy.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Policies;
+
+use App\Enums\PoolStatus;
+use App\Models\AssessmentStep;
+use App\Models\Pool;
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+use Illuminate\Auth\Access\Response;
+
+class AssessmentStepPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can create assessments. Same permission as being able to update assessments.
+     *
+     * @param    $request: The arguments included in the request, acquired with the injectArgs lighthouse directive
+     *      We need to use this because the model hasn't been created yet so we can't read from it
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function create(User $user, $request)
+    {
+        $poolId = null;
+        if (array_key_exists('pool_id', $request)) {
+            $poolId = $request['pool_id'];
+        } else {
+            return Response::deny('Cannot find pool.');
+        }
+
+        $pool = Pool::with('team')->find($poolId);
+
+        return $pool->getStatusAttribute() === PoolStatus::DRAFT->name
+        && $user->isAbleTo('update-team-draftPool', $pool->team);
+    }
+
+    /**
+     * Determine whether the user can update or delete the assessment step
+     *
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function update(User $user, AssessmentStep $assessmentStep)
+    {
+        $assessmentStep->loadMissing('pool.team');
+
+        return $assessmentStep->pool->getStatusAttribute() === PoolStatus::DRAFT->name
+        && $user->isAbleTo('update-team-draftPool', $assessmentStep->pool->team);
+    }
+}
