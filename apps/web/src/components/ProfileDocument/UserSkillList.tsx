9bad40dc998c1bd1e753088c2dec2e1e64fd8079
Eric Sizer <eric.sizer@gmail.com>
2023-10-06 13:54:19 -0400

    add skill showcase to printed document

---
+++ apps/web/src/components/ProfileDocument/UserSkillList.tsx
@@ -0,0 +1,73 @@
+import React from "react";
+import { useIntl } from "react-intl";
+
+import { UserSkill } from "@gc-digital-talent/graphql";
+import {
+  commonMessages,
+  getBehaviouralSkillLevel,
+  getLocalizedName,
+  getTechnicalSkillLevel,
+} from "@gc-digital-talent/i18n";
+import { Heading } from "@gc-digital-talent/ui";
+
+interface UserSkillListProps {
+  technical: UserSkill[];
+  behavioural: UserSkill[];
+}
+
+const UserSkillList = ({ technical, behavioural }: UserSkillListProps) => {
+  const intl = useIntl();
+
+  return (
+    <>
+      <Heading level="h6" data-h2-font-weight="base(700)">
+        {intl.formatMessage({
+          defaultMessage: "Behavioural skills",
+          id: "Ih9aGC",
+          description: "Heading for a list of users behavioural skills",
+        })}
+      </Heading>
+      {behavioural.length > 0 ? (
+        <ol>
+          {behavioural?.map((userSkill) => (
+            <li key={userSkill.id}>
+              {getLocalizedName(userSkill.skill.name, intl)}
+              {intl.formatMessage(commonMessages.dividingColon)}
+              {intl.formatMessage(
+                getBehaviouralSkillLevel(userSkill.skillLevel ?? ""),
+              )}
+            </li>
+          ))}
+        </ol>
+      ) : (
+        <p>{intl.formatMessage(commonMessages.notProvided)}</p>
+      )}
+
+      <Heading level="h6" data-h2-font-weight="base(700)">
+        {intl.formatMessage({
+          defaultMessage: "Technical skills",
+          id: "5DsB2D",
+          description: "Heading for a list of users technical skills",
+        })}
+      </Heading>
+
+      {technical.length > 0 ? (
+        <ol>
+          {technical.map((userSkill) => (
+            <li key={userSkill.id}>
+              {getLocalizedName(userSkill.skill.name, intl)}
+              {intl.formatMessage(commonMessages.dividingColon)}
+              {intl.formatMessage(
+                getTechnicalSkillLevel(userSkill.skillLevel ?? ""),
+              )}
+            </li>
+          ))}
+        </ol>
+      ) : (
+        <p>{intl.formatMessage(commonMessages.notProvided)}</p>
+      )}
+    </>
+  );
+};
+
+export default UserSkillList;
