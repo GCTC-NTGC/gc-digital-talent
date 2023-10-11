e2a046e3d14acff7124e3c74dffb7d3234bbdc66
Peter Giles <8978655+petertgiles@users.noreply.github.com>
2023-10-11 13:25:44 -0400

    rough out sections

--- apps/web/src/pages/Pools/AssessmentPlanBuilderPage/AssessmentPlanBuilderPage.tsx
+++ apps/web/src/pages/Pools/AssessmentPlanBuilderPage/AssessmentPlanBuilderPage.tsx
@@ -1,18 +1,79 @@
 import * as React from "react";
 import { useParams } from "react-router-dom";
 import { useIntl } from "react-intl";
+import ClipboardDocumentListIcon from "@heroicons/react/24/outline/ClipboardDocumentListIcon";
 
-import { Scalars } from "@gc-digital-talent/graphql";
+import { Pool, Scalars } from "@gc-digital-talent/graphql";
+import { commonMessages, getLocalizedName } from "@gc-digital-talent/i18n";
+import {
+  Heading,
+  NotFound,
+  Pending,
+  Pill,
+  Separator,
+} from "@gc-digital-talent/ui";
 
 import useRoutes from "~/hooks/useRoutes";
 import adminMessages from "~/messages/adminMessages";
 import AdminContentWrapper from "~/components/AdminContentWrapper/AdminContentWrapper";
+import { useGetAssessmentPlanBuilderDataQuery } from "~/api/generated";
+import SEO from "~/components/SEO/SEO";
+
+import OrganizeSection from "./components/OrganizeSection";
+import SkillSummarySection from "./components/SkillSummarySection";
+
+const pageTitle = {
+  defaultMessage: "Assessment plan",
+  id: "fkYYe3",
+  description: "Title for the assessment plan builder",
+};
+
+const pageSubtitle = {
+  defaultMessage:
+    "Select, organize and define the assessments used to evaluate each skill in the advertisement. Make sure every skill is assessed at least once to complete your assessment plan.",
+  id: "SSZY5w",
+  description: "Subtitle for the assessment plan builder",
+};
+export interface AssessmentPlanBuilderProps {
+  pool: Pool;
+}
+
+export const AssessmentPlanBuilder = ({ pool }: AssessmentPlanBuilderProps) => {
+  const intl = useIntl();
+
+  return (
+    <>
+      <SEO
+        title={intl.formatMessage(pageTitle)}
+        description={intl.formatMessage(pageSubtitle)}
+      />
+      <div data-h2-container="base(left, large, 0)">
+        <Heading level="h2" Icon={ClipboardDocumentListIcon} color="primary">
+          {intl.formatMessage(pageTitle)}
+          <div data-h2-flex-grow="base(2)" />
+          <Pill bold mode="outline" color="black">
+            TODO: Status
+          </Pill>
+        </Heading>
+        <p data-h2-margin="base(x1 0)">{intl.formatMessage(pageSubtitle)}</p>
+        <Separator
+          orientation="horizontal"
+          decorative
+          data-h2-background-color="base(gray.lighter)"
+          data-h2-margin="base(x1 0)"
+        />
+        <OrganizeSection pool={pool} />
+        <SkillSummarySection pool={pool} />
+      </div>
+    </>
+  );
+};
 
 type RouteParams = {
   poolId: Scalars["ID"];
 };
 
-export const EditPoolPage = () => {
+export const AssessmentPlanBuilderPage = () => {
   const intl = useIntl();
   const { poolId } = useParams<RouteParams>();
   const routes = useRoutes();
@@ -33,15 +94,10 @@ export const EditPoolPage = () => {
     });
   }
 
-  // const [{ data, fetching, error }] = useGetEditPoolDataQuery({
-  //   variables: { poolId: poolId || "" },
-  // });
-
-  // const { isFetching, mutations } = usePoolMutations();
-
-  // const ctx = React.useMemo(() => {
-  //   return { isSubmitting: isFetching };
-  // }, [isFetching]);
+  const [{ data: queryData, fetching: queryFetching, error: queryError }] =
+    useGetAssessmentPlanBuilderDataQuery({
+      variables: { poolId: poolId || "" },
+    });
 
   const navigationCrumbs = [
     {
@@ -56,25 +112,40 @@ export const EditPoolPage = () => {
       label: intl.formatMessage(adminMessages.pools),
       url: routes.poolTable(),
     },
-    // {
-    //   label: getLocalizedName(data?.pool?.name, intl),
-    //   url: routes.poolView(poolId),
-    // },
     {
-      label: intl.formatMessage({
-        defaultMessage: "Edit<hidden> pool</hidden>",
-        id: "D6HIId",
-        description: "Edit pool breadcrumb text",
-      }),
-      url: routes.poolUpdate(poolId),
+      label: getLocalizedName(queryData?.pool?.name, intl),
+      url: routes.poolView(poolId),
+    },
+    {
+      label: intl.formatMessage(pageTitle),
+      url: routes.assessmentPlanBuilder(poolId),
     },
   ];
 
   return (
     <AdminContentWrapper crumbs={navigationCrumbs}>
-      Hello, world
+      <Pending fetching={queryFetching} error={queryError}>
+        {queryData?.pool ? (
+          <AssessmentPlanBuilder pool={queryData.pool} />
+        ) : (
+          <NotFound
+            headingMessage={intl.formatMessage(commonMessages.notFound)}
+          >
+            <p>
+              {intl.formatMessage(
+                {
+                  defaultMessage: "Pool {poolId} not found.",
+                  id: "Sb2fEr",
+                  description: "Message displayed for pool not found.",
+                },
+                { poolId },
+              )}
+            </p>
+          </NotFound>
+        )}
+      </Pending>
     </AdminContentWrapper>
   );
 };
 
-export default EditPoolPage;
+export default AssessmentPlanBuilderPage;
---
+++ apps/web/src/pages/Pools/AssessmentPlanBuilderPage/components/SkillSummarySection.tsx
@@ -0,0 +1,100 @@
+import React from "react";
+import { useIntl } from "react-intl";
+
+import { Accordion, Heading } from "@gc-digital-talent/ui";
+import { AssessmentStep, Pool } from "@gc-digital-talent/graphql";
+import { notEmpty } from "@gc-digital-talent/helpers";
+import { getLocalizedName } from "@gc-digital-talent/i18n";
+
+const sectionTitle = {
+  defaultMessage: "Skill summary",
+  id: "x",
+  description:
+    "Title for the skill summary section in the assessment plan builder",
+};
+
+export interface SkillSummarySectionProps {
+  pool: Pool;
+}
+
+const SkillSummarySection = ({ pool }: SkillSummarySectionProps) => {
+  const intl = useIntl();
+  return (
+    <>
+      <Heading level="h3">{intl.formatMessage(sectionTitle)}</Heading>
+      <p>
+        {intl.formatMessage({
+          defaultMessage:
+            "Overview of all skills and assessments planned for this pool.",
+          id: "t0OrQA",
+          description:
+            "introduction to the skill summary section in the assessment plan builder",
+        })}
+      </p>
+      <Accordion.Root type="single" mode="simple" collapsible>
+        <Accordion.Item value="one">
+          <Accordion.Header
+            headingAs="h4"
+            data-h2-font-size="base(copy)"
+            data-h2-text-decoration="base(underline)"
+          >
+            <Accordion.Trigger data-h2-font-weight="base(700)">
+              {intl.formatMessage({
+                defaultMessage:
+                  "Why are most behavioral skills left out of the initial application?",
+                id: "dVZRH3",
+                description:
+                  "first question in the skill summary section in the assessment plan builder",
+              })}
+            </Accordion.Trigger>
+          </Accordion.Header>
+          <Accordion.Content>
+            <p>
+              {intl.formatMessage({
+                defaultMessage:
+                  "On the initial application, only technical information is collected from the applicants, except for a few very special behavioural skills. This means that as the reviewer, when you see the application information, you’ll only have experience data related to the technical skills and a few very specific behavioural skills. This is done for two reasons.",
+                id: "3W5IAV",
+                description:
+                  "First paragraph of first answer of the Frequently Asked Questions for logging in",
+              })}
+            </p>
+            <p data-h2-margin-top="base(x.5)">
+              {intl.formatMessage({
+                defaultMessage:
+                  "Firstly, asking for too much information on a job application can drive away strong candidates who feel the process is too cumbersome. You’ll need to find a balance between how much you want to ask for and how long it will take an applicant to complete the process if you want the chance to recruit top candidates.",
+                id: "HUU5sY",
+                description:
+                  "Second paragraph of first answer of the Frequently Asked Questions for logging in",
+              })}
+            </p>
+            <p data-h2-margin-top="base(x.5)">
+              {intl.formatMessage({
+                defaultMessage:
+                  "Secondly, after a year-long live pilot test, managers reported that they didn’t find the self-reported information on behavioural skills to be useful. Instead, they found other forms of assessment (such as reference checks and interviews) to be far better predictors than long paragraphs about how collaborative, positive or inclusive a candidate self-assessed as being.",
+                id: "T2WOLz",
+                description:
+                  "Third paragraph of first answer of the Frequently Asked Questions for logging in",
+              })}
+            </p>
+          </Accordion.Content>
+        </Accordion.Item>
+      </Accordion.Root>
+      <div data-h2-margin-top="base(x1)">
+        {/* TODO: replace with skill summary table */}
+        {pool.assessmentSteps
+          ?.filter(notEmpty)
+          .sort((a: AssessmentStep, b: AssessmentStep) =>
+            (a.sortOrder ?? 0) > (b.sortOrder ?? 0) ? 1 : -1,
+          )
+          .map((step: AssessmentStep) => (
+            <div key={step.id}>
+              <p>{step.type}</p>
+              <p>{getLocalizedName(step.title, intl)}</p>
+            </div>
+          ))}
+      </div>
+    </>
+  );
+};
+
+export default SkillSummarySection;
