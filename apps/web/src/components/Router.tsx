158c768e69321faa866824ecb8c9dd7630894b7b
Peter Giles <8978655+petertgiles@users.noreply.github.com>
2023-10-16 16:23:07 -0400

    [Feature] Assessment plan builder placeholder page (#8225)
    * basic page
    
    * rough out sections
    
    * validator
    
    * footer buttons
    
    * dialog
    
    * fix message IDs
    
    * remove debugging code
    
    * tidy up
    
    * multi-accordions
    
    * switch navigate button to link
    
    * get rid of smart apostrophes
    
    * French translations

--- apps/web/src/components/Router.tsx
+++ apps/web/src/components/Router.tsx
@@ -532,6 +532,14 @@ const EditPoolPage = React.lazy(() =>
       ),
   ),
 );
+const AssessmentPlanBuilderPage = React.lazy(() =>
+  lazyRetry(
+    () =>
+      import(
+        /* webpackChunkName: "adminAssessmentPlanBuilderPage" */ "../pages/Pools/AssessmentPlanBuilderPage/AssessmentPlanBuilderPage"
+      ),
+  ),
+);
 const PoolLayout = React.lazy(() =>
   lazyRetry(
     () =>
@@ -1464,6 +1472,22 @@ const createRoute = (
                           ],
                         },
                       ],
+                    },
+                    {
+                      path: "plan",
+                      element: featureFlags.recordOfDecision ? (
+                        <RequireAuth
+                          roles={[
+                            ROLE_NAME.PoolOperator,
+                            ROLE_NAME.PlatformAdmin,
+                          ]}
+                          loginPath={loginPath}
+                        >
+                          <AssessmentPlanBuilderPage />
+                        </RequireAuth>
+                      ) : (
+                        <AdminErrorPage />
+                      ),
                     },
                   ],
                 },
---
+++ apps/web/src/pages/Pools/AssessmentPlanBuilderPage/components/AssessmentDetailsDialogLabels.ts
@@ -0,0 +1,40 @@
+const labels = {
+  typeOfAssessment: {
+    defaultMessage: "Type of assessment",
+    id: "3/AA3n",
+    description:
+      "Label for 'Type of assessment' input on the assessment details dialog",
+  },
+  assessmentTitleEn: {
+    defaultMessage: "English - Optional assessment title",
+    id: "/+cTZi",
+    description:
+      "Label for 'English assessment title' input on the assessment details dialog",
+  },
+  assessmentTitleFr: {
+    defaultMessage: "French - Optional assessment title",
+    id: "OoZjap",
+    description:
+      "Label for 'French assessment title' input on the assessment details dialog",
+  },
+  screeningQuestionEn: {
+    defaultMessage: "English - Question {questionNumber}",
+    id: "2op8I5",
+    description:
+      "Label for 'English screening question' input on the assessment details dialog",
+  },
+  screeningQuestionFr: {
+    defaultMessage: "French - Question {questionNumber}",
+    id: "9i4kZp",
+    description:
+      "Label for 'English screening question' input on the assessment details dialog",
+  },
+  assessedSkills: {
+    defaultMessage: "Assessed skills",
+    id: "OoZjap",
+    description:
+      "Label for 'assessed skills' input on the assessment details dialog",
+  },
+};
+
+export default labels;
