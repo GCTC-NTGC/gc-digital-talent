fce15c305b520ea1583eaae6ed692934f7702760
Peter Giles <8978655+petertgiles@users.noreply.github.com>
2023-10-18 12:48:54 -0400

    refactor sidebar

---
+++ packages/ui/src/components/Sidebar/index.ts
@@ -0,0 +1,12 @@
+import Sidebar, { type SidebarProps as TocSidebarProps } from "./Sidebar";
+import Wrapper from "./Wrapper";
+import Content from "./Content";
+
+const TableOfContents = {
+  Sidebar,
+  Wrapper,
+  Content,
+};
+
+export default TableOfContents;
+export type { TocSidebarProps };
