baf3a26b552158de08fbeb9757d89d83e4e09d93
Eric Sizer <esizer@users.noreply.github.com>
2023-10-18 14:21:32 -0400

    [Debt] Update pool info table (#8237)
    * update pool info table with new table component
    
    * update application link text
    
    * add translations
    
    * update with default sort

---
+++ apps/web/src/components/PoolStatusTable/cells.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import { Pool, PoolCandidate, User } from "@gc-digital-talent/graphql";
+
+import ChangeStatusDialog from "../../pages/Users/UserInformationPage/components/ChangeStatusDialog";
+import ChangeDateDialog from "../../pages/Users/UserInformationPage/components/ChangeDateDialog";
+
+export const statusCell = (
+  candidate: PoolCandidate,
+  user: User,
+  pools: Pool[],
+) => (
+  <ChangeStatusDialog selectedCandidate={candidate} user={user} pools={pools} />
+);
+
+export const expiryCell = (candidate: PoolCandidate, user: User) => (
+  <ChangeDateDialog selectedCandidate={candidate} user={user} />
+);
