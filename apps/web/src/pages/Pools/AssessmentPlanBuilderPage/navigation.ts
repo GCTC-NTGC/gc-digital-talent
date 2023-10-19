5dcf57ec53a7d89f4306ecf336df0da9ba84f40a
Peter Giles <8978655+petertgiles@users.noreply.github.com>
2023-10-19 10:37:24 -0400

    sidebar built

---
+++ apps/web/src/pages/Pools/AssessmentPlanBuilderPage/navigation.ts
@@ -0,0 +1,6 @@
+/* eslint-disable import/prefer-default-export */
+// If a table of contents is added to this page this file might export titles, as well.
+export const PAGE_SECTION_ID = {
+  ORGANIZE_ASSESSMENT_APPROACH: "organize_assessment_approach",
+  SKILL_SUMMARY: "skill_summary",
+} as const;
