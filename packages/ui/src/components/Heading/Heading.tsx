3099ded3cb66824c3f5a69277963cdc7324efac4
Eric Sizer <eric.sizer@gmail.com>
2023-10-20 12:43:44 -0400

    update heading icon size, add story

--- packages/ui/src/components/Heading/Heading.tsx
+++ packages/ui/src/components/Heading/Heading.tsx
@@ -33,7 +33,6 @@ const Heading = React.forwardRef<HeadingRef, HeadingProps>(
             data-h2-display="p-tablet(inline-block)"
             data-h2-vertical-align="base(middle)"
             data-h2-height="base(auto)"
-            data-h2-width="base(1em)"
             data-h2-flex-shrink="base(0)"
             {...(color ? iconStyles[color] : {})}
           />
