90333734e2875e5ea03b3e82cea1c5caa2f60aa5
Eric Sizer <eric.sizer@gmail.com>
2023-10-20 14:30:17 -0400

    only accept keys of expected params

--- apps/web/src/hooks/useRequiredParams.ts
+++ apps/web/src/hooks/useRequiredParams.ts
@@ -10,7 +10,7 @@ export const isUUID = (str: string): boolean => {
   return !!str.match(uuidRegEx);
 };
 
-const assertParam = (param?: string, enforceUUID: boolean = false) => {
+const assertParam = (param?: string, enforceUUID: boolean = true) => {
   invariant(
     notEmpty(param),
     `Could not find required URL parameter "${param}"`,
@@ -21,18 +21,28 @@ const assertParam = (param?: string, enforceUUID: boolean = false) => {
   }
 };
 
-const useRequiredParams = <T extends Record<string, string | undefined>>(
-  keys: string | string[],
-  enforceUUID: boolean = false,
+type Keys<T, K extends keyof T> = K | Array<K>;
+
+const useRequiredParams = <
+  T extends Record<
+    keyof {
+      [P in keyof T as T[P] extends string ? P : never]: string | undefined;
+    },
+    string
+  >,
+>(
+  keys: Keys<T, keyof T>,
+  enforceUUID: boolean = true,
 ): Record<string, string> => {
   const params: Params<string> = useParams<T>();
   const keyArray = Array.isArray(keys) ? keys : [keys];
 
   let nonEmptyParams = {};
   keyArray.forEach((key) => {
-    const param = params[key];
+    const param = params[String(key)];
     assertParam(param, enforceUUID);
-    nonEmptyParams = { ...nonEmptyParams, [key]: param };
+
+    nonEmptyParams = { ...nonEmptyParams, [key]: param ?? "" };
   });
 
   return nonEmptyParams;
