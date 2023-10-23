b835548eb44274a8da175a0aa6c5f3827f5bd186
vd1992 <40485260+vd1992@users.noreply.github.com>
2023-10-23 14:36:14 -0600

    create model

---
+++ api/app/Models/AssessmentResult.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+/**
+ * Class AssessmentResult
+ *
+ * @property string $id
+ * @property string $assessment_step_id
+ * @property string $pool_candidate_id
+ * @property string $pool_skill_id
+ * @property string $assessment_result_type
+ * @property string $assessment_decision
+ * @property array $justifications
+ * @property string $other_justification_notes
+ * @property string $skill_decision_level
+ * @property string $skill_decision_notes
+ * @property Illuminate\Support\Carbon $created_at
+ * @property Illuminate\Support\Carbon $updated_at
+ */
+class AssessmentResult extends Model
+{
+    use HasFactory;
+
+    protected $keyType = 'string';
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'justifications' => 'array',
+    ];
+
+    /**
+     * The attributes that can be filled using mass-assignment.
+     *
+     * @var array
+     */
+    protected $fillable = [];
+
+    public function assessmentStep(): BelongsTo
+    {
+        return $this->belongsTo(AssessmentStep::class);
+    }
+
+    public function poolCandidate(): BelongsTo
+    {
+        return $this->belongsTo(PoolCandidate::class);
+    }
+
+    public function poolSkill(): BelongsTo
+    {
+        return $this->belongsTo(PoolSkill::class);
+    }
+}
