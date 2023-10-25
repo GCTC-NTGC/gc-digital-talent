e364a83c1d05a1e3f1b77be128289117a32ba993
Matt Nigh <matt@mattnigh.com>
2023-10-25 13:24:50 -0400

    Remove unused files

--- infrastructure/db/add-extensions-db.sql
+++
@@ -1,4 +0,0 @@
-\c gctalent;
--- Add extensions to match what's in production
-CREATE EXTENSION IF NOT EXISTS plpgsql WITH VERSION '1.0';
-CREATE EXTENSION IF NOT EXISTS pgcrypto WITH VERSION '1.3';
