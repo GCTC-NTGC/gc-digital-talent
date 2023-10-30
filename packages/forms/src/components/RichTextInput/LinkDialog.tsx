6fed62d509e6562bdf3da396e2807530ec5f22c0
Eric Sizer <eric.sizer@gmail.com>
2023-10-30 09:28:32 -0400

    remove unused import

--- packages/forms/src/components/RichTextInput/LinkDialog.tsx
+++ packages/forms/src/components/RichTextInput/LinkDialog.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { createIntlCache, useIntl } from "react-intl";
+import { useIntl } from "react-intl";
 import { Editor } from "@tiptap/react";
 import { useForm, FormProvider, SubmitHandler } from "react-hook-form";
 import LinkIcon from "@heroicons/react/20/solid/LinkIcon";
