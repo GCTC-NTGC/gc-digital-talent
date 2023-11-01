1e850dfdd5a5dd835ac4a63b448a8e7911634519
Eric Sizer <eric.sizer@gmail.com>
2023-11-01 09:43:14 -0400

    do not rename default import

--- apps/web/src/pages/SearchRequests/SearchPage/SearchPage.tsx
+++ apps/web/src/pages/SearchRequests/SearchPage/SearchPage.tsx
@@ -6,7 +6,7 @@ import { Heading } from "@gc-digital-talent/ui";
 import Hero from "~/components/Hero/Hero";
 import SEO from "~/components/SEO/SEO";
 
-import SearchFormContainer from "./components/SearchForm";
+import SearchFormApi from "./components/SearchForm";
 
 const SearchPage = () => {
   const intl = useIntl();
@@ -58,7 +58,7 @@ const SearchPage = () => {
           </p>
         </div>
       </Hero>
-      <SearchFormContainer />
+      <SearchFormApi />
     </>
   );
 };
