073ddeb2ac3305f3ea33b933ed9a30236628bc76
JamesHuf <jamiehufnagel@hotmail.com>
2023-11-02 13:01:59 -0700

    Remove automatic creating/deleting of the screening question assessment step

--- api/app/Models/ScreeningQuestion.php
+++ api/app/Models/ScreeningQuestion.php
@@ -63,23 +63,5 @@ class ScreeningQuestion extends Model
     protected static function boot()
     {
         parent::boot();
-
-        static::created(function (ScreeningQuestion $question) {
-            // Create a new Assessment step for screening questions if there isn't one yet
-            $question->pool()->get()->first()->assessmentSteps()->firstOrCreate([
-                'type' => AssessmentStepType::SCREENING_QUESTIONS_AT_APPLICATION->name,
-                'sort_order' => 2,
-            ]);
-        });
-
-        static::deleted(function (ScreeningQuestion $question) {
-            // Delete the Assessment step for screening questions if there there are no other questions
-            if ($question->pool()->get()->first()->screeningQuestions()->get()->count() < 1) {
-                $stepIds = $question->pool()->get()->first()->assessmentSteps()->get()->filter(function (AssessmentStep $step) {
-                    return $step->type == AssessmentStepType::SCREENING_QUESTIONS_AT_APPLICATION->name && $step->sort_order == 2;
-                })->pluck('id');
-                AssessmentStep::destroy($stepIds);
-            }
-        });
     }
 }
