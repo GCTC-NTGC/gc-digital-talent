4e36c30818b6614c745f63e53ab429d97cca0243
vd1992 <40485260+vd1992@users.noreply.github.com>
2023-11-02 15:55:07 -0600

    try webp for Skewed component

--- apps/web/src/components/SkewedContainer/SkewedContainer.tsx
+++ apps/web/src/components/SkewedContainer/SkewedContainer.tsx
@@ -1,7 +1,34 @@
 import React from "react";
+import { useImage } from "react-image";
 
 import desktopGraphicsLight1 from "~/assets/img/Desktop_Graphics_light_1.png";
+import desktopGraphicsLight1Webp from "~/assets/img/webp/Desktop_Graphics_light_1.webp";
 import desktopGraphicsDark1 from "~/assets/img/Desktop_Graphics_dark_1.png";
+import desktopGraphicsDark1Webp from "~/assets/img/webp/Desktop_Graphics_dark_1.webp";
+
+interface ReactImageProps {
+  UrlArray: string[];
+}
+
+const ReactImage = ({ UrlArray }: ReactImageProps) => {
+  const { src } = useImage({
+    srcList: UrlArray,
+  });
+
+  return (
+    <img
+      src={src}
+      alt=""
+      data-h2-display="base(block) base:dark(none)"
+      data-h2-position="base(absolute)"
+      data-h2-location="base(0, 0, auto, auto)"
+      data-h2-transform="base(translate(32%, -52%) skew(3deg)) l-tablet(translate(0, 0) skew(3deg))"
+      data-h2-height="base(auto)"
+      data-h2-width="base(250%) l-tablet(40%)"
+      data-h2-max-width="base(initial)"
+    />
+  );
+};
 
 interface SkewedContainerProps {
   children: React.ReactNode;
@@ -17,28 +44,10 @@ const SkewedContainer = ({ children }: SkewedContainerProps) => (
       data-h2-transform="base(skewY(-3deg))"
       data-h2-overflow="base(hidden)"
     >
-      <img
-        data-h2-display="base(block) base:dark(none)"
-        data-h2-position="base(absolute)"
-        data-h2-location="base(0, 0, auto, auto)"
-        data-h2-transform="base(translate(32%, -52%) skew(3deg)) l-tablet(translate(0, 0) skew(3deg))"
-        data-h2-height="base(auto)"
-        data-h2-width="base(250%) l-tablet(40%)"
-        data-h2-max-width="base(initial)"
-        src={desktopGraphicsLight1}
-        alt=""
+      <ReactImage
+        UrlArray={[desktopGraphicsLight1Webp, desktopGraphicsLight1]}
       />
-      <img
-        data-h2-display="base(none) base:dark(block)"
-        data-h2-position="base(absolute)"
-        data-h2-location="base(0, 0, auto, auto)"
-        data-h2-transform="base(translate(32%, -52%) skew(3deg)) l-tablet(translate(0, 0) skew(3deg))"
-        data-h2-height="base(auto)"
-        data-h2-width="base(250%) l-tablet(40%)"
-        data-h2-max-width="base(initial)"
-        src={desktopGraphicsDark1}
-        alt=""
-      />
+      <ReactImage UrlArray={[desktopGraphicsDark1Webp, desktopGraphicsDark1]} />
       <div
         data-h2-background="base(main-linear)"
         data-h2-location="base(0, 0, auto, 0)"
