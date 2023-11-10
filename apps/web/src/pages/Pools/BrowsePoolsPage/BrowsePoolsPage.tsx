1c489214d352edd5a6950f98adce2b5dcef45791
Josh Beveridge <git@joshbeveridge.ca>
2023-11-10 16:28:21 -0500

    Updates the browse jobs page typography and spacing

--- apps/web/src/pages/Pools/BrowsePoolsPage/BrowsePoolsPage.tsx
+++ apps/web/src/pages/Pools/BrowsePoolsPage/BrowsePoolsPage.tsx
@@ -120,6 +120,7 @@ export const BrowsePools = ({ pools }: BrowsePoolsProps) => {
         data-h2-color="base(black)"
         data-h2-border-bottom="base(1px solid black.50)"
         data-h2-position="base(relative)"
+        data-h2-padding="base(x3, 0)"
       >
         <img
           alt=""
@@ -131,7 +132,7 @@ export const BrowsePools = ({ pools }: BrowsePoolsProps) => {
           data-h2-container="base(center, large, x1) p-tablet(center, large, x2)"
           style={{ zIndex: 1 }}
         >
-          <div data-h2-padding="base(x3, 0, 0, 0) p-tablet(x4, 0, 0, 0)">
+          <div>
             <ActiveRecruitmentSection pools={activeRecruitmentPools} />
           </div>
           {executiveTeaser && (
@@ -142,7 +143,7 @@ export const BrowsePools = ({ pools }: BrowsePoolsProps) => {
                 description: "Heading for the teaser of executive processes",
               })}
               link={profileLink}
-              data-h2-margin="base(x3, 0) p-tablet(x4, 0)"
+              data-h2-margin="base(x1, 0, 0, 0)"
             >
               <p>
                 {intl.formatMessage({
@@ -156,7 +157,7 @@ export const BrowsePools = ({ pools }: BrowsePoolsProps) => {
             </CallToActionCard>
           )}
           {ongoingRecruitmentPools.length > 0 && (
-            <div data-h2-padding="base(x3, 0, 0, 0) p-tablet(x4, 0, 0, 0)">
+            <div>
               <OngoingRecruitmentSection pools={ongoingRecruitmentPools} />
             </div>
           )}
@@ -179,7 +180,7 @@ export const BrowsePools = ({ pools }: BrowsePoolsProps) => {
                     })
               }
               link={profileLink}
-              data-h2-margin="base(x3, 0) p-tablet(x4, 0)"
+              data-h2-margin="base(x1, 0, 0, 0)"
             >
               <p>
                 {loggedIn
@@ -210,7 +211,7 @@ export const BrowsePools = ({ pools }: BrowsePoolsProps) => {
       <div
         data-h2-background-color="base(white) base:dark(white)"
         data-h2-color="base(black) base:dark(white)"
-        data-h2-padding="base(x3, 0) p-tablet(x4, 0) l-tablet(x6, 0)"
+        data-h2-padding="base(x3, 0)"
       >
         <div
           data-h2-position="base(relative)"
