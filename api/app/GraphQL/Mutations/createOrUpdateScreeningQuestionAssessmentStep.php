d31827c02e6fa8e9b04f7716d78f7023180e6541
JamesHuf <jamiehufnagel@hotmail.com>
2023-11-10 11:22:49 -0800

    Rename createOrUpdateScreeningQuestionAssessmentStep mutation

--- api/app/GraphQL/Mutations/createOrUpdateScreeningQuestionAssessmentStep.php
+++ api/app/GraphQL/Mutations/createOrUpdateScreeningQuestionAssessmentStep.php
@@ -10,7 +10,7 @@ use App\Models\ScreeningQuestion;
 use Exception;
 use Illuminate\Support\Facades\DB;
 
-final class SyncPoolScreeningQuestions
+final class createOrUpdateScreeningQuestionAssessmentStep
 {
     /**
      * @param  null  $_
