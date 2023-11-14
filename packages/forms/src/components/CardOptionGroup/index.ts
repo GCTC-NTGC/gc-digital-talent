46523744fde61dc105cc7b7a648688ac4daed768
Peter Giles <8978655+petertgiles@users.noreply.github.com>
2023-11-14 11:35:02 -0500

    [Feature] Card Option Group Component (#8452)
    * basic functionality and styling
    
    * add pointer
    
    * dark mode story
    
    * better coloring
    
    * improve story
    
    * tidy up
    
    * Update packages/forms/src/components/CardOptionGroup/CardOptionGroup.tsx
    
    Co-authored-by: Matt <mnigh@users.noreply.github.com>
    
    * exclude new colors
    
    * color selected item a bit
    
    ---------
    
    Co-authored-by: Matt <mnigh@users.noreply.github.com>

---
+++ packages/forms/src/components/CardOptionGroup/index.ts
@@ -0,0 +1,5 @@
+import CardOptionGroup from "./CardOptionGroup";
+import type { CardOptionGroupProps, CardOption } from "./CardOptionGroup";
+
+export default CardOptionGroup;
+export type { CardOptionGroupProps, CardOption };
