53d4b23b46afe63f055446433df134431c224306
Eric Sizer <eric.sizer@gmail.com>
2023-11-16 12:17:12 -0500

    replace skill picker on search page

--- apps/web/src/pages/SearchRequests/SearchPage/components/AddSkillsToFilter.tsx
+++ apps/web/src/pages/SearchRequests/SearchPage/components/AddSkillsToFilter.tsx
@@ -1,11 +1,10 @@
 import React from "react";
 import { useIntl } from "react-intl";
-import { useFieldArray, useFormContext } from "react-hook-form";
+import { useWatch } from "react-hook-form";
 
-import { notEmpty } from "@gc-digital-talent/helpers";
 import { Heading, Well } from "@gc-digital-talent/ui";
 
-import SkillPicker from "~/components/SkillPicker";
+import SkillBrowser from "~/components/SkillBrowser/SkillBrowser";
 import { Skill } from "~/api/generated";
 
 interface AddSkillsToFilterProps {
@@ -15,36 +14,7 @@ interface AddSkillsToFilterProps {
 
 const AddSkillsToFilter = ({ allSkills, linkId }: AddSkillsToFilterProps) => {
   const intl = useIntl();
-  const { control, watch } = useFormContext();
-  const watchedSkills = watch("skills");
-  const { replace } = useFieldArray({
-    control,
-    name: "skills",
-  });
-  const [addedSkillIds, setAddedSkillIds] = React.useState<string[]>(
-    watchedSkills || [],
-  );
-
-  const addedSkills: Skill[] = React.useMemo(() => {
-    return addedSkillIds
-      .map((id) => allSkills.find((skill) => skill.id === id))
-      .filter(notEmpty);
-  }, [addedSkillIds, allSkills]);
-
-  React.useEffect(() => {
-    const newSkills = notEmpty(watchedSkills)
-      ? watchedSkills.map((watchedSkill: string) => {
-          const newSkill = allSkills.find((s) => s.id === watchedSkill);
-          return newSkill?.id || undefined;
-        })
-      : [];
-    setAddedSkillIds(newSkills.length > 0 ? newSkills : []);
-  }, [watchedSkills, setAddedSkillIds, allSkills]);
-
-  const handleChange = (skills: Skill[]) => {
-    const skillIds = skills.map(({ id }) => id);
-    replace(skillIds);
-  };
+  const addedSkills = useWatch({ name: "skills" });
 
   return (
     <div data-h2-margin="base(x1.5, 0)">
@@ -69,11 +39,7 @@ const AddSkillsToFilter = ({ allSkills, linkId }: AddSkillsToFilterProps) => {
             "Describing the purpose of the skill filters on the Search page.",
         })}
       </p>
-      <SkillPicker
-        skills={allSkills || []}
-        onUpdateSelectedSkills={handleChange}
-        selectedSkills={addedSkills}
-      />
+      <SkillBrowser skills={allSkills || []} name="skills" />
       {addedSkills && addedSkills.length > 0 && (
         <Well
           color="primary"
