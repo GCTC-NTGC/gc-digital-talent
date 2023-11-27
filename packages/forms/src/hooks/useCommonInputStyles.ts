b150e365f8a4ef2f2ee1446aa501e9d6a33f7b74
Josh Beveridge <git@joshbeveridge.ca>
2023-11-27 11:40:31 -0500

    Adds conditional padding to account for dropdown arrows

--- packages/forms/src/hooks/useCommonInputStyles.ts
+++ packages/forms/src/hooks/useCommonInputStyles.ts
@@ -10,10 +10,13 @@ const useCommonInputStyles: UseCommonInputStyles = (type) => {
     "data-h2-outline-offset": "base(2px)",
     "data-h2-radius": "base(rounded)",
     "data-h2-color": "base(black)",
-    "data-h2-padding": "base(x.5)",
   };
+  let padding = { "data-h2-padding": "base(x.5)" };
   let selectIcon = {};
   if (type === "select") {
+    padding = {
+      "data-h2-padding": "base(x.5, x1.5, x.5, x.5)",
+    };
     selectIcon = {
       "data-h2-appearance": "base(none)",
       "data-h2-background-image": `base(url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke-width='1.5' stroke='rgba(86, 86, 90, 1)'><path stroke-linecap='round' stroke-linejoin='round' d='M19.5 8.25l-7.5 7.5-7.5-7.5'/></svg>")) base:dark(url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke-width='1.5' stroke='rgba(191, 191, 191, 1)'><path stroke-linecap='round' stroke-linejoin='round' d='M19.5 8.25l-7.5 7.5-7.5-7.5'/></svg>"))`,
@@ -23,7 +26,7 @@ const useCommonInputStyles: UseCommonInputStyles = (type) => {
       "data-h2-background-size": "base(1rem)",
     };
   }
-  return { ...defaults, ...selectIcon };
+  return { ...defaults, ...padding, ...selectIcon };
 };
 
 export default useCommonInputStyles;
