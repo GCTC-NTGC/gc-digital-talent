96ab934d55fd8b8350487a315fe3481276dd5920
Brinda Sasikumar <Brinda.Sasikumar@gmail.com>
2023-11-28 22:58:28 -0500

    improve the migration to be readable

--- api/database/migrations/2023_11_22_152329_update_users_table_add_searchable_column.php
+++ api/database/migrations/2023_11_22_152329_update_users_table_add_searchable_column.php
@@ -17,36 +17,17 @@ return new class extends Migration
 
         // Update the tsvector column with the data for existing rows
 
-        \DB::statement('UPDATE users SET searchable = to_tsvector(\'english\',
-        coalesce(email, \'\') || \' \' || coalesce(first_name, \'\') || \' \' ||
-        coalesce(last_name, \'\') || \' \' ||
-        coalesce(telephone, \'\') || \' \' ||
-        coalesce(preferred_lang, \'\') || \' \' ||
-        coalesce(current_city, \'\') || \' \' ||
-
-        coalesce((SELECT ARRAY_TO_STRING(ARRAY_AGG(notes), \' \') FROM Pool_Candidates WHERE Pool_Candidates.user_id = users.id), \'\') ||
-
-        coalesce((SELECT ARRAY_TO_STRING(ARRAY_AGG(role), \' \') FROM work_experiences  WHERE work_experiences.user_id = users.id), \'\') ||
-        coalesce((SELECT  ARRAY_TO_STRING(ARRAY_AGG(organization), \' \') FROM work_experiences  WHERE work_experiences.user_id = users.id), \'\') ||
-        coalesce((SELECT  ARRAY_TO_STRING(ARRAY_AGG(division), \' \') FROM work_experiences  WHERE work_experiences.user_id = users.id), \'\') ||
-        coalesce((SELECT  ARRAY_TO_STRING(ARRAY_AGG(details), \' \') FROM work_experiences  WHERE work_experiences.user_id = users.id), \'\') ||
-
-        coalesce((SELECT ARRAY_TO_STRING(ARRAY_AGG(thesis_title), \' \') FROM education_experiences WHERE education_experiences.user_id = users.id), \'\') ||
-        coalesce((SELECT ARRAY_TO_STRING(ARRAY_AGG(details), \' \') FROM education_experiences WHERE education_experiences.user_id = users.id), \'\') ||
-        coalesce((SELECT ARRAY_TO_STRING(ARRAY_AGG(institution), \' \') FROM education_experiences WHERE education_experiences.user_id = users.id), \'\') ||
-        coalesce((SELECT ARRAY_TO_STRING(ARRAY_AGG(area_of_study), \' \') FROM education_experiences WHERE education_experiences.user_id = users.id), \'\') ||
-
-        coalesce((SELECT ARRAY_TO_STRING(ARRAY_AGG(title), \' \') FROM personal_experiences WHERE personal_experiences.user_id = users.id), \'\') ||
-        coalesce((SELECT ARRAY_TO_STRING(ARRAY_AGG(description), \' \') FROM personal_experiences WHERE personal_experiences.user_id = users.id), \'\') ||
-        coalesce((SELECT ARRAY_TO_STRING(ARRAY_AGG(details), \' \') FROM personal_experiences WHERE personal_experiences.user_id = users.id), \'\') ||
-
-        coalesce((SELECT ARRAY_TO_STRING(ARRAY_AGG(title), \' \') FROM award_experiences WHERE award_experiences.user_id = users.id), \'\') ||
-        coalesce((SELECT ARRAY_TO_STRING(ARRAY_AGG(issued_by), \' \') FROM award_experiences WHERE award_experiences.user_id = users.id), \'\') ||
-        coalesce((SELECT ARRAY_TO_STRING(ARRAY_AGG(details), \' \')details FROM award_experiences WHERE award_experiences.user_id = users.id), \'\') ||
-
-        coalesce((SELECT ARRAY_TO_STRING(ARRAY_AGG(title), \' \') FROM community_experiences WHERE community_experiences.user_id = users.id), \'\') ||
-        coalesce((SELECT ARRAY_TO_STRING(ARRAY_AGG(details), \' \')details FROM community_experiences WHERE community_experiences.user_id = users.id), \'\')
-        )');
+       \DB::statement('
+    UPDATE users
+    SET searchable = to_tsvector(\'english\', COALESCE(email, \'\') || \' \' ||
+        CONCAT_WS(\' \', COALESCE(first_name, \'\'), COALESCE(last_name, \' \'), COALESCE(telephone, \' \'), COALESCE(preferred_lang, \' \'), COALESCE(current_city, \' \')) ||
+        CONCAT_WS(\' \', ARRAY(SELECT notes FROM Pool_Candidates WHERE Pool_Candidates.user_id = users.id)) ||
+        CONCAT_WS(\' \', ARRAY(SELECT CONCAT_WS(\' \', role, organization, division, details) FROM work_experiences WHERE work_experiences.user_id = users.id)) ||
+        CONCAT_WS(\' \', ARRAY(SELECT CONCAT_WS(\' \', thesis_title, details) FROM education_experiences WHERE education_experiences.user_id = users.id)) ||
+        CONCAT_WS(\' \', ARRAY(SELECT CONCAT_WS(\' \', title, description, details) FROM personal_experiences WHERE personal_experiences.user_id = users.id)) ||
+        CONCAT_WS(\' \', ARRAY(SELECT CONCAT_WS(\' \', title, issued_by, details) FROM award_experiences WHERE award_experiences.user_id = users.id)) ||
+        CONCAT_WS(\' \', ARRAY(SELECT CONCAT_WS(\' \', title, details) FROM community_experiences WHERE community_experiences.user_id = users.id))
+    )');
 
 
         // Create a GIN index on the tsvector column for efficient full-text search
