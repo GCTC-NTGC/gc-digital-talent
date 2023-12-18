80ebab2d3acf789575bfba27842002d5b72d0711
Brinda Sasikumar <Brinda.Sasikumar@gmail.com>
2023-12-18 09:46:29 -0500

    fix linting

--- api/database/seeders/UserSeederLocal.php
+++ api/database/seeders/UserSeederLocal.php
@@ -5,7 +5,6 @@ namespace Database\Seeders;
 use App\Models\Role;
 use App\Models\User;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Artisan;
 
 class UserSeederLocal extends Seeder
 {
