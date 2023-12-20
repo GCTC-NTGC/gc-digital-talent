fd29abf9350d7aaa0a332e6058a77d73f4a5e590
Matt Nigh <matt@mattnigh.com>
2023-12-20 15:23:14 -0500

    Consolidate <strong>Yes</strong>, I am a Government of Canada employee.

--- apps/web/src/components/UserProfile/ProfileSections/GovernmentInformationSection.tsx
+++ apps/web/src/components/UserProfile/ProfileSections/GovernmentInformationSection.tsx
@@ -46,9 +46,9 @@ const GovernmentInformationSection = ({
                 {intl.formatMessage({
                   defaultMessage:
                     "<strong>Yes</strong>, I am a Government of Canada employee.",
-                  id: "5d0vbr",
+                  id: "gto/zD",
                   description:
-                    "Message to state user is employed by government",
+                    "Label displayed for is a government employee option",
                 })}
               </span>
             </p>
