09116d027f0a9699ff72984cbfb7feff1f55c3e7
Josh Beveridge <git@joshbeveridge.ca>
2023-12-20 09:41:45 -0500

    Lighten required asterisk in dark mode

--- packages/forms/src/components/Field/Required.tsx
+++ packages/forms/src/components/Field/Required.tsx
@@ -6,7 +6,7 @@ export interface RequiredProps {
 
 const Required = ({ required }: RequiredProps) =>
   required ? (
-    <span data-h2-color="base(error) base:dark(error.lighter)"> *</span>
+    <span data-h2-color="base(error) base:dark(error.lightest)"> *</span>
   ) : null;
 
 export default Required;
