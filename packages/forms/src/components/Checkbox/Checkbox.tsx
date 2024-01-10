7400a5f9ac048c6c437561213c2d04f9ca914e20
Eric Sizer <eric.sizer@gmail.com>
2024-01-10 11:02:54 -0500

    add custom checkbox styles

--- packages/forms/src/components/Checkbox/Checkbox.tsx
+++ packages/forms/src/components/Checkbox/Checkbox.tsx
@@ -69,10 +69,26 @@ const Checkbox = ({
             <input
               id={id}
               type="checkbox"
+              className="Input--Checkbox"
               aria-describedby={ariaDescribedBy}
               aria-required={!!rules.required && !inCheckList}
               aria-invalid={!!error}
-              data-h2-margin-top="base(x.26)"
+              data-h2-appearance="base(none)"
+              data-h2-background-color="base(white) base:focus-visible(focus) base:selectors[::before](primary) base:dark:selectors[::before](primary.light) base:all:selectors[:focus-visible::before](black)"
+              data-h2-border="base(thin solid black.light)"
+              data-h2-clip-path="base:selectors[::before](polygon(14% 44%, 0 65%, 50% 100%, 100% 16%, 80% 0%, 43% 62%))"
+              data-h2-color="base(currentColor)"
+              data-h2-content="base:selectors[::before]('')"
+              data-h2-display="base(grid)"
+              data-h2-flex-shrink="base(0)"
+              data-h2-place-content="base(center)"
+              data-h2-height="base(x.9) base:selectors[::before](x.5)"
+              data-h2-line-height="base(x1)"
+              data-h2-margin="base(x.1 0 0 0)"
+              data-h2-radius="base(input)"
+              data-h2-transform="base:selectors[::before](scale(0)) base:selectors[:checked::before](scale(1))"
+              data-h2-transition="base:selectors[::before](120ms transform ease-in-out)"
+              data-h2-width="base(x.9) base:selectors[::before](x.5)"
               {...register(name, rules)}
               {...rest}
             />
