42dc138b1cf122bfdf9cd130e73759bee05730fb
JamesHuf <jamiehufnagel@hotmail.com>
2024-01-17 12:28:20 -0800

    Replace screeningAndEvaluationOperations.graphql

--- apps/web/src/pages/Pools/ScreeningAndEvaluationPage/ScreeningAndEvaluationPage.tsx
+++ apps/web/src/pages/Pools/ScreeningAndEvaluationPage/ScreeningAndEvaluationPage.tsx
@@ -1,16 +1,17 @@
 import React from "react";
+import { useQuery } from "urql";
 
 import { Pending, ThrowNotFound } from "@gc-digital-talent/ui";
-
 import {
-  Pool,
+  graphql,
   Scalars,
-  useGetAssessmentEvaluationPoolWithCandidatesQuery,
-} from "~/api/generated";
+  ScreeningAndEvaluationQuery,
+} from "@gc-digital-talent/graphql";
+
 import useRequiredParams from "~/hooks/useRequiredParams";
 
 type ScreeningAndEvaluationProps = {
-  pool: Pool;
+  pool: NonNullable<ScreeningAndEvaluationQuery["pool"]>;
 };
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -19,15 +20,32 @@ const ScreeningAndEvaluation = ({ pool }: ScreeningAndEvaluationProps) => {
 };
 
 type RouteParams = {
-  poolId: Scalars["ID"];
+  poolId: Scalars["ID"]["output"];
 };
+
+const ScreeningAndEvaluation_Query = graphql(/* GraphQL */ `
+  query ScreeningAndEvaluation($poolId: UUID!) {
+    pool(id: $poolId) {
+      id
+      poolCandidates {
+        id
+        pool {
+          id
+        }
+        user {
+          id
+        }
+      }
+    }
+  }
+`);
 
 const ScreeningAndEvaluationPage = () => {
   const { poolId } = useRequiredParams<RouteParams>("poolId");
-  const [{ data: lookupData, fetching, error }] =
-    useGetAssessmentEvaluationPoolWithCandidatesQuery({
-      variables: { poolId },
-    });
+  const [{ data: lookupData, fetching, error }] = useQuery({
+    query: ScreeningAndEvaluation_Query,
+    variables: { poolId },
+  });
 
   return (
     <Pending fetching={fetching} error={error}>
