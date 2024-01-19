9c3a5d72881bb901276342ec7c7faeb8ff2d007d
Brinda Sasikumar <sasibrinda@gmail.com>
2024-01-18 21:46:26 -0500

    set up dev pipeline

---
+++ infrastructure/bin/dev_deploy.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+# Errors will fail out
+set -o errexit
+# Don't mask errors in piped commands
+set -o pipefail
+# Fail if using undefined variables
+set -o nounset
+
+if [ -z "$1" ]; then
+    echo "Must past abs path as argument."
+    exit 1
+fi
+
+ROOT_DIR=$1
+
+sudo composer selfupdate
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+
+source ~/.bash_profile
+nvm install v18.16.0
+#nvm install-latest-npm
+npm install -g npm@8.11.0
+
+### API
+
+cd $ROOT_DIR/api
+
+composer install
+sudo chown -R www-data ./storage ./vendor
+sudo chmod -R 775 ./ ./storage
+php artisan lighthouse:print-schema --write
+
+### Install all npm dependencies
+cd $ROOT_DIR
+npm ci --include=dev
+
+### Build frontend
+npm run build
+chmod -R a+r,a+w node_modules
+
