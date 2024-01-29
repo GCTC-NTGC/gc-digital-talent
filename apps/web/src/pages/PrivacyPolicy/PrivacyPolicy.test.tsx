f3234837d4ce76d4f41f29f7f6b9a3f15035ea40
Yonathan K <yoni.kid15@gmail.com>
2024-01-29 17:36:54 -0500

    Add accessibility axe test

---
+++ apps/web/src/pages/PrivacyPolicy/PrivacyPolicy.test.tsx
@@ -0,0 +1,20 @@
+/**
+ * @jest-environment jsdom
+ */
+import "@testing-library/jest-dom";
+import React from "react";
+
+import { renderWithProviders, axeTest } from "@gc-digital-talent/jest-helpers";
+
+import PrivacyPolicy from "./PrivacyPolicy";
+
+const renderPrivacyPolicy = () => {
+  return renderWithProviders(<PrivacyPolicy />);
+};
+
+describe("PrivacyPolicy", () => {
+  it("should have no accessibility errors", async () => {
+    const { container } = renderPrivacyPolicy();
+    await axeTest(container);
+  });
+});
