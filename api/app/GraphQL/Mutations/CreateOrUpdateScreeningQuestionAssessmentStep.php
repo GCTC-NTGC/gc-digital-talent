abc5c6498c071431b5fcfdd061fd5145df611325
vd1992 <40485260+vd1992@users.noreply.github.com>
2024-01-31 16:51:19 -0700

    switch mutation class to screening questions

--- api/app/GraphQL/Mutations/CreateOrUpdateScreeningQuestionAssessmentStep.php
+++ api/app/GraphQL/Mutations/CreateOrUpdateScreeningQuestionAssessmentStep.php
@@ -4,13 +4,13 @@ namespace App\GraphQL\Mutations;
 
 use App\Enums\AssessmentStepType;
 use App\Models\AssessmentStep;
-use App\Models\GeneralQuestion;
 use App\Models\Pool;
 use App\Models\PoolSkill;
+use App\Models\ScreeningQuestion;
 use Exception;
 use Illuminate\Support\Facades\DB;
 
-final class CreateOrUpdateGeneralQuestionAssessmentStep
+final class CreateOrUpdateScreeningQuestionAssessmentStep
 {
     /**
      * @param  null  $_
@@ -22,40 +22,10 @@ final class CreateOrUpdateGeneralQuestionAssessmentStep
         DB::beginTransaction();
         try {
             $pool = Pool::find($args['poolId']);
-            $existingQuestions = GeneralQuestion::where('pool_id', '=', $args['poolId'])->get();
-            $incomingQuestions = is_array($args['generalQuestions']) ? $args['generalQuestions'] : [];
+            $existingQuestions = ScreeningQuestion::where('pool_id', '=', $args['poolId'])->get();
+            $incomingQuestions = is_array($args['screeningQuestions']) ? $args['screeningQuestions'] : [];
             $incomingAssessmentStep = $args['assessmentStep'];
             $incomingQuestionIds = [];
-
-            // Create/update incoming questions based on the existence of an id
-            foreach ($incomingQuestions as $incomingQuestion) {
-                if (isset($incomingQuestion['id'])) {
-                    array_push($incomingQuestionIds, $incomingQuestion['id']);
-                    $questionToUpdate = $existingQuestions->find($incomingQuestion['id']);
-                    if ($questionToUpdate === null) {
-                        throw new Exception('GeneralQuestionNotExist');
-                    }
-
-                    $questionToUpdate->question = $incomingQuestion['question'];
-                    if (isset($incomingQuestion['sortOrder'])) {
-                        $questionToUpdate->sort_order = $incomingQuestion['sortOrder'];
-                    }
-
-                    $questionToUpdate->save();
-                } else {
-                    $pool->generalQuestions()->Create([
-                        'question' => $incomingQuestion['question'],
-                        'sort_order' => isset($incomingQuestion['sortOrder']) ? $incomingQuestion['sortOrder'] : null,
-                    ]);
-                }
-            }
-
-            // Remove old questions which aren't included in the sync request
-            foreach ($existingQuestions as $existing) {
-                if (! in_array($existing->id, $incomingQuestionIds)) {
-                    $existing->delete();
-                }
-            }
 
             // Upsert assessment step
             $assessmentStep = AssessmentStep::updateOrCreate(
@@ -70,6 +40,37 @@ final class CreateOrUpdateGeneralQuestionAssessmentStep
                     }
                 }
                 $assessmentStep->poolSkills()->sync($incomingAssessmentStep['poolSkills']['sync']);
+            }
+
+            // Create/update incoming questions based on the existence of an id
+            foreach ($incomingQuestions as $incomingQuestion) {
+                if (isset($incomingQuestion['id'])) {
+                    array_push($incomingQuestionIds, $incomingQuestion['id']);
+                    $questionToUpdate = $existingQuestions->find($incomingQuestion['id']);
+                    if ($questionToUpdate === null) {
+                        throw new Exception('ScreeningQuestionNotExist');
+                    }
+
+                    $questionToUpdate->question = $incomingQuestion['question'];
+                    if (isset($incomingQuestion['sortOrder'])) {
+                        $questionToUpdate->sort_order = $incomingQuestion['sortOrder'];
+                    }
+
+                    $questionToUpdate->save();
+                } else {
+                    $pool->screeningQuestions()->Create([
+                        'assessment_step_id' => $assessmentStep->id,
+                        'question' => $incomingQuestion['question'],
+                        'sort_order' => isset($incomingQuestion['sortOrder']) ? $incomingQuestion['sortOrder'] : null,
+                    ]);
+                }
+            }
+
+            // Remove old questions which aren't included in the sync request
+            foreach ($existingQuestions as $existing) {
+                if (! in_array($existing->id, $incomingQuestionIds)) {
+                    $existing->delete();
+                }
             }
 
             DB::commit();
