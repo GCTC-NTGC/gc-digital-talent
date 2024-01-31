1a6f6a25c78b37fa31d9c31d916b4445d95e452b
Eric Sizer <eric.sizer@gmail.com>
2024-01-31 09:40:46 -0500

    only retain trace on failures

--- apps/playwright/playwright.config.ts
+++ apps/playwright/playwright.config.ts
@@ -29,7 +29,7 @@ export default defineConfig({
     baseURL: "http://localhost:8000",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: "on",
+    trace: "retain-on-failure",
   },
 
   /* Configure projects for major browsers */
