1cfdb6b1905b087a8ad41c8394fd43466b815d58
vd1992 <40485260+vd1992@users.noreply.github.com>
2024-02-02 13:42:05 -0700

    add to comment

--- api/database/migrations/2024_01_31_193707_add_new_screening_questions_table.php
+++ api/database/migrations/2024_01_31_193707_add_new_screening_questions_table.php
@@ -66,6 +66,7 @@ return new class extends Migration
         Schema::dropIfExists('screening_questions');
 
         // make migration reversible by recreating the incorrect foreign key names
+        // renaming reversed after foreign keys created
         Schema::rename('general_questions', 'screening_questions');
         Schema::rename('general_question_responses', 'screening_question_responses');
         Schema::table('screening_question_responses', function (Blueprint $table) {
