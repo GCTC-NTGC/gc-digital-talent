05bd8a344ee906e6a7602718676d116d75480409
Eric Sizer <eric.sizer@gmail.com>
2024-02-05 14:38:37 -0500

    update application print button with anonymous option

--- apps/web/src/pages/PoolCandidates/ViewPoolCandidatePage/components/ApplicationPrintButton/ApplicationPrintButton.tsx
+++ apps/web/src/pages/PoolCandidates/ViewPoolCandidatePage/components/ApplicationPrintButton/ApplicationPrintButton.tsx
@@ -1,9 +1,15 @@
 import React, { useRef } from "react";
-import { useIntl } from "react-intl";
+import { defineMessage, useIntl } from "react-intl";
 import { useReactToPrint } from "react-to-print";
 import PrinterIcon from "@heroicons/react/20/solid/PrinterIcon";
+import ChevronDownIcon from "@heroicons/react/20/solid/ChevronDownIcon";
 
-import { Button, ButtonLinkMode, Color } from "@gc-digital-talent/ui";
+import {
+  Button,
+  ButtonLinkMode,
+  Color,
+  DropdownMenu,
+} from "@gc-digital-talent/ui";
 import { FragmentType, User } from "@gc-digital-talent/graphql";
 
 import printStyles from "~/styles/printStyles";
@@ -11,6 +17,12 @@ import printStyles from "~/styles/printStyles";
 import ApplicationPrintDocument, {
   ApplicationPrintDocument_PoolFragment,
 } from "./ApplicationPrintDocument";
+
+const documentTitle = defineMessage({
+  defaultMessage: "Application snapshot",
+  id: "ipsXat",
+  description: "Document title for printing a user's application snapshot.",
+});
 
 interface ApplicationPrintButtonProps {
   user: User;
@@ -27,35 +39,67 @@ const ApplicationPrintButton = ({
 }: ApplicationPrintButtonProps) => {
   const intl = useIntl();
 
-  const componentRef = useRef(null);
-  const handlePrint = useReactToPrint({
-    content: () => componentRef.current,
+  const commonArgs = {
     pageStyle: printStyles,
-    documentTitle: intl.formatMessage({
-      defaultMessage: "Application snapshot",
-      id: "ipsXat",
-      description: "Document title for printing a user's application snapshot.",
-    }),
+    documentTitle: intl.formatMessage(documentTitle),
+  };
+
+  const anonymousComponentRef = useRef(null);
+  const handleAnonymousPrint = useReactToPrint({
+    content: () => anonymousComponentRef.current,
+    ...commonArgs,
+  });
+
+  const fullComponentRef = useRef(null);
+  const handleFullPrint = useReactToPrint({
+    content: () => fullComponentRef.current,
+    ...commonArgs,
   });
 
   return (
     <>
-      <Button
-        color={color}
-        mode={mode}
-        onClick={handlePrint}
-        icon={PrinterIcon}
-      >
-        {intl.formatMessage({
-          defaultMessage: "Print application",
-          id: "0pDCvX",
-          description: "Print application",
-        })}
-      </Button>
+      <DropdownMenu.Root>
+        <DropdownMenu.Trigger>
+          <Button
+            color={color}
+            mode={mode}
+            utilityIcon={ChevronDownIcon}
+            icon={PrinterIcon}
+          >
+            {intl.formatMessage({
+              defaultMessage: "Print application",
+              id: "0pDCvX",
+              description: "Print application",
+            })}
+          </Button>
+        </DropdownMenu.Trigger>
+        <DropdownMenu.Content align="end" collisionPadding={2}>
+          <DropdownMenu.Item onSelect={handleFullPrint}>
+            {intl.formatMessage({
+              defaultMessage: "Print with all information",
+              id: "qN+dwB",
+              description: "Button label for print user profile.",
+            })}
+          </DropdownMenu.Item>
+          <DropdownMenu.Item onSelect={handleAnonymousPrint}>
+            {intl.formatMessage({
+              defaultMessage: "Print without contact information",
+              id: "c795MO",
+              description: "Button label for print user anonymous profile.",
+            })}
+          </DropdownMenu.Item>
+        </DropdownMenu.Content>
+      </DropdownMenu.Root>
+      <ApplicationPrintDocument
+        anonymous
+        user={user}
+        poolQuery={pool}
+        ref={anonymousComponentRef}
+      />
       <ApplicationPrintDocument
         user={user}
         poolQuery={pool}
-        ref={componentRef}
+        ref={fullComponentRef}
       />
     </>
   );
