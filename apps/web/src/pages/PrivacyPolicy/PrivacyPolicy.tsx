5d86c612ee5b3acc9b1f541179cf29e77da5585b
Yonathan K <yoni.kid15@gmail.com>
2024-02-06 14:38:29 -0500

    Remove table of contents on privacy policy

--- apps/web/src/pages/PrivacyPolicy/PrivacyPolicy.tsx
+++ apps/web/src/pages/PrivacyPolicy/PrivacyPolicy.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useIntl } from "react-intl";
 
-import { Flourish, Link, TableOfContents } from "@gc-digital-talent/ui";
+import { Flourish, Heading, Link } from "@gc-digital-talent/ui";
 import { Locales, getLocale } from "@gc-digital-talent/i18n";
 
 import Hero from "~/components/Hero";
@@ -119,100 +119,85 @@ const PrivacyPolicy = () => {
   return (
     <>
       <Hero title={pageTitle} subtitle={subtitle} crumbs={crumbs} />
-      <div data-h2-container="base(center, large, x1) p-tablet(center, large, x2)">
-        <TableOfContents.Wrapper data-h2-margin-top="base(x3)">
-          <TableOfContents.Navigation>
-            <TableOfContents.List>
-              <TableOfContents.ListItem key={id}>
-                <TableOfContents.AnchorLink id={id}>
-                  {pageTitle}
-                </TableOfContents.AnchorLink>
-              </TableOfContents.ListItem>
-            </TableOfContents.List>
-          </TableOfContents.Navigation>
-          <TableOfContents.Content>
-            <TableOfContents.Section id={id}>
-              <TableOfContents.Heading
-                size="h3"
-                data-h2-margin="base(0, 0, x1, 0)"
-              >
-                {pageTitle}
-              </TableOfContents.Heading>
-              <p data-h2-margin="base(x1 0)">
-                {intl.formatMessage(
-                  {
-                    defaultMessage:
-                      "Personal information collected through GC Digital Talent is used for staffing, external recruitment, and internal talent mobility within federal institutions pursuant to <justiceLaws7Link>section 7(1)</justiceLaws7Link> of the Financial Administration Act, <justiceLaws15Link>section 15(1)</justiceLaws15Link>, <justiceLaws29Link>section 29</justiceLaws29Link> and <justiceLaws30Link>30 (1), (2), and (3)</justiceLaws30Link> of the Public Service Employment Act and section 5 of the Employment Equity Act.",
-                    id: "e1JW62",
-                    description: "Paragraph for privacy policy page",
-                  },
-                  {
-                    justiceLaws7Link: (chunks: React.ReactNode) =>
-                      justiceLaws7Link(locale, chunks),
-                    justiceLaws15Link: (chunks: React.ReactNode) =>
-                      justiceLaws15Link(locale, chunks),
-                    justiceLaws29Link: (chunks: React.ReactNode) =>
-                      justiceLaws29Link(locale, chunks),
-                    justiceLaws30Link: (chunks: React.ReactNode) =>
-                      justiceLaws30Link(locale, chunks),
-                  },
-                )}
-              </p>
-              <p data-h2-margin="base(x1 0)">
-                {intl.formatMessage({
-                  defaultMessage:
-                    "Please do not provide additional personal information which is not required for this purpose.",
-                  id: "zMa59V",
-                  description: "Paragraph for privacy policy page",
-                })}
-              </p>
-              <p data-h2-margin="base(x1 0)">
-                {intl.formatMessage(
-                  {
-                    defaultMessage:
-                      "  The information you provide may also be used for statistical and research purposes, and may be disclosed to the <publicServiceLink>Public Service Commission Investigation Directorate</publicServiceLink> when necessary.",
-                    id: "H2l4rG",
-                    description: "Paragraph for privacy policy page",
-                  },
-                  {
-                    publicServiceLink: (chunks: React.ReactNode) =>
-                      publicServiceLink(locale, chunks),
-                  },
-                )}
-              </p>
-              <p data-h2-margin="base(x1 0)">
-                {intl.formatMessage({
-                  defaultMessage:
-                    "Failure to provide personal information will result in your ineligibility for employment opportunities associated with this tool.",
-                  id: "Ijz5oe",
-                  description: "Paragraph for privacy policy page",
-                })}
-              </p>
-              <p data-h2-margin="base(x1 0)">
-                {intl.formatMessage({
-                  defaultMessage:
-                    "You have the right to the correction of, the access to, and protection of your personal information under the Privacy Act and the right to complain to the Privacy Commissioner of Canada about the handling of your personal information.",
-                  id: "7ejG4K",
-                  description: "Paragraph for privacy policy page",
-                })}
-              </p>
-              <p data-h2-margin="base(x1 0)">
-                {intl.formatMessage(
-                  {
-                    defaultMessage:
-                      "Personal information collected through GC Digital Talent is described by the <personalInfoLink>Talent Cloud Personal Information Bank (TBS PPU 095).</personalInfoLink>",
-                    id: "XYF/1P",
-                    description: "Paragraph for privacy policy page",
-                  },
-                  {
-                    personalInfoLink: (chunks: React.ReactNode) =>
-                      personalInfoLink(locale, chunks),
-                  },
-                )}
-              </p>
-            </TableOfContents.Section>
-          </TableOfContents.Content>
-        </TableOfContents.Wrapper>
+      <div
+        data-h2-container="base(center, large, x1)"
+        data-h2-margin="base(x3)"
+      >
+        <Heading size="h3" data-h2-margin="base(0, 0, x1, 0)">
+          {pageTitle}
+        </Heading>
+        <p data-h2-margin="base(x1 0)">
+          {intl.formatMessage(
+            {
+              defaultMessage:
+                "Personal information collected through GC Digital Talent is used for staffing, external recruitment, and internal talent mobility within federal institutions pursuant to <justiceLaws7Link>section 7(1)</justiceLaws7Link> of the Financial Administration Act, <justiceLaws15Link>section 15(1)</justiceLaws15Link>, <justiceLaws29Link>section 29</justiceLaws29Link> and <justiceLaws30Link>30 (1), (2), and (3)</justiceLaws30Link> of the Public Service Employment Act and section 5 of the Employment Equity Act.",
+              id: "e1JW62",
+              description: "Paragraph for privacy policy page",
+            },
+            {
+              justiceLaws7Link: (chunks: React.ReactNode) =>
+                justiceLaws7Link(locale, chunks),
+              justiceLaws15Link: (chunks: React.ReactNode) =>
+                justiceLaws15Link(locale, chunks),
+              justiceLaws29Link: (chunks: React.ReactNode) =>
+                justiceLaws29Link(locale, chunks),
+              justiceLaws30Link: (chunks: React.ReactNode) =>
+                justiceLaws30Link(locale, chunks),
+            },
+          )}
+        </p>
+        <p data-h2-margin="base(x1 0)">
+          {intl.formatMessage({
+            defaultMessage:
+              "Please do not provide additional personal information which is not required for this purpose.",
+            id: "zMa59V",
+            description: "Paragraph for privacy policy page",
+          })}
+        </p>
+        <p data-h2-margin="base(x1 0)">
+          {intl.formatMessage(
+            {
+              defaultMessage:
+                "  The information you provide may also be used for statistical and research purposes, and may be disclosed to the <publicServiceLink>Public Service Commission Investigation Directorate</publicServiceLink> when necessary.",
+              id: "H2l4rG",
+              description: "Paragraph for privacy policy page",
+            },
+            {
+              publicServiceLink: (chunks: React.ReactNode) =>
+                publicServiceLink(locale, chunks),
+            },
+          )}
+        </p>
+        <p data-h2-margin="base(x1 0)">
+          {intl.formatMessage({
+            defaultMessage:
+              "Failure to provide personal information will result in your ineligibility for employment opportunities associated with this tool.",
+            id: "Ijz5oe",
+            description: "Paragraph for privacy policy page",
+          })}
+        </p>
+        <p data-h2-margin="base(x1 0)">
+          {intl.formatMessage({
+            defaultMessage:
+              "You have the right to the correction of, the access to, and protection of your personal information under the Privacy Act and the right to complain to the Privacy Commissioner of Canada about the handling of your personal information.",
+            id: "7ejG4K",
+            description: "Paragraph for privacy policy page",
+          })}
+        </p>
+        <p data-h2-margin="base(x1 0)">
+          {intl.formatMessage(
+            {
+              defaultMessage:
+                "Personal information collected through GC Digital Talent is described by the <personalInfoLink>Talent Cloud Personal Information Bank (TBS PPU 095).</personalInfoLink>",
+              id: "XYF/1P",
+              description: "Paragraph for privacy policy page",
+            },
+            {
+              personalInfoLink: (chunks: React.ReactNode) =>
+                personalInfoLink(locale, chunks),
+            },
+          )}
+        </p>
       </div>
       <Flourish />
     </>
