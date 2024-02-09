6de03dda2c0ed4e55d9e78ec50f8f91a18048599
Eric Sizer <eric.sizer@gmail.com>
2024-02-09 13:53:14 -0500

    create component to implement candidate nav hook

---
+++ apps/web/src/pages/PoolCandidates/ViewPoolCandidatePage/components/CandidateNavigation/CandidateNavigation.tsx
@@ -0,0 +1,85 @@
+import React from "react";
+import { useIntl } from "react-intl";
+import ArrowLeftCircleIcon from "@heroicons/react/20/solid/ArrowLeftCircleIcon";
+import ArrowRightCircleIcon from "@heroicons/react/20/solid/ArrowRightCircleIcon";
+
+import { Link, LinkProps } from "@gc-digital-talent/ui";
+
+import useRoutes from "~/hooks/useRoutes";
+
+import usePoolCandidateNavigation from "./usePoolCandidateNavigation";
+
+interface CandidateNavigationProps {
+  candidateId: string;
+  poolId: string;
+}
+
+const CandidateNavigation = ({
+  candidateId,
+  poolId,
+}: CandidateNavigationProps) => {
+  const intl = useIntl();
+  const paths = useRoutes();
+  const candidateNavigation = usePoolCandidateNavigation(candidateId);
+  if (!candidateNavigation) return null;
+  const { nextCandidate, previousCandidate, lastCandidate, candidateIds } =
+    candidateNavigation;
+
+  const commonLinkProps: LinkProps = {
+    color: "primary",
+    mode: "inline",
+    state: { candidateIds },
+  };
+
+  return (
+    <div
+      data-h2-display="base(flex)"
+      data-h2-flex-direction="base(column)"
+      data-h2-gap="base(x.5 0)"
+    >
+      {nextCandidate && (
+        <Link
+          href={paths.poolCandidateApplication(nextCandidate)}
+          icon={ArrowRightCircleIcon}
+          {...commonLinkProps}
+        >
+          {intl.formatMessage({
+            defaultMessage: "Go to next candidate",
+            id: "oTNbp0",
+            description:
+              "Link label to view next candidate on view pool candidate page",
+          })}
+        </Link>
+      )}
+      {previousCandidate && (
+        <Link
+          href={paths.poolCandidateApplication(previousCandidate)}
+          icon={ArrowLeftCircleIcon}
+          {...commonLinkProps}
+        >
+          {intl.formatMessage({
+            defaultMessage: "Go to previous candidate",
+            id: "xfHjyy",
+            description:
+              "Link label to view previous candidate on view pool candidate page",
+          })}
+        </Link>
+      )}
+      {lastCandidate && (
+        <Link
+          href={paths.screeningAndEvaluation(poolId)}
+          icon={ArrowLeftCircleIcon}
+          {...commonLinkProps}
+        >
+          {intl.formatMessage({
+            defaultMessage: "Back to assessments tracker",
+            id: "nUokX9",
+            description: "Link label to return to assessment tracker",
+          })}
+        </Link>
+      )}
+    </div>
+  );
+};
+
+export default CandidateNavigation;
