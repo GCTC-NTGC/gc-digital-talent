a74d8e65917d1fdc4cacff5670b3dd48a16a274f
Peter Giles <8978655+petertgiles@users.noreply.github.com>
2024-02-12 10:46:59 -0500

    new seeders

--- api/database/seeders/SkillFamilySeeder.php
+++ api/database/seeders/SkillFamilySeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\SkillFamily;
-use Database\Helpers\KeyStringHelpers;
 use Exception;
 use Illuminate\Database\Seeder;
 
@@ -16,139 +15,53 @@ class SkillFamilySeeder extends Seeder
      */
     public function run()
     {
-        // This is the raw data provided in the spreadsheet
-        $providedData = [
-            [
-                'name_en' => 'Programming Languages and Tools',
-                'name_fr' => 'Langages et outils de programmation',
-            ],
-            [
-                'name_en' => 'Development and Programming',
-                'name_fr' => 'Développement et programmation',
-            ],
-            [
-                'name_en' => 'Database Design & Data Administration',
-                'name_fr' => 'Conception de bases de données et administration des données',
-            ],
-            [
-                'name_en' => 'IT Architecture',
-                'name_fr' => 'Architecture informatique',
-            ],
-            [
-                'name_en' => 'IT Project Management',
-                'name_fr' => 'Gestion des projets informatiques',
-            ],
-            [
-                'name_en' => 'Technical Advising',
-                'name_fr' => 'Conseil technique',
-            ],
-            [
-                'name_en' => 'User Experience and Interface Design',
-                'name_fr' => 'Expérience utilisateur et conception d’interfaces',
-            ],
-            [
-                'name_en' => 'IT Security',
-                'name_fr' => 'Sécurité informatique',
-            ],
-            [
-                'name_en' => 'Infrastructure/Operations',
-                'name_fr' => 'Infrastructure/Opérations',
-            ],
-            [
-                'name_en' => 'Cyber Security',
-                'name_fr' => 'Cybersécurité',
-            ],
-            [
-                'name_en' => 'IT Management',
-                'name_fr' => 'Gestion informatique',
-            ],
-            [
-                'name_en' => 'Information Management',
-                'name_fr' => 'Gestion de l’information',
-            ],
-            [
-                'name_en' => 'Personal',
-                'name_fr' => 'Personnel',
-            ],
-            [
-                'name_en' => 'Interpersonal',
-                'name_fr' => 'Interpersonnel',
-            ],
-            [
-                'name_en' => 'Leadership',
-                'name_fr' => 'Leadership',
-            ],
-            [
-                'name_en' => 'Communication',
-                'name_fr' => 'Communication',
-            ],
-            [
-                'name_en' => 'Thinking',
-                'name_fr' => 'Réfléchir',
-            ],
-            [
-                'name_en' => 'Working in Government',
-                'name_fr' => 'Travailler au sein du gouvernement',
-            ],
-        ];
+        /* To recreate the JSON file, run this GraphQL query:
+            query SkillsFamilies {
+                skillFamilies {
+                key
+                name { en fr }
+                description { en fr }
+                }
+            }
 
-        $reshapedData = array_map(
-            function ($record) {
-                // Take the provided data and reshape it to our data model
-                $model = [
-                    'key' => KeyStringHelpers::toKeyString(trim($record['name_en']), '_'), // no key provided so making our own slug
-                    'name' => [
-                        'en' => trim($record['name_en']),
-                        'fr' => trim($record['name_fr']),
-                    ],
-                    'description' => [ // no descriptions provided so reusing name
-                        'en' => trim($record['name_en']),
-                        'fr' => trim($record['name_fr']),
-                    ],
-                ];
-
-                // unique identifier
-                $identifier = [
-                    'key' => $model['key'],
-                ];
-
-                return [
-                    'model' => $model,
-                    'identifier' => $identifier,
-                ];
-            },
-            $providedData
-        );
+            You can use VS Code extension "Thinker.sort-json" to sort the results for a good commit diff.
+        */
+        $fileContents = file_get_contents(base_path('database/seeders/SkillFamilySeeder.data.json'));
+        if (! $fileContents) {
+            throw new Exception('Failed to load JSON file');
+        }
+        $fileJson = json_decode($fileContents);
+        if (! $fileJson) {
+            throw new Exception('Failed to decode JSON file');
+        }
+        $models = $fileJson->data->skillFamilies;
 
         // Check for duplicate keys
         $keys = array_map(
-            function ($element) {
-                return $element['model']['key'];
+            function ($model) {
+                return $model->key;
             },
-            $reshapedData
+            $models
         );
-        if (count(array_unique($keys)) != count($reshapedData)) {
+        if (count(array_unique($keys)) != count($models)) {
             throw new Exception('The keys are not unique');
         }
 
-        // Check for duplicate English names (used for skill lookup)
-        $englishNames = array_map(
-            function ($element) {
-                return $element['model']['name']['en'];
-            },
-            $reshapedData
-        );
-        assert((count(array_unique($englishNames)) == count($reshapedData)));
-        if (count(array_unique($englishNames)) != count($reshapedData)) {
-            throw new Exception('The English names are not unique');
-        }
-
-        // Iterate the reshaped data to load it
-        foreach ($reshapedData as [
-            'model' => $model,
-            'identifier' => $identifier
-        ]) {
-            SkillFamily::updateOrCreate($identifier, $model);
+        // Iterate the data to load it
+        foreach ($models as $model) {
+            SkillFamily::updateOrCreate(
+                ['key' => $model->key],
+                [
+                    'name' => [
+                        'en' => $model->name->en,
+                        'fr' => $model->name->fr,
+                    ],
+                    'description' => [
+                        'en' => $model->description->en,
+                        'fr' => $model->description->fr,
+                    ],
+                ]
+            );
         }
     }
 }
