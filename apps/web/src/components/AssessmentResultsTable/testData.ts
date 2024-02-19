f8232a30a69135e0081a51d4a7e102e3ce1d1c69
Yonathan K <yoni.kid15@gmail.com>
2024-02-19 09:06:40 -0500

    Remove unused import

--- apps/web/src/components/AssessmentResultsTable/testData.ts
+++ apps/web/src/components/AssessmentResultsTable/testData.ts
@@ -12,7 +12,6 @@ import {
   AssessmentDecision,
   AssessmentDecisionLevel,
   AssessmentResult,
-  AssessmentResultJustification,
   AssessmentResultType,
   AssessmentStep,
   AssessmentStepType,
