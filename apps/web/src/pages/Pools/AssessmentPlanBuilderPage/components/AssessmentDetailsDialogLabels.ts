562e9abf139c4d33f37b372086eed7d6dfca7835
Matt Nigh <matt@mattnigh.com>
2024-02-21 07:22:26 -0500

    Add defineMessage

--- apps/web/src/pages/Pools/AssessmentPlanBuilderPage/components/AssessmentDetailsDialogLabels.ts
+++ apps/web/src/pages/Pools/AssessmentPlanBuilderPage/components/AssessmentDetailsDialogLabels.ts
@@ -1,40 +1,42 @@
+import { defineMessage } from "react-intl";
+
 const labels = {
-  typeOfAssessment: {
+  typeOfAssessment: defineMessage({
     defaultMessage: "Type of assessment",
     id: "3/AA3n",
     description:
       "Label for 'Type of assessment' input on the assessment details dialog",
-  },
-  assessmentTitleEn: {
+  }),
+  assessmentTitleEn: defineMessage({
     defaultMessage: "English - Optional assessment title",
     id: "/+cTZi",
     description:
       "Label for 'English assessment title' input on the assessment details dialog",
-  },
-  assessmentTitleFr: {
+  }),
+  assessmentTitleFr: defineMessage({
     defaultMessage: "French - Optional assessment title",
     id: "OoZjap",
     description:
       "Label for 'French assessment title' input on the assessment details dialog",
-  },
-  screeningQuestionEn: {
+  }),
+  screeningQuestionEn: defineMessage({
     defaultMessage: "English - Question {questionNumber}",
     id: "2op8I5",
     description:
       "Label for 'English screening question' input on the assessment details dialog",
-  },
-  screeningQuestionFr: {
+  }),
+  screeningQuestionFr: defineMessage({
     defaultMessage: "French - Question {questionNumber}",
     id: "9i4kZp",
     description:
       "Label for 'English screening question' input on the assessment details dialog",
-  },
-  assessedSkills: {
+  }),
+  assessedSkills: defineMessage({
     defaultMessage: "Assessed skills",
-    id: "OoZjap",
+    id: "7Jc8aA",
     description:
       "Label for 'assessed skills' input on the assessment details dialog",
-  },
+  }),
 };
 
 export default labels;
