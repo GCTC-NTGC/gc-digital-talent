4f70d63814766e03f700fe2dd2d0bbe45fbd0d93
Yonathan K <yoni.kid15@gmail.com>
2024-02-23 14:05:09 -0500

    Add pagination state to pool candidates table

--- apps/web/src/components/PoolCandidatesTable/PoolCandidatesTable.tsx
+++ apps/web/src/components/PoolCandidatesTable/PoolCandidatesTable.tsx
@@ -277,6 +277,7 @@ const PoolCandidatesTable = ({
   const filterRef = React.useRef<PoolCandidateSearchInput | undefined>(
     initialFilters,
   );
+
   const [paginationState, setPaginationState] = React.useState<PaginationState>(
     initialState.paginationState
       ? {
@@ -325,6 +326,10 @@ const PoolCandidatesTable = ({
   };
 
   const handleFilterSubmit: SubmitHandler<FormValues> = (data) => {
+    setPaginationState((previous) => ({
+      ...previous,
+      pageIndex: 0,
+    }));
     const transformedData: PoolCandidateSearchInput =
       transformFormValuesToFilterState(data);
 
@@ -789,6 +794,8 @@ const PoolCandidatesTable = ({
       }}
       pagination={{
         internal: false,
+        initialState: INITIAL_STATE.paginationState,
+        state: paginationState,
         total: data?.poolCandidatesPaginated?.paginatorInfo.total,
         pageSizes: [10, 20, 50, 100, 500],
         onPaginationChange: ({ pageIndex, pageSize }: PaginationState) => {
