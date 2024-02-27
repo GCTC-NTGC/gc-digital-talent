95793dfc31d2059e05f0155f5501efa31eda566d
Eric Sizer <eric.sizer@gmail.com>
2024-02-27 09:45:55 -0500

    remove classification_user table

--- api/database/migrations/2024_02_26_182809_remove_expected_salary_column_users_table.php
+++ api/database/migrations/2024_02_26_182809_remove_expected_salary_column_users_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -14,6 +15,8 @@ return new class extends Migration
         Schema::table('users', function (Blueprint $table) {
             $table->dropColumn('expected_salary');
         });
+
+        Schema::dropIfExists('classification_user');
     }
 
     /**
@@ -24,5 +27,17 @@ return new class extends Migration
         Schema::table('users', function (Blueprint $table) {
             $table->jsonb('expected_salary')->nullable();
         });
+
+        Schema::create('classification_user', function (Blueprint $table) {
+            $table->uuid('id')->primary('id');
+            $table->timestamps();
+            $table->uuid('user_id')->nullable(false);
+            $table->uuid('classification_id')->nullable(false);
+
+            $table->foreign('user_id')->references('id')->on('users')->cascadeOnDelete(true);
+            $table->foreign('classification_id')->references('id')->on('classifications');
+            $table->unique(['classification_id', 'user_id'], 'classification_user_unique');
+        });
+        DB::statement('ALTER TABLE classification_user ALTER COLUMN id SET DEFAULT gen_random_uuid();');
     }
 };
