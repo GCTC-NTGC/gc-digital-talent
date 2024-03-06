e316994f747ace4d09ae57f455b7c5d31be46b0a
Brinda Sasikumar <Brinda.Sasikumar@gmail.com>
2024-03-06 13:27:33 -0500

    test without prune

--- infrastructure/bin/deploy.sh
+++ infrastructure/bin/deploy.sh
@@ -73,8 +73,3 @@ npm ci --include=dev
 ### Build frontend
 npm run build
 chmod -R a+r,a+w node_modules
-
-if [ "$GCDT_DEV" = false ]; then
-### Cleanup frontend npm dependencies
- npm prune --production
-fi
