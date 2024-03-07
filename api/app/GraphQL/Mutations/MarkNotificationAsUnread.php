63a257fbe9aa264baec18f6e1846cc049d579d1b
Eric Sizer <eric.sizer@gmail.com>
2024-03-07 13:41:44 -0500

    add new mutations for notifications

---
+++ api/app/GraphQL/Mutations/MarkNotificationAsUnread.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\GraphQL\Mutations;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+final class MarkNotificationAsUnread
+{
+    /**
+     * Mark a specific notification as read
+     *
+     * @param  array{}  $args
+     */
+    public function __invoke($_, array $args)
+    {
+        $notificationId = $args['id'];
+        $user = User::find(Auth::id());
+        $notification = $user
+            ->notifications()
+            ->firstWhere('id', $notificationId);
+
+        if (! is_null($notification)) {
+            $notification->markAsUnread();
+            User::enrichNotification($notification);
+
+            return $notification;
+        }
+    }
+}
