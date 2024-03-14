40bf45b713cd9da43bd75b9f2817132fd4d00a01
Eric Sizer <eric.sizer@gmail.com>
2024-03-14 14:50:10 -0400

    use quieter remove command

--- infrastructure/bin/deploy.sh
+++ infrastructure/bin/deploy.sh
@@ -74,4 +74,4 @@ pnpm install --frozen-lockfile
 pnpm run build
 
 ### Remove modules once build completes:
-find . -name "node_modules" -type d -exec rm -rf {} \;
+find . -name 'node_modules' -type d -prune -exec rm -rf '{}' +
