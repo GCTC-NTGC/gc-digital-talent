5909c918f98198e7659813e22b260c43e0437314
JamesHuf <jamiehufnagel@hotmail.com>
2024-03-18 17:57:53 -0700

    Add mutation with validator for updating ignored notification fields

---
+++ api/app/GraphQL/Validators/Mutation/UpdateIgnoredNotificationsValidator.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\GraphQL\Validators\Mutation;
+
+use App\Enums\NotificationFamily;
+use Illuminate\Validation\Rule;
+use Nuwave\Lighthouse\Validation\Validator;
+
+final class UpdateIgnoredNotificationsValidator extends Validator
+{
+    /**
+     * Return the validation rules.
+     *
+     * @return array<string, array<mixed>>
+     */
+    public function rules(): array
+    {
+        // only some notification families can be ignored
+        $validFamilies = [NotificationFamily::APPLICATION_UPDATE->name, NotificationFamily::JOB_ALERT->name];
+        return [
+            'ignoredEmailNotifications' => ['nullable', 'array'],
+            'ignoredEmailNotifications.*' => ['distinct', Rule::in($validFamilies)],
+            'ignoredInAppNotifications' => ['nullable', 'array'],
+            'ignoredInAppNotifications.*' => ['distinct', Rule::in($validFamilies)],
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'in' => ':attribute InvalidValueDeletion',
+        ];
+    }
+}
