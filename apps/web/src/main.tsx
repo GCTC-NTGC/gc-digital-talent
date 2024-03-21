d4551561b5c541efe38e808ec4b6df1acf2b560a
Eric Sizer <eric.sizer@gmail.com>
2024-03-20 22:28:22 -0400

    move hydrogen out of css file

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -5,6 +5,7 @@ import * as messages from "~/lang/frCompiled.json";
 import ContextContainer from "~/components/Context/ContextProvider";
 import Router from "~/components/Router";
 import "~/assets/css/app.css";
+import "~/assets/css/hydrogen.css";
 
 const container = document.getElementById("root");
 if (container) {
