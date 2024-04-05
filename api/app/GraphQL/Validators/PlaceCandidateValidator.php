3579b55ca3ff9d0042120e770fe9907c58cb87a3
vd1992 <40485260+vd1992@users.noreply.github.com>
2024-04-05 17:06:18 -0600

    placeCandidate mutation

---
+++ api/app/GraphQL/Validators/PlaceCandidateValidator.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\GraphQL\Validators;
+
+use App\Enums\PlacementType;
+use App\Enums\PoolCandidateStatus;
+use App\Models\PoolCandidate;
+use Illuminate\Validation\Rule;
+use Nuwave\Lighthouse\Exceptions\ValidationException;
+use Nuwave\Lighthouse\Validation\Validator;
+
+final class PlaceCandidateValidator extends Validator
+{
+    /**
+     * Return the validation rules.
+     *
+     * @return array<string, array<mixed>>
+     */
+    public function rules(): array
+    {
+        $id = $this->arg('id');
+        $candidate = PoolCandidate::find($id);
+        $placedStatuses = array_column(PlacementType::cases(), 'name');
+        $statusesArray = [...$placedStatuses, PoolCandidateStatus::QUALIFIED_AVAILABLE->name];
+
+        if (! (in_array($candidate->pool_candidate_status, $statusesArray))) {
+            throw ValidationException::withMessages(['InvalidStatusForPlacing']);
+        }
+
+        return [
+            'placeCandidate.departmentId' => ['uuid', 'required', Rule::exists('departments', 'id')],
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'exists' => ':attribute does not exist.',
+        ];
+    }
+}
