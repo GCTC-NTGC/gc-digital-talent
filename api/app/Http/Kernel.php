63c56500080483adc99c2afece578fb1f27c5409
Peter Giles <8978655+petertgiles@users.noreply.github.com>
2024-04-09 10:19:27 -0400

    request middleware

--- api/app/Http/Kernel.php
+++ api/app/Http/Kernel.php
@@ -22,6 +22,7 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         // \App\Http\Middleware\SimulateFirewallMiddleware::class,
+        \App\Http\Middleware\ProtectedRequest::class,
     ];
 
     /**
---
+++ api/app/Http/Middleware/ProtectedRequest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+/**
+ * Notices if the request is a secure one that would have been
+ * protected by the VPN.
+ */
+class ProtectedRequest
+{
+    /**
+     * @return mixed Any kind of response
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $path = $request->path();
+        if (str_starts_with($path, 'admin')) {
+            $request->merge(['isSecureRequest' => true]);
+        }
+
+        return $next($request);
+    }
+}
