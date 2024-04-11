59e9be8c7de12c453a8a17eb2a98544b8c4f7c5c
Peter Giles <8978655+petertgiles@users.noreply.github.com>
2024-04-11 16:30:45 -0400

    new signed out page

--- packages/auth/src/const.ts
+++ packages/auth/src/const.ts
@@ -4,7 +4,7 @@ export const ID_TOKEN = "id_token";
 
 export const POST_LOGOUT_OVERRIDE_PATH_KEY = "post_logout_override_path";
 export const LOGOUT_REASON_KEY = "logout_reason";
-export type LogoutReason = "auth-error" | "user-action" | "session-expired";
+export type LogoutReason = "user-deleted" | "user-action" | "session-expired";
 
 // These constants come from api/config/rolepermission.php
 export const ROLE_NAME = {
