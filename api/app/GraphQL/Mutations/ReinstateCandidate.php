951dfb0088b419157bdfe5e9417fb6ea3aaa8ba0
JamesHuf <jamiehufnagel@hotmail.com>
2024-04-16 12:26:04 -0700

    Add mutations for removing and reinstating candidates

---
+++ api/app/GraphQL/Mutations/ReinstateCandidate.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\GraphQL\Mutations;
+
+use App\Enums\PoolCandidateStatus;
+use App\Models\PoolCandidate;
+use Nuwave\Lighthouse\Exceptions\ValidationException;
+
+final class ReinstateCandidate
+{
+    /**
+     * Marks a candidate as removed from the pool
+     *
+     * @param  array{}  $args
+     */
+    public function __invoke($_, array $args)
+    {
+        $candidate = PoolCandidate::find($args['id']);
+
+        // Update the candidates status based on the current status
+        // or throw an error if the candidate has an invalid status
+        switch ($candidate->pool_candidate_status) {
+            case PoolCandidateStatus::SCREENED_OUT_NOT_INTERESTED->name:
+            case PoolCandidateStatus::SCREENED_OUT_NOT_RESPONSIVE->name:
+                $candidate->pool_candidate_status = PoolCandidateStatus::NEW_APPLICATION->name;
+                break;
+            case PoolCandidateStatus::QUALIFIED_UNAVAILABLE->name:
+            case PoolCandidateStatus::QUALIFIED_WITHDREW->name:
+                $candidate->pool_candidate_status = PoolCandidateStatus::QUALIFIED_AVAILABLE->name;
+                break;
+            case PoolCandidateStatus::REMOVED->name:
+                $candidate->pool_candidate_status = PoolCandidateStatus::NEW_APPLICATION->name;
+                break;
+            default:
+                throw ValidationException::withMessages(['CandidateUnexpectedStatus']);
+                break;
+        }
+
+        $candidate->removed_at = null;
+        $candidate->removal_reason = null;
+        $candidate->removal_reason_other = null;
+
+        $candidate->save();
+
+        return $candidate;
+    }
+}
---
+++ api/app/GraphQL/Mutations/RemoveCandidate.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\GraphQL\Mutations;
+
+use App\Enums\CandidateRemovalReason;
+use App\Enums\PoolCandidateStatus;
+use App\Models\PoolCandidate;
+use Illuminate\Support\Carbon;
+use Nuwave\Lighthouse\Exceptions\ValidationException;
+
+final class RemoveCandidate
+{
+    /**
+     * Marks a candidate as removed from the pool
+     *
+     * @param  array{}  $args
+     */
+    public function __invoke($_, array $args)
+    {
+        $candidate = PoolCandidate::find($args['id']);
+
+        $candidate->removed_at = Carbon::now();
+        $candidate->removal_reason = $args['removalReason'];
+        if ($args['removalReason'] === CandidateRemovalReason::OTHER->name) {
+            $candidate->removal_reason_other = $args['removalReasonOther'];
+        }
+
+        // Update the candidates status based on the current status
+        // or throw an error if the candidate is already placed or removed
+        switch ($candidate->pool_candidate_status) {
+            case PoolCandidateStatus::SCREENED_OUT_APPLICATION->name:
+            case PoolCandidateStatus::SCREENED_OUT_ASSESSMENT->name:
+                $candidate->pool_candidate_status = PoolCandidateStatus::SCREENED_OUT_NOT_RESPONSIVE->name;
+                break;
+            case PoolCandidateStatus::QUALIFIED_AVAILABLE->name:
+            case PoolCandidateStatus::EXPIRED->name:
+                $candidate->pool_candidate_status = PoolCandidateStatus::QUALIFIED_UNAVAILABLE->name;
+                break;
+            case PoolCandidateStatus::NEW_APPLICATION->name:
+            case PoolCandidateStatus::APPLICATION_REVIEW->name:
+            case PoolCandidateStatus::SCREENED_IN->name:
+            case PoolCandidateStatus::UNDER_ASSESSMENT->name:
+                $candidate->pool_candidate_status = PoolCandidateStatus::REMOVED->name;
+                break;
+            case PoolCandidateStatus::PLACED_TENTATIVE->name:
+            case PoolCandidateStatus::PLACED_CASUAL->name:
+            case PoolCandidateStatus::PLACED_TERM->name:
+            case PoolCandidateStatus::PLACED_INDETERMINATE->name:
+                throw ValidationException::withMessages(['RemoveCandidateAlreadyPlaced']);
+                break;
+            case PoolCandidateStatus::SCREENED_OUT_NOT_INTERESTED->name:
+            case PoolCandidateStatus::SCREENED_OUT_NOT_RESPONSIVE->name:
+            case PoolCandidateStatus::QUALIFIED_UNAVAILABLE->name:
+            case PoolCandidateStatus::QUALIFIED_WITHDREW->name:
+            case PoolCandidateStatus::REMOVED->name:
+                throw ValidationException::withMessages(['RemoveCandidateAlreadyRemoved']);
+                break;
+            default:
+                throw ValidationException::withMessages(['CandidateUnexpectedStatus']);
+                break;
+        }
+
+        $candidate->save();
+
+        return $candidate;
+    }
+}
