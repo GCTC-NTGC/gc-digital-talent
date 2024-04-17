49928c6b33c85b76044d8ea2c4800605a7fc6034
Peter Giles <8978655+petertgiles@users.noreply.github.com>
2024-04-17 14:17:03 -0400

    environment variables and feature flag

--- packages/client/src/exchanges/protectedEndpointExchange.ts
+++ packages/client/src/exchanges/protectedEndpointExchange.ts
@@ -2,6 +2,7 @@ import { pipe, tap } from "wonka";
 import type { Exchange } from "@urql/core";
 
 const protectedPaths = ["/admin", "/en/admin", "/fr/admin"];
+const protectedUrl = process.env.API_PROTECTED_URI ?? "";
 
 // A custom exchange that changes to the protected endpoint depending on the current location
 
@@ -17,7 +18,9 @@ const protectedEndpointExchange: Exchange =
             window.location.pathname.startsWith(`${path}/`),
         );
         if (isProtectedLocation) {
-          op.context.url = "/admin/graphql";
+          // we're updating the operation on the fly
+          // eslint-disable-next-line no-param-reassign
+          op.context.url = protectedUrl;
         }
       }),
       forward,
