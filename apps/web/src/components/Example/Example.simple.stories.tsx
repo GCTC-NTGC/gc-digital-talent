2c7f47589f3075594bfc734c67fec2f4d2b058c0
Matt Nigh <matt@mattnigh.com>
2024-04-18 14:52:06 -0400

    Remove allModes

--- apps/web/src/components/Example/Example.simple.stories.tsx
+++ apps/web/src/components/Example/Example.simple.stories.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import type { Meta, StoryObj } from "@storybook/react";
 
-import { allModes } from "@gc-digital-talent/storybook-helpers";
-
 import Example from "./Example";
 
 // The default export metadata controls how Storybook lists your stories and provides information used by addons.
