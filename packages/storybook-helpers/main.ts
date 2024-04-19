a7dce29ceac76b7867b7f2b1f0e050eb6c0c5067
Eric Sizer <eric.sizer@gmail.com>
2024-04-19 17:44:17 -0400

    add storybook config for tailwind

--- packages/storybook-helpers/main.ts
+++ packages/storybook-helpers/main.ts
@@ -63,6 +63,29 @@ const main: StorybookConfig = {
     "@storybook/addon-viewport",
     "@storybook/addon-webpack5-compiler-swc",
     "storybook-react-intl",
+    {
+      name: "@storybook/addon-styling-webpack",
+      options: {
+        rules: [
+          // Replaces existing CSS rules to support PostCSS
+          {
+            test: /\.css$/,
+            use: [
+              "style-loader",
+              {
+                loader: "css-loader",
+                options: { importLoaders: 1 },
+              },
+              {
+                // Gets options from `postcss.config.js` in your project root
+                loader: "postcss-loader",
+                options: { implementation: require.resolve("postcss") },
+              },
+            ],
+          },
+        ],
+      },
+    },
   ],
   framework: {
     name: "@storybook/react-webpack5",
