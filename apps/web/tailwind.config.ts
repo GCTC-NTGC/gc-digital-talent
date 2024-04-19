9071a5d633ea67cd07362e09234064333fb80af3
Eric Sizer <eric.sizer@gmail.com>
2024-04-19 12:34:46 -0400

    add start of colour config

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -3,7 +3,12 @@ import type { Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{tsx,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: `rgb(var(--color-primary) / <alpha-value>)`,
+        background: `rgb(var(--color-background) / <alpha-value>)`,
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
