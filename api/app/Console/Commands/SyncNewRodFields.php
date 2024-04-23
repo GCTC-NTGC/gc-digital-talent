5486d6457f0e199fb2d5caafd18989c8d2ac94c9
vd1992 <40485260+vd1992@users.noreply.github.com>
2024-04-23 16:15:29 -0600

    refactor to use facade

--- api/app/Console/Commands/SyncNewRodFields.php
+++ api/app/Console/Commands/SyncNewRodFields.php
@@ -4,9 +4,9 @@ namespace App\Console\Commands;
 
 use App\Enums\CandidateRemovalReason;
 use App\Enums\PoolCandidateStatus;
-use App\Models\PoolCandidate;
 use Carbon\Carbon;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
 
 class SyncNewRodFields extends Command
 {
@@ -38,6 +38,7 @@ class SyncNewRodFields extends Command
             PoolCandidateStatus::QUALIFIED_AVAILABLE->name,
             PoolCandidateStatus::QUALIFIED_UNAVAILABLE->name,
             PoolCandidateStatus::QUALIFIED_WITHDREW->name,
+            PoolCandidateStatus::PLACED_TENTATIVE->name,
             PoolCandidateStatus::PLACED_CASUAL->name,
             PoolCandidateStatus::PLACED_TERM->name,
             PoolCandidateStatus::PLACED_INDETERMINATE->name,
@@ -46,87 +47,57 @@ class SyncNewRodFields extends Command
             PoolCandidateStatus::SCREENED_OUT_ASSESSMENT->name,
         ];
 
-        $applicableModels = PoolCandidate::with('user') // lazy load error otherwise
-            ->whereNotIn('pool_candidate_status', [
-                PoolCandidateStatus::DRAFT->name,
-                PoolCandidateStatus::DRAFT_EXPIRED->name,
-                PoolCandidateStatus::NEW_APPLICATION->name,
-                PoolCandidateStatus::APPLICATION_REVIEW->name,
-                PoolCandidateStatus::SCREENED_IN->name,
-                PoolCandidateStatus::UNDER_ASSESSMENT->name,
-            ])
-            ->get();
-        $modelCount = count($applicableModels);
-        $modelUpdatedCount = 0;
+        // set final_decision_at
+        DB::table('pool_candidates')
+            ->whereIn('pool_candidate_status', $finalDecisionStatuses)
+            ->whereNull('final_decision_at')
+            ->update(['final_decision_at' => $dateNow]);
 
-        foreach ($applicableModels as $model) {
-            $modelMutated = false;
+        // set removed_at
+        DB::table('pool_candidates')
+            ->whereIn('pool_candidate_status', $removedStatuses)
+            ->whereNull('removed_at')
+            ->update(['removed_at' => $dateNow]);
 
-            // set final_decision_at
-            if (in_array($model->pool_candidate_status, $finalDecisionStatuses) && is_null($model->final_decision_at)) {
-                $model->final_decision_at = $dateNow;
+        // set removal_reason and removal_reason_other
+        DB::table('pool_candidates')
+            ->where('pool_candidate_status', PoolCandidateStatus::SCREENED_OUT_NOT_INTERESTED->name)
+            ->whereNull('removal_reason')
+            ->update([
+                'removal_reason' => CandidateRemovalReason::REQUESTED_TO_BE_WITHDRAWN->name,
+            ]);
+        DB::table('pool_candidates')
+            ->where('pool_candidate_status', PoolCandidateStatus::SCREENED_OUT_NOT_RESPONSIVE->name)
+            ->whereNull('removal_reason')
+            ->update([
+                'removal_reason' => CandidateRemovalReason::NOT_RESPONSIVE->name,
+            ]);
+        DB::table('pool_candidates')
+            ->where('pool_candidate_status', PoolCandidateStatus::QUALIFIED_UNAVAILABLE->name)
+            ->whereNull('removal_reason')
+            ->update([
+                'removal_reason' => CandidateRemovalReason::OTHER->name,
+                'removal_reason_other' => 'Was "QUALIFIED_UNAVAILABLE"',
+            ]);
+        DB::table('pool_candidates')
+            ->where('pool_candidate_status', PoolCandidateStatus::QUALIFIED_WITHDREW->name)
+            ->whereNull('removal_reason')
+            ->update([
+                'removal_reason' => CandidateRemovalReason::REQUESTED_TO_BE_WITHDRAWN->name,
+            ]);
+        DB::table('pool_candidates')
+            ->where('pool_candidate_status', PoolCandidateStatus::REMOVED->name)
+            ->whereNull('removal_reason')
+            ->update([
+                'removal_reason' => CandidateRemovalReason::OTHER->name,
+                'removal_reason_other' => 'Was "REMOVED"',
+            ]);
 
-                $modelMutated = true;
-            }
-
-            // set removed_at
-            if (in_array($model->pool_candidate_status, $removedStatuses) && is_null($model->removed_at)) {
-                $model->removed_at = $dateNow;
-
-                $modelMutated = true;
-            }
-
-            // set removal_reason
-            if (in_array($model->pool_candidate_status, $removedStatuses) && is_null($model->removal_reason)) {
-                if ($model->pool_candidate_status === PoolCandidateStatus::SCREENED_OUT_NOT_INTERESTED->name) {
-                    $model->removal_reason = CandidateRemovalReason::REQUESTED_TO_BE_WITHDRAWN->name;
-                }
-                if ($model->pool_candidate_status === PoolCandidateStatus::SCREENED_OUT_NOT_RESPONSIVE->name) {
-                    $model->removal_reason = CandidateRemovalReason::NOT_RESPONSIVE->name;
-                }
-                if ($model->pool_candidate_status === PoolCandidateStatus::QUALIFIED_UNAVAILABLE->name) {
-                    $model->removal_reason = CandidateRemovalReason::OTHER->name;
-                }
-                if ($model->pool_candidate_status === PoolCandidateStatus::QUALIFIED_WITHDREW->name) {
-                    $model->removal_reason = CandidateRemovalReason::REQUESTED_TO_BE_WITHDRAWN->name;
-                }
-                if ($model->pool_candidate_status === PoolCandidateStatus::REMOVED->name) {
-                    $model->removal_reason = CandidateRemovalReason::OTHER->name;
-                }
-
-                $modelMutated = true;
-            }
-
-            // set removal_reason_other
-            if (
-                in_array($model->pool_candidate_status, [PoolCandidateStatus::QUALIFIED_UNAVAILABLE->name, PoolCandidateStatus::REMOVED->name]) &&
-                is_null($model->removal_reason_other)
-            ) {
-                if ($model->pool_candidate_status === PoolCandidateStatus::QUALIFIED_UNAVAILABLE->name) {
-                    $model->removal_reason_other = 'Was "QUALIFIED_UNAVAILABLE"';
-                }
-                if ($model->pool_candidate_status === PoolCandidateStatus::REMOVED->name) {
-                    $model->removal_reason_other = 'Was "REMOVED"';
-                }
-
-                $modelMutated = true;
-            }
-
-            // set placed_at
-            if (in_array($model->pool_candidate_status, $placedStatuses) && is_null($model->placed_at)) {
-                $model->placed_at = $dateNow;
-
-                $modelMutated = true;
-            }
-
-            if ($modelMutated) {
-                $modelUpdatedCount++;
-                $model->save();
-            }
-        }
-
-        $this->info("$modelCount records looped through");
-        $this->info("$modelUpdatedCount records mutated");
+        // set placed_at
+        DB::table('pool_candidates')
+            ->whereIn('pool_candidate_status', $placedStatuses)
+            ->whereNull('placed_at')
+            ->update(['placed_at' => $dateNow]);
 
         return Command::SUCCESS;
     }
