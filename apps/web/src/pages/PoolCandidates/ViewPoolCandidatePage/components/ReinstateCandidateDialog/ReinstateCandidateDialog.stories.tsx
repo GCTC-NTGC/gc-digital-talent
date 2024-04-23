b1bc2eb05e12792c0286a7383e88b50f5b7d0659
Eric Sizer <eric.sizer@gmail.com>
2024-04-23 12:20:12 -0400

    add missing decorators

--- apps/web/src/pages/PoolCandidates/ViewPoolCandidatePage/components/ReinstateCandidateDialog/ReinstateCandidateDialog.stories.tsx
+++ apps/web/src/pages/PoolCandidates/ViewPoolCandidatePage/components/ReinstateCandidateDialog/ReinstateCandidateDialog.stories.tsx
@@ -2,6 +2,7 @@ import type { Meta, StoryObj } from "@storybook/react";
 
 import { fakePoolCandidates } from "@gc-digital-talent/fake-data";
 import { makeFragmentData } from "@gc-digital-talent/graphql";
+import { OverlayOrDialogDecorator } from "@gc-digital-talent/storybook-helpers";
 
 import ReinstateCandidateDialog, {
   ReinstateCandidateDialog_Fragment,
@@ -16,6 +17,7 @@ const mockCandidateFragment = makeFragmentData(
 const meta = {
   title: "Components/Reinstate Candidate Dialog",
   component: ReinstateCandidateDialog,
+  decorators: [OverlayOrDialogDecorator],
   args: {
     reinstateQuery: mockCandidateFragment,
     defaultOpen: true,
--- apps/web/src/pages/PoolCandidates/ViewPoolCandidatePage/components/RemoveCandidateDialog/RemoveCandidateDialog.stories.tsx
+++ apps/web/src/pages/PoolCandidates/ViewPoolCandidatePage/components/RemoveCandidateDialog/RemoveCandidateDialog.stories.tsx
@@ -2,6 +2,7 @@ import type { Meta, StoryObj } from "@storybook/react";
 
 import { fakePoolCandidates } from "@gc-digital-talent/fake-data";
 import { makeFragmentData } from "@gc-digital-talent/graphql";
+import { OverlayOrDialogDecorator } from "@gc-digital-talent/storybook-helpers";
 
 import RemoveCandidateDialog, {
   RemoveCandidateDialog_Fragment,
@@ -16,6 +17,7 @@ const mockCandidateFragment = makeFragmentData(
 const meta = {
   title: "Components/Remove Candidate Dialog",
   component: RemoveCandidateDialog,
+  decorators: [OverlayOrDialogDecorator],
   args: {
     removalQuery: mockCandidateFragment,
     defaultOpen: true,
