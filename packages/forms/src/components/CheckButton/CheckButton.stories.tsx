6d19c5005e010a58560f4d8bda02b4095506de4e
Matt Nigh <matt@mattnigh.com>
2024-04-24 09:03:18 -0400

    Refactor story

--- packages/forms/src/components/CheckButton/CheckButton.stories.tsx
+++ packages/forms/src/components/CheckButton/CheckButton.stories.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { StoryFn, Meta } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
 
+import { allModes } from "@gc-digital-talent/storybook-helpers";
+
 import CheckButton from "./CheckButton";
 
 export default {
@@ -10,9 +12,15 @@ export default {
   args: {
     label: "Check button label",
   },
+  parameters: {
+    chromatic: {
+      modes: {
+        light: allModes.light,
+        dark: allModes.dark,
+      },
+    },
+  },
 } as Meta;
-
-const themes = ["light", "dark"];
 
 const Template: StoryFn<{ label: string }> = (args) => {
   const [isChecked, setChecked] = React.useState<boolean>(false);
@@ -25,78 +33,38 @@ const Template: StoryFn<{ label: string }> = (args) => {
   };
 
   return (
-    <div
-      data-h2-display="base(grid)"
-      data-h2-grid-template-columns="base(100%)"
-    >
-      {themes.map((theme) => (
-        <div data-h2={theme} key={theme}>
-          <div data-h2-background="base(background)" data-h2-padding="base(x2)">
-            <div
-              data-h2-display="base(flex)"
-              data-h2-justify-content="base(space-around)"
-              data-h2-margin="base(-x1)"
-              data-h2-color="base(black)"
-            >
-              <div
-                data-h2-display="base(flex)"
-                data-h2-flex-direction="base(column)"
-                data-h2-align-items="base(center)"
-                data-h2-padding="base(x1)"
-              >
-                <span>Not Checked</span>
-                <CheckButton
-                  checked={false}
-                  onToggle={() => action("clicked not checked")}
-                  label="Not Checked"
-                />
-              </div>
-              <div
-                data-h2-display="base(flex)"
-                data-h2-flex-direction="base(column)"
-                data-h2-align-items="base(center)"
-                data-h2-padding="base(x1)"
-              >
-                <span>Checked</span>
-                <CheckButton
-                  checked
-                  onToggle={() => action("clicked checked")}
-                  label="Checked"
-                />
-              </div>
-              <div
-                data-h2-display="base(flex)"
-                data-h2-flex-direction="base(column)"
-                data-h2-align-items="base(center)"
-                data-h2-padding="base(x1)"
-              >
-                <span>Indeterminate</span>
-                <CheckButton
-                  checked={false}
-                  indeterminate
-                  onToggle={() => action("clicked indeterminate")}
-                  label="Indeterminate"
-                />
-              </div>
-              <div
-                data-h2-display="base(flex)"
-                data-h2-flex-direction="base(column)"
-                data-h2-align-items="base(center)"
-                data-h2-padding="base(x1)"
-              >
-                <span>Controlled</span>
-                <CheckButton
-                  checked={isChecked}
-                  onToggle={handleCheck}
-                  label={label}
-                />
-              </div>
-            </div>
-          </div>
-        </div>
-      ))}
-    </div>
+    <>
+      <div>
+        <span>Not Checked</span>
+        <CheckButton
+          checked={false}
+          onToggle={() => action("clicked not checked")}
+          label="Not Checked"
+        />
+      </div>
+      <div>
+        <span>Checked</span>
+        <CheckButton
+          checked
+          onToggle={() => action("clicked checked")}
+          label="Checked"
+        />
+      </div>
+      <div>
+        <span>Indeterminate</span>
+        <CheckButton
+          checked={false}
+          indeterminate
+          onToggle={() => action("clicked indeterminate")}
+          label="Indeterminate"
+        />
+      </div>
+      <div>
+        <span>Controlled</span>
+        <CheckButton checked={isChecked} onToggle={handleCheck} label={label} />
+      </div>
+    </>
   );
 };
 
-export const BasicCheckButton = Template.bind({});
+export const Default = Template.bind({});
