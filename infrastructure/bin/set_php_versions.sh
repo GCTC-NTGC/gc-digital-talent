6256d13497236c8386e27aeee524b724d117e087
vd1992 <40485260+vd1992@users.noreply.github.com>
2024-04-30 14:18:10 -0600

    try something

--- infrastructure/bin/set_php_versions.sh
+++ infrastructure/bin/set_php_versions.sh
@@ -10,6 +10,12 @@ set -o nounset
 # Assign PHP version from arg if supplied. Runs default PHP bin without.
 PHP_VERSION=$1
 
+sudo update-alternatives --install /usr/bin/php php /usr/bin/php${PHP_VERSION} 100
+sudo update-alternatives --install /usr/bin/phar phar /usr/bin/php${PHP_VERSION} 100
+sudo update-alternatives --install /usr/bin/phpdbg phpdbg /usr/bin/php${PHP_VERSION} 100
+sudo update-alternatives --install /usr/bin/php-cgi php-cgi /usr/bin/php${PHP_VERSION} 100
+sudo update-alternatives --install /usr/bin/phar.phar phar.phar /usr/bin/php${PHP_VERSION} 100
+
 sudo update-alternatives --set php /usr/bin/php${PHP_VERSION}
 sudo update-alternatives --set phar /usr/bin/phar${PHP_VERSION}
 sudo update-alternatives --set phpdbg /usr/bin/phpdbg${PHP_VERSION}
