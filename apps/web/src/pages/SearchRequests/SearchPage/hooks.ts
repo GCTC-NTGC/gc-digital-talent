a57fd5027cde20ffe61182401ca4066396ccb5b9
Eric Sizer <eric.sizer@gmail.com>
2024-05-01 13:28:11 -0400

    remove additional references

--- apps/web/src/pages/SearchRequests/SearchPage/hooks.ts
+++ apps/web/src/pages/SearchRequests/SearchPage/hooks.ts
@@ -62,14 +62,17 @@ const CandidateCount_Query = graphql(/* GraphQL */ `
           group
           level
         }
-        essentialSkills {
+        poolSkills(type: ESSENTIAL) {
           id
-          key
-          name {
-            en
-            fr
+          skill {
+            id
+            key
+            name {
+              en
+              fr
+            }
+            category
           }
-          category
         }
         team {
           id
