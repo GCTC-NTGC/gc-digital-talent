f782fc7621ea85c1c68b9d79bc832802b773a78f
Peter Giles <8978655+petertgiles@users.noreply.github.com>
2024-05-08 16:32:08 -0400

    unit tests

--- api/app/Notifications/TalentRequestSubmissionConfirmation.php
+++ api/app/Notifications/TalentRequestSubmissionConfirmation.php
@@ -2,12 +2,11 @@
 
 namespace App\Notifications;
 
-use App\Models\User;
 use App\Notifications\Messages\GcNotifyEmailMessage;
+use Exception;
 use Illuminate\Bus\Queueable;
 use Illuminate\Notifications\AnonymousNotifiable;
 use Illuminate\Notifications\Notification;
-use Illuminate\Support\Facades\Log;
 
 class TalentRequestSubmissionConfirmation extends Notification implements CanBeSentViaGcNotifyEmail
 {
@@ -49,17 +48,8 @@ class TalentRequestSubmissionConfirmation extends Notification implements CanBeS
                     'requestor name' => $routingInfo['requestor name'],
                 ]
             );
-        } elseif ($notifiable instanceof User) {
-            $message = new GcNotifyEmailMessage(
-                $templateId,
-                $notifiable->email,
-                [
-                    'tracking number' => $this->trackingNumber,
-                    'requestor name' => $notifiable->first_name.' '.$notifiable->last_name,
-                ]
-            );
         } else {
-            Log::error('Could not notify notifiable of type '.get_class($notifiable));
+            throw new Exception('Not implemented');
         }
 
         return $message;
---
+++ api/tests/Feature/Notifications/TriggerTalentRequestSubmissionConfirmationTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Feature\Notifications;
+
+use App\Models\Department;
+use App\Models\PoolCandidateSearchRequest;
+use App\Notifications\TalentRequestSubmissionConfirmation;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Notification;
+use Tests\TestCase;
+
+class TriggerTalentRequestSubmissionConfirmationTest extends TestCase
+{
+    use RefreshDatabase;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        Notification::fake();
+    }
+
+    // triggers a notification when an application is moved into a final decision status
+    public function testNotifyWhenRequest(): void
+    {
+        Department::factory()->create();
+        PoolCandidateSearchRequest::factory()->create();
+
+        Notification::assertSentOnDemand(TalentRequestSubmissionConfirmation::class);
+    }
+}
