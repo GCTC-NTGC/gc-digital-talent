c0b613fd67977e970fc811b16bfaabe169bda19b
vd1992 <40485260+vd1992@users.noreply.github.com>
2024-05-08 13:16:58 -0600

    community query

---
+++ api/app/Policies/CommunityPolicy.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class CommunityPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view any models.
+     * Everyone is allowed to view the communities
+     *
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function viewAny(?User $user)
+    {
+        return $user->isAbleTo('view-any-community');
+    }
+}
