6ac3f018bf9268bdb3033b7f526c4dbd3cfb90d6
Peter Giles <8978655+petertgiles@users.noreply.github.com>
2024-05-13 17:10:17 -0400

    finish test

--- apps/web/src/pages/Profile/ProfilePage/ProfileSnapshot.test.ts
+++ apps/web/src/pages/Profile/ProfilePage/ProfileSnapshot.test.ts
@@ -1,11 +1,26 @@
 import { UserProfile_FragmentText as frontendQuery } from "./ProfilePage";
-import { diffLinesUnified } from "jest-diff";
+import { diffLinesRaw, DIFF_EQUAL, diffLinesUnified } from "jest-diff";
 
 const fs = require("fs");
 const path = require("path");
 
+function normalizeQuery(q: string[]): string[] {
+  const result = q.map((line) => {
+    return line
+      .replace(/#.*$/gm, "") // strip comments
+      .trimEnd(); // strip trailing spaces
+  });
+  return result.filter((line) => line.length > 0); // strip empty lines
+}
+
 describe("Profile snapshot tests", () => {
   test("Should have both queries matching", () => {
+    const frontendQueryLines = normalizeQuery(frontendQuery.split("\n"));
+    if (frontendQueryLines[0].trimStart().startsWith("fragment UserProfile")) {
+      // expect that first line will be different so remove it from test
+      frontendQueryLines.splice(0, 1);
+    }
+
     const backendQueryPath = path.resolve(
       "~/../../../api/app/GraphQL/Mutations/PoolCandidateSnapshot.graphql",
     );
@@ -16,14 +31,34 @@ describe("Profile snapshot tests", () => {
         return data;
       },
     );
-
-    const difference = diffLinesUnified(
-      frontendQuery.split("\n"),
-      backendQuery.split("\n"),
-    );
-
-    if (difference) {
-      console.error(difference);
+    const backendQueryLines = normalizeQuery(backendQuery.split("\n"));
+    if (
+      backendQueryLines[0].trimStart().startsWith("query getProfile(") &&
+      backendQueryLines[1].trimStart().startsWith("user(")
+    ) {
+      // expect that first two lines will be different so remove them from test
+      backendQueryLines.splice(0, 2);
     }
+
+    const diffAnalysis = diffLinesRaw(frontendQueryLines, backendQueryLines);
+    const unequalLineCount = diffAnalysis.filter(
+      // filter for lines that are not equal
+      (diff) => diff[0] != DIFF_EQUAL,
+    ).length;
+
+    if (unequalLineCount > 0) {
+      const prettyDiff = diffLinesUnified(
+        frontendQueryLines,
+        backendQueryLines,
+        {
+          aAnnotation: "Frontend",
+          bAnnotation: "Backend",
+          includeChangeCounts: true,
+        },
+      );
+      console.debug(prettyDiff);
+    }
+
+    expect(unequalLineCount).toEqual(0);
   });
 });
