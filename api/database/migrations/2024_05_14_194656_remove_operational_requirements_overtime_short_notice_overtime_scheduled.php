dcba949f34920b39247aec98948777ed6bfa4fea
Matt Nigh <matt@mattnigh.com>
2024-05-14 16:55:35 -0400

    Remove operational requirements overtime_short_notice, overtime_scheduled values

---
+++ api/database/migrations/2024_05_14_194656_remove_operational_requirements_overtime_short_notice_overtime_scheduled.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Console\Commands\RemoveOperationalRequirement;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\Artisan;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Artisan::call(RemoveOperationalRequirement::class, ["operationalRequirement" => "OVERTIME_SCHEDULED"]);
+        Artisan::call(RemoveOperationalRequirement::class, ["operationalRequirement" => "OVERTIME_SHORT_NOTICE"]);
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        // this migration is not reversible.
+    }
+};
