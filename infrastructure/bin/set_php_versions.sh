fb4f763d14d779e0665be3f3e18571a407b0630b
Matt Nigh <matt@mattnigh.com>
2024-05-21 09:51:16 -0400

    Add ppa:ondrej/php

--- infrastructure/bin/set_php_versions.sh
+++ infrastructure/bin/set_php_versions.sh
@@ -10,6 +10,11 @@ set -o nounset
 # Assign PHP version from arg if supplied. Runs default PHP bin without.
 PHP_VERSION=$1
 
+# GitHub runner only includes one version of PHP that is not necesarrily the value of the assigned therefore the Personal Package Archive (PPA) is necessary.
+sudo add-apt-repository ppa:ondrej/php
+sudo apt update
+sudo apt-get install php${PHP_VERSION}
+
 sudo update-alternatives --set php /usr/bin/php${PHP_VERSION}
 sudo update-alternatives --set phar /usr/bin/phar${PHP_VERSION}
 sudo update-alternatives --set phpdbg /usr/bin/phpdbg${PHP_VERSION}
