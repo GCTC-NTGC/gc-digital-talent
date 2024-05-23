d448b485ea75f8e4dc17f4dbc1ef5d2c3a3fc6b0
Brinda Sasikumar <Brinda.Sasikumar@gmail.com>
2024-05-23 10:05:00 -0400

    read the base url from env var

--- apps/playwright/playwright.config.ts
+++ apps/playwright/playwright.config.ts
@@ -28,7 +28,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: "https://dev-talentcloud.tbs-sct.gc.ca",
+    baseURL: process.env.BASE_URL ?? "http://localhost:3000",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "retain-on-failure",
