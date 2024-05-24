c09fc53d66a628870a8908e0620aaa83936083d9
Eric Sizer <eric.sizer@gmail.com>
2024-05-24 10:35:34 -0400

    create base claim ui with placeholder

---
+++ apps/web/src/pages/PoolCandidates/ViewPoolCandidatePage/components/ClaimVerification/messages.ts
@@ -0,0 +1,13 @@
+import { defineMessage } from "react-intl";
+
+export const priorityTitle = defineMessage({
+  defaultMessage: "Priority status",
+  id: "66vzXk",
+  description: "Title for a candidates priority status",
+});
+
+export const veteranTitle = defineMessage({
+  defaultMessage: "Veteran status",
+  id: "fI87Ov",
+  description: "Title for a candidates veteran status",
+});
