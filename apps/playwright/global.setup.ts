45953aadf0a008c25185987640f3ea68f9ad15df
Brinda Sasikumar <Brinda.Sasikumar@gmail.com>
2024-05-24 10:59:57 -0400

    fix playwright

--- apps/playwright/global.setup.ts
+++ apps/playwright/global.setup.ts
@@ -1,24 +1,14 @@
 import { test as setup } from "@playwright/test";
 
 import auth from "~/constants/auth";
-import { login } from "~/utils/auth";
+import { loginBySub } from "~/utils/auth";
 
 setup("authenticate as admin", async ({ page }) => {
-  await login(
-    page,
-    "admin@test.com",
-    process.env.ADMIN_USER_NAME,
-    process.env.ADMIN_USER_PASSWORD,
-  );
+  await loginBySub(page, "admin@test.com");
   await page.context().storageState({ path: auth.STATE.ADMIN });
 });
 
 setup("authenticate as applicant", async ({ page }) => {
-  await login(
-    page,
-    "applicant@test.com",
-    process.env.APPLICANT_USER_NAME,
-    process.env.APPLICANT_USER_PASSWORD,
-  );
+  await loginBySub(page, "applicant@test.com");
   await page.context().storageState({ path: auth.STATE.APPLICANT });
 });
