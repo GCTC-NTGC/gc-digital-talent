d0972b2099cca31eb131d40189f9a0caf74b7111
Matt Nigh <matt@mattnigh.com>
2024-05-27 15:16:11 -0400

    Update test

--- apps/playwright/tests/open-graph.spec.ts
+++ apps/playwright/tests/open-graph.spec.ts
@@ -1,14 +1,12 @@
 import { test, expect } from "@playwright/test";
 
 test.describe("Open Graph", () => {
-  test.beforeEach(async ({ page }) => {
-    await page.goto("/");
-  });
   test("should specify an image", async ({ page }) => {
+    await page.goto("/");
     const locator = await page
       .locator('head meta[property="og:image"]')
       .getAttribute("content");
-    page.goto(locator).then((response) => {
+    await page.goto(locator).then((response) => {
       expect(response.status()).toEqual(200);
     });
   });
