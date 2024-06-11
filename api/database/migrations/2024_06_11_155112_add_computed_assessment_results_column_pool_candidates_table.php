82ff370ae1a93560a51fed263258a8123b72753d
Eric Sizer <eric.sizer@gmail.com>
2024-06-11 14:41:08 -0400

    add computed assessment status field

---
+++ api/database/migrations/2024_06_11_155112_add_computed_assessment_results_column_pool_candidates_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('pool_candidates', function (Blueprint $table) {
+            $table->jsonb('computed_assessment_status')->nullable()->default(json_encode([
+                'decisions' => [],
+                'current_step' => 1,
+            ]));
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('pool_candidates', function (Blueprint $table) {
+            $table->dropColumn('computed_assessment_status');
+        });
+    }
+};
