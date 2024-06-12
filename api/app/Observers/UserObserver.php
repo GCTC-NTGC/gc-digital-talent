6ae5c3e4d5fe5f76b33e7e1bb6caf8fb98207a9b
Peter Giles <8978655+petertgiles@users.noreply.github.com>
2024-06-12 09:12:01 -0400

    [Feature] Email verification (#10582)
    * copy spike
    
    * full cycle
    
    * six chars
    
    * automated tests
    
    * translation

---
+++ api/app/Observers/UserObserver.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\User;
+
+class UserObserver
+{
+    /**
+     * Handle the User "created" event.
+     */
+    public function created(User $user): void
+    {
+        //
+    }
+
+    /**
+     * Handle the User "updated" event.
+     */
+    public function updated(User $user): void
+    {
+        $oldEmail = $user->getOriginal('email');
+        $newEmail = $user->email;
+        if ($newEmail != $oldEmail && ! is_null($user->email_verified_at)) {
+            $user->email_verified_at = null;
+        }
+
+        $user->saveQuietly();
+    }
+
+    /**
+     * Handle the User "deleted" event.
+     */
+    public function deleted(User $user): void
+    {
+        //
+    }
+
+    /**
+     * Handle the User "restored" event.
+     */
+    public function restored(User $user): void
+    {
+        //
+    }
+
+    /**
+     * Handle the User "force deleted" event.
+     */
+    public function forceDeleted(User $user): void
+    {
+        //
+    }
+}
