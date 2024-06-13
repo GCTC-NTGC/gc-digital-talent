8124cdf53a4a6bebfa5ded1bfe079749e657e2c9
Eric Sizer <eric.sizer@gmail.com>
2024-06-13 08:46:00 -0400

    revert test attribute

--- apps/web/src/pages/AccessibilityStatementPage/AccessibilityStatementPage.tsx
+++ apps/web/src/pages/AccessibilityStatementPage/AccessibilityStatementPage.tsx
@@ -273,7 +273,7 @@ export const Component = () => {
         })}
         crumbs={crumbs}
       />
-      <div data-h2-container="base(center, large, x1) p-tablet(center, large, x14)">
+      <div data-h2-container="base(center, large, x1) p-tablet(center, large, x2)">
         <TableOfContents.Wrapper data-h2-margin-top="base(x3)">
           <TableOfContents.Navigation>
             <TableOfContents.List>
