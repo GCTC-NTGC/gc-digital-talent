49f3de6f216d0db94d1d339a097b02e8cb1bb891
Eric Sizer <eric.sizer@gmail.com>
2024-06-24 08:25:36 -0400

    update to use short operational requirements

--- api/lang/en/operational_requirement.php
+++ api/lang/en/operational_requirement.php
@@ -1,11 +1,11 @@
 <?php
 
 return [
-    'shift_work' => 'Availability, willingness and ability to work shift-work.',
-    'on_call' => 'Availability, willingness and ability to work 24/7 on-call status.',
-    'travel' => 'Availability, willingness and ability to travel as required.',
-    'transport_equipment' => 'Availability, willingness and ability to transport, lift and set down equipment weighing up to 20kg.',
-    'drivers_license' => 'Must possess a valid driver\'s license or personal mobility to the degree normally associated with possession of a valid driver\'s license.',
-    'overtime_regular' => 'Availability, willingness and ability to work overtime (Regularly).',
-    'overtime_occasional' => 'Availability, willingness and ability to work overtime (Occasionally).',
+    'shift_work' => 'Shift-work',
+    'on_call' => '24/7 on-call',
+    'travel' => 'Travel as required',
+    'transport_equipment' => 'Transport equipment up to 20kg',
+    'drivers_license' => 'Driver\'s license',
+    'overtime_regular' => 'Overtime (regular)',
+    'overtime_occasional' => 'Overtime (occasional)',
 ];
