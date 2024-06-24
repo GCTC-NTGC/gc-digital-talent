c3a03e027c6c55d401a64dd57fe1ad4525b9aa95
Matt <mnigh@users.noreply.github.com>
2024-06-24 15:47:19 -0400

    [Fix] Refactors admin view user profile (#10765)
    * Refactor UserProfile
    
    * Remove divs
    
    * Remove divs; add margin-bottom
    
    * Update TableOfContents.Heading margin

--- apps/web/src/components/UserProfile/ProfileSections/DiversityEquityInclusionSection.tsx
+++ apps/web/src/components/UserProfile/ProfileSections/DiversityEquityInclusionSection.tsx
@@ -1,9 +1,9 @@
 import { useIntl } from "react-intl";
-import { ReactNode } from "react";
 
 import { unpackMaybes } from "@gc-digital-talent/helpers";
-import { Well, Link } from "@gc-digital-talent/ui";
+import { Well } from "@gc-digital-talent/ui";
 import {
+  commonMessages,
   getEmploymentEquityStatement,
   getIndigenousCommunity,
 } from "@gc-digital-talent/i18n";
@@ -15,13 +15,7 @@ import metisIcon from "~/assets/img/metis-true.webp";
 import otherIcon from "~/assets/img/other-true.webp";
 import { anyCriteriaSelected } from "~/validators/profile/diversityEquityInclusion";
 
-const DiversityEquityInclusionSection = ({
-  user,
-  editPath,
-}: {
-  user: User;
-  editPath?: string;
-}) => {
+const DiversityEquityInclusionSection = ({ user }: { user: User }) => {
   const intl = useIntl();
 
   const { isWoman, hasDisability, isVisibleMinority, indigenousCommunities } =
@@ -31,38 +25,10 @@ const DiversityEquityInclusionSection = ({
       (c) => c !== IndigenousCommunity.LegacyIsIndigenous,
     ) || [];
 
-  const pledgeLink = (text: ReactNode) => {
-    return editPath ? <Link href={editPath}>{text}</Link> : null;
-  };
-
   return (
     <Well>
       <div data-h2-flex-grid="base(flex-start, x2, x1)">
-        {!anyCriteriaSelected(user) && editPath && (
-          <div data-h2-flex-item="base(1of1)">
-            <p>
-              {intl.formatMessage({
-                defaultMessage:
-                  "You have not identified as a member of any employment equity groups.",
-                id: "G3K1/H",
-                description:
-                  "Message indicating the user has not been marked as part of an equity group",
-              })}
-            </p>
-            <p data-h2-margin="base(x.5, 0, 0, 0)">
-              <Link href={editPath}>
-                {intl.formatMessage({
-                  defaultMessage:
-                    "Edit your diversity, equity, and inclusion options.",
-                  id: "P6EhD6",
-                  description:
-                    "Link text to edit diversity, equity, and inclusion information on profile.",
-                })}
-              </Link>
-            </p>
-          </div>
-        )}
-        {!anyCriteriaSelected(user) && !editPath && (
+        {!anyCriteriaSelected(user) && (
           <div data-h2-flex-item="base(1of1)">
             <p>
               {intl.formatMessage({
@@ -116,16 +82,7 @@ const DiversityEquityInclusionSection = ({
                             data-h2-font-weight="base(700)"
                             data-h2-color="base(primary.dark)"
                           >
-                            {intl.formatMessage(
-                              {
-                                defaultMessage:
-                                  "You haven't identified your community yet! <pledgeLink>Please enter here.</pledgeLink>",
-                                id: "1GWQal",
-                                description:
-                                  "The text displayed if the user has selected as indigenous but not selected a community yet.",
-                              },
-                              { pledgeLink },
-                            )}
+                            {intl.formatMessage(commonMessages.notProvided)}
                           </span>
                         </li>
                       )}
@@ -210,18 +167,6 @@ const DiversityEquityInclusionSection = ({
                 )}
               </ul>
             )}
-            <p
-              data-h2-padding="base(x1, 0, 0, 0)"
-              data-h2-color="base(gray.darker)"
-            >
-              {intl.formatMessage({
-                defaultMessage:
-                  "You have identified as a member of an employment equity group.<strong> You can add additional employment equity groups to your profile by editing this section if they apply to you.</strong>",
-                id: "K4O5hP",
-                description:
-                  "Final paragraph in the employment equity profile section",
-              })}
-            </p>
           </div>
         )}
       </div>
