c28f6b7aae04c3f9e26b0bc8433b7bac241a64eb
Matt Nigh <matt@mattnigh.com>
2024-06-25 15:38:07 -0400

    Remove unreferenced message

--- apps/web/src/components/Table/tableMessages.ts
+++ apps/web/src/components/Table/tableMessages.ts
@@ -1,11 +1,6 @@
 import { defineMessages } from "react-intl";
 
 const messages = defineMessages({
-  noItems: {
-    defaultMessage: "There are no items to display.",
-    id: "cIqhQK",
-    description: "Message when there is no data to display in a table",
-  },
   noItemsTitle: {
     defaultMessage: "There aren't any items here.",
     id: "84XrK+",
