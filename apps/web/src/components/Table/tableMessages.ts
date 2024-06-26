820f4e8306e34071c5401abb8c13d47eb0271b52
Yoni K <yonikid15@gmail.com>
2024-06-26 13:17:34 -0400

    Add tableMessaeges back

---
+++ apps/web/src/components/Table/tableMessages.ts
@@ -0,0 +1,11 @@
+import { defineMessages } from "react-intl";
+
+const messages = defineMessages({
+  noItemsTitle: {
+    defaultMessage: "There aren't any items here.",
+    id: "84XrK+",
+    description: "Default message for an empty table",
+  },
+});
+
+export default messages;
