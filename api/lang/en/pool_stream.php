a51c5fdfa3ce02f7d2cafad7ee447d65efdff2e0
Eric Sizer <eric.sizer@gmail.com>
2024-06-26 17:19:10 -0400

    fix pool stream strings

--- api/lang/en/pool_stream.php
+++ api/lang/en/pool_stream.php
@@ -2,7 +2,7 @@
 
 return [
     'access_information_privacy' => 'Access to Information and Privacy',
-    'business_line_advisory_services' => 'Business Line Advisory Services',
+    'business_advisory_services' => 'Business Line Advisory Services',
     'database_management' => 'Database Management',
     'enterprise_architecture' => 'Enterprise Architecture',
     'infrastructure_operations' => 'Infrastructure Operations',
