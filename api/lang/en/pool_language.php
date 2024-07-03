474192498e210b853678b7bc3d7cb726f5f97a4b
Eric Sizer <eric.sizer@gmail.com>
2024-07-03 13:38:45 -0400

    move language codes to backend

--- api/lang/en/pool_language.php
+++ api/lang/en/pool_language.php
@@ -3,8 +3,8 @@
 use Illuminate\Support\Facades\Lang;
 
 return [
-    'bilingual_advanced' => 'Bilingual advanced',
-    'bilingual_intermediate' => 'Bilingual intermediate',
+    'bilingual_advanced' => 'Bilingual advanced (C B C)',
+    'bilingual_intermediate' => 'Bilingual intermediate (B B B)',
     'english' => Lang::get('common.english_only', [], 'en'),
     'french' => Lang::get('common.french_only', [], 'en'),
     'various' => 'Various (English or French)',
--- api/lang/fr/pool_language.php
+++ api/lang/fr/pool_language.php
@@ -3,8 +3,8 @@
 use Illuminate\Support\Facades\Lang;
 
 return [
-    'bilingual_advanced' => 'Bilingue avancé',
-    'bilingual_intermediate' => 'Bilingue intermédiaire',
+    'bilingual_advanced' => 'Bilingue avancé (C B C)',
+    'bilingual_intermediate' => 'Bilingue intermédiaire (B B B)',
     'english' => Lang::get('common.english_only', [], 'fr'),
     'french' => Lang::get('common.french_only', [], 'fr'),
     'various' => 'Divers (anglais ou français)',
