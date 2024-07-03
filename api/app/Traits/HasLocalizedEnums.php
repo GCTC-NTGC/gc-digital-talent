1140f746cca83ff20d35e53c4144cab1285e451a
Eric Sizer <eric.sizer@gmail.com>
2024-07-03 13:56:49 -0400

    log attempt to localize non-localizable enums

--- api/app/Traits/HasLocalizedEnums.php
+++ api/app/Traits/HasLocalizedEnums.php
@@ -2,11 +2,17 @@
 
 namespace App\Traits;
 
+use Illuminate\Support\Facades\Log;
+
 trait HasLocalizedEnums
 {
     protected function localizeEnum(?string $value, $enum)
     {
-        if (! $value || is_object($enum) || ! method_exists($enum, 'localizedString')) {
+        if (! $value || ! method_exists($enum, 'localizedString')) {
+            // It implements `HasLocalization`, the value is just null
+            if ($value) {
+                Log::warning("$enum is not localized.");
+            }
             return $value;
         }
 
