75256dc3df5c2b5dd95ae16fe44dba5c41f09b30
Eric Sizer <eric.sizer@gmail.com>
2024-07-04 15:16:30 -0400

    fix typo

--- api/lang/en/placement_type.php
+++ api/lang/en/placement_type.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'place_tentative' => 'Tentatively placed (Verbal or email confirmation)',
+    'placed_tentative' => 'Tentatively placed (Verbal or email confirmation)',
     'placed_casual' => 'Placed casual',
     'placed_term' => 'Placed term',
     'placed_indeterminate' => 'Placed indeterminate',
--- api/lang/fr/placement_type.php
+++ api/lang/fr/placement_type.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'place_tentative' => 'En placement provisoire (confirmation verbale ou par courriel)',
+    'placed_tentative' => 'En placement provisoire (confirmation verbale ou par courriel)',
     'placed_casual' => 'Placé occasionnels',
     'placed_term' => 'Période déterminée',
     'placed_indeterminate' => 'Période indéterminée',
