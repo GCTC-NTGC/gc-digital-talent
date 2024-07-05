0c6f0210c29628d82e59451d7df904da9046e20b
Eric Sizer <eric.sizer@gmail.com>
2024-07-05 13:44:16 -0400

    document fake enum data helpers

--- packages/fake-data/src/fakeLocalizedEnum.ts
+++ packages/fake-data/src/fakeLocalizedEnum.ts
@@ -8,17 +8,40 @@ faker.seed();
 
 const pascalSplitRegex = /(?=[A-Z])/;
 
+/**
+ * Convert an enum value to a more human-readable format
+ *
+ * @param value - The string to be transformed
+ * @param [delimiter="_"] - The parameter to split on
+ * @returns New string
+ */
 function enumToString(value: string, delimiter: string | RegExp = "_") {
   const string = value.split(delimiter).join(" ").toLowerCase();
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
+/**
+ * Converts a strin in PascalCase to SCREAMING_SNAKE_CASE
+ *
+ * @param value - The string to be transformed
+ * @returns New string
+ */
 function pascalToScreamingSnake<T extends string>(value: T): T {
   return value.split(pascalSplitRegex).join("_").toUpperCase() as T;
 }
 
 type EnumCase = "pascal" | "screaming_snake";
 
+/**
+ * Casts an enum case to a localized enum
+ *
+ * NOTE: This is not a real enum but a mocked one
+ *
+ * @param value - The value to be cast
+ * @param delimiter  - Character to split the string on
+ * @param [enumCase="screaming_snake"] - Case of the enum value
+ * @returns - Localized version of the enum
+ */
 function toLocalizedEnum<T extends string>(
   value: T,
   delimiter?: string | RegExp,
@@ -32,6 +55,12 @@ function toLocalizedEnum<T extends string>(
 
 type EnumType = { [k: number]: string };
 
+/**
+ * Converts an enum to a localized version
+ *
+ * @param enumerable - The enum to be cast
+ * @returns Array of the enum as localized enums
+ */
 export function fakeLocalizedEnum<T extends EnumType>(enumerable: T) {
   return Object.keys(enumerable).map((key) =>
     toLocalizedEnum(key, pascalSplitRegex, "pascal"),
