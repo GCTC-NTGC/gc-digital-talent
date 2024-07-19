30bfba8cbd82b8fbd238ceb1973cc61437beda6a
Yoni K <yonikid15@gmail.com>
2024-07-19 07:49:32 -0400

    Add form value types

---
+++ apps/web/src/components/AssessmentStepTracker/types.ts
@@ -0,0 +1,12 @@
+export type FormValues = {
+  equity: string[];
+  // expiryStatus: string;
+  govEmployee: string;
+  languageAbility: string;
+  operationalRequirement: string[];
+  pools: string[];
+  priorityWeight: string[];
+  skills: string[];
+  // suspendedStatus: string;
+  workRegion: string[];
+};
