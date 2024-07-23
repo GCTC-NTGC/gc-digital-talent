0d2a4d5b1cc3ecc86230b8cdf0312c253de18d34
Matt <mnigh@users.noreply.github.com>
2024-07-23 10:19:27 -0400

    [Fix] Updates colours for a11y contrast (#11021)
    * Update heavyWarning color
    
    * Update Required color

--- packages/forms/src/components/Field/Required.tsx
+++ packages/forms/src/components/Field/Required.tsx
@@ -4,7 +4,7 @@ export interface RequiredProps {
 
 const Required = ({ required }: RequiredProps) =>
   required ? (
-    <span data-h2-color="base(error) base:dark(error.lightest)"> *</span>
+    <span data-h2-color="base(error.dark) base:dark(error.lightest)"> *</span>
   ) : null;
 
 export default Required;
