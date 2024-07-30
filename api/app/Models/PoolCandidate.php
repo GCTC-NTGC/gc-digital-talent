2e2ec81719e9ebf54168c7640dd8fdd2e0702d8a
Eric Sizer <esizer@users.noreply.github.com>
2024-07-30 10:46:21 -0400

    remove candidate status attribute (#11081)

--- api/app/Models/PoolCandidate.php
+++ api/app/Models/PoolCandidate.php
@@ -584,36 +584,6 @@ class PoolCandidate extends Model
         return $query;
     }
 
-    /* accessor to obtain pool candidate status, additional logic exists to override database field sometimes*/
-    // pool_candidate_status database value passed into accessor as an argument
-    public function getPoolCandidateStatusAttribute($candidateStatus)
-    {
-        // pull info
-        // $submittedAt = $this->submitted_at;
-        // $expiryDate = $this->expiry_date;
-        // $currentTime = date('Y-m-d H:i:s');
-        // $isExpired = $currentTime > $expiryDate ? true : false;
-
-        // // ensure null submitted_at returns either draft or expired draft
-        // if ($submittedAt == null){
-        //     if($isExpired) {
-        //         return ApiEnums::CANDIDATE_STATUS_DRAFT_EXPIRED;
-        //     }
-        //     return ApiEnums::CANDIDATE_STATUS_DRAFT;
-        // }
-
-        // // ensure expired returned if past expiry date with exception for PLACED
-        // if ($candidateStatus != ApiEnums::CANDIDATE_STATUS_PLACED_CASUAL && $candidateStatus != ApiEnums::CANDIDATE_STATUS_PLACED_TERM && $candidateStatus != ApiEnums::CANDIDATE_STATUS_PLACED_INDETERMINATE) {
-        //     if ($isExpired) {
-        //         return ApiEnums::CANDIDATE_STATUS_EXPIRED;
-        //     }
-        //     return $candidateStatus;
-        // }
-
-        // no overriding
-        return $candidateStatus;
-    }
-
     public function scopePriorityWeight(Builder $query, ?array $priorityWeights): Builder
     {
         if (empty($priorityWeights)) {
