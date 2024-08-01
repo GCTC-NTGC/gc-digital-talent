0293bf08d05a8322ff8864bbff63a2d1d3d3b93f
Peter Giles <8978655+petertgiles@users.noreply.github.com>
2024-08-01 10:40:56 -0400

    tidy notification tests

--- api/tests/Unit/NotificationAuthorizationScopeTest.php
+++ api/tests/Unit/NotificationAuthorizationScopeTest.php
@@ -5,17 +5,12 @@ namespace Tests\Feature;
 use App\Models\Notification;
 use App\Models\User;
 use App\Notifications\Test;
-use Database\Seeders\ClassificationSeeder;
-use Database\Seeders\GenericJobTitleSeeder;
 use Database\Seeders\RolePermissionSeeder;
-use Database\Seeders\SkillFamilySeeder;
-use Database\Seeders\SkillSeeder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Auth;
 use Tests\TestCase;
 
-use function PHPUnit\Framework\assertCount;
-use function PHPUnit\Framework\assertEquals;
+use function PHPUnit\Framework\assertEqualsCanonicalizing;
 
 class NotificationAuthorizationScopeTest extends TestCase
 {
@@ -29,10 +24,6 @@ class NotificationAuthorizationScopeTest extends TestCase
     {
         parent::setUp();
 
-        $this->seed(ClassificationSeeder::class);
-        $this->seed(GenericJobTitleSeeder::class);
-        $this->seed(SkillFamilySeeder::class);
-        $this->seed(SkillSeeder::class);
         $this->seed(RolePermissionSeeder::class);
 
         $this->user1 = User::factory()
@@ -62,7 +53,7 @@ class NotificationAuthorizationScopeTest extends TestCase
             ->andReturn(null);
 
         $notifications = Notification::authorizedToView()->get();
-        assertCount(0, $notifications);
+        assertEqualsCanonicalizing([], $notifications->toArray());
     }
 
     // an applicant should be able to view just their own notifications
@@ -71,8 +62,7 @@ class NotificationAuthorizationScopeTest extends TestCase
         Auth::shouldReceive('user')
             ->andReturn($this->user1);
 
-        $notifications = Notification::authorizedToView()->get();
-        assertCount(1, $notifications);
-        assertEquals('Notification1', $notifications[0]->name);
+        $notifications = Notification::authorizedToView()->get()->pluck('name');
+        assertEqualsCanonicalizing(['Notification1'], $notifications->toArray());
     }
 }
