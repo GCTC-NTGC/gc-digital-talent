3fbd5505529ec88f25c540e2597c018d05e0f04d
Eric Sizer <eric.sizer@gmail.com>
2024-08-02 17:03:29 -0400

    use async mutation on users table

---
+++ apps/web/src/hooks/useUserDownloads.ts
@@ -0,0 +1,60 @@
+import { useIntl } from "react-intl";
+import { useMutation } from "urql";
+
+import { graphql, Language, Scalars } from "@gc-digital-talent/graphql";
+import { toast } from "@gc-digital-talent/toast";
+import {
+  commonMessages,
+  errorMessages,
+  getLocale,
+} from "@gc-digital-talent/i18n";
+
+const DownloadUsersDoc_Mutation = graphql(/* GraphQL */ `
+  mutation DownloadUsersDoc(
+    $ids: [UUID!]!
+    $anonymous: Boolean!
+    $locale: Language
+  ) {
+    downloadUsersDoc(ids: $ids, anonymous: $anonymous, locale: $locale)
+  }
+`);
+
+const useUserDownloads = () => {
+  const intl = useIntl();
+  const locale = getLocale(intl);
+  const [{ fetching: downloadingDoc }, executeDocMutation] = useMutation(
+    DownloadUsersDoc_Mutation,
+  );
+
+  const handleDownloadError = () => {
+    toast.error(intl.formatMessage(errorMessages.downloadRequestFailed));
+  };
+
+  const handleDownloadRes = (hasData: boolean) => {
+    if (hasData) {
+      toast.info(intl.formatMessage(commonMessages.preparingDownload));
+    } else {
+      handleDownloadError();
+    }
+  };
+
+  const downloadDoc = ({
+    ids,
+    anonymous,
+  }: {
+    ids: Scalars["UUID"]["input"][];
+    anonymous: boolean;
+  }) => {
+    executeDocMutation({
+      ids,
+      anonymous,
+      locale: locale === "fr" ? Language.Fr : Language.En,
+    })
+      .then((res) => handleDownloadRes(!!res.data))
+      .catch(handleDownloadError);
+  };
+
+  return { downloadDoc, downloadingDoc };
+};
+
+export default useUserDownloads;
