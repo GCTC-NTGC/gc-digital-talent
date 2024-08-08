075d9919effa3b307bcffa56b30757bba2c8fa3a
Matt Nigh <matt@mattnigh.com>
2024-08-08 12:02:15 -0400

    Replace classes with data-h2 attributes

--- packages/forms/src/components/CheckButton/CheckButton.tsx
+++ packages/forms/src/components/CheckButton/CheckButton.tsx
@@ -5,8 +5,6 @@ import MinusIcon from "@heroicons/react/24/outline/MinusIcon";
 import { formMessages } from "@gc-digital-talent/i18n";
 import { IconType } from "@gc-digital-talent/ui";
 
-import "./check-button.css";
-
 const borderMap = {
   black: {
     "data-h2-border": "base(1px solid black)",
@@ -57,12 +55,13 @@ const CheckButton = ({
 
   return (
     <button
-      className="check-button"
       type="button"
       onClick={handleClick}
       data-h2-padding="base(x.25)"
       data-h2-radius="base(s)"
       data-h2-display="base(inline-flex)"
+      data-h2-border="base(none)"
+      data-h2-cursor="base(pointer)"
       {...colorMap[color]}
     >
       <span data-h2-visually-hidden="base(invisible)">
@@ -71,16 +70,16 @@ const CheckButton = ({
           : intl.formatMessage(formMessages.selectCheck, { label })}
       </span>
       <span
-        className="check-button__inner"
         data-h2-padding="base(x.125)"
         data-h2-radius="base(input)"
         data-h2-background-color="base(foreground)"
         {...borderMap[color]}
       >
         <Icon
-          className="check-button__icon"
           data-h2-display="base(block)"
           data-h2-stroke-width="base(3)"
+          data-h2-height="base(x.5)"
+          data-h2-width="base(x.5)"
         />
       </span>
     </button>
