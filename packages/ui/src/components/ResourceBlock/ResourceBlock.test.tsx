6acd36c6e57eea8281c9472a4431709706d4d793
Peter Giles <8978655+petertgiles@users.noreply.github.com>
2024-08-13 10:00:17 -0400

    fix test

--- packages/ui/src/components/ResourceBlock/ResourceBlock.test.tsx
+++ packages/ui/src/components/ResourceBlock/ResourceBlock.test.tsx
@@ -7,17 +7,13 @@ import { faker } from "@faker-js/faker/locale/en";
 import { renderWithProviders, axeTest } from "@gc-digital-talent/jest-helpers";
 
 import ResourceBlock from "./ResourceBlock";
-import Link from "../Link";
 
 function renderComponent() {
   return renderWithProviders(
     <ResourceBlock.Root title={faker.lorem.words()} headingColor="primary">
       <ResourceBlock.Item
-        link={
-          <Link href={faker.internet.url()}>
-            {faker.commerce.productName()}
-          </Link>
-        }
+        title={faker.commerce.productName()}
+        href={faker.internet.url()}
         description={faker.lorem.paragraph()}
       />
     </ResourceBlock.Root>,
