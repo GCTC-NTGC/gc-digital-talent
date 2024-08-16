9e3fa694020a1e30d267a2c9e53772b07bd20241
Yoni K <yonikid15@gmail.com>
2024-08-16 08:57:07 -0400

    Create mutation

---
+++ api/database/migrations/2024_08_16_103756_add_user_id_to_pool_candidate_search_requests.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('pool_candidate_search_requests', function (Blueprint $table) {
+            $table->uuid('user_id')->nullable();
+            $table->foreign('user_id')->references('id')->on('users')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('pool_candidate_search_requests', function (Blueprint $table) {
+            $table->dropColumn('user_id');
+        });
+    }
+};
