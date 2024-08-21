2562b12f6ae335972f4776be5aed95b8df59bf11
Peter Giles <8978655+petertgiles@users.noreply.github.com>
2024-08-21 12:02:28 -0400

    Update apps/web/src/components/NavContext/NavContextContainer.tsx
    Co-authored-by: Matt <mnigh@users.noreply.github.com>

--- apps/web/src/components/NavContext/NavContextContainer.tsx
+++ apps/web/src/components/NavContext/NavContextContainer.tsx
@@ -13,7 +13,7 @@ const NAV_ROLES_BY_PRIVILEGE = [
   "admin",
 ] as const;
 
-export type NavRole = (typeof NAV_ROLES_BY_PRIVILEGE)[number];
+type NavRole = (typeof NAV_ROLES_BY_PRIVILEGE)[number];
 
 export interface NavContextState {
   navRole: NavRole;
