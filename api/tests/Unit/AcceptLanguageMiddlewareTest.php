2165249716adb2c67cb14959a39127bfd3fd2d60
Eric Sizer <eric.sizer@gmail.com>
2024-08-22 14:49:51 -0400

    improve parsing of header for locale

--- api/tests/Unit/AcceptLanguageMiddlewareTest.php
+++ api/tests/Unit/AcceptLanguageMiddlewareTest.php
@@ -17,7 +17,7 @@ class AcceptLanguageMiddlewareTest extends TestCase
         $request = Request::create(route('graphql'));
         if ($header) {
             $request->headers->set('Accept-Language', $header);
-        } else {
+        } elseif ($header == 'unset') {
             $request->headers->remove('Accept-Language');
         }
 
@@ -34,7 +34,17 @@ class AcceptLanguageMiddlewareTest extends TestCase
     public static function provider(): array
     {
         return [
-            'fallback to English when not set' => [false, 'en'],
+            // NOTE: Browsers send by default so we should only change when we get a value we can handle
+            'does not modify default value' => [false, 'en'],
+            'does not modify unexpected value' => ['mt-MT;q=0.9,ta-IN;q=0.8,eu-ES;q=0.7', 'en'],
+            'does not modify invalid value' => ['invalid-language-value', 'en'],
+            // If the header does not exist, we should fallback to English
+            'fallback to English when not set' => ['unset', 'en'],
+            // Can get an expected locale out of a list of unexpected values
+            'sets English from list' => ['mt-MT;q=0.9,en-US;q=0.8,eu-ES;q=0.7', 'en'],
+            'sets French from list' => ['mt-MT;q=0.9,fr-LU;q=0.8,eu-ES;q=0.7', 'fr'],
+            'weights by quality' => ['en;q=0.8,eu-ES;q=0.7,fr-LU;q=0.9;', 'fr'],
+            // If a valid value is sent, make sure locale is updated
             'sets English' => ['en', 'en'],
             'sets French' => ['fr', 'fr'],
         ];
