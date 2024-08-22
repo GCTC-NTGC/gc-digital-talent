1c59d82353e22d4428c94734024da6335f9796e8
Eric Sizer <eric.sizer@gmail.com>
2024-08-22 15:10:00 -0400

    use built in laravel method

--- api/app/Http/Middleware/AcceptLanguageMiddleware.php
+++ api/app/Http/Middleware/AcceptLanguageMiddleware.php
@@ -24,53 +24,11 @@ class AcceptLanguageMiddleware
      */
     public function handle(Request $request, Closure $next)
     {
-        $headerLocale = $request->header('Accept-Language', 'en');
-        $locale = $this->getLocale($headerLocale);
+        $locale = $request->getPreferredLanguage($this->availableLocales);
         if ($locale == 'en' || $locale == 'fr') {
             App::setLocale($locale);
         }
 
         return $next($request);
-    }
-
-    private function getLocale(?string $header)
-    {
-        if (! $header) {
-            return $this->availableLocales[0];
-        }
-
-        /**
-         *  Parse header string into a $locale => $quality array
-         *
-         *  If a quality is not present, it defaults to highest priority
-         *  We denote this by a decreasing value of 100, giving higher
-         *  priority to locales that appear earlier in the string
-         */
-        $defaultQuality = 100;
-        $localePairs = explode(',', $header);
-        $weightedLocales = [];
-        foreach ($localePairs as $pair) {
-            $pairArr = explode(';', $pair);
-            $quality = isset($pairArr[1]) ? str_replace('q=', '', $pairArr[1]) : null;
-            $quality = $quality ? floatval($quality) : $defaultQuality--;
-
-            $weightedLocales[$pairArr[0]] = $quality;
-        }
-
-        // Sort by quality in descending order
-        arsort($weightedLocales);
-
-        // Attempt to find a letter code that matches our
-        // supported locales in order of priority
-        $preferredLocale = $this->availableLocales[0];
-        foreach (array_keys($weightedLocales) as $locale) {
-            $twoLetter = substr($locale, 0, 2);
-            if (in_array($twoLetter, $this->availableLocales)) {
-                $preferredLocale = $twoLetter;
-                break;
-            }
-        }
-
-        return $preferredLocale;
     }
 }
