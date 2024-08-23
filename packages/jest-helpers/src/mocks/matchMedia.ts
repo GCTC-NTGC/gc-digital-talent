bb6748429aec2f51bb064ae27fef4e8b410ea9ee
Eric Sizer <eric.sizer@gmail.com>
2024-08-23 14:19:47 -0400

    fix jest-helpers errors

--- packages/jest-helpers/src/mocks/matchMedia.ts
+++ packages/jest-helpers/src/mocks/matchMedia.ts
@@ -2,7 +2,7 @@ Object.defineProperty(window, "matchMedia", {
   writable: true,
   value: jest.fn().mockImplementation((query) => ({
     matches: false,
-    media: query,
+    media: query as string,
     onchange: null,
     addListener: jest.fn(), // deprecated
     removeListener: jest.fn(), // deprecated
