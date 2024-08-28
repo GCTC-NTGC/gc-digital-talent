854e18cee811e213ede21a849394734fc3af77dd
Vachan <40485260+vd1992@users.noreply.github.com>
2024-08-28 08:52:50 -0600

    [Bug/copy] Have "Other reason" require decision notes (#11395)
    * copy update
    
    * conditional requirement

--- apps/web/src/components/ScreeningDecisions/useLabels.ts
+++ apps/web/src/components/ScreeningDecisions/useLabels.ts
@@ -28,11 +28,6 @@ const getLabels = (intl: IntlShape) => {
       description:
         "Label for demonstrated notes text area in the screening decision dialog.",
     }),
-    failedOther: intl.formatMessage({
-      defaultMessage: "Other reason for screening out.",
-      id: "C5uhBP",
-      description: "Label for radio option in the screening decision dialog.",
-    }),
   } as const;
 };
 
