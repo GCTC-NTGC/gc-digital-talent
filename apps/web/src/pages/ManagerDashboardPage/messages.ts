ec5608223c84ab431ae770d10df0c58026c7c9f9
Peter Giles <8978655+petertgiles@users.noreply.github.com>
2024-09-04 09:26:28 -0400

    update message descriptions

--- apps/web/src/pages/ManagerDashboardPage/messages.ts
+++ apps/web/src/pages/ManagerDashboardPage/messages.ts
@@ -3,14 +3,14 @@ import { defineMessages } from "react-intl";
 const messages = defineMessages({
   pageTitle: {
     defaultMessage: "Manager dashboard",
-    id: "o1jXs5",
-    description: "title for the 'Manager dashboard' page",
+    id: "Q914Cl",
+    description: "Title for the 'Manager dashboard' page",
   },
   subTitle: {
     defaultMessage:
       "Manage your talent searches and curate your manager profile.",
-    id: "lYp2e8",
-    description: "subtitle for the 'Manager dashboard' page",
+    id: "/18wM8",
+    description: "Subtitle for the 'Manager dashboard' page",
   },
 });
 
