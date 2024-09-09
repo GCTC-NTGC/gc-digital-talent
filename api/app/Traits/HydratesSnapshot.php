eb4df46959cb3b9b1773daf0c1b11df7f7b7ff01
Peter Giles <8978655+petertgiles@users.noreply.github.com>
2024-09-09 09:22:45 -0400

    remove enum flags

--- api/app/Traits/HydratesSnapshot.php
+++ api/app/Traits/HydratesSnapshot.php
@@ -31,8 +31,8 @@ trait HydratesSnapshot
     public static function hydrateFields(mixed $snapshot, array $fields, Model $model): Model
     {
         foreach ($fields as $attribute => $snapshotField) {
-            $isLocalizedEnum = Arr::has($snapshot, [$attribute.'.value', $attribute.'.label.en', $attribute.'.label.fr']);
-            if ($hydratedField = self::hydrateField($snapshot, $snapshotField[0], $isLocalizedEnum)) {
+            $isLocalizedEnum = Arr::has($snapshot, [$snapshotField.'.value', $snapshotField.'.label.en', $snapshotField.'.label.fr']);
+            if ($hydratedField = self::hydrateField($snapshot, $snapshotField, $isLocalizedEnum)) {
                 $model->$attribute = $hydratedField;
             }
         }
