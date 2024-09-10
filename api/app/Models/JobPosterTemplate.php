3ac49ad35e2a8f96cbc9e21caa132b92592e85e9
Eric Sizer <esizer@users.noreply.github.com>
2024-09-10 14:22:33 -0400

    [Feature] Add job poster template model (#11468)
    * add migration for job poster templates
    
    * add model, factory, seeder, policy
    
    * add queries, mutations to schema
    
    * make keywords non-nullable
    
    * remove debug code
    
    * update schema
    
    * rewrite schema again
    
    * rewrite ide helper
    
    * add additional permissions
    
    * make keywords array of strings in schema
    
    * make all fields non-nullable
    
    * fix permission check, enforce reference unique
    
    * add basic permission tests
    
    * add update tests
    
    * allow ssh into lighthouse workflow
    
    * revert ssh
    
    * update permissions on schema files
    
    * test skipping template tests
    
    * test adding deprecated method
    
    * attempt skipping user test
    
    * remove skip
    
    * make fields nullable in schema
    
    * testing something
    
    * fix typos
    
    * add delete mutation
    
    * split
    
    * re-arrange to test
    
    * run with verbose flag, stop
    
    * use phpunit directly
    
    * remove paraell flag
    
    * run with profile flag
    
    * no coverage run
    
    * Revert "no coverage run"
    
    This reverts commit 4bd988bbc5c3b3d3bd90101040f291c65e1fdf15.
    
    * Revert "re-arrange to test"
    
    This reverts commit cadcd8a81a7015c1bc9c9ffdc7ff01e05a4fa1f0.
    
    * Revert "split"
    
    This reverts commit d012a54124f0809587b0fdd1d4128c38e3e2c76b.
    
    * Revert "testing something"
    
    This reverts commit 4af5809c3d9b402e42c8a08bd5ffcb8b602d75b2.
    
    * update code cov config
    
    * remove profile flag
    
    * revert code cov config
    
    * run tests in parallel
    
    * enforce timelimit, reduce processes
    
    * disable code cov
    
    * attempt pcov coverage
    
    * add delete cascade on pivot table
    
    ---------
    
    Co-authored-by: vd1992 <40485260+vd1992@users.noreply.github.com>

---
+++ api/app/Models/JobPosterTemplate.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+/**
+ * Class JobPosterTemplate
+ *
+ * @property string $id
+ * @property array $name
+ * @property string $reference_id
+ * @property array $description
+ * @property string $supervisory_status
+ * @property string $stream
+ * @property array $work_description
+ * @property array $tasks
+ * @property array $keywords
+ * @property array $essential_technical_skills_notes
+ * @property array $essential_behavioural_skills_notes
+ * @property array $nonessential_technical_skills_notes
+ * @property Illuminate\Support\Carbon $created_at
+ * @property Illuminate\Support\Carbon $updated_at
+ */
+class JobPosterTemplate extends Model
+{
+    use HasFactory;
+
+    protected $keyType = 'string';
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'name' => 'array',
+        'description' => 'array',
+        'work_description' => 'array',
+        'tasks' => 'array',
+        'keywords' => 'array',
+        'essential_technical_skills_notes' => 'array',
+        'essential_behavioural_skills_notes' => 'array',
+        'nonessential_technical_skills_notes' => 'array',
+    ];
+
+    /**
+     * The attributes that can be filled using mass-assignment.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'description',
+        'key',
+        'stream',
+        'supervisory_status',
+        'work_description',
+        'tasks',
+        'keywords',
+        'essential_technical_skills_notes',
+        'essential_technical_skills_notes',
+        'nonessential_technical_skills_notes',
+    ];
+
+    /**
+     * Associated classification
+     */
+    public function classification(): BelongsTo
+    {
+        return $this->belongsTo(Classification::class);
+    }
+
+    /**
+     * Associated skills
+     */
+    public function skills(): BelongsToMany
+    {
+        return $this->belongsToMany(Skill::class)
+            ->withPivot('type', 'required_skill_level');
+    }
+}
---
+++ api/database/migrations/2024_09_05_162108_job_poster_templates_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Query\Expression;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('job_poster_templates', function (Blueprint $table) {
+            $table->uuid('id')->primary()->default(new Expression('public.gen_random_uuid()'));
+            $table->timestamps();
+            $table->string('reference_id');
+            $table->string('supervisory_status');
+            $table->string('stream');
+            $table->jsonb('name')->default(json_encode(['en' => '', 'fr' => '']));
+            $table->jsonb('description')->default(json_encode(['en' => '', 'fr' => '']));
+            $table->jsonb('work_description')->default(json_encode(['en' => '', 'fr' => '']));
+            $table->jsonb('tasks')->default(json_encode(['en' => '', 'fr' => '']));
+            $table->jsonb('keywords')->default(json_encode(['en' => [], 'fr' => []]));
+            $table->jsonb('essential_technical_skills_notes')->default(json_encode(['en' => '', 'fr' => '']));
+            $table->jsonb('essential_behavioural_skills_notes')->default(json_encode(['en' => '', 'fr' => '']));
+            $table->jsonb('nonessential_technical_skills_notes')->default(json_encode(['en' => '', 'fr' => '']));
+            $table->foreignUuid('classification_id')
+                ->constrained();
+        });
+
+        Schema::create('job_poster_template_skill', function (Blueprint $table) {
+            $table->uuid('id')->primary()->default(new Expression('public.gen_random_uuid()'));
+            $table->foreignUuid('job_poster_template_id')->constrained()->onDelete('cascade');
+            $table->foreignUuid('skill_id')->constrained()->onDelete('cascade');
+            $table->string('type');
+            $table->string('required_skill_level');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('job_poster_template_skill');
+        Schema::dropIfExists('job_poster_templates');
+    }
+};
