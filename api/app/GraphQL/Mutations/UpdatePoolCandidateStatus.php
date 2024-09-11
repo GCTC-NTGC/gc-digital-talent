a2c7954192b182fa833a036afac757ccd300dbed
Eric Sizer <eric.sizer@gmail.com>
2024-09-11 15:02:36 -0400

    add mutation resolver for candidate status timestamps

---
+++ api/app/GraphQL/Mutations/UpdatePoolCandidateStatus.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\GraphQL\Mutations;
+
+use App\Enums\PoolCandidateStatus;
+use App\Models\PoolCandidate;
+use Illuminate\Support\Facades\Log;
+
+final readonly class UpdatePoolCandidateStatus
+{
+    /** @param  array{}  $args */
+    public function __invoke(null $_, array $args)
+    {
+        $candidate = PoolCandidate::findOrFail($args['id']);
+
+        Log::debug($args);
+
+        if (isset($args['pool_candidate_status']) && $args['pool_candidate_status'] !== $candidate->pool_candidate_status) {
+            $status = $args['pool_candidate_status'];
+
+            $timestamp = match ($status) {
+                PoolCandidateStatus::SCREENED_OUT_ASSESSMENT->name,
+                PoolCandidateStatus::SCREENED_OUT_APPLICATION->name,
+                PoolCandidateStatus::QUALIFIED_AVAILABLE->name => 'final_decision_at',
+
+                PoolCandidateStatus::SCREENED_OUT_NOT_RESPONSIVE->name,
+                PoolCandidateStatus::QUALIFIED_UNAVAILABLE->name,
+                PoolCandidateStatus::QUALIFIED_WITHDREW->name,
+                PoolCandidateStatus::REMOVED->name => 'removed_at',
+
+                PoolCandidateStatus::PLACED_TENTATIVE->name,
+                PoolCandidateStatus::PLACED_CASUAL->name,
+                PoolCandidateStatus::PLACED_TERM->name,
+                PoolCandidateStatus::PLACED_INDETERMINATE->name => 'placed_at',
+
+                default => null// no-op
+            };
+
+            if ($timestamp) {
+                $candidate->$timestamp = now();
+            }
+
+            $candidate->pool_candidate_status = $status;
+        }
+
+        if (isset($args['expiry_date'])) {
+            $candidate->expiry_date = $args['expiry_date'];
+        }
+
+        $candidate->save();
+
+        return $candidate;
+    }
+}
