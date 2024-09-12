1a4149e0c94beef7eb94e4f0bb8e0c9453db0bfd
Peter Giles <8978655+petertgiles@users.noreply.github.com>
2024-09-12 13:03:10 -0400

    database loading

--- apps/web/src/pages/JobPosterTemplatePage/JobPosterTemplatePage.tsx
+++ apps/web/src/pages/JobPosterTemplatePage/JobPosterTemplatePage.tsx
@@ -1,28 +1,76 @@
-import { Scalars } from "@gc-digital-talent/graphql";
-import { Pending } from "@gc-digital-talent/ui";
+import { OperationContext, useQuery } from "urql";
+import { useIntl } from "react-intl";
+
+import {
+  FragmentType,
+  getFragment,
+  graphql,
+  Scalars,
+} from "@gc-digital-talent/graphql";
+import { Pending, ThrowNotFound } from "@gc-digital-talent/ui";
+import { commonMessages } from "@gc-digital-talent/i18n";
 
 import useRequiredParams from "~/hooks/useRequiredParams";
 
+const JobPosterTemplateTopLevel_Fragment = graphql(/* GraphQL */ `
+  fragment JobPosterTemplateTopLevel on JobPosterTemplate {
+    name {
+      en
+      fr
+    }
+  }
+`);
 interface JobPosterTemplateProps {
-  templateId: string;
+  jobPosterTemplateQuery: FragmentType<
+    typeof JobPosterTemplateTopLevel_Fragment
+  >;
 }
 
-const JobPosterTemplate = ({ templateId }: JobPosterTemplateProps) => {
-  return <span>{`Hello World! ${templateId}`}</span>;
+const JobPosterTemplate = ({
+  jobPosterTemplateQuery,
+}: JobPosterTemplateProps) => {
+  const jobPosterTemplate = getFragment(
+    JobPosterTemplateTopLevel_Fragment,
+    jobPosterTemplateQuery,
+  );
+  return <span>{`Hello World! ${jobPosterTemplate.name?.en}`}</span>;
 };
+
+const JobPosterTemplatePage_Query = graphql(/* GraphQL */ `
+  query JobPosterTemplatePage($templateId: UUID!) {
+    jobPosterTemplate(id: $templateId) {
+      id
+      ...JobPosterTemplateTopLevel
+    }
+  }
+`);
 
 // eslint-disable-next-line @typescript-eslint/consistent-type-definitions
 type RouteParams = {
   templateId: Scalars["ID"]["output"];
 };
 
+const context: Partial<OperationContext> = {
+  requestPolicy: "cache-first",
+};
+
 const JobPosterTemplatePage = () => {
+  const intl = useIntl();
   const { templateId } = useRequiredParams<RouteParams>("templateId");
-  const fetching = false;
-  const error = undefined;
+
+  const [{ data, fetching, error }] = useQuery({
+    query: JobPosterTemplatePage_Query,
+    context,
+    variables: { templateId },
+  });
+
   return (
     <Pending fetching={fetching} error={error}>
-      <JobPosterTemplate templateId={templateId} />
+      {data?.jobPosterTemplate ? (
+        <JobPosterTemplate jobPosterTemplateQuery={data.jobPosterTemplate} />
+      ) : (
+        <ThrowNotFound message={intl.formatMessage(commonMessages.notFound)} />
+      )}
     </Pending>
   );
 };
