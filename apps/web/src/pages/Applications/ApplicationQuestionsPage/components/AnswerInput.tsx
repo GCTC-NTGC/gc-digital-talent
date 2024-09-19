679ae38d33a327cc291abd2cee13b3ebbd9d9cda
Peter Giles <8978655+petertgiles@users.noreply.github.com>
2024-09-19 16:11:14 -0400

    [Feat] Change word counts for FR in application (#11577)
    * Change word counts for FR in application
    
    * fix var typo

--- apps/web/src/pages/Applications/ApplicationQuestionsPage/components/AnswerInput.tsx
+++ apps/web/src/pages/Applications/ApplicationQuestionsPage/components/AnswerInput.tsx
@@ -1,11 +1,18 @@
 import { useIntl } from "react-intl";
 
 import { TextArea } from "@gc-digital-talent/forms";
-import { errorMessages, getLocalizedName } from "@gc-digital-talent/i18n";
+import {
+  errorMessages,
+  getLocale,
+  getLocalizedName,
+  Locales,
+} from "@gc-digital-talent/i18n";
 import { GeneralQuestion, ScreeningQuestion } from "@gc-digital-talent/graphql";
 
+import { FRENCH_WORDS_PER_ENGLISH_WORD } from "~/constants/talentSearchConstants";
+
 const TEXT_AREA_ROWS = 3;
-const TEXT_AREA_MAX_WORDS = 200;
+const TEXT_AREA_MAX_WORDS_EN = 200;
 
 interface AnswerInputProps {
   index: number;
@@ -14,9 +21,15 @@ interface AnswerInputProps {
 
 const AnswerInput = ({ index, question }: AnswerInputProps) => {
   const intl = useIntl();
+  const locale = getLocale(intl);
   const isScreening = question.__typename === "ScreeningQuestion";
   const answerPrefix = isScreening ? "screeningAnswers" : "generalAnswers";
   const questionId = `${answerPrefix}.${index}.question`;
+
+  const wordCountLimits: Record<Locales, number> = {
+    en: TEXT_AREA_MAX_WORDS_EN,
+    fr: Math.round(TEXT_AREA_MAX_WORDS_EN * FRENCH_WORDS_PER_ENGLISH_WORD),
+  } as const;
 
   return (
     <>
@@ -32,7 +45,7 @@ const AnswerInput = ({ index, question }: AnswerInputProps) => {
         name={`${answerPrefix}.${index}.answer`}
         aria-labelledby={questionId}
         rows={TEXT_AREA_ROWS}
-        wordLimit={TEXT_AREA_MAX_WORDS}
+        wordLimit={wordCountLimits[locale]}
         label={intl.formatMessage(
           {
             defaultMessage: "Answer to question {number}",
