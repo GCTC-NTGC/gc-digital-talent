2935f8a15e1d24dd6d90805ca98c4b9836f7c87e
Eric Sizer <eric.sizer@gmail.com>
2024-09-20 09:57:14 -0400

    add zip generator

---
+++ api/app/Generators/ZipGenerator.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Generators;
+
+use Illuminate\Support\Facades\Log;
+use ZipArchive;
+
+abstract class ZipGenerator extends FileGenerator implements FileGeneratorInterface
+{
+    protected array $files;
+
+    public function __construct(public string $fileName, protected ?string $dir)
+    {
+        parent::__construct($fileName, $dir);
+
+        $this->files = [];
+    }
+
+    public function addFiles()
+    {
+
+        try {
+            $zip = new ZipArchive();
+            $zipPath = $this->getPath();
+            if ($zip->open($zipPath, ZipArchive::CREATE)) {
+                foreach ($this->files as $path => $name) {
+                    $zip->addFile($path, $name);
+                }
+                $zip->close();
+            }
+        } catch (\Throwable $e) {
+            Log::error($e);
+
+            return false;
+        }
+    }
+
+    public function write()
+    {
+        try {
+            $this->addFiles();
+        } catch (\Throwable $e) {
+            // Log message and bubble it up
+            Log::error('Error saving csv: '.$this->fileName.' '.$e->getMessage());
+            throw $e;
+        }
+    }
+}
