29e4768cdd3c2e84cb6c84e29b325d58ca9f2d9d
vd1992 <40485260+vd1992@users.noreply.github.com>
2024-09-20 11:31:32 -0600

    create community dialogs

---
+++ apps/web/src/pages/Users/UpdateUserPage/components/AddCommunityRoleDialog.tsx
@@ -0,0 +1,251 @@
+import { useState, useEffect } from "react";
+import { FormProvider, useForm } from "react-hook-form";
+import { useIntl } from "react-intl";
+import PlusIcon from "@heroicons/react/24/outline/PlusIcon";
+import { useQuery } from "urql";
+
+import { Dialog, Button } from "@gc-digital-talent/ui";
+import { Combobox, Select } from "@gc-digital-talent/forms";
+import { notEmpty } from "@gc-digital-talent/helpers";
+import { toast } from "@gc-digital-talent/toast";
+import {
+  commonMessages,
+  errorMessages,
+  formMessages,
+  getLocalizedName,
+} from "@gc-digital-talent/i18n";
+import {
+  UpdateUserRolesInput,
+  UpdateUserRolesMutation,
+  Role,
+  User,
+  graphql,
+  RoleInput,
+} from "@gc-digital-talent/graphql";
+
+import { getFullNameHtml } from "~/utils/nameUtils";
+import adminMessages from "~/messages/adminMessages";
+
+import { UpdateUserDataAuthInfoType } from "../UpdateUserPage";
+import { isCommunityTeamable } from "./helpers";
+
+const AddCommunityRoleCommunities_Query = graphql(/* GraphQL */ `
+  query AddCommunityRoleCommunities {
+    communities {
+      id
+      name {
+        en
+        fr
+      }
+      teamIdForRoleAssignment
+    }
+  }
+`);
+
+interface FormValues {
+  roles: string[];
+  community: string | null;
+}
+
+interface AddCommunityRoleDialogProps {
+  user: Pick<User, "id" | "firstName" | "lastName">;
+  authInfo: UpdateUserDataAuthInfoType;
+  availableRoles: Role[];
+  onAddRoles: (
+    submitData: UpdateUserRolesInput,
+  ) => Promise<UpdateUserRolesMutation["updateUserRoles"]>;
+}
+
+const AddCommunityRoleDialog = ({
+  user,
+  authInfo,
+  availableRoles,
+  onAddRoles,
+}: AddCommunityRoleDialogProps) => {
+  const intl = useIntl();
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const { id, firstName, lastName } = user;
+  const userName = getFullNameHtml(firstName, lastName, intl);
+
+  const methods = useForm<FormValues>({
+    defaultValues: {
+      roles: [],
+      community: null,
+    },
+  });
+
+  const {
+    handleSubmit,
+    watch,
+    setValue,
+    formState: { isSubmitting },
+  } = methods;
+
+  const handleAddRoles = async (formValues: FormValues) => {
+    let { roles } = formValues;
+    // despite the typing, roles is just a string may be complicated by there being only one team based role available
+    if (!Array.isArray(roles)) {
+      roles = [roles];
+    }
+
+    const roleInputArray: RoleInput[] = roles.map((role) => {
+      return { roleId: role, teamId: formValues.community };
+    });
+
+    return onAddRoles({
+      userId: id,
+      roleAssignmentsInput: {
+        attach: roleInputArray,
+      },
+    }).then(() => {
+      setIsOpen(false);
+      toast.success(
+        intl.formatMessage({
+          defaultMessage: "Role(s) added successfully",
+          id: "/17wgm",
+          description:
+            "Message displayed to user when one or more roles have been added to a user",
+        }),
+      );
+    });
+  };
+
+  const label = intl.formatMessage({
+    defaultMessage: "Add community roles",
+    id: "KpbTtg",
+    description: "Label for the form to add a community membership to a user",
+  });
+
+  const roleOptions = availableRoles
+    .filter((role) => role.isTeamBased)
+    .map((role) => ({
+      label: getLocalizedName(role.displayName, intl),
+      value: role.id,
+    }));
+
+  const communityTeamIdForRoleAssignment = watch("community");
+  useEffect(() => {
+    const roleAssignments = authInfo?.roleAssignments || [];
+    const activeRoleIds = roleAssignments
+      .filter((ra) => isCommunityTeamable(ra?.teamable))
+      .filter(
+        (ra) =>
+          ra?.community?.teamIdForRoleAssignment ===
+          communityTeamIdForRoleAssignment,
+      )
+      .map((r) => r?.role?.id)
+      .filter(notEmpty);
+    setValue("roles", activeRoleIds);
+  }, [authInfo?.roleAssignments, communityTeamIdForRoleAssignment, setValue]);
+
+  const [{ data: communityData }] = useQuery({
+    query: AddCommunityRoleCommunities_Query,
+  });
+
+  const communityOptions = communityData?.communities
+    .filter(notEmpty)
+    .map((community) => ({
+      label: getLocalizedName(community.name, intl),
+      value: community.teamIdForRoleAssignment,
+    }));
+
+  return (
+    <Dialog.Root open={isOpen} onOpenChange={setIsOpen}>
+      <Dialog.Trigger>
+        <Button color="primary" mode="solid" icon={PlusIcon}>
+          {label}
+        </Button>
+      </Dialog.Trigger>
+      <Dialog.Content>
+        <Dialog.Header>{label}</Dialog.Header>
+        <Dialog.Body>
+          <p data-h2-margin="base(0, 0 ,x1, 0)">
+            {intl.formatMessage({
+              defaultMessage:
+                "You are about to add roles for the following member:",
+              id: "0cevfA",
+              description: "Lead in text for the add role to user form.",
+            })}
+          </p>
+          <ul>
+            <li data-h2-font-weight="base(bold)">
+              <span>{userName}</span>
+            </li>
+          </ul>
+          <p data-h2-margin="base(x1, 0 ,x1, 0)">
+            {intl.formatMessage({
+              defaultMessage: "Select the community and roles you want to add",
+              id: "58l6yP",
+              description:
+                "Lead in text for the pick community and roles inputs.",
+            })}
+          </p>
+          <FormProvider {...methods}>
+            <form onSubmit={handleSubmit(handleAddRoles)}>
+              <div
+                data-h2-display="base(flex)"
+                data-h2-flex-direction="base(column)"
+                data-h2-gap="base(x1 0)"
+              >
+                <Select
+                  id="community"
+                  name="community"
+                  nullSelection={intl.formatMessage({
+                    defaultMessage: "Select community",
+                    id: "OTWd3v",
+                    description:
+                      "Placeholder text for community selection input",
+                  })}
+                  label={intl.formatMessage(adminMessages.community)}
+                  rules={{
+                    required: intl.formatMessage(errorMessages.required),
+                  }}
+                  options={communityOptions ?? []}
+                />
+                <Combobox
+                  id="roles"
+                  name="roles"
+                  isMulti
+                  label={intl.formatMessage({
+                    defaultMessage: "Community roles",
+                    id: "6UiKYE",
+                    description:
+                      "Label for the input to select role of a community role",
+                  })}
+                  rules={{
+                    required: intl.formatMessage(errorMessages.required),
+                  }}
+                  placeholder={intl.formatMessage({
+                    defaultMessage: "Select role",
+                    id: "mTsq+x",
+                    description: "Placeholder text for role selection input",
+                  })}
+                  options={roleOptions}
+                />
+              </div>
+              <Dialog.Footer>
+                <Dialog.Close>
+                  <Button color="secondary">
+                    {intl.formatMessage(formMessages.cancelGoBack)}
+                  </Button>
+                </Dialog.Close>
+                <Button
+                  mode="solid"
+                  color="secondary"
+                  type="submit"
+                  disabled={isSubmitting}
+                >
+                  {isSubmitting
+                    ? intl.formatMessage(commonMessages.saving)
+                    : intl.formatMessage(formMessages.saveChanges)}
+                </Button>
+              </Dialog.Footer>
+            </form>
+          </FormProvider>
+        </Dialog.Body>
+      </Dialog.Content>
+    </Dialog.Root>
+  );
+};
+
+export default AddCommunityRoleDialog;
---
+++ apps/web/src/pages/Users/UpdateUserPage/components/RemoveCommunityRoleDialog.tsx
@@ -0,0 +1,165 @@
+import { useState } from "react";
+import { useIntl } from "react-intl";
+import TrashIcon from "@heroicons/react/24/outline/TrashIcon";
+
+import { Dialog, Button, Chip, Chips } from "@gc-digital-talent/ui";
+import {
+  commonMessages,
+  formMessages,
+  getLocalizedName,
+  uiMessages,
+} from "@gc-digital-talent/i18n";
+import { toast } from "@gc-digital-talent/toast";
+import {
+  UpdateUserRolesInput,
+  UpdateUserRolesMutation,
+  Role,
+  User,
+  RoleInput,
+  Community,
+} from "@gc-digital-talent/graphql";
+
+import { getFullNameHtml } from "~/utils/nameUtils";
+
+interface RemoveCommunityRoleDialogProps {
+  user: Pick<User, "id" | "firstName" | "lastName">;
+  roles: Role[];
+  community: Pick<Community, "id" | "name" | "teamIdForRoleAssignment">;
+  onRemoveRoles: (
+    submitData: UpdateUserRolesInput,
+  ) => Promise<UpdateUserRolesMutation["updateUserRoles"]>;
+}
+
+const RemoveCommunityRoleDialog = ({
+  user,
+  roles,
+  community,
+  onRemoveRoles,
+}: RemoveCommunityRoleDialogProps) => {
+  const intl = useIntl();
+  const [isDeleting, setIsDeleting] = useState<boolean>(false);
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const { id, firstName, lastName } = user;
+
+  const handleRemove = async () => {
+    const roleInputArray: RoleInput[] = roles.map((r) => {
+      return { roleId: r.id, teamId: community.teamIdForRoleAssignment };
+    });
+    setIsDeleting(true);
+    return onRemoveRoles({
+      userId: id,
+      roleAssignmentsInput: {
+        detach: roleInputArray,
+      },
+    })
+      .then(() => {
+        setIsOpen(false);
+        toast.success(
+          intl.formatMessage({
+            defaultMessage: "Role removed successfully",
+            id: "XcS2q2",
+            description:
+              "Message displayed to user when a role has been removed from a user",
+          }),
+        );
+      })
+      .catch(() => {
+        toast.error(
+          intl.formatMessage({
+            defaultMessage: "Member role update failed",
+            id: "Ly2bBb",
+            description:
+              "Alert displayed to user when an error occurs while editing a team member's roles",
+          }),
+        );
+      })
+      .finally(() => setIsDeleting(false));
+  };
+
+  const userName = getFullNameHtml(firstName, lastName, intl);
+  const roleDisplayName = (role: Role) =>
+    getLocalizedName(role.displayName, intl);
+  const communityDisplayName = getLocalizedName(community.name, intl);
+
+  const label = intl.formatMessage({
+    defaultMessage: "Remove from community",
+    id: "pZ8pDw",
+    description: "Label for the form to remove a community role from a user",
+  });
+
+  return (
+    <Dialog.Root open={isOpen} onOpenChange={setIsOpen}>
+      <Dialog.Trigger>
+        <Button color="black">
+          <TrashIcon data-h2-height="base(x.75)" data-h2-width="base(x.75)" />
+          <span data-h2-visually-hidden="base(invisible)">{label}</span>
+        </Button>
+      </Dialog.Trigger>
+      <Dialog.Content>
+        <Dialog.Header>{label}</Dialog.Header>
+        <Dialog.Body>
+          <p data-h2-margin="base(0, 0 ,x1, 0)">
+            {intl.formatMessage({
+              defaultMessage: "You are about to remove this member:",
+              id: "95aNQ1",
+              description: "Lead in text for removing roles on user form.",
+            })}
+          </p>
+          <ul>
+            <li data-h2-font-weight="base(bold)">
+              <span>{userName}</span>
+            </li>
+          </ul>
+          <p data-h2-margin="base(x1, 0 ,x1, 0)">
+            {intl.formatMessage({
+              defaultMessage: "From the following community:",
+              id: "hJDRa/",
+              description: "Follow in text for the community being updated",
+            })}
+          </p>
+          <ul>
+            <li data-h2-font-weight="base(bold)">
+              <span>{communityDisplayName}</span>
+            </li>
+          </ul>
+          <p data-h2-margin="base(x1, 0)">
+            {intl.formatMessage({
+              defaultMessage:
+                "The user will lose all the following community roles:",
+              id: "zsOKOD",
+              description:
+                "Text notifying user which community roles will be removed from the user",
+            })}
+          </p>
+          <Chips>
+            {roles.map((r) => (
+              <Chip color="secondary" key={r.id}>
+                {roleDisplayName(r)}
+              </Chip>
+            ))}
+          </Chips>
+          <p data-h2-margin="base(x1, 0)">
+            {intl.formatMessage(uiMessages.confirmContinue)}
+          </p>
+          <Dialog.Footer>
+            <Dialog.Close>
+              <Button color="secondary">
+                {intl.formatMessage(formMessages.cancelGoBack)}
+              </Button>
+            </Dialog.Close>
+            <Button
+              mode="solid"
+              color="error"
+              onClick={handleRemove}
+              disabled={isDeleting}
+            >
+              {isDeleting ? intl.formatMessage(commonMessages.removing) : label}
+            </Button>
+          </Dialog.Footer>
+        </Dialog.Body>
+      </Dialog.Content>
+    </Dialog.Root>
+  );
+};
+
+export default RemoveCommunityRoleDialog;
