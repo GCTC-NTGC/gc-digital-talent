da0eb9553261d013ed08d80830617af7c6720fac
vd1992 <40485260+vd1992@users.noreply.github.com>
2024-09-27 14:42:40 -0600

    change

--- api/app/Policies/PoolPolicy.php
+++ api/app/Policies/PoolPolicy.php
@@ -172,7 +172,7 @@ class PoolPolicy
      */
     public function publish(User $user, Pool $pool)
     {
-        if ($pool->getStatusAttribute() !== PoolStatus::DRAFT->name) {
+        if ($pool->status !== PoolStatus::DRAFT->name) {
             return Response::deny('Pool has already been published.');
         }
 
