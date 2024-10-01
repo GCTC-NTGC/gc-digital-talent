32af211524f1aa0b82c095c92dc0358b418a1855
Eric Sizer <eric.sizer@gmail.com>
2024-10-01 12:43:07 -0400

    renable unsafe arg rule

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -106,7 +106,6 @@ module.exports = {
     "@typescript-eslint/prefer-promise-reject-errors": "off", // Remove in #11382
 
     // Remove in #11384
-    "@typescript-eslint/no-unsafe-argument": "off",
     "@typescript-eslint/no-unsafe-assignment": "off",
     "@typescript-eslint/no-unsafe-call": "off",
     "@typescript-eslint/no-unsafe-member-access": "off",
