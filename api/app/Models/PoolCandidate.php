7674642bc4b6ef68a7f21d85ef0825353d465ea3
Eric Sizer <esizer@users.noreply.github.com>
2024-10-04 13:33:36 -0400

    [Fix] `PoolCandidate` category accessor return (#11679)
    * revert change to return type
    
    * remove else statement
    
    (cherry picked from commit 62d788973e41bac3bea645747463b9ecbf671113)

--- api/app/Models/PoolCandidate.php
+++ api/app/Models/PoolCandidate.php
@@ -266,7 +266,7 @@ class PoolCandidate extends Model
 
     public function getCategoryAttribute()
     {
-        $category = null;
+        $category = PriorityWeight::OTHER;
 
         $this->loadMissing(['user' => [
             'citizenship',
@@ -280,11 +280,13 @@ class PoolCandidate extends Model
             $category = PriorityWeight::VETERAN;
         } elseif ($this->user->citizenship === CitizenshipStatus::CITIZEN->name || $this->user->citizenship === CitizenshipStatus::PERMANENT_RESIDENT->name) {
             $category = PriorityWeight::CITIZEN_OR_PERMANENT_RESIDENT;
-        } else {
-            $category = PriorityWeight::OTHER;
         }
 
-        return $category;
+        return [
+            'weight' => $category->weight($category->name),
+            'value' => $category->name,
+            'label' => PriorityWeight::localizedString($category->name),
+        ];
     }
 
     public static function scopeQualifiedStreams(Builder $query, ?array $streams): Builder
