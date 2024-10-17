ca52b2b975fb51f5fccbf65b51c0d088e10accab
vd1992 <40485260+vd1992@users.noreply.github.com>
2024-10-17 16:19:51 -0600

    style tweak Crumb

--- packages/ui/src/components/Breadcrumbs/Crumb.tsx
+++ packages/ui/src/components/Breadcrumbs/Crumb.tsx
@@ -15,6 +15,7 @@ const Crumb = ({ children, isCurrent, url }: CrumbProps) => (
       data-h2-color="base:all(white) base:all:hover(secondary.lighter) base:all:focus-visible(black) base:iap:all:hover(secondary.lightest) base:iap:all:focus-visible(black)"
       data-h2-background-color="base:all:focus-visible(focus)"
       data-h2-outline="base(none)"
+      data-h2-font-size="base(caption)"
       {...(isCurrent
         ? {
             "data-h2-font-weight": "base(700)",
@@ -28,15 +29,19 @@ const Crumb = ({ children, isCurrent, url }: CrumbProps) => (
     {!isCurrent && (
       <span
         aria-hidden="true"
-        data-h2-height="base(x1)"
-        data-h2-width="base(x1)"
+        data-h2-height="base(x1.1)"
+        data-h2-width="base(x.5)"
         data-h2-display="base(inline-block)"
         data-h2-margin="base(0, 0, 0, x.5)"
         data-h2-vertical-align="base(middle)"
-        data-h2-stroke="base:children[svg, svg path](tertiary) base:iap:all:children[svg, svg path](primary.light)"
-        data-h2-fill="base:children[svg, svg path](tertiary) base:iap:all:children[svg, svg path](primary.light)"
+        data-h2-stroke="base:children[svg, svg path](tertiary.light) base:iap:all:children[svg, svg path](primary.light)"
+        data-h2-fill="base:children[svg, svg path](tertiary.light) base:iap:all:children[svg, svg path](primary.light)"
       >
-        <ChevronRightIcon />
+        <ChevronRightIcon
+          data-h2-vertical-align="base(middle)"
+          data-h2-height="base(x.6)"
+          data-h2-width="base(x.6)"
+        />
       </span>
     )}
   </li>
