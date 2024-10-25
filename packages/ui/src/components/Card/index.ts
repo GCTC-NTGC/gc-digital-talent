6d27333f5ce547cffb5d8fc3d10bb19c933e4ae1
Peter Giles <8978655+petertgiles@users.noreply.github.com>
2024-10-25 14:35:48 -0400

    CardSectioned component

--- packages/ui/src/components/Card/index.ts
+++ packages/ui/src/components/Card/index.ts
@@ -1,5 +1,6 @@
 import CardFlat, { type CardFlatProps } from "./CardFlat";
 import CardBasic, { type CardBasicProps } from "./CardBasic/CardBasic";
+import CardSectioned from "./CardSectioned";
 
-export { CardFlat, CardBasic };
+export { CardFlat, CardBasic, CardSectioned };
 export type { CardFlatProps, CardBasicProps };
