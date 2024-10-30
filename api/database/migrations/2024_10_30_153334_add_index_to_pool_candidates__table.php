1f5bed76494ceeec8537fb65a119192ecaf9411d
Brinda Sasikumar <Brinda.Sasikumar@gmail.com>
2024-10-30 11:44:53 -0400

    add index to candidates table

---
+++ api/database/migrations/2024_10_30_153334_add_index_to_pool_candidates__table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+   /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('pool_candidates', function (Blueprint $table) {
+            $table->index(['submitted_at', 'user_id']);
+            $table->index('status_weight');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('pool_candidates', function (Blueprint $table) {
+            $table->dropIndex(['submitted_at', 'user_id']);
+            $table->dropIndex('status_weight');
+        });
+    }
+};
