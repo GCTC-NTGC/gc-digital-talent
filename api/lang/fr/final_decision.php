fd590f21055a8f048e3861f8d58ba21b85a799f6
Matt Nigh <matt@mattnigh.com>
2024-11-13 14:09:49 -0500

    Change Qualifié to Qualifié(e)

--- api/lang/fr/final_decision.php
+++ api/lang/fr/final_decision.php
@@ -5,7 +5,7 @@ return [
     'disqualified_pending' => 'Disqualifié(e) : Décision en attente',
     'disqualified_removed' => 'Retiré : Disqualifié(e)',
 
-    'qualified' => 'Qualifié',
+    'qualified' => 'Qualifié(e)',
     'qualified_expired' => 'Expiré : Qualifié',
     'qualified_pending' => 'Qualifié : Décision en attente',
     'qualified_placed' => 'Qualifié : Placed',
