8c9f416a7c9e1a080779d30145ee3dd8efb04084
Matt Nigh <matt@mattnigh.com>
2024-11-15 12:54:29 -0500

    Update test

--- apps/web/src/components/ApplicationCard/ApplicationCard.test.tsx
+++ apps/web/src/components/ApplicationCard/ApplicationCard.test.tsx
@@ -70,7 +70,7 @@ describe("ApplicationCard", () => {
       ),
     });
     const links = screen.queryAllByRole("link");
-    expect(links).toHaveLength(3);
+    expect(links).toHaveLength(2);
     expect(links[0]).toHaveAttribute(
       "href",
       expect.stringContaining(mockApplication.id),
@@ -92,7 +92,7 @@ describe("ApplicationCard", () => {
     });
 
     const links = screen.queryAllByRole("link");
-    expect(links).toHaveLength(4);
+    expect(links).toHaveLength(3);
     expect(links[0]).toHaveAttribute(
       "href",
       expect.stringContaining(mockApplication.id),
@@ -128,7 +128,7 @@ describe("ApplicationCard", () => {
       ),
     });
     const links = screen.queryAllByRole("link");
-    expect(links).toHaveLength(3);
+    expect(links).toHaveLength(2);
     const qualifiedLabel = screen.queryByText("Submission date passed");
 
     expect(qualifiedLabel).toBeInTheDocument();
