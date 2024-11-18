38ebeab15844ab67db8c40f2a577cc6f2c876432
vd1992 <40485260+vd1992@users.noreply.github.com>
2024-11-18 15:42:40 -0700

    change enum words

--- api/lang/en/course_language.php
+++ api/lang/en/course_language.php
@@ -1,9 +1,7 @@
 <?php
 
-use Illuminate\Support\Facades\Lang;
-
 return [
-    'english' => Lang::get('common.english_only', [], 'en'),
-    'french' => Lang::get('common.french_only', [], 'en'),
-    'bilingual' => 'Bilingual (English and French)',
+    'english' => 'English',
+    'french' => 'French',
+    'bilingual' => 'Bilingual',
 ];
--- api/lang/fr/course_language.php
+++ api/lang/fr/course_language.php
@@ -1,9 +1,7 @@
 <?php
 
-use Illuminate\Support\Facades\Lang;
-
 return [
-    'english' => Lang::get('common.english_only', [], 'fr'),
-    'french' => Lang::get('common.french_only', [], 'fr'),
-    'bilingual' => 'Bilingue (français et anglais)',
+    'english' => 'Anglais',
+    'french' => 'Français',
+    'bilingual' => 'Bilingue',
 ];
