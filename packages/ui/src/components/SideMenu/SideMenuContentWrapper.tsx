bac62ab270797d7597be810ffab443a47b0a2738
Matt <mnigh@users.noreply.github.com>
2024-11-19 14:47:51 -0500

    [Debt] Removes `SideMenu` component (#12015)
    * Remove SideMenu
    
    * Refactor story

--- packages/ui/src/components/SideMenu/SideMenuContentWrapper.tsx
+++
@@ -1,7 +0,0 @@
-import { ReactNode } from "react";
-
-const SideMenuContentWrapper = ({ children }: { children?: ReactNode }) => (
-  <div data-h2-flex-item="base(fill)">{children}</div>
-);
-
-export default SideMenuContentWrapper;
