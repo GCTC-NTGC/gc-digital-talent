012e8e2d580e4ad6ab3d0ae290726b65b0268dc9
vd1992 <40485260+vd1992@users.noreply.github.com>
2024-11-19 13:09:12 -0700

    refactor factory

--- api/database/factories/WorkExperienceFactory.php
+++ api/database/factories/WorkExperienceFactory.php
@@ -38,83 +38,100 @@ class WorkExperienceFactory extends Factory
     public function definition()
     {
         $startDate = $this->faker->date();
-        $employmentCategory = $this->faker->randomElement(EmploymentCategory::cases())->name;
-        $extSizeOfOrganization = null;
-        $extRoleSeniority = null;
-        $govEmploymentType = null;
-        $govPositionType = null;
-        $govContractStartDate = null;
-        $govContractEndDate = null;
-        $govContractorRoleSeniority = null;
-        $govContractorType = null;
-        $cafEmploymentType = null;
-        $cafForce = null;
-        $cafRank = null;
-        $classificationId = null;
-        $departmentId = null;
-
-        // generate the appropriate values depending off the cases of EmploymentCategory
-        if ($employmentCategory === EmploymentCategory::EXTERNAL_ORGANIZATION->name) {
-            $extSizeOfOrganization = $this->faker->randomElement(ExternalSizeOfOrganization::cases())->name;
-            $extRoleSeniority = $this->faker->randomElement(ExternalRoleSeniority::cases())->name;
-        }
-
-        if ($employmentCategory === EmploymentCategory::GOVERNMENT_OF_CANADA->name) {
-            $govEmploymentType = $this->faker->randomElement(WorkExperienceGovEmployeeType::cases())->name;
-
-            if ($govEmploymentType !== WorkExperienceGovEmployeeType::STUDENT->name && $govEmploymentType !== WorkExperienceGovEmployeeType::CONTRACTOR->name) {
-                $classification = Classification::inRandomOrder()->first();
-                if (! $classification) {
-                    $classification = Classification::factory()->create();
-                }
-                $classificationId = $classification->id;
-                $department = Department::inRandomOrder()->first();
-                if (! $department) {
-                    $department = Department::factory()->create();
-                }
-                $departmentId = $department->id;
-            }
-
-            if ($govEmploymentType === WorkExperienceGovEmployeeType::INDETERMINATE->name) {
-                $govPositionType = $this->faker->randomElement(GovPositionType::cases())->name;
-            }
-
-            if ($govEmploymentType === WorkExperienceGovEmployeeType::CONTRACTOR->name) {
-                $govContractStartDate = $this->faker->date();
-                $govContractEndDate = $this->faker->boolean() ? $this->faker->dateTimeBetween($govContractStartDate) : null;
-                $govContractorRoleSeniority = $this->faker->randomElement(GovContractorRoleSeniority::cases())->name;
-                $govContractorType = $this->faker->randomElement(GovContractorType::cases())->name;
-            }
-        }
-
-        if ($employmentCategory === EmploymentCategory::CANADIAN_ARMED_FORCES->name) {
-            $cafEmploymentType = $this->faker->randomElement(CafEmploymentType::cases())->name;
-            $cafForce = $this->faker->randomElement(CafForce::cases())->name;
-            $cafRank = $this->faker->randomElement(CafRank::cases())->name;
-        }
 
         return [
             'user_id' => User::factory(),
             'role' => $this->faker->jobTitle(),
-            'organization' => $employmentCategory === EmploymentCategory::CANADIAN_ARMED_FORCES->name ? null : $this->faker->company(),
-            'division' => $employmentCategory === EmploymentCategory::CANADIAN_ARMED_FORCES->name ? null : $this->faker->bs(),
+            'organization' => function (array $attributes) {
+                return $attributes['employment_category'] === EmploymentCategory::CANADIAN_ARMED_FORCES->name ?
+                    null : $this->faker->company();
+            },
+            'division' => function (array $attributes) {
+                return $attributes['employment_category'] === EmploymentCategory::CANADIAN_ARMED_FORCES->name ?
+                    null : $this->faker->bs();
+            },
             'start_date' => $startDate,
             'end_date' => $this->faker->boolean() ? $this->faker->dateTimeBetween($startDate) : null,
             'details' => $this->faker->text(),
-            'employment_category' => $employmentCategory,
-            'ext_size_of_organization' => $extSizeOfOrganization,
-            'ext_role_seniority' => $extRoleSeniority,
-            'gov_employment_type' => $govEmploymentType,
-            'gov_position_type' => $govPositionType,
-            'gov_contract_start_date' => $govContractStartDate,
-            'gov_contract_end_date' => $govContractEndDate,
-            'gov_contractor_role_seniority' => $govContractorRoleSeniority,
-            'gov_contractor_type' => $govContractorType,
-            'caf_employment_type' => $cafEmploymentType,
-            'caf_force' => $cafForce,
-            'caf_rank' => $cafRank,
-            'classification_id' => $classificationId,
-            'department_id' => $departmentId,
+            'employment_category' => $this->faker->randomElement(EmploymentCategory::cases())->name,
+            'ext_size_of_organization' => function (array $attributes) {
+                return $attributes['employment_category'] === EmploymentCategory::EXTERNAL_ORGANIZATION->name ?
+                    $this->faker->randomElement(ExternalSizeOfOrganization::cases())->name : null;
+            },
+            'ext_role_seniority' => function (array $attributes) {
+                return $attributes['employment_category'] === EmploymentCategory::EXTERNAL_ORGANIZATION->name ?
+                    $this->faker->randomElement(ExternalRoleSeniority::cases())->name : null;
+            },
+            'gov_employment_type' => function (array $attributes) {
+                return $attributes['employment_category'] === EmploymentCategory::GOVERNMENT_OF_CANADA->name ?
+                    $this->faker->randomElement(WorkExperienceGovEmployeeType::cases())->name : null;
+            },
+            'gov_position_type' => function (array $attributes) {
+                return $attributes['gov_employment_type'] === WorkExperienceGovEmployeeType::INDETERMINATE->name ?
+                    $this->faker->randomElement(GovPositionType::cases())->name : null;
+            },
+            'gov_contract_start_date' => function (array $attributes) {
+                return $attributes['gov_employment_type'] === WorkExperienceGovEmployeeType::CONTRACTOR->name ?
+                    $this->faker->date() : null;
+            },
+            'gov_contract_end_date' => function (array $attributes) {
+                return $attributes['gov_employment_type'] === WorkExperienceGovEmployeeType::CONTRACTOR->name && $this->faker->boolean() ?
+                    $this->faker->dateTimeBetween($attributes['gov_contract_start_date']) : null;
+            },
+            'gov_contractor_role_seniority' => function (array $attributes) {
+                return $attributes['gov_employment_type'] === WorkExperienceGovEmployeeType::CONTRACTOR->name ?
+                    $this->faker->randomElement(GovContractorRoleSeniority::cases())->name : null;
+            },
+            'gov_contractor_type' => function (array $attributes) {
+                return $attributes['gov_employment_type'] === WorkExperienceGovEmployeeType::CONTRACTOR->name ?
+                    $this->faker->randomElement(GovContractorType::cases())->name : null;
+            },
+            'caf_employment_type' => function (array $attributes) {
+                return $attributes['employment_category'] === EmploymentCategory::CANADIAN_ARMED_FORCES->name ?
+                    $this->faker->randomElement(CafEmploymentType::cases())->name : null;
+            },
+            'caf_force' => function (array $attributes) {
+                return $attributes['employment_category'] === EmploymentCategory::CANADIAN_ARMED_FORCES->name ?
+                    $this->faker->randomElement(CafForce::cases())->name : null;
+            },
+            'caf_rank' => function (array $attributes) {
+                return $attributes['employment_category'] === EmploymentCategory::CANADIAN_ARMED_FORCES->name ?
+                    $this->faker->randomElement(CafRank::cases())->name : null;
+            },
+            'classification_id' => function (array $attributes) {
+                if (
+                    $attributes['employment_category'] === EmploymentCategory::GOVERNMENT_OF_CANADA->name &&
+                    $attributes['gov_employment_type'] !== WorkExperienceGovEmployeeType::STUDENT->name &&
+                    $attributes['gov_employment_type'] !== WorkExperienceGovEmployeeType::CONTRACTOR->name
+                ) {
+                    $classification = Classification::inRandomOrder()->first();
+                    if (! $classification) {
+                        $classification = Classification::factory()->create();
+                    }
+                    $classificationId = $classification->id;
+
+                    return $classificationId;
+                }
+
+                return null;
+            },
+            'department_id' => function (array $attributes) {
+                if (
+                    $attributes['employment_category'] === EmploymentCategory::GOVERNMENT_OF_CANADA->name &&
+                    $attributes['gov_employment_type'] !== WorkExperienceGovEmployeeType::STUDENT->name &&
+                    $attributes['gov_employment_type'] !== WorkExperienceGovEmployeeType::CONTRACTOR->name
+                ) {
+                    $department = Department::inRandomOrder()->first();
+                    if (! $department) {
+                        $department = Department::factory()->create();
+                    }
+                    $departmentId = $department->id;
+
+                    return $departmentId;
+                }
+
+                return null;
+            },
         ];
     }
 }
