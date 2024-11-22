944eff1502ddc96acc01df454d870ec3cf80857a
Matt Nigh <matt@mattnigh.com>
2024-11-22 12:43:37 -0500

    Add all experience types

--- apps/playwright/fixtures/ExperiencePage.ts
+++ apps/playwright/fixtures/ExperiencePage.ts
@@ -1,6 +1,13 @@
 import { Locator, type Page } from "@playwright/test";
 
-import { InputMaybe, WorkExperienceInput } from "@gc-digital-talent/graphql";
+import {
+  InputMaybe,
+  WorkExperienceInput,
+  PersonalExperienceInput,
+  CommunityExperienceInput,
+  AwardExperienceInput,
+  EducationExperienceInput,
+} from "@gc-digital-talent/graphql";
 
 import AppPage from "./AppPage";
 
@@ -70,19 +77,166 @@ class ExperiencePage extends AppPage {
     await this.waitForGraphqlResponse("CreateWorkExperience");
   }
 
+  async addPersonalExperience(input: PersonalExperienceInput) {
+    await this.create();
+    await this.typeLocator.selectOption("personal");
+
+    await this.page
+      .getByRole("textbox", { name: /short title for this experience/i })
+      .fill(input.title ?? "test short title");
+
+    await this.page
+      .getByRole("textbox", { name: /experience description/i })
+      .fill(input.description ?? "test description");
+
+    await this.page
+      .getByRole("checkbox", {
+        name: /i agree to share this information with verified government of canada hiring managers and hr advisors who have access to this platform./i,
+      })
+      .click();
+
+    await this.fillDate(input.startDate);
+
+    if (!input.endDate) {
+      await this.page
+        .getByRole("checkbox", {
+          name: /i am currently active in this experience/i,
+        })
+        .click();
+    } else {
+      await this.fillDate(input.endDate, true);
+    }
+
+    await this.page
+      .getByRole("textbox", { name: /additional details/i })
+      .fill(input.details ?? "test details");
+
+    await this.save();
+    await this.waitForGraphqlResponse("CreatePersonalExperience");
+  }
+
+  async addCommunityExperience(input: CommunityExperienceInput) {
+    await this.create();
+    await this.typeLocator.selectOption("community");
+
+    await this.page
+      .getByRole("textbox", { name: /my role/i })
+      .fill(input.title ?? "test role");
+
+    await this.page
+      .getByRole("textbox", { name: /organization/i })
+      .fill(input?.organization ?? "test org");
+
+    await this.page
+      .getByRole("textbox", { name: /project/i })
+      .fill(input?.project ?? "test project");
+
+    await this.fillDate(input.startDate);
+
+    if (!input.endDate) {
+      await this.page
+        .getByRole("checkbox", { name: /i am currently active in this role/i })
+        .click();
+    } else {
+      await this.fillDate(input.endDate, true);
+    }
+
+    await this.page
+      .getByRole("textbox", { name: /additional details/i })
+      .fill(input.details ?? "test details");
+
+    await this.save();
+    await this.waitForGraphqlResponse("CreateCommunityExperience");
+  }
+
+  async addAwardExperience(input: AwardExperienceInput) {
+    await this.create();
+    await this.typeLocator.selectOption("award");
+
+    await this.page
+      .getByRole("textbox", { name: /award title/i })
+      .fill(input.title ?? "test award title");
+
+    await this.page
+      .getByRole("combobox", { name: /awarded to/i })
+      .selectOption({ label: "Me" });
+
+    await this.page
+      .getByRole("textbox", { name: /organization/i })
+      .fill(input?.issuedBy ?? "test org");
+
+    await this.page
+      .getByRole("combobox", { name: /award scope/i })
+      .selectOption({ label: "Local" });
+
+    await this.fillDate(input.awardedDate, false, /date awarded/i);
+
+    await this.page
+      .getByRole("textbox", { name: /additional details/i })
+      .fill(input.details ?? "test details");
+
+    await this.save();
+    await this.waitForGraphqlResponse("CreateAwardExperience");
+  }
+
+  async addEducationExperience(input: EducationExperienceInput) {
+    await this.create();
+    await this.typeLocator.selectOption("education");
+
+    await this.page
+      .getByRole("combobox", { name: /type of education/i })
+      .selectOption({ label: "PhD" });
+
+    await this.page
+      .getByRole("textbox", { name: /area of study/i })
+      .fill(input?.areaOfStudy ?? "test area of study");
+
+    await this.page
+      .getByRole("textbox", { name: /institution/i })
+      .fill(input?.areaOfStudy ?? "test institution");
+
+    await this.page
+      .getByRole("combobox", { name: /status/i })
+      .selectOption({ label: "Audited" });
+
+    await this.page
+      .getByRole("textbox", { name: /thesis title/i })
+      .fill(input?.thesisTitle ?? "test thesis title");
+
+    await this.fillDate(input.startDate);
+
+    if (!input.endDate) {
+      await this.page
+        .getByRole("checkbox", {
+          name: /i am currently active in this education/i,
+        })
+        .click();
+    } else {
+      await this.fillDate(input.endDate, true);
+    }
+
+    await this.page
+      .getByRole("textbox", { name: /additional details/i })
+      .fill(input.details ?? "test details");
+
+    await this.save();
+    await this.waitForGraphqlResponse("CreateEducationExperience");
+  }
+
   async save() {
     await this.page.getByRole("button", { name: /save and return/i }).click();
   }
 
-  async fillDate(d?: InputMaybe<string>, end?: boolean) {
+  async fillDate(d?: InputMaybe<string>, end?: boolean, label?: RegExp) {
     if (end && !d) {
       return;
     }
 
     const dArr = d?.split("-");
     if (dArr) {
+      const startOrEnd = end ? /end date/i : /start date/i;
       const input = this.page.getByRole("group", {
-        name: end ? /end date/i : /start date/i,
+        name: label ?? startOrEnd,
       });
 
       await input.getByRole("spinbutton", { name: /year/i }).fill(dArr[0]);
