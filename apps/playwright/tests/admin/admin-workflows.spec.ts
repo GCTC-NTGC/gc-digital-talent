b4d50c2c540540d6058c18ac9732ea648b440898
Eric Sizer <eric.sizer@gmail.com>
2024-11-27 11:55:54 -0500

    remove outdated queries in tests

--- apps/playwright/tests/admin/admin-workflows.spec.ts
+++ apps/playwright/tests/admin/admin-workflows.spec.ts
@@ -5,7 +5,9 @@ import { loginBySub } from "~/utils/auth";
 const USERS_PAGINATED_QUERY = "UsersPaginated";
 
 const searchForUser = async (appPage: AppPage, name: string) => {
-  await appPage.page.getByRole("textbox", { name: /search/i }).fill(name);
+  await appPage.page
+    .getByRole("textbox", { name: /search/i })
+    .fill(name, { timeout: 30000 });
 
   await appPage.waitForGraphqlResponse(USERS_PAGINATED_QUERY);
 };
@@ -45,7 +47,6 @@ test.describe("Admin workflows", () => {
 
     await appPage.page.getByRole("link", { name: /edit gul fields/i }).click();
     await appPage.waitForGraphqlResponse("UpdateUserData");
-    await appPage.waitForGraphqlResponse("UpdateUserOptions");
 
     await appPage.page
       .getByRole("textbox", { name: /telephone/i })
