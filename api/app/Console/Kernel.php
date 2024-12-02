b82fb22dcf85060ff7a9307c887081ad35b4acc5
Peter Giles <8978655+petertgiles@users.noreply.github.com>
2024-12-02 10:06:30 -0500

    switch scheduled tasks

--- api/app/Console/Kernel.php
+++ api/app/Console/Kernel.php
@@ -7,6 +7,7 @@ use App\Console\Commands\SendNotificationsApplicationDeadlineApproaching;
 use App\Console\Commands\SendNotificationsPoolPublished;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\Storage;
 
 class Kernel extends ConsoleKernel
 {
@@ -19,6 +20,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
+        $logFilesDisk = Storage::disk('logFiles');
+
         // $schedule->command(HardDeleteOldUsers::class)->dailyAt('08:00');
 
         // clean up old user generated files every day at 1:00 AM
@@ -26,20 +29,20 @@ class Kernel extends ConsoleKernel
             ->timezone('America/Toronto')
             ->dailyAt('1:00')
             ->withoutOverlapping()
-            ->appendOutputTo('/tmp/laravel-prune-user-generated-files.log');
+            ->appendOutputTo($logFilesDisk->path('laravel-prune-user-generated-files.log'));
 
         // queue up Application Deadline Approaching emails every day, close to the time the pool would close
         $schedule->command(SendNotificationsApplicationDeadlineApproaching::class)
             ->timezone('America/Toronto')
             // 10 PM Eastern is the same day across the country, close to the end of the day in NL
             ->dailyAt('22:00')
-            ->appendOutputTo('/tmp/send-notifications-application-deadline-approaching.log');
+            ->appendOutputTo($logFilesDisk->path('send-notifications-application-deadline-approaching.log'));
 
         // send out 'new pool' emails overnight
         $schedule->command(SendNotificationsPoolPublished::class)
             ->timezone('America/Toronto')
             ->dailyAt('3:00')
-            ->appendOutputTo('/tmp/send-notifications-pool-published.log');
+            ->appendOutputTo($logFilesDisk->path('send-notifications-pool-published.log'));
     }
 
     /**
