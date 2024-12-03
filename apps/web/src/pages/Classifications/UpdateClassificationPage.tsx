9336652642d9c4769c7b27f9fe7cd80fc0f4f498
Eric Sizer <eric.sizer@gmail.com>
2024-12-03 12:22:53 -0500

    set min value for salary fields

--- apps/web/src/pages/Classifications/UpdateClassificationPage.tsx
+++ apps/web/src/pages/Classifications/UpdateClassificationPage.tsx
@@ -254,6 +254,7 @@ export const UpdateClassificationForm = ({
                       "Label displayed for the classification form min salary field.",
                   })}
                   type="number"
+                  min="0"
                   rules={{
                     required: intl.formatMessage(errorMessages.required),
                     min: {
@@ -274,6 +275,7 @@ export const UpdateClassificationForm = ({
                       "Label displayed for the classification form max salary field.",
                   })}
                   type="number"
+                  min="0"
                   rules={{
                     required: intl.formatMessage(errorMessages.required),
                     min: {
