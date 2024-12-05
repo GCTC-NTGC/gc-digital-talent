1f8af715c24265180c3542abc5d855a4e0ca9ef5
Yoni K <yonikid15@gmail.com>
2024-12-05 09:54:54 -0500

    Split up work content by category

--- apps/web/src/components/ExperienceCard/WorkContent.tsx
+++ apps/web/src/components/ExperienceCard/WorkContent.tsx
@@ -1,266 +1,33 @@
 import { useIntl } from "react-intl";
 
-import { commonMessages, getLocalizedName } from "@gc-digital-talent/i18n";
-import {
-  EmploymentCategory,
-  GovContractorType,
-  GovEmployeeType,
-  WorkExperience,
-  WorkExperienceGovEmployeeType,
-} from "@gc-digital-talent/graphql";
-import { Separator } from "@gc-digital-talent/ui";
+import { commonMessages } from "@gc-digital-talent/i18n";
+import { EmploymentCategory, WorkExperience } from "@gc-digital-talent/graphql";
 
 import { getExperienceFormLabels } from "~/utils/experienceUtils";
 
 import ContentSection from "./ContentSection";
 import { ContentProps } from "./types";
+import ExternalContent from "./WorkContent/ExternalContent";
+import CafContent from "./WorkContent/CafContent";
+import GovContent from "./WorkContent/GovContent";
 
 const WorkContent = ({
-  experience: {
-    division,
-    employmentCategory,
-    extSizeOfOrganization,
-    extRoleSeniority,
-    govEmploymentType,
-    classification,
-    govPositionType,
-    govContractorRoleSeniority,
-    govContractorType,
-    contractorFirmAgencyName,
-    cafEmploymentType,
-    cafRank,
-  },
+  experience,
   headingLevel,
 }: ContentProps<Omit<WorkExperience, "user">>) => {
   const intl = useIntl();
   const experienceFormLabels = getExperienceFormLabels(intl);
+  const { division, employmentCategory } = experience;
 
   switch (employmentCategory?.value) {
     case EmploymentCategory.ExternalOrganization:
       return (
-        <>
-          <ContentSection
-            title={experienceFormLabels.team}
-            headingLevel={headingLevel}
-            data-h2-border-right="l-tablet(1px solid gray.lighter)"
-          >
-            {division ?? intl.formatMessage(commonMessages.notAvailable)}
-          </ContentSection>
-          <Separator space="sm" />
-          <div
-            data-h2-display="base(grid)"
-            data-h2-gap="base(x1)"
-            data-h2-grid-template-columns="l-tablet(repeat(2, 1fr))"
-          >
-            <ContentSection
-              title={experienceFormLabels.extSizeOfOrganization}
-              headingLevel={headingLevel}
-              data-h2-border-right="l-tablet(1px solid gray.lighter)"
-            >
-              {getLocalizedName(extSizeOfOrganization?.label, intl) ??
-                intl.formatMessage(commonMessages.notAvailable)}
-            </ContentSection>
-            <ContentSection
-              title={experienceFormLabels.extRoleSeniority}
-              headingLevel={headingLevel}
-            >
-              {getLocalizedName(extRoleSeniority?.label, intl) ??
-                intl.formatMessage(commonMessages.notAvailable)}
-            </ContentSection>
-          </div>
-        </>
+        <ExternalContent experience={experience} headingLevel={headingLevel} />
       );
     case EmploymentCategory.GovernmentOfCanada:
-      if (govEmploymentType?.value === WorkExperienceGovEmployeeType.Student) {
-        return (
-          <>
-            <ContentSection
-              title={experienceFormLabels.team}
-              headingLevel={headingLevel}
-              data-h2-border-right="l-tablet(1px solid gray.lighter)"
-            >
-              {division ?? intl.formatMessage(commonMessages.notAvailable)}
-            </ContentSection>
-            <Separator space="sm" />
-            <ContentSection
-              title={experienceFormLabels.govEmploymentType}
-              headingLevel={headingLevel}
-              data-h2-border-right="l-tablet(1px solid gray.lighter)"
-            >
-              {getLocalizedName(govEmploymentType.label, intl) ??
-                intl.formatMessage(commonMessages.notAvailable)}
-            </ContentSection>
-          </>
-        );
-      } else if (
-        govEmploymentType?.value === WorkExperienceGovEmployeeType.Casual ||
-        govEmploymentType?.value === WorkExperienceGovEmployeeType.Term
-      ) {
-        <>
-          <ContentSection
-            title={experienceFormLabels.team}
-            headingLevel={headingLevel}
-            data-h2-border-right="l-tablet(1px solid gray.lighter)"
-          >
-            {division ?? intl.formatMessage(commonMessages.notAvailable)}
-          </ContentSection>
-          <Separator space="sm" />
-          <div
-            data-h2-display="base(grid)"
-            data-h2-gap="base(x1)"
-            data-h2-grid-template-columns="l-tablet(repeat(2, 1fr))"
-          >
-            <ContentSection
-              title={experienceFormLabels.govEmploymentType}
-              headingLevel={headingLevel}
-              data-h2-border-right="l-tablet(1px solid gray.lighter)"
-            >
-              {getLocalizedName(govEmploymentType.label, intl) ??
-                intl.formatMessage(commonMessages.notAvailable)}
-            </ContentSection>
-            <ContentSection
-              title={experienceFormLabels.classification}
-              headingLevel={headingLevel}
-            >
-              {classification
-                ? `${classification.group}-0${classification.level}`
-                : intl.formatMessage(commonMessages.notAvailable)}
-            </ContentSection>
-          </div>
-        </>;
-      } else if (
-        govEmploymentType?.value === WorkExperienceGovEmployeeType.Indeterminate
-      ) {
-        <>
-          <ContentSection
-            title={experienceFormLabels.team}
-            headingLevel={headingLevel}
-            data-h2-border-right="l-tablet(1px solid gray.lighter)"
-          >
-            {division ?? intl.formatMessage(commonMessages.notAvailable)}
-          </ContentSection>
-          <Separator space="sm" />
-          <div
-            data-h2-display="base(grid)"
-            data-h2-gap="base(x1)"
-            data-h2-grid-template-columns="l-tablet(repeat(3, 1fr))"
-          >
-            <ContentSection
-              title={experienceFormLabels.govEmploymentType}
-              headingLevel={headingLevel}
-              data-h2-border-right="l-tablet(1px solid gray.lighter)"
-            >
-              {getLocalizedName(govEmploymentType.label, intl) ??
-                intl.formatMessage(commonMessages.notAvailable)}
-            </ContentSection>
-            <ContentSection
-              title={experienceFormLabels.positionType}
-              headingLevel={headingLevel}
-              data-h2-border-right="l-tablet(1px solid gray.lighter)"
-            >
-              {getLocalizedName(govPositionType?.label, intl) ??
-                intl.formatMessage(commonMessages.notAvailable)}
-            </ContentSection>
-            <ContentSection
-              title={experienceFormLabels.classification}
-              headingLevel={headingLevel}
-            >
-              {classification
-                ? `${classification.group}-0${classification.level}`
-                : intl.formatMessage(commonMessages.notAvailable)}
-            </ContentSection>
-          </div>
-        </>;
-      } else if (
-        govEmploymentType?.value === WorkExperienceGovEmployeeType.Contractor
-      ) {
-        <>
-          <ContentSection
-            title={experienceFormLabels.team}
-            headingLevel={headingLevel}
-            data-h2-border-right="l-tablet(1px solid gray.lighter)"
-          >
-            {division ?? intl.formatMessage(commonMessages.notAvailable)}
-          </ContentSection>
-          <Separator space="sm" />
-          <div
-            data-h2-display="base(grid)"
-            data-h2-gap="base(x1)"
-            data-h2-grid-template-columns="l-tablet(repeat(3, 1fr))"
-          >
-            <ContentSection
-              title={experienceFormLabels.govEmploymentType}
-              headingLevel={headingLevel}
-              data-h2-border-right="l-tablet(1px solid gray.lighter)"
-            >
-              {getLocalizedName(govEmploymentType.label, intl) ??
-                intl.formatMessage(commonMessages.notAvailable)}
-            </ContentSection>
-            <ContentSection
-              title={experienceFormLabels.govContractorRoleSeniority}
-              headingLevel={headingLevel}
-              data-h2-border-right="l-tablet(1px solid gray.lighter)"
-            >
-              {getLocalizedName(govContractorRoleSeniority?.label, intl) ??
-                intl.formatMessage(commonMessages.notAvailable)}
-            </ContentSection>
-            <ContentSection
-              title={experienceFormLabels.govContractorType}
-              headingLevel={headingLevel}
-              data-h2-border-right="l-tablet(1px solid gray.lighter)"
-            >
-              {getLocalizedName(govContractorType?.label, intl) ??
-                intl.formatMessage(commonMessages.notAvailable)}
-            </ContentSection>
-          </div>
-          {govContractorType?.value === GovContractorType.FirmOrAgency && (
-            <>
-              <Separator space="sm" />
-              <ContentSection
-                title={experienceFormLabels.contractorFirmAgencyName}
-                headingLevel={headingLevel}
-                data-h2-border-right="l-tablet(1px solid gray.lighter)"
-              >
-                {contractorFirmAgencyName ??
-                  intl.formatMessage(commonMessages.notAvailable)}
-              </ContentSection>
-            </>
-          )}
-        </>;
-      } else {
-        return null;
-      }
-      break;
+      return <GovContent experience={experience} headingLevel={headingLevel} />;
     case EmploymentCategory.CanadianArmedForces:
-      return (
-        <>
-          <ContentSection
-            title={experienceFormLabels.team}
-            headingLevel={headingLevel}
-            data-h2-border-right="l-tablet(1px solid gray.lighter)"
-          >
-            {division ?? intl.formatMessage(commonMessages.notAvailable)}
-          </ContentSection>
-          <Separator space="sm" />
-          <ContentSection
-            title={experienceFormLabels.cafEmploymentType}
-            headingLevel={headingLevel}
-            data-h2-border-right="l-tablet(1px solid gray.lighter)"
-          >
-            {getLocalizedName(cafEmploymentType?.label, intl) ??
-              intl.formatMessage(commonMessages.notAvailable)}
-          </ContentSection>
-          <Separator space="sm" />
-          <ContentSection
-            title={experienceFormLabels.cafRank}
-            headingLevel={headingLevel}
-            data-h2-border-right="l-tablet(1px solid gray.lighter)"
-          >
-            {getLocalizedName(cafRank?.label, intl) ??
-              intl.formatMessage(commonMessages.notAvailable)}
-          </ContentSection>
-        </>
-      );
+      return <CafContent experience={experience} headingLevel={headingLevel} />;
     default:
       return (
         <ContentSection
---
+++ apps/web/src/components/ExperienceCard/WorkContent/CafContent.tsx
@@ -0,0 +1,50 @@
+import { useIntl } from "react-intl";
+
+import { commonMessages, getLocalizedName } from "@gc-digital-talent/i18n";
+import { WorkExperience } from "@gc-digital-talent/graphql";
+import { Separator } from "@gc-digital-talent/ui";
+
+import { getExperienceFormLabels } from "~/utils/experienceUtils";
+
+import ContentSection from "../ContentSection";
+import { ContentProps } from "../types";
+
+const CafContent = ({
+  experience: { division, cafEmploymentType, cafRank },
+  headingLevel,
+}: ContentProps<Omit<WorkExperience, "user">>) => {
+  const intl = useIntl();
+  const experienceFormLabels = getExperienceFormLabels(intl);
+
+  return (
+    <>
+      <ContentSection
+        title={experienceFormLabels.team}
+        headingLevel={headingLevel}
+        data-h2-border-right="l-tablet(1px solid gray.lighter)"
+      >
+        {division ?? intl.formatMessage(commonMessages.notAvailable)}
+      </ContentSection>
+      <Separator space="sm" />
+      <ContentSection
+        title={experienceFormLabels.cafEmploymentType}
+        headingLevel={headingLevel}
+        data-h2-border-right="l-tablet(1px solid gray.lighter)"
+      >
+        {getLocalizedName(cafEmploymentType?.label, intl) ??
+          intl.formatMessage(commonMessages.notAvailable)}
+      </ContentSection>
+      <Separator space="sm" />
+      <ContentSection
+        title={experienceFormLabels.cafRank}
+        headingLevel={headingLevel}
+        data-h2-border-right="l-tablet(1px solid gray.lighter)"
+      >
+        {getLocalizedName(cafRank?.label, intl) ??
+          intl.formatMessage(commonMessages.notAvailable)}
+      </ContentSection>
+    </>
+  );
+};
+
+export default CafContent;
