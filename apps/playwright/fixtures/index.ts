8b8fb9e926c260cfb83d978d4ab922c63a777380
Matt Nigh <matt@mattnigh.com>
2024-12-06 14:19:54 -0500

    Revert import

--- apps/playwright/fixtures/index.ts
+++ apps/playwright/fixtures/index.ts
@@ -1,5 +1,5 @@
 import { test as base } from "@playwright/test";
-import { AxeBuilder } from "@axe-core/playwright";
+import AxeBuilder from "@axe-core/playwright";
 
 import AppPage from "./AppPage";
 
