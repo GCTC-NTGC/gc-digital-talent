8f685c677e04bf0443ad51d0029a3d520bed3561
Eric Sizer <eric.sizer@gmail.com>
2024-12-12 14:09:59 -0500

    add custom cast for localized string

---
+++ api/app/Casts/LocalizedString.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Casts;
+
+use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\App;
+
+class LocalizedString implements CastsAttributes
+{
+    /**
+     * Cast the given value.
+     *
+     * @param  array<string, mixed>  $attributes
+     */
+    public function get(Model $model, string $key, mixed $value, array $attributes): mixed
+    {
+        if (is_null($value) || $value === '') {
+            return null;
+        }
+
+        $decodedValue = json_decode($value, true);
+
+        return [
+            ...$decodedValue,
+            'localized' => $decodedValue[App::getLocale()] ?? null,
+        ];
+    }
+
+    /**
+     * Prepare the given value for storage.
+     *
+     * @param  array<string, mixed>  $attributes
+     */
+    public function set(Model $model, string $key, mixed $value, array $attributes): mixed
+    {
+        return json_encode($value);
+    }
+}
