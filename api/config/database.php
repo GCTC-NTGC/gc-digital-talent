f9264a94a0a9c968fd067b6db93334f3cd2eed27
Peter Giles <8978655+petertgiles@users.noreply.github.com>
2024-12-17 15:12:13 -0500

    try requiring sslmode

--- api/config/database.php
+++ api/config/database.php
@@ -75,7 +75,7 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
             'search_path' => 'public',
-            'sslmode' => 'disable',
+            'sslmode' => 'require',
         ],
 
         'sqlsrv' => [
