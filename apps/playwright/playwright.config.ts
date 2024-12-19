76c883a8ce3dc6871af9c53a6130f1141d279b15
Eric Sizer <eric.sizer@gmail.com>
2024-12-19 11:50:36 -0500

    ignore lint warning

--- apps/playwright/playwright.config.ts
+++ apps/playwright/playwright.config.ts
@@ -3,6 +3,7 @@ import path from "path";
 import { defineConfig, devices } from "@playwright/test";
 import dotenv from "dotenv";
 
+// eslint-disable-next-line import/no-named-as-default-member
 dotenv.config({ path: path.resolve(__dirname, ".env") });
 
 /**
