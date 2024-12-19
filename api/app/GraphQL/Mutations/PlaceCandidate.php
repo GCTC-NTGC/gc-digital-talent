c7763074018cac3c3469c2f0755669d2f51f6cc7
Vachan <40485260+vd1992@users.noreply.github.com>
2024-12-19 12:45:20 -0700

    [Feature] Opt into search placed candidates backend (#12330)
    * create artisan command
    
    * update placement mutations
    
    * update qualifiedEquivalent
    
    * test mutations
    
    * search testing
    
    * fix test
    
    * switch to Eloquent
    
    * add comment
    
    * tests feedback
    
    * phpstan

--- api/app/GraphQL/Mutations/PlaceCandidate.php
+++ api/app/GraphQL/Mutations/PlaceCandidate.php
@@ -2,6 +2,7 @@
 
 namespace App\GraphQL\Mutations;
 
+use App\Enums\PlacementType;
 use App\Models\PoolCandidate;
 use Carbon\Carbon;
 
@@ -24,6 +25,13 @@ final class PlaceCandidate
         $finalDecision = $candidate->computeFinalDecision();
         $candidate->computed_final_decision = $finalDecision['decision'];
         $candidate->computed_final_decision_weight = $finalDecision['weight'];
+
+        // If setting to term or indeterminate automatically suspend the candidate, otherwise null the field
+        if ($placementType === PlacementType::PLACED_TERM->name || $placementType === PlacementType::PLACED_INDETERMINATE->name) {
+            $candidate->suspended_at = $now;
+        } else {
+            $candidate->suspended_at = null;
+        }
 
         $candidate->save();
 
