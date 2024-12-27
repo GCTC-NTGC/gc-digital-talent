76d02e96b8913ad57510fccd43ef32ba48ae72b8
Matt <mnigh@users.noreply.github.com>
2024-12-27 16:20:34 -0500

    [Feature] Update French workstream name for `SECURITY` (#12376)
    * Update PoolStream case for SECURITY in French
    
    * Add migration
    
    * Update PHP formatting

--- api/lang/fr/pool_stream.php
+++ api/lang/fr/pool_stream.php
@@ -8,7 +8,7 @@ return [
     'infrastructure_operations' => 'Opérations d’infrastructure de la TI',
     'planning_and_reporting' => 'Planification et établissement de rapports en matière de TI',
     'project_portfolio_management' => 'Gestion de portefeuilles de projets de la TI',
-    'security' => 'Sécurité de la TI',
+    'security' => 'Sécurité',
     'software_solutions' => 'Solutions logicielles de la TI',
     'information_data_functions' => 'Fonctions d’information et de données',
     'executive_group' => 'Groupe de la direction',
