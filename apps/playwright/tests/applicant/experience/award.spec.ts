b5a5b2300a725a1be1d0ba30a07cb80604d06bb6
Eric Sizer <eric.sizer@gmail.com>
2024-12-30 12:09:04 -0500

    split up experience tests

---
+++ apps/playwright/tests/applicant/experience/award.spec.ts
@@ -0,0 +1,21 @@
+import { test, expect } from "~/fixtures";
+import ExperiencePage from "~/fixtures/ExperiencePage";
+import { loginBySub } from "~/utils/auth";
+
+test.describe("Award experience", () => {
+  const uniqueTestId = Date.now().valueOf();
+  test("Can create award experience", async ({ appPage }) => {
+    const title = `Test add award experience (${uniqueTestId})`;
+    const experiencePage = new ExperiencePage(appPage.page);
+    await loginBySub(experiencePage.page, "applicant@test.com");
+
+    await experiencePage.addAwardExperience({
+      title,
+      awardedDate: "2001-01",
+    });
+
+    await expect(experiencePage.page.getByRole("alert")).toContainText(
+      /successfully added experience/i,
+    );
+  });
+});
