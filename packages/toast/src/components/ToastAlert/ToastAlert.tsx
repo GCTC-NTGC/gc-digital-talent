257c9627bf4ecb2996f5088556a46216b2a430fd
Matt Nigh <matt@mattnigh.com>
2025-01-07 15:17:18 -0500

    Remove role attribute role is on parent

--- packages/toast/src/components/ToastAlert/ToastAlert.tsx
+++ packages/toast/src/components/ToastAlert/ToastAlert.tsx
@@ -1,7 +1,7 @@
 import { Alert, AlertProps } from "@gc-digital-talent/ui";
 
 const ToastAlert = ({ type, children }: AlertProps) => (
-  <Alert.Root type={type} data-h2-margin="base(0)" tabIndex={0}>
+  <Alert.Root live={false} type={type} data-h2-margin="base(0)" tabIndex={0}>
     <div
       data-h2-padding-right="p-tablet(x2)"
       data-h2-line-height="base(var(--h2-base-line-height))"
