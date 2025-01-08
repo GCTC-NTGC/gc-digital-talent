46f50025c5e202b0598e8c448af8f18921372460
Matt Nigh <matt@mattnigh.com>
2025-01-08 14:04:02 -0500

    Add messages

---
+++ apps/web/src/messages/employeeProfileMessages.ts
@@ -0,0 +1,26 @@
+import { defineMessages } from "react-intl";
+
+const messages = defineMessages({
+  aboutYou: {
+    defaultMessage: "About you",
+    id: "tSvuK1",
+    description: "Label for a employee profile about you field",
+  },
+  careerGoals: {
+    defaultMessage: "Your career goals",
+    id: "NlcepR",
+    description: "Label for a employee profile career goals field",
+  },
+  learningGoals: {
+    defaultMessage: "Your learning goals",
+    id: "j5Fwi6",
+    description: "Label for a employee profile learning goals field",
+  },
+  workStyle: {
+    defaultMessage: "How you work best",
+    id: "9pYZOU",
+    description: "Label for a employee profile work style field",
+  },
+});
+
+export default messages;
