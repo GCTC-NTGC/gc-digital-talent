bac292e58c480f7698b65f1b2eb04fc91d749d38
Peter Giles <8978655+petertgiles@users.noreply.github.com>
2025-01-13 15:04:18 -0500

    non-localized enum

--- api/app/Enums/DevelopmentProgramParticipationStatus.php
+++ api/app/Enums/DevelopmentProgramParticipationStatus.php
@@ -2,19 +2,10 @@
 
 namespace App\Enums;
 
-use App\Traits\HasLocalization;
-
 enum DevelopmentProgramParticipationStatus
 {
-    use HasLocalization;
-
     case NOT_INTERESTED;
     case INTERESTED;
     case ENROLLED;
     case COMPLETED;
-
-    public static function getLangFilename(): string
-    {
-        return 'development_program_participation_status';
-    }
 }
