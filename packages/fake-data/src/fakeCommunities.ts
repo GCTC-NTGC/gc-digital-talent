79b7aa2e8cb697d56a15bb2f8004a36896cada3f
Eric Sizer <eric.sizer@gmail.com>
2025-01-15 15:58:01 -0500

    add fake data generators for new types

--- packages/fake-data/src/fakeCommunities.ts
+++ packages/fake-data/src/fakeCommunities.ts
@@ -1,19 +1,35 @@
 import { faker } from "@faker-js/faker/locale/en";
 
-import { Community } from "@gc-digital-talent/graphql";
+import {
+  Community,
+  DevelopmentProgram,
+  WorkStream,
+} from "@gc-digital-talent/graphql";
 
 import toLocalizedString from "./fakeLocalizedString";
+import fakeWorkStreams from "./fakeWorkStreams";
+import fakeDevelopmentPrograms from "./fakeDevelopmentPrograms";
 
-const generateCommunity = (): Community => {
+const generateCommunity = (
+  developmentPrograms: DevelopmentProgram[],
+  workStreams: WorkStream[],
+): Community => {
   return {
     id: faker.string.uuid(),
     key: faker.helpers.slugify(faker.lorem.word()),
     name: toLocalizedString(faker.company.name()),
     description: toLocalizedString(faker.lorem.paragraph()),
+    developmentPrograms:
+      faker.helpers.arrayElements<DevelopmentProgram>(developmentPrograms),
+    workStreams: faker.helpers.arrayElements<WorkStream>(workStreams),
   };
 };
 
 export default (numToGenerate = 10): Community[] => {
   faker.seed(0); // repeatable results
-  return Array.from({ length: numToGenerate }, () => generateCommunity());
+  const developmentPrograms = fakeDevelopmentPrograms();
+  const workStreams = fakeWorkStreams();
+  return Array.from({ length: numToGenerate }, () =>
+    generateCommunity(developmentPrograms, workStreams),
+  );
 };
