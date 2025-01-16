4eb905992b6171e25a01e2c0f66cbdf2121aacea
vd1992 <40485260+vd1992@users.noreply.github.com>
2025-01-16 14:25:05 -0700

    tweaks

--- packages/ui/src/components/Sidebar/Sidebar.tsx
+++ packages/ui/src/components/Sidebar/Sidebar.tsx
@@ -9,13 +9,15 @@ const Sidebar = ({ children, ...rest }: SidebarProps) => (
     <div
       data-h2-height="base(100%)"
       data-h2-position="base(relative)"
+      data-h2-margin-top="p-tablet(-x3)"
       data-h2-margin-bottom="base(x1)"
     >
       <div
         data-h2-position="base(sticky)"
         data-h2-location="base(x5, auto, auto, auto)"
-        data-h2-height="p-tablet(85vh)"
+        data-h2-height="p-tablet(80vh)"
         data-h2-overflow="p-tablet(auto)"
+        data-h2-padding-top="p-tablet(x3)"
         data-h2-padding-right="p-tablet(x.5)"
       >
         {children}
