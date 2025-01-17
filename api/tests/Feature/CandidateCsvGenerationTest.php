a092383144fe41eec3221d79f6fa82ebbfdac6fa
Matt Nigh <matt@mattnigh.com>
2025-01-17 15:02:50 -0500

    Refactor tests

--- api/tests/Feature/CandidateCsvGenerationTest.php
+++ api/tests/Feature/CandidateCsvGenerationTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 use App\Enums\PoolCandidateStatus;
 use App\Jobs\GenerateUserFile;
+use App\Models\Community;
 use App\Models\Pool;
 use App\Models\PoolCandidate;
 use App\Models\Team;
@@ -23,80 +24,66 @@ class CandidateCsvGenerationTest extends TestCase
     use RefreshesSchemaCache;
     use UsesProtectedGraphqlEndpoint;
 
+    protected $community;
+
+    protected $pool;
+
+    protected $adminUser;
+
+    protected $candidate;
+
     protected function setUp(): void
     {
         parent::setUp();
 
         $this->seed([RolePermissionSeeder::class]);
 
+        $this->community = Community::factory()->create();
+
+        $this->pool = Pool::factory()
+            ->published()
+            ->create([
+                'community_id' => $this->community->id,
+            ]);
+
+        $this->adminUser = User::factory()
+        ->asApplicant()
+        ->asCommunityRecruiter($this->community->id)
+        ->asAdmin()
+        ->create();
+
+        $this->candidate = PoolCandidate::factory()->create([
+            'pool_id' => $this->pool->id,
+            'pool_candidate_status' => PoolCandidateStatus::NEW_APPLICATION->name,
+        ]);
+
+
     }
 
     public function testCandidateCsvGenerated()
     {
-
         Queue::fake();
 
-        $team = Team::factory()->create([
-            'name' => 'candidate-csv-team',
-        ]);
-
-        $pool = Pool::factory()->published()->create([
-            'team_id' => $team->id,
-        ]);
-
-        $candidate = PoolCandidate::factory()->create([
-            'pool_id' => $pool->id,
-            'pool_candidate_status' => PoolCandidateStatus::NEW_APPLICATION->name,
-        ]);
-
-        $adminUser = User::factory()
-            ->asApplicant()
-            ->asProcessOperator('candidate-csv-team')
-            ->asRequestResponder()
-            ->asAdmin()
-            ->create();
-
-        $this->actingAs($adminUser, 'api')
+        $this->actingAs($this->adminUser, 'api')
             ->graphQL(/** GraphQL */ '
                 mutation DownloadPoolCandidatesCsv($ids: [UUID!]!) {
                     downloadPoolCandidatesCsv(ids: $ids)
                 }
-            ', ['ids' => [$candidate->id]]);
+            ', ['ids' => [$this->candidate->id]]);
 
         Queue::assertPushed(GenerateUserFile::class);
     }
 
     public function testCandidateCsvGeneratedWithROD()
     {
-
         Queue::fake();
 
-        $team = Team::factory()->create([
-            'name' => 'candidate-csv-team',
-        ]);
-
-        $pool = Pool::factory()->published()->create([
-            'team_id' => $team->id,
-        ]);
-
-        $candidate = PoolCandidate::factory()->create([
-            'pool_id' => $pool->id,
-            'pool_candidate_status' => PoolCandidateStatus::NEW_APPLICATION->name,
-        ]);
-
-        $adminUser = User::factory()
-            ->asApplicant()
-            ->asProcessOperator('candidate-csv-team')
-            ->asRequestResponder()
-            ->asAdmin()
-            ->create();
-
-        $this->actingAs($adminUser, 'api')
+        $this->actingAs($this->adminUser, 'api')
             ->graphQL(/** GraphQL */ '
                 mutation DownloadPoolCandidatesCsv($ids: [UUID!]!, $withROD: Boolean) {
                     downloadPoolCandidatesCsv(ids: $ids, withROD: $withROD)
                 }
-            ', ['ids' => [$candidate->id], 'withROD' => true]);
+            ', ['ids' => [$this->candidate->id], 'withROD' => true]);
 
         Queue::assertPushed(GenerateUserFile::class);
     }
