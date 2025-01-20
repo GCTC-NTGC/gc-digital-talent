c1734bec25f69f1cd070d547bfb1d6c3ea3cd9d7
Matt Nigh <matt@mattnigh.com>
2025-01-20 12:21:28 -0500

    Remove testPlaceCandidateMutationSuspensionLogic

--- api/tests/Feature/PoolCandidateUpdateTest.php
+++ api/tests/Feature/PoolCandidateUpdateTest.php
@@ -505,59 +505,6 @@ class PoolCandidateUpdateTest extends TestCase
         assertSame($response['placedDepartment']['id'], $department->id);
     }
 
-    // placing candidates sets suspended_at automatically or keeps/makes it null appropriately
-    public function testPlaceCandidateMutationSuspensionLogic(): void
-    {
-        $department = Department::factory()->create();
-        $this->poolCandidate->pool_candidate_status = PoolCandidateStatus::QUALIFIED_AVAILABLE->name;
-        $this->poolCandidate->placed_at = null;
-        $this->poolCandidate->save();
-        $placedDoNotSuspend = [
-            PlacementType::PLACED_TENTATIVE->name,
-            PlacementType::PLACED_CASUAL->name,
-        ];
-        $placedDoSuspend = [
-            PlacementType::PLACED_TERM->name,
-            PlacementType::PLACED_INDETERMINATE->name,
-        ];
-
-        foreach ($placedDoNotSuspend as $placementType) {
-            $response = $this->actingAs($this->communityRecruiterUser, 'api')
-                ->graphQL(
-                    $this->placeCandidateMutation,
-                    [
-                        'id' => $this->poolCandidate->id,
-                        'placeCandidate' => [
-                            'placementType' => $placementType,
-                            'departmentId' => $department->id,
-                        ],
-                    ]
-                )->json('data.placeCandidate');
-
-            assertSame($response['status']['value'], $placementType);
-            assertNotNull($response['placedAt']);
-            assertNull($response['suspendedAt']);
-        }
-
-        foreach ($placedDoSuspend as $placementType) {
-            $response = $this->actingAs($this->communityRecruiterUser, 'api')
-                ->graphQL(
-                    $this->placeCandidateMutation,
-                    [
-                        'id' => $this->poolCandidate->id,
-                        'placeCandidate' => [
-                            'placementType' => $placementType,
-                            'departmentId' => $department->id,
-                        ],
-                    ]
-                )->json('data.placeCandidate');
-
-            assertSame($response['status']['value'], $placementType);
-            assertNotNull($response['placedAt']);
-            assertNotNull($response['suspendedAt']);
-        }
-    }
-
     public function testRevertPlaceCandidateMutation(): void
     {
         $department = Department::factory()->create();
