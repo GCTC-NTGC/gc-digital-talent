4bedfbec24ea0407708634409e1e4b7f2d3e168f
vd1992 <40485260+vd1992@users.noreply.github.com>
2025-01-20 11:43:39 -0700

    add command

---
+++ api/app/Console/Commands/UnsuspendPlacedCandidates.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Enums\PoolCandidateStatus;
+use App\Models\PoolCandidate;
+use Illuminate\Console\Command;
+use Illuminate\Database\Eloquent\Collection;
+
+class UnsuspendPlacedCandidates extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:unsuspend-placed-candidates';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Set suspended at date to null for placed term and indeterminate candidates';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $applicableStatuses = [PoolCandidateStatus::PLACED_TERM->name, PoolCandidateStatus::PLACED_INDETERMINATE->name];
+
+        PoolCandidate::whereIn('pool_candidate_status', $applicableStatuses)
+            ->whereNotNull('suspended_at')
+            ->with('user')
+            ->chunkById(100, function (Collection $candidates) {
+                foreach ($candidates as $candidate) {
+                    /** @var \App\Models\PoolCandidate $candidate */
+                    $candidate->suspended_at = null;
+                    $candidate->save();
+                }
+            }
+            );
+
+        return Command::SUCCESS;
+    }
+}
