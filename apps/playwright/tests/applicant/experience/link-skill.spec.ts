9e13c02eda78d9f81d20e6d46e4d83fcbd0d0e2c
Eric Sizer <eric.sizer@gmail.com>
2025-01-24 08:28:09 -0500

    update linking skill test to use entire workflow

--- apps/playwright/tests/applicant/experience/link-skill.spec.ts
+++ apps/playwright/tests/applicant/experience/link-skill.spec.ts
@@ -2,9 +2,22 @@ import { test, expect } from "~/fixtures";
 import ExperiencePage from "~/fixtures/ExperiencePage";
 import { loginBySub } from "~/utils/auth";
 
+const uniqueTestId = Date.now().valueOf();
+
 test("Can link skill to experience", async ({ appPage }) => {
+  const role = `Test add goc term or indeterminate work experience (${uniqueTestId})`;
   const experiencePage = new ExperiencePage(appPage.page);
   await loginBySub(experiencePage.page, "applicant@test.com");
+
+  // Ensure the other fields are filled out first
+  await experiencePage.addGovTermOrIndeterminateWorkExperience(
+    {
+      role,
+      startDate: "2001-01",
+      endDate: "2023-01",
+    },
+    false,
+  );
 
   const skill = "Courage";
 
@@ -15,5 +28,12 @@ test("Can link skill to experience", async ({ appPage }) => {
 
   await expect(experiencePage.page.getByRole("alert")).toContainText(
     skill + " selected.",
+  );
+
+  await experiencePage.save();
+  await experiencePage.waitForGraphqlResponse("CreateWorkExperience");
+
+  await expect(experiencePage.page.getByRole("alert")).toContainText(
+    /successfully added experience/i,
   );
 });
