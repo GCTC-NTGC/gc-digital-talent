c1ec04b6a8d9864e7951eafd1e1caa9fd120814f
Matt <mnigh@users.noreply.github.com>
2025-01-30 10:41:57 -0500

    Remove export for CLASSIFICATION_GROUP (#12638)

--- apps/web/src/types/classificationGroup.ts
+++ apps/web/src/types/classificationGroup.ts
@@ -1,11 +1,4 @@
-export const CLASSIFICATION_GROUP = [
-  "AS",
-  "CR",
-  "EC",
-  "EX",
-  "IT",
-  "PM",
-] as const; // List of classification groups that we expect need to be handled.
+const CLASSIFICATION_GROUP = ["AS", "CR", "EC", "EX", "IT", "PM"] as const; // List of classification groups that we expect need to be handled.
 
 export type ClassificationGroup = (typeof CLASSIFICATION_GROUP)[number];
 
