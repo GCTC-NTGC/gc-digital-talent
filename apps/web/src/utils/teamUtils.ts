f1df33ecc656073fb069e8a411cad4d5aa084946
Matt Nigh <matt@mattnigh.com>
2025-01-31 09:37:08 -0500

    Remove unreferenced groupRoleAssignmentsByUser function

--- apps/web/src/utils/teamUtils.ts
+++ apps/web/src/utils/teamUtils.ts
@@ -11,37 +11,6 @@ type TeamMember = {
   roles: Role[];
 } & UserPublicProfile;
 
-// eslint-disable-next-line import/prefer-default-export
-export const groupRoleAssignmentsByUser = (assignments: RoleAssignment[]) => {
-  let users: TeamMember[] = [];
-  const filteredAssignments = assignments.filter((assignment) => {
-    return (
-      notEmpty(assignment.user) &&
-      notEmpty(assignment.role) &&
-      assignment.role.isTeamBased
-    );
-  });
-
-  filteredAssignments.forEach((assignment) => {
-    const userIndex = users.findIndex(
-      (user) => user.id === assignment.user?.id,
-    );
-    if (userIndex >= 0 && assignment.role) {
-      users[userIndex].roles = [...users[userIndex].roles, assignment.role];
-    } else if (assignment.user && assignment.role) {
-      users = [
-        ...users,
-        {
-          ...assignment.user,
-          roles: [assignment.role],
-        },
-      ];
-    }
-  });
-
-  return users;
-};
-
 /**
  * Check to see if user contains one or more roles
  *
