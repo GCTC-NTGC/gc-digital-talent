6b302c0f9437e8175ae346eb614ded091f4f8a97
Peter Giles <8978655+petertgiles@users.noreply.github.com>
2025-02-03 12:07:34 -0500

    Update api/database/seeders/CommunityRandomSeeder.php
    Co-authored-by: Matt <mnigh@users.noreply.github.com>

--- api/database/seeders/CommunityRandomSeeder.php
+++ api/database/seeders/CommunityRandomSeeder.php
@@ -22,7 +22,7 @@ class CommunityRandomSeeder extends Seeder
                 ->count(3))
             ->create();
 
-        // The digital community has most of the (real) workstreams so lets add a few fake development programs, too
+        // The digital community has most of the (real) workStreams so let's add a few fake development programs, too.
         DevelopmentProgram::factory()->count(2)
             ->withEligibleClassifications()
             ->for(Community::where('key', 'digital')->sole())
