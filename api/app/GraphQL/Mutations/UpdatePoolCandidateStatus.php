d061155b50ebf89aee02b22eef0609e86a31196f
Eric Sizer <eric.sizer@gmail.com>
2025-02-10 13:51:39 -0500

    allow setting multiple timestamps

--- api/app/GraphQL/Mutations/UpdatePoolCandidateStatus.php
+++ api/app/GraphQL/Mutations/UpdatePoolCandidateStatus.php
@@ -17,33 +17,41 @@ final readonly class UpdatePoolCandidateStatus
         if (isset($args['pool_candidate_status']) && $args['pool_candidate_status'] !== $candidate->pool_candidate_status) {
             $status = $args['pool_candidate_status'];
 
+            $now = now();
             $values = [
                 'removed_at' => null,
                 'final_decision_at' => null,
                 'placed_at' => null,
             ];
 
-            $timestamp = match ($status) {
+            $timestamps = match ($status) {
+                PoolCandidateStatus::EXPIRED->name,
                 PoolCandidateStatus::SCREENED_OUT_ASSESSMENT->name,
                 PoolCandidateStatus::SCREENED_OUT_APPLICATION->name,
-                PoolCandidateStatus::QUALIFIED_AVAILABLE->name => 'final_decision_at',
+                PoolCandidateStatus::QUALIFIED_UNAVAILABLE->name,
+                PoolCandidateStatus::QUALIFIED_WITHDREW->name,
+                PoolCandidateStatus::QUALIFIED_AVAILABLE->name => ['final_decision_at'],
 
                 PoolCandidateStatus::SCREENED_OUT_NOT_RESPONSIVE->name,
                 PoolCandidateStatus::SCREENED_OUT_NOT_INTERESTED->name,
-                PoolCandidateStatus::QUALIFIED_UNAVAILABLE->name,
-                PoolCandidateStatus::QUALIFIED_WITHDREW->name,
-                PoolCandidateStatus::REMOVED->name => 'removed_at',
+                PoolCandidateStatus::REMOVED->name => ['removed_at'],
 
                 PoolCandidateStatus::PLACED_TENTATIVE->name,
                 PoolCandidateStatus::PLACED_CASUAL->name,
                 PoolCandidateStatus::PLACED_TERM->name,
-                PoolCandidateStatus::PLACED_INDETERMINATE->name => 'placed_at',
+                PoolCandidateStatus::PLACED_INDETERMINATE->name => ['placed_at', 'final_decision_at'],
 
                 default => null// no-op
             };
 
-            if ($timestamp) {
-                $values[$timestamp] = now();
+            if ($timestamps) {
+                foreach ($timestamps as $timestamp) {
+                    $values[$timestamp] = $now;
+                }
+            }
+
+            if ($status === PoolCandidateStatus::QUALIFIED_WITHDREW->name) {
+                $values['suspended_at'] = $now;
             }
 
             $values['pool_candidate_status'] = $status;
