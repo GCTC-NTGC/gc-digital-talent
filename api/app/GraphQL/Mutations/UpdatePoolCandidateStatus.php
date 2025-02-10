06d65f0091c5514b319a92ae0dc3c8c219722fb1
Eric Sizer <eric.sizer@gmail.com>
2025-02-10 10:33:16 -0500

    null timestamps on status update

--- api/app/GraphQL/Mutations/UpdatePoolCandidateStatus.php
+++ api/app/GraphQL/Mutations/UpdatePoolCandidateStatus.php
@@ -12,9 +12,16 @@ final readonly class UpdatePoolCandidateStatus
     public function __invoke(null $_, array $args)
     {
         $candidate = PoolCandidate::findOrFail($args['id']);
+        $values = [];
 
         if (isset($args['pool_candidate_status']) && $args['pool_candidate_status'] !== $candidate->pool_candidate_status) {
             $status = $args['pool_candidate_status'];
+
+            $values = [
+                'removed_at' => null,
+                'final_decision_at' => null,
+                'placed_at' => null,
+            ];
 
             $timestamp = match ($status) {
                 PoolCandidateStatus::SCREENED_OUT_ASSESSMENT->name,
@@ -36,17 +43,19 @@ final readonly class UpdatePoolCandidateStatus
             };
 
             if ($timestamp) {
-                $candidate->$timestamp = now();
+                $values[$timestamp] = now();
             }
 
-            $candidate->pool_candidate_status = $status;
+            $values['pool_candidate_status'] = $status;
+
         }
 
         if (isset($args['expiry_date'])) {
-            $candidate->expiry_date = $args['expiry_date'];
+            $values['expiry_date'] = $args['expiry_date'];
         }
 
-        $candidate->save();
+        $candidate->update($values);
+        $candidate->refresh();
 
         return $candidate;
     }
