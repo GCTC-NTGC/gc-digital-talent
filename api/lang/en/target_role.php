d954ac06989a178fc0614d571928f240967aa7c1
Peter Giles <8978655+petertgiles@users.noreply.github.com>
2025-02-14 14:47:48 -0500

    eloquent models

---
+++ api/lang/en/target_role.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'individual_contributor' => 'Individual contributor',
+    'manager' => 'Manager',
+    'director' => 'Director',
+    'director_general' => 'Director general',
+    'executive_director' => 'Executive director',
+    'assistant_deputy_minister' => 'Assistant deputy minister',
+    'deputy_minister' => 'Deputy minister',
+    'other' => 'Other',
+];
