00db08fc26853d7e28f82dead162065c65351d47
JamesHuf <49166751+JamesHuf@users.noreply.github.com>
2025-02-20 08:05:05 -0800

    Add way too many more government email domains to regex (#12790)

--- api/app/GraphQL/Validators/UpdateUserAsUserInputValidator.php
+++ api/app/GraphQL/Validators/UpdateUserAsUserInputValidator.php
@@ -46,7 +46,7 @@ final class UpdateUserAsUserInputValidator extends Validator
                 Rule::unique('users', 'email')->ignore($this->arg('id'), 'id'),
                 Rule::unique('users', 'work_email')->ignore($this->arg('id'), 'id'),
                 // Note: Should be kept in sync with the workEmailDomainRegex
-                'regex:/@([A-Za-z0-9-]+\.)*(gc\.ca|canada\.ca|elections\.ca|ccc\.ca|canadapost-postescanada\.ca|gg\.ca)$/i',
+                'regex:/@([A-Za-z0-9-]+\.)*(gc\.ca|canada\.ca|elections\.ca|ccc\.ca|canadapost-postescanada\.ca|gg\.ca|scics\.ca|scc-csc\.ca|ccohs\.ca|cchst\.ca|edc\.ca|invcanada\.ca|parl\.ca|telefilm\.ca|bankofcanada\.ca|banqueducanada\.ca|ncc-ccn\.ca|bank-banque-canada\.ca|cef-cce\.ca|cgc\.ca|nfb\.ca|onf\.ca|canadacouncil\.ca|conseildesarts\.ca|humanrights\.ca|droitsdelapersonne\.ca|ingeniumcanada\.org|cjc-ccm\.ca|bdc\.ca|idrc\.ca|museedelhistoire\.ca|historymuseum\.ca|cdic\.ca|sadc\.ca|scc\.ca|clc\.ca|clc-sic\.ca|cntower\.ca|latourcn\.ca)$/i',
             ],
             'sub' => [
                 'sometimes',
