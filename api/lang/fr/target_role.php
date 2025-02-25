99c0a1e66e9add9400dac9643bc6f11f0ff09930
Peter Giles <8978655+petertgiles@users.noreply.github.com>
2025-02-25 13:40:35 -0500

    [Feature] Next Role and Career Objective API (#12780)
    * database migration
    
    * eloquent models
    
    * factory
    
    * generated schema update
    
    * query schema
    
    * makefile shortcut
    
    * input validation
    
    * update tests
    
    * translations

---
+++ api/lang/fr/target_role.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Support\Facades\Lang;
+
+return [
+    'individual_contributor' => 'Contributeur individuel',
+    'manager' => 'Gestionnaire',
+    'director' => 'Directrice ou directeur',
+    'director_general' => 'Directrice générale ou directeur général',
+    'executive_director' => 'Directrice exécutive ou directeur exécutif',
+    'assistant_deputy_minister' => 'Sous-ministre adjointe ou adjoint',
+    'deputy_minister' => 'Sous-ministre',
+    'other' => Lang::get('common.other', [], 'fr'),
+];
