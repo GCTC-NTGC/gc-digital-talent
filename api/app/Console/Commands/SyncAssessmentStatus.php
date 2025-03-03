ccb97626179aa9308da77198bdaf3098be0bdb43
Matt Nigh <matt@mattnigh.com>
2025-03-03 14:22:28 -0500

    Revert "Remove SyncAssessmentStatus"
    This reverts commit 9ad9c9b49258628c46937d79bd8b1787f3dfd987.

---
+++ api/app/Console/Commands/SyncAssessmentStatus.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Events\CandidateStatusChanged;
+use App\Models\PoolCandidate;
+use Illuminate\Console\Command;
+use Illuminate\Database\Eloquent\Collection;
+
+class SyncAssessmentStatus extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:sync-assessment-status';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Syncs pool candidate assessment statuses with new logic';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        PoolCandidate::chunk(100, function (Collection $candidates) {
+            foreach ($candidates as $candidate) {
+                $assessmentStatus = $candidate->computeAssessmentStatus();
+
+                $candidate->computed_assessment_status = $assessmentStatus;
+
+                $candidate->save();
+
+                // If assessment status changes, "final decision" may as well.
+                CandidateStatusChanged::dispatch($candidate);
+            }
+        });
+    }
+}
