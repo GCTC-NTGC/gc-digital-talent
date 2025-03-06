de9c39d41a93e9a060cd518986dfe8aa4e6df015
Peter Giles <8978655+petertgiles@users.noreply.github.com>
2025-03-06 15:12:56 -0500

    maybe finished create input validator

--- api/app/GraphQL/Validators/CreateTalentNominationInputValidator.php
+++ api/app/GraphQL/Validators/CreateTalentNominationInputValidator.php
@@ -2,12 +2,11 @@
 
 namespace App\GraphQL\Validators;
 
+use App\Enums\TalentNominationLateralMovementOption;
 use App\Enums\TalentNominationSubmitterRelationshipToNominator;
 use App\Enums\TalentNominationUserReview;
-use App\Models\DevelopmentProgram;
-use App\Models\WorkStream;
+use App\Models\SkillFamily;
 use Database\Helpers\ApiErrorEnums;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Validation\Rule;
 use Nuwave\Lighthouse\Validation\Validator;
 
@@ -20,14 +19,12 @@ final class CreateTalentNominationInputValidator extends Validator
      */
     public function rules(): array
     {
-        // this is injected into the mutation by a schema directive but does not show up in the $args array
-        $submitterId = Auth::user()->id;
-        // $communityId = $this->arg('community.connect');
-        // $workStreams = $communityId ? WorkStream::where('community_id', $communityId)->get('id')->pluck('id') : [];
-        // $developmentProgramIds = $communityId ? DevelopmentProgram::where('community_id', $communityId)->get('id')->pluck('id') : [];
-
         return [
-            'talentNominationEvent.connect' => ['uuid', 'required', 'exists:talent_nomination_events,id'],
+            'talentNominationEvent.connect' => [
+                'uuid',
+                'required',
+                'exists:talent_nomination_events,id',
+            ],
             'nominator' => ['required_array_keys:connect'],
             'nominator.connect' => [
                 'uuid',
@@ -35,27 +32,11 @@ final class CreateTalentNominationInputValidator extends Validator
                 'prohibits:nominatorFallbackWorkEmail,nominatorFallbackName,nominatorFallbackClassification,nominatorFallbackDepartment',
             ],
             'submitterRelationshipToNominator' => [
-                'required_if:nominator.connect,'.$submitterId,
-                'prohibited_unless:nominator.connect,'.$submitterId,
-                // Rule::when(
-                //     fn (): bool => $this->arg('nominator.connect') !== $submitterId,
-                //     ['filled']
-                // ),
-                // Rule::when(
-                //     fn (): bool => $this->arg('nominator.connect') == $submitterId,
-                //     ['prohibited']
-                // ),
                 Rule::in(array_column(TalentNominationSubmitterRelationshipToNominator::cases(), 'name')),
             ],
             'submitterRelationshipToNominatorOther' => [
-                Rule::when(
-                    fn (): bool => $this->arg('submitterRelationshipToNominator') == TalentNominationSubmitterRelationshipToNominator::OTHER->name,
-                    ['filled']
-                ),
-                Rule::when(
-                    fn (): bool => $this->arg('submitterRelationshipToNominator') !== TalentNominationSubmitterRelationshipToNominator::OTHER->name,
-                    ['prohibited']
-                ),
+                'required_if:submitterRelationshipToNominator,'.TalentNominationSubmitterRelationshipToNominator::OTHER->name,
+                'prohibited_unless:submitterRelationshipToNominator,'.TalentNominationSubmitterRelationshipToNominator::OTHER->name,
                 'string',
             ],
             'nominatorFallbackClassification' => ['required_array_keys:connect'],
@@ -69,29 +50,76 @@ final class CreateTalentNominationInputValidator extends Validator
                 'exists:departments,id',
             ],
             'nominatorReview' => [
-                'required_with:nominator',
-                'prohibited_if:nominator,null',
                 Rule::in(array_column(TalentNominationUserReview::cases(), 'name')),
             ],
-
             'nominee' => ['required_array_keys:connect'],
             'nominee.connect' => [
                 'uuid',
                 'exists:users,id',
             ],
             'nomineeReview' => [
-                'required_with:nominee',
-                'prohibited_if:nominee,null',
                 Rule::in(array_column(TalentNominationUserReview::cases(), 'name')),
             ],
+            'nomineeRelationshipToNominator' => [
+                Rule::in(array_column(TalentNominationSubmitterRelationshipToNominator::cases(), 'name')),
+            ],
+            'nomineeRelationshipToNominatorOther' => [
+                'required_if:nomineeRelationshipToNominator,'.TalentNominationSubmitterRelationshipToNominator::OTHER->name,
+                'prohibited_unless:nomineeRelationshipToNominator,'.TalentNominationSubmitterRelationshipToNominator::OTHER->name,
+                'string',
+            ],
+            'nominateForAdvancement' => ['boolean'],
+            'nominateForLateralMovement' => ['boolean'],
+            'nominateForDevelopmentPrograms' => ['boolean'],
 
-            // When a nominator has been specified that is different from the current user...
+            'advancementReference' => ['required_array_keys:connect'],
+            'advancementReference.connect' => [
+                'uuid',
+                'exists:users,id',
+                'prohibits:advancementReferenceFallbackWorkEmail,advancementReferenceFallbackName,advancementReferenceFallbackClassification,advancementReferenceFallbackDepartment',
+            ],
+            'advancementReferenceReview' => [
+                Rule::in(array_column(TalentNominationUserReview::cases(), 'name')),
+            ],
+            'advancementReferenceFallbackClassification' => ['required_array_keys:connect'],
+            'advancementReferenceFallbackClassification.connect' => [
+                'uuid',
+                'exists:classifications,id',
+            ],
+            'advancementReferenceFallbackDepartment' => ['required_array_keys:connect'],
+            'advancementReferenceFallbackDepartment.connect' => [
+                'uuid',
+                'exists:departments,id',
+            ],
 
-            // 'workStreams.sync.*' => ['uuid', 'exists:work_streams,id', Rule::in($workStreams)],
-            // 'jobInterest' => ['nullable', 'boolean'],
-            // 'trainingInterest' => ['nullable', 'boolean'],
-            // 'additionalInformation' => ['nullable', 'string'],
-            // 'interestInDevelopmentPrograms.create.*.developmentProgramId' => ['uuid', Rule::in($developmentProgramIds)],
+            'lateralMovementOptions' => ['array'],
+            'lateralMovementOptions.*' => [
+                'distinct',
+                Rule::in(array_column(TalentNominationLateralMovementOption::cases(), 'name')),
+            ],
+            'lateralMovementOptionsOther' => [
+                'string',
+                Rule::requiredIf(in_array(TalentNominationLateralMovementOption::OTHER->name, $this->arg('lateralMovementOptions'))),
+                Rule::prohibitedIf(! in_array(TalentNominationLateralMovementOption::OTHER->name, $this->arg('lateralMovementOptions'))),
+            ],
+
+            'developmentPrograms' => ['required_array_keys:sync'],
+            'developmentPrograms.sync' => [
+                'array',
+                'exists:development_programs,id',
+            ],
+            'developmentProgramOptionsOther' => ['string'],
+
+            'nominationRationale' => ['string'],
+            'skills' => ['required_array_keys:sync'],
+            'skills.sync' => [
+                'array',
+                'exists:skills,id',
+                'distinct',
+                'max:3',
+            ],
+            'skills.sync.*' => [Rule::in(SkillFamily::where('key', 'klc')->sole()->skills->pluck('id')->toArray())],
+            'additionalComments' => ['string'],
         ];
     }
 
@@ -99,7 +127,16 @@ final class CreateTalentNominationInputValidator extends Validator
     {
         return [
             'talentNominationEvent.connect.exists' => ApiErrorEnums::TALENT_NOMINATION_EVENT_NOT_FOUND,
-
+            'nominator.connect.exists' => ApiErrorEnums::NOMINATOR_NOT_FOUND,
+            'nominatorFallbackClassification.connect.exists' => ApiErrorEnums::NOMINATOR_CLASSIFICATION_NOT_FOUND,
+            'nominatorFallbackDepartment.connect.exists' => ApiErrorEnums::NOMINATOR_DEPARTMENT_NOT_FOUND,
+            'nominee.connect.exists' => ApiErrorEnums::NOMINEE_NOT_FOUND,
+            'advancementReference.connect.exists' => ApiErrorEnums::REFERENCE_NOT_FOUND,
+            'advancementReferenceFallbackClassification.connect.exists' => ApiErrorEnums::REFERENCE_CLASSIFICATION_NOT_FOUND,
+            'advancementReferenceFallbackDepartment.connect.exists' => ApiErrorEnums::REFERENCE_DEPARTMENT_NOT_FOUND,
+            'developmentPrograms.sync.exists' => ApiErrorEnums::DEVELOPMENT_PROGRAM_NOT_FOUND,
+            'skills.sync.exists' => ApiErrorEnums::SKILL_NOT_FOUND,
+            'skills.sync.*.in' => ApiErrorEnums::SKILL_NOT_KLC,
         ];
     }
 }
