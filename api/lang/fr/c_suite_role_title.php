797da6ab4679fe1053f385b396cb37213eeb1d84
Matt Nigh <matt@mattnigh.com>
2025-03-11 13:56:49 -0400

    Add CSuiteRoleTitle enum

---
+++ api/lang/fr/c_suite_role_title.php
@@ -0,0 +1,20 @@
+<?php
+
+// TODO: add translation strings.
+return [
+    'chief_audit_officer' => '',
+    'chief_finance_officer' => '',
+    'chief_data_officer' => '',
+    'chief_digital_officer' => '',
+    'chief_information_officer' => '',
+    'chief_privacy_officer' => '',
+    'chief_technology_officer' => '',
+    'deputy_chief_audit_officer' => '',
+    'deputy_chief_finance_officer' => '',
+    'deputy_chief_data_officer' => '',
+    'deputy_chief_digital_officer' => '',
+    'deputy_chief_information_officer' => '',
+    'deputy_chief_privacy_officer' => '',
+    'deputy_chief_technology_officer' => '',
+    'other' => '',
+];
