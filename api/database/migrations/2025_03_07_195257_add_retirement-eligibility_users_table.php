b008e420240a76c31288666e97fd29b47db4928a
JamesHuf <jamiehufnagel@hotmail.com>
2025-03-11 11:44:32 -0700

    Add fields to table and schema

---
+++ api/database/migrations/2025_03_07_195257_add_retirement-eligibility_users_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->boolean('eligible_retirement_year_known')->nullable();
+            $table->date('eligible_retirement_year')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('eligible_retirement_year_known');
+            $table->dropColumn('eligible_retirement_year');
+        });
+    }
+};
