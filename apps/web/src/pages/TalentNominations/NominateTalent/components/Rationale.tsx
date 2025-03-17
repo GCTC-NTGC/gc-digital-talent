0c2e3b7f413d41ad38955d82799daf42eda194eb
Eric Sizer <esizer@users.noreply.github.com>
2025-03-17 12:09:49 -0400

    [Feat] Talent nomination stub pages (#12972)
    * stub out wrapper, first step template
    
    * add all page stubs
    
    * add stepper, success step
    
    * clean up stepper forms
    
    * improve mutations
    
    * Typo fixes
    
    Co-authored-by: Peter Giles <8978655+petertgiles@users.noreply.github.com>
    
    * update after typo fixes
    
    * fix instruction icon
    
    * fix more typos
    
    * update date format string
    
    * fix redirect to first unsubmitted step
    
    * fix navigation highlighting first step on success
    
    * fix stepper styles
    
    * add translations
    
    * update and consolidate translations
    
    * fix translations
    
    * update lang string snapshot
    
    * fix imports
    
    * swap submit translations
    
    * update snapshot
    
    ---------
    
    Co-authored-by: Peter Giles <8978655+petertgiles@users.noreply.github.com>

---
+++ apps/web/src/pages/TalentNominations/NominateTalent/components/Rationale.tsx
@@ -0,0 +1,71 @@
+import ChatBubbleBottomCenterTextIcon from "@heroicons/react/24/outline/ChatBubbleBottomCenterTextIcon";
+import { useIntl } from "react-intl";
+
+import {
+  FragmentType,
+  getFragment,
+  graphql,
+  TalentNominationStep,
+} from "@gc-digital-talent/graphql";
+
+import { BaseFormValues } from "../types";
+import useCurrentStep from "../useCurrentStep";
+import UpdateForm, { SubmitDataTransformer } from "./UpdateForm";
+import SubHeading from "./SubHeading";
+
+// TO DO: Populate when building form
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
+interface FormValues extends BaseFormValues {}
+
+const NominateTalentRationale_Fragment = graphql(/* GraphQL */ `
+  fragment NominateTalentRationale on TalentNomination {
+    id
+  }
+`);
+
+// TO DO: Values to be used when form is created
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const transformSubmitData: SubmitDataTransformer<FormValues> = (values) => {
+  return {};
+};
+
+interface RationaleProps {
+  rationaleQuery: FragmentType<typeof NominateTalentRationale_Fragment>;
+}
+
+const Rationale = ({ rationaleQuery }: RationaleProps) => {
+  const intl = useIntl();
+  const { current } = useCurrentStep();
+  // TO DO: Use in the form population
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const talentNomination = getFragment(
+    NominateTalentRationale_Fragment,
+    rationaleQuery,
+  );
+
+  if (current !== TalentNominationStep.Rationale) {
+    return null;
+  }
+
+  return (
+    <UpdateForm<FormValues> submitDataTransformer={transformSubmitData}>
+      <SubHeading level="h2" Icon={ChatBubbleBottomCenterTextIcon}>
+        {intl.formatMessage({
+          defaultMessage: "Rationale and additional comments",
+          id: "LA0AM1",
+          description: "Heading for rationale step of a talent nomination",
+        })}
+      </SubHeading>
+      <p data-h2-margin="base(x1 0)">
+        {intl.formatMessage({
+          defaultMessage:
+            "The final step in the nomination process is to explain why this candidate is being nominated. If you've nominated an executive or equivalent level employee, please also provide the top 3 key leadership competencies demonstrated by the nominee.",
+          id: "43Tiyp",
+          description: "Subtitle for nomination rationale step",
+        })}
+      </p>
+    </UpdateForm>
+  );
+};
+
+export default Rationale;
---
+++ apps/web/src/pages/TalentNominations/NominateTalent/components/ReviewAndSubmit.tsx
@@ -0,0 +1,73 @@
+import DocumentMagnifyingGlassIcon from "@heroicons/react/24/outline/DocumentMagnifyingGlassIcon";
+import { useIntl } from "react-intl";
+import { FormProvider, useForm } from "react-hook-form";
+
+import {
+  FragmentType,
+  getFragment,
+  graphql,
+  TalentNominationStep,
+} from "@gc-digital-talent/graphql";
+
+import pageTitles from "~/messages/pageTitles";
+
+import useCurrentStep from "../useCurrentStep";
+import { BaseFormValues } from "../types";
+import Actions from "./Actions";
+import SubHeading from "./SubHeading";
+import useMutations from "../useMutations";
+
+const NominateTalentReviewAndSubmit_Fragment = graphql(/* GraphQL */ `
+  fragment NominateTalentReviewAndSubmit on TalentNomination {
+    id
+  }
+`);
+
+interface ReviewAndSubmitProps {
+  reviewAndSubmitQuery: FragmentType<
+    typeof NominateTalentReviewAndSubmit_Fragment
+  >;
+}
+
+const ReviewAndSubmit = ({ reviewAndSubmitQuery }: ReviewAndSubmitProps) => {
+  const intl = useIntl();
+  const { current } = useCurrentStep();
+  const [fetching, { submit }] = useMutations();
+  // TO DO: Use in the form population
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const talentNomination = getFragment(
+    NominateTalentReviewAndSubmit_Fragment,
+    reviewAndSubmitQuery,
+  );
+
+  const methods = useForm<BaseFormValues>({ disabled: fetching });
+
+  if (current !== TalentNominationStep.ReviewAndSubmit) {
+    return null;
+  }
+
+  const handleSubmit = async (values: BaseFormValues) => {
+    await submit(values.intent);
+  };
+
+  return (
+    <FormProvider {...methods}>
+      <form onSubmit={methods.handleSubmit(handleSubmit)}>
+        <SubHeading level="h2" Icon={DocumentMagnifyingGlassIcon}>
+          {intl.formatMessage(pageTitles.reviewAndSubmit)}
+        </SubHeading>
+        <p data-h2-margin="base(x1 0)">
+          {intl.formatMessage({
+            defaultMessage:
+              "Please review all the information collected as a part of your nomination form before submitting. Each section contains a link to edit the associated step, should you need to make changes.",
+            id: "Lqm83x",
+            description: "Subtitle for submit step of a talent nomination",
+          })}
+        </p>
+        <Actions />
+      </form>
+    </FormProvider>
+  );
+};
+
+export default ReviewAndSubmit;
