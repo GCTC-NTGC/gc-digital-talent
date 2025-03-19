a64f000d752c3f78753a9f916d539c63747f241d
Eric Sizer <eric.sizer@gmail.com>
2025-03-19 13:01:25 -0400

    WIP: details review section

--- apps/web/src/pages/TalentNominations/NominateTalent/components/ReviewAndSubmit/NominationDetailsReview.tsx
+++ apps/web/src/pages/TalentNominations/NominateTalent/components/ReviewAndSubmit/NominationDetailsReview.tsx
@@ -6,15 +6,69 @@ import {
   graphql,
   TalentNominationStep,
 } from "@gc-digital-talent/graphql";
+import { unpackMaybes } from "@gc-digital-talent/helpers";
 
 import FieldDisplay from "~/components/FieldDisplay/FieldDisplay";
 
 import messages from "../../messages";
 import ReviewHeading from "./ReviewHeading";
 
+interface ListItem {
+  key: string;
+  name: string;
+}
+
 const NominationDetailsReview_Fragment = graphql(/* GraphQL */ `
   fragment NominationDetailsReview on TalentNomination {
-    id
+    # Nomination types
+    nominateForAdvancement
+    nominateForLateralMovement
+    nominateForDevelopmentPrograms
+
+    # Advancement details
+    advancementReference {
+      firstName
+      lastName
+      workEmail
+      department {
+        name {
+          localized
+        }
+      }
+      classification {
+        group
+        level
+      }
+    }
+    advancementReferenceFallbackName
+    advancementReferenceFallbackWorkEmail
+    advancementReferenceFallbackDepartment {
+      name {
+        localized
+      }
+    }
+    advancementReferenceFallbackClassification {
+      group
+      level
+    }
+
+    # Lateral movement details
+    lateralMovementOptionsOther
+    lateralMovementOptions {
+      value
+      label {
+        localized
+      }
+    }
+
+    # Development program details
+    developmentProgramOptionsOther
+    developmentPrograms {
+      id
+      name {
+        localized
+      }
+    }
   }
 `);
 
@@ -30,6 +84,45 @@ const NominationDetailsReview = ({
     NominationDetailsReview_Fragment,
     detailsQuery,
   );
+
+  const referenceClassification =
+    talentNomination?.advancementReference?.classification ??
+    talentNomination?.advancementReferenceFallbackClassification;
+  const referenceDepartment =
+    talentNomination?.advancementReference?.department ??
+    talentNomination?.advancementReferenceFallbackDepartment;
+
+  let lateralMoveOptions: ListItem[] = unpackMaybes(
+    talentNomination?.lateralMovementOptions,
+  ).map((option) => ({
+    key: option.value,
+    name: option.label.localized ?? "",
+  }));
+  if (talentNomination?.lateralMovementOptionsOther) {
+    lateralMoveOptions = [
+      ...lateralMoveOptions,
+      {
+        key: "laterMoveOptionsOther",
+        name: talentNomination.lateralMovementOptionsOther,
+      },
+    ];
+  }
+
+  let developmentPrograms: ListItem[] = unpackMaybes(
+    talentNomination?.developmentPrograms,
+  ).map((program) => ({
+    key: program.id,
+    name: program.name?.localized ?? "",
+  }));
+  if (talentNomination?.developmentProgramOptionsOther) {
+    developmentPrograms = [
+      ...developmentPrograms,
+      {
+        key: "developmentProgramsOther",
+        name: talentNomination.developmentProgramOptionsOther,
+      },
+    ];
+  }
 
   return (
     <>
