e1656cab03571a6223feeb35e156e63e38d77763
JamesHuf <jamiehufnagel@hotmail.com>
2025-03-19 09:56:35 -0700

    Only ask if Chief (not deputy)

--- apps/web/src/components/CommunityInterestDialog/CommunityInterestDialog.tsx
+++ apps/web/src/components/CommunityInterestDialog/CommunityInterestDialog.tsx
@@ -313,14 +313,14 @@ const CommunityInterestDialog = ({
                     {communityInterest.financeIsChief
                       ? intl.formatMessage({
                           defaultMessage:
-                            "I'm a Chief Financial Officer (CFO) or Deputy Chief Financial Officer (DCFO).",
-                          id: "p8kdz7",
+                            "I'm a Chief Financial Officer (CFO).",
+                          id: "duKO+o",
                           description: "Message when user is a finance chief",
                         })
                       : intl.formatMessage({
                           defaultMessage:
-                            "I'm not a Chief Financial Officer (CFO) or Deputy Chief Financial Officer (DCFO).",
-                          id: "1QNXzN",
+                            "I'm not a Chief Financial Officer (CFO).",
+                          id: "+/6UIs",
                           description:
                             "Message when user is not a finance chief",
                         })}
