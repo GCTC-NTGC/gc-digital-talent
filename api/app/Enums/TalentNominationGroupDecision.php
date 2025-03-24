ed7b0a79bdb19d8356f703c9c0e86496516888f0
Peter Giles <8978655+petertgiles@users.noreply.github.com>
2025-03-24 09:34:35 -0400

    add descriptions for decision enum

--- api/app/Enums/TalentNominationGroupDecision.php
+++ api/app/Enums/TalentNominationGroupDecision.php
@@ -2,9 +2,18 @@
 
 namespace App\Enums;
 
+use App\Traits\HasLocalization;
+
 // A decision that a nomination evaluator can make regarding a specific nomination option
 enum TalentNominationGroupDecision
 {
+    use HasLocalization;
+
     case APPROVED;
     case REJECTED;
+
+    public static function getLangFilename(): string
+    {
+        return 'talent_nomination_group_decision';
+    }
 }
