a6d25d27e2ca03b1ab9ca7970075bc6e99285fc2
Brinda Sasikumar <Brinda.Sasikumar@gmail.com>
2025-03-26 22:38:18 -0400

    Revert "fix enum"
    This reverts commit 1e04ffeb05c20058a8104fa2ba7a08ea4966b5b2.

--- api/app/Enums/LearningOpportunitiesInterest.php
+++ api/app/Enums/LearningOpportunitiesInterest.php
@@ -2,10 +2,19 @@
 
 namespace App\Enums;
 
+use App\Traits\HasLocalization;
+
 enum LearningOpportunitiesInterest
 {
+    use HasLocalization;
+
     case INTERCHANGE;
     case ACADEMIC_PROGRAM;
     case PEER_NETWORKING;
     case PROFESSIONAL_ACCREDITATION;
+
+    public static function getLangFilename(): string
+    {
+        return 'learning_opportunities_interest';
+    }
 }
