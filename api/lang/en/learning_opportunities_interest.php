b922c217947383848477c459a3ffaaa815d14e47
Brinda Sasikumar <Brinda.Sasikumar@gmail.com>
2025-03-26 23:07:28 -0400

    add missing file

---
+++ api/lang/en/learning_opportunities_interest.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'INTERCHANGE' => 'Interchange',
+    'ACADEMIC_PROGRAM' => 'Academic Program',
+    'PEER_NETWORKING' => 'Peer Networking',
+    'PROFESSIONAL_ACCREDITATION' => 'Professional Accreditation',
+];
---
+++ api/lang/fr/learning_opportunities_interest.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'INTERCHANGE' => 'échange',
+    'ACADEMIC_PROGRAM' => 'Programme académique',
+    'PEER_NETWORKING' => 'Réseautage entre pairs',
+    'PROFESSIONAL_ACCREDITATION' => 'Accréditation professionnelle',
+];
