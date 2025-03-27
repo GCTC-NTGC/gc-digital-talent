a41102fd47a98bfc398bc7d1f30fd8acc452e48f
vd1992 <40485260+vd1992@users.noreply.github.com>
2025-03-27 14:33:51 -0600

    split into file + aria labels

---
+++ apps/web/src/pages/TalentNominations/NominationGroup/components/ComputedIcon.tsx
@@ -0,0 +1,74 @@
+import { useIntl } from "react-intl";
+import QuestionMarkCircleIcon from "@heroicons/react/20/solid/QuestionMarkCircleIcon";
+import CheckCircleIcon from "@heroicons/react/20/solid/CheckCircleIcon";
+import XCircleIcon from "@heroicons/react/24/solid/XCircleIcon";
+import NoSymbolIcon from "@heroicons/react/24/solid/NoSymbolIcon";
+
+import { commonMessages } from "@gc-digital-talent/i18n";
+import { TalentNominationGroupDecision } from "@gc-digital-talent/graphql";
+
+interface ComputedIconProps {
+  count: number;
+  decision?: TalentNominationGroupDecision | null;
+}
+
+const ComputedIcon = ({ count, decision }: ComputedIconProps) => {
+  const intl = useIntl();
+
+  const sharedIconStyling = {
+    "data-h2-height": "base(x.70)",
+    "data-h2-width": "base(x.70)",
+    "data-h2-display": "base(inline-block)",
+    "data-h2-vertical-align": "base(text-bottom)",
+    "data-h2-margin-right": "base(x.25)",
+    "data-h2-margin-bottom": "base(x.125)",
+  };
+
+  if (count === 0) {
+    return (
+      <span>
+        <NoSymbolIcon
+          data-h2-color="base(black.lighter) base:dark(black)"
+          aria-label={intl.formatMessage(commonMessages.notApplicable)}
+          {...sharedIconStyling}
+        />
+      </span>
+    );
+  }
+
+  if (decision === TalentNominationGroupDecision.Approved) {
+    return (
+      <span>
+        <CheckCircleIcon
+          data-h2-color="base(success) base:dark(success.lighter)"
+          aria-label={intl.formatMessage(commonMessages.approved)}
+          {...sharedIconStyling}
+        />
+      </span>
+    );
+  }
+
+  if (decision === TalentNominationGroupDecision.Rejected) {
+    return (
+      <span>
+        <XCircleIcon
+          data-h2-color="base(error) base:dark(error.lighter)"
+          aria-label={intl.formatMessage(commonMessages.rejected)}
+          {...sharedIconStyling}
+        />
+      </span>
+    );
+  }
+
+  return (
+    <span>
+      <QuestionMarkCircleIcon
+        data-h2-color="base(secondary.dark) base:dark(secondary)"
+        aria-label={intl.formatMessage(commonMessages.inProgress)}
+        {...sharedIconStyling}
+      />
+    </span>
+  );
+};
+
+export default ComputedIcon;
--- apps/web/src/pages/TalentNominations/NominationGroup/components/NominatedForList.tsx
+++ apps/web/src/pages/TalentNominations/NominationGroup/components/NominatedForList.tsx
@@ -1,74 +1,9 @@
 import { useIntl } from "react-intl";
-import QuestionMarkCircleIcon from "@heroicons/react/20/solid/QuestionMarkCircleIcon";
-import CheckCircleIcon from "@heroicons/react/20/solid/CheckCircleIcon";
-import XCircleIcon from "@heroicons/react/24/solid/XCircleIcon";
-import NoSymbolIcon from "@heroicons/react/24/solid/NoSymbolIcon";
 
 import { commonMessages } from "@gc-digital-talent/i18n";
-import {
-  FragmentType,
-  getFragment,
-  graphql,
-  TalentNominationGroupDecision,
-} from "@gc-digital-talent/graphql";
+import { FragmentType, getFragment, graphql } from "@gc-digital-talent/graphql";
 
-interface ComputedIconProps {
-  count: number;
-  decision?: TalentNominationGroupDecision | null;
-}
-
-const ComputedIcon = ({ count, decision }: ComputedIconProps) => {
-  const sharedIconStyling = {
-    "data-h2-height": "base(x.70)",
-    "data-h2-width": "base(x.70)",
-    "data-h2-display": "base(inline-block)",
-    "data-h2-vertical-align": "base(text-bottom)",
-    "data-h2-margin-right": "base(x.25)",
-    "data-h2-margin-bottom": "base(x.125)",
-  };
-
-  if (count === 0) {
-    return (
-      <span>
-        <NoSymbolIcon
-          data-h2-color="base(black.lighter) base:dark(black)"
-          {...sharedIconStyling}
-        />
-      </span>
-    );
-  }
-
-  if (decision === TalentNominationGroupDecision.Approved) {
-    return (
-      <span>
-        <CheckCircleIcon
-          data-h2-color="base(success) base:dark(success.lighter)"
-          {...sharedIconStyling}
-        />
-      </span>
-    );
-  }
-
-  if (decision === TalentNominationGroupDecision.Rejected) {
-    return (
-      <span>
-        <XCircleIcon
-          data-h2-color="base(error) base:dark(error.lighter)"
-          {...sharedIconStyling}
-        />
-      </span>
-    );
-  }
-
-  return (
-    <span>
-      <QuestionMarkCircleIcon
-        data-h2-color="base(secondary.dark) base:dark(secondary)"
-        {...sharedIconStyling}
-      />
-    </span>
-  );
-};
+import ComputedIcon from "./ComputedIcon";
 
 const NominationGroupSidebarForList_Fragment = graphql(/* GraphQL */ `
   fragment NominationGroupSidebarForList on TalentNominationGroup {
