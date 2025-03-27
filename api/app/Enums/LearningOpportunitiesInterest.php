1e04ffeb05c20058a8104fa2ba7a08ea4966b5b2
Brinda Sasikumar <Brinda.Sasikumar@gmail.com>
2025-03-26 22:06:45 -0400

    fix enum

--- api/app/Enums/LearningOpportunitiesInterest.php
+++ api/app/Enums/LearningOpportunitiesInterest.php
@@ -2,19 +2,10 @@
 
 namespace App\Enums;
 
-use App\Traits\HasLocalization;
-
 enum LearningOpportunitiesInterest
 {
-    use HasLocalization;
-
     case INTERCHANGE;
     case ACADEMIC_PROGRAM;
     case PEER_NETWORKING;
     case PROFESSIONAL_ACCREDITATION;
-
-    public static function getLangFilename(): string
-    {
-        return 'learning_opportunities_interest';
-    }
 }
