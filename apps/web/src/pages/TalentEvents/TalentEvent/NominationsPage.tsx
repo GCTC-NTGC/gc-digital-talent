75d566acd25cacee6d4d548e3d09281bc469f23f
vd1992 <40485260+vd1992@users.noreply.github.com>
2025-04-01 16:16:56 -0600

    fill in page, sketch out table

--- apps/web/src/pages/TalentEvents/TalentEvent/NominationsPage.tsx
+++ apps/web/src/pages/TalentEvents/TalentEvent/NominationsPage.tsx
@@ -1,40 +1,92 @@
 import { useQuery } from "urql";
+import { ColumnDef, createColumnHelper } from "@tanstack/react-table";
+import { useMemo } from "react";
+import { useIntl } from "react-intl";
+import EnvelopeOpenIcon from "@heroicons/react/24/outline/EnvelopeOpenIcon";
 
-import { FragmentType, getFragment, graphql } from "@gc-digital-talent/graphql";
-import { Pending, ThrowNotFound } from "@gc-digital-talent/ui";
+import {
+  FragmentType,
+  getFragment,
+  graphql,
+  TalentEventNominationsTableFragment as TalentEventNominationsTableFragmentType,
+} from "@gc-digital-talent/graphql";
+import { Heading, Pending, ThrowNotFound } from "@gc-digital-talent/ui";
 import { ROLE_NAME } from "@gc-digital-talent/auth";
+import { commonMessages } from "@gc-digital-talent/i18n";
 
 import RequireAuth from "~/components/RequireAuth/RequireAuth";
 import useRequiredParams from "~/hooks/useRequiredParams";
+import Table from "~/components/Table/ResponsiveTable/ResponsiveTable";
+import messages from "~/messages/talentNominationMessages";
 
 import { RouteParams } from "./types";
 
-const TalentEventNominations_Fragment = graphql(/* GraphQL */ `
-  fragment TalentEventNominations on TalentNominationEvent {
+const TalentEventNominationsTable_Fragment = graphql(/* GraphQL */ `
+  fragment TalentEventNominationsTable on TalentNominationGroup {
     id
   }
 `);
 
+const columnHelper =
+  createColumnHelper<TalentEventNominationsTableFragmentType>();
+
 interface TalentEventNominationsProps {
-  query: FragmentType<typeof TalentEventNominations_Fragment>;
+  query: FragmentType<typeof TalentEventNominationsTable_Fragment>[];
 }
 
 const TalentEventNominations = ({ query }: TalentEventNominationsProps) => {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const talentEvent = getFragment(TalentEventNominations_Fragment, query);
+  const intl = useIntl();
 
-  return null;
+  const talentEventNominations = getFragment(
+    TalentEventNominationsTable_Fragment,
+    query,
+  );
+  const talentEventNominationsData = useMemo(
+    () => talentEventNominations,
+    [talentEventNominations],
+  );
+
+  const columns = [
+    columnHelper.accessor(({ id }) => id, {
+      id: "id",
+      header: intl.formatMessage(commonMessages.name),
+      enableColumnFilter: false,
+      meta: {
+        isRowTitle: true,
+      },
+    }),
+  ] as ColumnDef<TalentEventNominationsTableFragmentType>[];
+
+  return (
+    <Table<TalentEventNominationsTableFragmentType>
+      data={talentEventNominationsData}
+      caption={intl.formatMessage(messages.talentNominations)}
+      columns={columns}
+      sort={{
+        internal: true,
+      }}
+      pagination={{
+        internal: true,
+        total: talentEventNominations.length,
+        pageSizes: [10, 20, 50],
+      }}
+    />
+  );
 };
 
 const TalentEventNominations_Query = graphql(/* GraphQL */ `
   query TalentEventNominations($talentEventId: UUID!) {
     talentNominationEvent(id: $talentEventId) {
-      ...TalentEventNominations
+      talentNominationGroups {
+        ...TalentEventNominationsTable
+      }
     }
   }
 `);
 
 const TalentEventNominationsPage = () => {
+  const intl = useIntl();
+
   const { eventId } = useRequiredParams<RouteParams>("eventId");
   const [{ data, fetching, error }] = useQuery({
     query: TalentEventNominations_Query,
@@ -42,13 +94,32 @@ const TalentEventNominationsPage = () => {
   });
 
   return (
-    <Pending fetching={fetching} error={error}>
-      {data?.talentNominationEvent ? (
-        <TalentEventNominations query={data.talentNominationEvent} />
-      ) : (
-        <ThrowNotFound />
-      )}
-    </Pending>
+    <>
+      <Heading
+        Icon={EnvelopeOpenIcon}
+        color="primary"
+        data-h2-margin-bottom="base(x1)"
+      >
+        {intl.formatMessage(messages.talentNominations)}
+      </Heading>
+      <p data-h2-margin-bottom="base(x1.5)">
+        {intl.formatMessage({
+          defaultMessage:
+            'Manage and action nominations received from managers and executives. Nominations will arrive as "Unassigned", from which you can choose to assign them to team members to triage and assess. When approved, a nomination will automatically add the candidate to the candidates table for you to talent manage.',
+          id: "1Q6Elv",
+          description: "Talent nominations table page, table information",
+        })}
+      </p>
+      <Pending fetching={fetching} error={error}>
+        {data?.talentNominationEvent?.talentNominationGroups ? (
+          <TalentEventNominations
+            query={data.talentNominationEvent.talentNominationGroups}
+          />
+        ) : (
+          <ThrowNotFound />
+        )}
+      </Pending>
+    </>
   );
 };
 
