62fb8fb31bcbb3ede61050e2c1781df468d84f59
vd1992 <40485260+vd1992@users.noreply.github.com>
2025-04-09 10:52:13 -0600

    fill in nominator info

--- apps/web/src/pages/TalentNominations/NominationGroup/components/NominatorInfoDialog.tsx
+++ apps/web/src/pages/TalentNominations/NominationGroup/components/NominatorInfoDialog.tsx
@@ -1,22 +1,33 @@
 import { useState } from "react";
-import { useIntl } from "react-intl";
+import { IntlShape, useIntl } from "react-intl";
 
 import { Dialog, Button } from "@gc-digital-talent/ui";
-import { graphql, FragmentType, getFragment } from "@gc-digital-talent/graphql";
+import {
+  graphql,
+  FragmentType,
+  getFragment,
+  TalentNominationNomineeRelationshipToNominator,
+  NominatorInfoDialog_NominationFragment as NominatorInfoDialogNominationFragmentType,
+} from "@gc-digital-talent/graphql";
 import { commonMessages } from "@gc-digital-talent/i18n";
 
 import { getFullNameLabel } from "~/utils/nameUtils";
+import FieldDisplay from "~/components/FieldDisplay/FieldDisplay";
+import messages from "~/messages/talentNominationMessages";
+import { getClassificationName } from "~/utils/poolUtils";
 
-export const NominatorInfoDialog_NominationFragment = graphql(/* GraphQL */ `
+const NominatorInfoDialog_NominationFragment = graphql(/* GraphQL */ `
   fragment NominatorInfoDialog_Nomination on TalentNomination {
     id
     nomineeRelationshipToNominator {
+      value
       label {
         localized
       }
     }
     nomineeRelationshipToNominatorOther
     submitterRelationshipToNominator {
+      value
       label {
         localized
       }
@@ -26,8 +37,10 @@ export const NominatorInfoDialog_NominationFragment = graphql(/* GraphQL */ `
       firstName
     }
     nominator {
+      id
       firstName
       lastName
+      workEmail
       department {
         name {
           localized
@@ -39,8 +52,10 @@ export const NominatorInfoDialog_NominationFragment = graphql(/* GraphQL */ `
       }
     }
     submitter {
+      id
       firstName
       lastName
+      workEmail
       department {
         name {
           localized
@@ -53,6 +68,25 @@ export const NominatorInfoDialog_NominationFragment = graphql(/* GraphQL */ `
     }
   }
 `);
+
+const generateNominatorNomineeRelation = (
+  relationEnumObject: NominatorInfoDialogNominationFragmentType["nomineeRelationshipToNominator"],
+  otherValue: string | null | undefined,
+  intlShape: IntlShape,
+): string => {
+  if (!!relationEnumObject?.label?.localized && !!relationEnumObject.value) {
+    if (
+      relationEnumObject.value !==
+      TalentNominationNomineeRelationshipToNominator.Other
+    ) {
+      return relationEnumObject.label.localized;
+    } else if (otherValue) {
+      return otherValue;
+    }
+  }
+
+  return intlShape.formatMessage(commonMessages.notProvided);
+};
 
 interface NominatorInfoDialogProps {
   nominationQuery: FragmentType<typeof NominatorInfoDialog_NominationFragment>;
@@ -84,25 +118,75 @@ const NominatorInfoDialog = ({ nominationQuery }: NominatorInfoDialogProps) => {
       <Dialog.Content>
         <Dialog.Header
           subtitle={intl.formatMessage({
-            defaultMessage: "Abc",
-            id: "agQygy",
-            description: "Abc",
+            defaultMessage:
+              "Review the nominator's information and, when applicable, information on the employee who submitted the nomination on their behalf.",
+            id: "YvFsW9",
+            description: "Nominator information view subtitle for dialog",
           })}
         >
-          {intl.formatMessage({
-            defaultMessage: "Abc",
-            id: "agQygy",
-            description: "Abc",
-          })}
+          {intl.formatMessage(
+            {
+              defaultMessage: "{nomineeName}'s nominator",
+              id: "J6HyzP",
+              description: "Dialog header, nominee's nominator",
+            },
+            {
+              nomineeName:
+                nomination.nominee?.firstName ??
+                intl.formatMessage(commonMessages.notProvided),
+            },
+          )}
         </Dialog.Header>
         <Dialog.Body>
-          <p>
-            {intl.formatMessage({
-              defaultMessage: "Abc",
-              id: "agQygy",
-              description: "Abc",
-            })}
-          </p>
+          <div
+            data-h2-display="base(grid)"
+            data-h2-grid-template-columns="base(repeat(1, 1fr)) p-tablet(repeat(2, 1fr))"
+            data-h2-gap="base(x1)"
+            data-h2-overflow-wrap="base(anywhere)"
+          >
+            <FieldDisplay label={intl.formatMessage(messages.nominatorName)}>
+              {getFullNameLabel(
+                nomination.nominator?.firstName,
+                nomination.nominator?.lastName,
+                intl,
+              )}
+            </FieldDisplay>
+            <FieldDisplay
+              label={intl.formatMessage(messages.nominatorWorkEmail)}
+            >
+              {nomination.nominator?.workEmail ??
+                intl.formatMessage(commonMessages.notProvided)}
+            </FieldDisplay>
+            <FieldDisplay
+              label={intl.formatMessage(messages.nominatorClassification)}
+            >
+              {!!nomination.nominator?.classification?.group &&
+              !!nomination.nominator.classification.level
+                ? getClassificationName(
+                    {
+                      group: nomination.nominator.classification.group,
+                      level: nomination.nominator.classification.level,
+                    },
+                    intl,
+                  )
+                : intl.formatMessage(commonMessages.notProvided)}
+            </FieldDisplay>
+            <FieldDisplay
+              label={intl.formatMessage(messages.nominatorDepartmentAgency)}
+            >
+              {nomination.nominator?.department?.name?.localized ??
+                intl.formatMessage(commonMessages.notProvided)}
+            </FieldDisplay>
+            <FieldDisplay
+              label={intl.formatMessage(messages.nominatorRelationNominee)}
+            >
+              {generateNominatorNomineeRelation(
+                nomination.nomineeRelationshipToNominator,
+                nomination.nomineeRelationshipToNominatorOther,
+                intl,
+              )}
+            </FieldDisplay>
+          </div>
           <Dialog.Footer
             data-h2-gap="base(x1 0) p-tablet(0 x1)"
             data-h2-flex-direction="base(column) p-tablet(row)"
