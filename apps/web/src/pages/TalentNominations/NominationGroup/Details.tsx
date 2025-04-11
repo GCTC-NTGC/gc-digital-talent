06e6a77e47ddcc6ea5a7356ba51449af18e4e8d1
Peter Giles <8978655+petertgiles@users.noreply.github.com>
2025-04-11 14:14:35 -0400

    [Feature] Talent nomination group details page (#13099)
    * first commit
    
    * accordion metadata
    
    * trigger and reference fields
    
    * message refactoring
    
    * lateral movement options
    
    * development programs and rationale
    
    * skills and comments
    
    * add classification for reference
    
    * expand all
    
    * fix heading hierarchy
    
    * post merge cleanup
    
    * align naming
    
    * consolidate strings
    
    * more memory for jest
    
    * more memory for jest
    
    * increased timeout for slow tests
    
    * fix fragment key bug
    
    * more memory for jest
    
    * remove jest memory setting
    
    * smaller memory limit
    
    * one at a time
    
    * pass with no tests
    
    * fix coverage flag
    
    * remove memory limit
    
    * workerIdleMemoryLimit
    
    * workerIdleMemoryLimit as cli arg
    
    * undo jest hacking
    
    * fix skills sorting
    
    * fix dark mode icon colour
    
    * OK'd duplicate translation from Nienke
    
    * English copy edits
    
    * translations
    
    * make translations consistent

--- apps/web/src/pages/TalentNominations/NominationGroup/Details.tsx
+++ apps/web/src/pages/TalentNominations/NominationGroup/Details.tsx
@@ -1,38 +1,165 @@
 import { useQuery } from "urql";
+import { useIntl } from "react-intl";
+import ClipboardDocumentListIcon from "@heroicons/react/24/outline/ClipboardDocumentListIcon";
+import { useState } from "react";
 
 import { FragmentType, getFragment, graphql } from "@gc-digital-talent/graphql";
-import { Pending, ThrowNotFound } from "@gc-digital-talent/ui";
+import {
+  Accordion,
+  Button,
+  CardForm,
+  CardFormSeparator,
+  Heading,
+  Pending,
+  ThrowNotFound,
+} from "@gc-digital-talent/ui";
 import { ROLE_NAME } from "@gc-digital-talent/auth";
+import { unpackMaybes } from "@gc-digital-talent/helpers";
 
 import RequireAuth from "~/components/RequireAuth/RequireAuth";
 import useRequiredParams from "~/hooks/useRequiredParams";
 
 import { RouteParams } from "./types";
+import { detailTabMessages } from "./messages";
+import TalentNominationAccordionItem from "./components/TalentNominationAccordionItem";
+
+const TalentNominationGroupDetailsOptions_Fragment = graphql(/* GraphQL */ `
+  fragment TalentNominationGroupDetailsOptions on Query {
+    ...TalentNominationAccordionItemOptions
+  }
+`);
 
 const TalentNominationGroupDetails_Fragment = graphql(/* GraphQL */ `
   fragment TalentNominationGroupDetails on TalentNominationGroup {
     id
+    nominations {
+      id
+      nominator {
+        firstName
+        lastName
+      }
+      submittedAt
+      ...TalentNominationAccordionItem
+    }
   }
 `);
 
 interface TalentNominationGroupDetailsProps {
   query: FragmentType<typeof TalentNominationGroupDetails_Fragment>;
+  optionsQuery: FragmentType<
+    typeof TalentNominationGroupDetailsOptions_Fragment
+  >;
 }
 
 const TalentNominationGroupDetails = ({
   query,
+  optionsQuery,
 }: TalentNominationGroupDetailsProps) => {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const talentNominationGroup = getFragment(
     TalentNominationGroupDetails_Fragment,
     query,
   );
+  const options = getFragment(
+    TalentNominationGroupDetailsOptions_Fragment,
+    optionsQuery,
+  );
 
-  return null; // TO DO: Add page in #12852
+  // sort nominations by last name, first name, submitted date
+  const talentNominations = unpackMaybes(talentNominationGroup.nominations);
+  talentNominations.sort(
+    (a, b) =>
+      (a.nominator?.lastName?.localeCompare(b.nominator?.lastName ?? "") ??
+        0) ||
+      (a.nominator?.firstName?.localeCompare(b.nominator?.firstName ?? "") ??
+        0) ||
+      (a.submittedAt?.localeCompare(b.submittedAt ?? "") ?? 0),
+  );
+
+  // if there's only one then open it, otherwise start closed
+  const defaultExpandedNominations =
+    talentNominationGroup.nominations?.length === 1
+      ? [talentNominationGroup.nominations[0].id]
+      : [];
+
+  const intl = useIntl();
+  const [expandedNominationsValue, setExpandedNominationsValue] = useState<
+    string[]
+  >(defaultExpandedNominations);
+
+  const toggleExpandedNominationsValue = () => {
+    if (expandedNominationsValue.length > 0) {
+      setExpandedNominationsValue([]);
+    } else {
+      const keys =
+        talentNominationGroup.nominations?.map((nomination) => nomination.id) ??
+        [];
+
+      setExpandedNominationsValue(keys);
+    }
+  };
+
+  return (
+    <CardForm>
+      <div
+        data-h2-display="base(flex)"
+        data-h2-flex-direction="base(column)"
+        data-h2-gap="base(x1) l-tablet(x1.5)"
+      >
+        {/* heading section */}
+        <div
+          data-h2-display="base(flex)"
+          data-h2-align-items="base(center)"
+          data-h2-justify-content="base(space-between)"
+        >
+          <div>
+            <Heading
+              level="h2"
+              Icon={ClipboardDocumentListIcon}
+              color="primary"
+              data-h2-font-weight="base(400)"
+              data-h2-margin="base(0)"
+            >
+              {intl.formatMessage(detailTabMessages.nominationDetailsPageTitle)}
+            </Heading>
+          </div>
+          <div>
+            <Button
+              type="button"
+              mode="inline"
+              color="secondary"
+              onClick={toggleExpandedNominationsValue}
+            >
+              {intl.formatMessage(
+                expandedNominationsValue.length > 0
+                  ? detailTabMessages.collapseNominations
+                  : detailTabMessages.expandNominations,
+              )}
+            </Button>
+          </div>
+        </div>
+        <CardFormSeparator />
+        <Accordion.Root
+          mode="simple"
+          type="multiple"
+          value={expandedNominationsValue}
+          onValueChange={setExpandedNominationsValue}
+        >
+          {talentNominationGroup.nominations?.map((nomination) => (
+            <TalentNominationAccordionItem
+              key={nomination.id}
+              query={nomination}
+              optionsQuery={options}
+            />
+          ))}
+        </Accordion.Root>
+      </div>
+    </CardForm>
+  );
 };
 
 const TalentNominationGroupDetails_Query = graphql(/* GraphQL */ `
   query TalentNominationGroupDetails($talentNominationGroupId: UUID!) {
+    ...TalentNominationGroupDetailsOptions
     talentNominationGroup(id: $talentNominationGroupId) {
       ...TalentNominationGroupDetails
     }
@@ -51,7 +178,10 @@ const TalentNominationGroupDetailsPage = () => {
   return (
     <Pending fetching={fetching} error={error}>
       {data?.talentNominationGroup ? (
-        <TalentNominationGroupDetails query={data.talentNominationGroup} />
+        <TalentNominationGroupDetails
+          query={data.talentNominationGroup}
+          optionsQuery={data}
+        />
       ) : (
         <ThrowNotFound />
       )}
