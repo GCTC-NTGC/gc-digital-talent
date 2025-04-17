500c72b4d3f70ddb0ad67a4c4c43bf636fd7be02
Eric Sizer <eric.sizer@gmail.com>
2025-04-17 10:21:15 -0400

    adjust table to display fallback nomninator names

--- apps/web/src/pages/TalentNominations/NominationGroup/components/NominatorList.tsx
+++ apps/web/src/pages/TalentNominations/NominationGroup/components/NominatorList.tsx
@@ -4,6 +4,7 @@ import { FragmentType, getFragment, graphql } from "@gc-digital-talent/graphql";
 import { unpackMaybes } from "@gc-digital-talent/helpers";
 
 import { getFullNameLabel } from "~/utils/nameUtils";
+import { getSortedNominatorNames } from "~/utils/talentNominations";
 
 const NominatorList_Fragment = graphql(/** GraphQL */ `
   fragment NominatorList on TalentNomination {
@@ -23,31 +24,16 @@ interface NominatorListProps {
 const NominatorList = ({ query }: NominatorListProps) => {
   const intl = useIntl();
   const talentNominations = getFragment(NominatorList_Fragment, query);
+  const sortedNominators = getSortedNominatorNames(
+    unpackMaybes(talentNominations),
+    intl,
+  );
 
-  const nominationsSortedByNominator = unpackMaybes(talentNominations)
-    .map((nomination) => {
-      let name = nomination.nominatorFallbackName;
-      if (nomination.nominator) {
-        name = getFullNameLabel(
-          nomination.nominator.firstName,
-          nomination.nominator.lastName,
-          intl,
-        );
-      }
-
-      return {
-        id: nomination.id,
-        name,
-      };
-    })
-    .filter((nominator) => !!nominator.name)
-    .sort((a, b) => (a?.name ?? "").localeCompare(b?.name ?? ""));
-
-  return nominationsSortedByNominator.map((nominator, index) => (
+  return sortedNominators.map((nominator, index) => (
     <span key={nominator.id}>
       {nominator.name}
       {/* eslint-disable-next-line formatjs/no-literal-string-in-jsx */}
-      {index + 1 < nominationsSortedByNominator.length ? ", " : ""}
+      {index + 1 < sortedNominators.length ? ", " : ""}
     </span>
   ));
 };
