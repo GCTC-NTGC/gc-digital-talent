84b3899cfa02247a2e9b55a5e5e61cb85319948a
Yoni K <yonikid15@gmail.com>
2025-04-25 10:10:12 -0400

    Add back label for spoken interview language

--- apps/web/src/pages/Users/UserInformationPage/components/AboutSection.tsx
+++ apps/web/src/pages/Users/UserInformationPage/components/AboutSection.tsx
@@ -37,7 +37,7 @@ const AboutSection = ({ user }: BasicUserInformationProps) => {
         </div>
         <div data-h2-flex-item="base(1of1) p-tablet(1of2) desktop(1of3)">
           <p data-h2-font-weight="base(700)">
-            {}
+            {labels.prefSpokenInterviewLang}
             {intl.formatMessage(commonMessages.dividingColon)}
           </p>
           <p>
