3b45ebff59e32b4dff289d434b6d8abe5aa40bff
Matt Nigh <matt@mattnigh.com>
2025-05-01 08:24:12 -0400

    Fix lint

--- api/app/Models/Team.php
+++ api/app/Models/Team.php
@@ -3,7 +3,6 @@
 namespace App\Models;
 
 use App\Casts\LocalizedString;
-use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
@@ -57,5 +56,4 @@ class Team extends LaratrustTeam
     {
         return $this->hasMany(RoleAssignment::class);
     }
-
 }
