7f4f562c9ca275060e980029337cbce6de6b4329
Vachan <40485260+vd1992@users.noreply.github.com>
2025-05-06 09:59:37 -0600

    [Bug] Scroll sidebar on view candidate page (#12482)
    * sidebar overflow
    
    * p-tablet breakpoint
    
    * tweaks
    
    * switch breakpoint to L
    
    * Refactor styling on scrollbar
    
    * Fix lint error
    
    * Fix scrollbar for view candidate page
    
    * Only add right padding if scrollbar exists
    
    * top pad sidebar
    
    * Sidebar style changes when a scrollbar is present
    
    * tweak spacing
    
    ---------
    
    Co-authored-by: Yoni K <yonikid15@gmail.com>
    Co-authored-by: Josh Beveridge <git@joshbeveridge.ca>

--- packages/ui/src/components/Sidebar/Sidebar.tsx
+++ packages/ui/src/components/Sidebar/Sidebar.tsx
@@ -2,9 +2,10 @@ import { HTMLProps, ReactNode } from "react";
 
 export interface SidebarProps extends HTMLProps<HTMLElement> {
   children: ReactNode;
+  scrollbar?: boolean; // Temp fix for view pool candidate page
 }
 
-const Sidebar = ({ children, ...rest }: SidebarProps) => (
+const Sidebar = ({ children, scrollbar, ...rest }: SidebarProps) => (
   <aside data-h2-flex-item="base(1of1) l-tablet(1of4)" {...rest}>
     <div
       data-h2-height="base(100%)"
@@ -13,7 +14,18 @@ const Sidebar = ({ children, ...rest }: SidebarProps) => (
     >
       <div
         data-h2-position="base(sticky)"
-        data-h2-location="base(x5, auto, auto, auto)"
+        data-h2-overflow="base(auto)"
+        {...(scrollbar
+          ? {
+              "data-h2-location":
+                "base(0, auto, auto, auto) l-tablet(x3, auto, auto, auto)",
+              "data-h2-height": "l-tablet(calc(100vh - x3))",
+              "data-h2-padding-top": "base(x3)",
+            }
+          : {
+              "data-h2-location": "base(x5, auto, auto, auto)",
+              "data-h2-height": "base(auto)",
+            })}
       >
         {children}
       </div>
