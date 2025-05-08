69a25d7cb0dbfdf193c274ccb595d9f2730ad198
Chantelle <mrspasceri@hotmail.com>
2025-05-08 08:53:52 -0700

    fix header size for cards

--- apps/web/src/components/FeatureBlock/FeatureBlock.tsx
+++ apps/web/src/components/FeatureBlock/FeatureBlock.tsx
@@ -32,6 +32,11 @@ const FeatureBlock = ({ content }: FeatureBlockProps) => {
         data-h2-color="base:all(white)"
         data-h2-background-color="base:all(black.darker)"
         data-h2-padding="base(x1)"
+        data-h2-display="base(flex)"
+        data-h2-flex-direction="base(column)"
+        data-h2-justify-content="base(center)"
+        data-h2-align-items="base(center)"
+        data-h2-min-height="base(auto) p-tablet(7rem)"
       >
         <Heading
           level="h3"
