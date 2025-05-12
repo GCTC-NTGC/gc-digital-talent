7b68b7232838ea1c8b474b7e6794065aed86ea72
Eric Sizer <esizer@users.noreply.github.com>
2025-05-12 09:50:32 -0400

    [Feat] Add OCG fields to department model (#13424)
    * update api with new department fields
    
    * update admin department pages with new fields
    
    * fix department test with new fields
    
    * fix view fragment
    
    * remove empty file
    
    * make department size required
    
    * add new columns to departments table
    
    * update ID message
    
    * add translations

---
+++ api/lang/fr/department_size.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'micro' => 'Mini (jusqu\'à 250 membres du personnel)',
+    'small' => 'Petit (jusqu\'à 1 000 membres du personnel)',
+    'medium' => 'Moyen (jusqu\'à 5 000 membres du personnel)',
+    'large' => 'Grand (plus de 5 000 membres du personnel)',
+];
