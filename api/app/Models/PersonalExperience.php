040e9d73974a50bf1ae70ce235dd33d4aa488b1b
Peter Giles <8978655+petertgiles@users.noreply.github.com>
2025-05-12 15:31:51 -0400

    award experience factory with skills and experiences completes

--- api/app/Models/PersonalExperience.php
+++ api/app/Models/PersonalExperience.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
-use App\Models\Scopes\MatchExperienceType;
-use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Staudenmeir\EloquentHasManyDeep\HasManyDeep;
 
 /**
  * Class PersonalExperience
@@ -20,23 +23,19 @@ use Illuminate\Database\Eloquent\SoftDeletes;
  * @property \Illuminate\Support\Carbon $created_at
  * @property \Illuminate\Support\Carbon $updated_at
  */
-class PersonalExperience extends Experience
+class PersonalExperience extends Model
 {
     use HasFactory;
     use SoftDeletes;
 
     /**
-     * The table associated with the model.
+     * The attributes that should be cast.
      *
-     * @var string
+     * @var array
      */
-    protected $table = 'experiences';
-
-    /**
-     * Default values for attributes
-     */
-    protected $attributes = [
-        'experience_type' => PersonalExperience::class,
+    protected $casts = [
+        'start_date' => 'date',
+        'end_date' => 'date',
     ];
 
     protected static $hydrationFields = [
@@ -51,43 +50,134 @@ class PersonalExperience extends Experience
         return $this->title;
     }
 
-    /**
-     * The "booted" method of the model.
-     */
-    protected static function booted(): void
+    public function getExperienceType(): string
     {
-        static::addGlobalScope(new MatchExperienceType);
+        return PersonalExperience::class;
+    }
+
+    /** @return BelongsTo<User, $this> */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /** @return BelongsToMany<UserSkill, $this> */
+    public function userSkills(): BelongsToMany
+    {
+        return $this->belongsToMany(UserSkill::class, 'experience_skill', 'experience_id')
+            ->withTimestamps()
+            ->withPivot(['details', 'deleted_at'])
+            ->wherePivotNull('deleted_at')
+            ->as('experience_skill');
+    }
+
+    public function skills(): HasManyDeep
+    {
+        return $this->hasManyDeepFromRelations($this->userSkills(), (new UserSkill)->skill())
+            ->withPivot('experience_skill', ['created_at', 'updated_at', 'details'])
+            ->whereNull('experience_skill.deleted_at')
+            ->withTrashed(); // from the deep relation $this->userSkills->skills fetch soft deleted skills but not userSkills
+    }
+
+    /** @return HasMany<ExperienceSkill, $this> */
+    public function experienceSkills(): HasMany
+    {
+        return $this->hasMany(ExperienceSkill::class);
     }
 
     /**
-     * Interact with the experience's title
+     * Sync means we will add missing skills, remove skills not in this array, and update the details of existing skills.
+     *
+     * @param  array<string, array<string, string>>|null  $skills  - Skills must be an array of items, each of which must have an id, and optionally have a details string.
+     * @return void
      */
-    protected function title(): Attribute
+    public function syncSkills($skills)
     {
-        return $this->makeJsonPropertyStringAttribute('title');
+        if ($skills === null) {
+            return; // Just like the Eloquent sync operation, null will be ignored instead of overwriting existing values.
+        }
+
+        $skillIds = collect($skills)->pluck('id');
+        // First ensure that UserSkills exist for each of these skills
+        $this->user->addSkills($skillIds);
+
+        // Soft-delete any existing ExperienceSkills left out of this sync operation
+        ExperienceSkill::where('experience_id', $this->id)
+            ->whereHas('userSkill', function ($query) use ($skillIds) {
+                $query->whereNotIn('skill_id', $skillIds);
+            })
+            ->delete();
+
+        // Now connect the skills which ARE in this sync operation
+        $this->connectSkills($skills);
     }
 
     /**
-     * Interact with the experience's description
+     * Connect means we will add missing skills and update the details of existing skills, but not remove any skills.
+     *
+     * @param  array<string, array<string, string>>|null  $skills  - Skills must be an array of items, each of which must have an id, and optionally have a details string.
+     * @return void
      */
-    protected function description(): Attribute
+    public function connectSkills($skills)
     {
-        return $this->makeJsonPropertyStringAttribute('description');
+        if ($skills === null) {
+            return; // Just like the Eloquent sync operation, null will be ignored instead of overwriting existing values.
+        }
+
+        // First ensure that UserSkills exist for each of these skills
+        $skillIds = collect($skills)->pluck('id');
+        $this->user->addSkills($skillIds);
+
+        $userSkills = UserSkill::where('user_id', $this->user_id)->get(); // Get this users UserSkills once, to avoid repeated db calls.
+
+        // Restore soft-deleted experience-skills which need to be connected.
+        ExperienceSkill::onlyTrashed()
+            ->where('experience_id', $this->id)
+            ->whereHas('userSkill', function ($query) use ($skillIds) {
+                $query->whereIn('skill_id', $skillIds);
+            })
+            ->with('userSkill')
+            ->restore();
+
+        // Now get existing pivots (for updating details)
+        $existingExperienceSkills = ExperienceSkill::where('experience_id', $this->id)
+            ->whereHas('userSkill', function ($query) use ($skillIds) {
+                $query->whereIn('skill_id', $skillIds);
+            })
+            ->with('userSkill')
+            ->get();
+
+        // We can't use the userSkills()->sync() operation because it will hard-delete ExperienceSkills, so loop through manually.
+        foreach ($skills as $newSkill) {
+            $newSkill = collect($newSkill);
+            $existingPivot = $existingExperienceSkills->firstWhere('userSkill.skill_id', $newSkill->get('id'));
+            if ($existingPivot) { // If pivot already exists, update details
+                if ($newSkill->has('details')) { // Only update details if it was defined in the input args
+                    $existingPivot->details = $newSkill->get('details');
+                    $existingPivot->save();
+                }
+            } else { // If pivot doesn't exist yet, create it
+                $userSkillId = $userSkills->where('skill_id', $newSkill->get('id'))->first()->id;
+                $detailsArray = $newSkill->only('details')->toArray();
+                $this->userSkills()->attach($userSkillId, $detailsArray);
+            }
+        }
+        // If this experience instance continues to be used, ensure the in-memory instance is updated.
+        $this->refresh();
     }
 
-    /**
-     * Interact with the experience's start date
-     */
-    protected function startDate(): Attribute
+    public function disconnectSkills($skillIds)
     {
-        return $this->makeJsonPropertyDateAttribute('start_date');
-    }
-
-    /**
-     * Interact with the experience's end date
-     */
-    protected function endDate(): Attribute
-    {
-        return $this->makeJsonPropertyDateAttribute('end_date');
+        if ($skillIds === null) {
+            return; // Just like the Eloquent sync operation, null will be ignored instead of overwriting existing values.
+        }
+        // Find the userSkills that correspond to these skills.
+        $userSkillIds = $this->user->userSkills()->whereIn('skill_id', $skillIds)->pluck('id');
+        // Soft-delete these experience-skills
+        ExperienceSkill::where('experience_id', $this->id)
+            ->whereIn('user_skill_id', $userSkillIds)
+            ->delete();
+        // If this experience instance continues to be used, ensure the in-memory instance is updated.
+        $this->refresh();
     }
 }
