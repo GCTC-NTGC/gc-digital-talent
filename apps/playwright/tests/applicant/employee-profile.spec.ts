91bd23bdefa887f7dc5e6c37e004308bc71ae30e
JamesHuf <jamiehufnagel@hotmail.com>
2025-05-15 16:38:40 -0700

    Other forms

--- apps/playwright/tests/applicant/employee-profile.spec.ts
+++ apps/playwright/tests/applicant/employee-profile.spec.ts
@@ -84,5 +84,98 @@ test.describe("Employee Profile", () => {
     await expect(appPage.page.getByRole("alert").last()).toContainText(
       /career development preferences updated successfully/i,
     );
+
+    // Fill out the your next role form
+    await appPage.page
+      .getByRole("button", { name: /edit your next role/i })
+      .click();
+    await appPage.page
+      .getByRole("group", {
+        name: /target role/i,
+      })
+      .getByRole("radio", { name: /other/i })
+      .click();
+    await appPage.page
+      .getByRole("textbox", { name: /other role/i })
+      .fill("Ruler of the Universe");
+    await appPage.page
+      .getByRole("checkbox", {
+        name: /this is a chief or deputy chief \(C-suite\) role/i,
+      })
+      .click();
+    await appPage.page
+      .getByRole("combobox", { name: /c-suite role title/i })
+      .selectOption({ label: "Chief Data Officer" });
+    await appPage.page
+      .getByRole("combobox", { name: /desired functional community/i })
+      .selectOption({ label: "Other community" });
+    await appPage.page
+      .getByRole("textbox", { name: /other community/i })
+      .fill("Best community");
+    await appPage.page
+      .getByRole("button", { name: /save your next role/i })
+      .click();
+    await appPage.waitForGraphqlResponse("UpdateEmployeeProfileNextRole");
+    await expect(appPage.page.getByRole("alert").last()).toContainText(
+      /next role information updated successfully/i,
+    );
+
+    // Fill out the your career objective form
+    await appPage.page
+      .getByRole("button", { name: /edit your career objective/i })
+      .click();
+    await appPage.page
+      .getByRole("group", {
+        name: /target role/i,
+      })
+      .getByRole("radio", { name: /other/i })
+      .click();
+    await appPage.page
+      .getByRole("textbox", { name: /other role/i })
+      .fill("Ruler of the Universe");
+    await appPage.page
+      .getByRole("checkbox", {
+        name: /this is a chief or deputy chief \(C-suite\) role/i,
+      })
+      .click();
+    await appPage.page
+      .getByRole("combobox", { name: /c-suite role title/i })
+      .selectOption({ label: "Chief Data Officer" });
+    await appPage.page
+      .getByRole("combobox", { name: /desired functional community/i })
+      .selectOption({ label: "Other community" });
+    await appPage.page
+      .getByRole("textbox", { name: /other community/i })
+      .fill("Best community");
+    await appPage.page
+      .getByRole("button", { name: /save your career objective/i })
+      .click();
+    await appPage.waitForGraphqlResponse(
+      "UpdateEmployeeProfileCareerObjective",
+    );
+    await expect(appPage.page.getByRole("alert").last()).toContainText(
+      /career objective information updated successfully/i,
+    );
+
+    // Fill out your goals and work style form
+    await appPage.page
+      .getByRole("button", { name: /edit your goals and work style/i })
+      .click();
+    await appPage.page
+      .getByRole("textbox", { name: /about you/i })
+      .fill("I'm batman");
+    await appPage.page
+      .getByRole("textbox", { name: /your learning goals/i })
+      .fill("Everything");
+    await appPage.page
+      .getByRole("textbox", { name: /how you work best/i })
+      .fill("Alone");
+    await appPage.page
+      .getByRole("button", { name: /save goals and work style/i })
+      .click();
+    await appPage.waitForGraphqlResponse("UpdateEmployeeProfile");
+    await expect(appPage.page.getByRole("alert").last()).toContainText(
+      /goals and work style information updated successfully/i,
+    );
   });
 });
