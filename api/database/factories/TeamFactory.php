137a0f0de44ac3ab4b1c118ea4c0e6810d6567b1
Matt Nigh <matt@mattnigh.com>
2025-05-16 10:31:39 -0400

    Revert "Remove TeamFactory"
    This reverts commit 82d231bba8c5d796b10c190fc35ba8ed4d65df9d.

---
+++ api/database/factories/TeamFactory.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Department;
+use App\Models\Team;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class TeamFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Team::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->unique()->word(),
+        ];
+    }
+
+    public function configure()
+    {
+        return $this->afterCreating(function (Team $team) {
+            $departments = Department::inRandomOrder()->limit(2)->get();
+            $team->departments()->saveMany($departments);
+        });
+    }
+}
