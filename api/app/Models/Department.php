065b2af0f58e1a632ef3653ddfac720bfbd975a5
Matt Nigh <matt@mattnigh.com>
2025-05-16 09:26:20 -0400

    Remove unused imports

--- api/app/Models/Department.php
+++ api/app/Models/Department.php
@@ -6,7 +6,6 @@ use App\Casts\LocalizedString;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Staudenmeir\EloquentJsonRelations\HasJsonRelationships;
--- api/app/Models/Team.php
+++ api/app/Models/Team.php
@@ -3,7 +3,6 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
 use Laratrust\Models\Team as LaratrustTeam;
