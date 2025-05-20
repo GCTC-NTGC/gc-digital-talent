96f4b85c6c2d33646d88ae3346c745be6fb51fd1
Eric Sizer <eric.sizer@gmail.com>
2025-05-20 16:14:14 -0400

    rough update of chip

--- packages/ui/src/components/Chip/Chip.tsx
+++ packages/ui/src/components/Chip/Chip.tsx
@@ -1,5 +1,6 @@
 import XCircleIcon from "@heroicons/react/20/solid/XCircleIcon";
 import { useIntl } from "react-intl";
+import { tv, type VariantProps } from "tailwind-variants";
 import {
   DetailedHTMLProps,
   HTMLAttributes,
@@ -11,8 +12,7 @@ import {
 
 import { uiMessages } from "@gc-digital-talent/i18n";
 
-import { Color, IconType } from "../../types";
-import colorMap from "./styles";
+import { IconType } from "../../types";
 
 /**
  * List of acceptable key presses
@@ -24,31 +24,91 @@ const isDeleteEvent = (event: KeyboardEvent<HTMLSpanElement>): boolean => {
   return deleteKeys.includes(event.code);
 };
 
-export type ChipProps = DetailedHTMLProps<
-  HTMLAttributes<HTMLSpanElement>,
-  HTMLSpanElement
-> & {
-  color?: Color;
-  icon?: IconType;
+const chip = tv({
+  slots: {
+    base: "items-center inline-flex text-xs font-bold rounded-full border px-2 py-1 gap-0.75 leading-none",
+    icon: "size-3",
+  },
+  variants: {
+    color: {
+      primary:
+        "bg-primary-100 text-primary-700 dark:bg-primary-700 dark:text-primary-100 border-primary-700 dark:border-primary-100",
+      secondary:
+        "bg-secondary-100 text-secondary-700 dark:bg-secondary-700 dark:text-secondary-100 border-secondary-700 dark:border-secondary-100",
+      success:
+        "bg-success-100 text-success-700 dark:bg-success-700 dark:text-success-100 border-success-700 dark:border-success-100",
+      warning:
+        "bg-warning-100 text-warning-700 dark:bg-warning-700 dark:text-warning-100 border-warning-700 dark:border-warning-100",
+      error:
+        "bg-error-100 text-error-700 dark:bg-error-700 dark:text-error-100 border-error-700 dark:border-error-100",
+    },
+    dismissable: {
+      true: "cursor-pointer outline-none focus-visible:bg-focus focus-visible:text-black",
+    },
+  },
+  compoundVariants: [
+    {
+      dismissable: true,
+      color: "primary",
+      class: {
+        base: "hover:bg-primary-200",
+      },
+    },
+    {
+      dismissable: true,
+      color: "secondary",
+      class: {
+        base: "hover:bg-secondary-200",
+      },
+    },
+    {
+      dismissable: true,
+      color: "success",
+      class: {
+        base: "hover:bg-success-200",
+      },
+    },
+    {
+      dismissable: true,
+      color: "warning",
+      class: {
+        base: "hover:bg-warning-200",
+      },
+    },
+    {
+      dismissable: true,
+      color: "error",
+      class: {
+        base: "hover:bg-error-200",
+      },
+    },
+  ],
+});
+
+type ChipVariants = VariantProps<typeof chip>;
+
+export interface ChipProps
+  extends ChipVariants,
+    Omit<
+      DetailedHTMLProps<HTMLAttributes<HTMLSpanElement>, HTMLSpanElement>,
+      "color"
+    > {
   onDismiss?: () => void;
-};
+  icon?: IconType;
+}
 
 const Chip = ({
   color = "primary",
   onDismiss,
   icon,
   children,
+  className,
   ...rest
 }: ChipProps) => {
   const intl = useIntl();
   const Icon = icon;
+  const dismissable = !!onDismiss;
   const chipRef = useRef<HTMLSpanElement | null>(null);
-  const styles = colorMap.get(color);
-
-  const iconProps = {
-    "data-h2-width": "base(x.5)",
-    "data-h2-height": "base(x.5)",
-  };
 
   const handleClick: MouseEventHandler<HTMLSpanElement> = (event) => {
     event.stopPropagation();
@@ -73,27 +133,23 @@ const Chip = ({
     }
   };
 
+  const { base, icon: iconStyles } = chip();
+
   return (
     <span
       ref={chipRef}
-      data-h2-align-items="base(center)"
-      data-h2-gap="base(x.25)"
-      data-h2-display="base(inline-flex)"
-      data-h2-font-size="base(caption)"
-      data-h2-font-weight="base(700)"
-      data-h2-padding="base(x.05 x.5)"
-      data-h2-radius="base(m)"
       {...(onDismiss && {
-        className: "Chip Chip--dismissible",
         role: "button",
         tabIndex: 0,
         onClick: handleClick,
         onKeyDown: handleKeyDown,
         onKeyUp: handleKeyUp,
-        "data-h2-cursor": "base(pointer)",
-        "data-h2-outline": "base(none)",
       })}
-      {...styles}
+      className={base({
+        color,
+        dismissable,
+        class: className,
+      })}
       {...rest}
     >
       {onDismiss && (
@@ -102,11 +158,10 @@ const Chip = ({
           aria-hidden="false"
           // DOM order is first for readability "Remove X"
           // Move to end in visual order for design
-          data-h2-order="base(3)"
-          {...iconProps}
+          className={iconStyles({ class: "order-3" })}
         />
       )}
-      {Icon && <Icon {...iconProps} />}
+      {Icon && <Icon className={iconStyles()} />}
       <span>{children}</span>
     </span>
   );
--- packages/ui/src/components/Chip/Chips.tsx
+++ packages/ui/src/components/Chip/Chips.tsx
@@ -1,22 +1,20 @@
 import { HTMLProps, ReactElement } from "react";
+import { tv } from "tailwind-variants";
 
 import { notEmpty } from "@gc-digital-talent/helpers";
 
 import { ChipProps } from "./Chip";
 
+const chips = tv({
+  base: "flex flex-wrap gap-x-2 gap-x-1 list-none p-0",
+});
+
 type ChipsProps = Omit<HTMLProps<HTMLUListElement>, "children"> & {
   children: ReactElement<ChipProps> | (ReactElement<ChipProps> | null)[] | null;
 };
 
-const Chips = ({ children, ...rest }: ChipsProps) => (
-  <ul
-    data-h2-display="base(flex)"
-    data-h2-flex-wrap="base(wrap)"
-    data-h2-gap="base(x.25, x.125)"
-    data-h2-list-style="base(none)"
-    data-h2-padding="base(0)"
-    {...rest}
-  >
+const Chips = ({ children, className, ...rest }: ChipsProps) => (
+  <ul {...rest} className={chips({ class: className })}>
     {Array.isArray(children) ? (
       children.filter(notEmpty).map((child) => <li key={child.key}>{child}</li>)
     ) : (
