6606abab9c9ccfb5e07711ec157a0d9b255c5607
Chantelle <mrspasceri@hotmail.com>
2025-05-23 10:08:23 -0700

    removed 'remove from process' copy

--- api/lang/fr/assessment_decision.php
+++ api/lang/fr/assessment_decision.php
@@ -3,5 +3,5 @@
 return [
     'successful' => 'Démontré',
     'hold' => 'Non démontrée (en attente d’une évaluation plus poussée)',
-    'unsuccessful' => 'Non démontrée (supprimée du processus)',
+    'unsuccessful' => 'Non démontrée',
 ];
