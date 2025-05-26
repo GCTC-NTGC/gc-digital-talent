606be121e2fdd7f4a7de7217866af90dd0f01ce6
Eric Sizer <eric.sizer@gmail.com>
2025-05-26 10:26:46 -0400

    migrate alert to tailwind

--- packages/ui/src/components/Alert/utils.ts
+++ packages/ui/src/components/Alert/utils.ts
@@ -10,84 +10,11 @@ import { uiMessages } from "@gc-digital-talent/i18n";
 import { IconType } from "../../types";
 import { AlertType } from "./types";
 
-export const styleMap: Record<AlertType, Record<string, string>> = {
-  success: {
-    "data-h2-border":
-      "base(3px solid success.darker) base:dark(3px solid success.lightest)",
-  },
-  warning: {
-    "data-h2-border":
-      "base(3px solid warning.darker) base:dark(3px solid warning.lightest)",
-  },
-  info: {
-    "data-h2-border":
-      "base(3px solid secondary.darker) base:dark(3px solid secondary.lightest)",
-  },
-  error: {
-    "data-h2-border":
-      "base(3px solid error.darker) base:dark(3px solid error.lightest)",
-  },
-};
-
-export const dismissStyleMap: Record<AlertType, Record<string, string>> = {
-  success: {
-    "data-h2-background-color":
-      "base(transparent) base:hover(success.lightest) base:focus-visible(focus.light)",
-    "data-h2-color":
-      "base:(inherit) base:hover(success.darker)  base:all:focus-visible(black)",
-  },
-  warning: {
-    "data-h2-background-color":
-      "base(transparent) base:hover(warning.lightest) base:focus-visible(focus.light)",
-    "data-h2-color":
-      "base:(inherit) base:hover(warning.darker)  base:all:focus-visible(black)",
-  },
-  info: {
-    "data-h2-background-color":
-      "base(transparent) base:hover(secondary.lightest) base:focus-visible(focus.light)",
-    "data-h2-color":
-      "base:(inherit) base:hover(secondary.darker)  base:all:focus-visible(black)",
-  },
-  error: {
-    "data-h2-background-color":
-      "base(transparent) base:hover(error.lightest) base:focus-visible(focus.light)",
-    "data-h2-color":
-      "base:(inherit) base:hover(error.darker)  base:all:focus-visible(black)",
-  },
-};
-
 export const iconMap: Record<AlertType, IconType> = {
   success: CheckCircleIcon,
   info: BellAlertIcon,
   warning: ExclamationCircleIcon,
   error: ExclamationTriangleIcon,
-};
-
-export const iconStyleMap: Record<AlertType, Record<string, string>> = {
-  success: {
-    "data-h2-background-color": "base:all(success.lightest)",
-    "data-h2-color": "base:all(success.darker)",
-    "data-h2-border-color":
-      "base:selectors[::after](transparent transparent transparent rgba(var(--h2-color-success-lightest-locked), 1))",
-  },
-  warning: {
-    "data-h2-background-color": "base:all(warning.lightest)",
-    "data-h2-color": "base:all(warning.darker)",
-    "data-h2-border-color":
-      "base:selectors[::after](transparent transparent transparent rgba(var(--h2-color-warning-lightest-locked), 1))",
-  },
-  info: {
-    "data-h2-background-color": "base:all(secondary.lightest)",
-    "data-h2-color": "base:all(secondary.darker)",
-    "data-h2-border-color":
-      "base:selectors[::after](transparent transparent transparent rgba(var(--h2-color-secondary-lightest-locked), 1))",
-  },
-  error: {
-    "data-h2-background-color": "base:all(error.lightest)",
-    "data-h2-color": "base:all(error.darker)",
-    "data-h2-border-color":
-      "base:selectors[::after](transparent transparent transparent rgba(var(--h2-color-error-lightest-locked), 1))",
-  },
 };
 
 export const getAlertLevelTitle = (type: AlertType, intl: IntlShape) => {
