0ee3936bc2a519ab1cde7fd8a7b84ffcf4247a64
Matt <mnigh@users.noreply.github.com>
2025-06-02 13:18:38 -0400

    [Debt] Migrate `TextArea` to tailwind (#13682)
    * Migrate TextArea to tailwind
    
    * Deprecate useInputStyles
    
    * Add inputStyles, selectStyles
    
    Co-authored-by: Eric Sizer <esizer@users.noreply.github.com>
    
    * Remove useInputStyles
    replaced by useInputStylesDeprecated
    
    * Replace baseStyles with inputStyles
    
    * Add color for dark
    
    ---------
    
    Co-authored-by: Eric Sizer <esizer@users.noreply.github.com>

--- packages/forms/src/styles.ts
+++ packages/forms/src/styles.ts
@@ -1,3 +1,5 @@
+import { tv } from "tailwind-variants";
+
 import { FieldState } from "./types";
 
 const fieldStateStyles: Record<FieldState, Record<string, string>> = {
@@ -14,5 +16,14 @@ const fieldStateStyles: Record<FieldState, Record<string, string>> = {
     "data-h2-border-color": "base(secondary.darker) base:focus-visible(focus)",
   },
 };
+
+export const inputStyles = tv({
+  base: "rounded-md border-1 p-3 text-black outline-offset-2 outline-none placeholder:text-gray-600 placeholder:opacity-70 focus-visible:outline-2 focus-visible:outline-focus dark:text-white",
+});
+
+export const selectStyles = tv({
+  extend: inputStyles,
+  base: "bg-[url(data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke-width='1.5' stroke='rgba(86, 86, 90, 1)'><path stroke-linecap='round' stroke-linejoin='round' d='M19.5 8.25l-7.5 7.5-7.5-7.5'/></svg>)] dark:bg-[url(data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke-width='1.5' stroke='rgba(191, 191, 191, 1)'><path stroke-linecap='round' stroke-linejoin='round' d='M19.5 8.25l-7.5 7.5-7.5-7.5'/></svg>)] appearance-none bg-size-[1rem] bg-position-[var(--spacing*4.5)_calc(100%-.75rem)] bg-no-repeat p-3 pr-9",
+});
 
 export default fieldStateStyles;
