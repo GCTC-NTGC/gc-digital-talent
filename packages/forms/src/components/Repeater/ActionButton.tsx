4a04607b1d32f715ac7385229dbca0f43e5f7749
Eric Sizer <eric.sizer@gmail.com>
2025-06-03 12:53:36 -0400

    migrate repeater tailwind

--- packages/forms/src/components/Repeater/ActionButton.tsx
+++ packages/forms/src/components/Repeater/ActionButton.tsx
@@ -1,57 +1,73 @@
 import { DetailedHTMLProps, ButtonHTMLAttributes, forwardRef } from "react";
+import { tv, VariantProps } from "tailwind-variants";
 
-type Animation = "none" | "translate-up" | "translate-down";
+import { IconType } from "@gc-digital-talent/ui";
 
-type ActionButtonProps = DetailedHTMLProps<
-  ButtonHTMLAttributes<HTMLButtonElement>,
-  HTMLButtonElement
-> & {
-  animation?: Animation;
-};
+const DisabledIcon = () => (
+  // eslint-disable-next-line formatjs/no-literal-string-in-jsx
+  <span data-h2-color="base(gray)" aria-hidden data-h2-width="base(x.75)">
+    &bull;
+  </span>
+);
+
+const actionBtn = tv({
+  slots: {
+    base: "group flex cursor-pointer items-center rounded-full bg-transparent p-3 text-black dark:text-white",
+    icon: "size-4.5",
+  },
+  variants: {
+    disabled: {
+      true: {
+        base: "text-gray-500 dark:text-gray-300",
+      },
+      false: {
+        base: "hover:bg-gray-100 focus-visible:bg-black dark:hover:bg-gray-700",
+      },
+    },
+    animation: {
+      up: {
+        icon: "translate-y-0 group-hover:-translate-y-1/5 focus-visible:-translate-y-1/5",
+      },
+      down: {
+        icon: "translate-y-0 group-hover:translate-y-1/5 focus-visible:translate-y-1/5",
+      },
+    },
+  },
+  compoundVariants: [
+    {
+      disabled: false,
+      animation: ["up", "down"],
+      class: {
+        icon: "transform transition-transform duration-200 ease-linear",
+      },
+    },
+  ],
+});
+
+type ActionButtonVariants = VariantProps<typeof actionBtn>;
+
+interface ActionButtonProps
+  extends ActionButtonVariants,
+    DetailedHTMLProps<
+      ButtonHTMLAttributes<HTMLButtonElement>,
+      HTMLButtonElement
+    > {
+  icon: IconType;
+}
 
 /**
  * Generic button to apply styles to a
  * fieldset action button
  */
 const ActionButton = forwardRef<HTMLButtonElement, ActionButtonProps>(
-  ({ animation = "none", disabled, ...rest }, ref) => {
-    const animationStyles: Record<Animation, Record<string, string>> = {
-      none: {},
-      "translate-up": {
-        "data-h2-transform":
-          "base:children[svg](translateY(0%)) base:hover:children[svg](translateY(-20%)) base:focus-visible:children[svg](translateY(-20%))",
-      },
-      "translate-down": {
-        "data-h2-transform":
-          "base:children[svg](translateY(0%)) base:hover:children[svg](translateY(20%)) base:focus-visible:children[svg](translateY(20%))",
-      },
-    };
+  ({ animation, disabled, icon, ...rest }, ref) => {
+    const Icon = disabled ? DisabledIcon : icon;
+    const { base, icon: iconStyles } = actionBtn({ animation, disabled });
 
     return (
-      <button
-        ref={ref}
-        type="button"
-        data-h2-border="base(none)"
-        data-h2-radius="base(50%)"
-        data-h2-cursor="base(pointer)"
-        data-h2-display="base(flex)"
-        data-h2-align-items="base(center)"
-        data-h2-padding="base(x.5)"
-        data-h2-background-color="base(foreground) base:hover(gray.lightest) base:focus(focus)"
-        data-h2-transition="base:children[svg](transform 200ms ease)"
-        data-h2-font-weight="base(700)"
-        {...(disabled
-          ? {
-              disabled: true,
-              "data-h2-color": "base(gray.dark) base:dark(gray)",
-              "data-h2-background-color": "base(foreground)",
-            }
-          : {
-              "data-h2-color": "base(black)",
-              ...animationStyles[animation],
-            })}
-        {...rest}
-      />
+      <button ref={ref} type="button" className={base()} {...rest}>
+        <Icon className={iconStyles()} />
+      </button>
     );
   },
 );
