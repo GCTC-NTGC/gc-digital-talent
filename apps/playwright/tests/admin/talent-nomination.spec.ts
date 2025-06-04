47b3b6ab8418ac89f7d18a1ce0701bab02784c05
JamesHuf <jamiehufnagel@hotmail.com>
2025-06-04 13:23:00 -0700

    Test main steps in nomination submission

--- apps/playwright/tests/admin/talent-nomination.spec.ts
+++ apps/playwright/tests/admin/talent-nomination.spec.ts
@@ -1,9 +1,175 @@
+import { nowUTCDateTime } from "@gc-digital-talent/date-helpers";
+
 import { test, expect } from "~/fixtures";
 import TalentManagement from "~/fixtures/TalentManagement";
+import graphql from "~/utils/graphql";
+import { createUserWithRoles } from "~/utils/user";
 
 import { loginBySub } from "../../utils/auth";
 
 test.describe("Talent nomination management", () => {
+  test("Create a talent nomination", async ({ appPage }) => {
+    // Create a couple of users to test with
+    const adminCtx = await graphql.newContext();
+    const uniqueTestId = Date.now().valueOf();
+    const nominatorSub = `playwright.sub.${uniqueTestId}.nominator`;
+    const nomineeSub = `playwright.sub.${uniqueTestId}.nominee`;
+
+    await createUserWithRoles(adminCtx, {
+      user: {
+        email: `${nominatorSub}@example.org`,
+        sub: nominatorSub,
+        isGovEmployee: true,
+        workEmail: `${nominatorSub}@gc.ca`,
+        workEmailVerifiedAt: nowUTCDateTime(),
+      },
+      roles: ["guest", "base_user", "applicant"],
+    });
+    await createUserWithRoles(adminCtx, {
+      user: {
+        email: `${nomineeSub}@example.org`,
+        sub: nomineeSub,
+        isGovEmployee: true,
+        workEmail: `${nomineeSub}@gc.ca`,
+        workEmailVerifiedAt: nowUTCDateTime(),
+      },
+      roles: ["guest", "base_user", "applicant"],
+    });
+
+    // Navigate from the homepage to start a nomination
+    await loginBySub(appPage.page, "admin@test.com");
+    await appPage.page.goto("/en");
+
+    await appPage.page
+      .getByRole("link", {
+        name: /learn more about talent management/i,
+      })
+      .click();
+    await appPage.page
+      .getByRole("link", {
+        name: /nominate talent/i,
+      })
+      .click();
+    await appPage.waitForGraphqlResponse("TalentManagementEventsPage");
+    await appPage.page
+      .getByRole("link", {
+        name: /start a nomination for test talent nomination event active en 0/i,
+      })
+      .click();
+    await appPage.waitForGraphqlResponse("NominateTalent");
+    await expect(appPage.page.getByRole("alert").last()).toContainText(
+      /nomination created successfully/i,
+    );
+    await appPage.page.getByRole("button", { name: /next step/i }).click();
+
+    // Fill out the nominator form
+    await appPage.page
+      .getByRole("radio", {
+        name: /I’m submitting the nomination on the nominator’s behalf/i,
+      })
+      .click();
+    await appPage.page
+      .getByRole("group", {
+        name: /relationship to the nominator/i,
+      })
+      .getByRole("radio", { name: /other/i })
+      .click();
+    await appPage.page
+      .getByRole("textbox", { name: /other relationship/i })
+      .fill("Minion");
+    await appPage.page
+      .getByRole("textbox", { name: /Search nominator's work email/i })
+      .fill(`${nominatorSub}@gc.ca`);
+    await appPage.page
+      .getByRole("button", { name: /Search work email/i })
+      .click();
+    await appPage.waitForGraphqlResponse("EmployeeSearch");
+    await expect(
+      appPage.page.getByText(/we found a user with this email address/i),
+    ).toBeVisible();
+    await appPage.page
+      .getByRole("radio", { name: /the information provided is correct/i })
+      .click();
+    await appPage.page.getByRole("button", { name: /next step/i }).click();
+
+    // Fill out the nominee form
+    await appPage.page
+      .getByRole("textbox", { name: /Search nominee's work email/i })
+      .fill(`${nomineeSub}@gc.ca`);
+    await appPage.page
+      .getByRole("button", { name: /Search work email/i })
+      .click();
+    await appPage.waitForGraphqlResponse("EmployeeSearch");
+    await expect(
+      appPage.page.getByText(/we found a user with this email address/i),
+    ).toBeVisible();
+    await appPage.page
+      .getByRole("radio", { name: /the information provided is correct/i })
+      .click();
+    await appPage.page
+      .getByRole("group", {
+        name: /relationship to nominator/i,
+      })
+      .getByRole("radio", { name: "Other", exact: true })
+      .click();
+    await appPage.page
+      .getByRole("textbox", { name: /other relationship/i })
+      .fill("Sidekick");
+    await appPage.page.getByRole("button", { name: /next step/i }).click();
+
+    // Fill out the nomination details form
+    await appPage.page.getByRole("checkbox", { name: /advancement/i }).click();
+    await appPage.page
+      .getByRole("checkbox", { name: /lateral movement/i })
+      .click();
+    await appPage.page
+      .getByRole("checkbox", { name: /development program/i })
+      .click();
+    await appPage.page
+      .getByRole("textbox", { name: /Search reference's work email/i })
+      .fill("does_not_exist@gc.ca");
+    await appPage.page
+      .getByRole("button", { name: /Search work email/i })
+      .click();
+    await appPage.waitForGraphqlResponse("EmployeeSearch");
+    await expect(
+      appPage.page.getByText(/We couldn't find a matching profile/i),
+    ).toBeVisible();
+    await appPage.page
+      .getByRole("textbox", { name: /Reference’s name/i })
+      .fill("Peter Parker");
+    await appPage.page
+      .getByRole("textbox", { name: /Reference's work email/ })
+      .fill("not_spiderman@gc.ca");
+    await appPage.page.getByRole("combobox", { name: /group/i }).click();
+    await appPage.page.getByRole("option", { name: "IT", exact: true }).click();
+    await appPage.page
+      .getByRole("combobox", { name: /level/i })
+      .selectOption({ label: "2" });
+    await appPage.page
+      .getByRole("combobox", { name: /department or agency/i })
+      .selectOption({ label: "Senate" });
+    await appPage.page
+      .getByRole("group", {
+        name: /lateral movement options/i,
+      })
+      .getByRole("checkbox", { name: /other/i })
+      .click();
+    await appPage.page
+      .getByRole("textbox", { name: /other lateral move option/i })
+      .fill("Right");
+    await appPage.page
+      .getByRole("group", {
+        name: /development program options/i,
+      })
+      .getByRole("checkbox", { name: /other/i })
+      .click();
+    await appPage.page
+      .getByRole("textbox", { name: /other development program option/i })
+      .fill("Sidekick training");
+    await appPage.page.getByRole("button", { name: /next step/i }).click();
+  });
+
   test("Evaluate a nominee", async ({ appPage }) => {
     await loginBySub(appPage.page, "talent-coordinator@test.com");
     await appPage.page.goto("/en/community");
