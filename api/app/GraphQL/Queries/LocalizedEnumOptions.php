f7ac8794ca0f18ec1257814f8bd5ed52ac81db6c
Eric Sizer <eric.sizer@gmail.com>
2025-06-06 13:11:58 -0400

    clean up code a bit

--- api/app/GraphQL/Queries/LocalizedEnumOptions.php
+++ api/app/GraphQL/Queries/LocalizedEnumOptions.php
@@ -4,14 +4,26 @@ declare(strict_types=1);
 
 namespace App\GraphQL\Queries;
 
-use App\Traits\QueriesLocalizedEnums;
-
 final class LocalizedEnumOptions
 {
-    use QueriesLocalizedEnums;
-
-    public function __invoke($root, array $args)
+    public function __invoke($_, array $args)
     {
-        return $this->buildEnumList($args['enumName'], fn ($case) => $case->name);
+        $enumClass = 'App\\Enums\\'.$args['enumName'];
+        $typename = 'Localized'.$args['enumName'];
+
+        if (! class_exists($enumClass) || ! method_exists($enumClass, 'cases')) {
+            throw new \InvalidArgumentException("No enum found for {$args['enumName']}");
+        }
+
+        return array_map(function ($case) use ($enumClass, $typename) {
+            // Supports both pure and backed enums
+            $name = is_object($case) ? $case->name : $case; // Defensive for string name
+
+            return [
+                '__typename' => $typename,
+                'value' => $name,
+                'label' => $enumClass::localizedString($name),
+            ];
+        }, $enumClass::cases());
     }
 }
