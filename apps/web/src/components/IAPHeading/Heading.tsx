4f69e8c3efa55a1b660c8679c3fc60c4ad6493b8
Matt Nigh <matt@mattnigh.com>
2025-06-10 14:19:33 -0400

    Update compoundVariants for thin

--- apps/web/src/components/IAPHeading/Heading.tsx
+++ apps/web/src/components/IAPHeading/Heading.tsx
@@ -17,26 +17,26 @@ const heading = tv({
   },
   variants: {
     thin: {
-      true: "",
+      true: "font-thin",
     },
     color: {
       pink: "text-primary-500 dark:text-primary-100",
       white: "text-white dark:text-white",
     },
     size: {
-      h1: "text-5xl/[1.1] font-bold lg:text-6xl/[1.1]",
-      h2: "mt-12 mb-3 text-4xl/[1.1] font-bold lg:text-5xl/[1.1]",
+      h1: "text-5xl/[1.1] lg:text-6xl/[1.1]",
+      h2: "mt-12 mb-3 text-4xl/[1.1] lg:text-5xl/[1.1]",
       h3: "mt-9 mb-1.5 text-3xl/[1.1] lg:text-4xl/[1.1]",
       h4: "mt-9 mb-1.5 text-2xl/[1.1] lg:text-3xl/[1.1]",
       h5: "mt-6 mb-1.5 text-xl/[1.1] lg:text-2xl/[1.1]",
-      h6: "mt-6 mb-1.5 text-lg/[1.1] font-bold lg:text-xl/[1.1]",
+      h6: "mt-6 mb-1.5 text-lg/[1.1] lg:text-xl/[1.1]",
     },
   },
   compoundVariants: [
     {
-      thin: true,
+      thin: false,
       size: ["h1", "h2", "h6"],
-      class: "font-thin",
+      class: "font-bold",
     },
   ],
 });
