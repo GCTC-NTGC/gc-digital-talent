d48dbac335d6fa710828b0535342898f66848f6a
Eric Sizer <eric.sizer@gmail.com>
2025-06-10 08:14:41 -0400

    migrate flourish container

--- apps/web/src/components/FlourishContainer/FlourishContainer.tsx
+++ apps/web/src/components/FlourishContainer/FlourishContainer.tsx
@@ -1,6 +1,8 @@
 import { ReactNode } from "react";
+import { tv } from "tailwind-variants";
 
 import { useTheme } from "@gc-digital-talent/theme";
+import { Container } from "@gc-digital-talent/ui";
 
 import desktopGraphicsLight2 from "~/assets/img/Desktop_Graphics_light_2.webp";
 import desktopGraphicsLight3 from "~/assets/img/Desktop_Graphics_light_3.webp";
@@ -9,6 +11,37 @@ import desktopGraphicsDark3 from "~/assets/img/Desktop_Graphics_dark_3.webp";
 
 type Side = "top" | "bottom";
 type Size = "sm" | "lg";
+
+const flourishContainer = tv({
+  slots: {
+    layout: "absolute h-full w-full overflow-hidden bg-white dark:bg-gray-700",
+    topImg: "top-0 right-0",
+    bottomImg: "bottom-0 left-0",
+  },
+  variants: {
+    skew: {
+      true: {
+        layout: "-skew-y-3",
+      },
+    },
+    size: {
+      sm: {
+        topImg: "w-[60%] xs:h-1/5 xs:w-auto",
+        bottomImg: "w-[45%] xs:w-[32%] xl:h-[30%] xl:w-auto",
+      },
+      lg: {
+        topImg: "w-[150%] xs:h-1/2 xs:w-auto",
+        bottomImg: "w-[150%] xs:w-full xl:h-[90%] xl:w-auto",
+      },
+    },
+  },
+  compoundSlots: [
+    {
+      slots: ["topImg", "bottomImg"],
+      class: "max-w-initial absolute h-auto skew-3",
+    },
+  ],
+});
 
 interface FlourishContainerProps {
   children: ReactNode;
@@ -24,65 +57,32 @@ const FlourishContainer = ({
   skew = true,
 }: FlourishContainerProps) => {
   const { mode } = useTheme();
+  const { layout, topImg, bottomImg } = flourishContainer({ skew, size });
   return (
-    <div data-h2-layer="base(3, relative)">
-      <div
-        data-h2-background="base(background)"
-        data-h2-height="base(100%)"
-        data-h2-width="base(100%)"
-        data-h2-position="base(absolute)"
-        {...(skew && {
-          "data-h2-transform": "base(skewY(-3deg))",
-        })}
-        data-h2-overflow="base(hidden)"
-      >
+    <div className="relative z-[3]">
+      <div className={layout()}>
         {show.includes("top") && (
           <img
-            data-h2-position="base(absolute)"
-            data-h2-location="base(0, 0, auto, auto)"
-            data-h2-transform="base(skew(3deg))"
-            data-h2-max-width="base(initial)"
-            {...(size === "lg"
-              ? {
-                  "data-h2-height": "base(auto) p-tablet(50%)",
-                  "data-h2-width": "base(150%) p-tablet(auto)",
-                }
-              : {
-                  "data-h2-height": "base(auto) p-tablet(20%)",
-                  "data-h2-width": "base(60%) p-tablet(auto)",
-                })}
+            className={topImg()}
             src={mode === "dark" ? desktopGraphicsDark2 : desktopGraphicsLight2}
             alt=""
           />
         )}
         {show.includes("bottom") && (
           <img
-            data-h2-position="base(absolute)"
-            data-h2-location="base(auto, auto, 0, 0)"
-            data-h2-transform="base(skew(3deg))"
-            data-h2-max-width="base(initial)"
-            {...(size === "lg"
-              ? {
-                  "data-h2-height": "base(auto) desktop(90%)",
-                  "data-h2-width": "base(150%) p-tablet(100%) desktop(auto)",
-                }
-              : {
-                  "data-h2-height": "base(auto) desktop(30%)",
-                  "data-h2-width": "base(45%) p-tablet(32%) desktop(auto)",
-                })}
+            className={bottomImg()}
             src={mode === "dark" ? desktopGraphicsDark3 : desktopGraphicsLight3}
             alt=""
           />
         )}
       </div>
-      <div
-        data-h2-position="base(relative)"
-        data-h2-wrapper="base(center, large, x1) p-tablet(center, large, x2)"
+      <Container
+        className="relative px-6 py-18 xs:px-12 xs:pt-35 xs:pb-24 sm:pt-42 sm:pb-36"
+        size="lg"
+        center
       >
-        <div data-h2-padding="base(x3, 0) p-tablet(x5, 0, x4, 0) l-tablet(x7, 0, x6, 0)">
-          {children}
-        </div>
-      </div>
+        {children}
+      </Container>
     </div>
   );
 };
