dae34c146a845cc7d9bed119aa78fa17b2f49599
Eric Sizer <eric.sizer@gmail.com>
2025-06-11 08:54:32 -0400

    migrate hero to tailwind

--- apps/web/src/components/Hero/Hero.tsx
+++ apps/web/src/components/Hero/Hero.tsx
@@ -1,5 +1,6 @@
 import { ReactNode, useEffect, useRef } from "react";
 import { useIntl } from "react-intl";
+import { tv, VariantProps } from "tailwind-variants";
 
 import {
   Heading,
@@ -7,26 +8,52 @@ import {
   type BreadcrumbsProps,
   Crumb,
   NavTabs,
+  Container,
 } from "@gc-digital-talent/ui";
 import { uiMessages } from "@gc-digital-talent/i18n";
 
 import BackgroundGraphic from "./BackgroundPattern";
 import ButtonLinksArray, { ButtonLinkType } from "./ButtonLinksArray";
 
-const paddingMap = {
-  default: {
-    "data-h2-padding": "base(x2.5 0 x2 0)",
+const hero = tv({
+  slots: {
+    wrapper:
+      "relative overflow-hidden bg-black pt-15 iap:bg-linear-90 iap:from-primary iap:to-primary-600",
+    content: "relative z-[3] text-center text-white",
+    breadcrumbs: "flex flex-wrap justify-center gap-3 p-0 [&_li]:inline-block",
+    tabs: "mt-12 flex",
   },
-  image: {
-    "data-h2-padding": "base(x2.5 0 0 0) p-tablet(x2.5 0 x2 0)",
+  variants: {
+    centered: {
+      true: {
+        tabs: "justify-center",
+      },
+      false: {
+        content: "xs:text-left",
+        breadcrumbs: "xs:justify-start",
+      },
+    },
+    mode: {
+      default: {
+        wrapper: "pb-12",
+      },
+      image: {
+        wrapper: "pb-0 xs:pb-12",
+      },
+      overlap: {
+        wrapper: "pb-36",
+      },
+      navTabs: {
+        wrapper: "",
+      },
+    },
   },
-  overlap: {
-    "data-h2-padding": "base(x2.5, 0, x6, 0)",
+  defaultVariants: {
+    mode: "default",
   },
-  navTabs: {
-    "data-h2-padding": "base(x2.5 0 0 0)",
-  },
-} as const;
+});
+
+type HeroVariants = VariantProps<typeof hero>;
 
 interface NavTab {
   url: string;
@@ -77,22 +104,6 @@ const Hero = (props: HeroWithNavTabsProps | HeroWithOverlapProps) => {
   const headingRef = useRef<HeadingRef>(null);
   const showImg = imgPath && !children;
   const applyOverlap = overlap && !navTabs;
-  const textAlignment = centered
-    ? {
-        "data-h2-text-align": "base(center)",
-      }
-    : {
-        "data-h2-text-align": "base(center) p-tablet(left)",
-      };
-  let padding: (typeof paddingMap)[keyof typeof paddingMap] =
-    paddingMap.default;
-  if (showImg) {
-    padding = paddingMap.image;
-  } else if (applyOverlap) {
-    padding = paddingMap.overlap;
-  } else if (navTabs) {
-    padding = paddingMap.navTabs;
-  }
 
   useEffect(() => {
     if (headingRef.current) {
@@ -100,37 +111,33 @@ const Hero = (props: HeroWithNavTabsProps | HeroWithOverlapProps) => {
     }
   }, []);
 
+  let mode: HeroVariants["mode"] = "default";
+  if (showImg) {
+    mode = "image";
+  } else if (applyOverlap) {
+    mode = "overlap";
+  } else if (navTabs) {
+    mode = "navTabs";
+  }
+
+  const { wrapper, content, breadcrumbs, tabs } = hero({ mode });
+
   return (
     <>
-      <div
-        data-h2-background="base(rgba(0, 0, 0, 1)) base:iap(linear-gradient(90deg, primary, rgb(104, 23, 53)))"
-        data-h2-overflow="base(hidden)"
-        data-h2-position="base(relative)"
-        {...padding}
-      >
-        <div data-h2-wrapper="base(center, large, x1) p-tablet(center, large, x2)">
-          <div
-            data-h2-color="base:all(white)"
-            data-h2-position="base(relative)"
-            data-h2-z-index="base(3)"
-            {...textAlignment}
-          >
+      <div className={wrapper()}>
+        <Container size="lg">
+          <div className={content()}>
             <Heading
               ref={headingRef}
               tabIndex={-1}
-              data-h2-outline="base(none)"
               level="h1"
               size="h2"
-              data-h2-margin="base(0)"
-              data-h2-margin-top="base(x2) p-tablet(0)"
+              className="mt-12 outline-none xs:m-0"
             >
               {title}
             </Heading>
             {subtitle && (
-              <p
-                data-h2-font-size="base(h5, 1.4)"
-                data-h2-margin="base(x0.25, 0, x1.25, 0)"
-              >
+              <p className="mt-1.5 mb-7.5 text-xl/[1.4] lg:text-2xl/[1.4]">
                 {subtitle}
               </p>
             )}
@@ -143,23 +150,9 @@ const Hero = (props: HeroWithNavTabsProps | HeroWithOverlapProps) => {
             {crumbs && (
               <nav
                 aria-label={intl.formatMessage(uiMessages.breadcrumbs)}
-                data-h2-margin-top="base(x1.25)"
+                className="mt-7.5"
               >
-                <ol
-                  data-h2-list-style="base(none)"
-                  data-h2-display="base(flex) base:children[>li](inline-block)"
-                  data-h2-flex-wrap="base(wrap)"
-                  data-h2-gap="base(x.5)"
-                  data-h2-padding="base(0)"
-                  {...(centered
-                    ? {
-                        "data-h2-justify-content": "base(center)",
-                      }
-                    : {
-                        "data-h2-justify-content":
-                          "base(center) p-tablet(flex-start)",
-                      })}
-                >
+                <ol className={breadcrumbs()}>
                   {crumbs.map((crumb, index) => (
                     <Crumb
                       key={crumb.url}
@@ -175,40 +168,19 @@ const Hero = (props: HeroWithNavTabsProps | HeroWithOverlapProps) => {
           </div>
           {showImg ? (
             <div
-              data-h2-position="p-tablet(absolute)"
-              data-h2-top="p-tablet(x1)"
-              data-h2-right="base(0)"
-              data-h2-height="base(50vh) p-tablet(100%)"
-              data-h2-width="base(100%)"
-              data-h2-background-position="base(50% 110%) p-tablet(140% 100%) l-tablet(115% 100%) laptop(110% 100%) desktop(105% 100%)"
-              data-h2-background-size="base(auto 50vh) p-tablet(auto 60vh) p-tablet(auto 110%)"
-              data-h2-background-repeat="base(no-repeat)"
+              className="right-0 h-[50vh] w-full bg-size-[auto_50vh] bg-position-[50%_110%] bg-no-repeat xs:absolute xs:top-6 xs:h-full xs:bg-size-[auto_110%] xs:bg-position-[140%_100%] sm:bg-position-[115%_100%] md:bg-position-[110%_100%] lg:bg-position-[105%_100%]"
               style={{ backgroundImage: `url('${imgPath}')` }}
             />
           ) : (
             <BackgroundGraphic
               aria-hidden="true"
-              data-h2-display="base(block) base:iap(none)"
-              data-h2-position="base(absolute)"
-              data-h2-location="base(0, 0, auto, auto)"
-              data-h2-height="base(auto)"
-              data-h2-min-width="base(x20)"
-              data-h2-width="base(75%)"
-              data-h2-z-index="base(0)"
+              className="absolute top-0 right-0 z-0 block h-auto w-3/4 min-w-120 iap:hidden"
             />
           )}
           {navTabs ? (
-            <div
-              data-h2-margin-top="base(x2)"
-              data-h2-display="base(flex)"
-              {...(centered
-                ? {
-                    "data-h2-justify-content": "base(center)",
-                  }
-                : {})}
-            >
+            <div className={tabs()}>
               <NavTabs.Root>
-                <NavTabs.List data-h2-wrapper="base(center, full, 0)">
+                <NavTabs.List className="mx-auto w-full p-0">
                   {navTabs.map((navTab) => (
                     <NavTabs.Item key={navTab.url}>
                       <NavTabs.Link href={navTab.url}>
@@ -220,36 +192,24 @@ const Hero = (props: HeroWithNavTabsProps | HeroWithOverlapProps) => {
               </NavTabs.Root>
             </div>
           ) : null}
-        </div>
-        <div
-          data-h2-position="base(absolute)"
-          data-h2-top="base(x1) p-tablet(x1.5)"
-          data-h2-right="base(x1) p-tablet(x5)"
-        >
-          {status}
-        </div>
+        </Container>
+        {status && (
+          <div className="absolute top-6 right-6 xs:top-9 xs:right-30">
+            {status}
+          </div>
+        )}
         {additionalContent ? (
           <>
-            <div
-              data-h2-wrapper="base(center, large, x1) p-tablet(center, large, x2)"
-              data-h2-position="base(relative)"
-              data-h2-z-index="base(3)"
-            >
+            <Container size="lg" className="z-[3]">
               {additionalContent}
-            </div>
+            </Container>
           </>
         ) : null}
       </div>
       {children ? (
-        <>
-          <div
-            data-h2-wrapper="base(center, large, x1) p-tablet(center, large, x2)"
-            data-h2-position="base(relative)"
-            data-h2-margin="base(-x5, auto, 0, auto)"
-          >
-            {children}
-          </div>
-        </>
+        <Container size="lg" className="relative z-[3] mx-auto -mt-30 mb-0">
+          {children}
+        </Container>
       ) : null}
     </>
   );
