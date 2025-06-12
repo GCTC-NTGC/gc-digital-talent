7102a4a048d1d8f89a623b4734550982ddfbbdf4
Eric Sizer <eric.sizer@gmail.com>
2025-06-12 15:50:25 -0400

    allow class name for flourish

--- packages/ui/src/components/Flourish/Flourish.tsx
+++ packages/ui/src/components/Flourish/Flourish.tsx
@@ -1,5 +1,11 @@
-const Flourish = () => (
-  <div className="block h-6 bg-linear-(--gradient-main-linear)" />
+import { tv } from "tailwind-variants";
+
+const flourish = tv({
+  base: "block h-6 bg-linear-(--gradient-main-linear)",
+});
+
+const Flourish = ({ className }: { className: string }) => (
+  <div className={flourish({ class: className })} />
 );
 
 export default Flourish;
