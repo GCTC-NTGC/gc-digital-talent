c835ad34be8db23734f8538ce0b4c16322090649
Brinda Sasikumar <Brinda.Sasikumar@gmail.com>
2025-06-13 10:47:37 -0400

    adding this back as we need to return as work experience like isWorkExperience method

--- apps/web/src/utils/experienceUtils.tsx
+++ apps/web/src/utils/experienceUtils.tsx
@@ -528,7 +528,9 @@ export const isPersonalExperience = (
 export const isWorkExperience = (
   e: SimpleAnyExperience,
 ): e is Omit<WorkExperience, "user"> => e.__typename === "WorkExperience";
-export const isGovWorkExperience = (e: SimpleAnyExperience): boolean =>
+export const isGovWorkExperience = (
+  e: SimpleAnyExperience,
+): e is Omit<WorkExperience, "user"> =>
   isWorkExperience(e) &&
   e.employmentCategory?.value === EmploymentCategory.GovernmentOfCanada;
 
