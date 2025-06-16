3559d99684c3ef7b0a047889164ab495c476503d
Eric Sizer <eric.sizer@gmail.com>
2025-06-16 12:38:37 -0400

    fix reset button on dark mode

--- apps/web/src/components/Table/ResetButton.tsx
+++ apps/web/src/components/Table/ResetButton.tsx
@@ -6,7 +6,7 @@ const ResetButton = (props: Omit<HTMLProps<HTMLButtonElement>, "children">) => {
   const intl = useIntl();
   return (
     <button
-      className="flex shrink-0 cursor-pointer items-center rounded bg-transparent p-1.5 text-gray outline-none hover:bg-gray-100 hover:text-black focus-visible:bg-focus focus-visible:text-black dark:bg-gray-700 dark:hover:text-white"
+      className="flex shrink-0 cursor-pointer items-center rounded bg-transparent p-1.5 text-gray outline-none hover:bg-gray-100 hover:text-black focus-visible:bg-focus focus-visible:text-black dark:hover:bg-gray-700 dark:hover:text-white"
       aria-label={intl.formatMessage({
         defaultMessage: "Reset search",
         id: "Yh/4po",
