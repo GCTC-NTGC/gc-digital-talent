c0c5c90f15ed9cfc396ff25a72bea193de254976
Yoni K <yonikid15@gmail.com>
2025-06-19 17:56:53 -0400

    Fix menu styling again

--- packages/ui/src/components/NavMenu/NavMenu.tsx
+++ packages/ui/src/components/NavMenu/NavMenu.tsx
@@ -144,7 +144,7 @@ const useActiveLink = (
 };
 
 const navMenuLink = tv({
-  base: "font-normal text-black hover:text-primary-600 focus-visible:text-black data-active:font-bold data-active:text-primary-600 data-active:hover:text-primary-700 dark:text-white dark:hover:text-primary-100 dark:data-active:text-primary-200 dark:data-active:hover:text-primary-700 data-active:[&_span]:no-underline",
+  base: "font-normal text-black hover:text-primary-600 focus-visible:text-black data-active:font-bold data-active:text-primary-600 data-active:hover:text-primary-100 hover:data-[icon=true]:text-primary-700 dark:text-white dark:hover:text-primary-100 dark:data-active:text-primary-200 dark:data-active:hover:text-primary-200 dark:hover:data-[icon=true]:text-primary-700 data-active:[&_span]:no-underline",
   variants: {
     isSmallScreen: {
       true: "",
@@ -160,7 +160,7 @@ const navMenuLink = tv({
       isSmallScreen: false,
       type: "link",
       class:
-        "text-white hover:text-primary-200 data-active:text-primary-200 hover:data-[icon=true]:text-primary-700 dark:data-active:text-primary-200 dark:hover:data-[icon=true]:text-primary-200",
+        "text-white hover:text-primary-200 data-active:text-primary-200 hover:data-[icon=true]:text-primary-200 dark:data-active:text-primary-100 dark:hover:data-[icon=true]:text-primary-700",
     },
   ],
 });
@@ -205,6 +205,7 @@ const IconLink = forwardRef<
           isSmallScreen,
           type,
         })}
+        data-icon
       />
     </NavigationMenuPrimitive.Link>
   );
