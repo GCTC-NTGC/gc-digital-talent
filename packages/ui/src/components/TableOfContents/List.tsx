2e4e1bc12192f2ab0a100a9bd48e08be389e6f22
Eric Sizer <eric.sizer@gmail.com>
2025-06-26 14:33:04 -0400

    remove comment to remove important

--- packages/ui/src/components/TableOfContents/List.tsx
+++ packages/ui/src/components/TableOfContents/List.tsx
@@ -9,8 +9,6 @@ export const ListItem = ({ children, ...rest }: ListItemProps) => (
   </li>
 );
 
-// TO DO: Create a base `List` component to avoid needing important (#13624)
-// This is because we use `ul` and apply styles in plain css
 const list = tv({
   base: "list-inside list-disc pl-6",
   variants: {
