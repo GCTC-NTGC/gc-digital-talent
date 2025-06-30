4a12bb82d8b567e42ab6d2a8d4ba35585769989d
Eric Sizer <eric.sizer@gmail.com>
2025-06-30 12:40:04 -0400

    migrate IAP homepage tailwind

--- apps/web/src/pages/Home/IAPHomePage/components/CTAButtons.tsx
+++ apps/web/src/pages/Home/IAPHomePage/components/CTAButtons.tsx
@@ -7,21 +7,15 @@ interface CTAButtonsProps {
 }
 
 const CTAButtons = ({ latestPoolId }: CTAButtonsProps) => (
-  <div data-h2-display="p-tablet(flex)">
-    <div
-      data-h2-width="p-tablet(50%)"
-      data-h2-margin="base(0, 0, x1, 0) p-tablet(0, x.5, x1, 0)"
-    >
+  <div className="mb-6 grid gap-6 xs:grid-cols-2">
+    <div>
       {latestPoolId ? (
         <ApplyLink id={latestPoolId} />
       ) : (
         <ApplyDialog btnProps={{ block: true }} />
       )}
     </div>
-    <div
-      data-h2-width="p-tablet(50%)"
-      data-h2-margin="base(0, 0, x1, 0) p-tablet(0, 0, x1, x.5)"
-    >
+    <div>
       <LearnDialog btnProps={{ block: true }} />
     </div>
   </div>
