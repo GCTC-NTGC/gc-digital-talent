e95adb3a23d02f1e0f6001b23f3d528e5724ce34
Eric Sizer <eric.sizer@gmail.com>
2025-07-02 15:39:48 -0400

    fix linting

--- apps/web/src/components/SpinnerIcon/SpinnerIcon.tsx
+++ apps/web/src/components/SpinnerIcon/SpinnerIcon.tsx
@@ -1,7 +1,9 @@
-import { ComponentPropsWithoutRef } from "react";
+import { forwardRef } from "react";
 import { m, useReducedMotion } from "motion/react";
 import ArrowPathIcon from "@heroicons/react/20/solid/ArrowPathIcon";
 import { tv } from "tailwind-variants";
+
+import { type IconProps } from "@gc-digital-talent/ui";
 
 const icon = tv({
   base: "mr-0 inline-flex",
@@ -9,29 +11,28 @@ const icon = tv({
 
 const Icon = m.create(ArrowPathIcon);
 
-const SpinnerIcon = ({
-  className,
-  ...rest
-}: ComponentPropsWithoutRef<typeof Icon>) => {
-  const shouldReduceMotion = useReducedMotion();
+const SpinnerIcon = forwardRef<SVGSVGElement, IconProps>(
+  ({ className }, forwardedRef) => {
+    const shouldReduceMotion = useReducedMotion();
 
-  return (
-    <Icon
-      {...(!shouldReduceMotion && {
-        animate: {
-          rotate: [0, 360],
-        },
-        transition: {
-          ease: "linear",
-          duration: 1,
-          repeat: Infinity,
-          repeatDelay: 0,
-        },
-      })}
-      className={icon({ class: className })}
-      {...rest}
-    />
-  );
-};
+    return (
+      <Icon
+        ref={forwardedRef}
+        {...(!shouldReduceMotion && {
+          animate: {
+            rotate: [0, 360],
+          },
+          transition: {
+            ease: "linear",
+            duration: 1,
+            repeat: Infinity,
+            repeatDelay: 0,
+          },
+        })}
+        className={icon({ class: className })}
+      />
+    );
+  },
+);
 
 export default SpinnerIcon;
