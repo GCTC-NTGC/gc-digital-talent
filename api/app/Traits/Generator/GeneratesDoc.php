b402663fff2ace952b69a1e01c0f7370e28acf25
Eric Sizer <eric.sizer@gmail.com>
2025-07-08 14:42:10 -0400

    add support for links and footnotes

--- api/app/Traits/Generator/GeneratesDoc.php
+++ api/app/Traits/Generator/GeneratesDoc.php
@@ -3,6 +3,7 @@
 namespace App\Traits\Generator;
 
 use PhpOffice\PhpWord\Element\Section;
+use PhpOffice\PhpWord\Element\TextRun;
 
 trait GeneratesDoc
 {
@@ -13,14 +14,45 @@ trait GeneratesDoc
      *
      * @param  Section  $section  The section to add it to
      * @param  string  $label  Label for the text
-     * @param  string  $text  The value
+     * @param  string|null  $text  The value
+     * @param  string|null  $footnote  Footnote text
      */
-    protected function addLabelText(Section $section, string $label, ?string $text)
+    protected function addLabelText(Section $section, string $label, ?string $text, ?string $footnote = null): void
     {
         $run = $section->addTextRun();
         $run->addText($label.$this->colon());
-        if (! is_null($text)) {
+        if ($text !== null) {
             $run->addText($text);
+        }
+        $this->appendFootnote($run, $footnote);
+    }
+
+    /**
+     * Adds link accompanied by a strong label
+     *
+     * @param  Section  $section  The section to add it to
+     * @param  string  $label  Label for the text
+     * @param  array{href: string, text: string}|null  $link
+     * @param  string|null  $footnote  Footnote text
+     */
+    protected function addLabelLink(Section $section, string $label, ?array $link, ?string $footnote = null): void
+    {
+        $run = $section->addTextRun();
+        $run->addText($label.$this->colon());
+        if ($link !== null) {
+            $run->addLink($link['href'], $link['text']);
+        }
+        $this->appendFootnote($run, $footnote);
+    }
+
+    /**
+     * Appends a footnote to the given run if text is provided.
+     */
+    private function appendFootnote(TextRun $run, ?string $text): void
+    {
+        if ($text) {
+            $footnote = $run->addFootnote();
+            $footnote->addText($text); // Fixed bug: was $footnote->addText($footnote);
         }
     }
 }
