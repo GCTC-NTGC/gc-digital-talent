b2687a8f9e01bb11820f6b45e22267f3b189bb6a
Eric Sizer <esizer@users.noreply.github.com>
2025-07-10 21:41:07 -0400

    improve email verification alignment (#14084)

---
+++ apps/web/src/components/Profile/components/EmailVerificationStatus.tsx
@@ -0,0 +1,51 @@
+import { useIntl } from "react-intl";
+
+import { Button, Chip } from "@gc-digital-talent/ui";
+
+interface EmailVerificationStatusProps {
+  isEmailVerified?: boolean;
+  onClickVerify: () => Promise<void>;
+}
+
+const EmailVerificationStatus = ({
+  isEmailVerified,
+  onClickVerify,
+}: EmailVerificationStatusProps) => {
+  const intl = useIntl();
+
+  return isEmailVerified ? (
+    <Chip color="success" className="-mb-0.25">
+      {intl.formatMessage({
+        defaultMessage: "Verified",
+        id: "GMglI5",
+        description: "The email address has been verified to be owned by user",
+      })}
+    </Chip>
+  ) : (
+    <>
+      <Chip color="error" className="-mb-0.25">
+        {intl.formatMessage({
+          defaultMessage: "Unverified",
+          id: "tUIvbq",
+          description:
+            "The email address has not been verified to be owned by user",
+        })}
+      </Chip>
+      <Button
+        type="button"
+        mode="inline"
+        color="error"
+        className="-mt-0.25"
+        onClick={onClickVerify}
+      >
+        {intl.formatMessage({
+          defaultMessage: "Verify now",
+          id: "ADPfNp",
+          description: "Button to start the email address verification process",
+        })}
+      </Button>
+    </>
+  );
+};
+
+export default EmailVerificationStatus;
