f259dbfef9f8fc0d1d8d6d68b6eda111653c823f
Eric Sizer <eric.sizer@gmail.com>
2025-07-11 15:45:08 -0400

    add test script

--- apps/web/src/components/Layout/Layout.tsx
+++ apps/web/src/components/Layout/Layout.tsx
@@ -1,9 +1,11 @@
 import { useIntl } from "react-intl";
 import { Outlet, ScrollRestoration } from "react-router";
+import { useSubscription } from "urql";
 
 import { useAuthentication, useAuthorization } from "@gc-digital-talent/auth";
 import { getLocale } from "@gc-digital-talent/i18n";
 import { Flourish } from "@gc-digital-talent/ui";
+import { graphql } from "@gc-digital-talent/graphql";
 
 import SEO, { Favicon } from "~/components/SEO/SEO";
 import Header from "~/components/Header/Header";
@@ -15,6 +17,19 @@ import SitewideBanner from "./SitewideBanner";
 import SkipLink from "./SkipLink";
 import MainNavMenu from "../NavMenu/MainNavMenu";
 import { Project } from "../SEO/Favicon";
+
+const NotificationSent_Subscription = graphql(/** GraphQL */ `
+  subscription NotificationSent {
+    notificationSent {
+      id
+      ... on UserFileGeneratedNotification {
+        id
+        fileName
+        createdAt
+      }
+    }
+  }
+`);
 
 interface LayoutProps {
   project: Project;
@@ -35,6 +50,25 @@ const Layout = ({
 
   const { userAuthInfo } = useAuthorization();
   const { loggedIn } = useAuthentication();
+
+  const socket = new WebSocket(
+    "ws://localhost:6001/gcdtReverbKey?protocol=7&client=js&version=7.0.3",
+  );
+
+  // Log successful connection
+  socket.onopen = () => {
+    console.log("✅ WebSocket connected");
+  };
+
+  // Log errors
+  socket.onerror = (err) => {
+    console.error("❌ WebSocket error:", err);
+  };
+
+  // Log disconnects
+  socket.onclose = (event) => {
+    console.log("⚠️ WebSocket closed:", event);
+  };
 
   return (
     <>
