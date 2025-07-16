0f00b1f898c4e92719a395f7b63a058d229713d4
Yoni K <yonikid15@gmail.com>
2025-07-16 13:09:43 -0400

    Remove unused imports from TalentNomination

--- api/app/Models/TalentNomination.php
+++ api/app/Models/TalentNomination.php
@@ -4,12 +4,10 @@ namespace App\Models;
 
 use App\Enums\TalentNominationSubmitterRelationshipToNominator;
 use App\Observers\TalentNominationObserver;
-use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
-use Illuminate\Support\Facades\Auth;
 use Spatie\Activitylog\LogOptions;
 use Spatie\Activitylog\Traits\LogsActivity;
 
