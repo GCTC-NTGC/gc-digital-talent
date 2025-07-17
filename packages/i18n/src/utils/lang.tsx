92a38698b8955e2481d73a50e6ce57b2dd0f06de
Matt Nigh <matt@mattnigh.com>
2025-07-17 13:41:20 -0400

    Add appendLanguageName

---
+++ packages/i18n/src/utils/lang.tsx
@@ -0,0 +1,33 @@
+import { IntlShape } from "react-intl";
+import { Fragment, ReactNode } from "react";
+
+import { commonMessages } from "../messages";
+import { Locales } from "../types";
+
+const Gray = ({ children }: { children: ReactNode }) => (
+  <span className="text-gray-500 dark:text-gray-300">{children}</span>
+);
+
+export const appendLanguageName = ({
+  label,
+  lang,
+  intl,
+  formatted,
+}: {
+  label: ReactNode;
+  lang: Locales;
+  intl: IntlShape;
+  formatted?: boolean;
+}): ReactNode => {
+  const Wrapper = formatted ? Gray : Fragment;
+  return (
+    <>
+      {label}{" "}
+      <Wrapper>
+        {lang === "en"
+          ? intl.formatMessage(commonMessages.englishLabel)
+          : intl.formatMessage(commonMessages.frenchLabel)}
+      </Wrapper>
+    </>
+  );
+};
