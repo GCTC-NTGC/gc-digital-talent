ec8178674526af15f2ffe11aa3f3efaf8482813f
Eric Sizer <eric.sizer@gmail.com>
2025-07-22 14:37:56 -0400

    refactor test to use data provider

--- api/tests/Unit/ProtectedRequestUserCheckerTest.php
+++ api/tests/Unit/ProtectedRequestUserCheckerTest.php
@@ -11,9 +11,6 @@ use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Facades\Route;
 use Tests\TestCase;
 
-use function PHPUnit\Framework\assertFalse;
-use function PHPUnit\Framework\assertTrue;
-
 class ProtectedRequestUserCheckerTest extends TestCase
 {
     use RefreshDatabase;
@@ -39,113 +36,82 @@ class ProtectedRequestUserCheckerTest extends TestCase
         $this->testRoute = new RoutingRoute('get', '/test', fn () => null);
     }
 
-    public function testCanUseLimitedPermissionUnprotected()
+    /**
+     * @dataProvider userCheckerProvider
+     */
+    public function testHasPermission(string|array $permission, ?bool $isProtectedRequest, bool $expected)
     {
-        // simulate a regular request context
-        Request::merge(['isProtectedRequest' => null]);
+        Request::merge(['isProtectedRequest' => $isProtectedRequest]);
+        Route::shouldReceive('current')->andReturn($this->testRoute);
         $checker = new ProtectedRequestUserChecker($this->adminUser);
+        $result = $checker->currentUserHasPermission($permission);
 
-        // a limited permission can be used in an unprotected request
-        $limitedPermission = 'view-any-skill';
-
-        assertTrue($checker->currentUserHasPermission($limitedPermission));
+        $this->assertEquals($expected, $result);
     }
 
-    public function testCanUseLimitedPermissionProtected()
+    public static function userCheckerProvider()
     {
-        // simulate a protected request context
-        Request::merge(['isProtectedRequest' => true]);
-        $checker = new ProtectedRequestUserChecker($this->adminUser);
+        /**
+         * [
+         *   string|array $permission - Permissions to be checked
+         *   ?bool $isProtectedRequest - If this is a protected request
+         *   bool $expected - The expected result of the check
+         * ]
+         */
+        return [
+            // Single permission checks
+            'limited permission unprotected' => [
+                'view-any-skill',
+                null,
+                true,
+            ],
+            'limited permission protected' => [
+                'view-any-skill',
+                true,
+                true,
+            ],
+            'privileged permission protected' => [
+                'create-any-classification',
+                true,
+                true,
+            ],
+            'privileged permission unprotected' => [
+                'create-any-classification',
+                null,
+                false,
+            ],
+            'unknown permission unprotected' => [
+                'not-a-real-permission',
+                null,
+                false,
+            ],
 
-        // a limited permission can be used in an protected request
-        $limitedPermission = 'view-any-skill';
-
-        assertTrue($checker->currentUserHasPermission($limitedPermission));
-    }
-
-    public function testCanUsePrivilegedPermissionProtected()
-    {
-        // simulate a protected request context
-        Request::merge(['isProtectedRequest' => true]);
-        $checker = new ProtectedRequestUserChecker($this->adminUser);
-
-        // a privileged permission can be used only in a protected request
-        $privilegedPermission = 'create-any-classification';
-
-        assertTrue($checker->currentUserHasPermission($privilegedPermission));
-    }
-
-    public function testCanNotUsePrivilegedPermissionUnprotected()
-    {
-        // simulate an unprotected request context
-        Request::merge(['isProtectedRequest' => null]);
-        Route::expects('current')->andReturn($this->testRoute);
-        $checker = new ProtectedRequestUserChecker($this->adminUser);
-
-        // a privileged permission can be used only in a protected request
-        $privilegedPermission = 'create-any-classification';
-
-        assertFalse($checker->currentUserHasPermission($privilegedPermission));
-    }
-
-    public function testCanNotUseUnknownPermissionUnprotected()
-    {
-        // simulate an unprotected request context
-        Request::merge(['isProtectedRequest' => null]);
-        $checker = new ProtectedRequestUserChecker($this->adminUser);
-
-        // an unknown permission can not be used in a protected request
-        $unknownPermission = 'not-a-real-permission';
-
-        assertFalse($checker->currentUserHasPermission($unknownPermission));
-    }
-
-    public function testCanUseLimitedArrayPermissionUnprotected()
-    {
-        // simulate a regular request context
-        Request::merge(['isProtectedRequest' => null]);
-        $checker = new ProtectedRequestUserChecker($this->adminUser);
-
-        // a limited permission can be used in an unprotected request
-        $limitedPermission = ['view-any-skill', 'view-any-skillFamily'];
-
-        assertTrue($checker->currentUserHasPermission($limitedPermission));
-    }
-
-    public function testCanUseLimitedArrayPermissionProtected()
-    {
-        // simulate a protected request context
-        Request::merge(['isProtectedRequest' => true]);
-        $checker = new ProtectedRequestUserChecker($this->adminUser);
-
-        // a limited permission can be used in an protected request
-        $limitedPermission = ['view-any-skill', 'view-any-skillFamily'];
-
-        assertTrue($checker->currentUserHasPermission($limitedPermission));
-    }
-
-    public function testCanUsePrivilegedArrayPermissionProtected()
-    {
-        // simulate a protected request context
-        Request::merge(['isProtectedRequest' => true]);
-        $checker = new ProtectedRequestUserChecker($this->adminUser);
-
-        // a privileged permission can be used only in a protected request
-        $privilegedPermission = ['create-any-classification', 'create-any-department'];
-
-        assertTrue($checker->currentUserHasPermission($privilegedPermission));
-    }
-
-    public function testCanNotUsePrivilegedArrayPermissionUnprotected()
-    {
-        // simulate an unprotected request context
-        Request::merge(['isProtectedRequest' => null]);
-        Route::expects('current')->andReturn($this->testRoute);
-        $checker = new ProtectedRequestUserChecker($this->adminUser);
-
-        // a privileged permission can be used only in a protected request
-        $privilegedPermission = ['create-any-classification', 'create-any-department'];
-
-        assertFalse($checker->currentUserHasPermission($privilegedPermission));
+            // permission array checks checks
+            'limited permission array unprotected' => [
+                ['view-any-skill', 'view-any-skillFamily'],
+                null,
+                true,
+            ],
+            'limited permission array protected' => [
+                ['view-any-skill', 'view-any-skillFamily'],
+                true,
+                true,
+            ],
+            'privileged permission array protected' => [
+                ['create-any-classification', 'create-any-department'],
+                true,
+                true,
+            ],
+            'privileged permission array unprotected' => [
+                ['create-any-classification', 'create-any-department'],
+                null,
+                false,
+            ],
+            'unknown permission array unprotected' => [
+                ['not-a-real-permission', 'another-not-real-permission'],
+                null,
+                false,
+            ],
+        ];
     }
 }
