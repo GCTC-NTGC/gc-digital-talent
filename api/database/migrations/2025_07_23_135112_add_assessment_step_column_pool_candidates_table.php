13be740f938d736b163adbd4f6608e71a5d40c72
Eric Sizer <eric.sizer@gmail.com>
2025-07-23 10:06:18 -0400

    add migration of new field

---
+++ api/database/migrations/2025_07_23_135112_add_assessment_step_column_pool_candidates_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('pool_candidates', function (Blueprint $table) {
+            $table->integer('assessment_step')->nullable()->default(1);
+        });
+
+        DB::statement("UPDATE pool_candidates SET assessment_step = (computed_assessment_status->>'currentStep')::int WHERE computed_assessment_status IS NOT NULL");
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        DB::statement(<<<'SQL'
+            UPDATE pool_candidates
+                SET computed_assessment_status = jsonb_set(
+                    COALESCE(computed_assessment_status, '{}'::jsonb),
+                    '{currentStep}',
+                    to_jsonb(assessment_step)
+                )
+        SQL);
+
+        Schema::table('pool_candidates', function (Blueprint $table) {
+            $table->dropColumn('assessment_step');
+        });
+
+    }
+};
