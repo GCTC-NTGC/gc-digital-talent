24b569ae8ece3630b79f5ce373a5d35f90c3c1e2
Eric Sizer <esizer@users.noreply.github.com>
2025-07-23 10:38:14 -0400

    [Fix] Support arrays in protected user checker (#14187)
    * add support for array permissions
    
    * support array roles
    
    * update tests with arrays
    
    * use route facade
    
    * fix tests and remove skip
    
    Co-authored-by: Peter Giles
    <8978655+petertgiles@users.noreply.github.com>
    
    * refactor checker to use collection methods
    
    * refactor test to use data provider
    
    * improve test structure, add additional tests
    
    * fix test case name

--- api/app/Checkers/ProtectedRequestUserChecker.php
+++ api/app/Checkers/ProtectedRequestUserChecker.php
@@ -5,9 +5,11 @@ namespace App\Checkers;
 use App\Models\Permission;
 use BackedEnum;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Request;
+use Illuminate\Support\Facades\Route;
 use Laratrust\Checkers\User\UserDefaultChecker;
 use Laratrust\Helper;
 
@@ -22,13 +24,14 @@ class ProtectedRequestUserChecker extends UserDefaultChecker
     ];
 
     // decide if it is safe for the current user to use the given role
-    protected function isSafeToUseRole(string|array|BackedEnum $name): bool
+    protected function isSafeToUseRole(string|array|BackedEnum $name, bool $requireAll = false): bool
     {
         $name = Helper::standardize($name);
 
         $isProtectedRequest = Request::get('isProtectedRequest');
-        $isLimitedRole = in_array($name, $this::LIMITED_ROLES);
-        $isNotRoutedRequest = is_null(Request::route());
+        $isNotRoutedRequest = is_null(Route::current());
+
+        $isLimitedRole = $this->isLimited($name, $this::LIMITED_ROLES, $requireAll);
 
         return
             $isProtectedRequest      // if it's a protected request then any role is safe to use
@@ -37,7 +40,7 @@ class ProtectedRequestUserChecker extends UserDefaultChecker
     }
 
     // decide if it is safe for the current user to use the given permission
-    protected function isSafeToUsePermission(string|array|BackedEnum $permission): bool
+    protected function isSafeToUsePermission(string|array|BackedEnum $permission, bool $requireAll = false): bool
     {
         $permission = Helper::standardize($permission);
 
@@ -49,8 +52,9 @@ class ProtectedRequestUserChecker extends UserDefaultChecker
               $query->whereIn('name', $this::LIMITED_ROLES);
           })->pluck('name')->toArray();
         });
-        $isLimitedPermission = in_array($permission, $limitedPermissions);
-        $isNotRoutedRequest = is_null(Request::route());
+
+        $isLimitedPermission = $this->isLimited($permission, $limitedPermissions, $requireAll);
+        $isNotRoutedRequest = is_null(Route::current());
 
         return $isProtectedRequest  // if it's a protected request then any permission is safe to use
         || $isLimitedPermission     // if it's a limited (unprivileged) permission then it's always safe to use
@@ -62,7 +66,7 @@ class ProtectedRequestUserChecker extends UserDefaultChecker
         mixed $team = null,
         bool $requireAll = false
     ): bool {
-        if (! $this->isSafeToUseRole($name)) {
+        if (! $this->isSafeToUseRole($name, $requireAll)) {
             Log::debug('Tried to unsafely use role '.json_encode($name));
 
             return false; // user effectively doesn't have role if it is unsafe to use it
@@ -76,12 +80,25 @@ class ProtectedRequestUserChecker extends UserDefaultChecker
         mixed $team = null,
         bool $requireAll = false
     ): bool {
-        if (! $this->isSafeToUsePermission($permission)) {
+        if (! $this->isSafeToUsePermission($permission, $requireAll)) {
             Log::debug('Tried to unsafely use permission '.json_encode($permission));
 
             return false; // user effectively doesn't have permission if it is unsafe to use it
         }
 
         return parent::currentUserHasPermission($permission, $team, $requireAll);
+    }
+
+    private function isLimited(string|array|BackedEnum $needle, array $haystack, bool $requireAll = false)
+    {
+        $needleCollection = collect(Arr::wrap($needle));
+        $haystackCollection = collect($haystack);
+        $compareFn = fn (string $name) => $haystackCollection->contains($name);
+
+        if ($requireAll) {
+            return $needleCollection->every($compareFn);
+        }
+
+        return $needleCollection->some($compareFn);
     }
 }
