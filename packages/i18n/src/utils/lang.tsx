eb3e44db3ca401bd49a890e420613a3ab2f69023
Matt Nigh <matt@mattnigh.com>
2025-07-23 09:25:21 -0400

    Add comment

--- packages/i18n/src/utils/lang.tsx
+++ packages/i18n/src/utils/lang.tsx
@@ -4,6 +4,7 @@ import { Fragment, ReactNode } from "react";
 import { commonMessages } from "../messages";
 import { Locales } from "../types";
 
+// style should be the same as gray in packages/i18n/src/components/richTextElements.tsx.
 const Gray = ({ children }: { children: ReactNode }) => (
   <span className="text-gray-500 dark:text-gray-300">{children}</span>
 );
