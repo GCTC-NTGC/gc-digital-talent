1e764e21ce20daf3597be529cde8b3b961e6271c
Eric Sizer <eric.sizer@gmail.com>
2025-07-23 14:34:07 -0400

    add user doc snapshot test

--- api/tests/Feature/Generators/UserDocGeneratorTest.php
+++ api/tests/Feature/Generators/UserDocGeneratorTest.php
@@ -3,12 +3,16 @@
 namespace Tests\Feature\Generators;
 
 use App\Generators\UserDocGenerator;
+use App\Models\Community;
 use App\Models\User;
+use Database\Seeders\CommunitySeeder;
 use Database\Seeders\RolePermissionSeeder;
 use Database\Seeders\SkillFamilySeeder;
 use Database\Seeders\SkillSeeder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Facades\Storage;
+use Spatie\Snapshots\MatchesSnapshots;
 use Tests\TestCase;
 
 use function PHPUnit\Framework\assertGreaterThan;
@@ -16,41 +20,57 @@ use function PHPUnit\Framework\assertTrue;
 
 class UserDocGeneratorTest extends TestCase
 {
+    use MatchesSnapshots;
     use RefreshDatabase;
+    use WithFaker;
+
+    protected User $adminUser;
+
+    protected User $targetUser;
 
     protected function setUp(): void
     {
         parent::setUp();
+        $this->setUpFaker();
+
+        $this->faker->seed(0);
+
         $this->seed([
             RolePermissionSeeder::class,
             SkillFamilySeeder::class,
             SkillSeeder::class,
+            CommunitySeeder::class,
         ]);
-    }
 
-    // test that a file can be generated
-    public function testCanGenerateFile(): void
-    {
-        // arrange
-        $adminUser = User::factory()
+        $community = Community::where('key', 'digital')->sole();
+
+        $this->adminUser = User::factory()
             ->asApplicant()
             ->asAdmin()
             ->create();
 
-        $targetUser = User::factory()
+        $this->targetUser = User::factory()
             ->asApplicant()
+            ->asGovEmployee()
+            ->withCommunityInterests([$community->id])
+            ->withEmployeeProfile()
             ->withSkillsAndExperiences()
             ->create();
 
+    }
+
+    // test that a file can be generated
+    public function test_can_generate_file(): void
+    {
         // act
         $generator = new UserDocGenerator(
-            user: $targetUser,
+            user: $this->targetUser,
             anonymous: false,
             dir: 'test',
             lang: 'en'
         );
 
-        $generator->setUserId($adminUser->id);
+        $generator->setUserId($this->adminUser->id);
         $generator->generate()->write();
         $fileName = $generator->getFileNameWithExtension();
 
@@ -62,5 +82,23 @@ class UserDocGeneratorTest extends TestCase
         assertTrue($fileExists, 'File was not generated');
         $fileSize = $disk->size($path);
         assertGreaterThan(0, $fileSize, 'File is empty');
+    }
+
+    public function test_user_profile_doc()
+    {
+        $generator = new UserDocGenerator(user: $this->targetUser, anonymous: false, dir: 'test', lang: 'en');
+        $generator
+            ->setUserId($this->adminUser->id)
+            ->setExtension('html')
+            ->generate()
+            ->write();
+
+        $disk = Storage::disk('userGenerated');
+        $contents = $disk->get($generator->getRelativePath());
+
+        echo $generator->getPath();
+
+        $this->assertMatchesHtmlSnapshot($contents);
+
     }
 }
