5dd10d5ca77ddec60f15279452b647f94b4a0fde
Eric Sizer <esizer@users.noreply.github.com>
2025-07-24 11:34:47 -0400

    [Feat] New `Image` component (#14199)
    * add new image component
    
    * update hero, image container with new image component
    
    * update feature block with new component
    
    * remove old export
    
    * comment the media query map
    
    * add story for image component
    
    * add high fetch priority for hero images
    
    * remove fethc priortity

---
+++ packages/ui/src/components/Image/Image.tsx
@@ -0,0 +1,65 @@
+import { ImgHTMLAttributes } from "react";
+
+import { notEmpty } from "@gc-digital-talent/helpers";
+
+type Breakpoint = "xs" | "sm" | "md" | "lg";
+type PartialBreakpoints = Partial<Record<Breakpoint, string>>;
+
+/**
+ * NOTE:
+ *  Should match breakpoints defined in tailwind.css config
+ *  This is required because we cannot use css variables in
+ *  media queries
+ *
+ *  REF: https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_cascading_variables/Using_CSS_custom_properties
+ */
+const sourceMediaMap = new Map<Breakpoint, string>([
+  ["xs", "(max-width: 48rem)"],
+  ["sm", "(max-width: 67.5rem)"],
+  ["md", "(max-width: 80rem)"],
+  ["lg", "(max-width: 100rem)"],
+]);
+
+interface PictureSource {
+  srcSet: string;
+  media: string;
+}
+
+const buildPictureSource = (
+  sources?: PartialBreakpoints,
+): PictureSource[] | null => {
+  if (!sources) {
+    return null;
+  }
+
+  return Object.keys(sources)
+    .map((k) => {
+      const srcSet = sources[k as Breakpoint];
+      const media = sourceMediaMap.get(k as Breakpoint);
+      if (!media || !srcSet) return null;
+
+      return { srcSet, media };
+    })
+    .filter(notEmpty);
+};
+
+export interface ImgProps extends ImgHTMLAttributes<HTMLImageElement> {
+  sources?: PartialBreakpoints;
+}
+
+const Image = ({ sources, alt, ...rest }: ImgProps) => {
+  const pictureSources = buildPictureSource(sources);
+
+  return (
+    <picture>
+      {pictureSources?.length
+        ? pictureSources?.map((sourceProps) => (
+            <source key={sourceProps.srcSet} {...sourceProps} />
+          ))
+        : null}
+      <img alt={alt ?? ""} {...rest} />
+    </picture>
+  );
+};
+
+export default Image;
