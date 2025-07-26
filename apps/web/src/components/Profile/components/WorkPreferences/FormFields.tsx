cf9b3d2f794fa700e70123c90a5f2718bbb5a9e9
vd1992 <40485260+vd1992@users.noreply.github.com>
2025-07-25 19:45:01 -0600

    add checklist content below

--- apps/web/src/components/Profile/components/WorkPreferences/FormFields.tsx
+++ apps/web/src/components/Profile/components/WorkPreferences/FormFields.tsx
@@ -1,6 +1,7 @@
 import { useIntl } from "react-intl";
 
 import {
+  CheckboxOption,
   Checklist,
   Field,
   Input,
@@ -15,7 +16,12 @@ import {
   getOperationalRequirement,
   sortFlexibleWorkLocations,
 } from "@gc-digital-talent/i18n";
-import { FragmentType, getFragment, graphql } from "@gc-digital-talent/graphql";
+import {
+  FlexibleWorkLocation,
+  FragmentType,
+  getFragment,
+  graphql,
+} from "@gc-digital-talent/graphql";
 
 import { FormFieldProps } from "../../types";
 import useDirtyFields from "../../hooks/useDirtyFields";
@@ -53,6 +59,50 @@ const FormFields = ({
   const intl = useIntl();
   const data = getFragment(WorkPreferencesFormOptions_Fragment, optionsQuery);
   useDirtyFields("work");
+
+  // taking the enum collection from the API request
+  // turn them into options, append a content-below node
+  const formOptionsWithContentBelow: CheckboxOption[] =
+    sortFlexibleWorkLocations(data?.flexibleWorkLocation).map((loc) => {
+      if (loc.value === (FlexibleWorkLocation.Remote as string)) {
+        return {
+          value: loc.value,
+          label: loc.label.localized,
+          contentBelow: intl.formatMessage({
+            defaultMessage: "I'm willing to work 100% remotely.",
+            id: "WoFBmk",
+            description: "Checklist option explanatory note",
+          }),
+        };
+      }
+      if (loc.value === (FlexibleWorkLocation.Hybrid as string)) {
+        return {
+          value: loc.value,
+          label: loc.label.localized,
+          contentBelow: intl.formatMessage({
+            defaultMessage:
+              "I'm willing to work on-site for a minimum of 3 days per week, with the rest being remote.",
+            id: "47D8AR",
+            description: "Checklist option explanatory note",
+          }),
+        };
+      }
+      if (loc.value === (FlexibleWorkLocation.Onsite as string)) {
+        return {
+          value: loc.value,
+          label: loc.label.localized,
+          contentBelow: intl.formatMessage({
+            defaultMessage: "I'm willing to work on-site full-time.",
+            id: "JcSzqF",
+            description: "Checklist option explanatory note",
+          }),
+        };
+      }
+      return {
+        value: loc.value,
+        label: loc.label.localized,
+      };
+    });
 
   return (
     <div className="flex flex-col gap-6">
@@ -136,10 +186,7 @@ const FormFields = ({
           legend={labels.flexibleWorkLocationOptions}
           name="flexibleWorkLocations"
           id="flexibleWorkLocations"
-          items={localizedEnumToOptions(
-            sortFlexibleWorkLocations(data?.flexibleWorkLocation),
-            intl,
-          )}
+          items={formOptionsWithContentBelow}
           rules={{
             required: intl.formatMessage(errorMessages.required),
           }}
