710481c40ed9218dd4f3d1ce11774433c875f5d8
Eric Sizer <eric.sizer@gmail.com>
2025-07-28 14:57:14 -0400

    add translations

--- api/lang/fr/errors.php
+++ api/lang/fr/errors.php
@@ -1,11 +1,11 @@
 <?php
 
 return [
-    'unknown' => '',
+    'unknown' => 'Une erreur inconnue s’est produite. Si le problème persiste, veuillez communiquer avec notre équipe de soutien.',
     'application' => [
         'missing_snapshot' => [
-            'unknown' => '',
-            'known' => '',
+            'unknown' => 'Personne candidate inconnue : Le téléchargement de la candidature a échoué en raison de l’absence de la capture d’écran de la candidature. Veuillez contacter notre équipe de soutien pour obtenir de l’aide.',
+            'known' => ':candidate: Le téléchargement de la candidature a échoué en raison de l’absence de la capture d’écran de la candidature. Veuillez contacter notre équipe de soutien pour obtenir de l’aide.',
         ],
     ],
 ];
