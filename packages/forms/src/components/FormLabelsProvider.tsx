5df92a90639cf11195b4f050d7a873db1e21e5d2
Eric Sizer <eric.sizer@gmail.com>
2025-07-29 15:40:53 -0400

    improve basic provider

--- packages/forms/src/components/FormLabelsProvider.tsx
+++ packages/forms/src/components/FormLabelsProvider.tsx
@@ -1,15 +1,30 @@
 import { createContext, ReactNode, useContext, useRef } from "react";
 
+const numberRegex = /(\d+)/g;
+
+export const normalizeName = (name: string): string => {
+  let normalized = name;
+  // This is a root error for a field array
+  if (name.includes(".root")) {
+    normalized = name.replace(".root", "");
+  } else if (numberRegex.test(name)) {
+    // If a number exists in the field name, replace it with an asterisk
+    normalized = name.replace(numberRegex, "*");
+  }
+
+  return normalized;
+};
+
 type FieldLabels = Record<string, ReactNode>;
 
 interface LabelsContextState {
   labels: FieldLabels;
-  setLabel: (name: string, label: ReactNode) => void;
+  registerLabel: (name: string, label: ReactNode) => void;
 }
 
 const LabelsContext = createContext<LabelsContextState>({
   labels: {},
-  setLabel: () => {
+  registerLabel: () => {
     // noop
   },
 });
@@ -21,15 +36,21 @@ interface FormLabelsProviderProps {
 export const FormLabelsProvider = ({ children }: FormLabelsProviderProps) => {
   const labelsRef = useRef<FieldLabels>({});
 
-  const setLabel = (name: string, label: ReactNode) => {
-    labelsRef.current[name] = label;
+  const registerLabel = (name: string, label: ReactNode) => {
+    labelsRef.current[normalizeName(name)] = label;
   };
 
   return (
-    <LabelsContext.Provider value={{ labels: labelsRef.current, setLabel }}>
+    <LabelsContext.Provider
+      value={{ labels: labelsRef.current, registerLabel }}
+    >
       {children}
     </LabelsContext.Provider>
   );
 };
 
 export const useFormLabels = () => useContext(LabelsContext);
+export const useRegisterFormLabel = (name: string, label: ReactNode) => {
+  const { registerLabel } = useFormLabels();
+  registerLabel(name, label);
+};
