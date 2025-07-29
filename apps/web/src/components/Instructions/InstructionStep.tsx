bebd8394553732d4bd5d4ff98041ebf5ca982d2c
Eric Sizer <eric.sizer@gmail.com>
2025-07-29 11:51:53 -0400

    update auth info page images

--- apps/web/src/components/Instructions/InstructionStep.tsx
+++ apps/web/src/components/Instructions/InstructionStep.tsx
@@ -3,27 +3,32 @@ import ArrowDownCircleIcon from "@heroicons/react/24/solid/ArrowDownCircleIcon";
 import { ReactNode } from "react";
 
 import { useTheme } from "@gc-digital-talent/theme";
+import { Image } from "@gc-digital-talent/ui";
 
 interface InstructionStepProps {
-  image: string;
-  imageDark?: string;
+  img: {
+    src: string;
+    darkSrc?: string;
+    lazy?: boolean;
+    width?: number;
+    height?: number;
+  };
   includeArrow?: boolean;
   children: ReactNode;
 }
 
 const InstructionStep = ({
-  image,
-  imageDark,
+  img: { src, darkSrc, lazy, ...imgProps },
   includeArrow = true,
   children,
 }: InstructionStepProps) => {
   const { mode } = useTheme();
-  const imgSrc = mode === "dark" && imageDark ? imageDark : image;
+  const imgSrc = mode === "dark" && darkSrc ? darkSrc : src;
 
   return (
     <li className="flex flex-col">
       <div className="flex">
-        <img src={imgSrc} alt="" />
+        <Image {...imgProps} src={imgSrc} alt="" />
         {includeArrow && (
           <ArrowRightCircleIcon className="hidden h-auto w-9 overflow-visible px-1.5 align-middle text-gray xs:block" />
         )}
