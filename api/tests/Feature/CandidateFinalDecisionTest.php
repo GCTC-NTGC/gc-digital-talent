989c0b571ce3555b8dd5457309dc2fcb261dc4ae
Eric Sizer <eric.sizer@gmail.com>
2025-07-30 09:19:12 -0400

    update tests with new placement type

--- api/tests/Feature/CandidateFinalDecisionTest.php
+++ api/tests/Feature/CandidateFinalDecisionTest.php
@@ -150,6 +150,7 @@ class CandidateFinalDecisionTest extends TestCase
             'Placed indeterminate' => [PoolCandidateStatus::PLACED_INDETERMINATE->name, $qualifiedPlaced],
             'Placed term' => [PoolCandidateStatus::PLACED_TERM->name, $qualifiedPlaced],
             'Placed tentative' => [PoolCandidateStatus::PLACED_TENTATIVE->name, $qualifiedPlaced],
+            'Under consideration' => [PoolCandidateStatus::UNDER_CONSIDERATION->name, $qualifiedPlaced],
 
             // Disqualified
             'Screened out assessment' => [PoolCandidateStatus::SCREENED_OUT_ASSESSMENT->name, $disqualified],
