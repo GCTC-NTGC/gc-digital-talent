87a05d49d93eb870fee5447f2fc2350ec5387407
Eric Sizer <esizer@users.noreply.github.com>
2025-08-01 13:23:03 -0400

    [Debt] Reduce common image sizes (#14263)
    * resize feature block images
    
    * reduce quality to 90
    
    * update auth info page images
    
    * fix no arrow image size
    
    * prevent spacer from shrinking

--- apps/web/src/components/Instructions/InstructionStep.tsx
+++ apps/web/src/components/Instructions/InstructionStep.tsx
@@ -3,29 +3,39 @@ import ArrowDownCircleIcon from "@heroicons/react/24/solid/ArrowDownCircleIcon";
 import { ReactNode } from "react";
 
 import { useTheme } from "@gc-digital-talent/theme";
+import { Image } from "@gc-digital-talent/ui";
 
 interface InstructionStepProps {
-  image: string;
-  imageDark?: string;
+  img: {
+    src: string;
+    darkSrc?: string;
+    lazy?: boolean;
+    width?: number;
+    height?: number;
+  };
   includeArrow?: boolean;
   children: ReactNode;
 }
 
 const InstructionStep = ({
-  image,
-  imageDark,
+  img: { src, darkSrc, lazy, ...imgProps },
   includeArrow = true,
   children,
 }: InstructionStepProps) => {
   const { mode } = useTheme();
-  const imgSrc = mode === "dark" && imageDark ? imageDark : image;
+  const imgSrc = mode === "dark" && darkSrc ? darkSrc : src;
 
   return (
     <li className="flex flex-col">
       <div className="flex">
-        <img src={imgSrc} alt="" />
-        {includeArrow && (
-          <ArrowRightCircleIcon className="hidden h-auto w-9 overflow-visible px-1.5 align-middle text-gray xs:block" />
+        <Image {...imgProps} src={imgSrc} alt="" />
+        {includeArrow ? (
+          <ArrowRightCircleIcon className="hidden h-auto w-9 overflow-visible px-1.5 align-middle text-gray xs:block xs:shrink-0" />
+        ) : (
+          <span
+            aria-hidden="true"
+            className="hidden w-9 px-1.5 xs:block xs:shrink-0"
+          />
         )}
       </div>
       <div className="mt-3 grow pl-4 -indent-4">{children}</div>
