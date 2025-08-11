d4e8ab845c9d65f3c056bbe22591333521fc4066
Matt <mnigh@users.noreply.github.com>
2025-08-11 15:39:53 -0400

    [Copy] Updates French strings (#14329)
    * Update description of the application on the homepage
    
    * Update Description for the digital government job opportunities
    
    * Update Title displayed on hero for Search and Request pages
    
    * Update Subtitle for the search page
    
    * Update IT-03 classification label including titles
    
    * Update IT-04 classification label including titles
    
    * Update PM-05 classification label including titles
    
    * Update The operational requirement described as driver's license
    
    * Update The operational requirement described as driver's license
    
    * Update Description of the employee profile page
    
    * Update interested in lateral movement
    
    * Update Label for an employee profile career development preference field
    
    * Update Label for an employee profile career development preference field
    
    * Update lateralMoveOrganizationType, promotionMoveOrganizationType messages
    
    * Replace mentor ou mentore with mentor
    
    * Update The email address has been verified to be owned by user
    
    * Update Legend text for spoken interview language preference for interviews
    
    * Replace dans le cadre duquel with où
    
    * Update Title for the government employee information section
    
    * Update Description for the Career timeline page in applicant profile
    
    * Update Title for in app notifications
    
    * Update Subtitle for application updates checklist
    
    * Fix typo
    
    * Update notification settings table to account for longer headings
    
    * Update The mentorship status described as mentee
    
    * Add missing space
    
    * Replace Délai cible pour une mutation latérale with Échéancier visé pour une mutation latérale
    
    * Update Délai cible with Échéancier visé

--- api/lang/fr/operational_requirement.php
+++ api/lang/fr/operational_requirement.php
@@ -14,7 +14,7 @@ return [
         'on_call' => 'Être disponible, disposé(e) et apte à travailler sur appel 24/7.',
         'travel' => 'Être disponible, disposé(e) et apte à voyager au besoin.',
         'transport_equipment' => 'Être disponible, disposé(e) et apte à transporter, soulever et déposer de l\'équipement pouvant peser jusqu\'à 20 kg.',
-        'drivers_license' => 'Doit posséder un permis de conduire valide ou une mobilité personnelle du degré normalement associé à la possession d\'un permis de conduire valide.',
+        'drivers_license' => 'Être titulaire d\'un permis de conduire valide ou pouvoir se déplacer au même titre qu\'une personne qui possède un permis de conduire valide.',
         'overtime_regular' => 'Être disponible, disposé(e) et apte à travailler des heures supplémentaires (régulièrement).',
         'overtime_occasional' => 'Être disponible, disposé(e) et apte à travailler des heures supplémentaires (occasionnellement).',
     ],
