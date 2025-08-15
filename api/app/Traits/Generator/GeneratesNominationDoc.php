ee24d894fd4391045e89d4443da231288be2ec47
Yoni K <yonikid15@gmail.com>
2025-08-15 09:16:14 -0400

    Fix null handling on fields

--- api/app/Traits/Generator/GeneratesNominationDoc.php
+++ api/app/Traits/Generator/GeneratesNominationDoc.php
@@ -53,10 +53,10 @@ trait GeneratesNominationDoc
         }
 
         $section->addTitle($this->localizeHeading('nominee'), $headingRank);
-        $this->addLabelText($section, $this->localizeHeading('functional_community'), "{$talentNominationGroup->nominee->first_name} {$talentNominationGroup->nominee->last_name}");
+        $this->addLabelText($section, $this->localizeHeading('functional_community'), $talentNominationGroup->nominee->getFullName());
         $this->addLabelText($section, $this->localizeHeading('work_email'), $talentNominationGroup->nominee->work_email);
         $this->addLabelText($section, $this->localizeHeading('communication_language'), $this->localizeEnum($talentNominationGroup->nominee->preferred_lang, Language::class));
-        $this->addLabelText($section, $this->localizeHeading('nominated_for'), implode(',', $nominatedFor));
+        $this->addLabelText($section, $this->localizeHeading('nominated_for'), implode(', ', $nominatedFor));
     }
 
     /**
@@ -78,45 +78,56 @@ trait GeneratesNominationDoc
                 $nomination->loadMissing('nominator', 'advancementReferenceFallbackClassification', 'advancementReferenceFallbackDepartment');
 
                 if ($nomination->nominator) {
-                    $section->addTitle("{$this->localizeHeading('nominated_by')} {$nomination->nominator->first_name} {$nomination->nominator->last_name}", $headingRank + 1);
-                    $this->addLabelText($section, $this->localizeHeading('date_received'), $nomination->submitted_at->format('F d, Y'));
-                    $this->addLabelText($section, $this->localizeHeading('nominators_work_email'), $nomination->nominator->work_email);
-                    $this->addLabelText($section, $this->localizeHeading('nominators_classification'), $nomination->nominator->getClassification());
-                    $this->addLabelText($section, $this->localizeHeading('nominators_department'), $nomination->nominator->department->name[$this->lang]);
+                    $nominatorFullName = $nomination->nominator->getFullName() ?? Lang::get('common.not_available', [], $this->lang);
+                    $section->addTitle("{$this->localizeHeading('nominated_by')} {$nominatorFullName}", $headingRank + 1);
+                    $this->addLabelText($section, $this->localizeHeading('date_received'), $nomination->submitted_at->format('F d, Y') ?? Lang::get('common.not_available', [], $this->lang));
+                    $this->addLabelText($section, $this->localizeHeading('nominators_work_email'), $nomination->nominator->work_email ?? Lang::get('common.not_available', [], $this->lang));
+                    $this->addLabelText($section, $this->localizeHeading('nominators_classification'), $nomination->nominator->currentClassification ? $nomination->nominator->currentClassification->displayName : Lang::get('common.not_available', [], $this->lang));
+                    $this->addLabelText($section, $this->localizeHeading('nominators_department'), $nomination->nominator->department ? $nomination->nominator->department->name[$this->lang] : Lang::get('common.not_available', [], $this->lang));
                 } else {
-                    $section->addTitle("{$this->localizeHeading('nominated_by')} {$nomination->nominator_fallback_name}", $headingRank + 1);
-                    $this->addLabelText($section, $this->localizeHeading('date_received'), $nomination->submitted_at->format('F d, Y'));
-                    $this->addLabelText($section, $this->localizeHeading('nominators_work_email'), $nomination->nominator_fallback_work_email);
+                    $nominatorFallbackName = $nomination->nominator_fallback_name ?? Lang::get('common.not_available', [], $this->lang);
+                    $section->addTitle("{$this->localizeHeading('nominated_by')} {$nominatorFallbackName}", $headingRank + 1);
+                    $this->addLabelText($section, $this->localizeHeading('date_received'), $nomination->submitted_at->format('F d, Y') ?? Lang::get('common.not_available', [], $this->lang));
+                    $this->addLabelText($section, $this->localizeHeading('nominators_work_email'), $nomination->nominator_fallback_work_email ?? Lang::get('common.not_available', [], $this->lang));
 
-                    $fallBackClassification = Classification::findOrFail($nomination->nominator_fallback_classification_id);
-                    $this->addLabelText($section, $this->localizeHeading('nominators_classification'), $fallBackClassification->displayName);
+                    $fallBackClassification = Classification::find($nomination->nominator_fallback_classification_id);
+                    $this->addLabelText($section, $this->localizeHeading('nominators_classification'), $fallBackClassification ? $fallBackClassification->displayName : Lang::get('common.not_available', [], $this->lang));
 
-                    $fallbackDepartment = Department::findOrFail($nomination->nominator_fallback_department_id);
-                    $this->addLabelText($section, $this->localizeHeading('nominators_department'), $fallbackDepartment->name[$this->lang]);
+                    $fallbackDepartment = Department::find($nomination->nominator_fallback_department_id);
+                    $this->addLabelText($section, $this->localizeHeading('nominators_department'), $fallbackDepartment ? $fallbackDepartment->name[$this->lang] : Lang::get('common.not_available', [], $this->lang));
 
                 }
 
                 $nominatorRelationship = $nomination->submitter_relationship_to_nominator ? $this->localizeEnum($nomination->submitter_relationship_to_nominator->name, TalentNominationSubmitterRelationshipToNominator::class) : $nomination->submitter_relationship_to_nominator_other;
                 $this->addLabelText($section, $this->localizeHeading('nominators_relationship'), $nominatorRelationship);
 
-                $this->addLabelText($section, $this->localizeHeading('nominated_for'), '');
+                $nominatedFor = [];
                 if ($nomination->nominate_for_advancement) {
-                    $section->addListItem($this->localizeHeading('advancement'));
-                }
-                if ($nomination->nominate_for_development_programs) {
-                    $section->addListItem($this->localizeHeading('development_programs'));
+                    array_push($nominatedFor, $this->localizeHeading('advancement'));
                 }
                 if ($nomination->nominate_for_lateral_movement) {
-                    $section->addListItem($this->localizeHeading('lateral_movement'));
+                    array_push($nominatedFor, $this->localizeHeading('lateral_movement'));
+                }
+                if ($nomination->nominate_for_development_programs) {
+                    array_push($nominatedFor, $this->localizeHeading('development_programs'));
+                }
+
+                if (count($nominatedFor)) {
+                    $this->addLabelText($section, $this->localizeHeading('nominated_for'), '');
+                    foreach ($nominatedFor as $nominatedFor) {
+                        $section->addListItem($nominatedFor);
+                    }
                 }
 
                 if ($nomination->advancement_reference_id) {
                     $advancementReference = User::findOrFail($nomination->advancement_reference_id);
-                    $this->addLabelText($section, $this->localizeHeading('advancement_secondary_reference'), $advancementReference->getFullName());
-                    $this->addLabelText($section, $this->localizeHeading('references_work_email'), $advancementReference->work_email);
+                    $this->addLabelText($section, $this->localizeHeading('advancement_secondary_reference'), $advancementReference->getFullName() ?? Lang::get('common.not_available', [], $this->lang));
+                    $this->addLabelText($section, $this->localizeHeading('references_work_email'), $advancementReference->work_email ?? Lang::get('common.not_available', [], $this->lang));
                     $this->addLabelText($section, $this->localizeHeading('references_classification'), $advancementReference->currentClassification ? $advancementReference->currentClassification->displayName : Lang::get('common.not_available', [], $this->lang));
                     $this->addLabelText($section, $this->localizeHeading('references_department'), $advancementReference->department ? $advancementReference->department->name[$this->lang] : Lang::get('common.not_available', [], $this->lang));
-                } else {
+                }
+
+                if ($nomination->advancement_reference_fallback_name) {
                     $this->addLabelText($section, $this->localizeHeading('advancement_secondary_reference'), $nomination->advancement_reference_fallback_name);
                     $this->addLabelText($section, $this->localizeHeading('references_work_email'), $nomination->advancement_reference_fallback_work_email);
                     $this->addLabelText($section, $this->localizeHeading('references_classification'), $nomination->advancementReferenceFallbackClassification ? $nomination->advancementReferenceFallbackClassification->displayName : Lang::get('common.not_available', [], $this->lang));
@@ -142,8 +153,11 @@ trait GeneratesNominationDoc
                     $section->addListItem("{$this->localizeHeading('other')} {$nomination->lateral_movement_options_other}");
                 }
 
-                if ($nomination->developmentPrograms()) {
+                if ($nomination->developmentPrograms()->count() > 0 || $nomination->development_program_options_other) {
                     $this->addLabelText($section, $this->localizeHeading('development_program_recommendations'), '');
+                }
+
+                if ($nomination->developmentPrograms()->count() > 0) {
                     $developmentPrograms = $nomination->developmentPrograms()->get();
                     foreach ($developmentPrograms as $developmentProgram) {
                         $section->addListItem($developmentProgram->name[$this->lang]);
@@ -154,20 +168,22 @@ trait GeneratesNominationDoc
                     $section->addListItem("{$this->localizeHeading('other')} {$nomination->development_program_options_other}");
                 }
 
-                $this->addLabelText($section, $this->localizeHeading('rationale'), $nomination->nomination_rationale);
-
-                $this->addLabelText($section, $this->localizeHeading('leadership_competencies'), '');
-                $skills = $nomination->skills()->get();
-                foreach ($skills as $skill) {
-                    $section->addListItem($skill->name[$this->lang]);
+                if ($nomination->nomination_rationale) {
+                    $this->addLabelText($section, $this->localizeHeading('rationale'), $nomination->nomination_rationale);
                 }
 
-                $this->addLabelText($section, $this->localizeHeading('additional_comments'), $nomination->additional_comments);
+                $skills = $nomination->skills()->get();
+                if ($skills->count() > 0) {
+                    $this->addLabelText($section, $this->localizeHeading('leadership_competencies'), '');
+                    foreach ($skills as $skill) {
+                        $section->addListItem($skill->name[$this->lang]);
+                    }
+                }
 
+                if ($nomination->additional_comments) {
+                    $this->addLabelText($section, $this->localizeHeading('additional_comments'), $nomination->additional_comments);
+                }
             }
-        } else {
-            // TODO: Should I add null message if no nominations are found (edge case)?
-            $section->addText('No nominations found.');
         }
     }
 
