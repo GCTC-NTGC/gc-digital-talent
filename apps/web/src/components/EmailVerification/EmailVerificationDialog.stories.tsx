b80ca71dcc063e1c520c1768da188d2119f1d246
Brinda Sasikumar <Brinda.Sasikumar@gmail.com>
2025-08-25 16:36:54 -0400

    draft changes

---
+++ apps/web/src/components/EmailVerification/EmailVerificationDialog.stories.tsx
@@ -0,0 +1,50 @@
+import { StoryFn } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
+
+import { EmailType } from "@gc-digital-talent/graphql";
+
+import EmailVerificationDialog from "./EmailVerificationDialog";
+
+export default {
+  component: EmailVerificationDialog,
+};
+
+const Template: StoryFn<typeof EmailVerificationDialog> = (args) => {
+  return <EmailVerificationDialog {...args} />;
+};
+
+export const ContactEmail = Template.bind({});
+ContactEmail.args = {
+  emailType: EmailType.Contact,
+  emailAddress: "user@example.com",
+  onVerificationSuccess: action("onVerificationSuccess"),
+  onSkip: action("onSkip"),
+};
+
+export const WorkEmail = Template.bind({});
+WorkEmail.args = {
+  emailType: EmailType.Work,
+  emailAddress: "user@canada.ca",
+  onVerificationSuccess: action("onVerificationSuccess"),
+  onSkip: action("onSkip"),
+};
+
+export const ErrorState = Template.bind({});
+ErrorState.args = {
+  emailType: EmailType.Contact,
+  emailAddress: "user@example.com",
+  onVerificationSuccess: action("onVerificationSuccess"),
+  onSkip: action("onSkip"),
+};
+ErrorState.play = ({ canvasElement }) => {
+  // Simulate entering an invalid code and submitting to show error state
+  const input = canvasElement.querySelector('input[name="verificationCode"]');
+  if (input) {
+    input.value = "wrong";
+    input.dispatchEvent(new Event("input", { bubbles: true }));
+  }
+  const form = canvasElement.querySelector("form");
+  if (form) {
+    form.dispatchEvent(new Event("submit", { bubbles: true }));
+  }
+};
