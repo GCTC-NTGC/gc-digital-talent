3eed4b48b886099f932b8998d926a12f2b9cb4ae
Chantelle <mrspasceri@hotmail.com>
2025-09-02 10:27:05 -0700

    update placement type

--- api/lang/en/placement_type.php
+++ api/lang/en/placement_type.php
@@ -2,7 +2,7 @@
 
 return [
     'under_consideration' => 'Under consideration',
-    'placed_tentative' => 'Tentatively placed (Verbal or email confirmation)',
+    'placed_tentative' => 'Offer in progress',
     'placed_casual' => 'Placed casual',
     'placed_term' => 'Placed term',
     'placed_indeterminate' => 'Placed indeterminate',
