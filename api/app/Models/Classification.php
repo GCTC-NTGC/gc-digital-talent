70a593dc294bddfcaf08d591b35893a9156b89c2
Peter Giles <8978655+petertgiles@users.noreply.github.com>
2025-09-04 14:12:48 -0400

    search page

--- api/app/Models/Classification.php
+++ api/app/Models/Classification.php
@@ -61,9 +61,6 @@ class Classification extends Model
 
     /**
      * Used to limit the results for the search page input
-     * to IT up to level 5; PM up to level 6; CR level 4; EX level 3, EX level 4; AS level 3, AS level 5; EC level 2 to 8.
-     *
-     * TODO: Update in #9483 to derive from new column
      */
     public static function scopeAvailableInSearch(Builder $query, bool $availableInSearch)
     {
@@ -71,22 +68,6 @@ class Classification extends Model
             return;
         }
 
-        $query->where(function ($query) {
-            $query->where('group', 'IT')->where('level', '<=', 5);
-        })->orWhere(function ($query) {
-            $query->where('group', 'PM')->where('level', '<=', 6);
-        })->orWhere(function ($query) {
-            $query->where('group', 'CR')->where('level', '=', 4);
-        })->orWhere(function ($query) {
-            $query->where('group', 'EX')->where('level', '=', 3);
-        })->orWhere(function ($query) {
-            $query->where('group', 'EX')->where('level', '=', 4);
-        })->orWhere(function ($query) {
-            $query->where('group', 'AS')->where('level', '=', 3);
-        })->orWhere(function ($query) {
-            $query->where('group', 'AS')->where('level', '=', 5);
-        })->orWhere(function ($query) {
-            $query->where('group', 'EC')->where('level', '>=', 2)->where('level', '<=', 8);
-        });
+        $query->where('is_available_in_search', true);
     }
 }
