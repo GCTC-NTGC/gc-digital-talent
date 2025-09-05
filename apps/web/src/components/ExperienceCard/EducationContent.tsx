08327c1ed649a8a65b459511e0d5043b79d6c5e8
Matt Nigh <matt@mattnigh.com>
2025-09-05 09:13:43 -0400

    Add thesisTitle

--- apps/web/src/components/ExperienceCard/EducationContent.tsx
+++ apps/web/src/components/ExperienceCard/EducationContent.tsx
@@ -9,14 +9,14 @@ import ContentSection from "./ContentSection";
 import { ContentProps } from "./types";
 
 const EducationContent = ({
-  experience: { areaOfStudy, status },
+  experience: { areaOfStudy, status, thesisTitle },
   headingLevel,
 }: ContentProps<Omit<EducationExperience, "user">>) => {
   const intl = useIntl();
   const experienceFormLabels = getExperienceFormLabels(intl);
 
   return (
-    <div className="grid gap-6 sm:grid-cols-2">
+    <div className="grid gap-6 sm:grid-cols-3">
       <ContentSection
         title={experienceFormLabels.areaOfStudy}
         headingLevel={headingLevel}
@@ -27,9 +27,18 @@ const EducationContent = ({
       <ContentSection
         title={experienceFormLabels.educationStatus}
         headingLevel={headingLevel}
+        className="sm:border-r sm:border-gray-200 dark:border-gray-500"
       >
         {status?.label
           ? getLocalizedName(status.label, intl)
+          : intl.formatMessage(commonMessages.notAvailable)}
+      </ContentSection>
+      <ContentSection
+        title={experienceFormLabels.thesisTitle}
+        headingLevel={headingLevel}
+      >
+        {thesisTitle
+          ? thesisTitle
           : intl.formatMessage(commonMessages.notAvailable)}
       </ContentSection>
     </div>
