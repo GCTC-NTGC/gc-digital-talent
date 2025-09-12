16cda2c57f9002c7c956134724f9120ff7d43f73
Peter Giles <8978655+petertgiles@users.noreply.github.com>
2025-09-12 10:06:14 -0400

    disable broken story

--- apps/web/src/components/EmailVerification/EmailVerificationDialog.stories.tsx
+++ apps/web/src/components/EmailVerification/EmailVerificationDialog.stories.tsx
@@ -27,21 +27,21 @@ WorkEmail.args = {
   onVerificationSuccess: action("onVerificationSuccess"),
 };
 
-export const ErrorState = Template.bind({});
-ErrorState.args = {
-  emailType: EmailType.Contact,
-  emailAddress: "user@example.com",
-  onVerificationSuccess: action("onVerificationSuccess"),
-};
-ErrorState.play = ({ canvasElement }) => {
-  // Simulate entering an invalid code and submitting to show error state
-  const input = canvasElement.querySelector('input[name="verificationCode"]');
-  if (input) {
-    input.value = "wrong";
-    input.dispatchEvent(new Event("input", { bubbles: true }));
-  }
-  const form = canvasElement.querySelector("form");
-  if (form) {
-    form.dispatchEvent(new Event("submit", { bubbles: true }));
-  }
-};
+// export const ErrorState = Template.bind({});
+// ErrorState.args = {
+//   emailType: EmailType.Contact,
+//   emailAddress: "user@example.com",
+//   onVerificationSuccess: action("onVerificationSuccess"),
+// };
+// ErrorState.play = ({ canvasElement }) => {
+//   // Simulate entering an invalid code and submitting to show error state
+//   const input = canvasElement.querySelector('input[name="verificationCode"]');
+//   if (input) {
+//     input.value = "wrong";
+//     input.dispatchEvent(new Event("input", { bubbles: true }));
+//   }
+//   const form = canvasElement.querySelector("form");
+//   if (form) {
+//     form.dispatchEvent(new Event("submit", { bubbles: true }));
+//   }
+// };
