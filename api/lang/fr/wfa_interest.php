d57099fde4bddee09d734cf13e6a1f9b9e1c2075
Eric Sizer <eric.sizer@gmail.com>
2025-09-16 15:50:08 -0400

    add translations

---
+++ api/lang/fr/wfa_interest.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'not_applicable' => 'Cette section ne me concerne pas',
+    'term_ending' => 'Mon poste pour une période déterminée n’est pas renouvelé ou prend fin tôt',
+    'letter_received' => 'J’occupe un poste pour une durée indéterminée, mais j’ai reçu une lettre sur le réaménagement des effectifs',
+    'not_sure' => 'Je ne sais pas vraiment si le réaménagement des effectifs touche mon poste',
+    'voluntary_departure' => 'J’occupe un poste pour une durée indéterminée, mais je souhaite quitter volontairement la fonction publique',
+];
