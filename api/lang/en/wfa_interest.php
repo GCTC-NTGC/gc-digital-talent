1cfef03a8efa879406301db0925df3f90b072d22
Eric Sizer <eric.sizer@gmail.com>
2025-09-18 13:06:59 -0400

    update English strings for interest

--- api/lang/en/wfa_interest.php
+++ api/lang/en/wfa_interest.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-    'not_applicable' => 'This section does not apply to me',
-    'term_ending' => 'I have a term position that isn\'t being renewed or is ending early',
-    'letter_received' => 'I\'m an indeterminate employee and have received a workforce adjustment letter',
-    'not_sure' => 'I believe my position may be affected but I\'m not sure',
-    'voluntary_departure' => 'I\'m an indeterminate employee interested in voluntarily leaving the public service',
+    'not_applicable' => 'Not applicable',
+    'term_ending' => 'Term ending',
+    'letter_received' => 'WFA letter received',
+    'not_sure' => 'Not sure',
+    'voluntary_departure' => 'Voluntary departure',
 ];
