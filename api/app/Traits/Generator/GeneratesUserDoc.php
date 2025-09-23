0b127f83335e5acae66e96879b0617aface2882a
Chantelle <mrspasceri@hotmail.com>
2025-09-23 07:40:24 -0700

    fix lint again

--- api/app/Traits/Generator/GeneratesUserDoc.php
+++ api/app/Traits/Generator/GeneratesUserDoc.php
@@ -620,13 +620,13 @@ trait GeneratesUserDoc
                     }
                 }
             }
-               $section->addText($this->localize('common.work_streams'));
-                    collect(Arr::sortRecursive($workStreamsByCommunity))->each(function ($community) use ($section) {
-                        $section->addListItem($community['community'], 0);
-                        foreach ($community['workStreams'] as $workStream) {
-                            $section->addListItem($workStream, 1);
-                        }
-                    });
+            $section->addText($this->localize('common.work_streams'));
+            collect(Arr::sortRecursive($workStreamsByCommunity))->each(function ($community) use ($section) {
+                $section->addListItem($community['community'], 0);
+                foreach ($community['workStreams'] as $workStream) {
+                    $section->addListItem($workStream, 1);
+                }
+            });
         }
     }
 
