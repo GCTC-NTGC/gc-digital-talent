c85375d7d42ab91e00d645ce40e944fb42cae878
Chantelle <82847249+bella77-69@users.noreply.github.com>
2025-09-25 09:04:14 -0700

    [Fix] Featured Skills in Education Experience added to Downloads (#14676)
    * add featured skills in education experince for downloads
    
    * fix lint
    
    * add colon
    
    * add skills to other sections
    
    * move work stream section
    
    * fix lint
    
    * fix lint again
    
    * fix phpunit error
    
    * update snapshots
    
    * only show work streams heading when not empty
    
    * update snapshot
    
    * removed and added colon
    
    * update snapshot

--- api/lang/fr/common.php
+++ api/lang/fr/common.php
@@ -28,10 +28,10 @@ return [
     'not_interested' => 'Non intéressé(e)',
     'essential_skills' => 'Compétences essentielles',
     'nonessential_skills' => 'Compétences constituant un atout',
-    'work_streams' => 'Volets de travail',
+    'work_streams' => 'Volets de travail :',
     'in' => 'à',
     'from' => 'de',
-    'featured_skills' => 'Compétences mises en évidence',
+    'featured_skills' => 'Compétences mises en évidence :',
     // position types
     'en_only' => 'Postes en anglais uniquement',
     'fr_only' => 'Postes en français uniquement',
