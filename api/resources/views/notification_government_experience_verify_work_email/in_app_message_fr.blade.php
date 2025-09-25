35b1d0409033ea48155889029f5c5db572a808aa
Matt <mnigh@users.noreply.github.com>
2025-09-25 14:05:06 -0400

    [Copy] Improves consistency of email verification copy in French (#14708)
    * Change vérifiée to confirmée
    
    * Change Valider dès maintenant to Confirmer maintenant
    
    * Update strings in French

--- api/resources/views/notification_government_experience_verify_work_email/in_app_message_fr.blade.php
+++ api/resources/views/notification_government_experience_verify_work_email/in_app_message_fr.blade.php
@@ -1,1 +1,1 @@
-Vous avez ajouté votre poste actuel au gouvernement du Canada. Vérifiez votre adresse courriel professionnelle pour accéder aux outils réservés aux employées et employés.
+Vous avez ajouté votre poste actuel au gouvernement du Canada. Confirmez votre adresse courriel professionnelle pour accéder aux outils réservés aux employées et employés.
