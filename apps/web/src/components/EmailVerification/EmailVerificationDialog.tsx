07ea46d8cc2dda55fb4cbff50c8dd9486ec0f423
Peter Giles <8978655+petertgiles@users.noreply.github.com>
2025-10-01 09:59:47 -0400

    email verification provider

--- apps/web/src/components/EmailVerification/EmailVerificationDialog.tsx
+++ apps/web/src/components/EmailVerification/EmailVerificationDialog.tsx
@@ -1,39 +1,17 @@
-import { useState, useEffect, ReactNode, useRef, ComponentProps } from "react";
+import { useState, ReactNode } from "react";
 import { useIntl } from "react-intl";
-import {
-  FormProvider,
-  SubmitHandler,
-  useForm,
-  useWatch,
-} from "react-hook-form";
+import { FormProvider, SubmitHandler, useForm } from "react-hook-form";
 import { useMutation } from "urql";
 
 import { Button, Dialog } from "@gc-digital-talent/ui";
 import { Input, Submit } from "@gc-digital-talent/forms";
 import { errorMessages, commonMessages } from "@gc-digital-talent/i18n";
-import { toast } from "@gc-digital-talent/toast";
 import { EmailType, graphql } from "@gc-digital-talent/graphql";
-import {
-  emptyToNull,
-  notEmpty,
-  workEmailDomainRegex,
-} from "@gc-digital-talent/helpers";
 
-import { descriptions, labels, subtitles } from "./messages";
-import RequestACodeContextMessage from "./RequestACodeContextMessage";
+import { subtitles } from "./messages";
 import SubmitACodeContextMessage from "./SubmitACodeContextMessage";
-
-export const CODE_REQUEST_THROTTLE_DELAY_S = 60;
-
-const EmailVerificationRequestACode_Mutation = graphql(/* GraphQL */ `
-  mutation EmailVerificationRequestACode(
-    $input: SendUserEmailsVerificationInput!
-  ) {
-    sendUserEmailsVerification(sendUserEmailsVerificationInput: $input) {
-      id
-    }
-  }
-`);
+import SendVerificationEmailSubform from "./SendVerificationEmailSubform";
+import { useEmailVerification } from "./EmailVerificationProvider";
 
 const EmailVerificationSubmitACode_Mutation = graphql(/* GraphQL */ `
   mutation EmailVerificationSubmitACode($code: String!) {
@@ -43,18 +21,13 @@ const EmailVerificationSubmitACode_Mutation = graphql(/* GraphQL */ `
   }
 `);
 
-interface RequestACodeFormValues {
-  emailAddress: string;
-  emailType: string;
-}
-
 interface SubmitACodeFormValues {
   verificationCode: string;
 }
 
 export interface EmailVerificationProps {
   emailType: EmailType;
-  emailAddress?: string | null;
+  emailAddress: string | null;
   onVerificationSuccess: () => void;
   children?: ReactNode;
   defaultOpen?: boolean;
@@ -68,35 +41,19 @@ export const EmailVerificationDialog = ({
   defaultOpen = false,
 }: EmailVerificationProps) => {
   const intl = useIntl();
-  const [, executeRequestACodeMutation] = useMutation(
-    EmailVerificationRequestACode_Mutation,
-  );
-  const [isOpen, setOpen] = useState<boolean>(defaultOpen);
-  const [emailAddressContacted, setEmailAddressContacted] = useState<
-    string | null
-  >(null); // what address was an email sent to
-  const [requestACodeMessage, setRequestACodeMessage] =
-    useState<ComponentProps<typeof RequestACodeContextMessage>["message"]>(
-      null,
-    ); // messages that can be shown for the top half of the dialog where you can request a code
-  const [submitACodeMessage, setSubmitACodeMessage] =
-    useState<ComponentProps<typeof SubmitACodeContextMessage>["message"]>(null); // messages that can be shown for the bottom half of the dialog where you can submit a code
 
-  const [canRequestCode, setCanRequestCode] = useState<boolean>(true); // can the user request a code (or do they have to wait)
+  const [isOpen, setOpen] = useState<boolean>(defaultOpen);
+
+  const {
+    emailAddressContacted,
+    setRequestACodeMessage,
+    setSubmitACodeMessage,
+    setEmailAddressContacted,
+  } = useEmailVerification();
 
   const [, executeSubmitACodeMutation] = useMutation(
     EmailVerificationSubmitACode_Mutation,
   );
-  const requestACodeFormMethods = useForm<RequestACodeFormValues>({
-    defaultValues: {
-      emailType: dialogEmailType,
-    },
-  });
-
-  const watchEmailAddressInput = useWatch({
-    control: requestACodeFormMethods.control,
-    name: "emailAddress",
-  });
 
   const submitACodeFormMethods = useForm<SubmitACodeFormValues>({
     defaultValues: {
@@ -104,14 +61,12 @@ export const EmailVerificationDialog = ({
     },
   });
 
-  const timerIdRef = useRef<ReturnType<typeof setTimeout>>(null); // timer for throttling requests
-
   // Reset all the states back, for example, when closing the dialog.
   const resetDialog = () => {
     setEmailAddressContacted(null);
     setRequestACodeMessage(null);
     setSubmitACodeMessage(null);
-    setCanRequestCode(true);
+    // setCanRequestCode(true);
     submitACodeFormMethods.reset();
   };
 
@@ -123,110 +78,15 @@ export const EmailVerificationDialog = ({
     setOpen(open);
   };
 
-  useEffect(() => {
-    // When the user can't request a code (for example, they justed request one) then wait before allowing it again.
-    if (!canRequestCode) {
-      timerIdRef.current = setTimeout(() => {
-        setCanRequestCode(true);
-        setRequestACodeMessage(null);
-      }, CODE_REQUEST_THROTTLE_DELAY_S * 1000);
-    }
-
-    // When the user can request a code (for example, the timer expired) then remove the timer
-    if (canRequestCode) {
-      if (timerIdRef.current) {
-        clearTimeout(timerIdRef.current);
-      }
-    }
-
-    return () => {
-      if (timerIdRef.current) {
-        clearTimeout(timerIdRef.current);
-      }
-    };
-  }, [canRequestCode]);
-
-  // watch the form to see if the user changes the address input after requesting a code
-  useEffect(() => {
-    const sentAddress = emptyToNull(emailAddressContacted);
-    const formAddress = emptyToNull(watchEmailAddressInput);
-    if (
-      notEmpty(sentAddress) &&
-      notEmpty(formAddress) &&
-      sentAddress != formAddress
-    ) {
-      // show message
-      setRequestACodeMessage("address-changed");
-      setCanRequestCode(true);
-    } else if (
-      notEmpty(sentAddress) &&
-      notEmpty(formAddress) &&
-      sentAddress == formAddress &&
-      requestACodeMessage == "address-changed"
-    ) {
-      // clear message if they undo the change
-      setRequestACodeMessage(null);
-    }
-  }, [emailAddressContacted, requestACodeMessage, watchEmailAddressInput]);
-
   /*  Populate the initial email address in the form.
    *  This can change if the email address is updated and the dialog is reopened.
    *  Therefore, it is a useEffect instead of using ReactHookForms default values.
    */
-  useEffect(() => {
-    if (initialEmailAddress) {
-      requestACodeFormMethods.setValue("emailAddress", initialEmailAddress);
-    }
-  }, [initialEmailAddress, requestACodeFormMethods]);
-
-  const submitHandlerRequestACode: SubmitHandler<RequestACodeFormValues> = ({
-    emailAddress,
-    emailType,
-  }): Promise<void> => {
-    setRequestACodeMessage(null);
-    setSubmitACodeMessage(null);
-    if (!canRequestCode) {
-      setRequestACodeMessage("throttled");
-      return Promise.resolve();
-    }
-    let emailTypes: EmailType[];
-    switch (emailType) {
-      case EmailType.Contact.toString():
-        if (workEmailDomainRegex.test(emailAddress)) {
-          // Appears to be a valid work email address.  We'll update both at the same time.
-          emailTypes = [EmailType.Contact, EmailType.Work];
-          setSubmitACodeMessage("contact-matches-work");
-        } else {
-          emailTypes = [EmailType.Contact];
-        }
-        break;
-      case EmailType.Work.toString():
-        emailTypes = [EmailType.Work];
-        break;
-      default:
-        throw new Error("Unexpected email type: " + emailType);
-    }
-    const mutationResult = executeRequestACodeMutation({
-      input: {
-        emailAddress,
-        emailTypes,
-      },
-    }).then((result) => {
-      if (!result.data?.sendUserEmailsVerification?.id) {
-        throw new Error("Send email error");
-      }
-    });
-
-    return mutationResult
-      .then(() => {
-        setRequestACodeMessage("request-sent");
-        setCanRequestCode(false);
-        setEmailAddressContacted(emailAddress);
-      })
-      .catch(() => {
-        toast.error(intl.formatMessage(errorMessages.error));
-      });
-  };
+  // useEffect(() => {
+  //   if (initialEmailAddress) {
+  //     requestACodeFormMethods.setValue("emailAddress", initialEmailAddress);
+  //   }
+  // }, [initialEmailAddress, requestACodeFormMethods]);
 
   const submitHandlerSubmitACode: SubmitHandler<SubmitACodeFormValues> = ({
     verificationCode,
@@ -292,59 +152,10 @@ export const EmailVerificationDialog = ({
           })}
         </Dialog.Header>
         <Dialog.Body>
-          {/* "Request a code" part of dialog */}
-          <FormProvider {...requestACodeFormMethods}>
-            <form
-              onSubmit={requestACodeFormMethods.handleSubmit(
-                submitHandlerRequestACode,
-              )}
-              className="mb-6 flex flex-col gap-6"
-            >
-              <p>{intl.formatMessage(descriptions[dialogEmailType])}</p>
-              <div className="flex flex-col gap-2 xs:flex-row">
-                <div className="grow">
-                  <Input
-                    id="emailAddress"
-                    name="emailAddress"
-                    type="email"
-                    label={intl.formatMessage(labels[dialogEmailType])}
-                    rules={{
-                      required: intl.formatMessage(errorMessages.required),
-                      pattern:
-                        dialogEmailType == EmailType.Work
-                          ? {
-                              value: workEmailDomainRegex,
-                              message: intl.formatMessage({
-                                defaultMessage:
-                                  "This does not appear to be a Government of Canada email. If you are entering a Government of Canada email and still getting this error, please contact our support team.",
-                                id: "BLOt/e",
-                                description:
-                                  "Description for rule pattern on work email field",
-                              }),
-                            }
-                          : undefined,
-                    }}
-                  />
-                </div>
-                <div className="w-full self-center xs:w-auto xs:self-end">
-                  <Submit
-                    className="block w-full"
-                    text={intl.formatMessage({
-                      defaultMessage: "Send verification email",
-                      id: "xKj/Lr",
-                      description: "Button to send verification code",
-                    })}
-                    submittedText={intl.formatMessage({
-                      defaultMessage: "Send verification email",
-                      id: "xKj/Lr",
-                      description: "Button to send verification code",
-                    })}
-                  />
-                </div>
-              </div>
-              <RequestACodeContextMessage message={requestACodeMessage} />
-            </form>
-          </FormProvider>
+          <SendVerificationEmailSubform
+            emailType={dialogEmailType}
+            emailAddress={initialEmailAddress}
+          />
           {/* "Submit a code" part of dialog */}
           <FormProvider {...submitACodeFormMethods}>
             <form
@@ -368,7 +179,7 @@ export const EmailVerificationDialog = ({
                     }}
                   />
                 ) : null}
-                <SubmitACodeContextMessage message={submitACodeMessage} />
+                <SubmitACodeContextMessage />
               </div>
               <Dialog.Footer>
                 <Submit
