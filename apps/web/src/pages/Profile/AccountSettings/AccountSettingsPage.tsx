180f9ea1ef793a6e6c746eae23dc509509ebe7c2
Yoni K <yonikid15@gmail.com>
2025-10-02 11:24:14 -0400

    Add personal info and email cards to account settings page

--- apps/web/src/pages/Profile/AccountSettings/AccountSettingsPage.tsx
+++ apps/web/src/pages/Profile/AccountSettings/AccountSettingsPage.tsx
@@ -1,16 +1,20 @@
 import { useIntl, defineMessage } from "react-intl";
 import Cog8ToothIcon from "@heroicons/react/24/outline/Cog8ToothIcon";
-import { useQuery } from "urql";
-import { ReactNode } from "react";
+import { useMutation, useQuery } from "urql";
 
 import {
   Button,
   Container,
+  NotFound,
   Pending,
   TableOfContents,
-  ThrowNotFound,
 } from "@gc-digital-talent/ui";
-import { EmailType, graphql } from "@gc-digital-talent/graphql";
+import {
+  EmailType,
+  FragmentType,
+  getFragment,
+  graphql,
+} from "@gc-digital-talent/graphql";
 import { unpackMaybes } from "@gc-digital-talent/helpers";
 import { ROLE_NAME } from "@gc-digital-talent/auth";
 import { toast } from "@gc-digital-talent/toast";
@@ -20,30 +24,69 @@ import useBreadcrumbs from "~/hooks/useBreadcrumbs";
 import useRoutes from "~/hooks/useRoutes";
 import SEO from "~/components/SEO/SEO";
 import Hero from "~/components/Hero";
-import profileMessages from "~/messages/profileMessages";
 import RequireAuth from "~/components/RequireAuth/RequireAuth";
 import EmailVerificationDialog from "~/components/EmailVerification/EmailVerificationDialog";
+import PersonalInformation from "~/components/Profile/components/PersonalInformation/PersonalInformation";
+import { SectionProps } from "~/components/Profile/types";
+import StatusItem, { Status } from "~/components/StatusItem/StatusItem";
+import { aboutSectionHasEmptyRequiredFields } from "~/validators/profile";
+import messages from "~/messages/profileMessages";
+import ContactEmailCard from "~/components/ContactEmailCard/ContactEmailCard";
+import WorkEmailCard from "~/components/WorkEmailCard.tsx/WorkEmailCard";
 
 import AccountManagement from "./AccountManagement";
 import NotificationSettings from "./NotificationSettings";
 
-const AccountSettings_Query = graphql(/* GraphQL */ `
-  query AccountSettings {
-    me {
+const PersonalInformation_Fragment = graphql(/** GraphQL */ `
+  fragment PersonalInformation on User {
+    id
+    firstName
+    lastName
+    telephone
+    email
+    isEmailVerified
+    workEmail
+    isWorkEmailVerified
+    preferredLang {
+      value
+    }
+    preferredLanguageForInterview {
+      value
+    }
+    preferredLanguageForExam {
+      value
+    }
+    citizenship {
+      value
+    }
+    armedForcesStatus {
+      value
+    }
+    enabledEmailNotifications
+    enabledInAppNotifications
+    ...ProfilePersonalInformation
+    ...ContactEmailCard
+    ...WorkEmailCard
+  }
+`);
+
+const ProfileUpdateUser_Mutation = graphql(/* GraphQL */ `
+  mutation UpdateUserAsUser($id: ID!, $user: UpdateUserAsUserInput!) {
+    updateUserAsUser(id: $id, user: $user) {
       id
-      email
-      workEmail
-      enabledEmailNotifications
-      enabledInAppNotifications
     }
   }
 `);
 
-export type SectionKey = "accountManagement" | "notificationSettings";
+export type SectionKey =
+  | "personalInfo"
+  | "notificationSettings"
+  | "accountManagement";
 
 interface Section {
   id: string;
-  title: ReactNode;
+  title: string;
+  status?: Status;
 }
 
 const pageTitle = defineMessage({
@@ -58,13 +101,18 @@ const subTitle = defineMessage({
   description: "Subtitle for the account settings page.",
 });
 
-const AccountSettingsPage = () => {
+interface AccountSettingsProps {
+  personalInfoQuery: FragmentType<typeof PersonalInformation_Fragment>;
+}
+
+const AccountSettings = ({ personalInfoQuery }: AccountSettingsProps) => {
   const intl = useIntl();
   const paths = useRoutes();
 
-  const [{ data, fetching, error }] = useQuery({
-    query: AccountSettings_Query,
-  });
+  const personalInfo = getFragment(
+    PersonalInformation_Fragment,
+    personalInfoQuery,
+  );
 
   // hide dialog triggers unless wanting to test them
   const searchParams = new URLSearchParams(window.location.search);
@@ -72,16 +120,27 @@ const AccountSettingsPage = () => {
     searchParams.get("test-dialog-triggers") === "true";
 
   const enabledEmailNotifications = unpackMaybes(
-    data?.me?.enabledEmailNotifications,
+    personalInfo.enabledEmailNotifications,
   );
   const enabledInAppNotifications = unpackMaybes(
-    data?.me?.enabledInAppNotifications,
+    personalInfo.enabledInAppNotifications,
   );
 
   const formattedPageTitle = intl.formatMessage(pageTitle);
   const formattedSubTitle = intl.formatMessage(subTitle);
 
   const sections: Record<SectionKey, Section> = {
+    personalInfo: {
+      id: "personal-info",
+      title: intl.formatMessage({
+        defaultMessage: "Personal and contact information",
+        id: "GMLXGl",
+        description: "Title for the personal and contact info settings.",
+      }),
+      status: aboutSectionHasEmptyRequiredFields(personalInfo)
+        ? "error"
+        : "success",
+    },
     notificationSettings: {
       id: "notification-settings",
       title: intl.formatMessage({
@@ -109,143 +168,185 @@ const AccountSettingsPage = () => {
     ],
   });
 
+  const [{ fetching: isUpdating }, executeUpdateMutation] = useMutation(
+    ProfileUpdateUser_Mutation,
+  );
+
+  const handleUpdate: SectionProps["onUpdate"] = async (userId, userData) => {
+    return executeUpdateMutation({
+      id: userId,
+      user: userData,
+    }).then((res) => res.data?.updateUserAsUser);
+  };
+
+  const sectionProps = {
+    query: personalInfo,
+    isUpdating,
+    onUpdate: handleUpdate,
+    pool: null,
+  };
+
+  return (
+    <>
+      <SEO title={formattedPageTitle} description={formattedSubTitle} />
+      <Hero
+        title={formattedPageTitle}
+        subtitle={formattedSubTitle}
+        crumbs={crumbs}
+      />
+      <Container className="my-18">
+        <TableOfContents.Wrapper>
+          <TableOfContents.Navigation>
+            <TableOfContents.List>
+              {Object.values(sections).map((section) => {
+                if (section.status) {
+                  return (
+                    <TableOfContents.ListItem
+                      key={section.id}
+                      className="-ml-7px list-none"
+                    >
+                      <StatusItem
+                        asListItem={false}
+                        title={section.title}
+                        status={section.status}
+                        scrollTo={section.id}
+                      />
+                    </TableOfContents.ListItem>
+                  );
+                }
+                return (
+                  <TableOfContents.ListItem key={section.id}>
+                    <TableOfContents.AnchorLink id={section.id}>
+                      {section.title}
+                    </TableOfContents.AnchorLink>
+                  </TableOfContents.ListItem>
+                );
+              })}
+            </TableOfContents.List>
+          </TableOfContents.Navigation>
+          <TableOfContents.Content>
+            <TableOfContents.Section id={sections.personalInfo.id}>
+              <div className="grid grid-cols-1 gap-1.5 xs:grid-cols-2">
+                <div className="col-span-2">
+                  <PersonalInformation {...sectionProps} />
+                </div>
+                <ContactEmailCard query={personalInfo} />
+                <WorkEmailCard query={personalInfo} />
+              </div>
+            </TableOfContents.Section>
+            <TableOfContents.Section
+              id={sections.notificationSettings.id}
+              className="mt-12 xs:mt-18"
+            >
+              <TableOfContents.Heading
+                size="h3"
+                icon={Cog8ToothIcon}
+                color="secondary"
+                className="mt-0 mb-6"
+              >
+                {sections.notificationSettings.title}
+              </TableOfContents.Heading>
+              <p className="mb-6">
+                {intl.formatMessage({
+                  defaultMessage:
+                    "The settings provided in this section allow you to control the types of notifications you receive and where they are delivered. Email notifications are delivered to the contact email provided on your profile, while in-app notifications are delivered to the notification pane found in the main menu.",
+                  id: "J/gp3y",
+                  description:
+                    "Subtitle for notification settings section on account settings page.",
+                })}
+              </p>
+              <NotificationSettings
+                enabledEmailNotifications={enabledEmailNotifications}
+                enabledInAppNotifications={enabledInAppNotifications}
+              />
+            </TableOfContents.Section>
+            <TableOfContents.Section
+              id={sections.accountManagement.id}
+              className="mt-12 xs:mt-18"
+            >
+              <TableOfContents.Heading
+                size="h3"
+                icon={Cog8ToothIcon}
+                color="primary"
+                className="mt-0 mb-6"
+              >
+                {sections.accountManagement.title}
+              </TableOfContents.Heading>
+              {showTestDialogTriggers ? (
+                <>
+                  <p>
+                    {intl.formatMessage(commonMessages.email)}
+                    {intl.formatMessage(commonMessages.dividingColon)}
+                    {personalInfo.email}
+                  </p>
+                  <p></p>
+                  <p>
+                    {intl.formatMessage(commonMessages.workEmail)}
+                    {intl.formatMessage(commonMessages.dividingColon)}
+                    {personalInfo.workEmail}
+                  </p>
+                  <p>
+                    <EmailVerificationDialog
+                      defaultOpen={false}
+                      emailType={EmailType.Work}
+                      emailAddress={personalInfo.workEmail}
+                      onVerificationSuccess={function (): void {
+                        toast.info(
+                          "EmailVerificationDialog work onVerificationSuccess",
+                        );
+                      }}
+                    >
+                      <Button mode="inline">
+                        {intl.formatMessage({
+                          defaultMessage: "Verify a GC work email",
+                          id: "Vd9VIn",
+                          description: "Link to update the work email",
+                        })}
+                      </Button>
+                    </EmailVerificationDialog>
+                  </p>
+                </>
+              ) : null}
+              <p className="mb-6">
+                {intl.formatMessage({
+                  defaultMessage:
+                    "This section focuses on general account management and information related to how we link to your GCKey.",
+                  id: "G6PxDn",
+                  description:
+                    "Subtitle for account management section on account settings page.",
+                })}
+              </p>
+              <AccountManagement />
+            </TableOfContents.Section>
+          </TableOfContents.Content>
+        </TableOfContents.Wrapper>
+      </Container>
+    </>
+  );
+};
+
+const AccountSettings_Query = graphql(/* GraphQL */ `
+  query AccountSettings {
+    me {
+      ...PersonalInformation
+    }
+  }
+`);
+
+export const AccountSettingsPage = () => {
+  const intl = useIntl();
+  const [{ data, fetching, error }] = useQuery({
+    query: AccountSettings_Query,
+  });
+
   return (
     <Pending fetching={fetching} error={error}>
       {data?.me ? (
-        <>
-          <SEO title={formattedPageTitle} description={formattedSubTitle} />
-          <Hero
-            title={formattedPageTitle}
-            subtitle={formattedSubTitle}
-            crumbs={crumbs}
-          />
-          <Container className="my-18">
-            <TableOfContents.Wrapper>
-              <TableOfContents.Navigation>
-                <TableOfContents.List>
-                  {Object.values(sections).map((section) => {
-                    return (
-                      <TableOfContents.ListItem key={section.id}>
-                        <TableOfContents.AnchorLink id={section.id}>
-                          {section.title}
-                        </TableOfContents.AnchorLink>
-                      </TableOfContents.ListItem>
-                    );
-                  })}
-                </TableOfContents.List>
-              </TableOfContents.Navigation>
-              <TableOfContents.Content>
-                <TableOfContents.Section
-                  id={sections.notificationSettings.id}
-                  className="mt-12 xs:mt-18"
-                >
-                  <TableOfContents.Heading
-                    size="h3"
-                    icon={Cog8ToothIcon}
-                    color="secondary"
-                    className="mt-0 mb-6"
-                  >
-                    {sections.notificationSettings.title}
-                  </TableOfContents.Heading>
-                  <p className="mb-6">
-                    {intl.formatMessage({
-                      defaultMessage:
-                        "The settings provided in this section allow you to control the types of notifications you receive and where they are delivered. Email notifications are delivered to the contact email provided on your profile, while in-app notifications are delivered to the notification pane found in the main menu.",
-                      id: "J/gp3y",
-                      description:
-                        "Subtitle for notification settings section on account settings page.",
-                    })}
-                  </p>
-                  <NotificationSettings
-                    enabledEmailNotifications={enabledEmailNotifications}
-                    enabledInAppNotifications={enabledInAppNotifications}
-                  />
-                </TableOfContents.Section>
-                <TableOfContents.Section
-                  id={sections.accountManagement.id}
-                  className="mt-12 xs:mt-18"
-                >
-                  <TableOfContents.Heading
-                    size="h3"
-                    icon={Cog8ToothIcon}
-                    color="primary"
-                    className="mt-0 mb-6"
-                  >
-                    {sections.accountManagement.title}
-                  </TableOfContents.Heading>
-                  {showTestDialogTriggers ? (
-                    <>
-                      <p>
-                        {intl.formatMessage(commonMessages.email)}
-                        {intl.formatMessage(commonMessages.dividingColon)}
-                        {data.me.email}
-                      </p>
-                      <p>
-                        <EmailVerificationDialog
-                          defaultOpen={false}
-                          emailType={EmailType.Contact}
-                          emailAddress={data.me.email}
-                          onVerificationSuccess={function (): void {
-                            toast.info(
-                              "EmailVerificationDialog contact onVerificationSuccess",
-                            );
-                          }}
-                        >
-                          <Button mode="inline">
-                            {intl.formatMessage({
-                              defaultMessage: "Update contact email",
-                              id: "Xc3Y7t",
-                              description: "Link to update the contact email",
-                            })}
-                          </Button>
-                        </EmailVerificationDialog>
-                      </p>
-                      <p>
-                        {intl.formatMessage(commonMessages.workEmail)}
-                        {intl.formatMessage(commonMessages.dividingColon)}
-                        {data.me.workEmail}
-                      </p>
-                      <p>
-                        <EmailVerificationDialog
-                          defaultOpen={false}
-                          emailType={EmailType.Work}
-                          emailAddress={data.me.workEmail}
-                          onVerificationSuccess={function (): void {
-                            toast.info(
-                              "EmailVerificationDialog work onVerificationSuccess",
-                            );
-                          }}
-                        >
-                          <Button mode="inline">
-                            {intl.formatMessage({
-                              defaultMessage: "Verify a GC work email",
-                              id: "Vd9VIn",
-                              description: "Link to update the work email",
-                            })}
-                          </Button>
-                        </EmailVerificationDialog>
-                      </p>
-                    </>
-                  ) : null}
-                  <p className="mb-6">
-                    {intl.formatMessage({
-                      defaultMessage:
-                        "This section focuses on general account management and information related to how we link to your GCKey.",
-                      id: "G6PxDn",
-                      description:
-                        "Subtitle for account management section on account settings page.",
-                    })}
-                  </p>
-                  <AccountManagement />
-                </TableOfContents.Section>
-              </TableOfContents.Content>
-            </TableOfContents.Wrapper>
-          </Container>
-        </>
+        <AccountSettings personalInfoQuery={data?.me} />
       ) : (
-        <ThrowNotFound
-          message={intl.formatMessage(profileMessages.userNotFound)}
-        />
+        <NotFound headingMessage={intl.formatMessage(commonMessages.notFound)}>
+          <p>{intl.formatMessage(messages.userNotFound)}</p>
+        </NotFound>
       )}
     </Pending>
   );
