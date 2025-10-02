2e0586eb1d944066d9ab21c05e8cf3a5897c9e31
Yoni K <yonikid15@gmail.com>
2025-10-02 11:03:14 -0400

    Update email verification status component

--- apps/web/src/components/Profile/components/EmailVerificationStatus.tsx
+++ apps/web/src/components/Profile/components/EmailVerificationStatus.tsx
@@ -1,56 +1,27 @@
 import { useIntl } from "react-intl";
+import CheckCircleIcon from "@heroicons/react/24/solid/CheckCircleIcon";
 
-import { Button, Chip } from "@gc-digital-talent/ui";
+import { Maybe } from "@gc-digital-talent/graphql";
 
 interface EmailVerificationStatusProps {
-  isEmailVerified?: boolean;
-  readOnly?: boolean;
-  onClickVerify: () => Promise<void>;
+  isEmailVerified?: Maybe<boolean>;
 }
 
 const EmailVerificationStatus = ({
   isEmailVerified,
-  onClickVerify,
-  readOnly,
 }: EmailVerificationStatusProps) => {
   const intl = useIntl();
 
   return isEmailVerified ? (
-    <Chip color="success" className="-mb-0.25">
-      {intl.formatMessage({
+    <CheckCircleIcon
+      className="size-6 text-success"
+      aria-label={intl.formatMessage({
         defaultMessage: "Verified",
         id: "GMglI5",
         description: "The email address has been verified to be owned by user",
       })}
-    </Chip>
-  ) : (
-    <>
-      <Chip color="error" className="-mb-0.25">
-        {intl.formatMessage({
-          defaultMessage: "Unverified",
-          id: "tUIvbq",
-          description:
-            "The email address has not been verified to be owned by user",
-        })}
-      </Chip>
-      {!readOnly && (
-        <Button
-          type="button"
-          mode="inline"
-          color="error"
-          className="-mt-0.25"
-          onClick={onClickVerify}
-        >
-          {intl.formatMessage({
-            defaultMessage: "Verify now",
-            id: "ADPfNp",
-            description:
-              "Button to start the email address verification process",
-          })}
-        </Button>
-      )}
-    </>
-  );
+    />
+  ) : null;
 };
 
 export default EmailVerificationStatus;
