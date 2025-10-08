0f600017c63353e9b3f22c1c7058c5f856d4e157
Eric Sizer <eric.sizer@gmail.com>
2025-10-08 10:37:36 -0400

    update banner copy

--- apps/web/src/components/WfaBanner/WfaBanner.tsx
+++ apps/web/src/components/WfaBanner/WfaBanner.tsx
@@ -31,28 +31,52 @@ const WfaBanner = () => {
       <Alert.Title>
         {intl.formatMessage({
           defaultMessage:
-            "Are you in the Digital community and need support through workforce adjustment?",
-          id: "jT3Ytq",
+            "New platform services to support employees facing workforce adjustment",
+          id: "eI2/4I",
           description: "Title for banner about workforce adjustment",
         })}
       </Alert.Title>
       <p className="my-6">
         {intl.formatMessage({
           defaultMessage:
-            "If you're working in the core public administration and you find yourself in one of the following groups, we may be able to help.",
-          id: "0Grydu",
+            "If you're a Government of Canada employee in one of the functional communities supported by this platform and you find yourself in one of the following groups, we may be able to help.",
+          id: "z+emiI",
           description:
             "Lead in text for who workforce adjustment tools can help",
         })}
       </p>
       <Ul space="md" className="mb-6">
-        <li></li>
+        <li>
+          {intl.formatMessage({
+            defaultMessage:
+              "You're an indeterminate employee who’s received a workforce adjustment letter and you’re looking for a new job or an alternation",
+            id: "QiOSUi",
+            description:
+              "Item one, indeterminate employee that received wfa letter",
+          })}
+        </li>
+        <li>
+          {intl.formatMessage({
+            defaultMessage:
+              "You're an indeterminate employee in a stable role and you’re interested in a financial package to leave the public service",
+            id: "cT3RkN",
+            description:
+              "Item two, indeterminate employee interested in a package",
+          })}
+        </li>
+        <li>
+          {intl.formatMessage({
+            defaultMessage:
+              "You're in a term position that isn't being renewed or is ending earlier than expected and you’re hoping to find a new role quickly",
+            id: "VGu61z",
+            description: "Item two, term employee that is not being renewed",
+          })}
+        </li>
       </Ul>
       <Link href="#">
         {intl.formatMessage({
-          defaultMessage:
-            "Visit our work force adjustment page to learn more about your options.",
-          id: "IzTyKU",
+          defaultMessage: "Learn more about new services to support you.",
+          id: "wlfZcP",
           description: "Link text to the workforce adjustment information page",
         })}
       </Link>
