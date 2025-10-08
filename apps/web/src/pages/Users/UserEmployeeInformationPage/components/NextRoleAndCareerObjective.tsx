eead7b3dd1670a3a77b78d913c06fe4b11d7ba1a
Chantelle <82847249+bella77-69@users.noreply.github.com>
2025-10-08 13:02:29 -0700

    [Refactor] GC Profile Career Planning Sections to Accordions (#14760)
    * update sections to accordions
    
    * update styling in accordion sections
    
    * update links
    
    * add nextroleandcareerobjective component
    
    * add header subtitle prop to dialogs, translations
    
    * add todo, add keys for translations
    
    * update snapshot
    
    * add translations
    
    * rename careerObjectiveDialogSubtitle and nextRoleDialogSubtitle to dialogSubtitle
    
    * import graphQL fragments instead of redefining
    
    * remove duplicated translation
    
    * update snapshot
    
    * fix lint

---
+++ apps/web/src/pages/Users/UserEmployeeInformationPage/components/NextRoleAndCareerObjective.tsx
@@ -0,0 +1,111 @@
+import { useIntl } from "react-intl";
+
+import { FragmentType, getFragment } from "@gc-digital-talent/graphql";
+import { Well } from "@gc-digital-talent/ui";
+
+import { hasAllEmptyFields as hasAllEmptyFieldsNextRole } from "~/validators/employeeProfile/nextRole";
+import { hasAllEmptyFields as hasAllEmptyFieldsCareerObjective } from "~/validators/employeeProfile/careerObjective";
+import NextRolePreview from "~/components/NextRoleAndCareerObjective/NextRolePreview";
+import CareerObjectivePreview from "~/components/NextRoleAndCareerObjective/CareerObjectivePreview";
+import {
+  CareerObjectiveInfo_Fragment,
+  NextRoleInfo_Fragment,
+} from "~/components/NextRoleAndCareerObjective/utils";
+import { NextRoleAndCareerObjective_Fragment } from "~/components/NextRoleAndCareerObjective/NextRoleAndCareerObjective";
+
+interface NextRoleAndCareerObjectiveProps {
+  nextRoleAndCareerObjectiveQuery: FragmentType<
+    typeof NextRoleAndCareerObjective_Fragment
+  >;
+  sectionKey: string;
+  nextRoleDialogSubtitle?: string;
+  dialogSubtitle?: string;
+}
+
+const NextRoleAndCareerObjective = ({
+  nextRoleAndCareerObjectiveQuery,
+  nextRoleDialogSubtitle,
+  dialogSubtitle,
+}: NextRoleAndCareerObjectiveProps) => {
+  const intl = useIntl();
+  const nextRoleAndCareerObjective = getFragment(
+    NextRoleAndCareerObjective_Fragment,
+    nextRoleAndCareerObjectiveQuery,
+  );
+
+  const nextRole = getFragment(
+    NextRoleInfo_Fragment,
+    nextRoleAndCareerObjective.employeeProfile,
+  );
+
+  const careerObjective = getFragment(
+    CareerObjectiveInfo_Fragment,
+    nextRoleAndCareerObjective.employeeProfile,
+  );
+
+  const hasNextRole = !hasAllEmptyFieldsNextRole({ ...nextRole });
+  const hasCareerObjective = !hasAllEmptyFieldsCareerObjective({
+    ...careerObjective,
+  });
+
+  if (!hasNextRole && !hasCareerObjective) {
+    return (
+      <Well>
+        <p>
+          {intl.formatMessage({
+            defaultMessage:
+              "The employee hasn't added any information about their next role or career objective to their profile",
+            id: "ctOBXo",
+            description:
+              "Message displayed if employee hasn't filled out next role or career objective",
+          })}
+        </p>
+      </Well>
+    );
+  }
+  return (
+    <div className="space-y-6">
+      {hasNextRole ? (
+        <NextRolePreview
+          nextRolePreviewQuery={nextRoleAndCareerObjective}
+          dialogSubtitle={nextRoleDialogSubtitle}
+        />
+      ) : (
+        <Well>
+          <p>
+            {intl.formatMessage({
+              defaultMessage:
+                "The employee has not provided information about the next role they'd like to achieve.",
+              id: "U5plJQ",
+              description:
+                "Message displayed if employee hasn't filled out next role",
+            })}
+          </p>
+        </Well>
+      )}
+
+      {/* Career Objective Section */}
+
+      {hasCareerObjective ? (
+        <CareerObjectivePreview
+          careerObjectivePreviewQuery={nextRoleAndCareerObjective}
+          dialogSubtitle={dialogSubtitle}
+        />
+      ) : (
+        <Well>
+          <p>
+            {intl.formatMessage({
+              defaultMessage:
+                "The employee has not provided information about their ultimate career objective.",
+              id: "ZhccbH",
+              description:
+                "Message displayed if employee hasn't filled out career objective",
+            })}
+          </p>
+        </Well>
+      )}
+    </div>
+  );
+};
+
+export default NextRoleAndCareerObjective;
