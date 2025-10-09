f3db16f81d654fe2313a5740c4ec50e672663652
Mauli Soni <maulisonitbs01@gmail.com>
2025-10-09 10:21:34 -0400

    [Playwright Test] Verify Create Department  (#14782)
    * Add generateUniqueNumber function
    
    * Add Create department test
    
    Co-authored-by: Mauli Soni <mauli0110@users.noreply.github.com>
    
    * delete_mutation
    
    * fix_lint
    
    * delete_department_mutation_update
    
    ---------
    
    Co-authored-by: Matt Nigh <matt@mattnigh.com>
    Co-authored-by: Mauli Soni <mauli0110@users.noreply.github.com>

---
+++ apps/playwright/tests/admin/department-action-create.spec.ts
@@ -0,0 +1,61 @@
+import { test, expect } from "~/fixtures";
+import { loginBySub } from "~/utils/auth";
+import { deleteDepartment } from "~/utils/departments";
+import graphql from "~/utils/graphql";
+import {
+  generateUniqueNumber,
+  generateUniqueTestId,
+  fetchIdentificationNumber,
+} from "~/utils/id";
+
+test("Create department", async ({ appPage }) => {
+  const uniqueTestId = generateUniqueTestId();
+  const uniqueDepartmentNumber = generateUniqueNumber();
+  const DEPARTMENT_TITLE = `Test department ${uniqueTestId}`;
+  const adminCtx = await graphql.newContext();
+  await loginBySub(appPage.page, "platform@test.com");
+  await appPage.page.goto("/en/admin/settings/departments");
+  await appPage.waitForGraphqlResponse("Departments");
+
+  await appPage.page.getByRole("link", { name: /create department/i }).click();
+  await appPage.waitForGraphqlResponse("CreateDepartmentOptions");
+
+  await appPage.page
+    .getByRole("textbox", { name: /name \(english\)/i })
+    .fill(`${DEPARTMENT_TITLE} (EN)`);
+
+  await appPage.page
+    .getByRole("textbox", { name: /name \(french\)/i })
+    .fill(`${DEPARTMENT_TITLE} (FR)`);
+
+  await appPage.page
+    .getByRole("spinbutton", { name: /department number/i })
+    .fill(uniqueDepartmentNumber);
+
+  await appPage.page
+    .getByRole("spinbutton", { name: /organization id/i })
+    .fill(`1`);
+
+  await appPage.page
+    .getByRole("combobox", { name: /department size/i })
+    .selectOption({ label: "Small (up to 1000 employees)" });
+
+  await appPage.page
+    .getByRole("button", { name: /create department/i })
+    .click();
+  await appPage.waitForGraphqlResponse("CreateDepartment");
+  await expect(appPage.page.getByRole("alert").last()).toContainText(
+    /department created successfully/i,
+  );
+  await appPage.waitForGraphqlResponse("ViewDepartmentPage");
+  await expect(
+    appPage.page.getByRole("heading", {
+      name: /department information/i,
+    }),
+  ).toBeVisible();
+
+  const departmentID = fetchIdentificationNumber(appPage.page.url());
+  await deleteDepartment(adminCtx, {
+    id: departmentID,
+  });
+});
