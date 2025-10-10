dc18e236ae94ed43dcec65d963ba715147cb97f8
Peter Giles <8978655+petertgiles@users.noreply.github.com>
2025-10-10 16:28:45 -0400

    storybook and jest

--- apps/web/src/pages/Auth/RegistrationPages/EmployeeInformationPage/EmployeeInformationForm.stories.tsx
+++ apps/web/src/pages/Auth/RegistrationPages/EmployeeInformationPage/EmployeeInformationForm.stories.tsx
@@ -1,36 +1,62 @@
 import { Meta, StoryFn } from "@storybook/react-vite";
 import { action } from "storybook/actions";
 
-import {
-  fakeClassifications,
-  fakeDepartments,
-} from "@gc-digital-talent/fake-data";
-import { makeFragmentData } from "@gc-digital-talent/graphql";
+import { MockGraphqlDecorator } from "@gc-digital-talent/storybook-helpers";
+import { EmploymentCategory } from "@gc-digital-talent/graphql";
 
-import {
-  EmployeeInformationForm,
-  EmployeeInformation_QueryFragment,
-} from "./EmployeeInformationPage";
+import { EmployeeInformationForm } from "./EmployeeInformationPage";
 
-const departments = fakeDepartments();
-const classifications = fakeClassifications();
-const mockFragmentData = makeFragmentData(
-  {
-    departments,
-    classifications,
+export const employmentCategoriesApiResponse = {
+  data: {
+    employmentCategoryTypes: [
+      {
+        value: EmploymentCategory.ExternalOrganization,
+        label: {
+          en: "External organization",
+          fr: "Organisation externe",
+        },
+        __typename: "LocalizedEmploymentCategory",
+      },
+      {
+        value: EmploymentCategory.GovernmentOfCanada,
+        label: {
+          en: "Government of Canada",
+          fr: "Gouvernement du Canada",
+        },
+        __typename: "LocalizedEmploymentCategory",
+      },
+      {
+        value: EmploymentCategory.CanadianArmedForces,
+        label: {
+          en: "Canadian Armed Forces",
+          fr: "Forces arm\u00e9es canadiennes",
+        },
+        __typename: "LocalizedEmploymentCategory",
+      },
+    ],
   },
-  EmployeeInformation_QueryFragment,
-);
+} as const;
 
 export default {
   component: EmployeeInformationForm,
+  decorators: [MockGraphqlDecorator],
+  parameters: {
+    apiResponsesConfig: {
+      latency: {
+        min: 0,
+        max: 0,
+      },
+    },
+    apiResponses: {
+      WorkFieldOptions: employmentCategoriesApiResponse,
+    },
+  },
 } as Meta<typeof EmployeeInformationForm>;
 
 const Template: StoryFn<typeof EmployeeInformationForm> = () => {
   return (
     <EmployeeInformationForm
-      cacheKey=""
-      query={mockFragmentData}
+      navigationTarget="#"
       // Note: Needed for function colouring
       // eslint-disable-next-line @typescript-eslint/require-await
       onSubmit={async (data) => {
