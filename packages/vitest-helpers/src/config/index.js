fe1473be3fbab536a65290f66c2f807e8058581d
Eric Sizer <eric.sizer@gmail.com>
2025-10-22 14:48:31 -0400

    update to mjs

--- packages/vitest-helpers/src/config/index.js
+++
@@ -1,4 +0,0 @@
-import baseConfig from "./base-config.js";
-import uiConfig from "./ui-config.js";
-
-export { baseConfig, uiConfig };
---
+++ packages/vitest-helpers/src/config/index.mjs
@@ -0,0 +1,4 @@
+import baseConfig from "./base-config.mjs";
+import uiConfig from "./ui-config.mjs";
+
+export { baseConfig, uiConfig };
