1c2bfe5e4230cf872e4da884fd58e3036ca60704
Eric Sizer <eric.sizer@gmail.com>
2025-10-23 12:14:10 -0400

    create department fixture

---
+++ apps/playwright/fixtures/Department.ts
@@ -0,0 +1,169 @@
+import { Locator, type Page } from "@playwright/test";
+
+import {
+  CreateDepartmentInput,
+  DepartmentSize,
+  InputMaybe,
+  LocalizedStringInput,
+  UpdateDepartmentInput,
+} from "@gc-digital-talent/graphql";
+
+import AppPage from "./AppPage";
+
+const FIELD = {
+  NAME_EN: "nameEn",
+  NAME_FR: "nameFr",
+  NUMBER: "number",
+  ORG_ID: "orgId",
+  TYPE: "type",
+  SIZE: "size",
+} as const;
+
+type ObjectValues<T> = T[keyof T];
+export type Field = ObjectValues<typeof FIELD>;
+
+type DepartmentTypeInput = Pick<
+  UpdateDepartmentInput,
+  | "isScience"
+  | "isRegulatory"
+  | "isCentralAgency"
+  | "isCorePublicAdministration"
+>;
+
+type DepartmentType = keyof DepartmentTypeInput;
+
+/**
+ * Department
+ *
+ * Page containing utilities to interact with departments
+ */
+class Department extends AppPage {
+  readonly locators: Record<Field, Locator>;
+  readonly baseUrl: string = "/en/admin/settings/departments";
+  readonly sizeMap = new Map<DepartmentSize, string>([
+    [DepartmentSize.Micro, "Micro (up to 250 employees)"],
+    [DepartmentSize.Small, "Small (up to 1000 employees)"],
+    [DepartmentSize.Medium, "Medium (up to 5000 employees)"],
+    [DepartmentSize.Large, "Large (more than 5000 employees)"],
+  ]);
+  readonly typeMap = new Map<DepartmentType, RegExp>([
+    ["isCorePublicAdministration", /core public administration/i],
+    ["isCentralAgency", /central agency/i],
+    ["isRegulatory", /regulatory/i],
+    ["isScience", /science/i],
+  ]);
+
+  constructor(page: Page) {
+    super(page);
+    this.locators = {
+      [FIELD.NAME_EN]: page.getByRole("textbox", { name: /name \(english\)/i }),
+      [FIELD.NAME_FR]: page.getByRole("textbox", { name: /name \(french\)/i }),
+      [FIELD.NUMBER]: page.getByRole("textbox", { name: /department number/i }),
+      [FIELD.ORG_ID]: page.getByRole("textbox", { name: /organization id/i }),
+      [FIELD.TYPE]: page.getByRole("group", { name: /department type/i }),
+      [FIELD.SIZE]: page.getByRole("combobox", { name: /department size/i }),
+    };
+  }
+
+  async view(id: string) {
+    await this.page.goto(`${this.baseUrl}/${id}`);
+    await this.waitForGraphqlResponse("ViewDepartmentPage");
+  }
+
+  async goToUpdate(id: string) {
+    await this.page.goto(`${this.baseUrl}/${id}/edit`);
+    await this.waitForGraphqlResponse("Department");
+  }
+
+  async updateDepartment(id: string, department: UpdateDepartmentInput) {
+    await this.goToUpdate(id);
+
+    await this.fillName(department.name);
+    await this.fillNumber(department.departmentNumber);
+    await this.fillOrgId(department.orgIdentifier);
+    await this.fillSize(department.size);
+
+    const {
+      isScience,
+      isRegulatory,
+      isCentralAgency,
+      isCorePublicAdministration,
+    } = department;
+    await this.fillType({
+      isCorePublicAdministration,
+      isCentralAgency,
+      isRegulatory,
+      isScience,
+    });
+  }
+
+  async fillName(name?: InputMaybe<LocalizedStringInput>) {
+    if (name?.en) {
+      await this.locators.nameEn.fill(name.en);
+    }
+
+    if (name?.fr) {
+      await this.locators.nameEn.fill(name.fr);
+    }
+  }
+
+  async fillNumber(
+    number?:
+      | UpdateDepartmentInput["departmentNumber"]
+      | CreateDepartmentInput["departmentNumber"],
+  ) {
+    if (number) {
+      await this.locators.number.fill(String(number));
+    }
+  }
+
+  async fillOrgId(
+    orgId?:
+      | UpdateDepartmentInput["orgIdentifier"]
+      | CreateDepartmentInput["orgIdentifier"],
+  ) {
+    if (orgId) {
+      await this.locators.orgId.fill(String(orgId));
+    }
+  }
+
+  async fillSize(
+    size?: UpdateDepartmentInput["size"] | CreateDepartmentInput["size"],
+  ) {
+    if (size) {
+      const label = this.sizeMap.get(size);
+      if (label) {
+        await this.locators.size.selectOption({ label });
+      }
+    }
+  }
+
+  async fillType(type?: DepartmentTypeInput) {
+    if (type) {
+      const keys: DepartmentType[] = Object.keys(type) as DepartmentType[];
+      for (const key of keys) {
+        const label = this.typeMap.get(key);
+        if (label && this.typeIsSet(key, type)) {
+          await this.locators.type
+            .getByRole("checkbox", { name: label })
+            .click();
+        }
+      }
+    }
+  }
+
+  typeIsSet(
+    key: DepartmentType,
+    type?: DepartmentTypeInput,
+  ): type is Record<string, boolean | null> & Record<DepartmentType, boolean> {
+    if (!type) return false;
+
+    if (!(key in type)) {
+      return false;
+    }
+
+    return typeof type[key] === "boolean" && type?.[key] !== null;
+  }
+}
+
+export default Department;
