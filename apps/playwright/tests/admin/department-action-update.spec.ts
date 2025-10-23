877aba13cb223518d35fb993c25289df77737ee2
Eric Sizer <eric.sizer@gmail.com>
2025-10-23 16:09:24 -0400

    add spec for updating department

---
+++ apps/playwright/tests/admin/department-action-update.spec.ts
@@ -0,0 +1,61 @@
+import { DepartmentSize } from "@gc-digital-talent/graphql";
+
+import { test, expect } from "~/fixtures";
+import Department from "~/fixtures/Department";
+import { loginBySub } from "~/utils/auth";
+import { deleteDepartment, createDepartment } from "~/utils/departments";
+import graphql from "~/utils/graphql";
+import { generateUniqueNumber, generateUniqueTestId } from "~/utils/id";
+
+test("Update department", async ({ appPage }) => {
+  const uniqueTestId = generateUniqueTestId();
+  const DEPARTMENT_TITLE = `Test update department ${uniqueTestId}`;
+
+  const adminCtx = await graphql.newContext();
+  const { id } = await createDepartment(adminCtx, {
+    name: {
+      en: `${DEPARTMENT_TITLE} (EN)`,
+      fr: `${DEPARTMENT_TITLE} (FR)`,
+    },
+    departmentNumber: parseInt(generateUniqueNumber()),
+    orgIdentifier: parseInt(generateUniqueNumber()),
+    size: DepartmentSize.Large,
+    isCentralAgency: false,
+    isCorePublicAdministration: false,
+    isRegulatory: true,
+    isScience: false,
+  });
+
+  await loginBySub(appPage.page, "plaform@test.com");
+
+  const dept = new Department(appPage.page);
+
+  const expectedNumber = parseInt(generateUniqueNumber());
+  const expectedIdentifier = parseInt(generateUniqueNumber());
+
+  await dept.updateDepartment(id, {
+    name: {
+      en: `${DEPARTMENT_TITLE} (EN) UPDATED`,
+      fr: `${DEPARTMENT_TITLE} (FR) UPDATED`,
+    },
+    departmentNumber: expectedNumber,
+    orgIdentifier: expectedIdentifier,
+    size: DepartmentSize.Micro,
+    isCentralAgency: true,
+    isCorePublicAdministration: true,
+    isRegulatory: false,
+    isScience: false,
+  });
+
+  await expect(appPage.page.getByRole("alert").last()).toContainText(
+    /department created successfully/i,
+  );
+
+  await dept.view(id);
+
+  await expect(
+    dept.page.getByText(`Department number ${expectedNumber}`),
+  ).toBeVisible();
+
+  await deleteDepartment(adminCtx, { id });
+});
