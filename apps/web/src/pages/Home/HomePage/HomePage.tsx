223da4cbf0714dc70feec46c98f8cdf16c86d1c2
vd1992 <40485260+vd1992@users.noreply.github.com>
2025-10-27 11:08:32 -0600

    workflow + diff

--- apps/web/src/pages/Home/HomePage/HomePage.tsx
+++ apps/web/src/pages/Home/HomePage/HomePage.tsx
@@ -16,14 +16,14 @@ export const Component = ({ defaultImage }: HeroProps) => {
     <>
       <SEO
         title={intl.formatMessage({
-          defaultMessage: "Welcome",
-          id: "4I6WIU",
+          defaultMessage: "Welcome?",
+          id: "PgsiWD",
           description: "Title for the homepage",
         })}
         description={intl.formatMessage({
           defaultMessage:
-            "Whether you're thinking about joining government or already an employee, hoping to hire or considering a new role, this is the place to come to be part of the GC digital community.",
-          id: "tJnBx1",
+            "Whether whether whether you're thinking about joining government or already an employee, hoping to hire or considering a new role, this is the place to come to be part of the GC digital community.",
+          id: "PbDWVm",
           description: "SEO description of the application on the homepage",
         })}
       />
