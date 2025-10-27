37413ac4f344cd729242c592b5aff9e1bcd72ac2
Chantelle <mrspasceri@hotmail.com>
2025-10-27 11:10:44 -0700

    keep menu id in DOM for aria-controls

--- apps/web/src/components/NavMenu/Menu.tsx
+++ apps/web/src/components/NavMenu/Menu.tsx
@@ -117,7 +117,11 @@ const Menu = ({
   }, [isMenuOpen]);
 
   return (
-    <div className="fixed right-4.5 bottom-21 left-4.5 z-40 max-h-[85vh] overflow-y-auto sm:sticky sm:top-[-1px] sm:right-auto sm:bottom-auto sm:left-auto sm:w-full sm:overflow-y-visible md:max-h-none">
+    <div
+      className="fixed right-4.5 bottom-21 left-4.5 z-40 max-h-[85vh] overflow-y-auto sm:sticky sm:top-[-1px] sm:right-auto sm:bottom-auto sm:left-auto sm:w-full sm:overflow-y-visible md:max-h-none"
+      // NOTE: Do not remove, required by anchor link offsets
+      id="main-nav"
+    >
       <FocusLock returnFocus disabled={!showOverlay}>
         <NavMenuProvider
           open={isMenuOpen}
@@ -127,8 +131,6 @@ const Menu = ({
           <div className="relative z-10">
             {showMenu ? (
               <NavMenu.Root
-                // NOTE: Do not remove, required by anchor link offsets
-                id="main-nav"
                 onKeyDown={handleKeyDown}
                 aria-label={
                   label ??
