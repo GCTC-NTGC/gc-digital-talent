abb2b920c6e4cf8c2041fc5e0ac6a4dfcbfc457d
Matt Nigh <matt@mattnigh.com>
2025-10-27 13:56:51 -0400

    Add missing expect

--- apps/web/src/pages/Home/IAPHomePage/Home.test.tsx
+++ apps/web/src/pages/Home/IAPHomePage/Home.test.tsx
@@ -1,3 +1,5 @@
+import { screen } from "@testing-library/react";
+
 import {
   renderWithProviders,
   axeTest,
@@ -10,6 +12,11 @@ const renderHome = () => renderWithProviders(<Home />);
 describe("Basic test for Home", () => {
   it("should render", () => {
     renderHome();
+    expect(
+      screen.getByRole("h1", {
+        name: "IT Apprenticeship Program for Indigenous Peoples",
+      }),
+    ).toBeInTheDocument();
   });
 
   it("should have no accessibility errors", async () => {
