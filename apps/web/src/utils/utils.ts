8030ef5376dec7e2eda51cfb5fbf78c5347b1ac4
Eric Sizer <eric.sizer@gmail.com>
2025-11-03 09:28:23 -0500

    document the url match function

--- apps/web/src/utils/utils.ts
+++ apps/web/src/utils/utils.ts
@@ -1,3 +1,9 @@
+/**
+ * Checks if the provided URL matches the current app's host name
+ *
+ * @param {string} url - The URL to test
+ * @returns {boolean} True if the URL matches the current host name
+ */
 export function urlMatchesAppHostName(url: string): boolean {
   const pattern = new URLPattern({ hostname: window.location.hostname });
   return pattern.test(url);
