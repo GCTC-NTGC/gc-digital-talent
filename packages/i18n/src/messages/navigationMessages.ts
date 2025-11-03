8e782862a79c7c462ba9316b80ffae996ac38a23
Yoni K <yoni.kid15@gmail.com>
2025-11-03 10:56:59 -0500

    Remove unused strings (#14988)
    Co-authored-by: Yoni K <yonikid15@gmail.com>

--- packages/i18n/src/messages/navigationMessages.ts
+++ packages/i18n/src/messages/navigationMessages.ts
@@ -1,11 +1,6 @@
 import { defineMessages } from "react-intl";
 
 const navigationMessages = defineMessages({
-  aboutMe: {
-    defaultMessage: "About me",
-    id: "Gx8qCK",
-    description: "Name of About me page",
-  },
   advancedTools: {
     defaultMessage: "Advanced tools",
     id: "qP2HGB",
@@ -20,16 +15,6 @@ const navigationMessages = defineMessages({
     defaultMessage: "Diversity, equity, and inclusion",
     id: "0ClMl1",
     description: "Name of Diversity, equity, and inclusion page",
-  },
-  languageInformation: {
-    defaultMessage: "Language information",
-    id: "v9XiWV",
-    description: "Name of Language information page",
-  },
-  governmentInformation: {
-    defaultMessage: "Government information",
-    id: "FvV+QW",
-    description: "Name of Government information page",
   },
   workLocation: {
     defaultMessage: "Work location preferences",
@@ -80,11 +65,6 @@ const navigationMessages = defineMessages({
     defaultMessage: "Find talent",
     id: "Og8iHn",
     description: "Name of Find talent (search) page",
-  },
-  profileAndApplications: {
-    defaultMessage: "Profile and applications",
-    id: "gBoG9M",
-    description: "Name of Profile and applications page",
   },
   home: {
     defaultMessage: "Home",
