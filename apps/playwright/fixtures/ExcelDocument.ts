5f6f94f568ae6b6c7383afce6c8fc6de7c05301b
Eric Sizer <eric.sizer@gmail.com>
2025-11-04 09:44:15 -0500

    add dep for excel files

---
+++ apps/playwright/fixtures/ExcelDocument.ts
@@ -0,0 +1,27 @@
+import fs from "node:fs/promises";
+
+import { Page } from "@playwright/test";
+import mammoth from "mammoth";
+
+/**
+ * Word document
+ *
+ * Page containing utilities for interacting with a word document
+ */
+class WordDocument {
+  public readonly page: Page;
+
+  constructor(public readonly newPage: Page) {
+    this.page = newPage;
+  }
+
+  async setContent(path: string) {
+    const html = await mammoth
+      .convertToHtml({ path })
+      .then((result) => result.value);
+
+    await this.page.setContent(html);
+  }
+}
+
+export default WordDocument;
