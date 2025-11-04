14c1a125754079f0ee04a2e6f6c1e88b87dfd613
Eric Sizer <eric.sizer@gmail.com>
2025-11-04 08:58:10 -0500

    create basic spec example

---
+++ apps/playwright/tests/admin/application-download.spec.ts
@@ -0,0 +1,114 @@
+import { FAR_PAST_DATE } from "@gc-digital-talent/date-helpers";
+import {
+  ArmedForcesStatus,
+  CitizenshipStatus,
+  FlexibleWorkLocation,
+  PoolCandidate,
+  PositionDuration,
+  ProvinceOrTerritory,
+  SkillCategory,
+  User,
+  WorkRegion,
+} from "@gc-digital-talent/graphql";
+
+import { test, expect } from "~/fixtures";
+import PoolCandidatePage from "~/fixtures/PoolCandidatePage";
+import WordDocument from "~/fixtures/WordDocument";
+import { createAndSubmitApplication } from "~/utils/applications";
+import { loginBySub } from "~/utils/auth";
+import graphql from "~/utils/graphql";
+import { generateUniqueTestId } from "~/utils/id";
+import { createAndPublishPool } from "~/utils/pools";
+import { getSkills } from "~/utils/skills";
+import { createUserWithRoles, me } from "~/utils/user";
+
+test.describe("Application download", () => {
+  let user: User;
+  let application: PoolCandidate;
+
+  test.beforeAll(async () => {
+    const testId = generateUniqueTestId();
+    const adminCtx = await graphql.newContext();
+    const sub = `application.download.${testId}`;
+
+    const skill = await getSkills(adminCtx, {}).then((skills) => {
+      return skills.find((s) => s.category.value === SkillCategory.Technical);
+    });
+
+    const createdUser = await createUserWithRoles(adminCtx, {
+      roles: ["guest", "base_user", "applicant"],
+      user: {
+        email: `${sub}@example.org`,
+        sub,
+        currentProvince: ProvinceOrTerritory.Alberta,
+        currentCity: "Test city",
+        telephone: "+10123456789",
+        armedForcesStatus: ArmedForcesStatus.Veteran,
+        citizenship: CitizenshipStatus.Citizen,
+        lookingForEnglish: true,
+        isGovEmployee: false,
+        hasPriorityEntitlement: true,
+        priorityNumber: "123",
+        locationPreferences: [WorkRegion.Atlantic],
+        flexibleWorkLocations: [FlexibleWorkLocation.Hybrid],
+        positionDuration: [PositionDuration.Permanent],
+        personalExperiences: {
+          create: [
+            {
+              description: "Test Experience Description",
+              details: "A Playwright test personal experience",
+              skills: {
+                sync: [
+                  {
+                    details: `Test Skill ${skill?.name.en}`,
+                    id: skill?.id ?? "",
+                  },
+                ],
+              },
+              startDate: FAR_PAST_DATE,
+              title: "Test Experience",
+            },
+          ],
+        },
+      },
+    });
+
+    const createdPool = await createAndPublishPool(adminCtx, {
+      userId: createdUser?.id ?? "",
+      skillIds: skill ? [skill?.id] : undefined,
+      name: {
+        en: `App download ${testId} (EN)`,
+        fr: `App download ${testId} (FR)`,
+      },
+    });
+
+    const applicantCtx = await graphql.newContext(
+      createdUser?.authInfo?.sub ?? "applicant@test.com",
+    );
+    const applicant = await me(applicantCtx, {});
+
+    const candidate = await createAndSubmitApplication(applicantCtx, {
+      userId: applicant.id,
+      poolId: createdPool.id,
+      personalExperienceId: applicant?.experiences?.[0]?.id ?? "",
+      signature: `${applicant.firstName} signature`,
+    });
+
+    application = candidate;
+    user = createdUser ?? { id: "" };
+  });
+
+  test("Verify application download contents", async ({ appPage }) => {
+    const candidatePage = new PoolCandidatePage(appPage.page);
+    await loginBySub(candidatePage.page, "admin@test.com");
+
+    await candidatePage.toGoCandidate(application.id);
+
+    const path = await candidatePage.downloadApplication();
+
+    const doc = new WordDocument(appPage.page);
+    await doc.setContent(path);
+
+    await expect(doc.page.getByText(user?.firstName ?? "N/A")).toBeVisible();
+  });
+});
