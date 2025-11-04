49ef89b3d1ce00f5999b62264d87e1866e995dff
Mauli <maulisonitbs01@gmail.com>
2025-11-04 16:07:34 -0500

    solution_implemented_for_departments

---
+++ apps/playwright/constants/config.ts
@@ -0,0 +1,15 @@
+const testConfig = {
+  AllSignInEmails: {
+    adminSignIn: "admin@test.com",
+    platformAdminSignIn: "platform@test.com",
+  },
+
+  departmentsConfig: {
+    url: "/en/admin/settings/departments",
+  },
+
+  usersConfig: {
+    url: "/en/admin/users",
+  },
+};
+export default testConfig;
