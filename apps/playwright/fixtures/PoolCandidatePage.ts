e22168f42acd8dd6a7a24ef10c243d1dfb16641d
Eric Sizer <esizer@users.noreply.github.com>
2025-11-04 13:05:23 -0500

    [Spike] Investigate testing contents of downloads (#15001)
    * add dep for converting word doc to html
    
    * add supprting fixtures
    
    * create basic spec example
    
    * fix tmp path
    
    * improve test
    
    * add dep for excel files
    
    * add excel document fixture
    
    * add test for excel document
    
    * fix path
    
    * update test
    
    * remove unused import
    
    * attempt to start queue worker for playwright
    
    * fix queued file download
    
    * add a real assertion
    
    * format files

---
+++ apps/playwright/fixtures/PoolCandidatePage.ts
@@ -0,0 +1,80 @@
+import { Download } from "@playwright/test";
+
+import AppPage from "./AppPage";
+
+/**
+ * Pool Candidate Page
+ *
+ * Page containing utilities for interacting with pool candidates as an admin
+ */
+class PoolCandidatePage extends AppPage {
+  async goToIndex() {
+    await this.page.goto("/en/admin/pool-candidates");
+  }
+
+  async toGoCandidate(id: string) {
+    await this.page.goto(`/en/admin/candidates/${id}/application`);
+  }
+
+  async searchForCandidate(name: string) {
+    await this.goToIndex();
+    await this.page
+      .getByRole("textbox", { name: /search by keyword/i })
+      .fill(name);
+    await this.waitForGraphqlResponse(
+      "CandidatesTableCandidatesPaginated_Query",
+    );
+  }
+
+  async resolveDownloadPromise(
+    downloadPromise: Promise<Download>,
+  ): Promise<string> {
+    const download = await downloadPromise;
+
+    const name = download.suggestedFilename();
+    const path = "/tmp/" + name;
+
+    // Wait for the download process to complete and save the downloaded file somewhere.
+    await download.saveAs(path);
+
+    return path;
+  }
+
+  async downloadProfileExcel() {
+    const downloadPromise = this.page.waitForEvent("download");
+    await this.page
+      .getByRole("button", { name: /download excel/i })
+      .first()
+      .click();
+    await this.page
+      .getByRole("menuitem", { name: /download profiles excel/i })
+      .click();
+
+    const now = new Date();
+    const today = now.toISOString().split("T")[0];
+
+    await this.page
+      .getByRole("button", { name: /view notifications/i })
+      .click();
+    await this.page
+      .getByRole("link", { name: new RegExp(`profiles_${today}`, "i") })
+      .first()
+      .click();
+    return await this.resolveDownloadPromise(downloadPromise);
+  }
+
+  async downloadApplication() {
+    const downloadPromise = this.page.waitForEvent("download");
+    await this.page
+      .getByRole("button", { name: /download/i })
+      .first()
+      .click();
+    await this.page
+      .getByRole("menuitem", { name: /download application/i })
+      .click();
+
+    return await this.resolveDownloadPromise(downloadPromise);
+  }
+}
+
+export default PoolCandidatePage;
