64414e71c30bc2d6bccabe4b6cc903394f50dcb1
Mauli <maulisonitbs01@gmail.com>
2025-11-07 13:23:17 -0500

    update_existing_user_code

--- apps/playwright/fixtures/locationPreferenceUpdatePage.ts
+++ apps/playwright/fixtures/locationPreferenceUpdatePage.ts
@@ -9,12 +9,15 @@ import config from "~/constants/config";
 import AppPage from "./AppPage";
 
 const FIELD = {
+  FLEXIBLE_WORK_LOCATION_TITLE: "flexibleWorkLocationTitle",
   FLEXIBLE_WORK_LOCATION_HEADING: "flexibleWorkLocationHeading",
   FLEXIBLE_WORK_LOCATION_OPTIONS: "flexibleWorkLocationOptions",
   SELECTED_FLEXIBLE_WORK_LOCATION_OPTION: "selectedFlexibleWorkLocationOption",
   EDIT_WORK_PREFERENCE: "editWorkPreference",
   WORK_LOCATION_PREFERENCE: "workLocationPreference",
   LOCATION_EXCLUSIONS: "locationExclusions",
+  TELEWORK_OPTION_LIST_ITEM: "teleworkOptionListItem",
+  TELEWORK_OPTION: "teleworkOption",
 } as const;
 
 type ObjectValues<T> = T[keyof T];
@@ -40,7 +43,7 @@ class LocationPreferenceUpdatePage extends AppPage {
   constructor(page: Page) {
     super(page);
     this.locators = {
-      [FIELD.FLEXIBLE_WORK_LOCATION_HEADING]: page.getByText(
+      [FIELD.FLEXIBLE_WORK_LOCATION_TITLE]: page.getByText(
         /Flexible work location options/i,
       ),
       [FIELD.FLEXIBLE_WORK_LOCATION_OPTIONS]: page
@@ -48,17 +51,26 @@ class LocationPreferenceUpdatePage extends AppPage {
         .filter({
           hasText: /Remote work|Hybrid work|On-site work/i,
         }),
+      [FIELD.FLEXIBLE_WORK_LOCATION_HEADING]: page.getByText(
+        /Select the flexible work location options you're interested in. Keep in mind that most Government of Canada jobs are hybrid./i,
+      ),
       [FIELD.SELECTED_FLEXIBLE_WORK_LOCATION_OPTION]: page.locator(
         "xpath=//span[normalize-space(.)='Interested']//following-sibling::span",
       ),
       [FIELD.EDIT_WORK_PREFERENCE]: page.getByRole("button", {
         name: /Edit work preferences/i,
       }),
-      [FIELD.WORK_LOCATION_PREFERENCE]: page.getByRole("heading", {
-        name: /Work location preferences/i,
-      }),
+      [FIELD.WORK_LOCATION_PREFERENCE]: page.getByText(
+        /Work location preferences/i,
+      ),
       [FIELD.LOCATION_EXCLUSIONS]: page.getByRole("textbox", {
         name: /location exclusions/i,
+      }),
+      [FIELD.TELEWORK_OPTION_LIST_ITEM]: page.getByRole("listitem", {
+        name: /Telework/i,
+      }),
+      [FIELD.TELEWORK_OPTION]: page.getByRole("checkbox", {
+        name: /Telework/i,
       }),
     };
   }
@@ -71,8 +83,9 @@ class LocationPreferenceUpdatePage extends AppPage {
 
   async validateSelectedFlexWorkLocOptions() {
     await expect(
-      this.locators[FIELD.FLEXIBLE_WORK_LOCATION_HEADING],
+      this.locators[FIELD.FLEXIBLE_WORK_LOCATION_TITLE],
     ).toBeVisible();
+    await expect(this.locators[FIELD.TELEWORK_OPTION_LIST_ITEM]).toHaveCount(0);
     const flexWorkLocOptions =
       await this.locators[
         FIELD.FLEXIBLE_WORK_LOCATION_OPTIONS
@@ -82,7 +95,11 @@ class LocationPreferenceUpdatePage extends AppPage {
         FIELD.SELECTED_FLEXIBLE_WORK_LOCATION_OPTION
       ].allTextContents();
     for (const option of selectedOptions) {
-      expect(flexWorkLocOptions).toContainEqual(option?.trim());
+      expect(
+        flexWorkLocOptions.some((o) =>
+          o.toLowerCase().includes(option.toLowerCase()),
+        ),
+      ).toBeTruthy();
     }
   }
 
@@ -95,6 +112,7 @@ class LocationPreferenceUpdatePage extends AppPage {
     await expect(
       this.locators[FIELD.FLEXIBLE_WORK_LOCATION_HEADING],
     ).toBeVisible();
+    await expect(this.locators[FIELD.TELEWORK_OPTION]).toHaveCount(0);
     await this.deSelectOptions(this.optionsMap);
     await this.selectOptions(this.optionsMap, locOptions);
   }
--- apps/playwright/tests/applicant/location-preference-update.spec.ts
+++ apps/playwright/tests/applicant/location-preference-update.spec.ts
@@ -3,9 +3,12 @@ import { FlexibleWorkLocation, WorkRegion } from "@gc-digital-talent/graphql";
 import { expect, test } from "~/fixtures";
 import LocationPreferenceUpdatePage from "~/fixtures/locationPreferenceUpdatePage";
 
+import { Registration } from "./registration.spec";
+
 test("Work Location Update for Existing users", async ({ appPage }) => {
   const locationPrefPage = new LocationPreferenceUpdatePage(appPage.page);
   await locationPrefPage.goToPersonalInformationPage();
+  // Validate existing selected flexible work location options
   await locationPrefPage.validateSelectedFlexWorkLocOptions();
   await locationPrefPage.navigateToEditWorkPreference();
   await locationPrefPage.updateFlexWorkLocationOption([
@@ -22,6 +25,8 @@ test("Work Location Update for Existing users", async ({ appPage }) => {
   await expect(appPage.page.getByRole("alert").last()).toContainText(
     /work preferences updated successfully/i,
   );
-
+  // Once the changes are saved, validate the updated selected flexible work location options
   await locationPrefPage.validateSelectedFlexWorkLocOptions();
 });
+
+test("Update Location preferences for New Users", async ({ appPage }) => {});
