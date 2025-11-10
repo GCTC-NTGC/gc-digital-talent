27ec641a87fc863875fbe9351a636c4d5b16444d
Eric Sizer <eric.sizer@gmail.com>
2025-11-10 13:07:13 -0500

    refactor client provider
    removes hooks, add helpers

---
+++ packages/client/src/utils/isTokenProbablyExpired.ts
@@ -0,0 +1,21 @@
+import { JwtPayload, jwtDecode } from "jwt-decode";
+
+import { allowableClockSkewSeconds } from "../constants";
+
+/**
+ * Attempt to determine if an access token is expired
+ */
+export function isTokenProbablyExpired(accessToken: string | null): boolean {
+  let tokenProbablyExpired = false;
+  if (accessToken) {
+    const decoded = jwtDecode<JwtPayload>(accessToken);
+    if (decoded.exp) {
+      const tokenExpiryDateSeconds = decoded.exp;
+      const safeTokenExpiryDateSeconds =
+        tokenExpiryDateSeconds - allowableClockSkewSeconds; // allow for the client's machine to be a bit off
+      tokenProbablyExpired = Date.now() > safeTokenExpiryDateSeconds * 1000; // JWT expiry date in seconds to milliseconds
+    }
+  }
+
+  return tokenProbablyExpired;
+}
