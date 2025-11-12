409489c0f74382f21d1d37b5e8dcb7e6911e54e1
Eric Sizer <eric.sizer@gmail.com>
2025-11-12 11:48:45 -0500

    fix polling interval

--- apps/web/src/constants/notifications.ts
+++ apps/web/src/constants/notifications.ts
@@ -3,6 +3,6 @@ import { getRuntimeVariable } from "@gc-digital-talent/env";
 // NOTE: Value is in mins so multiply by 60 here
 const envPollingInterval = getRuntimeVariable("NOTIFICATION_POLLING_INTERVAL");
 const NOTIFICATION_POLLING_INTERVAL =
-  (envPollingInterval ? parseInt(envPollingInterval) : 1) * 5;
+  (envPollingInterval ? parseInt(envPollingInterval) : 1) * 60;
 
 export { NOTIFICATION_POLLING_INTERVAL };
