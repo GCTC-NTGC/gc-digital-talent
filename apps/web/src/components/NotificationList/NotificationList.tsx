bb77c38fc3a3fa8bd8eb590b85ba72e87602961c
Eric Sizer <esizer@users.noreply.github.com>
2025-11-13 13:59:24 -0500

    [Refactor] Update `NotificationList` to use env polling interval (#15058)
    * update notification list to use env interval
    
    * pause count polling when dialog open
    
    * remove unused import
    
    * fix polling interval
    
    * refactor for refresh button
    
    * ifx icon button styles
    
    * add translation
    
    * fix icon import
    
    * remove unused var
    
    * spin icon while fetching
    
    * remove console statement
    
    (cherry picked from commit d00366b4b801954fb56676974f23152eaee7eff7)

--- apps/web/src/components/NotificationList/NotificationList.tsx
+++ apps/web/src/components/NotificationList/NotificationList.tsx
@@ -3,14 +3,9 @@ import { useQuery } from "urql";
 import { tv } from "tailwind-variants";
 
 import { graphql } from "@gc-digital-talent/graphql";
-import { unpackMaybes } from "@gc-digital-talent/helpers";
-import { nowUTCDateTime } from "@gc-digital-talent/date-helpers";
-
-import usePollingQuery from "~/hooks/usePollingQuery";
 
 import NotificationActions from "./NotificationActions";
 import NotificationListPage from "./NotificationListPage";
-import NotificationItem from "./NotificationItem";
 import NotificationPortal from "./NotificationPortal";
 
 const actions = tv({
@@ -32,21 +27,7 @@ const MaxNotificationPages_Query = graphql(/* GraphQL */ `
   }
 `);
 
-const NotificationPolling_Query = graphql(/* GraphQL */ `
-  query NotificationPolling($where: NotificationFilterInput) {
-    notifications(where: $where) {
-      data {
-        id
-        ...NotificationItem
-      }
-    }
-  }
-`);
-
-const pageLoadedAt = nowUTCDateTime();
-
 interface NotificationListProps {
-  live?: boolean;
   paginate?: boolean;
   limit?: number;
   inDialog?: boolean;
@@ -54,37 +35,20 @@ interface NotificationListProps {
 }
 
 const NotificationList = ({
-  live,
   paginate,
   limit,
   inDialog,
   onRead,
 }: NotificationListProps) => {
-  const now = nowUTCDateTime();
   const [searchParams] = useSearchParams();
   const onlyUnread =
     searchParams.has("unread") && searchParams.get("unread") !== null;
-  const [{ data: maxPagesData, fetching: fetchingMaxPages }] = useQuery({
+  const [{ data: maxPagesData, fetching }, executeQuery] = useQuery({
     query: MaxNotificationPages_Query,
     variables: {
       where: { onlyUnread },
     },
   });
-  const [{ data, fetching: fetchingLiveNotifications }] = usePollingQuery(
-    {
-      query: NotificationPolling_Query,
-      pause: !live || fetchingMaxPages,
-      variables: {
-        where: {
-          createdAt: {
-            from: pageLoadedAt,
-            to: now,
-          },
-        },
-      },
-    },
-    60,
-  );
   const lastPage = maxPagesData?.notifications.paginatorInfo.lastPage ?? 1;
   let pagesToLoad =
     paginate && searchParams.has("page") ? Number(searchParams.get("page")) : 1;
@@ -93,35 +57,27 @@ const NotificationList = ({
   }
 
   const pagesArray = Array.from(Array(pagesToLoad).keys());
-  const liveNotifications = unpackMaybes(data?.notifications?.data);
-  const liveIds = liveNotifications.map(({ id }) => id);
+
+  const handleRefetch = () => {
+    executeQuery({ requestPolicy: "network-only" });
+  };
+
   return (
     <>
       <NotificationActions
         onRead={onRead}
         onlyUnread={onlyUnread}
         inDialog={inDialog}
+        onRefresh={handleRefetch}
+        fetching={fetching}
       />
       <ul className={actions({ inDialog })}>
-        {liveNotifications.length > 0 ? (
-          <>
-            {liveNotifications.map((notification) => (
-              <NotificationItem
-                key={notification.id}
-                notification={notification}
-                inDialog={inDialog}
-                onRead={onRead}
-              />
-            ))}
-          </>
-        ) : null}
         {pagesArray.map((page) => {
           const currentPage = page + 1;
           return (
             <NotificationListPage
               key={`notification-page-${currentPage}`}
               page={currentPage}
-              excludeIds={liveIds}
               isLastPage={currentPage === pagesToLoad}
               onlyUnread={onlyUnread}
               inDialog={inDialog}
@@ -129,9 +85,6 @@ const NotificationList = ({
               {...((!paginate || limit) && {
                 first: limit ?? 100,
               })}
-              fetchingLiveNotifications={
-                fetchingLiveNotifications || fetchingMaxPages
-              }
             />
           );
         })}
