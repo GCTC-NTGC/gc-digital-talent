c925cee67e87970a75a038b9ba148c83414cf18f
vd1992 <40485260+vd1992@users.noreply.github.com>
2025-11-20 11:21:57 -0700

    create mutation

---
+++ api/app/GraphQL/Mutations/WipeUserWorkEmail.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\GraphQL\Mutations;
+
+use App\Models\User;
+
+final class WipeUserWorkEmail
+{
+    /**
+     * Wipe the user's work email and verification fields
+     */
+    public function __invoke($_, array $args)
+    {
+        /** @var User $user */
+        $user = User::find($args['id']);
+
+        $user->work_email = null;
+        $user->work_email_verified_at = null;
+        $user->save();
+
+        return $user;
+    }
+}
