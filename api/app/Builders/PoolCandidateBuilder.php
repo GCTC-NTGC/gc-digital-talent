0e8d0cbf55ab3b64772953285b2106e5179c5eef
vd1992 <40485260+vd1992@users.noreply.github.com>
2025-11-25 16:36:46 -0700

    rename schema stuff

--- api/app/Builders/PoolCandidateBuilder.php
+++ api/app/Builders/PoolCandidateBuilder.php
@@ -601,7 +601,7 @@ class PoolCandidateBuilder extends Builder
                 ->join('users', 'users.id', '=', 'pool_candidates.user_id')
                 ->select('users.citizenship', 'pool_candidates.*');
 
-            if (isset($args['useBookmark']) && $args['useBookmark']) {
+            if (isset($args['useFlag']) && $args['useFlag']) {
                 $this->orderBy('is_flagged', 'DESC');
             }
 
