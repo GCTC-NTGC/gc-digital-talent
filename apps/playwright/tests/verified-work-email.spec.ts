fb7dfb7e08293bb0ce78156d437c5e1adc9c3791
Eric Sizer <eric.sizer@gmail.com>
2025-11-26 08:54:26 -0500

    add test spec for verfified work email

---
+++ apps/playwright/tests/verified-work-email.spec.ts
@@ -0,0 +1,111 @@
+import { FAR_PAST_DATE } from "@gc-digital-talent/date-helpers";
+
+import { test, expect } from "~/fixtures";
+import AccountSettings from "~/fixtures/AccountSettings";
+import AdminUser from "~/fixtures/AdminUser";
+import { loginBySub } from "~/utils/auth";
+import graphql from "~/utils/graphql";
+import { generateUniqueTestId } from "~/utils/id";
+import { createUserWithRoles } from "~/utils/user";
+
+interface UserInfo {
+  id: string;
+  sub: string;
+}
+
+test.describe("Verified work email", () => {
+  let verified: UserInfo = { sub: "", id: "" };
+  let unverified: UserInfo = { sub: "", id: "" };
+
+  test.beforeAll(async () => {
+    const adminCtx = await graphql.newContext();
+    const testId = generateUniqueTestId();
+    const verifiedSub = `verified.${testId}@gc.ca`;
+    const unverifiedSub = `unverified.${testId}@gc.ca`;
+
+    const verifiedUser = await createUserWithRoles(adminCtx, {
+      roles: ["guest", "base_user", "applicant"],
+      user: {
+        email: `verified.contact@test.com`,
+        sub: verifiedSub,
+        workEmail: verifiedSub,
+        workEmailVerifiedAt: FAR_PAST_DATE,
+      },
+    });
+
+    verified = {
+      sub: verifiedSub,
+      id: verifiedUser?.id ?? "",
+    };
+
+    const unverifiedUser = await createUserWithRoles(adminCtx, {
+      roles: ["guest", "base_user", "applicant"],
+      user: {
+        email: `unverified.contact@test.com`,
+        sub: unverifiedSub,
+        workEmail: unverifiedSub,
+        workEmailVerifiedAt: null,
+      },
+    });
+
+    unverified = {
+      sub: unverifiedSub,
+      id: unverifiedUser?.id ?? "",
+    };
+  });
+
+  test("Verified user shows icon in account settings", async ({ appPage }) => {
+    const accountSettings = new AccountSettings(appPage.appPage);
+    await loginBySub(accountSettings.page, verified.sub);
+    await accountSettings.goToSettings();
+
+    await expect(
+      accountSettings.page.getByText(
+        new RegExp(`verified ${verified.sub}`, "i"),
+      ),
+    ).toBeVisible();
+  });
+
+  test("Verified user shows icon in admin", async ({ appPage }) => {
+    const adminUser = new AdminUser(appPage.page);
+    await loginBySub(adminUser.page, "admin@test.com");
+    await adminUser.goToUser(verified.id);
+
+    await adminUser.locators.govInfoTrigger.click();
+
+    await expect(
+      adminUser.page.getByText(new RegExp(`verified ${verified.sub}`, "i")),
+    ).toBeVisible();
+  });
+
+  test("Unverified user does not show icon in account settings", async ({
+    appPage,
+  }) => {
+    const accountSettings = new AccountSettings(appPage.appPage);
+    await loginBySub(accountSettings.page, unverified.sub);
+    await accountSettings.goToSettings();
+
+    await expect(
+      accountSettings.page.getByText(
+        new RegExp(`verified ${verified.sub}`, "i"),
+      ),
+    ).toBeHidden();
+  });
+
+  test("Unverified user does not show icon in admin", async ({ appPage }) => {
+    const adminUser = new AdminUser(appPage.page);
+    await loginBySub(adminUser.page, "admin@test.com");
+    await adminUser.goToUser(unverified.id);
+
+    await adminUser.locators.govInfoTrigger.click();
+
+    await expect(
+      adminUser.page.getByText(new RegExp(`verified ${verified.sub}`, "i")),
+    ).toBeHidden();
+
+    // Admins cannot verify other users work email
+    await expect(
+      adminUser.page.getByRole("button", { name: /verify now/i }),
+    ).toBeHidden();
+  });
+});
