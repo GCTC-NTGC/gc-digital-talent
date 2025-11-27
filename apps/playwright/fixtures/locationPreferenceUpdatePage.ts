4714eaff4e8c91dcc7b21e989f625d0cc2b01f03
Mauli Soni <maulisonitbs01@gmail.com>
2025-11-27 14:52:21 -0500

    PW Test: Update applicant location preferences: Remote, Hybrid, On-site (#15064)
    * solution_implemented_for_departments
    
    * location_preference_update_initial_code
    
    * update_existing_user_code
    
    * implemented_review_comments
    
    * update_env_example
    
    * second_script_code
    
    * delete_user_code
    
    * admin_test
    
    * Revert "Merge branch 'msoni/testdata_setup' into msoni/location_preference_update"
    
    This reverts commit 6e184c07a1a4a94679ce01f2c9a8062099912ccf, reversing
    changes made to 1c16433327e2c97e570083b02540a8494d116c61.
    
    * remove_config_file
    
    * Revert "solution_implemented_for_departments"
    
    This reverts commit 49ef89b3d1ce00f5999b62264d87e1866e995dff.
    
    * update_name
    
    * remove_unnecessary_validations_ and_updated_locators
    
    * updating subs from config

---
+++ apps/playwright/fixtures/locationPreferenceUpdatePage.ts
@@ -0,0 +1,150 @@
+import { expect, Locator, type Page } from "@playwright/test";
+import { Key } from "react";
+
+import { FlexibleWorkLocation, WorkRegion } from "@gc-digital-talent/graphql";
+
+import { loginBySub } from "~/utils/auth";
+
+import AppPage from "./AppPage";
+
+const FIELD = {
+  FLEXIBLE_WORK_LOCATION_TITLE: "flexibleWorkLocationTitle",
+  FLEXIBLE_WORK_LOCATION_OPTIONS: "flexibleWorkLocationOptions",
+  SELECTED_WORK_LOCATION_OPTIONS: "selectedWorkLocationOptions",
+  EDIT_WORK_PREFERENCE: "editWorkPreference",
+  WORK_LOCATION_PREFERENCE: "workLocationPreference",
+  LOCATION_EXCLUSIONS: "locationExclusions",
+  TELEWORK_OPTION_LIST_ITEM: "teleworkOptionListItem",
+  TELEWORK_OPTION: "teleworkOption",
+} as const;
+
+type ObjectValues<T> = T[keyof T];
+export type Field = ObjectValues<typeof FIELD>;
+
+class LocationPreferenceUpdatePage extends AppPage {
+  readonly baseUrl: string = "/en/applicant/personal-information";
+  readonly locators: Record<Field, Locator>;
+  readonly optionsMap = new Map<FlexibleWorkLocation, string>([
+    [FlexibleWorkLocation.Hybrid, "Hybrid work"],
+    [FlexibleWorkLocation.Onsite, "On-site work"],
+    [FlexibleWorkLocation.Remote, "Remote work"],
+  ]);
+  readonly regionsMap = new Map<WorkRegion, string>([
+    [WorkRegion.Atlantic, "Atlantic (NB, NS, PE and NL)"],
+    [WorkRegion.Quebec, "Quebec (excluding Gatineau area)"],
+    [WorkRegion.Ontario, "Ontario (excluding Ottawa area)"],
+    [WorkRegion.Prairie, "Prairies (AB, SK, MB)"],
+    [WorkRegion.BritishColumbia, "British Columbia"],
+    [WorkRegion.NationalCapital, "National Capital Region (Ottawa/Gatineau)"],
+    [WorkRegion.North, "Northern (NU, NT, YT)"],
+  ]);
+  constructor(page: Page) {
+    super(page);
+    this.locators = {
+      [FIELD.FLEXIBLE_WORK_LOCATION_TITLE]: page.getByRole("group", {
+        name: /Flexible work location options/i,
+      }),
+      [FIELD.FLEXIBLE_WORK_LOCATION_OPTIONS]: page
+        .getByRole("listitem")
+        .filter({
+          hasText: /Remote work|Hybrid work|On-site work/i,
+        }),
+      [FIELD.SELECTED_WORK_LOCATION_OPTIONS]: page
+        .getByRole("listitem")
+        .filter({
+          hasText: /Interested/i,
+        }),
+      [FIELD.EDIT_WORK_PREFERENCE]: page.getByRole("button", {
+        name: /Edit work preferences/i,
+      }),
+      [FIELD.WORK_LOCATION_PREFERENCE]: page.getByRole("group", {
+        name: /Work location preferences/i,
+      }),
+      [FIELD.LOCATION_EXCLUSIONS]: page.getByRole("textbox", {
+        name: /location exclusions/i,
+      }),
+      [FIELD.TELEWORK_OPTION_LIST_ITEM]: page.getByRole("listitem", {
+        name: /Telework/i,
+      }),
+      [FIELD.TELEWORK_OPTION]: page.getByRole("checkbox", {
+        name: /Telework/i,
+      }),
+    };
+  }
+
+  async goToPersonalInformationPage(sub: string) {
+    await loginBySub(this.page, sub);
+    await this.page.goto(`${this.baseUrl}`);
+    await this.waitForGraphqlResponse("ProfileUser");
+  }
+
+  async validateSelectedFlexWorkLocOptions() {
+    await expect(this.locators[FIELD.TELEWORK_OPTION_LIST_ITEM]).toHaveCount(0);
+    const flexWorkLocOptions =
+      await this.locators[
+        FIELD.FLEXIBLE_WORK_LOCATION_OPTIONS
+      ].allTextContents();
+    const selectedOptions =
+      await this.locators[
+        FIELD.SELECTED_WORK_LOCATION_OPTIONS
+      ].allTextContents();
+    for (const option of selectedOptions) {
+      expect(
+        flexWorkLocOptions.some((o) =>
+          o.toLowerCase().includes(option.toLowerCase()),
+        ),
+      ).toBeTruthy();
+    }
+  }
+
+  async navigateToEditWorkPreference() {
+    await this.locators[FIELD.EDIT_WORK_PREFERENCE].click();
+    await this.waitForGraphqlResponse("WorkPreferencesForm_Query");
+  }
+
+  async updateFlexWorkLocationOption(locOptions: FlexibleWorkLocation[]) {
+    await expect(this.locators[FIELD.TELEWORK_OPTION]).toHaveCount(0);
+    await expect(
+      this.locators[FIELD.FLEXIBLE_WORK_LOCATION_TITLE],
+    ).toBeVisible();
+    await this.deSelectOptions(this.optionsMap);
+    await this.selectOptions(this.optionsMap, locOptions);
+  }
+
+  async selectWorkLocationPreferences(regionOptions: WorkRegion[]) {
+    await expect(this.locators[FIELD.WORK_LOCATION_PREFERENCE]).toBeVisible();
+    await this.deSelectOptions(this.regionsMap);
+    await this.selectOptions(this.regionsMap, regionOptions);
+  }
+
+  async fillLocationExclusions(exclusions: string) {
+    await this.locators[FIELD.LOCATION_EXCLUSIONS].fill(exclusions);
+  }
+
+  async deSelectOptions(MapType: Map<Key, string>) {
+    for (const [, label] of Array.from(MapType.entries())) {
+      const checkbox = this.page
+        .getByLabel(label, { exact: true })
+        .or(this.page.getByRole("checkbox", { name: label, exact: true }));
+
+      if (await checkbox.isChecked()) {
+        await checkbox.uncheck();
+      }
+    }
+  }
+
+  async selectOptions(MapType: Map<Key, string>, locOptions: readonly Key[]) {
+    for (const location of locOptions) {
+      const label = MapType.get(location);
+      if (!label) continue;
+      const checkbox = this.page
+        .getByLabel(label, { exact: true })
+        .or(this.page.getByRole("checkbox", { name: label, exact: true }));
+
+      if (!(await checkbox.isChecked())) {
+        await checkbox.check();
+      }
+    }
+  }
+}
+export default LocationPreferenceUpdatePage;
