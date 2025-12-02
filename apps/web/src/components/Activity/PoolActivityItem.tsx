bdf4bd0ce6e3f2ebf7fe9e5c79de1c0d9b3011f5
Eric Sizer <eric.sizer@gmail.com>
2025-12-02 14:04:03 -0500

    add localized key map to process activity

--- apps/web/src/components/Activity/PoolActivityItem.tsx
+++ apps/web/src/components/Activity/PoolActivityItem.tsx
@@ -1,6 +1,7 @@
 import DocumentArrowUpIcon from "@heroicons/react/16/solid/DocumentArrowUpIcon";
 import ExclamationTriangleIcon from "@heroicons/react/16/solid/ExclamationTriangleIcon";
 import { isAfter } from "date-fns/isAfter";
+import { defineMessage, MessageDescriptor } from "react-intl";
 
 import {
   ActivityProperties,
@@ -9,8 +10,12 @@ import {
   Scalars,
 } from "@gc-digital-talent/graphql";
 import { parseDateTimeUtc } from "@gc-digital-talent/date-helpers";
+import { commonMessages, navigationMessages } from "@gc-digital-talent/i18n";
 
 import activityMessages from "~/messages/activityMessages";
+import adminMessages from "~/messages/adminMessages";
+import jobPosterTemplateMessages from "~/messages/jobPosterTemplateMessages";
+import processMessages from "~/messages/processMessages";
 
 import ActivityItem, {
   ActivityItem_Fragment,
@@ -37,6 +42,50 @@ function updatedAfterPublish(
   if (!publishedAt || !createdAt) return false;
   return isAfter(parseDateTimeUtc(createdAt), parseDateTimeUtc(publishedAt));
 }
+
+const keyMap = new Map<string, MessageDescriptor>([
+  ["name", commonMessages.name],
+  ["user_id", adminMessages.user],
+  ["operational_requirements", navigationMessages.workPreferences],
+  ["key_tasks", jobPosterTemplateMessages.keyTasks],
+  ["closing_date", processMessages.closingDate],
+  ["your_impact", processMessages.yourImpact],
+  ["advertisement_location", processMessages.location],
+  ["security_clearance", processMessages.securityRequirement],
+  ["advertisement_language", processMessages.languageRequirement],
+  [
+    "is_remote",
+    defineMessage({
+      defaultMessage: "Remote",
+      id: "4vScqR",
+      description: "Label for when a position is remote",
+    }),
+  ],
+  ["process_number", processMessages.processNumber],
+  ["publishing_group", processMessages.publishingGroup],
+  ["published_at", commonMessages.published],
+  ["what_to_expect", processMessages.whatToExpectApplication],
+  ["special_note", processMessages.specialNote],
+  ["opportunity_length", processMessages.employmentDuration],
+  ["what_to_expect_admission", processMessages.whatToExpectAdmission],
+  ["about_us", processMessages.aboutUs],
+  ["classification_id", processMessages.classification],
+  ["closing_reason", processMessages.closingReason],
+  ["change_justification", processMessages.changeJustification],
+  ["department_id", commonMessages.department],
+  ["community_id", adminMessages.community],
+  ["area_of_selection", processMessages.areaOfSelection],
+  [
+    "selection_limitations",
+    defineMessage({
+      defaultMessage: "Selection limitations",
+      id: "CprAHM",
+      description: "Label for candidate selection limitations",
+    }),
+  ],
+  ["work_stream_id", processMessages.stream],
+  ["contact_email", commonMessages.email],
+]);
 
 interface PoolActivityItemProps
   extends Omit<ActivityItemProps, "info" | "properties"> {
@@ -73,7 +122,7 @@ const PoolActivityItem = ({
     };
   }
 
-  return <ActivityItem info={info} query={query} {...rest} />;
+  return <ActivityItem info={info} query={query} keyMap={keyMap} {...rest} />;
 };
 
 export default PoolActivityItem;
