85857a5f9ed7fca1c599b16aa9f6dc3edd9b3faa
Eric Sizer <esizer@users.noreply.github.com>
2025-12-05 07:52:30 -0500

    [Fix] Automated a11y errors (#15228)
    * require both labels when one is provided
    
    * supply labels for check icons
    
    * add label for employee search input
    
    * skip a11y test with note
    
    * skip ally checks, all errors are inconclusive
    
    * add note for known issue
    
    * attemtp to ignore all dialog triggers
    
    * exclude focus guard with known issue
    
    * exlcude link with false positive
    
    * ignore colour contrast
    
    * add ignore to storybook config
    
    * ignore hero colour contrast
    
    * ignore inconclusive colour contrast
    
    * fix heading order
    
    * remove tag
    
    * fix inconclusive colour contrast
    
    * exclude inconclusive errors
    
    * exclude errors, add issue note
    
    * centralize global excludes
    
    * remove global exclude from vitest config
    
    * address some issues from main merge
    
    * fix/ignore additional errors
    
    * fix formatting
    
    * Apply suggestions from code review
    
    Co-authored-by: Matt <mnigh@users.noreply.github.com>
    
    * remove remaining skip tags
    
    ---------
    
    Co-authored-by: Matt <mnigh@users.noreply.github.com>

--- apps/web/src/pages/SearchRequests/SearchPage/components/SearchForm.stories.tsx
+++ apps/web/src/pages/SearchRequests/SearchPage/components/SearchForm.stories.tsx
@@ -8,7 +8,10 @@ import {
   fakeLocalizedEnum,
   fakeWorkStreams,
 } from "@gc-digital-talent/fake-data";
-import { MockGraphqlDecorator } from "@gc-digital-talent/storybook-helpers";
+import {
+  GLOBAL_A11Y_EXCLUDES,
+  MockGraphqlDecorator,
+} from "@gc-digital-talent/storybook-helpers";
 import {
   FlexibleWorkLocation,
   LanguageAbility,
@@ -50,6 +53,17 @@ export const Default = Template.bind({});
 Default.parameters = {
   apiResponses: {
     SearchRequestOptions,
+  },
+  a11y: {
+    context: {
+      exclude: [
+        // NOTE: There are no colour contrast issues here
+        ...GLOBAL_A11Y_EXCLUDES,
+        "select",
+        "h3#results",
+        'a[href="/en/skills"]',
+      ],
+    },
   },
 };
 
