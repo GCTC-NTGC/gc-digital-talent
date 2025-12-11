112105128b4ce7d07e177cb364112456b1680c21
vd1992 <40485260+vd1992@users.noreply.github.com>
2025-12-11 10:57:16 -0700

    add to model files

--- api/app/Models/User.php
+++ api/app/Models/User.php
@@ -305,6 +305,13 @@ class User extends Model implements Authenticatable, HasLocalePreference, Laratr
         return $this->belongsToMany(Pool::class, 'pool_user_bookmarks', 'user_id', 'pool_id')->withTimestamps();
     }
 
+    /** @return BelongsToMany<PoolCandidate, $this> */
+    public function poolCandidateBookmarks(): BelongsToMany
+    {
+        return $this->belongsToMany(PoolCandidate::class, 'pool_candidate_user_bookmarks', 'user_id', 'pool_candidate_id')
+            ->withTimestamps();
+    }
+
     /** @return HasMany<PoolCandidate, $this> */
     public function poolCandidates(): HasMany
     {
