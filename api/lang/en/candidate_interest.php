dbbb127e656e230a40e5c12b2c8db51146d4a96d
Eric Sizer <esizer@users.noreply.github.com>
2025-12-16 12:08:56 -0500

    [Refactor] Candidate facing status (#15351)
    * add new candidate status attribute
    
    * update table with candidate status
    
    * update applicant card/dialog
    
    * add backend support for candidate interest
    
    * update frontend with new candidate interest
    
    * remove old code
    
    * add new attributes to type hint
    
    * fix issue with chip always being open to jobs
    
    * add expired case for candidate interest
    
    * update test with expired case

---
+++ api/lang/en/candidate_interest.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Support\Facades\Lang;
+
+return [
+    'not_interested' => 'Not interested',
+    'open_to_jobs' => 'Open to jobs',
+    'hired' => 'Hired',
+    'expired' => Lang::get('common.expired', [], 'en'),
+];
