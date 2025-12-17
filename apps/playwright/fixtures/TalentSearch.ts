f2e676bffc1dc225ea8478716268dc84a876316b
mauli0110 <maulisonitbs01@gmail.com>
2025-12-17 16:03:15 -0500

    create_fixture file

---
+++ apps/playwright/fixtures/TalentSearch.ts
@@ -0,0 +1,173 @@
+import { expect, Page } from "playwright/test";
+
+import { Classification, Skill, WorkStream } from "@gc-digital-talent/graphql";
+
+import AppPage from "./AppPage";
+
+class TalentSearch extends AppPage {
+  readonly baseUrl: string = "/en/search";
+  readonly classification: Classification;
+  readonly workStream: WorkStream;
+  readonly skill: Skill;
+
+  constructor(page: Page) {
+    super(page);
+  }
+
+  async goToIndex() {
+    await this.page.goto(this.baseUrl);
+    await this.waitForGraphqlResponse("SearchForm");
+  }
+
+  expectNoCandidates(poolName: string) {
+    const noCandidates = async (page: Page) => {
+      await expect(
+        page.getByRole("article", { name: new RegExp(poolName, "i") }),
+      ).toBeHidden();
+    };
+    return noCandidates;
+  }
+
+  async poolCardVisibility(poolName: string) {
+    const poolCard = this.page.getByRole("article", {
+      name: new RegExp(poolName, "i"),
+    });
+
+    await expect(poolCard).toBeVisible();
+    await expect(poolCard).toContainText(/1 approximate match/i);
+    return poolCard;
+  }
+
+  async fillSearchFormAndRequestCandidates(
+    poolName: string,
+    classification: Classification,
+    workStream: WorkStream,
+    skill: Skill,
+  ) {
+    const poolCard = await this.poolCardVisibility(poolName);
+    const classificationFilter = this.page.getByRole("combobox", {
+      name: /classification/i,
+    });
+    await classificationFilter.selectOption({ index: 2 });
+    this.expectNoCandidates(poolName);
+
+    await classificationFilter.selectOption({
+      value: `${classification.group}-${classification.level < 10 ? "0" : ""}${classification.level}`,
+    });
+
+    const streamFilter = this.page.getByRole("combobox", {
+      name: /stream/i,
+    });
+
+    await streamFilter.selectOption({ label: "Database Management" });
+    this.expectNoCandidates(poolName);
+
+    await streamFilter.selectOption({
+      label: workStream.name?.en ?? "",
+    });
+
+    await expect(poolCard).toBeVisible();
+
+    // Update in #13844
+    await this.page.getByRole("checkbox", { name: /ontario/i }).click();
+
+    this.expectNoCandidates(poolName);
+
+    await this.page.getByRole("checkbox", { name: /atlantic/i }).click();
+
+    await expect(poolCard).toBeVisible();
+
+    await this.page.getByRole("checkbox", { name: /woman/i }).click();
+
+    const skillFilter = this.page.getByRole("combobox", {
+      name: /^skill$/i,
+    });
+
+    await skillFilter.fill(`${skill?.name.en}`);
+    await skillFilter.press("ArrowDown");
+    await skillFilter.press("Enter");
+
+    await this.page.getByRole("radio", { name: /french only/i }).click();
+
+    await this.page
+      .getByRole("button", { name: /expand all advanced filters/i })
+      .click();
+
+    await this.page
+      .getByRole("radio", {
+        name: /required diploma from post-secondary institution/i,
+      })
+      .click();
+
+    await this.page
+      .getByRole("radio", { name: /indeterminate duration/i })
+      .click();
+
+    await this.page
+      .getByRole("checkbox", { name: /overtime \(occasionally\)/i })
+      .click();
+
+    await this.waitForGraphqlResponse("CandidateCount");
+    await expect(poolCard).toBeVisible();
+    await poolCard.getByRole("button", { name: /request candidates/i }).click();
+  }
+
+  async submitSearchForm(
+    classification: Classification,
+    workStream: WorkStream,
+    skill: Skill,
+  ) {
+    await this.page
+      .getByRole("textbox", { name: /full name/i })
+      .fill("Test user");
+    await this.page
+      .getByRole("textbox", { name: /government of canada email/i })
+      .fill("test@tbs-sct.gc.ca");
+    await this.page
+      .getByRole("textbox", { name: /what is your job title/i })
+      .fill("Manager");
+    await this.page
+      .getByRole("textbox", {
+        name: /what is the job title for this position/i,
+      })
+      .fill("Test job title");
+    await this.page.getByRole("radio", { name: /general interest/i }).click();
+    await this.page
+      .getByRole("textbox", { name: /additional comments/i })
+      .fill("Test comments");
+    const departmentInput = this.page.getByRole("combobox", {
+      name: /department/i,
+    });
+    await departmentInput.press("ArrowDown");
+    await departmentInput.press("Enter");
+
+    await expect(
+      this.page.getByText(
+        new RegExp(
+          `${classification.group}-${classification.level < 10 ? "0" : ""}${classification.level}: search pool`,
+          "i",
+        ),
+      ),
+    ).toBeVisible();
+
+    await expect(this.page.getByText(workStream?.name?.en ?? "")).toBeVisible();
+
+    await expect(
+      this.page.getByText(new RegExp(skill?.name.en ?? "")),
+    ).toBeVisible();
+
+    await expect(this.page.getByText(/required diploma/i)).toBeVisible();
+    await expect(this.page.getByText(/french only/i)).toBeVisible();
+    await expect(this.page.getByText(/indeterminate duration/i)).toBeVisible();
+    await expect(
+      this.page.getByText(/overtime \(occasionally\)/i),
+    ).toBeVisible();
+
+    await expect(this.page.getByText(/woman/i)).toBeVisible();
+    await expect(this.page.getByText(/1 estimated candidate/i)).toBeVisible();
+
+    await this.page.getByRole("button", { name: /submit request/i }).click();
+    await this.waitForGraphqlResponse("RequestForm_CreateRequest");
+  }
+}
+export default TalentSearch;
--- apps/playwright/tests/search-workflows.spec.ts
+++ apps/playwright/tests/search-workflows.spec.ts
@@ -1,5 +1,3 @@
-import { Page } from "@playwright/test";
-
 import { FAR_PAST_DATE, PAST_DATE } from "@gc-digital-talent/date-helpers";
 import {
   Classification,
@@ -25,6 +23,7 @@ import { createAndPublishPool } from "~/utils/pools";
 import { getClassifications } from "~/utils/classification";
 import { getWorkStreams } from "~/utils/workStreams";
 import { generateUniqueTestId } from "~/utils/id";
+import TalentSearch from "~/fixtures/TalentSearch";
 
 test.describe("Talent search", () => {
   let uniqueTestId: string;
@@ -33,12 +32,7 @@ test.describe("Talent search", () => {
   let classification: Classification;
   let workStream: WorkStream;
   let skill: Skill | undefined;
-
-  const expectNoCandidate = async (page: Page) => {
-    await expect(
-      page.getByRole("article", { name: new RegExp(poolName, "i") }),
-    ).toBeHidden();
-  };
+  let talentSearch: TalentSearch;
 
   test.beforeAll(async () => {
     uniqueTestId = generateUniqueTestId();
@@ -126,146 +120,16 @@ test.describe("Talent search", () => {
   });
 
   test("Search and submit request", async ({ appPage }) => {
-    await appPage.page.goto("/en/search");
-    await appPage.waitForGraphqlResponse("SearchForm");
-
-    const poolCard = appPage.page.getByRole("article", {
-      name: new RegExp(poolName, "i"),
-    });
-
-    await expect(poolCard).toBeVisible();
-    await expect(poolCard).toContainText(/1 approximate match/i);
-
-    const classificationFilter = appPage.page.getByRole("combobox", {
-      name: /classification/i,
-    });
-
-    await classificationFilter.selectOption({ index: 2 });
-
-    await expectNoCandidate(appPage.page);
-
-    await classificationFilter.selectOption({
-      value: `${classification.group}-${classification.level < 10 ? "0" : ""}${classification.level}`,
-    });
-
-    const streamFilter = appPage.page.getByRole("combobox", {
-      name: /stream/i,
-    });
-
-    await streamFilter.selectOption({ label: "Database Management" });
-    await expectNoCandidate(appPage.page);
-
-    await streamFilter.selectOption({
-      label: workStream.name?.en ?? "",
-    });
-
-    await expect(poolCard).toBeVisible();
-
-    // Update in #13844
-    await appPage.page.getByRole("checkbox", { name: /ontario/i }).click();
-
-    await expectNoCandidate(appPage.page);
-
-    await appPage.page.getByRole("checkbox", { name: /atlantic/i }).click();
-
-    await expect(poolCard).toBeVisible();
-
-    await appPage.page.getByRole("checkbox", { name: /woman/i }).click();
-
-    const skillFilter = appPage.page.getByRole("combobox", {
-      name: /^skill$/i,
-    });
-
-    await skillFilter.fill(`${skill?.name.en}`);
-    await skillFilter.press("ArrowDown");
-    await skillFilter.press("Enter");
-
-    await appPage.page.getByRole("radio", { name: /french only/i }).click();
-
-    await appPage.page
-      .getByRole("button", { name: /expand all advanced filters/i })
-      .click();
-
-    await appPage.page
-      .getByRole("radio", {
-        name: /required diploma from post-secondary institution/i,
-      })
-      .click();
-
-    await appPage.page
-      .getByRole("radio", { name: /indeterminate duration/i })
-      .click();
-
-    await appPage.page
-      .getByRole("checkbox", { name: /overtime \(occasionally\)/i })
-      .click();
-
-    await appPage.waitForGraphqlResponse("CandidateCount");
-    await expect(poolCard).toBeVisible();
-
-    await poolCard.getByRole("button", { name: /request candidates/i }).click();
+    talentSearch = new TalentSearch(appPage.page);
+    await talentSearch.goToIndex();
+    await talentSearch.fillSearchFormAndRequestCandidates(
+      poolName,
+      classification,
+      workStream,
+      skill!,
+    );
     await appPage.waitForGraphqlResponse("RequestForm_SearchRequestData");
-
-    await appPage.page
-      .getByRole("textbox", { name: /full name/i })
-      .fill("Test user");
-    await appPage.page
-      .getByRole("textbox", { name: /government of canada email/i })
-      .fill("test@tbs-sct.gc.ca");
-    await appPage.page
-      .getByRole("textbox", { name: /what is your job title/i })
-      .fill("Manager");
-    await appPage.page
-      .getByRole("textbox", {
-        name: /what is the job title for this position/i,
-      })
-      .fill("Test job title");
-    await appPage.page
-      .getByRole("radio", { name: /general interest/i })
-      .click();
-    await appPage.page
-      .getByRole("textbox", { name: /additional comments/i })
-      .fill("Test comments");
-    const departmentInput = appPage.page.getByRole("combobox", {
-      name: /department/i,
-    });
-    await departmentInput.press("ArrowDown");
-    await departmentInput.press("Enter");
-
-    await expect(
-      appPage.page.getByText(
-        new RegExp(
-          `${classification.group}-${classification.level < 10 ? "0" : ""}${classification.level}: search pool`,
-          "i",
-        ),
-      ),
-    ).toBeVisible();
-
-    await expect(
-      appPage.page.getByText(workStream?.name?.en ?? ""),
-    ).toBeVisible();
-
-    await expect(
-      appPage.page.getByText(new RegExp(skill?.name.en ?? "")),
-    ).toBeVisible();
-
-    await expect(appPage.page.getByText(/required diploma/i)).toBeVisible();
-    await expect(appPage.page.getByText(/french only/i)).toBeVisible();
-    await expect(
-      appPage.page.getByText(/indeterminate duration/i),
-    ).toBeVisible();
-    await expect(
-      appPage.page.getByText(/overtime \(occasionally\)/i),
-    ).toBeVisible();
-
-    await expect(appPage.page.getByText(/woman/i)).toBeVisible();
-    await expect(
-      appPage.page.getByText(/1 estimated candidate/i),
-    ).toBeVisible();
-
-    await appPage.page.getByRole("button", { name: /submit request/i }).click();
-    await appPage.waitForGraphqlResponse("RequestForm_CreateRequest");
-
+    await talentSearch.submitSearchForm(classification, workStream, skill!);
     await expect(appPage.page.getByRole("alert").last()).toContainText(
       /request created successfully/i,
     );
