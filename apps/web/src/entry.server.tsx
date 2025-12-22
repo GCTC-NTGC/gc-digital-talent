3671f6b1b6454e3ff6e59ecdd6bb0a989d4c9076
Eric Sizer <eric.sizer@gmail.com>
2025-12-22 08:52:16 -0500

    simplify server entry back to default

--- apps/web/src/entry.server.tsx
+++ apps/web/src/entry.server.tsx
@@ -1,33 +1,38 @@
 import { PassThrough } from "node:stream";
 
+import type { EntryContext } from "react-router";
 import { createReadableStreamFromReadable } from "@react-router/node";
 import { ServerRouter } from "react-router";
 import { renderToPipeableStream } from "react-dom/server";
-import type { AppLoadContext, EntryContext } from "react-router";
 
+/**
+ * NOTE: Even though we do not do SSR, this is required
+ * to compile the root.tsx during buildtime.
+ *
+ * It should not be necessary except we need to pass
+ * nonce to it so that it passes our CSP.
+ *
+ * Based on default:
+ *   https://reactrouter.com/api/framework-conventions/entry.server.tsx#default
+ */
 export default function handleRequest(
   request: Request,
   responseStatusCode: number,
   responseHeaders: Headers,
   routerContext: EntryContext,
-  _loadContext: AppLoadContext,
 ) {
-  // This MUST match the placeholder in your Nginx sub_filter and root.tsx
   const nonce = "**CSP_NONCE**";
 
   return new Promise((resolve, reject) => {
-    let shellRendered = false;
-    const { pipe, abort } = renderToPipeableStream(
+    const { pipe } = renderToPipeableStream(
       <ServerRouter context={routerContext} url={request.url} nonce={nonce} />,
       {
-        // This is the magic line that fixes the missing nonces at the bottom of your HTML
         nonce,
         onShellReady() {
-          shellRendered = true;
+          responseHeaders.set("Content-Type", "text/html");
+
           const body = new PassThrough();
           const stream = createReadableStreamFromReadable(body);
-
-          responseHeaders.set("Content-Type", "text/html");
 
           resolve(
             new Response(stream, {
@@ -39,17 +44,9 @@ export default function handleRequest(
           pipe(body);
         },
         onShellError(error: unknown) {
-          reject(error);
-        },
-        onError(error: unknown) {
-          responseStatusCode = 500;
-          if (shellRendered) {
-            console.error(error);
-          }
+          reject(error instanceof Error ? error : new Error(String(error)));
         },
       },
     );
-
-    setTimeout(abort, 5000);
   });
 }
