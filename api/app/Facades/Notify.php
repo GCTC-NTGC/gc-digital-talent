14a8330e62c5261c84485d51660ba367082a476c
Peter Giles <8978655+petertgiles@users.noreply.github.com>
2026-01-02 13:37:12 -0500

    some extra phpdoc

--- api/app/Facades/Notify.php
+++ api/app/Facades/Notify.php
@@ -4,6 +4,11 @@ namespace App\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static \Illuminate\Http\Client\Response sendEmail(string $to, string $template, array<mixed>$personalisation = [], string $reference = null, string $replyTo = null, array<mixed> $attachment = null)
+ *
+ * @see App\Notify\Client
+ */
 class Notify extends Facade
 {
     protected static function getFacadeAccessor()
