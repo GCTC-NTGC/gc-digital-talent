0d819fe3c566faa06c62a9d101b12f9cb22b749b
Eric Sizer <eric.sizer@gmail.com>
2026-01-14 08:35:29 -0500

    back out custom log name for pool skills

--- api/app/Models/PoolSkill.php
+++ api/app/Models/PoolSkill.php
@@ -67,7 +67,6 @@ class PoolSkill extends Model
     public function getActivitylogOptions(): LogOptions
     {
         return LogOptions::defaults()
-            ->useLogName(ActivityLog::PROCESS->value)
             ->logOnly(['*'])
             ->logOnlyDirty()
             ->dontSubmitEmptyLogs();
