31b70b6c50a6813a0972fca4f7f7e0f34aa00a28
Eric Sizer <eric.sizer@gmail.com>
2026-01-19 08:52:22 -0500

    fix down migration

--- api/database/migrations/2026_01_13_145058_add_application_status_column_pool_candidates_table.php
+++ api/database/migrations/2026_01_13_145058_add_application_status_column_pool_candidates_table.php
@@ -124,8 +124,7 @@ return new class extends Migration
                 WHEN ((application_status)::text = 'QUALIFIED'::text) THEN 40
                 WHEN ((application_status)::text = 'REMOVED'::text) THEN 50
                 ELSE NULL::integer
-            END
-            ) STORED;
+            END) STORED;
             SQL
         );
     }
@@ -157,6 +156,35 @@ return new class extends Migration
             $table->dropColumn('disqualification_reason');
             $table->dropColumn('status_updated_at');
             $table->dropColumn('placement_type');
+            $table->dropColumn('referring');
+            $table->dropColumn('status_weight');
         });
+
+        DB::statement(<<<'SQL'
+            ALTER TABLE pool_candidates
+            ADD COLUMN status_weight integer GENERATED ALWAYS AS (
+            CASE
+                WHEN ((pool_candidate_status)::text = 'DRAFT'::text) THEN 10
+                WHEN ((pool_candidate_status)::text = 'DRAFT_EXPIRED'::text) THEN 20
+                WHEN ((pool_candidate_status)::text = 'NEW_APPLICATION'::text) THEN 30
+                WHEN ((pool_candidate_status)::text = 'APPLICATION_REVIEW'::text) THEN 40
+                WHEN ((pool_candidate_status)::text = 'SCREENED_IN'::text) THEN 50
+                WHEN ((pool_candidate_status)::text = 'SCREENED_OUT_APPLICATION'::text) THEN 60
+                WHEN ((pool_candidate_status)::text = 'SCREENED_OUT_NOT_INTERESTED'::text) THEN 64
+                WHEN ((pool_candidate_status)::text = 'SCREENED_OUT_NOT_RESPONSIVE'::text) THEN 65
+                WHEN ((pool_candidate_status)::text = 'UNDER_ASSESSMENT'::text) THEN 70
+                WHEN ((pool_candidate_status)::text = 'SCREENED_OUT_ASSESSMENT'::text) THEN 80
+                WHEN ((pool_candidate_status)::text = 'QUALIFIED_AVAILABLE'::text) THEN 90
+                WHEN ((pool_candidate_status)::text = 'QUALIFIED_UNAVAILABLE'::text) THEN 100
+                WHEN ((pool_candidate_status)::text = 'QUALIFIED_WITHDREW'::text) THEN 110
+                WHEN ((pool_candidate_status)::text = 'PLACED_TENTATIVE'::text) THEN 115
+                WHEN ((pool_candidate_status)::text = 'PLACED_CASUAL'::text) THEN 120
+                WHEN ((pool_candidate_status)::text = 'PLACED_TERM'::text) THEN 130
+                WHEN ((pool_candidate_status)::text = 'PLACED_INDETERMINATE'::text) THEN 140
+                WHEN ((pool_candidate_status)::text = 'EXPIRED'::text) THEN 150
+                WHEN ((pool_candidate_status)::text = 'REMOVED'::text) THEN 160
+                ELSE NULL::integer
+            END) STORED,
+        SQL);
     }
 };
