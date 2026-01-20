0c396a18604699fcabf54358a4ba84324c0ec93e
Yoni K <yonikid15@gmail.com>
2026-01-20 11:56:24 -0500

    Add unique name validation for work stream

---
+++ api/app/GraphQL/Validators/UpdateWorkStreamInputValidator.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\GraphQL\Validators;
+
+use App\Enums\ErrorCode;
+use Illuminate\Validation\Rule;
+use Nuwave\Lighthouse\Validation\Validator;
+
+final class UpdateWorkStreamInputValidator extends Validator
+{
+    /**
+     * Return the validation rules.
+     *
+     * @return array<string, array<mixed>>
+     */
+    public function rules(): array
+    {
+        return [
+            'name.en' => ['required', Rule::unique('work_streams', 'name->en')],
+            'name.fr' => ['required', Rule::unique('work_streams', 'name->fr')],
+        ];
+    }
+
+    /**
+     * Return the validation messages
+     */
+    public function messages(): array
+    {
+        return [
+            'name.en.unique' => ErrorCode::WORK_STREAM_NAME_IN_USE->name,
+            'name.fr.unique' => ErrorCode::WORK_STREAM_NAME_IN_USE->name,
+        ];
+    }
+}
