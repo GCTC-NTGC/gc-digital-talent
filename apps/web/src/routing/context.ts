a3005ff9c9532c91b6d449c9d3d5344a7e9c8011
Eric Sizer <eric.sizer@gmail.com>
2026-01-21 12:09:11 -0500

    add default values

--- apps/web/src/routing/context.ts
+++ apps/web/src/routing/context.ts
@@ -4,18 +4,20 @@ import { IntlShape } from "react-intl";
 
 import { Maybe, UserAuthInfo } from "@gc-digital-talent/graphql";
 
-export const graphqlClientContext = createContext<Client>();
+export type GraphqlClientContext = Client | null;
+
+export const graphqlClientContext = createContext<GraphqlClientContext>(null);
 
 export type UserContext = Maybe<Partial<UserAuthInfo>>;
 
-export const userContext = createContext<UserContext>();
+export const userContext = createContext<UserContext>(null);
 
-export type IntlContext = IntlShape;
+export type IntlContext = IntlShape | null;
 
-export const intlContext = createContext<IntlContext>();
+export const intlContext = createContext<IntlContext>(null);
 
 export interface AppContext {
   get(token: typeof userContext): UserContext;
   get(token: typeof intlContext): IntlContext;
-  get(token: typeof graphqlClientContext): Client;
+  get(token: typeof graphqlClientContext): GraphqlClientContext;
 }
