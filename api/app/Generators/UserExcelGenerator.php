5e1873f6f05151630579a28d1025a16f42808a0c
Chantelle <mrspasceri@hotmail.com>
2026-01-23 11:02:01 -0800

    fix lint

--- api/app/Generators/UserExcelGenerator.php
+++ api/app/Generators/UserExcelGenerator.php
@@ -408,29 +408,29 @@ class UserExcelGenerator extends ExcelGenerator implements FileGeneratorInterfac
             $this->yesOrNo($employeeProfile?->career_planning_exec_interest), // Career planning - Executive interest
             $this->localizeEnumArray($employeeProfile?->career_planning_exec_coaching_status, ExecCoaching::class), // Career planning - Executive coaching status
             $this->localizeEnumArray($employeeProfile?->career_planning_exec_coaching_interest, ExecCoaching::class), // Career planning - Executive interest
-            $employeeProfile?->nextRoleClassification?->group ?? '', // Next role - target classification group
-            $employeeProfile?->nextRoleClassification?->level ?? '', // Next role - Target classification level
+            $employeeProfile?->nextRoleClassification->group ?? '', // Next role - target classification group
+            $employeeProfile?->nextRoleClassification->level ?? '', // Next role - Target classification level
             $this->localizeEnum($employeeProfile?->next_role_target_role, TargetRole::class), // Next role - Target role
             $this->yesOrNo($employeeProfile?->next_role_is_c_suite_role), // Next role - C-suite role
             $this->localizeEnum($employeeProfile?->next_role_c_suite_role_title, CSuiteRoleTitle::class),
-            $employeeProfile?->next_role_job_title ?? '', // Next role - Job title
+            $employeeProfile->next_role_job_title ?? '', // Next role - Job title
             $employeeProfile?->nextRoleCommunity?->name[$this->lang] ?? '', // Next role - Functional community
             $nextRoleWorkStreams->join(','), // Next role - Work streams
             $nextRoleDepartments->join(', '), // next role - Departments
-            $employeeProfile?->next_role_additional_information ?? '', // Next role - Additional information
-            $employeeProfile?->careerObjectiveClassification?->group ?? '', // Career objective - Target classification group
-            $employeeProfile?->careerObjectiveClassification?->level ?? '', // Career objective - Target classification level
+            $employeeProfile->next_role_additional_information ?? '', // Next role - Additional information
+            $employeeProfile->careerObjectiveClassification?->group ?? '', // Career objective - Target classification group
+            $employeeProfile->careerObjectiveClassification?->level ?? '', // Career objective - Target classification level
             $this->localizeEnum($employeeProfile?->career_objective_target_role, TargetRole::class), // Career objective - Target role
             $this->yesOrNo($employeeProfile?->career_objective_is_c_suite_role), // Career objective - C-suite role
             $this->localizeEnum($employeeProfile?->career_objective_c_suite_role_title, CSuiteRoleTitle::class), // Career objective - C-suite role title
-            $employeeProfile?->career_objective_job_title ?? '', // Career objective - Job title
+            $employeeProfile->career_objective_job_title ?? '', // Career objective - Job title
             $employeeProfile?->careerObjectiveCommunity?->name[$this->lang] ?? ' ', // Career objective - Functional community
             $careerObjectiveWorkStreams->join(', '), // career objective - Work streams
             $careerObjectiveDepartments->join(', '), // career objective - Departments
-            $employeeProfile?->career_objective_additional_information ?? '', // Career objective - Additional information
-            $employeeProfile?->career_planning_about_you ?? '', // Career planning - About you
-            $employeeProfile?->career_planning_learning_goals ?? '',  // Career planning - Learning goals
-            $employeeProfile?->career_planning_work_style ?? '', // Career planning - Work style
+            $employeeProfile->career_objective_additional_information ?? '', // Career objective - Additional information
+            $employeeProfile->career_planning_about_you ?? '', // Career planning - About you
+            $employeeProfile->career_planning_learning_goals ?? '',  // Career planning - Learning goals
+            $employeeProfile->career_planning_work_style ?? '', // Career planning - Work style
             $appliedPools->join(', '), // Digital talent processes
             $offPlatformProcesses->join(', '), // Off-platform processes
         ];
