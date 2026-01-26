b2e3e4e3c9020d6d1f66b64a7894962b1ad85a7b
Matt Nigh <matt@mattnigh.com>
2026-01-26 13:58:23 -0500

    Update subtitle for the profile page

--- apps/web/src/pages/Profile/ProfilePage/messages.ts
+++ apps/web/src/pages/Profile/ProfilePage/messages.ts
@@ -8,8 +8,8 @@ const messages = defineMessages({
   },
   subTitle: {
     defaultMessage:
-      "View and update account information including contact and work preferences.",
-    id: "NflJW7",
+      "View and update personal information work preferences and language profile.",
+    id: "g/ReTI",
     description: "subtitle for the profile page",
   },
 });
