218e99a54a12a1a7e556647a466c951ad022d993
Eric Sizer <eric.sizer@gmail.com>
2026-01-30 14:13:52 -0500

    add translations

---
+++ api/lang/fr/activity_event.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'created' => 'Créé',
+    'updated' => 'Mis à jour',
+    'deleted' => 'Supprimé',
+    'submitted' => 'Soumis',
+    'qualified' => 'Qualifié',
+    'disqualified' => 'Disqualifié',
+    'placed' => 'Placé',
+    'added' => 'Ajouté',
+    'removed' => 'Retiré',
+    'reinstated' => 'Réintégré',
+    'reverted' => 'Restauré',
+    'published' => 'Publié',
+];
