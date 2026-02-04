7dbcc16832e6e5922c7018caa825c9b083952dd0
Eric Sizer <esizer@users.noreply.github.com>
2026-02-04 12:49:56 -0500

    [Fix] CSP report controller (#15751)
    * prevent large payloads in CSP controller
    
    * throttle requests to endpoint

--- api/app/Http/Controllers/CspReportController.php
+++ api/app/Http/Controllers/CspReportController.php
@@ -9,12 +9,32 @@ class CspReportController extends Controller
 {
     public function report(Request $request)
     {
-        $report = $request->json()->all();
-        $message = sprintf(
-            'CSP violation, %s',
-            json_encode($report)
-        );
+        // Limit to 32kb
+        if (strlen($request->getContent()) > 32768) {
+            return response()->noContent(413);
+        }
 
-        Log::warning($message);
+        $data = $request->json()->all();
+        $report = $data['csp-report'] ?? null;
+
+        // Do not log anything if we don't have a report
+        if (! $report) {
+            return response()->noContent(400);
+        }
+
+        // Trim fields before logging
+        $trim = fn ($value) => is_string($value) ? mb_strimwidth($value, 0, 3000, '...') : $value;
+
+        // Only log keys we care about, prevent artificial inflation
+        $logData = [
+            'uri' => $trim($report['document-uri'] ?? 'N/A'),
+            'directive' => $report['effective-directive'] ?? ($report['violated-directive'] ?? 'N/A'),
+            'blocked' => $trim($report['blocked-uri'] ?? 'N/A'),
+            'disposition' => $report['disposition'] ?? 'N/A',
+        ];
+
+        Log::warning('CSP Violation', $logData);
+
+        return response()->noContent();
     }
 }
