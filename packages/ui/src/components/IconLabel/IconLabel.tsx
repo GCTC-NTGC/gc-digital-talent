c9bb941f2a1437beea41e2f76a846b7f6ef374ad
Yoni K <yonikid15@gmail.com>
2026-02-16 14:07:45 -0500

    Add icon label component

---
+++ packages/ui/src/components/IconLabel/IconLabel.tsx
@@ -0,0 +1,27 @@
+import { ReactNode } from "react";
+
+import { IconType } from "@gc-digital-talent/ui";
+
+interface IconLabelProps {
+  label: ReactNode;
+  icon: IconType;
+  children?: ReactNode;
+}
+
+const IconLabel = ({ label, icon, children }: IconLabelProps) => {
+  const Icon = icon;
+
+  return (
+    <p className="grid grid-cols-[calc(var(--spacing)*6)_1fr] gap-x-1.5">
+      <span className="inline-block size-6 align-middle">
+        <Icon />
+      </span>
+      <span>
+        <span className="mr-1 inline-block">{label}</span>
+        {children && <span>{children}</span>}
+      </span>
+    </p>
+  );
+};
+
+export default IconLabel;
