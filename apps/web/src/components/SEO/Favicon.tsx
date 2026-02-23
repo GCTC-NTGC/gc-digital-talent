bb306d22c574ee33abc7f5b4321e1bc1fd61bc3e
Matt Nigh <matt@mattnigh.com>
2026-02-23 11:17:50 -0500

    Replace react-helmet-async with dr.pogodin/react-helmet

--- apps/web/src/components/SEO/Favicon.tsx
+++ apps/web/src/components/SEO/Favicon.tsx
@@ -1,4 +1,4 @@
-import { Helmet } from "react-helmet-async";
+import { Helmet } from "@dr.pogodin/react-helmet";
 
 type Locale = "en" | "fr";
 export type Project = "digital-talent" | "iap" | "admin";
