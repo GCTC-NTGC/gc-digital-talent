25b17d85f63f0da1dcc22368beeddd1e381b3bb3
Peter Giles <8978655+petertgiles@users.noreply.github.com>
2026-02-24 14:00:26 -0500

    remove curl

--- api/app/Logging/Azure/AzureHandler.php
+++ api/app/Logging/Azure/AzureHandler.php
@@ -5,14 +5,12 @@ declare(strict_types=1);
 namespace App\Logging\Azure;
 
 use App\Contracts\ManagedIdentityService;
+use Illuminate\Support\Facades\Http;
 use Monolog\Formatter\FormatterInterface;
 use Monolog\Handler\AbstractProcessingHandler;
-use Monolog\Handler\Curl\Util;
 use Monolog\Handler\HandlerInterface;
-use Monolog\Handler\MissingExtensionException;
 use Monolog\Level;
 use Monolog\LogRecord;
-use Monolog\Utils;
 
 /**
  * Sends notifications through Azure endpoints
@@ -31,8 +29,6 @@ class AzureHandler extends AbstractProcessingHandler
      * @param  non-empty-string  $endpoint  The REST API endpoint for the Logs Ingestion API can either be a data collection endpoint (DCE) or the DCR logs ingestion endpoint.
      * @param  non-empty-string  $dcrImmutableId  The DCR Immutable ID is generated for the DCR when it's created. You can retrieve it from the Overview page for the DCR in the Azure portal.
      * @param  non-empty-string  $streamName  Stream Name refers to the stream in the DCR that should handle the custom data.
-     *
-     * @throws MissingExtensionException If the curl extension is missing
      */
     public function __construct(
         public ManagedIdentityService $identityService,
@@ -56,10 +52,6 @@ class AzureHandler extends AbstractProcessingHandler
         $level = Level::Critical,
         bool $bubble = true,
     ) {
-        if (! \extension_loaded('curl')) {
-            throw new MissingExtensionException('The curl extension is needed to use the AzureHandler');
-        }
-
         parent::__construct($level, $bubble);
 
         $this->azureRecord = new AzureRecord(
@@ -83,35 +75,19 @@ class AzureHandler extends AbstractProcessingHandler
         return $this->azureRecord;
     }
 
-    public function getConstructedUrl(): string
-    {
-        return $this->endpoint.'/dataCollectionRules/'.$this->dcrImmutableId.'/streams/'.$this->streamName.'?api-version=2023-01-01';
-    }
-
     /**
      * {@inheritDoc}
      */
     protected function write(LogRecord $record): void
     {
-        $postData = [$this->azureRecord->getAzureData($record)];
-        $postString = Utils::jsonEncode($postData);
-        $accessToken = $this->identityService->getAccessToken();
-
-        $ch = curl_init();
-        $options = [
-            CURLOPT_URL => $this->getConstructedUrl(),
-            CURLOPT_POST => true,
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_HTTPHEADER => [
-                'Authorization: Bearer '.$accessToken,
-                'Content-type: application/json',
-            ],
-            CURLOPT_POSTFIELDS => $postString,
-        ];
-
-        curl_setopt_array($ch, $options);
-
-        Util::execute($ch);
+        Http::withToken($this->identityService->getAccessToken())
+            ->post(
+                $this->endpoint.'/dataCollectionRules/'.$this->dcrImmutableId.'/streams/'.$this->streamName.'?api-version=2023-01-01',
+                [
+                    $this->azureRecord->getAzureData($record),
+                ]
+            )
+            ->throwUnlessStatus(204);
     }
 
     public function setFormatter(FormatterInterface $formatter): HandlerInterface
