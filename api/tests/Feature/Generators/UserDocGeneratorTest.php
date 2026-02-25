1a3fb336666d62bc936e4887424e336cfa88ffd3
Eric Sizer <eric.sizer@gmail.com>
2026-02-25 16:42:44 -0500

    update to hardcode determistic values

--- api/tests/Feature/Generators/UserDocGeneratorTest.php
+++ api/tests/Feature/Generators/UserDocGeneratorTest.php
@@ -2,7 +2,22 @@
 
 namespace Tests\Feature\Generators;
 
+use App\Enums\ArmedForcesStatus;
+use App\Enums\CitizenshipStatus;
+use App\Enums\CSuiteRoleTitle;
+use App\Enums\ExecCoaching;
+use App\Enums\FlexibleWorkLocation;
+use App\Enums\IndigenousCommunity;
+use App\Enums\Language;
+use App\Enums\LearningOpportunitiesInterest;
+use App\Enums\Mentorship;
+use App\Enums\OperationalRequirement;
+use App\Enums\OrganizationTypeInterest;
+use App\Enums\PositionDuration;
+use App\Enums\ProvinceOrTerritory;
 use App\Enums\SkillLevel;
+use App\Enums\TargetRole;
+use App\Enums\TimeFrame;
 use App\Generators\UserDocGenerator;
 use App\Models\Classification;
 use App\Models\Community;
@@ -13,6 +28,7 @@ use App\Models\WorkStream;
 use Database\Seeders\RolePermissionSeeder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Storage;
 use Spatie\Snapshots\MatchesSnapshots;
 use Tests\TestCase;
@@ -38,8 +54,20 @@ class UserDocGeneratorTest extends TestCase
         $this->seed(RolePermissionSeeder::class);
 
         $community = Community::factory()->create();
-        Department::factory()->create();
-        Classification::factory()->create();
+        Department::factory()->create([
+            'name' => [
+                'en' => 'Snapshot dept EN',
+                'fr' => 'Snapshot dept FR',
+            ],
+        ]);
+        $classification = Classification::factory()->create([
+            'group' => 'XX',
+            'level' => 1,
+            'name' => [
+                'en' => 'Snapshot classification EN',
+                'fr' => 'Snapshot classification FR',
+            ],
+        ]);
         WorkStream::factory()->create();
 
         $adminUser = User::factory()
@@ -51,7 +79,93 @@ class UserDocGeneratorTest extends TestCase
             ->asApplicant()
             ->withGovEmployeeProfile()
             ->withCommunityInterests([$community->id])
-            ->create();
+            ->create([
+                // Contact info section
+                'first_name' => 'Snapshot',
+                'last_name' => 'User',
+                'email' => 'snapshot@test.com',
+                'telephone' => '555-555-5555',
+                'preferred_lang' => Language::FR->name,
+                'preferred_language_for_interview' => Language::FR->name,
+                'preferred_language_for_exam' => Language::FR->name,
+                // Status
+                'armed_forces_status' => ArmedForcesStatus::NON_CAF->name,
+                'citizenship' => CitizenshipStatus::CITIZEN->name,
+                // Language info
+                'looking_for_english' => true,
+                'looking_for_french' => true,
+                'looking_for_bilingual' => true,
+                // Work info
+                'work_email' => 'employee@test.com',
+                'has_priority_entitlement' => true,
+                'priority_number' => '12345',
+                'position_duration' => [PositionDuration::PERMANENT->name],
+                'accepted_operational_requirements' => [
+                    OperationalRequirement::DRIVERS_LICENSE->name,
+                    OperationalRequirement::ON_CALL->name,
+                    OperationalRequirement::OVERTIME_OCCASIONAL->name,
+                ],
+                'current_city' => 'The Town',
+                'current_province' => ProvinceOrTerritory::ONTARIO->name,
+                'flexible_work_locations' => [
+                    FlexibleWorkLocation::REMOTE->name,
+                    FlexibleWorkLocation::HYBRID->name,
+                ],
+                'location_exemptions' => 'Gotham City',
+                // DEI
+                'indigenous_communities' => [
+                    IndigenousCommunity::INUIT->name,
+                ],
+                'is_woman' => true,
+                'is_visible_minority' => true,
+                'has_disability' => true,
+            ]);
+
+        $targetUser->employeeProfile()->update([
+            'career_planning_lateral_move_interest' => true,
+            'career_planning_lateral_move_time_frame' => TimeFrame::THIS_YEAR->name,
+            'career_planning_lateral_move_organization_type' => [
+                OrganizationTypeInterest::CURRENT->name,
+                OrganizationTypeInterest::LARGE->name,
+            ],
+            'career_planning_promotion_move_interest' => true,
+            'career_planning_promotion_move_time_frame' => TimeFrame::ONE_TO_TWO_YEARS->name,
+            'career_planning_promotion_move_organization_type' => [
+                OrganizationTypeInterest::SMALL->name,
+                OrganizationTypeInterest::MEDIUM->name,
+            ],
+            'career_planning_learning_opportunities_interest' => [
+                LearningOpportunitiesInterest::ACADEMIC_PROGRAM->name,
+                LearningOpportunitiesInterest::PEER_NETWORKING->name,
+            ],
+            'eligible_retirement_year_known' => true,
+            'eligible_retirement_year' => Carbon::parse('3001'),
+            'career_planning_mentorship_status' => [Mentorship::MENTOR->name],
+            'career_planning_mentorship_interest' => [Mentorship::MENTEE->name],
+            'career_planning_exec_interest' => true,
+            'career_planning_exec_coaching_status' => [ExecCoaching::LEARNING->name],
+            'career_planning_exec_coaching_interest' => [ExecCoaching::COACHING->name],
+            'career_planning_about_you' => 'Snapshot about my career planning.',
+            'career_planning_learning_goals' => 'Snapshot learning goals.',
+            'career_planning_work_style' => 'Snapshot career planning work style.',
+            'next_role_job_title' => 'My next snapshot role',
+            'career_objective_job_title' => 'My career objective role',
+            'next_role_additional_information' => 'Additional snapshot information for my next role.',
+            'career_objective_additional_information' => 'Additional snapshot information for my career objective.',
+            'next_role_community_id' => $community->id,
+            'career_objective_community_id' => $community->id,
+            'next_role_community_other' => 'Snapshot other community next role',
+            'career_objective_community_other' => 'Snapshot other community career objective',
+            'next_role_classification_id' => $classification->id,
+            'career_objective_classification_id' => $classification->id,
+
+            'next_role_target_role' => TargetRole::ASSISTANT_DEPUTY_MINISTER->name,
+            'career_objective_target_role' => TargetRole::DEPUTY_MINISTER->name,
+            'next_role_is_c_suite_role' => true,
+            'career_objective_is_c_suite_role' => true,
+            'next_role_c_suite_role_title' => CSuiteRoleTitle::CHIEF_AUDIT_EXECUTIVE->name,
+            'career_objective_c_suite_role_title' => CSuiteRoleTitle::CHIEF_DATA_OFFICER->name,
+        ]);
 
         $skills = Skill::factory()->count(4)->create();
 
