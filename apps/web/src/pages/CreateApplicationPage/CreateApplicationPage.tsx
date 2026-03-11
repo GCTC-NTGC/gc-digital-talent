453639da7bcc0a838a975cc835677904f979b27b
Eric Sizer <eric.sizer@gmail.com>
2026-03-11 08:28:15 -0400

    remove unused hook

--- apps/web/src/pages/CreateApplicationPage/CreateApplicationPage.tsx
+++ apps/web/src/pages/CreateApplicationPage/CreateApplicationPage.tsx
@@ -10,7 +10,7 @@ import {
   tryFindMessageDescriptor,
   errorMessages,
 } from "@gc-digital-talent/i18n";
-import { ROLE_NAME, useAuthorization } from "@gc-digital-talent/auth";
+import { ROLE_NAME } from "@gc-digital-talent/auth";
 import { graphql, Scalars } from "@gc-digital-talent/graphql";
 import { appInsights } from "@gc-digital-talent/app-insights";
 
@@ -59,7 +59,6 @@ const CreateApplication = () => {
   const intl = useIntl();
   const paths = useRoutes();
   const navigate = useNavigate();
-  const auth = useAuthorization();
   const [{ data: newApplicationData }, executeMutation] = useMutation(
     CreateApplication_Mutation,
   );
