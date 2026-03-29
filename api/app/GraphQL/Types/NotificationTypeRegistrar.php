50b9af2bc6f13cc5c4dd46d7c999ad3509a5074c
Eric Sizer <eric.sizer@gmail.com>
2026-03-29 11:02:19 -0400

    improve types

---
+++ api/app/GraphQL/Types/NotificationTypeRegistrar.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\GraphQL\Types;
+
+use App\Discoverers\SubscriptionNotificationDiscoverer;
+use GraphQL\Type\Definition\EnumType;
+use Nuwave\Lighthouse\Schema\TypeRegistry;
+
+final class NotificationTypeRegistrar implements TypeRegistrarInterface
+{
+    public static int $weight = 5;
+
+    public static function register(TypeRegistry $typeRegistry): void
+    {
+        $typeRegistry->registerLazy('NotificationType', function () {
+            $notifications = SubscriptionNotificationDiscoverer::discover();
+            $values = [];
+
+            foreach ($notifications as $notification) {
+                $name = class_basename($notification);
+                $values[$name] = ['value' => $name];
+            }
+
+            return new EnumType([
+                'name' => 'NotificationType',
+                'values' => $values,
+            ]);
+        });
+    }
+}
