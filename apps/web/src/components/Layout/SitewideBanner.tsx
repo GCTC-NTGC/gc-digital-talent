5749123ad3e1408a17e57f6bbb5e0acc4ea383f4
Peter Giles <8978655+petertgiles@users.noreply.github.com>
2026-04-07 09:49:12 -0400

    banner dismissible

--- apps/web/src/components/Layout/SitewideBanner.tsx
+++ apps/web/src/components/Layout/SitewideBanner.tsx
@@ -8,6 +8,7 @@ import { graphql } from "@gc-digital-talent/graphql";
 import { parseDateTimeUtc } from "@gc-digital-talent/date-helpers";
 import { Loading } from "@gc-digital-talent/ui";
 import { getLocale } from "@gc-digital-talent/i18n";
+import { useLocalStorage } from "@gc-digital-talent/storage";
 
 const BannerContent = lazy(() => import("./BannerContent"));
 
@@ -15,6 +16,7 @@ const SitewideBanner_Query = graphql(/* GraphQL */ `
   query SitewideBanner {
     sitewideAnnouncement {
       isEnabled
+      isDismissible
       publishDate
       expiryDate
       title {
@@ -25,9 +27,13 @@ const SitewideBanner_Query = graphql(/* GraphQL */ `
         en
         fr
       }
+      updatedAt
     }
   }
 `);
+
+// local storage key
+const AnnouncementDismissedAtKey = "sitewide_announcement_dismissed_at";
 
 const SitewideBanner = () => {
   const intl = useIntl();
@@ -37,29 +43,53 @@ const SitewideBanner = () => {
     query: SitewideBanner_Query,
   });
 
+  const [announcementDismissedAt, setAnnouncementDismissedAt] =
+    useLocalStorage<number>(AnnouncementDismissedAtKey, 0);
+
   // Outage time range, date, starttime
   const announcementIsEnabled = !!data?.sitewideAnnouncement?.isEnabled;
+  const announcementIsDismissible = !!data?.sitewideAnnouncement?.isDismissible;
+
+  // publish date
   const announcementPublishDate = data?.sitewideAnnouncement?.publishDate
     ? parseDateTimeUtc(data.sitewideAnnouncement.publishDate)
     : null;
   const announcementPublishDateIsValid =
     announcementPublishDate && !Number.isNaN(announcementPublishDate.getTime());
+  const nowIsAfterValidPublishDate =
+    announcementPublishDateIsValid &&
+    isAfter(Date.now(), announcementPublishDate);
 
+  // expiry date
   const announcementExpiryDate = data?.sitewideAnnouncement?.expiryDate
     ? parseDateTimeUtc(data.sitewideAnnouncement.expiryDate)
     : null;
   const announcementExpiryDateIsValid =
     announcementExpiryDate && !Number.isNaN(announcementExpiryDate.getTime());
-
-  const showMaintenanceBanner =
-    announcementIsEnabled &&
-    announcementPublishDateIsValid &&
-    isAfter(Date.now(), announcementPublishDate) &&
+  const nowIsBeforeValidExpiryDate =
     announcementExpiryDateIsValid &&
     isBefore(Date.now(), announcementExpiryDate);
 
+  // dismissed
+  const announcementUpdatedAt = data?.sitewideAnnouncement?.updatedAt
+    ? parseDateTimeUtc(data?.sitewideAnnouncement?.updatedAt).valueOf()
+    : 0;
+  const announcementIsDismissed =
+    announcementIsDismissible &&
+    announcementDismissedAt >= announcementUpdatedAt;
+
+  const showMaintenanceBanner =
+    announcementIsEnabled &&
+    nowIsAfterValidPublishDate &&
+    nowIsBeforeValidExpiryDate &&
+    !announcementIsDismissed;
+
   const title = data?.sitewideAnnouncement?.title[locale];
   const message = data?.sitewideAnnouncement?.message[locale];
+
+  const handleDismiss = () => {
+    setAnnouncementDismissedAt(announcementUpdatedAt);
+  };
 
   if (!title || !message) {
     return null;
@@ -68,7 +98,11 @@ const SitewideBanner = () => {
   return (
     showMaintenanceBanner && (
       <Suspense fallback={<Loading inline />}>
-        <BannerContent title={title} message={message} />
+        <BannerContent
+          title={title}
+          message={message}
+          onDismiss={announcementIsDismissible ? handleDismiss : undefined}
+        />
       </Suspense>
     )
   );
