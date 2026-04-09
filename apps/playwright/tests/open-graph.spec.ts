9e336ef7a0ede42ed8f2775e437e09120fb471aa
Eric Sizer <eric.sizer@gmail.com>
2026-04-09 09:27:35 -0400

    update test to mock substitute file

--- apps/playwright/tests/open-graph.spec.ts
+++ apps/playwright/tests/open-graph.spec.ts
@@ -3,13 +3,21 @@ import { test, expect } from "@playwright/test";
 test.describe("Open Graph", () => {
   test("should specify an image", async ({ page }) => {
     await page.goto("/en");
-    const locator = String(
-      await page
-        .locator('head meta[property="og:image"]')
-        .getAttribute("content"),
+
+    const ogImage = await page
+      .locator('meta[property="og:image"]')
+      .getAttribute("content");
+
+    const rawUrl: string = ogImage ?? "";
+    expect(rawUrl).not.toBe("");
+
+    // Mock substitute_file.sh
+    const testUrl: string = rawUrl.replace(
+      "$APP_URL",
+      "https://talent.canada.ca",
     );
-    await page.goto(locator).then((response) => {
-      expect(response?.status()).toEqual(200);
-    });
+
+    const response = await page.request.get(testUrl);
+    expect(response.status()).toBe(200);
   });
 });
