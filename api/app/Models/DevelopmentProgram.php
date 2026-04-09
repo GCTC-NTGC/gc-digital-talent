dad7f3870efd4f93a50103c603f5fb02bae8632d
Vachan <vachanbolt@hotmail.com>
2026-04-09 10:59:12 -0600

    remove deprecated

--- api/app/Models/DevelopmentProgram.php
+++ api/app/Models/DevelopmentProgram.php
@@ -5,7 +5,6 @@ namespace App\Models;
 use App\Casts\LocalizedString;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Support\Carbon;
@@ -39,12 +38,6 @@ class DevelopmentProgram extends Model
         'description_for_profile' => LocalizedString::class,
         'description_for_nominations' => LocalizedString::class,
     ];
-
-    /** @return BelongsTo<Community, $this> */
-    public function community(): BelongsTo
-    {
-        return $this->belongsTo(Community::class);
-    }
 
     /** @return BelongsToMany<Classification, $this> */
     public function eligibleClassifications(): BelongsToMany
