1f230e45504e38846e8cbfa56458037f6401876c
Matt Nigh <matt@mattnigh.com>
2026-04-10 11:33:06 -0400

    Fix input validators

--- api/app/GraphQL/Validators/CreateDevelopmentProgramInputValidator.php
+++ api/app/GraphQL/Validators/CreateDevelopmentProgramInputValidator.php
@@ -17,8 +17,8 @@ final class CreateDevelopmentProgramInputValidator extends Validator
         return [
             'informationUrl.en' => ['required_with:informationUrl.fr', 'string', 'nullable', 'url:http,https'],
             'informationUrl.fr' => ['required_with:informationUrl.en', 'string', 'nullable', 'url:http,https'],
-            'abbreviation.en' => ['required_with:abbreviation.fr', 'string', 'nullable', 'url:http,https'],
-            'abbreviation.fr' => ['required_with:abbreviation.en', 'string', 'nullable', 'url:http,https'],
+            'abbreviation.en' => ['required_with:abbreviation.fr', 'string', 'nullable'],
+            'abbreviation.fr' => ['required_with:abbreviation.en', 'string', 'nullable'],
         ];
     }
 
