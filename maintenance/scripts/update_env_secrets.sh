3956a4f9c791cebd79cdda97709ee3352e691f0c
Eric Sizer <esizer@users.noreply.github.com>
2026-04-15 12:16:14 -0400

    Update maintenance/scripts/update_env_secrets.sh
    Co-authored-by: Matt <mnigh@users.noreply.github.com>

--- maintenance/scripts/update_env_secrets.sh
+++ maintenance/scripts/update_env_secrets.sh
@@ -3,7 +3,7 @@
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 source ${parent_path}/lib/common.sh
 
-# Random string generation that works on OSX and Linux. (Plus not pipefails.)
+# Random string generation that works on macOS and Linux. (Plus not pipefails.)
 generate_secret() {
   dd if=/dev/urandom bs=48 count=1 2>/dev/null | base64 | tr -dc '[a-zA-Z0-9]' | fold -w32 -- | head -n 1
 }
