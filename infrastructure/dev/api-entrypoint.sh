ce3293d93b59537b4766cb09c8eb8b6222367214
Eric Sizer <eric.sizer@gmail.com>
2026-04-15 12:55:22 -0400

    push queue to background

--- infrastructure/dev/api-entrypoint.sh
+++ infrastructure/dev/api-entrypoint.sh
@@ -20,7 +20,7 @@ php artisan lighthouse:print-schema --write
 
 # Start background workers
 echo "Starting background jobs..."
-php artisan queue:work
+php artisan queue:work &
 
 # Start the Laravel development server
 exec php artisan serve --host=0.0.0.0 --port=8080
