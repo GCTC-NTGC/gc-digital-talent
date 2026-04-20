9d75950f692ced8429e62c49f4efb3cf45aefcf3
Eric Sizer <eric.sizer@gmail.com>
2026-04-20 15:58:21 -0400

    reorg snapshot components

---
+++ apps/web/src/components/ApplicationSnapshot/Sections/QuestionResponses/ScreeningQuestionResponsesSnapshot.tsx
@@ -0,0 +1,40 @@
+import {
+  getFragment,
+  graphql,
+  type FragmentType,
+} from "@gc-digital-talent/graphql";
+import { unpackMaybes } from "@gc-digital-talent/helpers";
+import QuestionResponses from "./QuestionResponses";
+
+const ScreeningQuestionResponsesSnapshot_Fragment = graphql(/** GraphQL */ `
+  fragment ScreeningQuestionResponsesSnapshot on PoolCandidate {
+    screeningQuestionResponses {
+      id
+      answer
+      screeningQuestion {
+        question {
+          localized
+        }
+      }
+    }
+  }
+`);
+
+interface ScreeningQuestionResponsesSnapshotProps {
+  query: FragmentType<typeof ScreeningQuestionResponsesSnapshot_Fragment>;
+}
+
+const ScreeningQuestionResponsesSnapshot = ({
+  query,
+}: ScreeningQuestionResponsesSnapshotProps) => {
+  const application = getFragment(
+    ScreeningQuestionResponsesSnapshot_Fragment,
+    query,
+  );
+
+  return (
+    <QuestionResponses responses={application.screeningQuestionResponses} />
+  );
+};
+
+export default ScreeningQuestionResponsesSnapshot;
