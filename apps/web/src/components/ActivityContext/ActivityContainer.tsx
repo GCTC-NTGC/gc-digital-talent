0795079f90f4ae7954c020d963b58edb1548ab99
Peter Giles <8978655+petertgiles@users.noreply.github.com>
2026-04-20 15:21:13 -0400

    set prod timeouts

--- apps/web/src/components/ActivityContext/ActivityContainer.tsx
+++ apps/web/src/components/ActivityContext/ActivityContainer.tsx
@@ -6,8 +6,8 @@ import { useFeatureFlags } from "@gc-digital-talent/env";
 
 import InactivityDialog from "./InactivityDialog";
 
-const timeout = 10_000;
-const promptBeforeIdle = 4_000;
+const timeout = 1000 * 60 * 60; // 60 minutes to milliseconds
+const promptBeforeIdle = 1000 * 60 * 55; // prompt five minutes before timing out
 
 interface InnerActivityContainerProps {
   logout: ReturnType<typeof useAuthentication>["logout"];
