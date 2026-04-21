b44d7280a21cb9f066400f8e19c8774a8a88ab88
Vachan <vachanbolt@hotmail.com>
2026-04-21 15:21:54 -0600

    use a transaction

--- api/app/GraphQL/Mutations/CreateCommunityInterestWithDevelopmentPrograms.php
+++ api/app/GraphQL/Mutations/CreateCommunityInterestWithDevelopmentPrograms.php
@@ -4,6 +4,7 @@ namespace App\GraphQL\Mutations;
 
 use App\Models\CommunityInterest;
 use App\Models\DevelopmentProgramUser;
+use Illuminate\Support\Facades\DB;
 
 final class CreateCommunityInterestWithDevelopmentPrograms
 {
@@ -16,43 +17,57 @@ final class CreateCommunityInterestWithDevelopmentPrograms
         $communityInterestInput = $args['communityInterest'];
         $developmentProgramUserInput = array_key_exists('developmentPrograms', $args) ? $args['developmentPrograms'] : [];
 
-        // start with the community interest
-        $communityInterest = CommunityInterest::create(
-            [
-                'community_id' => $communityInterestInput['community_id'],
-                'user_id' => $userId,
-                'job_interest' => $communityInterestInput['job_interest'] ?? null,
-                'training_interest' => $communityInterestInput['training_interest'] ?? null,
-                'additional_information' => $communityInterestInput['additional_information'] ?? null,
-                'finance_is_chief' => $communityInterestInput['finance_is_chief'] ?? null,
-                'finance_additional_duties' => $communityInterestInput['finance_additional_duties'] ?? null,
-                'finance_other_roles' => $communityInterestInput['finance_other_roles'] ?? null,
-                'finance_other_roles_other' => $communityInterestInput['finance_other_roles_other'] ?? null,
-                'consent_to_share_profile' => $communityInterestInput['consent_to_share_profile'] ?? null,
-            ],
-        );
+        // combine all mutating in a transaction
+        DB::beginTransaction();
 
-        // sync the workstreams relation if applicable
-        if (
-            array_key_exists('workStreams', $communityInterestInput) &&
-            array_key_exists('sync', $communityInterestInput['workStreams'])
-        ) {
-            $communityInterest->workStreams()->sync($communityInterestInput['workStreams']['sync']);
-        }
-
-        // next the development programs
-        foreach ($developmentProgramUserInput as $input) {
-            DevelopmentProgramUser::updateOrCreate(
+        try {
+            // start with the community interest
+            $communityInterest = CommunityInterest::create(
                 [
-                    'development_program_id' => $input['development_program_id'],
+                    'community_id' => $communityInterestInput['community_id'],
                     'user_id' => $userId,
+                    'job_interest' => $communityInterestInput['job_interest'] ?? null,
+                    'training_interest' => $communityInterestInput['training_interest'] ?? null,
+                    'additional_information' => $communityInterestInput['additional_information'] ?? null,
+                    'finance_is_chief' => $communityInterestInput['finance_is_chief'] ?? null,
+                    'finance_additional_duties' => $communityInterestInput['finance_additional_duties'] ?? null,
+                    'finance_other_roles' => $communityInterestInput['finance_other_roles'] ?? null,
+                    'finance_other_roles_other' => $communityInterestInput['finance_other_roles_other'] ?? null,
+                    'consent_to_share_profile' => $communityInterestInput['consent_to_share_profile'] ?? null,
                 ],
-                [
-                    'education_experience_id' => $input['education_experience_id'] ?? null,
-                    'participation_status' => $input['participation_status'] ?? null,
-                    'completion_date' => $input['completion_date'] ?? null,
-                ]
             );
+
+            // sync the workstreams relation if applicable
+            if (
+                array_key_exists('workStreams', $communityInterestInput) &&
+                array_key_exists('sync', $communityInterestInput['workStreams'])
+            ) {
+                $communityInterest->workStreams()->sync($communityInterestInput['workStreams']['sync']);
+            }
+
+            // next the development programs
+            foreach ($developmentProgramUserInput as $input) {
+                DevelopmentProgramUser::updateOrCreate(
+                    [
+                        'development_program_id' => $input['development_program_id'],
+                        'user_id' => $userId,
+                    ],
+                    [
+                        'education_experience_id' => $input['education_experience_id'] ?? null,
+                        'participation_status' => $input['participation_status'] ?? null,
+                        'completion_date' => $input['completion_date'] ?? null,
+                    ]
+                );
+            }
+
+            // commit all together
+            DB::commit();
+
+        } catch (\Throwable $error) {
+
+            // rollback and error
+            DB::rollBack();
+            throw $error;
         }
 
         return $communityInterest;
--- api/app/GraphQL/Mutations/UpdateCommunityInterestWithDevelopmentPrograms.php
+++ api/app/GraphQL/Mutations/UpdateCommunityInterestWithDevelopmentPrograms.php
@@ -4,6 +4,7 @@ namespace App\GraphQL\Mutations;
 
 use App\Models\CommunityInterest;
 use App\Models\DevelopmentProgramUser;
+use Illuminate\Support\Facades\DB;
 
 final class UpdateCommunityInterestWithDevelopmentPrograms
 {
@@ -19,57 +20,71 @@ final class UpdateCommunityInterestWithDevelopmentPrograms
         $communityInterest = CommunityInterest::findOrFail($communityInterestId);
         $userId = $communityInterest->user_id;
 
-        // update values only if an input key is present
-        if (array_key_exists('job_interest', $communityInterestInput)) {
-            $communityInterest->job_interest = $communityInterestInput['job_interest'];
-        }
-        if (array_key_exists('training_interest', $communityInterestInput)) {
-            $communityInterest->training_interest = $communityInterestInput['training_interest'];
-        }
-        if (array_key_exists('additional_information', $communityInterestInput)) {
-            $communityInterest->additional_information = $communityInterestInput['additional_information'];
-        }
-        if (array_key_exists('finance_is_chief', $communityInterestInput)) {
-            $communityInterest->finance_is_chief = $communityInterestInput['finance_is_chief'];
-        }
-        if (array_key_exists('finance_additional_duties', $communityInterestInput)) {
-            $communityInterest->finance_additional_duties = $communityInterestInput['finance_additional_duties'];
-        }
-        if (array_key_exists('finance_other_roles', $communityInterestInput)) {
-            $communityInterest->finance_other_roles = $communityInterestInput['finance_other_roles'];
-        }
-        if (array_key_exists('finance_other_roles_other', $communityInterestInput)) {
-            $communityInterest->finance_other_roles_other = $communityInterestInput['finance_other_roles_other'];
-        }
-        if (array_key_exists('consent_to_share_profile', $communityInterestInput)) {
-            $communityInterest->consent_to_share_profile = $communityInterestInput['consent_to_share_profile'];
-        }
+        // combine all mutating in a transaction
+        DB::beginTransaction();
 
-        // sync the workstreams relation if applicable
-        if (
-            array_key_exists('workStreams', $communityInterestInput) &&
-            array_key_exists('sync', $communityInterestInput['workStreams'])
-        ) {
-            $communityInterest->workStreams()->sync($communityInterestInput['workStreams']['sync']);
-        }
+        try {
+            // update values only if an input key is present
+            if (array_key_exists('job_interest', $communityInterestInput)) {
+                $communityInterest->job_interest = $communityInterestInput['job_interest'];
+            }
+            if (array_key_exists('training_interest', $communityInterestInput)) {
+                $communityInterest->training_interest = $communityInterestInput['training_interest'];
+            }
+            if (array_key_exists('additional_information', $communityInterestInput)) {
+                $communityInterest->additional_information = $communityInterestInput['additional_information'];
+            }
+            if (array_key_exists('finance_is_chief', $communityInterestInput)) {
+                $communityInterest->finance_is_chief = $communityInterestInput['finance_is_chief'];
+            }
+            if (array_key_exists('finance_additional_duties', $communityInterestInput)) {
+                $communityInterest->finance_additional_duties = $communityInterestInput['finance_additional_duties'];
+            }
+            if (array_key_exists('finance_other_roles', $communityInterestInput)) {
+                $communityInterest->finance_other_roles = $communityInterestInput['finance_other_roles'];
+            }
+            if (array_key_exists('finance_other_roles_other', $communityInterestInput)) {
+                $communityInterest->finance_other_roles_other = $communityInterestInput['finance_other_roles_other'];
+            }
+            if (array_key_exists('consent_to_share_profile', $communityInterestInput)) {
+                $communityInterest->consent_to_share_profile = $communityInterestInput['consent_to_share_profile'];
+            }
 
-        // save and refresh the model
-        $communityInterest->save();
-        $communityInterest->refresh();
+            // sync the workstreams relation if applicable
+            if (
+                array_key_exists('workStreams', $communityInterestInput) &&
+                array_key_exists('sync', $communityInterestInput['workStreams'])
+            ) {
+                $communityInterest->workStreams()->sync($communityInterestInput['workStreams']['sync']);
+            }
 
-        // next the development programs
-        foreach ($developmentProgramUserInput as $input) {
-            DevelopmentProgramUser::updateOrCreate(
-                [
-                    'development_program_id' => $input['development_program_id'],
-                    'user_id' => $userId,
-                ],
-                [
-                    'education_experience_id' => $input['education_experience_id'] ?? null,
-                    'participation_status' => $input['participation_status'] ?? null,
-                    'completion_date' => $input['completion_date'] ?? null,
-                ]
-            );
+            // save and refresh the model
+            $communityInterest->save();
+            $communityInterest->refresh();
+
+            // next the development programs
+            foreach ($developmentProgramUserInput as $input) {
+                DevelopmentProgramUser::updateOrCreate(
+                    [
+                        'development_program_id' => $input['development_program_id'],
+                        'user_id' => $userId,
+                    ],
+                    [
+                        'education_experience_id' => $input['education_experience_id'] ?? null,
+                        'participation_status' => $input['participation_status'] ?? null,
+                        'completion_date' => $input['completion_date'] ?? null,
+                    ]
+                );
+            }
+
+            // commit all together
+            DB::commit();
+
+        } catch (\Throwable $error) {
+
+            // rollback and error
+            DB::rollBack();
+            throw $error;
         }
 
         return $communityInterest;
