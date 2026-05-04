f1b16a5f2c35ca2f50c368bed6afb2d68a8ddc78
Eric Sizer <eric.sizer@gmail.com>
2026-05-04 14:25:18 -0400

    add stub dialog

---
+++ apps/web/src/pages/Applications/components/DeleteApplicationDialog/DeleteApplicationDialog.tsx
@@ -0,0 +1,110 @@
+import TrashIcon from "@heroicons/react/16/solid/TrashIcon";
+import { useIntl } from "react-intl";
+import { useNavigate } from "react-router";
+import { useMutation } from "urql";
+
+import { Button, Dialog } from "@gc-digital-talent/ui";
+import { toast } from "@gc-digital-talent/toast";
+import { commonMessages } from "@gc-digital-talent/i18n";
+import {
+  ApplicationStatus,
+  getFragment,
+  graphql,
+  type FragmentType,
+} from "@gc-digital-talent/graphql";
+
+import useRoutes from "~/hooks/useRoutes";
+
+const DeleteApplication_Mutation = graphql(/** GraphQL */ `
+  mutation DeleteApplication($id: ID!) {
+    deleteApplication(id: $id) {
+      id
+    }
+  }
+`);
+
+const DeleteApplicationDialog_Fragment = graphql(/** GraphQL */ `
+  fragment DeleteApplicationDialog on PoolCandidate {
+    id
+    status {
+      value
+    }
+  }
+`);
+
+interface DeleteApplicationDialogProps {
+  query: FragmentType<typeof DeleteApplicationDialog_Fragment>;
+}
+
+const DeleteApplicationDialog = ({ query }: DeleteApplicationDialogProps) => {
+  const intl = useIntl();
+  const navigate = useNavigate();
+  const paths = useRoutes();
+  const application = getFragment(DeleteApplicationDialog_Fragment, query);
+  const [, executeMutation] = useMutation(DeleteApplication_Mutation);
+
+  // NOTE: Only drafts can be deleted
+  if (application.status?.value !== ApplicationStatus.Draft) return null;
+
+  const title = intl.formatMessage({
+    defaultMessage: "Delete application",
+    id: "Vu6IUs",
+    description: "Title for deleting an application",
+  });
+
+  const handleDelete = async () => {
+    executeMutation({ id: application.id })
+      .then(async (res) => {
+        if (!res?.data?.deleteApplication || res?.error) {
+          throw new Error();
+        }
+
+        toast.success(
+          intl.formatMessage({
+            defaultMessage: "Application deleted successfully!",
+            id: "A9zJ2+",
+            description: "Message displayed after deleting an application",
+          }),
+        );
+
+        await navigate(paths.applicantDashboard());
+      })
+      .catch(() => {
+        toast.error(
+          intl.formatMessage({
+            defaultMessage: "Failed to delete application.",
+            id: "uOrm+o",
+            description:
+              "Message displayed when an application could no be deleted",
+          }),
+        );
+      });
+  };
+
+  return (
+    <Dialog.Root>
+      <Dialog.Trigger>
+        <Button mode="inline" color="error" icon={TrashIcon}>
+          {title}
+        </Button>
+      </Dialog.Trigger>
+      <Dialog.Content>
+        <Dialog.Header>{title}</Dialog.Header>
+        <Dialog.Body>
+          <Dialog.Footer>
+            <Button color="error" onClick={handleDelete}>
+              {title}
+            </Button>
+            <Dialog.Close>
+              <Button mode="inline" color="warning">
+                {intl.formatMessage(commonMessages.cancel)}
+              </Button>
+            </Dialog.Close>
+          </Dialog.Footer>
+        </Dialog.Body>
+      </Dialog.Content>
+    </Dialog.Root>
+  );
+};
+
+export default DeleteApplicationDialog;
