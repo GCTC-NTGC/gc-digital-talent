dbe393aeb237c210650ce680fb0cd860082dc3df
Peter Giles <8978655+petertgiles@users.noreply.github.com>
2026-05-05 14:44:55 -0400

    index department page

--- apps/web/src/pages/Departments/IndexDepartmentPage.tsx
+++ apps/web/src/pages/Departments/IndexDepartmentPage.tsx
@@ -5,14 +5,26 @@ import { ROLE_NAME } from "@gc-digital-talent/auth";
 import SEO from "~/components/SEO/SEO";
 import useRoutes from "~/hooks/useRoutes";
 import useBreadcrumbs from "~/hooks/useBreadcrumbs";
-import RequireAuth from "~/components/RequireAuth/RequireAuth";
 import pageTitles from "~/messages/pageTitles";
 import Hero from "~/components/Hero";
 import AdminContentWrapper from "~/components/AdminContentWrapper/AdminContentWrapper";
+import { requireUser } from "~/routing/auth";
 
+import type { Route } from "./+types/IndexDepartmentPage";
 import DepartmentTableApi from "./components/DepartmentTable";
 
-export const DepartmentPage = () => {
+export const clientMiddleware: Route.ClientMiddlewareFunction[] = [
+  async ({ context, request }, next) => {
+    requireUser(context, request, [
+      { name: ROLE_NAME.PlatformAdmin },
+      { name: ROLE_NAME.DepartmentAdmin },
+      { name: ROLE_NAME.DepartmentHRAdvisor },
+    ]);
+    return await next();
+  },
+];
+
+const Component = () => {
   const intl = useIntl();
   const routes = useRoutes();
   const formattedPageTitle = intl.formatMessage(pageTitles.departments);
@@ -36,18 +48,6 @@ export const DepartmentPage = () => {
     </>
   );
 };
-
-export const Component = () => (
-  <RequireAuth
-    roles={[
-      ROLE_NAME.PlatformAdmin,
-      ROLE_NAME.DepartmentAdmin,
-      ROLE_NAME.DepartmentHRAdvisor,
-    ]}
-  >
-    <DepartmentPage />
-  </RequireAuth>
-);
 
 Component.displayName = "AdminIndexDepartmentPage";
 
