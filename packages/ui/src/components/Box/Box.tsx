974b7c28d2a7c0c5578c5e621b225a46ce813567
Eric Sizer <eric.sizer@gmail.com>
2026-05-09 09:31:45 -0400

    add polymorphic box

---
+++ packages/ui/src/components/Box/Box.tsx
@@ -0,0 +1,23 @@
+import type { ElementType, ComponentPropsWithRef, ReactNode } from "react";
+
+export type BoxProps<T extends ElementType> = {
+  as?: T;
+  children?: ReactNode;
+} & Omit<ComponentPropsWithRef<T>, "as" | "children">;
+
+function Box<T extends ElementType = "div">({
+  as,
+  children,
+  ref,
+  ...props
+}: BoxProps<T>) {
+  const Component = as ?? "div";
+
+  return (
+    <Component {...props} ref={ref}>
+      {children}
+    </Component>
+  );
+}
+
+export default Box;
