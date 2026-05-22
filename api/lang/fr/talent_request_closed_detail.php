127944a2f3c57825ac3c86fc0b6580b71376512f
Eric Sizer <eric.sizer@gmail.com>
2026-05-22 16:11:52 -0400

    fix translation

--- api/lang/fr/talent_request_closed_detail.php
+++ api/lang/fr/talent_request_closed_detail.php
@@ -3,7 +3,7 @@
 return [
     'hire_made' => 'Embauche effectuée',
     'process_will_be_launched' => 'Le processus sera lancé',
-    'hiring_manager_not_responsive' => 'Gestionnaire d’embauche non réactif\'embauche ne répond pas',
+    'hiring_manager_not_responsive' => 'Gestionnaire d’embauche non réactif',
     'no_candidates_found' => 'Aucune candidature trouvée',
     'no_longer_required' => 'N’est plus requis',
     'duplicate_request' => 'Demande en double',
