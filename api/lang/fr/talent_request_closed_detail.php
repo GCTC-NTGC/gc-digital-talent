f0af331645dd781a3f4b2af0f91de6cbb0f5d320
Eric Sizer <eric.sizer@gmail.com>
2026-05-22 16:10:19 -0400

    update translations

--- api/lang/fr/talent_request_closed_detail.php
+++ api/lang/fr/talent_request_closed_detail.php
@@ -2,10 +2,10 @@
 
 return [
     'hire_made' => 'Embauche effectuée',
-    'process_will_be_launched' => 'Un processus sera lancé',
-    'hiring_manager_not_responsive' => 'Le gestionnaire d\'embauche ne répond pas',
-    'no_candidates_found' => 'Aucun candidat trouvé',
-    'no_longer_required' => 'Plus nécessaire',
+    'process_will_be_launched' => 'Le processus sera lancé',
+    'hiring_manager_not_responsive' => 'Gestionnaire d’embauche non réactif\'embauche ne répond pas',
+    'no_candidates_found' => 'Aucune candidature trouvée',
+    'no_longer_required' => 'N’est plus requis',
     'duplicate_request' => 'Demande en double',
     'non_compliant' => 'Non conforme',
 ];
