9a9b48e126b3f0a183df0ba541646510cdad94e1
Eric Sizer <esizer@users.noreply.github.com>
2026-05-22 13:39:01 -0400

    [Dep] Upgrade activty log to v5 (#16853)
    * bump version
    
    * update namespaces
    
    * add migration
    
    * update config
    
    * fix empty log submission
    
    * update base activity model
    
    * fix custom logging trait
    
    * fix test
    
    * lint fix

--- api/tests/Feature/ActivityLogTest.php
+++ api/tests/Feature/ActivityLogTest.php
@@ -6,6 +6,7 @@ use App\Enums\ActivityLog;
 use App\Enums\ApplicationStatus;
 use App\Enums\PoolCandidateSearchPositionType;
 use App\Enums\PoolCandidateSearchRequestReason;
+use App\Models\Activity;
 use App\Models\Community;
 use App\Models\Department;
 use App\Models\Pool;
@@ -14,7 +15,6 @@ use Database\Seeders\RolePermissionSeeder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Nuwave\Lighthouse\Testing\MakesGraphQLRequests;
 use Nuwave\Lighthouse\Testing\RefreshesSchemaCache;
-use Spatie\Activitylog\Models\Activity;
 use Tests\TestCase;
 use Tests\UsesProtectedGraphqlEndpoint;
 
@@ -227,7 +227,7 @@ class ActivityLogTest extends TestCase
 
         // assert can query all the actions undertaken or caused by a user
         $actingUser = User::where('email', 'admin-user@test.com')->sole();
-        $actions = $actingUser->actions;
+        $actions = $actingUser->activitiesAsCauser;
         assertEquals(4, count($actions)); // total
         assertEquals(2, count($actions->where('description', 'updated'))); // two update actions
         assertEquals(2, count($actions->where('subject_type', 'App\Models\PoolCandidate'))); // two events on PoolCandidate
