c9442768e8f8f359fbe9d02a2887ad809deca4f4
Yoni K <yonikid15@gmail.com>
2026-05-26 13:56:24 -0400

    Remove empty className

--- apps/web/src/pages/CommunityInterests/sections/ReviewAndSubmit.tsx
+++ apps/web/src/pages/CommunityInterests/sections/ReviewAndSubmit.tsx
@@ -57,7 +57,7 @@ const ReviewAndSubmit = ({
     )?.name?.localized ?? intl.formatMessage(commonMessages.notFound);
 
   return (
-    <div className="">
+    <div>
       {/* heading and description */}
       <Heading
         level="h2"
