ab8251c8a40ba324c7c46d0855626791d1a8493b
Eric Sizer <eric.sizer@gmail.com>
2026-05-27 09:56:30 -0400

    add builder

---
+++ api/app/Builders/TalentRequestBuilder.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Builders;
+
+use App\Models\Department;
+use App\Models\User;
+use Database\Helpers\TeamHelpers;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Facades\Auth;
+
+class TalentRequestBuilder extends Builder
+{
+    public function whereId(?string $id): self
+    {
+        return $this->when($id, fn ($q) => $q->where('id', 'ilike', "%{$id}%"));
+    }
+
+    public function whereFullName(?string $fullName): self
+    {
+        return $this->when($fullName, fn ($q) => $q->where('full_name', 'ilike', "%{$fullName}%"));
+    }
+
+    public function whereEmail(?string $email): self
+    {
+        return $this->when($email, fn ($q) => $q->where('email', 'ilike', "%{$email}%"));
+    }
+
+    public function whereJobTitle(?string $jobTitle): self
+    {
+        return $this->when($jobTitle, fn ($q) => $q->where('job_title', 'ilike', "%{$jobTitle}%"));
+    }
+
+    public function whereAdditionalComments(?string $additionalComments): self
+    {
+        return $this->when($additionalComments, fn ($q) => $q->where('additional_comments', 'ilike', "%{$additionalComments}%"));
+    }
+
+    public function whereAdminNotes(?string $adminNotes): self
+    {
+        return $this->when($adminNotes, fn ($q) => $q->where('admin_notes', 'ilike', "%{$adminNotes}%"));
+    }
+
+    public function whereGeneralSearch(?string $search): self
+    {
+        return $this->when($search, fn ($q) => $q->where(fn (Builder $q) => $q
+            ->where('full_name', 'ilike', "%{$search}%")
+            ->orWhere('id', 'ilike', "%{$search}%")
+            ->orWhere('email', 'ilike', "%{$search}%")
+            ->orWhere('job_title', 'ilike', "%{$search}%")
+            ->orWhere('additional_comments', 'ilike', "%{$search}%")
+            ->orWhere('admin_notes', 'ilike', "%{$search}%")
+        ));
+    }
+
+    public function whereTalentRequestStatus(?array $statuses): self
+    {
+        return $this->when(! empty($statuses), fn ($q) => $q->whereIn('status', $statuses));
+    }
+
+    public function whereWorkStreams(?array $streamIds): self
+    {
+        return $this->when(! empty($streamIds), fn ($q) => $q->whereHas('applicantFilter', fn ($q) => $q
+            ->whereHas('qualifiedInWorkStreams', fn ($q) => $q
+                ->whereIn('applicant_filter_work_stream.work_stream_id', $streamIds)
+            )
+        ));
+    }
+
+    public function whereDepartments(?array $departmentIds): self
+    {
+        return $this->when(! empty($departmentIds), fn ($q) => $q->whereHas('department', fn (Builder $q) =>
+            Department::scopeDepartmentsByIds($q, $departmentIds)
+        ));
+    }
+
+    public function whereClassifications(?array $classificationIds): self
+    {
+        return $this->when(! empty($classificationIds), fn ($q) => $q->whereHas('applicantFilter', fn ($q) => $q
+            ->whereHas('qualifiedInClassifications', fn ($q) => $q
+                ->whereIn('classifications.id', $classificationIds)
+            )
+        ));
+    }
+
+    public function whereAuthorizedToView(): self
+    {
+        /** @var User | null */
+        $user = Auth::user();
+
+        if ($user?->isAbleTo('view-any-searchRequest')) {
+            return $this;
+        }
+
+        if ($user?->isAbleTo('view-team-searchRequest')) {
+            $teamIds = TeamHelpers::getTeamIdsForPermission($user, 'view-team-searchRequest');
+
+            return $this->whereHas('community.team', fn (Builder $q) => $q->whereIn('id', $teamIds));
+        }
+
+        return $this->where('id', null);
+    }
+
+    public function withPolicyEagerLoads(): self
+    {
+        return $this->with(['community.team']);
+    }
+}
