ad80acb59d07c08cde94a6c5824eb1f8dcdd7e6b
Eric Sizer <eric.sizer@gmail.com>
2026-05-27 16:22:32 -0400

    remove uneeded migrations

--- api/database/migrations/2026_05_26_133943_rename_closed_to_complete_in_talent_request_status.php
+++ api/database/migrations/2026_05_26_133943_rename_closed_to_complete_in_talent_request_status.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Schema;
 
 return new class() extends Migration
 {
@@ -23,23 +22,6 @@ return new class() extends Migration
         );
 
         DB::statement('ALTER TABLE pool_candidate_search_requests RENAME COLUMN closed_details TO completion_details');
-
-        if (Schema::hasTable('talent_requests')) {
-            DB::statement('ALTER TABLE talent_requests DROP COLUMN status_weight');
-            DB::statement("UPDATE talent_requests SET status = 'COMPLETED' WHERE status = 'CLOSED'");
-            DB::statement(<<<'SQL'
-                ALTER TABLE talent_requests
-                ADD COLUMN status_weight integer GENERATED ALWAYS AS (
-                CASE
-                    WHEN (status::text = 'NEW'::text)         THEN 10
-                    WHEN (status::text = 'IN_PROGRESS'::text) THEN 20
-                    WHEN (status::text = 'COMPLETED'::text)   THEN 30
-                    ELSE NULL::integer
-                END) STORED
-                SQL
-            );
-            DB::statement('ALTER TABLE talent_requests RENAME COLUMN closed_details TO completion_details');
-        }
     }
 
     public function down(): void
@@ -58,22 +40,5 @@ return new class() extends Migration
             END) STORED
             SQL
         );
-
-        if (Schema::hasTable('talent_requests')) {
-            DB::statement('ALTER TABLE talent_requests RENAME COLUMN completion_details TO closed_details');
-            DB::statement('ALTER TABLE talent_requests DROP COLUMN status_weight');
-            DB::statement("UPDATE talent_requests SET status = 'CLOSED' WHERE status = 'COMPLETED'");
-            DB::statement(<<<'SQL'
-                ALTER TABLE talent_requests
-                ADD COLUMN status_weight integer GENERATED ALWAYS AS (
-                CASE
-                    WHEN (status::text = 'NEW'::text)         THEN 10
-                    WHEN (status::text = 'IN_PROGRESS'::text) THEN 20
-                    WHEN (status::text = 'CLOSED'::text)      THEN 30
-                    ELSE NULL::integer
-                END) STORED
-                SQL
-            );
-        }
     }
 };
