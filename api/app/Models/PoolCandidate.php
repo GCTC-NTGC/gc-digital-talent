3fe4c9f8bbbea901aec16b01c1319df688732207
Yoni K <yonikid15@gmail.com>
2026-06-01 13:05:38 -0400

    Fix parameter error

--- api/app/Models/PoolCandidate.php
+++ api/app/Models/PoolCandidate.php
@@ -841,7 +841,7 @@ class PoolCandidate extends Model
     }
 
     // mark the pool candidate as placed
-    public function place(string $placementType, string $departmentId, ?string $placedStartDate = null, ?string $placedEndDate = null)
+    public function place(string $placementType, string $departmentId, ?string $placedStartDate, ?string $placedEndDate)
     {
         $this->disableLogging();
 
