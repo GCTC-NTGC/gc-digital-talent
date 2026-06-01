2b6cba1a61fc6986441dfd7a7424f0607282b425
Eric Sizer <eric.sizer@gmail.com>
2026-06-01 15:16:23 -0400

    refactor selected rows hook

--- apps/web/src/hooks/useSelectedRows.ts
+++ apps/web/src/hooks/useSelectedRows.ts
@@ -1,5 +1,5 @@
 import type { Dispatch, SetStateAction } from "react";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 
 interface UseSelectedRowsReturn<T> {
   setSelectedRows: Dispatch<SetStateAction<T[]>>;
@@ -8,24 +8,19 @@ interface UseSelectedRowsReturn<T> {
 }
 
 function useSelectedRows<T>(defaultSelected?: T[]): UseSelectedRowsReturn<T> {
-  const hasSelected = useRef<boolean>(false);
+  const [hasSelected, setHasSelected] = useState<boolean>(false);
   const [selectedRows, setSelectedRows] = useState<T[]>(defaultSelected ?? []);
 
   useEffect(() => {
-    if (selectedRows.length > 0 && !hasSelected.current) {
-      hasSelected.current = true;
+    if (selectedRows.length > 0 && !hasSelected) {
+      setHasSelected(true);
     }
-  }, [selectedRows]);
-
-  let pause = !hasSelected.current;
-  if (selectedRows.length > 0) {
-    pause = false;
-  }
+  }, [selectedRows, hasSelected]);
 
   return {
     selectedRows,
     setSelectedRows,
-    hasSelected: !pause,
+    hasSelected: hasSelected || selectedRows.length > 0,
   };
 }
 
