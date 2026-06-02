432ef87f23b3b82f49259cb7817e5401f93595d2
Eric Sizer <eric.sizer@gmail.com>
2026-06-02 10:45:40 -0400

    enable both rules

--- packages/eslint-config-custom/react.mjs
+++ packages/eslint-config-custom/react.mjs
@@ -30,11 +30,6 @@ export default [
       "react-hooks/incompatible-library": "off",
       "react-hooks/preserve-manual-memoization": "off",
 
-      // TO DO: Enable in #16677
-      "react-hooks/refs": "off",
-      // TO DO: Enable in #16676
-      "react-hooks/static-components": "off",
-
       ...vitest.configs.recommended.rules,
       "vitest/expect-expect": [
         "error",
