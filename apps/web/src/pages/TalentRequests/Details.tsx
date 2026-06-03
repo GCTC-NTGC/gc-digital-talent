8fdb3b6c0ebe1aff5dc069b8a0194590d4682e2b
Eric Sizer <eric.sizer@gmail.com>
2026-06-03 13:27:31 -0400

    add reqeust details card

--- apps/web/src/pages/TalentRequests/Details.tsx
+++ apps/web/src/pages/TalentRequests/Details.tsx
@@ -1,41 +1,39 @@
 import { useIntl } from "react-intl";
 import ClipboardDocumentListIcon from "@heroicons/react/24/outline/ClipboardDocumentListIcon";
 import IdentificationIcon from "@heroicons/react/24/outline/IdentificationIcon";
+import { useQuery } from "urql";
 
-import { Card, Heading } from "@gc-digital-talent/ui";
+import { Card, Heading, Pending, ThrowNotFound } from "@gc-digital-talent/ui";
 import { ROLE_NAME } from "@gc-digital-talent/auth";
+import {
+  getFragment,
+  graphql,
+  type FragmentType,
+} from "@gc-digital-talent/graphql";
 
 import RequireAuth from "~/components/RequireAuth/RequireAuth";
+import useRequiredParams from "~/hooks/useRequiredParams";
 
-const Details = () => {
+import type { RouteParams } from "./types";
+import TalentRequestDetailsCard from "./components/TalentRequestDetailsCard";
+
+const TalentRequestDetails_Fragment = graphql(/** GraphQL */ `
+  fragment TalentRequestDetails on TalentRequest {
+    ...TalentRequestDetailsCard
+  }
+`);
+
+interface DetailsProps {
+  query: FragmentType<typeof TalentRequestDetails_Fragment>;
+}
+
+const Details = ({ query }: DetailsProps) => {
   const intl = useIntl();
+  const talentRequest = getFragment(TalentRequestDetails_Fragment, query);
 
   return (
     <div className="flex flex-col gap-y-6">
-      <Card>
-        <Heading
-          color="secondary"
-          icon={IdentificationIcon}
-          size="h4"
-          className="mt-0 font-normal"
-        >
-          {intl.formatMessage({
-            defaultMessage: "Request details",
-            id: "lW30Pe",
-            description: "Heading for details submitted with a talent request",
-          })}
-        </Heading>
-        <p>
-          {intl.formatMessage({
-            defaultMessage:
-              "The details tab provides the specific information and context about the requirements for this request.",
-            id: "M0hPlm",
-            description:
-              "Description of the details submitted with a talent request",
-          })}
-        </p>
-        <Card.Separator className="my-6" />
-      </Card>
+      <TalentRequestDetailsCard query={talentRequest} />
 
       <Card>
         <Heading
@@ -89,9 +87,35 @@ const Details = () => {
   );
 };
 
+const TalentRequestDetails_Query = graphql(/** GraphQL */ `
+  query TalentRequestDetails($id: UUID!) {
+    talentRequest(id: $id) {
+      ...TalentRequestDetails
+    }
+  }
+`);
+
+const TalentRequestDetailsPage = () => {
+  const { talentRequestId } = useRequiredParams<RouteParams>("talentRequestId");
+  const [{ data, fetching, error }] = useQuery({
+    query: TalentRequestDetails_Query,
+    variables: { id: talentRequestId },
+  });
+
+  return (
+    <Pending fetching={fetching} error={error}>
+      {data?.talentRequest ? (
+        <Details query={data.talentRequest} />
+      ) : (
+        <ThrowNotFound />
+      )}
+    </Pending>
+  );
+};
+
 export const Component = () => (
   <RequireAuth roles={[ROLE_NAME.CommunityRecruiter, ROLE_NAME.CommunityAdmin]}>
-    <Details />
+    <TalentRequestDetailsPage />
   </RequireAuth>
 );
 
