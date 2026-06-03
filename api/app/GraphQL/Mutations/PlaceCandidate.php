439bb437ff1271b98ddc8c98980f6e8982f829af
Yoni K <yonikid15@gmail.com>
2026-06-03 08:42:17 -0400

    Fix nullable field error

--- api/app/GraphQL/Mutations/PlaceCandidate.php
+++ api/app/GraphQL/Mutations/PlaceCandidate.php
@@ -14,8 +14,8 @@ final class PlaceCandidate
         $candidate = PoolCandidate::findOrFail($args['id']);
         $placementType = $args['placementType'];
         $departmentId = $args['department']['connect']; // validator asserts this value is valid
-        $placedStartDate = $args['placedStartDate'];
-        $placedEndDate = $args['placedEndDate'];
+        $placedStartDate = $args['placedStartDate'] ?? null;
+        $placedEndDate = $args['placedEndDate'] ?? null;
 
         $candidate->place($placementType, $departmentId, $placedStartDate, $placedEndDate);
 
