0e493bb4253888cd7aaa5677998faf99ddc0ab34
Matt <mnigh@users.noreply.github.com>
2026-06-03 10:14:16 -0400

    Replace ProcessPreviw with ProcessPreview (#16967)

--- apps/web/src/components/ProcessPreviewLink/ProcessPreviewLink.tsx
+++ apps/web/src/components/ProcessPreviewLink/ProcessPreviewLink.tsx
@@ -7,12 +7,16 @@ import { Link } from "@gc-digital-talent/ui";
 import useRoutes from "~/hooks/useRoutes";
 import type { PoolCompleteness } from "~/types/pool";
 
-interface ProcessPreviwLinkProps extends Omit<LinkProps, "href"> {
+interface ProcessPreviewLinkProps extends Omit<LinkProps, "href"> {
   id: Scalars["UUID"]["output"];
   status: PoolCompleteness;
 }
 
-const ProcessPreviwLink = ({ id, status, ...rest }: ProcessPreviwLinkProps) => {
+const ProcessPreviewLink = ({
+  id,
+  status,
+  ...rest
+}: ProcessPreviewLinkProps) => {
   const intl = useIntl();
   const paths = useRoutes();
 
@@ -41,4 +45,4 @@ const ProcessPreviwLink = ({ id, status, ...rest }: ProcessPreviwLinkProps) => {
   );
 };
 
-export default ProcessPreviwLink;
+export default ProcessPreviewLink;
