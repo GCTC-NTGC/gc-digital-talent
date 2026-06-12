faa8fb611561e5b4af958c76ef7788e0233e7b91
Eric Sizer <eric.sizer@gmail.com>
2026-06-12 16:02:40 -0400

    fix tests

--- api/tests/TestCase.php
+++ api/tests/TestCase.php
@@ -7,4 +7,9 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected function makeFaker($locale = null)
+    {
+        return $this->app->make(\Faker\Generator::class);
+    }
 }
