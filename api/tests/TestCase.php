10c29758ca732bb5364ccfa80dfe4bb70ac1daaa
Eric Sizer <eric.sizer@gmail.com>
2026-06-15 10:46:43 -0400

    another attempt to fix flaky tests

--- api/tests/TestCase.php
+++ api/tests/TestCase.php
@@ -8,8 +8,21 @@ abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
 
+    protected function tearDown(): void
+    {
+        // Faker's destructor re-randomises mt_rand, and cycle GC means it can fire
+        // after the next test's seed() call. Force it to happen now instead.
+        $this->faker = null;
+        parent::tearDown();
+        gc_collect_cycles();
+    }
+
     protected function makeFaker($locale = null)
     {
-        return $this->app->make(\Faker\Generator::class);
+        $faker = \Faker\Factory::create();
+        $faker->addProvider(new \App\Faker\ExtendedFormatsProvider($faker));
+        $this->app->instance(\Faker\Generator::class, $faker);
+
+        return $faker;
     }
 }
