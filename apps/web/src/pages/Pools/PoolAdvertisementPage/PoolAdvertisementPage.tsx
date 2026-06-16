eb8cb86fe89c55aca21424e9f4e989599b16e1a8
Vachan <vachanbolt@hotmail.com>
2026-06-16 10:38:26 -0600

    use attribute

--- apps/web/src/pages/Pools/PoolAdvertisementPage/PoolAdvertisementPage.tsx
+++ apps/web/src/pages/Pools/PoolAdvertisementPage/PoolAdvertisementPage.tsx
@@ -122,16 +122,16 @@ const gocGCKeyLink = (locale: Locales, chunks: ReactNode) => (
 
 const DeadlineDialogReturn = ({
   closingDate,
-  closingReason,
+  wasClosedEarly,
 }: {
   closingDate: string | null | undefined;
-  closingReason: string | null | undefined;
+  wasClosedEarly: boolean | null | undefined;
 }): ReactNode | null => {
-  if (closingDate && !closingReason) {
+  if (closingDate && !wasClosedEarly) {
     return <DeadlineDialog deadline={parseDateTimeUtc(closingDate)} />;
   }
 
-  if (closingReason) {
+  if (wasClosedEarly) {
     return <ClosedEarlyDeadlineDialog />;
   }
 
@@ -153,7 +153,7 @@ export const PoolAdvertisement_Fragment = graphql(/* GraphQL */ `
       }
     }
     closingDate
-    closingReason
+    wasClosedEarly
     status {
       value
       label {
@@ -699,13 +699,13 @@ export const PoolPoster = ({
                   value={
                     <DeadlineValue
                       closingDate={pool.closingDate}
-                      closingReason={pool.closingReason}
+                      wasClosedEarly={pool.wasClosedEarly}
                     />
                   }
                   suffix={
                     <DeadlineDialogReturn
                       closingDate={pool.closingDate}
-                      closingReason={pool.closingReason}
+                      wasClosedEarly={pool.wasClosedEarly}
                     />
                   }
                 />
