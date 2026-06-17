e530943e832a5318658094d1a50a98cb78c7f403
Eric Sizer <eric.sizer@gmail.com>
2026-06-17 10:05:29 -0400

    format code

--- packages/helpers/src/utils/util.ts
+++ packages/helpers/src/utils/util.ts
@@ -156,7 +156,7 @@ export function pickMap<T, K extends keyof T>(
   keys: K | K[],
 ): Pick<T, K>[] | undefined {
   const keyArr = (Array.isArray(keys) ? keys : [keys]) as K[];
-  return unpackMaybes(list).map((item) =>
-    Object.fromEntries(keyArr.map((k) => [k, item[k]])) as Pick<T, K>,
+  return unpackMaybes(list).map(
+    (item) => Object.fromEntries(keyArr.map((k) => [k, item[k]])) as Pick<T, K>,
   );
 }
