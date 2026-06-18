5aa9a825c289f3fe8c1dfb54e57220fd5fcc643e
Eric Sizer <eric.sizer@gmail.com>
2026-06-18 09:57:28 -0400

    add new heading components

---
+++ packages/ui/src/components/Heading/SectionHeading.tsx
@@ -0,0 +1,39 @@
+import type { HTMLAttributes } from "react";
+import { forwardRef } from "react";
+import { tv, type VariantProps } from "tailwind-variants";
+
+import type { IconType } from "../../types";
+import type { HeadingRef } from "./types";
+import headingBase from "./variants";
+
+const sectionHeading = tv({
+  extend: headingBase,
+  slots: {
+    base: "flex items-center gap-x-2.5 text-4xl",
+    icon: "size-9 stroke-[1.6]",
+  },
+});
+
+type SectionHeadingVariants = VariantProps<typeof sectionHeading>;
+
+export interface SectionHeadingProps
+  extends SectionHeadingVariants,
+    Omit<HTMLAttributes<HTMLHeadingElement>, "color"> {
+  icon: IconType;
+}
+
+const SectionHeading = forwardRef<HeadingRef, SectionHeadingProps>(
+  ({ icon, color, center, children, className, ...rest }, ref) => {
+    const Icon = icon;
+    const { base, icon: iconStyles } = sectionHeading({ color, center });
+
+    return (
+      <h2 ref={ref} className={base({ class: className })} {...rest}>
+        <Icon className={iconStyles()} />
+        {children}
+      </h2>
+    );
+  },
+);
+
+export default SectionHeading;
