8315f5de25d445370f64b6dd02f841a8047890ff
Yoni K <yonikid15@gmail.com>
2026-06-24 14:08:02 -0400

    Remove ai translations

--- api/lang/fr/employee_verification.php
+++ api/lang/fr/employee_verification.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'verified' => 'Employé(e) gouvernemental(e) vérifié(e)',
-    'not_verified' => 'Employé(e) gouvernemental(e) non vérifié(e)',
+    'verified' => 'Verified government employee',
+    'not_verified' => 'Not-verified government employee',
 ];
