36290abca69bafd2221fa58b6aa8bf7f9b28d36b
Peter Giles <8978655+petertgiles@users.noreply.github.com>
2026-06-24 13:02:06 -0400

    start of dialog

--- apps/web/src/pages/TalentNominations/NominationGroup/History.tsx
+++ apps/web/src/pages/TalentNominations/NominationGroup/History.tsx
@@ -1,39 +1,96 @@
 import FolderIcon from "@heroicons/react/24/outline/FolderIcon";
 import { useIntl } from "react-intl";
+import { useQuery } from "urql";
 
-import { Card, Heading, Button } from "@gc-digital-talent/ui";
+import { Card, Heading, Pending, ThrowNotFound } from "@gc-digital-talent/ui";
+import {
+  getFragment,
+  graphql,
+  type FragmentType,
+} from "@gc-digital-talent/graphql";
 
 import RequireAuth from "~/components/RequireAuth/RequireAuth";
 import permissionConstants from "~/constants/permissionConstants";
+import useRequiredParams from "~/hooks/useRequiredParams";
 
-import { detailTabMessages } from "./messages";
+import NominationDetailsDialog from "./components/NominationDetailsDialog";
 
-const TalentNominationGroupHistoryPage = () => {
+const TalentNominationGroupHistory_Fragment = graphql(/* GraphQL */ `
+  fragment TalentNominationGroupHistory on TalentNominationGroup {
+    id
+    nominations {
+      id
+      ...TalentNominationDetailsDialog
+    }
+  }
+`);
+
+interface TalentNominationGroupHistoryProps {
+  query: FragmentType<typeof TalentNominationGroupHistory_Fragment>;
+}
+
+const TalentNominationGroupHistory = ({
+  query,
+}: TalentNominationGroupHistoryProps) => {
   const intl = useIntl();
-
+  const talentNominationGroup = getFragment(
+    TalentNominationGroupHistory_Fragment,
+    query,
+  );
   return (
     <>
       <Card space="lg">
-        <div className="flex flex-col items-center justify-between gap-y-6 sm:flex-row sm:gap-x-3 sm:gap-y-0">
-          <Heading
-            icon={FolderIcon}
-            level="h2"
-            size="h4"
-            color="secondary"
-            className="mt-0 font-normal"
-          >
-            {intl.formatMessage({
-              defaultMessage: "Nomination history",
-              id: "85Y9eS",
-              description: "Heading for the nomination history page",
-            })}
-          </Heading>
-          <Button type="button" mode="inline" color="primary">
-            {intl.formatMessage(detailTabMessages.collapseNominations)}
-          </Button>
-        </div>
+        <Heading
+          icon={FolderIcon}
+          level="h2"
+          size="h4"
+          color="secondary"
+          className="mt-0 font-normal"
+        >
+          {intl.formatMessage({
+            defaultMessage: "Nomination history",
+            id: "85Y9eS",
+            description: "Heading for the nomination history page",
+          })}
+        </Heading>
+
+        {talentNominationGroup.nominations?.map((nomination) => (
+          <NominationDetailsDialog query={nomination} key={nomination.id} />
+        ))}
       </Card>
     </>
+  );
+};
+
+const TalentNominationHistoryPage_Query = graphql(/* GraphQL */ `
+  query TalentNominationHistoryPage($talentNominationGroupId: UUID!) {
+    talentNominationGroup(id: $talentNominationGroupId) {
+      ...TalentNominationGroupHistory
+    }
+  }
+`);
+
+interface RouteParams extends Record<string, string> {
+  talentNominationGroupId: string;
+}
+
+const TalentNominationGroupHistoryPage = () => {
+  const { talentNominationGroupId } = useRequiredParams<RouteParams>(
+    "talentNominationGroupId",
+  );
+  const [{ data, fetching, error }] = useQuery({
+    query: TalentNominationHistoryPage_Query,
+    variables: { talentNominationGroupId },
+  });
+
+  return (
+    <Pending fetching={fetching} error={error}>
+      {data?.talentNominationGroup ? (
+        <TalentNominationGroupHistory query={data.talentNominationGroup} />
+      ) : (
+        <ThrowNotFound />
+      )}
+    </Pending>
   );
 };
 
---
+++ apps/web/src/pages/TalentNominations/NominationGroup/components/NominationDetailsDialog.tsx
@@ -0,0 +1,120 @@
+import { useState } from "react";
+import { useIntl } from "react-intl";
+
+import {
+  getFragment,
+  graphql,
+  type FragmentType,
+} from "@gc-digital-talent/graphql";
+import { Button, Dialog } from "@gc-digital-talent/ui";
+import { commonMessages } from "@gc-digital-talent/i18n";
+import { notEmpty } from "@gc-digital-talent/helpers";
+
+import talentNominationMessages from "~/messages/talentNominationMessages";
+
+const TalentNominationDetailsDialog_Fragment = graphql(/* GraphQL */ `
+  fragment TalentNominationDetailsDialog on TalentNomination {
+    id
+    talentNominationEvent {
+      name {
+        localized
+      }
+    }
+    nominee {
+      firstName
+    }
+    nominator {
+      firstName
+      lastName
+    }
+    nominatorFallbackName
+    nominateForAdvancement
+    nominateForLateralMovement
+    nominateForDevelopmentPrograms
+  }
+`);
+
+interface NominationDetailsDialogProps {
+  query: FragmentType<typeof TalentNominationDetailsDialog_Fragment>;
+}
+
+const NominationDetailsDialog = ({ query }: NominationDetailsDialogProps) => {
+  const intl = useIntl();
+  const nomination = getFragment(TalentNominationDetailsDialog_Fragment, query);
+
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const nomineeName =
+    nomination.nominee?.firstName ??
+    intl.formatMessage(commonMessages.notFound);
+
+  const nominatorName =
+    (nomination.nominator
+      ? `${nomination.nominator.firstName} ${nomination.nominator.lastName}`.trim()
+      : nomination.nominatorFallbackName) ??
+    intl.formatMessage(commonMessages.notFound);
+
+  const eventName = nomination.talentNominationEvent.name.localized;
+
+  const nominationOptionMessages = [
+    nomination.nominateForAdvancement
+      ? talentNominationMessages.nominateForAdvancement
+      : null,
+    nomination.nominateForLateralMovement
+      ? talentNominationMessages.nominateForLateralMovement
+      : null,
+    nomination.nominateForDevelopmentPrograms
+      ? talentNominationMessages.development
+      : null,
+  ];
+
+  const nominationOptions =
+    nominationOptionMessages
+      .filter(notEmpty)
+      .map((message) => intl.formatMessage(message).toLocaleLowerCase())
+      .join(", ") ?? intl.formatMessage(commonMessages.notFound);
+
+  return (
+    <Dialog.Root open={isOpen} onOpenChange={setIsOpen}>
+      <Dialog.Trigger asChild>
+        <Button>{nomination.id}</Button>
+      </Dialog.Trigger>
+      <Dialog.Content>
+        <Dialog.Header
+          subtitle={intl.formatMessage(
+            {
+              defaultMessage:
+                "Nominated for {nominationOptions} by {nominatorName}",
+              id: "Vq0BPN",
+              description: "Subtitle for the nomination details dialog",
+            },
+            {
+              nominationOptions,
+              nominatorName,
+            },
+          )}
+        >
+          {intl.formatMessage(
+            {
+              defaultMessage: "{nomineeName}’s nomination to {eventName}",
+              id: "1l/JbH",
+              description: "Title for the nomination details dialog",
+            },
+            {
+              nomineeName,
+              eventName,
+            },
+          )}
+        </Dialog.Header>
+        <Dialog.Body>
+          Body Here
+          <Dialog.Footer className="flex-col gap-x-6 gap-y-6 xs:flex-row">
+            Footer here
+          </Dialog.Footer>
+        </Dialog.Body>
+      </Dialog.Content>
+    </Dialog.Root>
+  );
+};
+
+export default NominationDetailsDialog;
