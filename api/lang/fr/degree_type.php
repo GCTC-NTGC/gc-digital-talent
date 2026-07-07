0f5b8bcbffbc96a143f4fd17e0e3e35bcfba98ec
JamesHuf <jamiehufnagel@hotmail.com>
2026-07-07 13:20:10 -0700

    Add translations

---
+++ api/lang/fr/degree_type.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'high_school' => 'ôme d\'études secondaires, GED, CCEA ou l\'équivalent',
+    'college_diploma' => 'Diplôme d\'études collégiales',
+    'bachelors_degree' => 'Baccalauréat',
+    'masters_degree' => 'Maîtrise',
+    'phd' => 'Ph. D.',
+];
