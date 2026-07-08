f2d152575b51e2c90ca1f00069f025729021e5d7
Peter Giles <8978655+petertgiles@users.noreply.github.com>
2026-07-08 16:30:07 -0400

    model, schema

--- api/app/Models/TalentNominationGroup.php
+++ api/app/Models/TalentNominationGroup.php
@@ -11,6 +11,7 @@ use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Auth;
@@ -94,6 +95,12 @@ class TalentNominationGroup extends Model
     public function nominations(): HasMany
     {
         return $this->hasMany(TalentNomination::class, 'talent_nomination_group_id');
+    }
+
+    /** @return BelongsToMany<Classification, $this> */
+    public function advancementClassifications(): BelongsToMany
+    {
+        return $this->belongsToMany(Classification::class, 'classification_talent_nomination_group_advancement');
     }
 
     /**
