ac82db7ee2491c81aa145486363218de2e0f2712
Eric Sizer <eric.sizer@gmail.com>
2026-07-08 08:33:28 -0400

    add test to confirm no emails sent to null emails

---
+++ api/tests/Feature/Notifications/SendNotificationsAdHocEmailTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Tests\Feature\Notifications;
+
+use App\Models\User;
+use App\Notifications\AdHocEmail;
+use Database\Seeders\RolePermissionSeeder;
+use Illuminate\Console\Command;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Notification;
+use Tests\TestCase;
+
+class SendNotificationsAdHocEmailTest extends TestCase
+{
+    use RefreshDatabase;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        Notification::fake();
+        $this->seed(RolePermissionSeeder::class);
+    }
+
+    // will not send a notification to a user with no email address when notifying all users
+    public function testDoesNotNotifyUserWithNullEmail(): void
+    {
+        $userWithEmail = User::factory()->create(['email' => 'has-email@example.org']);
+        $userWithoutEmail = User::factory()->create(['email' => null]);
+
+        $this->artisan('send-notifications:ad-hoc-email', [
+            'templateIdEn' => 'template-en',
+            'templateIdFr' => 'template-fr',
+            '--notifyAllUsers' => true,
+        ])
+            ->expectsConfirmation('Do you wish to send notifications to 1 users?', 'yes')
+            ->assertExitCode(Command::SUCCESS)
+            ->run();
+
+        Notification::assertSentTo([$userWithEmail], AdHocEmail::class);
+        Notification::assertNotSentTo([$userWithoutEmail], AdHocEmail::class);
+    }
+}
