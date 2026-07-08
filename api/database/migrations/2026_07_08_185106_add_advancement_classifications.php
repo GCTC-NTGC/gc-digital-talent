6623b1b0e5ac80b8342b16ca91f2cf65cb04dbbd
Peter Giles <8978655+petertgiles@users.noreply.github.com>
2026-07-08 15:25:54 -0400

    migration

---
+++ api/database/migrations/2026_07_08_185106_add_advancement_classifications.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Query\Expression;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class() extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('classification_talent_nomination_group_advancement', function (Blueprint $table) {
+            $table->uuid('id')
+                ->primary()
+                ->default(new Expression('public.gen_random_uuid()'));
+            $table->timestamps();
+            $table->foreignUuid('classification_id')
+                ->constrained();
+            $table->foreignUuid('talent_nomination_group_id')
+                ->constrained()
+                ->onDelete('cascade');
+            $table->unique(['classification_id', 'talent_nomination_group_id'], 'classification_nomination_group_advancement_unique');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('classification_talent_nomination_group_advancement');
+    }
+};
