a6b3532158762dc0e29513fc0f57fde323bc60d0
Eric Sizer <eric.sizer@gmail.com>
2026-07-08 13:00:16 -0400

    propogate fix to family method

--- api/app/Console/Commands/SendNotificationsAdHocEmail.php
+++ api/app/Console/Commands/SendNotificationsAdHocEmail.php
@@ -138,10 +138,13 @@ class SendNotificationsAdHocEmail extends Command implements PromptsForMissingIn
             }
         }
 
-        $builder = User::whereJsonContains('enabled_email_notifications', $notificationFamilies[0]);
-        for ($i = 1; $i < count($notificationFamilies); $i++) {
-            $builder->orWhereJsonContains('enabled_email_notifications', $notificationFamilies[$i]);
-        }
+        $builder = User::whereNotNull('email')
+            ->where(function (Builder $query) use ($notificationFamilies) {
+                $query->whereJsonContains('enabled_email_notifications', $notificationFamilies[0]);
+                for ($i = 1; $i < count($notificationFamilies); $i++) {
+                    $query->orWhereJsonContains('enabled_email_notifications', $notificationFamilies[$i]);
+                }
+            });
 
         return $builder;
     }
--- api/tests/Feature/Notifications/SendNotificationsAdHocEmailTest.php
+++ api/tests/Feature/Notifications/SendNotificationsAdHocEmailTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\Notifications;
 
+use App\Enums\NotificationFamily;
 use App\Models\User;
 use App\Notifications\AdHocEmail;
 use Database\Seeders\RolePermissionSeeder;
@@ -32,6 +33,31 @@ class SendNotificationsAdHocEmailTest extends TestCase
             'templateIdEn' => 'template-en',
             'templateIdFr' => 'template-fr',
             '--notifyAllUsers' => true,
+        ])
+            ->expectsConfirmation('Do you wish to send notifications to 1 users?', 'yes')
+            ->assertExitCode(Command::SUCCESS)
+            ->run();
+
+        Notification::assertSentTo([$userWithEmail], AdHocEmail::class);
+        Notification::assertNotSentTo([$userWithoutEmail], AdHocEmail::class);
+    }
+
+    // will not send a notification to a user with no email address when filtering by notification family
+    public function testDoesNotNotifyUserWithNullEmailByNotificationFamily(): void
+    {
+        $userWithEmail = User::factory()->create([
+            'email' => 'has-email@example.org',
+            'enabled_email_notifications' => [NotificationFamily::JOB_ALERT->name],
+        ]);
+        $userWithoutEmail = User::factory()->create([
+            'email' => null,
+            'enabled_email_notifications' => [NotificationFamily::JOB_ALERT->name],
+        ]);
+
+        $this->artisan('send-notifications:ad-hoc-email', [
+            'templateIdEn' => 'template-en',
+            'templateIdFr' => 'template-fr',
+            '--notificationFamily' => [NotificationFamily::JOB_ALERT->name],
         ])
             ->expectsConfirmation('Do you wish to send notifications to 1 users?', 'yes')
             ->assertExitCode(Command::SUCCESS)
