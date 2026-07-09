bf858d31eaa2bc191038beea3784a0780cb85e89
JamesHuf <jamiehufnagel@hotmail.com>
2026-07-09 15:27:35 -0700

    Add spinner while fetching

--- apps/web/src/components/ExperienceFormFields/EducationFields/EducationFields.tsx
+++ apps/web/src/components/ExperienceFormFields/EducationFields/EducationFields.tsx
@@ -12,7 +12,7 @@ import {
   EducationType,
   FellowshipType,
 } from "@gc-digital-talent/graphql";
-import { Notice } from "@gc-digital-talent/ui";
+import { Loading, Notice } from "@gc-digital-talent/ui";
 
 import type {
   SubExperienceFormProps,
@@ -40,7 +40,7 @@ const EducationFields = ({
   const intl = useIntl();
   const locale = getLocale(intl);
   const experienceLabels = getExperienceFormLabels(intl);
-  const [{ data }] = useQuery({ query: EducationOptions_Query });
+  const [{ data, fetching }] = useQuery({ query: EducationOptions_Query });
 
   const { resetField, formState } = useFormContext<EducationFormValues>();
 
@@ -143,210 +143,218 @@ const EducationFields = ({
   }, [showThesisTitleField, resetDirtyField]);
 
   return (
-    <div className="grid gap-6">
-      <RadioGroup
-        idPrefix="educationType"
-        name="educationType"
-        legend={labels.educationType}
-        items={getEducationTypeOptions(data?.educationTypes, intl)}
-        rules={{
-          required: intl.formatMessage(errorMessages.required),
-        }}
-      />
-      {!watchEducationType && (
-        <Notice.Root>
-          <Notice.Content>
-            <p className="text-center">
-              {intl.formatMessage({
-                defaultMessage:
-                  "Please select a type of education or certificate to continue.",
-                id: "gBPQ/R",
-                description:
-                  "Text displayed on the education experience form when a user has not selected an education type.",
-              })}
-            </p>
-          </Notice.Content>
-        </Notice.Root>
-      )}
-      {watchEducationType === EducationType.DegreeDiplomaCertificate && (
-        <RadioGroup
-          idPrefix="degreeType"
-          name="degreeType"
-          legend={labels.degreeType}
-          items={getDegreeTypeOptions(data?.degreeTypes, intl)}
-          rules={{
-            required: intl.formatMessage(errorMessages.required),
-          }}
-        />
-      )}
-      {watchEducationType === EducationType.DegreeDiplomaCertificate &&
-        !watchDegreeType && (
-          <Notice.Root>
-            <Notice.Content>
-              <p className="text-center">
+    <div>
+      {fetching ? (
+        <Loading inline />
+      ) : (
+        <div className="grid gap-6">
+          <RadioGroup
+            idPrefix="educationType"
+            name="educationType"
+            legend={labels.educationType}
+            items={getEducationTypeOptions(data?.educationTypes, intl)}
+            rules={{
+              required: intl.formatMessage(errorMessages.required),
+            }}
+          />
+          {!watchEducationType && (
+            <Notice.Root>
+              <Notice.Content>
+                <p className="text-center">
+                  {intl.formatMessage({
+                    defaultMessage:
+                      "Please select a type of education or certificate to continue.",
+                    id: "gBPQ/R",
+                    description:
+                      "Text displayed on the education experience form when a user has not selected an education type.",
+                  })}
+                </p>
+              </Notice.Content>
+            </Notice.Root>
+          )}
+          {watchEducationType === EducationType.DegreeDiplomaCertificate && (
+            <RadioGroup
+              idPrefix="degreeType"
+              name="degreeType"
+              legend={labels.degreeType}
+              items={getDegreeTypeOptions(data?.degreeTypes, intl)}
+              rules={{
+                required: intl.formatMessage(errorMessages.required),
+              }}
+            />
+          )}
+          {watchEducationType === EducationType.DegreeDiplomaCertificate &&
+            !watchDegreeType && (
+              <Notice.Root>
+                <Notice.Content>
+                  <p className="text-center">
+                    {intl.formatMessage({
+                      defaultMessage:
+                        "Please select a type of degree or diploma to continue.",
+                      id: "qpcvEM",
+                      description:
+                        "Text displayed on the education experience form when a user has not selected the type of their degree/diploma/certificate.",
+                    })}
+                  </p>
+                </Notice.Content>
+              </Notice.Root>
+            )}
+          {watchEducationType === EducationType.Other && (
+            <Input
+              id="otherEducationType"
+              label={labels.otherEducationType}
+              name="otherEducationType"
+              type="text"
+              rules={{ required: intl.formatMessage(errorMessages.required) }}
+            />
+          )}
+          {showInstitutionField && (
+            <div>
+              <Input
+                id="institution"
+                label={labels.institution}
+                name="institution"
+                type="text"
+                rules={{ required: intl.formatMessage(errorMessages.required) }}
+                list={
+                  organizationSuggestions.length
+                    ? "organizationSuggestions"
+                    : undefined
+                }
+              />
+              {organizationSuggestions.length > 0 && (
+                <datalist id="organizationSuggestions">
+                  {organizationSuggestions.map((suggestion) => {
+                    return (
+                      <option key={suggestion} value={suggestion}></option>
+                    );
+                  })}
+                </datalist>
+              )}
+            </div>
+          )}
+          {watchEducationType === EducationType.Fellowship && (
+            <RadioGroup
+              idPrefix="fellowshipType"
+              name="fellowshipType"
+              legend={labels.fellowshipType}
+              items={getFellowshipTypeOptions(data?.fellowshipTypes, intl)}
+              rules={{
+                required: intl.formatMessage(errorMessages.required),
+              }}
+            />
+          )}
+          {showOtherFellowshipTypeField && (
+            <Input
+              id="otherFellowshipType"
+              label={labels.otherFellowshipType}
+              name="otherFellowshipType"
+              type="text"
+              rules={{ required: intl.formatMessage(errorMessages.required) }}
+            />
+          )}
+          {showAreaOfStudyField && (
+            <Input
+              id="areaOfStudy"
+              label={labels.areaOfStudy}
+              name="areaOfStudy"
+              type="text"
+              rules={
+                watchEducationType !== EducationType.Fellowship &&
+                watchEducationType !== EducationType.Other
+                  ? {
+                      required: intl.formatMessage(errorMessages.required),
+                    }
+                  : {}
+              }
+            />
+          )}
+          {showThesisTitleField && (
+            <Input
+              id="thesisTitle"
+              label={labels.thesisTitle}
+              name="thesisTitle"
+              type="text"
+            />
+          )}
+          {watchEducationType === EducationType.LicenseAccreditation && (
+            <Input
+              id="licenseOrAccreditation"
+              label={labels.licenseOrAccreditation}
+              name="licenseOrAccreditation"
+              type="text"
+              rules={{ required: intl.formatMessage(errorMessages.required) }}
+            />
+          )}
+          {watchEducationType === EducationType.ProfessionalCertification && (
+            <Input
+              id="certification"
+              label={labels.certification}
+              name="certification"
+              type="text"
+              rules={{ required: intl.formatMessage(errorMessages.required) }}
+            />
+          )}
+          {watchEducationType === EducationType.IndividualCourse && (
+            <Input
+              id="courseName"
+              label={labels.courseName}
+              name="courseName"
+              type="text"
+              rules={{ required: intl.formatMessage(errorMessages.required) }}
+            />
+          )}
+          {showStatusField && (
+            <RadioGroup
+              idPrefix="educationStatus"
+              name="educationStatus"
+              legend={labels.educationStatus}
+              items={getEducationStatusOptions(
+                data?.educationStatuses,
+                intl,
+                licenseOrCertification,
+              )}
+              rules={{
+                required: intl.formatMessage(errorMessages.required),
+              }}
+            />
+          )}
+          {showStatusField && !watchEducationStatus && (
+            <Notice.Root>
+              <Notice.Content>
+                <p className="text-center">
+                  {intl.formatMessage({
+                    defaultMessage: "Select a completion status to continue.",
+                    id: "/nD36n",
+                    description:
+                      "Text displayed on the education experience form when a user has not selected an education status.",
+                  })}
+                </p>
+              </Notice.Content>
+            </Notice.Root>
+          )}
+          {showStatusField && !!watchEducationStatus && (
+            <>
+              <DateFields labels={labels} />
+              <p>
                 {intl.formatMessage({
                   defaultMessage:
-                    "Please select a type of degree or diploma to continue.",
-                  id: "qpcvEM",
+                    "If there are any other relevant details about this experience you'd like to share, you can do so here.",
+                  id: "ioYWI6",
                   description:
-                    "Text displayed on the education experience form when a user has not selected the type of their degree/diploma/certificate.",
+                    "Help text for the experience additional details field",
                 })}
               </p>
-            </Notice.Content>
-          </Notice.Root>
-        )}
-      {watchEducationType === EducationType.Other && (
-        <Input
-          id="otherEducationType"
-          label={labels.otherEducationType}
-          name="otherEducationType"
-          type="text"
-          rules={{ required: intl.formatMessage(errorMessages.required) }}
-        />
-      )}
-      {showInstitutionField && (
-        <div>
-          <Input
-            id="institution"
-            label={labels.institution}
-            name="institution"
-            type="text"
-            rules={{ required: intl.formatMessage(errorMessages.required) }}
-            list={
-              organizationSuggestions.length
-                ? "organizationSuggestions"
-                : undefined
-            }
-          />
-          {organizationSuggestions.length > 0 && (
-            <datalist id="organizationSuggestions">
-              {organizationSuggestions.map((suggestion) => {
-                return <option key={suggestion} value={suggestion}></option>;
-              })}
-            </datalist>
+              <TextArea
+                id={"details"}
+                name={"details"}
+                rows={TEXT_AREA_ROWS}
+                wordLimit={wordCountLimits[locale]}
+                label={experienceLabels.details}
+              />
+            </>
           )}
         </div>
-      )}
-      {watchEducationType === EducationType.Fellowship && (
-        <RadioGroup
-          idPrefix="fellowshipType"
-          name="fellowshipType"
-          legend={labels.fellowshipType}
-          items={getFellowshipTypeOptions(data?.fellowshipTypes, intl)}
-          rules={{
-            required: intl.formatMessage(errorMessages.required),
-          }}
-        />
-      )}
-      {showOtherFellowshipTypeField && (
-        <Input
-          id="otherFellowshipType"
-          label={labels.otherFellowshipType}
-          name="otherFellowshipType"
-          type="text"
-          rules={{ required: intl.formatMessage(errorMessages.required) }}
-        />
-      )}
-      {showAreaOfStudyField && (
-        <Input
-          id="areaOfStudy"
-          label={labels.areaOfStudy}
-          name="areaOfStudy"
-          type="text"
-          rules={
-            watchEducationType !== EducationType.Fellowship &&
-            watchEducationType !== EducationType.Other
-              ? {
-                  required: intl.formatMessage(errorMessages.required),
-                }
-              : {}
-          }
-        />
-      )}
-      {showThesisTitleField && (
-        <Input
-          id="thesisTitle"
-          label={labels.thesisTitle}
-          name="thesisTitle"
-          type="text"
-        />
-      )}
-      {watchEducationType === EducationType.LicenseAccreditation && (
-        <Input
-          id="licenseOrAccreditation"
-          label={labels.licenseOrAccreditation}
-          name="licenseOrAccreditation"
-          type="text"
-          rules={{ required: intl.formatMessage(errorMessages.required) }}
-        />
-      )}
-      {watchEducationType === EducationType.ProfessionalCertification && (
-        <Input
-          id="certification"
-          label={labels.certification}
-          name="certification"
-          type="text"
-          rules={{ required: intl.formatMessage(errorMessages.required) }}
-        />
-      )}
-      {watchEducationType === EducationType.IndividualCourse && (
-        <Input
-          id="courseName"
-          label={labels.courseName}
-          name="courseName"
-          type="text"
-          rules={{ required: intl.formatMessage(errorMessages.required) }}
-        />
-      )}
-      {showStatusField && (
-        <RadioGroup
-          idPrefix="educationStatus"
-          name="educationStatus"
-          legend={labels.educationStatus}
-          items={getEducationStatusOptions(
-            data?.educationStatuses,
-            intl,
-            licenseOrCertification,
-          )}
-          rules={{
-            required: intl.formatMessage(errorMessages.required),
-          }}
-        />
-      )}
-      {showStatusField && !watchEducationStatus && (
-        <Notice.Root>
-          <Notice.Content>
-            <p className="text-center">
-              {intl.formatMessage({
-                defaultMessage: "Select a completion status to continue.",
-                id: "/nD36n",
-                description:
-                  "Text displayed on the education experience form when a user has not selected an education status.",
-              })}
-            </p>
-          </Notice.Content>
-        </Notice.Root>
-      )}
-      {showStatusField && !!watchEducationStatus && (
-        <>
-          <DateFields labels={labels} />
-          <p>
-            {intl.formatMessage({
-              defaultMessage:
-                "If there are any other relevant details about this experience you'd like to share, you can do so here.",
-              id: "ioYWI6",
-              description:
-                "Help text for the experience additional details field",
-            })}
-          </p>
-          <TextArea
-            id={"details"}
-            name={"details"}
-            rows={TEXT_AREA_ROWS}
-            wordLimit={wordCountLimits[locale]}
-            label={experienceLabels.details}
-          />
-        </>
       )}
     </div>
   );
