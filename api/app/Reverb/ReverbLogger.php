fce55e3b54199e4fdf04be3664dcacc7abc5002e
Eric Sizer <eric.sizer@gmail.com>
2026-07-14 13:35:58 -0400

    update reverb logger to be local

--- api/app/Reverb/ReverbLogger.php
+++ api/app/Reverb/ReverbLogger.php
@@ -2,14 +2,28 @@
 
 namespace App\Reverb;
 
+use Illuminate\Log\Logger as IlluminateLogger;
 use Illuminate\Support\Facades\Log;
 use Laravel\Reverb\Contracts\Logger;
 
 class ReverbLogger implements Logger
 {
+    protected IlluminateLogger $logger;
+
+    public function __construct()
+    {
+        // Reverb runs as a separate process from the web server, so it can't
+        // use the default (Azure) logging channel. On-demand channel writes
+        // straight to a local file instead. https://laravel.com/docs/13.x/logging#on-demand-channels
+        $this->logger = Log::build([
+            'driver' => 'single',
+            'path' => storage_path('logs/reverb.log'),
+        ]);
+    }
+
     public function error(string $message): void
     {
-        Log::error($message);
+        $this->logger->error($message);
     }
 
     public function info(string $title, ?string $message = null): void {}
