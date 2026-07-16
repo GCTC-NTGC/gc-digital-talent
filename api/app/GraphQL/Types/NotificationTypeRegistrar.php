70fc3a9d6056f902aec8b3314c26b28e17165ea2
Peter Giles <8978655+petertgiles@users.noreply.github.com>
2026-07-16 13:11:47 -0400

    clean up

--- api/app/GraphQL/Types/NotificationTypeRegistrar.php
+++ api/app/GraphQL/Types/NotificationTypeRegistrar.php
@@ -4,6 +4,8 @@ namespace App\GraphQL\Types;
 
 use App\Discoverers\SubscriptionNotificationDiscoverer;
 use GraphQL\Type\Definition\EnumType;
+use GraphQL\Type\Definition\NamedType;
+use GraphQL\Type\Definition\Type;
 use Nuwave\Lighthouse\Schema\TypeRegistry;
 
 final class NotificationTypeRegistrar implements TypeRegistrarInterface
@@ -12,28 +14,21 @@ final class NotificationTypeRegistrar implements TypeRegistrarInterface
 
     public static function register(TypeRegistry $typeRegistry): void
     {
-        $notifications = SubscriptionNotificationDiscoverer::discover();
-        $values = [];
+        $typeRegistry->registerLazy(
+            'NotificationType',
+            function (): Type&NamedType {
+                $notifications = SubscriptionNotificationDiscoverer::discover();
+                $values = [];
 
-        foreach ($notifications as $notification) {
-            $name = class_basename($notification);
-            $values[$name] = ['value' => $name];
-        }
+                foreach ($notifications as $notification) {
+                    $name = class_basename($notification);
+                    $values[$name] = ['value' => $name];
+                }
 
-        $typeRegistry->register(
-            new EnumType([
-                'name' => 'NotificationType',
-                'values' => $values,
-            ])
-        );
-
-        $typeRegistry->register(
-            new EnumType([
-                'name' => 'DummyType',
-                'values' => [
-                    'one', 'two',
-                ],
-            ])
-        );
+                return new EnumType([
+                    'name' => 'NotificationType',
+                    'values' => $values,
+                ]);
+            });
     }
 }
