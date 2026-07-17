6cbc10533f89749327d51385bd4a0df8a793aa74
Vachan <vachanbolt@hotmail.com>
2026-07-17 13:18:04 -0600

    implement accordion

--- apps/web/src/pages/Users/AdminUserRecruitmentPage/components/RecruitmentTools.tsx
+++ apps/web/src/pages/Users/AdminUserRecruitmentPage/components/RecruitmentTools.tsx
@@ -1,7 +1,8 @@
 import { defineMessage, useIntl } from "react-intl";
 import WrenchScrewdriverIcon from "@heroicons/react/24/outline/WrenchScrewdriverIcon";
+import { useState } from "react";
 
-import { Heading, TableOfContents } from "@gc-digital-talent/ui";
+import { Accordion, TableOfContents } from "@gc-digital-talent/ui";
 import type { FragmentType } from "@gc-digital-talent/graphql";
 import { getFragment, graphql } from "@gc-digital-talent/graphql";
 
@@ -14,6 +15,8 @@ export const title = defineMessage({
 });
 
 export const RECRUITMENT_TOOLS_ID = "recruitment-tools";
+
+type AccordionItems = "special_application" | "";
 
 const RecruitmentTools_Fragment = graphql(/** GraphQL */ `
   fragment RecruitmentTools on User {
@@ -29,6 +32,8 @@ const RecruitmentTools = ({ query }: RecruitmentToolsProps) => {
   const intl = useIntl();
   const user = getFragment(RecruitmentTools_Fragment, query);
 
+  const [accordionOpen, setAccordionOpen] = useState<AccordionItems>("");
+
   return (
     <TableOfContents.Section id={RECRUITMENT_TOOLS_ID}>
       <TableOfContents.Heading
@@ -38,30 +43,36 @@ const RecruitmentTools = ({ query }: RecruitmentToolsProps) => {
       >
         {intl.formatMessage(title)}
       </TableOfContents.Heading>
-      <p className="my-6">
-        {intl.formatMessage({
-          defaultMessage:
-            "Users will normally apply to a recruitment process on their own. The following tools can be used to help create or manage applications in rare cases.",
-          id: "5LkwLx",
-          description: "Description of adding a user to a specific process",
-        })}
-      </p>
-      <Heading level="h3" size="h6">
-        {intl.formatMessage({
-          defaultMessage: "Add user to recruitment process",
-          id: "N7ACFf",
-          description: "Title of section to add users to processes",
-        })}
-      </Heading>
-      <p className="my-6">
-        {intl.formatMessage({
-          defaultMessage:
-            "This will include a user in a recruitment process, but it will not fill out any application information. This means that the application snapshot, as well as all other questions and skill justifications will be empty.",
-          id: "92M/Ie",
-          description: "Caveats of adding a user to a process manually",
-        })}
-      </p>
-      <CreateSpecialApplicationDialog query={user} />
+      <Accordion.Root
+        mode="simple"
+        type="single"
+        collapsible
+        value={accordionOpen}
+        onValueChange={(value: AccordionItems) => setAccordionOpen(value)}
+      >
+        <Accordion.Item value={"special_application"}>
+          <Accordion.Trigger as="h3">
+            {intl.formatMessage({
+              defaultMessage: "Create special application",
+              id: "FJDA+9",
+              description:
+                "Title for creating a special application for a user",
+            })}
+          </Accordion.Trigger>
+          <Accordion.Content>
+            <p className="my-6">
+              {intl.formatMessage({
+                defaultMessage:
+                  "This will create a draft application for a user in a recruitment process. This special application will ignore the normal deadline and employee verification requirements.",
+                id: "axxGXe",
+                description:
+                  "Description of creating a special application for a user",
+              })}
+            </p>
+            <CreateSpecialApplicationDialog query={user} />
+          </Accordion.Content>
+        </Accordion.Item>
+      </Accordion.Root>
     </TableOfContents.Section>
   );
 };
