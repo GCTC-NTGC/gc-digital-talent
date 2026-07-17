e7da9d9fde39204e72eae6c4cdf1ce598973a6bf
Vachan <vachanbolt@hotmail.com>
2026-07-17 13:02:47 -0600

    Delete "rules"

--- apps/web/src/pages/Users/AdminUserRecruitmentPage/components/CreateSpecialApplicationDialog.tsx
+++ apps/web/src/pages/Users/AdminUserRecruitmentPage/components/CreateSpecialApplicationDialog.tsx
@@ -235,8 +235,8 @@ const CreateSpecialApplicationDialog = ({
             <Notice.Content>
               {intl.formatMessage({
                 defaultMessage:
-                  "Only perform this action after having confirmed the user’s identity and verified that adding them to this process is in compliance with HR policy rules.",
-                id: "/wMGW+",
+                  "Only perform this action after having confirmed the user’s identity and verified that adding them to this process is in compliance with HR policy.",
+                id: "G8+gPB",
                 description:
                   "Warning about adding a user to a process manually",
               })}
