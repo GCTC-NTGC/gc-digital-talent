e73d44b640a7b0fd49e951ce5aa466448c94ef45
Peter Giles <8978655+petertgiles@users.noreply.github.com>
2026-07-20 16:02:28 -0400

    token service

--- api/app/Providers/BearerTokenServiceProvider.php
+++ api/app/Providers/BearerTokenServiceProvider.php
@@ -4,7 +4,6 @@ namespace App\Providers;
 
 use App\Contracts\BearerTokenService;
 use App\Services\CanadaLoginBearerTokenService;
-use App\Services\SignInCanadaBearerTokenService;
 use App\Services\TestBearerTokenService;
 use Illuminate\Support\ServiceProvider;
 use Psr\Clock\ClockInterface;
@@ -19,20 +18,11 @@ class BearerTokenServiceProvider extends ServiceProvider
         $this->app->singleton(BearerTokenService::class, function () {
             $clock = $this->app->make(ClockInterface::class);
 
-            // for Sign In Canada, use the older service
-            if (config('oauth.server_root') == 'https://te-auth.id.tbs-sct.gc.ca/oxauth' || config('oauth.server_root') == 'https://auth.id.canada.ca/oxauth') {
-                $realService = new SignInCanadaBearerTokenService(
-                    config('oauth.server_root').'/.well-known/openid-configuration',
-                    $clock,
-                    config('oauth.allowable_clock_skew')
-                );
-            } else {
-                $realService = new CanadaLoginBearerTokenService(
-                    config('oauth.server_root').'/.well-known/openid-configuration',
-                    $clock,
-                    config('oauth.allowable_clock_skew')
-                );
-            }
+            $realService = new CanadaLoginBearerTokenService(
+                config('oauth.server_root').'/.well-known/openid-configuration',
+                $clock,
+                config('oauth.allowable_clock_skew')
+            );
 
             // When TESTING_TOKEN_ENABLED=true and APP_ENV_VERTICAL != production, wrap the
             // real service so test tokens are validated locally while real GCKey tokens
