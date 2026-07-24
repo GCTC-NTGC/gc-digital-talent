2b26b11fb4990fdefaf562013c99860fa7e1f792
Yoni K <yonikid15@gmail.com>
2026-07-24 16:29:02 -0400

    Fix lint error

--- apps/web/src/types/talentRequestForm.ts
+++ apps/web/src/types/talentRequestForm.ts
@@ -44,6 +44,5 @@ export interface BrowserHistoryState {
 
 export type PartialApplicantFilter = Omit<ApplicantFilter, "pools"> & {
   pools?:
-    | (Omit<Pool, "activities" | "teamId" | "wasClosedEarly"> | null)[]
-    | null;
+    (Omit<Pool, "activities" | "teamId" | "wasClosedEarly"> | null)[] | null;
 };
