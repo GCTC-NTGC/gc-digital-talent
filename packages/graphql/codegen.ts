7a202bdb0441c335dc63ab41d0423a5817dcac20
Eric Sizer <eric.sizer@gmail.com>
2026-07-24 09:41:27 -0400

    support re-exporting utility types

--- packages/graphql/codegen.ts
+++ packages/graphql/codegen.ts
@@ -1,5 +1,14 @@
 import { type CodegenConfig } from "@graphql-codegen/cli";
 
+const scalars = {
+  Date: "string",
+  DateTime: "string",
+  Email: "string",
+  PhoneNumber: "string",
+  KeyString: "string",
+  UUID: "string",
+};
+
 const config: CodegenConfig = {
   schema: "../../api/storage/app/lighthouse-schema.graphql",
   documents: [
@@ -15,19 +24,20 @@ const config: CodegenConfig = {
     "./src/gql/": {
       preset: "client",
       config: {
-        scalars: {
-          Date: "string",
-          DateTime: "string",
-          Email: "string",
-          PhoneNumber: "string",
-          KeyString: "string",
-          UUID: "string",
-        },
+        scalars,
       },
       presetConfig: {
         fragmentMasking: {
           unmaskFunctionName: "getFragment",
         },
+      },
+    },
+    // Schema types for code outside of a GraphQL query, where a fragment can't
+    // be used. Re-export the ones you need from src/index.ts.
+    "./src/gql/schema-types.ts": {
+      plugins: ["typescript"],
+      config: {
+        scalars,
       },
     },
   },
