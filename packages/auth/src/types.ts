298e3a17624ebe3ddd62be21597fe310ffa94085
Eric Sizer <eric.sizer@gmail.com>
2026-07-28 10:55:50 -0400

    add new types

--- packages/auth/src/types.ts
+++ packages/auth/src/types.ts
@@ -1,5 +1,31 @@
+import type { LocalizedString, Permission } from "@gc-digital-talent/graphql";
+
 export interface AuthenticationState {
   loggedIn: boolean;
   logout: (postLogoutUri?: string) => void;
   refreshTokenSet: () => Promise<void>;
+}
+
+export interface AuthRole {
+  name: string;
+  isTeamBased?: boolean | null;
+  permissions?: Permission[] | null;
+  displayName?: LocalizedString | null;
+}
+
+export interface AuthTeam {
+  id: string;
+  name?: string | null;
+}
+
+export interface AuthRoleAssignment {
+  id: string;
+  role?: AuthRole | null;
+  team?: AuthTeam | null;
+}
+
+export interface AuthUserInfo {
+  id: string;
+  deletedDate?: string | null;
+  roleAssignments?: (AuthRoleAssignment | null)[] | null;
 }
