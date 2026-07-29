ca7597cfaf19de282282cee44227b7e0ee9222b5
Peter Giles <8978655+petertgiles@users.noreply.github.com>
2026-07-29 12:44:42 -0400

    fill out layout

--- apps/web/src/pages/EmployeeProfile/EmployeeProfileLayout.tsx
+++ apps/web/src/pages/EmployeeProfile/EmployeeProfileLayout.tsx
@@ -1,7 +1,79 @@
+import { useIntl } from "react-intl";
 import { Outlet } from "react-router";
 
+import { navigationMessages } from "@gc-digital-talent/i18n";
+import { Container } from "@gc-digital-talent/ui";
+
+import Hero from "~/components/Hero";
+import SEO from "~/components/SEO/SEO";
+import useRoutes from "~/hooks/useRoutes";
+
 const Component = () => {
-  return <Outlet />;
+  const intl = useIntl();
+  const paths = useRoutes();
+
+  const title = intl.formatMessage({
+    defaultMessage: "Employee profile",
+    id: "4BWod5",
+    description: "Page title for a user's GC employee profile",
+  });
+
+  const subtitle = intl.formatMessage({
+    defaultMessage:
+      "Manage your government employee information, including career development preferences and work styles",
+    id: "ecHyZS",
+    description: "Description of the employee profile page",
+  });
+
+  const crumbs = [
+    {
+      label: intl.formatMessage(navigationMessages.applicantDashboard),
+      url: paths.applicantDashboard(),
+    },
+    {
+      label: intl.formatMessage({
+        defaultMessage: "Employee profile",
+        id: "QgE+ei",
+        description: "Short title for a user's GC employee profile",
+      }),
+      url: paths.employeeVerification(),
+    },
+  ];
+
+  const navTabs = [
+    {
+      label: intl.formatMessage({
+        defaultMessage: "Employee verification",
+        id: "ke6NUW",
+        description:
+          "Description of employee profile, employee verification tab",
+      }),
+      url: paths.employeeVerification(),
+    },
+    {
+      label: intl.formatMessage({
+        defaultMessage: "Career planning",
+        id: "fh6CuQ",
+        description: "Description of employee profile, career planning tab",
+      }),
+      url: paths.careerPlanning(),
+    },
+  ];
+
+  return (
+    <>
+      <SEO title={title} description={subtitle} />
+      <Hero
+        title={title}
+        subtitle={subtitle}
+        crumbs={crumbs}
+        navTabs={navTabs}
+      />
+      <Container className="my-18">
+        <Outlet />
+      </Container>
+    </>
+  );
 };
 
 Component.displayName = "EmployeeProfileLayout";
--- apps/web/src/pages/EmployeeProfile/EmployeeVerificationPage.tsx
+++ apps/web/src/pages/EmployeeProfile/EmployeeVerificationPage.tsx
@@ -7,7 +7,6 @@ import { commonMessages, navigationMessages } from "@gc-digital-talent/i18n";
 import type { FragmentType } from "@gc-digital-talent/graphql";
 import { getFragment, graphql } from "@gc-digital-talent/graphql";
 import {
-  Container,
   Heading,
   Link,
   Pending,
@@ -18,12 +17,10 @@ import {
 import { ROLE_NAME } from "@gc-digital-talent/auth";
 import { NotFoundError } from "@gc-digital-talent/helpers";
 
-import Hero from "~/components/Hero";
-import SEO from "~/components/SEO/SEO";
-import useRoutes from "~/hooks/useRoutes";
 import RequireAuth from "~/components/RequireAuth/RequireAuth";
 import profileMessages from "~/messages/profileMessages";
 import StatusItem from "~/components/StatusItem/StatusItem";
+import useRoutes from "~/hooks/useRoutes";
 
 import messages from "./messages";
 import EmployeeVerificationSection from "../../components/EmployeeVerificationSection/EmployeeVerificationSection";
@@ -94,30 +91,6 @@ export const EmployeeProfile = ({
     throw new NotFoundError();
   }
 
-  const pageTitle = intl.formatMessage({
-    defaultMessage: "GC employee profile",
-    id: "V2ML6q",
-    description: "Page title for a user's GC employee profile",
-  });
-
-  const subtitle = intl.formatMessage({
-    defaultMessage:
-      "Manage government employee information, including verification and career development preferences",
-    id: "S6fUdZ",
-    description: "Description of the employee profile page",
-  });
-
-  const crumbs = [
-    {
-      label: intl.formatMessage(navigationMessages.applicantDashboard),
-      url: paths.applicantDashboard(),
-    },
-    {
-      url: paths.employeeVerification(),
-      label: intl.formatMessage(navigationMessages.employeeProfileGC),
-    },
-  ];
-
   // for validation
   const careerDevelopment = getFragment(
     EmployeeProfileCareerDevelopment_Fragment,
@@ -169,157 +142,150 @@ export const EmployeeProfile = ({
 
   return (
     <>
-      <SEO title={pageTitle} description={subtitle} />
-      <Hero title={pageTitle} subtitle={subtitle} crumbs={crumbs} />
-      <Container className="my-18">
-        <TableOfContents.Wrapper>
-          <TableOfContents.Navigation>
-            <TableOfContents.List className="list-none">
-              <TableOfContents.ListItem>
-                <StatusItem
-                  asListItem={false}
-                  title={intl.formatMessage(
-                    commonMessages.employeeVerification,
-                  )}
-                  status={user.isVerifiedGovEmployee ? "success" : "optional"}
-                  scrollTo={SECTION_ID.EMPLOYEE_VERIFICATION}
-                  hiddenContextPrefix={intl.formatMessage(
-                    user.isVerifiedGovEmployee
-                      ? commonMessages.complete
-                      : commonMessages.optional,
-                  )}
-                />
-              </TableOfContents.ListItem>
-              <TableOfContents.ListItem>
-                <StatusItem
-                  asListItem={false}
-                  title={intl.formatMessage(commonMessages.careerPlanning)}
-                  status={overallStatus}
-                  scrollTo={SECTION_ID.CAREER_PLANNING}
-                  hiddenContextPrefix={intl.formatMessage(
-                    statusDescriptions[overallStatus],
-                  )}
-                />
-                <TableOfContents.List className="list-none pl-3">
-                  <TableOfContents.ListItem>
-                    <StatusItem
-                      asListItem={false}
-                      title={intl.formatMessage(messages.careerDevelopment)}
-                      status={careerDevelopmentStatus}
-                      scrollTo={SECTION_ID.CAREER_DEVELOPMENT}
-                      hiddenContextPrefix={intl.formatMessage(
-                        statusDescriptions[careerDevelopmentStatus],
-                      )}
-                    />
-                  </TableOfContents.ListItem>
-                  <TableOfContents.ListItem>
-                    <StatusItem
-                      asListItem={false}
-                      title={intl.formatMessage(messages.yourNextRole)}
-                      status={nextRoleStatus}
-                      scrollTo={SECTION_ID.NEXT_ROLE}
-                      hiddenContextPrefix={intl.formatMessage(
-                        statusDescriptions[nextRoleStatus],
-                      )}
-                    />
-                  </TableOfContents.ListItem>
-                  <TableOfContents.ListItem>
-                    <StatusItem
-                      asListItem={false}
-                      title={intl.formatMessage(messages.careerObjective)}
-                      status={careerObjectiveStatus}
-                      scrollTo={SECTION_ID.CAREER_OBJECTIVE}
-                      hiddenContextPrefix={intl.formatMessage(
-                        statusDescriptions[careerObjectiveStatus],
-                      )}
-                    />
-                  </TableOfContents.ListItem>
-                  <TableOfContents.ListItem>
-                    <StatusItem
-                      asListItem={false}
-                      title={intl.formatMessage(messages.goalsWorkStyle)}
-                      status={goalsWorkStyleStatus}
-                      scrollTo={SECTION_ID.GOALS_WORK_STYLE}
-                      hiddenContextPrefix={intl.formatMessage(
-                        statusDescriptions[goalsWorkStyleStatus],
-                      )}
-                    />
-                  </TableOfContents.ListItem>
-                </TableOfContents.List>
-              </TableOfContents.ListItem>
-            </TableOfContents.List>
-            <Separator space="sm" />
-            <div className="flex flex-col gap-y-3">
-              <Link href={paths.profile()}>
-                {intl.formatMessage(navigationMessages.applicantProfile)}
-              </Link>
-              <Link href={paths.accountSettings()}>
-                {intl.formatMessage(navigationMessages.accountSettings)}
-              </Link>
-            </div>
-          </TableOfContents.Navigation>
-          <TableOfContents.Content>
-            <div className="flex flex-col gap-y-18">
-              <TableOfContents.Section id={SECTION_ID.EMPLOYEE_VERIFICATION}>
-                <EmployeeVerificationSection
-                  userQuery={user}
-                  context="applicant"
-                />
-              </TableOfContents.Section>
-              <TableOfContents.Section id={SECTION_ID.CAREER_PLANNING}>
-                <Heading
-                  level="h2"
-                  icon={
-                    user.isVerifiedGovEmployee
-                      ? ChartBarSquareIcon
-                      : LockClosedIcon
-                  }
-                  {...(user.isVerifiedGovEmployee && { color: "primary" })}
-                  className="mt-0 font-normal sm:text-left"
-                >
-                  {intl.formatMessage(commonMessages.careerPlanning)}
-                </Heading>
-                <p>
-                  {intl.formatMessage({
-                    defaultMessage:
-                      "We'd like to learn more about the career path you'd like to follow. Providing information about preferences and aspirations will help talent managers make more informed decisions when you've been nominated for a promotion, lateral movement, or professional development opportunity.",
-                    id: "6KS1jD",
-                    description:
-                      "Lead-in text explaining the users career plan",
-                  })}
-                </p>
-              </TableOfContents.Section>
-              <TableOfContents.Section id={SECTION_ID.CAREER_DEVELOPMENT}>
-                <CareerDevelopmentSection
-                  employeeProfileQuery={user.employeeProfile}
-                  careerDevelopmentOptionsQuery={options}
-                  isVerifiedGovEmployee={!!user.isVerifiedGovEmployee}
-                />
-              </TableOfContents.Section>
-              <TableOfContents.Section id={SECTION_ID.NEXT_ROLE}>
-                <NextRoleSection
-                  employeeProfileQuery={user.employeeProfile}
-                  optionsQuery={options}
-                  isVerifiedGovEmployee={!!user.isVerifiedGovEmployee}
-                />
-              </TableOfContents.Section>
-              <TableOfContents.Section id={SECTION_ID.CAREER_OBJECTIVE}>
-                <CareerObjectiveSection
-                  employeeProfileQuery={user.employeeProfile}
-                  optionsQuery={options}
-                  isVerifiedGovEmployee={!!user.isVerifiedGovEmployee}
-                />
-              </TableOfContents.Section>
-              <TableOfContents.Section id={SECTION_ID.GOALS_WORK_STYLE}>
-                <GoalsWorkStyleSection
-                  employeeProfileQuery={user.employeeProfile}
-                  isVerifiedGovEmployee={!!user.isVerifiedGovEmployee}
-                />
-              </TableOfContents.Section>
-            </div>
-          </TableOfContents.Content>
-        </TableOfContents.Wrapper>
-      </Container>
+      <TableOfContents.Wrapper>
+        <TableOfContents.Navigation>
+          <TableOfContents.List className="list-none">
+            <TableOfContents.ListItem>
+              <StatusItem
+                asListItem={false}
+                title={intl.formatMessage(commonMessages.employeeVerification)}
+                status={user.isVerifiedGovEmployee ? "success" : "optional"}
+                scrollTo={SECTION_ID.EMPLOYEE_VERIFICATION}
+                hiddenContextPrefix={intl.formatMessage(
+                  user.isVerifiedGovEmployee
+                    ? commonMessages.complete
+                    : commonMessages.optional,
+                )}
+              />
+            </TableOfContents.ListItem>
+            <TableOfContents.ListItem>
+              <StatusItem
+                asListItem={false}
+                title={intl.formatMessage(commonMessages.careerPlanning)}
+                status={overallStatus}
+                scrollTo={SECTION_ID.CAREER_PLANNING}
+                hiddenContextPrefix={intl.formatMessage(
+                  statusDescriptions[overallStatus],
+                )}
+              />
+              <TableOfContents.List className="list-none pl-3">
+                <TableOfContents.ListItem>
+                  <StatusItem
+                    asListItem={false}
+                    title={intl.formatMessage(messages.careerDevelopment)}
+                    status={careerDevelopmentStatus}
+                    scrollTo={SECTION_ID.CAREER_DEVELOPMENT}
+                    hiddenContextPrefix={intl.formatMessage(
+                      statusDescriptions[careerDevelopmentStatus],
+                    )}
+                  />
+                </TableOfContents.ListItem>
+                <TableOfContents.ListItem>
+                  <StatusItem
+                    asListItem={false}
+                    title={intl.formatMessage(messages.yourNextRole)}
+                    status={nextRoleStatus}
+                    scrollTo={SECTION_ID.NEXT_ROLE}
+                    hiddenContextPrefix={intl.formatMessage(
+                      statusDescriptions[nextRoleStatus],
+                    )}
+                  />
+                </TableOfContents.ListItem>
+                <TableOfContents.ListItem>
+                  <StatusItem
+                    asListItem={false}
+                    title={intl.formatMessage(messages.careerObjective)}
+                    status={careerObjectiveStatus}
+                    scrollTo={SECTION_ID.CAREER_OBJECTIVE}
+                    hiddenContextPrefix={intl.formatMessage(
+                      statusDescriptions[careerObjectiveStatus],
+                    )}
+                  />
+                </TableOfContents.ListItem>
+                <TableOfContents.ListItem>
+                  <StatusItem
+                    asListItem={false}
+                    title={intl.formatMessage(messages.goalsWorkStyle)}
+                    status={goalsWorkStyleStatus}
+                    scrollTo={SECTION_ID.GOALS_WORK_STYLE}
+                    hiddenContextPrefix={intl.formatMessage(
+                      statusDescriptions[goalsWorkStyleStatus],
+                    )}
+                  />
+                </TableOfContents.ListItem>
+              </TableOfContents.List>
+            </TableOfContents.ListItem>
+          </TableOfContents.List>
+          <Separator space="sm" />
+          <div className="flex flex-col gap-y-3">
+            <Link href={paths.profile()}>
+              {intl.formatMessage(navigationMessages.applicantProfile)}
+            </Link>
+            <Link href={paths.accountSettings()}>
+              {intl.formatMessage(navigationMessages.accountSettings)}
+            </Link>
+          </div>
+        </TableOfContents.Navigation>
+        <TableOfContents.Content>
+          <div className="flex flex-col gap-y-18">
+            <TableOfContents.Section id={SECTION_ID.EMPLOYEE_VERIFICATION}>
+              <EmployeeVerificationSection
+                userQuery={user}
+                context="applicant"
+              />
+            </TableOfContents.Section>
+            <TableOfContents.Section id={SECTION_ID.CAREER_PLANNING}>
+              <Heading
+                level="h2"
+                icon={
+                  user.isVerifiedGovEmployee
+                    ? ChartBarSquareIcon
+                    : LockClosedIcon
+                }
+                {...(user.isVerifiedGovEmployee && { color: "primary" })}
+                className="mt-0 font-normal sm:text-left"
+              >
+                {intl.formatMessage(commonMessages.careerPlanning)}
+              </Heading>
+              <p>
+                {intl.formatMessage({
+                  defaultMessage:
+                    "We'd like to learn more about the career path you'd like to follow. Providing information about preferences and aspirations will help talent managers make more informed decisions when you've been nominated for a promotion, lateral movement, or professional development opportunity.",
+                  id: "6KS1jD",
+                  description: "Lead-in text explaining the users career plan",
+                })}
+              </p>
+            </TableOfContents.Section>
+            <TableOfContents.Section id={SECTION_ID.CAREER_DEVELOPMENT}>
+              <CareerDevelopmentSection
+                employeeProfileQuery={user.employeeProfile}
+                careerDevelopmentOptionsQuery={options}
+                isVerifiedGovEmployee={!!user.isVerifiedGovEmployee}
+              />
+            </TableOfContents.Section>
+            <TableOfContents.Section id={SECTION_ID.NEXT_ROLE}>
+              <NextRoleSection
+                employeeProfileQuery={user.employeeProfile}
+                optionsQuery={options}
+                isVerifiedGovEmployee={!!user.isVerifiedGovEmployee}
+              />
+            </TableOfContents.Section>
+            <TableOfContents.Section id={SECTION_ID.CAREER_OBJECTIVE}>
+              <CareerObjectiveSection
+                employeeProfileQuery={user.employeeProfile}
+                optionsQuery={options}
+                isVerifiedGovEmployee={!!user.isVerifiedGovEmployee}
+              />
+            </TableOfContents.Section>
+            <TableOfContents.Section id={SECTION_ID.GOALS_WORK_STYLE}>
+              <GoalsWorkStyleSection
+                employeeProfileQuery={user.employeeProfile}
+                isVerifiedGovEmployee={!!user.isVerifiedGovEmployee}
+              />
+            </TableOfContents.Section>
+          </div>
+        </TableOfContents.Content>
+      </TableOfContents.Wrapper>
     </>
   );
 };
