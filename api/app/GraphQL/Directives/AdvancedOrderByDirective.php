f7a5822b59a4d327cd3a28d8e525743eb7d605ba
Eric Sizer <eric.sizer@gmail.com>
2026-08-04 12:35:15 -0400

    paramterized json keys in directive

--- api/app/GraphQL/Directives/AdvancedOrderByDirective.php
+++ api/app/GraphQL/Directives/AdvancedOrderByDirective.php
@@ -16,6 +16,7 @@ use Illuminate\Database\Eloquent\Relations\HasOneOrMany;
 use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Database\Query\Builder as QueryBuilder;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Str;
 use Nuwave\Lighthouse\Schema\AST\DocumentAST;
 use Nuwave\Lighthouse\Schema\Directives\BaseDirective;
 use Nuwave\Lighthouse\Support\Contracts\ArgBuilderDirective;
@@ -102,7 +103,7 @@ class AdvancedOrderByDirective extends BaseDirective implements ArgBuilderDirect
             throw new UserError("Invalid column: {$baseColumn}");
         }
 
-        return $builder->getQuery()->getGrammar()->wrap($column);
+        return $this->wrapColumn($builder, $column);
     }
 
     /**
@@ -142,7 +143,7 @@ class AdvancedOrderByDirective extends BaseDirective implements ArgBuilderDirect
             throw new UserError('Relation type '.get_class($relation).' is not supported for sub-query sorting.');
         }
 
-        $safeColumn = $grammar->wrap("{$relatedTable}.{$column}");
+        $safeColumn = $this->wrapColumn($builder, $column, $relatedTable);
         $wrappedOwnerKey = $grammar->wrap("{$relatedTable}.{$ownerKey}");
         $wrappedForeignKey = $grammar->wrap("{$model->getTable()}.{$foreignKey}");
 
@@ -179,6 +180,31 @@ class AdvancedOrderByDirective extends BaseDirective implements ArgBuilderDirect
     protected function getBaseColumn(string $column): string
     {
         return str_contains($column, '->') ? explode('->', $column, 2)[0] : $column;
+    }
+
+    /**
+     * Wrap a column for use in raw SQL, binding any JSON key as a parameter.
+     */
+    protected function wrapColumn($builder, string $column, ?string $table = null): string
+    {
+        $query = $builder->getQuery();
+        $baseColumn = $this->getBaseColumn($column);
+        $qualifiedColumn = $table ? "{$table}.{$baseColumn}" : $baseColumn;
+        $wrappedColumn = $query->getGrammar()->wrap($qualifiedColumn);
+
+        if ($baseColumn === $column) {
+            return $wrappedColumn;
+        }
+
+        $jsonKey = Str::after($column, '->');
+
+        if (str_contains($jsonKey, '->')) {
+            throw new UserError("Only one JSON key is supported: {$column}");
+        }
+
+        $query->addBinding($jsonKey, 'order');
+
+        return "{$wrappedColumn}->>?::text";
     }
 
     /**
