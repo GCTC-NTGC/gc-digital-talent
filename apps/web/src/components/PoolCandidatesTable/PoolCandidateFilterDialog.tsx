6306c7320b3c36977037f69a77ed25a0650d5741
Eric Sizer <eric.sizer@gmail.com>
2026-08-04 11:08:46 -0400

    update magic number, improve comment

--- apps/web/src/components/PoolCandidatesTable/PoolCandidateFilterDialog.tsx
+++ apps/web/src/components/PoolCandidatesTable/PoolCandidateFilterDialog.tsx
@@ -236,8 +236,9 @@ const PoolCandidateFilterDialog = ({
     <FilterDialog<FormValues>
       options={{ defaultValues: initialValues }}
       // Remove hidden pools filters from count
+      // Default hard-coded filters: expiryStatus, suspendedStatus and poolId
       {...(hidePoolFilter && {
-        modifyFilterCount: -5,
+        modifyFilterCount: -3,
       })}
       {...{ resetValues, onSubmit }}
     >
