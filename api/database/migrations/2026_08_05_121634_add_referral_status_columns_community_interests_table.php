33e63e82f0bfa3d4d8de8f0f217fca028d43aa04
Eric Sizer <eric.sizer@gmail.com>
2026-08-05 08:27:51 -0400

    add migration for referral status columns

---
+++ api/database/migrations/2026_08_05_121634_add_referral_status_columns_community_interests_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class() extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('community_interests', function (Blueprint $table) {
+            $table->string('referral_status')->default('NEW');
+            $table->foreignUuid('community_classification_id')
+                ->nullable()
+                ->constrained(table: 'classifications')
+                ->nullOnDelete();
+            $table->date('follow_up_date')->nullable();
+            $table->text('referral_notes')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('community_interests', function (Blueprint $table) {
+            $table->dropColumn(['referral_status', 'follow_up_date', 'referral_notes']);
+            $table->dropConstrainedForeignId('community_classification_id');
+        });
+    }
+};
