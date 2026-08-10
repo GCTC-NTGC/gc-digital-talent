00073bfa7d8cafe20d377b9c3786d74e9101ea32
Brinda Sasikumar <Brinda.Sasikumar@gmail.com>
2026-08-10 12:39:50 -0400

    [Feature] Add PKCE to CanadaLogin auth flow

--- api/tests/Unit/AuthControllerTest.php
+++ api/tests/Unit/AuthControllerTest.php
@@ -21,6 +21,46 @@ class AuthControllerTest extends TestCase
         parent::setUp();
     }
 
+    public function testLoginSendsPkceChallengeAndStoresVerifier()
+    {
+        $response = $this->call('GET', '/login');
+
+        $location = $response->headers->get('Location');
+        parse_str(parse_url($location, PHP_URL_QUERY), $query);
+
+        $this->assertSame('S256', $query['code_challenge_method']);
+        $this->assertNotEmpty($query['code_challenge']);
+
+        $codeVerifier = $this->app['session']->get('code_verifier');
+        $this->assertNotEmpty($codeVerifier);
+        $this->assertSame($query['code_challenge'], rtrim(strtr(base64_encode(hash('sha256', $codeVerifier, true)), '+/', '-_'), '='));
+    }
+
+    public function testAuthCallbackSendsCodeVerifierToTokenEndpoint()
+    {
+        $this->seed(RolePermissionSeeder::class);
+
+        Http::fakeSequence()
+            ->push([
+                'id_token' => 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibm9uY2UiOiJhYmMiLCJzdGF0ZSI6ImFiYyIsImlhdCI6MTUxNjIzOTAyMn0.p4GMaQjmIcUAxjAZ7Y51C1q1mu5sVXJLdX1zybt4jFc',
+            ], 200)
+            ->whenEmpty(Http::response());
+
+        $this->withSession([
+            'state' => 'abc',
+            'nonce' => 'abc',
+            'code_verifier' => 'test-verifier',
+        ])->call('GET', '/auth-callback', [
+            'state' => 'abc',
+            'nonce' => 'abc',
+            'code' => 'code',
+        ]);
+
+        Http::assertSent(function ($request) {
+            return $request['code_verifier'] === 'test-verifier';
+        });
+    }
+
     public function testNewUserCreationOnLogin()
     {
         $this->seed(RolePermissionSeeder::class);
