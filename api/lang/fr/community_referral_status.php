5a85062cceac2a2031747879b0be51dbb9c87af4
Eric Sizer <eric.sizer@gmail.com>
2026-08-10 10:56:01 -0400

    fix translation key

--- api/lang/fr/community_referral_status.php
+++ api/lang/fr/community_referral_status.php
@@ -3,6 +3,6 @@
 return [
     'new' => 'Nouveau',
     'pending' => 'En attente',
-    'referring' => 'Disponible aux fins de recommandation',
+    'available_for_referral' => 'Disponible aux fins de recommandation',
     'not_referred' => 'Pas de recommandation',
 ];
