a6586f934e1640092760f71d2af92397b661e1e5
Eric Sizer <eric.sizer@gmail.com>
2026-08-10 10:03:15 -0400

    add translations

---
+++ api/lang/fr/community_referral_status.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'new' => 'Nouveau',
+    'pending' => 'En attente',
+    'referring' => 'Disponible aux fins de recommandation',
+    'not_referred' => 'Pas de recommandation',
+];
