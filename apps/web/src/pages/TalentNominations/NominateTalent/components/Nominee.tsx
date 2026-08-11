c77b3cde4e3510043be3a6f25ce9a16f611f5b6e
Matt Nigh <matt@mattnigh.com>
2026-08-11 14:33:26 -0400

    Update FR copy 3

--- apps/web/src/pages/TalentNominations/NominateTalent/components/Nominee.tsx
+++ apps/web/src/pages/TalentNominations/NominateTalent/components/Nominee.tsx
@@ -226,8 +226,8 @@ const Nominee = ({ nomineeQuery, optionsQuery }: NomineeProps) => {
       <p className="my-6">
         {intl.formatMessage({
           defaultMessage:
-            "Now that we know a little about who is submitting the nomination, let’s collect some information about the nominee. We'll start by checking if they have a GC Digital Talent account using their work email.",
-          id: "BG9koi",
+            "Now let’s collect some information about the nominee. We'll start by checking if they have a GC Digital Talent account using their work email.",
+          id: "sOEIhS",
           description: "Subtitle for nomination nominee step",
         })}
       </p>
