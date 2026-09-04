8b63a8dd9f8e92be1195c37bd631bafde0a65c36
Yoni K <yoni.kid15@gmail.com>
2026-09-04 10:37:11 -0400

    Rename french rod filename

--- api/lang/fr/filename.php
+++ api/lang/fr/filename.php
@@ -2,7 +2,7 @@
 
 return [
     'candidates' => 'candidats',
-    'candidates_rod' => 'évaluation_des_candidat(e)s',
+    'candidates_rod' => 'évaluation_des_candidats',
     'candidate' => 'candidat',
     'user' => 'utilisateur',
     'users' => 'utilisateurs',
