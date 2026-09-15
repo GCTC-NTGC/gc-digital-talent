622b07990778ba6a8d464fa7fccc0e23b63642a5
mauli0110 <maulisonitbs01@gmail.com>
2026-09-15 12:11:14 -0400

    fix_azure_failing_delete_pool

--- apps/playwright/tests/admin/process-action-delete.spec.ts
+++ apps/playwright/tests/admin/process-action-delete.spec.ts
@@ -1,21 +1,33 @@
 import { test, expect } from "~/fixtures";
 import { loginBySub } from "~/utils/auth";
 import graphql from "~/utils/graphql";
-import { createPool } from "~/utils/pools";
+import { generateUniqueTestId } from "~/utils/id";
+import { createPool, updatePool } from "~/utils/pools";
 import { me } from "~/utils/user";
 
 test("Delete pool", { tag: "@uat" }, async ({ appPage }) => {
-  const platformAdminCtx = await graphql.newContext();
-  const platformAdminSub =
+  const communityAdminSub =
     process.env.PLAYWRIGHT_COMMUNITY_ADMIN_SUB ?? "community@test.com";
+  const communityAdminCtx = await graphql.newContext(communityAdminSub);
+  const user = await me(communityAdminCtx, {});
+  const uniqueTestId = generateUniqueTestId();
+  const poolName = `pool auth test ${uniqueTestId}`;
 
-  const user = await me(platformAdminCtx, {});
-
-  const createdPool = await createPool(platformAdminCtx, {
+  const createdPool = await createPool(communityAdminCtx, {
     userId: user.id,
   });
 
-  await loginBySub(appPage.page, platformAdminSub);
+  await updatePool(communityAdminCtx, {
+    poolId: createdPool.id,
+    pool: {
+      name: {
+        en: poolName,
+        fr: poolName,
+      },
+    },
+  });
+
+  await loginBySub(appPage.page, communityAdminSub);
   await appPage.page.goto(`/en/admin/pools/${createdPool.id}`);
   await appPage.waitForGraphqlResponse("ViewPoolPage");
 
