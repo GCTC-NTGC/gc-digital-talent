b029c40683db44a4b71aa30c5a2f59ae957648cd
Matt Nigh <matt@mattnigh.com>
2026-09-18 08:29:26 -0400

    Update spacing

--- api/database/migrations/2026_09_15_000000_add_is_hidden.php
+++ api/database/migrations/2026_09_15_000000_add_is_hidden.php
@@ -17,7 +17,7 @@ return new class() extends Migration
     public function down(): void
     {
         Schema::table('pools', function (Blueprint $table) {
-           $table->dropColumn('is_hidden');
+            $table->dropColumn('is_hidden');
         });
     }
 };
