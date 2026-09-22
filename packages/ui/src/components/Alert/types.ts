fcd08b7ae25ac3f5c28267ca0ef791f6644f5cb8
Matt Nigh <matt@mattnigh.com>
2026-09-22 10:07:06 -0400

    Replace AlertHeadingLevel with AlertHeadingRank

--- packages/ui/src/components/Alert/types.ts
+++ packages/ui/src/components/Alert/types.ts
@@ -2,4 +2,4 @@ import type { HeadingRank } from "../../types";
 
 export type AlertType = "success" | "warning" | "info" | "error";
 
-export type AlertHeadingLevel = HeadingRank | "p";
+export type AlertHeadingRank = HeadingRank | "p";
