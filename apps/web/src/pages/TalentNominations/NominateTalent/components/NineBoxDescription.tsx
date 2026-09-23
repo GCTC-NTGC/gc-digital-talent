7800411131e434a32cd8c243ba4c0f4d5555864d
Chantelle <mrspasceri@hotmail.com>
2026-09-23 10:08:35 -0700

    update low-high

--- apps/web/src/pages/TalentNominations/NominateTalent/components/NineBoxDescription.tsx
+++ apps/web/src/pages/TalentNominations/NominateTalent/components/NineBoxDescription.tsx
@@ -47,8 +47,8 @@ const messages: Record<
     [NineBoxRating.High]: {
       title: defineMessage({
         defaultMessage:
-          "We recommend this person be nominated for lateral movement",
-        id: "QwoLNe",
+          "This person’s nomination is recommended for lateral movement",
+        id: "UOvfEq",
         description:
           "Nine-box recommendation title for development and lateral",
       }),
