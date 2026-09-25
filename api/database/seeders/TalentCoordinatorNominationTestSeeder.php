4e5da794a02f7e4197d73d8ac7be41ca222866e6
Chantelle <mrspasceri@hotmail.com>
2026-09-25 06:58:43 -0700

    add a few seeded nominations

--- api/database/seeders/TalentCoordinatorNominationTestSeeder.php
+++ api/database/seeders/TalentCoordinatorNominationTestSeeder.php
@@ -3,7 +3,10 @@
 namespace Database\Seeders;
 
 use App\Models\Community;
+use App\Models\TalentNomination;
 use App\Models\TalentNominationEvent;
+use App\Models\User;
+use Faker\Factory as Faker;
 use Illuminate\Database\Seeder;
 
 class TalentCoordinatorNominationTestSeeder extends Seeder
@@ -15,20 +18,45 @@ class TalentCoordinatorNominationTestSeeder extends Seeder
      */
     public function run()
     {
+        $faker = Faker::create();
         $digital = Community::where('key', 'digital')->first();
+        $coordinator = User::where('sub', 'talent-coordinator@test.com')->sole();
 
-        TalentNominationEvent::factory()
+        $event = TalentNominationEvent::factory()
             ->withDevelopmentPrograms()
             ->create([
                 'name' => [
                     'en' => 'Test Talent Coordinator Event active EN',
                     'fr' => 'Test Talent Coordinator Event active FR',
                 ],
-                'community_id' => $digital->id,
-                'open_date' => now()->subMonth(),
-                'close_date' => now()->addMonths(2),
+                'community_id' => $digital,
+                'open_date' => $faker->dateTimeBetween('-1 year', 'now'),
+                'close_date' => $faker->dateTimeBetween('+1 year', '+2 years'),
                 'include_leadership_competencies' => true,
                 'include_nine_box' => true,
             ]);
+
+        // Seed a few nominations with a mix of advancement, lateral movement, and development.
+        $selectionNominees = User::whereIsVerifiedGovEmployee()
+            ->whereKeyNot($coordinator->id)
+            ->inRandomOrder()
+            ->take(10)
+            ->get();
+
+        foreach ($selectionNominees as $index => $nominee) {
+            $nominateForAdvancement = $index % 3 === 0;
+            $nominateForLateralMovement = $index % 3 === 1;
+            $nominateForDevelopmentPrograms = $index % 3 === 2;
+
+            TalentNomination::factory()
+                ->submittedReviewAndSubmit()
+                ->create([
+                    'talent_nomination_event_id' => $event->id,
+                    'nominee_id' => $nominee->id,
+                    'nominate_for_advancement' => $nominateForAdvancement,
+                    'nominate_for_lateral_movement' => $nominateForLateralMovement,
+                    'nominate_for_development_programs' => $nominateForDevelopmentPrograms,
+                ]);
+        }
     }
 }
